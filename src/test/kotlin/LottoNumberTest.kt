4d51cd065c637a6f8f536fdd0cb6d109a3596379
lowoon <sangbo0229@gmail.com>
2020-08-01 16:13:42 +0900

    feat: lotto number

---
+++ src/test/kotlin/LottoNumberTest.kt
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class LottoNumberTest {
+    @Test
+    internal fun `로또 숫자 조회`() {
+        val lottoNumber: LottoNumber = LottoNumber.of(1)
+
+        assertThat(lottoNumber).isEqualTo(LottoNumber(1))
+    }
+}
