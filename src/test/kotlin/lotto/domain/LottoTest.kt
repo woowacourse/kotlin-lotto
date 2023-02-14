00d12b95a5423d534e98e276d569a221ed04c115
changhwan <chang5hustle@gmail.com>
2023-02-14 20:19:21 +0900

    feat(racingcar): 로또 숫자는 중복되면 안된다. 기능 구현 및 테스트

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -6,15 +6,21 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 class LottoTest {
-    @MethodSource("provideLottoNumbers")
+    @MethodSource("provideWrongAmountLottoNumbers")
     @ParameterizedTest
     fun `로또는 LottoNumber 6개로 이루어진 리스트를 가지고있다`(lottoNumbers: List<LottoNumber>) {
         assertThrows<IllegalArgumentException> { Lotto(lottoNumbers) }
     }
 
+    @MethodSource("provideDuplicateLottoNumbers")
+    @ParameterizedTest
+    fun `로또 숫자는 중복되면 안된다`(lottoNumbers: List<LottoNumber>) {
+        assertThrows<IllegalArgumentException> { Lotto(lottoNumbers) }
+    }
+
     companion object {
         @JvmStatic
-        fun provideLottoNumbers() = listOf(
+        fun provideWrongAmountLottoNumbers() = listOf(
             Arguments.of(
                 listOf(
                     LottoNumber(4),
@@ -34,6 +40,30 @@ class LottoTest {
                 ),
             ),
             Arguments.of(listOf<LottoNumber>()),
+        )
+
+        @JvmStatic
+        fun provideDuplicateLottoNumbers() = listOf(
+            Arguments.of(
+                listOf(
+                    LottoNumber(4),
+                    LottoNumber(3),
+                    LottoNumber(12),
+                    LottoNumber(11),
+                    LottoNumber(11),
+                    LottoNumber(12),
+                ),
+            ),
+            Arguments.of(
+                listOf(
+                    LottoNumber(4),
+                    LottoNumber(3),
+                    LottoNumber(12),
+                    LottoNumber(12),
+                    LottoNumber(35),
+                    LottoNumber(35),
+                ),
+            ),
         )
     }
 }
