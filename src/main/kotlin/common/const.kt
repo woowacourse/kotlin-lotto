8c50219e688e37350805eb0caf65a1fb3e8214f6
krrong <khk9664@naver.com>
2023-02-14 17:57:25 +0900

    refactor(Lotto): 매직넘버 상수화, 프로퍼티 getter 재지정

---
+++ src/main/kotlin/common/const.kt
@@ -0,0 +1,4 @@
+package common
+
+const val MINIMUM_LOTTO_RANGE = 1
+const val MAXIMUM_LOTTO_RANGE = 45
--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,8 +1,20 @@
 package domain
 
-class Lotto(val numbers: Set<Int>) {
+import common.MAXIMUM_LOTTO_RANGE
+import common.MINIMUM_LOTTO_RANGE
+
+class Lotto(private val _numbers: Set<Int>) {
+    val numbers: Set<Int>
+        get() = _numbers
+
     init {
-        require(numbers.size == 6) { "로또 번호는 6개여야 합니다." }
-        require(numbers.all { number -> number in 1..45 } ) { "로또 번호는 1이상 45이하여야 합니다." }
+        require(_numbers.size == LOTTO_SIZE) { ERROR_LOTTO_SIZE }
+        require(_numbers.all { number -> number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE } ) { ERROR_LOTTO_RANGE }
+    }
+
+    companion object{
+        private const val LOTTO_SIZE = 6
+        private const val ERROR_LOTTO_SIZE = "로또 번호는 6개여야 합니다."
+        private const val ERROR_LOTTO_RANGE = "로또 번호는 1이상 45이하여야 합니다."
     }
 }
