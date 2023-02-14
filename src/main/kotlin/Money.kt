3ba8248b54c4ffa68b0d0e33e26f24d8ebe302d7
hyunji1203 <hj27720@gmail.com>
2023-02-14 16:17:31 +0900

    feat(money): 입력된 금액이 null인 경우 예외 처리 추가

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -1,1 +1,10 @@
-class Money(val price: Int) {}
+class Money(val price: Int?) {
+
+    init {
+        require(price != null) { INPUT_MONEY_NULL_ERROR_MESSAGE}
+    }
+
+    companion object {
+        const val INPUT_MONEY_NULL_ERROR_MESSAGE = "금액이 입력되지 않았습니다."
+    }
+}
