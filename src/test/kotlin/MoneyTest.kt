a7308692fa6bbacaf8264dae203720c68cd3d85f
hyunji1203 <hj27720@gmail.com>
2023-02-14 16:17:05 +0900

    test(MoneyTest): 입력된 금액이 null인 경우 예외 처리 테스트 추가

--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -1,5 +1,6 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class MoneyTest {
 
@@ -7,6 +8,13 @@ class MoneyTest {
     fun `입력받은 금액 확인`() {
         val money = Money(1000)
         assertThat(money.price).isEqualTo(1000)
+    }
+
+    @Test
+    fun `입력된 금액이 null 일 때`(){
+        assertThrows<IllegalArgumentException> {
+            Money(null)
+        }
     }
 
 }
