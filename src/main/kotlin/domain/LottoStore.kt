dd48bcf84e243548d3e4da7c8240e6245154c8f1
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-14 17:21:59 +0900

    feat(LottoStore) : 입력 받은 금액에 맞는 구매 개수만큼 로또를 반환한다.

---
+++ src/main/kotlin/domain/LottoStore.kt
@@ -0,0 +1,19 @@
+package domain
+
+class LottoStore {
+
+    fun buyLotto(amount: Int): List<Lotto> {
+        require(amount in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) { ERROR_CREATE_COUNT.format(amount) }
+        val lottoFactory = LottoFactory(RandomNumberGenerator())
+        return lottoFactory.create(getCount(amount))
+    }
+
+    private fun getCount(amount: Int) = amount / LOTTO_PRICE
+
+    companion object {
+        private const val MINIMUM_AMOUNT = 1000
+        private const val MAXIMUM_AMOUNT = 100000
+        private const val LOTTO_PRICE = 1000
+        private const val ERROR_CREATE_COUNT = "구매 할 수 있는 금액은 1000원 이상 100000원 이하입니다.\n잘못된 값: %d"
+    }
+}
