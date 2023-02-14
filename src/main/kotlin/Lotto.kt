2a8b838965641951cb24a62613b51532db189980
hyunji1203 <hj27720@gmail.com>
2023-02-14 16:53:02 +0900

    feat(Lotto): 로또 당첨번호가 null일 경우 예외 처리 추가

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -1,2 +1,10 @@
-class Lotto(val numbers: List<Int>) {
+class Lotto(val numbers: List<Int>?) {
+
+    init {
+        require( numbers != null) { INPUT_LOTTO_NULL_ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val INPUT_LOTTO_NULL_ERROR_MESSAGE = "당첨 번호가 입력되지 않았습니다."
+    }
 }
