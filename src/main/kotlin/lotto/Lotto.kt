ba5b882827d18d1bbc6d5d783750275693ee9d4a
whk06061 <gju06061@naver.com>
2023-02-14 16:18:50 +0900

    feat: 로또는 1~45 사이의 중복없는 값을 가진다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,9 @@
 package lotto
 
-class Lotto() {
-    val numbers: List<Int> = listOf(0, 0, 0, 0, 0, 0)
+class Lotto(val numbers: List<Int>) {
+    init {
+        require(numbers.size == 6)
+        require(numbers.all { it in 1..45 })
+        require(numbers.distinct().size == 6)
+    }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,12 +1,40 @@
 package lotto
 
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoTest {
     @Test
-    fun `로또는 번호 여섯 개를 갖는다`() {
-        val lotto = Lotto()
-        assertThat(lotto.numbers.size).isEqualTo(6)
+    fun `로또는 번호 여섯 개를 가져야 한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @MethodSource("numbers")
+    @ParameterizedTest
+    fun `로또는 1~45 사이의 숫자를 가져야 한다`(numbers: List<Int>) {
+        assertThrows<IllegalArgumentException> {
+            Lotto(numbers)
+        }
+    }
+
+    @Test
+    fun `로또는 중복 되지 않는 숫자를 가져야 한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(4, 3, 1, 2, 2, 2))
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun numbers(): List<Arguments> {
+            return listOf(
+                Arguments.of(listOf(0, 1, 2, 3, 4, 5), listOf(41, 42, 43, 44, 45, 46))
+            )
+        }
     }
 }
