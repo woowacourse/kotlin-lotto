41be45dbf1162dfd66da75f4c597877104f5d335
Sujin Kim <sujinkim919@gmail.com>
2023-02-14 16:27:21 +0900

    feat: 가격으로부터 로또 개수 구하기

---
+++ src/main/kotlin/lotto/LottoController.kt
@@ -0,0 +1,12 @@
+package lotto
+
+class LottoController {
+
+    fun getNumberOfLotto(money: Int): Int {
+        return money / LOTTO_PRICE
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
+    }
+}
---
+++ src/test/kotlin/lotto/LottoControllerTest.kt
@@ -0,0 +1,13 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoControllerTest {
+
+    @Test
+    fun `금액을 통해 몇 장의 로또가 발급되는지 구한다`() {
+        val money = 14000
+        assertThat(LottoController().getNumberOfLotto(money)).isEqualTo(14)
+    }
+}
