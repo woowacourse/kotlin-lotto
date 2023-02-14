ef62c4880e3d82917ea60f42966e0d25271175cb
hyunji1203 <hj27720@gmail.com>
2023-02-14 16:23:52 +0900

    feat(Money): 입력된 금액이 음수인 경우 예외 처리 추가

--- src/main/kotlin/Money.kt
+++ src/main/kotlin/Money.kt
@@ -1,10 +1,12 @@
 class Money(val price: Int?) {
 
     init {
-        require(price != null) { INPUT_MONEY_NULL_ERROR_MESSAGE}
+        require(price != null) { INPUT_MONEY_NULL_ERROR_MESSAGE }
+        require(price >= 0) { INPUT_MONEY_NEGATIVE_ERROR_MESSAGE }
     }
 
     companion object {
         const val INPUT_MONEY_NULL_ERROR_MESSAGE = "금액이 입력되지 않았습니다."
+        const val INPUT_MONEY_NEGATIVE_ERROR_MESSAGE = "금액은 양수여야 합니다."
     }
 }
