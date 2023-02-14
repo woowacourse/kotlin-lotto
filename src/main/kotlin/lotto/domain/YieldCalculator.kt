3fdb0c830fed36e4afbb238b7adbbd83eea2c867
whk06061 <gju06061@naver.com>
2023-02-14 17:46:40 +0900

    feat: 수익률을 구한다.

---
+++ src/main/kotlin/lotto/domain/YieldCalculator.kt
@@ -0,0 +1,17 @@
+package lotto.domain
+
+import kotlin.math.floor
+import lotto.constants.Rank
+
+class YieldCalculator {
+    fun calculateYield(lottoCount: Int, ranks: List<Rank>): Double {
+        val yield = calculatePrize(ranks).toDouble() / (lottoCount * 1000)
+        return floor(yield * 100.0) / 100.0
+    }
+
+    private fun calculatePrize(ranks: List<Rank>): Int {
+        var prize = 0
+        ranks.forEach { rank -> prize += rank.winningMoney }
+        return prize
+    }
+}
