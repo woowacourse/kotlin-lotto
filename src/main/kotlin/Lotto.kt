10a06b8120e00a684d8b73775108b00c9a27e627
hyunji1203 <hj27720@gmail.com>
2023-02-14 17:11:40 +0900

    feat(Lotto): 당첨 로또에 중복된 번호가 존재하는 경우 예외처리 추가

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -3,10 +3,12 @@ class Lotto(val numbers: List<Int>?) {
     init {
         require( numbers != null) { INPUT_LOTTO_NULL_ERROR_MESSAGE }
         require( numbers.size == 6 ) { INPUT_LOTTO_SIZE_ERROR_MESSAGE }
+        require( numbers.toSet().size == 6 ) { INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE }
     }
 
     companion object {
         const val INPUT_LOTTO_NULL_ERROR_MESSAGE = "당첨 번호가 입력되지 않았습니다."
         const val INPUT_LOTTO_SIZE_ERROR_MESSAGE = "당첨 번호가 6개가 아닙니다"
+        const val INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE = "당첨 번호가 중복되었습니다."
     }
 }
