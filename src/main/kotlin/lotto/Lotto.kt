9b81ad7d4530ffeae0823dbf101c7f66edf900aa
Sujin Kim <sujinkim919@gmail.com>
2023-02-14 17:39:42 +0900

    feat: 보너스 넘버에 따라 Rank를 구하는 기능을 포함하도록 추가한다.

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,8 +3,14 @@ package lotto
 class Lotto(val lotto: List<Int>) {
     fun getCountOfMatch(winningLotto: WinningLotto): Rank {
         val count = lotto.count { winningLotto.winningNumbers.lotto.contains(it) }
+        if (count == Rank.SECOND.countOfMatch) {
+            return getRankByBonusNumber(winningLotto)
+        }
         return Rank.values().find {
             it.countOfMatch == count
         } ?: Rank.MISS
     }
+
+    private fun getRankByBonusNumber(winningLotto: WinningLotto) =
+        if (lotto.contains(winningLotto.bonusNumber)) Rank.SECOND else Rank.THIRD
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -7,11 +7,10 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class LottoTest {
-
     @ParameterizedTest
     @MethodSource("matchLottoNumbers")
     fun `일치하는 번호 개수에 따라 Rank를 구한다`(numbers: List<Int>, result: Rank) {
-        val winning = WinningLotto(listOf(1,2,3,4,5,6), 7)
+        val winning = WinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
         assertThat(
             Lotto(numbers).getCountOfMatch(winning)
         ).isEqualTo(result)
@@ -26,6 +25,12 @@ class LottoTest {
                 ),
                 Arguments.of(
                     listOf(1, 2, 3, 4, 5, 6), Rank.FIRST
+                ),
+                Arguments.of(
+                    listOf(1, 2, 3, 4, 5, 9), Rank.THIRD
+                ),
+                Arguments.of(
+                    listOf(1, 2, 3, 4, 5, 7), Rank.SECOND
                 )
             )
         }
