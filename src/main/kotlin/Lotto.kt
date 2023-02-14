b0a3fe013a93c064643226fd328c46bc5141c54f
EmilyCh0 <emily9803@ewhain.net>
2023-02-14 16:32:11 +0900

    feat: 로또 번호는 1 이상 45 이하인지 검증

---
+++ src/main/kotlin/Lotto.kt
@@ -0,0 +1,15 @@
+class Lotto(numbers: List<Int>) {
+    init {
+        require(
+            numbers.all { number ->
+                number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
+            }
+        ) { NUMBER_RANGE_ERROR }
+    }
+
+    companion object {
+        private const val MIN_LOTTO_NUMBER = 1
+        private const val MAX_LOTTO_NUMBER = 45
+        private const val NUMBER_RANGE_ERROR = "[ERROR] 번호는 1 이상 45 이하입니다."
+    }
+}
---
+++ src/test/kotlin/LottoTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+class LottoTest {
+    @Test
+    fun `로또 번호는 1 이상 45 이하가 아닌 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 50))
+        }
+    }
+
+    @Test
+    fun `로또 번호는 1 이상 45 이하인 경우`() {
+        assertDoesNotThrow {
+            Lotto(listOf(1, 45, 10, 11, 12, 13))
+        }
+    }
+}
