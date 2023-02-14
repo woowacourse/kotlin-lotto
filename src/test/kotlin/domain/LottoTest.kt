82e19f6174d8a9135e60e2568ec3624408c2023d
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-14 16:39:48 +0900

    fix(Lotto) : 같은 list<> 인자로 인해 JVM 변환 발생으로 오류 수정

--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -9,14 +9,14 @@ class LottoTest {
     @Test
     fun `숫자가 6개인 로또 번호를 뽑는다`() {
         assertDoesNotThrow {
-            Lotto(listOf(1, 24, 34, 35, 36, 38))
+            Lotto.create(listOf(1, 24, 34, 35, 36, 38))
         }
     }
 
     @Test
     fun `숫자가 6개가 아니면 에러가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 24, 34, 35, 37, 45, 22))
+            Lotto.create(listOf(1, 24, 34, 35, 37, 45, 22))
         }
     }
 }
