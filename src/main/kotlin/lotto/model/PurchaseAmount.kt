0a9156275c63a2085deafdb59f2bc3dfd93e51a3
otter <a33397397@gmail.com>
2023-02-14 19:06:51 +0900

    feat: 구입금액이 1000원 이상이 아니고 5만원을 초과하면 에러가 발생하는 기능 구현

--- src/main/kotlin/lotto/model/PurchaseAmount.kt
+++ src/main/kotlin/lotto/model/PurchaseAmount.kt
@@ -1,5 +1,19 @@
 package lotto.model
 
+import java.lang.IllegalArgumentException
+
 class PurchaseAmount(
     val amount: Int
-)
+) {
+    init {
+        require(amount in PURCHASE_AMOUNT_LOWER_BOUNDARY..PURCHASE_AMOUNT_UPPER_BOUNDARY) {
+            PURCHASE_AMOUNT_BOUNDARY_ERROR_MESSAGE
+        }
+    }
+
+    companion object {
+        private const val PURCHASE_AMOUNT_BOUNDARY_ERROR_MESSAGE = "구입금액은 1000원 이상 5만원 이하여야 합니다."
+        private const val PURCHASE_AMOUNT_LOWER_BOUNDARY = 1000
+        private const val PURCHASE_AMOUNT_UPPER_BOUNDARY = 50000
+    }
+}
--- src/test/kotlin/lotto/model/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/model/PurchaseAmountTest.kt
@@ -1,6 +1,9 @@
 package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -11,4 +14,21 @@ class PurchaseAmountTest {
         val purchaseAmount = PurchaseAmount(amount)
         assertThat(purchaseAmount.amount).isEqualTo(amount)
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1000, 50000])
+    fun `구입금액이 1000원 이상이고 5만원 이하면 에러가 발생하지 않는다`(amount: Int) {
+        assertDoesNotThrow {
+            PurchaseAmount(amount)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [999, 900, 51000, 50001])
+    fun `구입금액이 1000원 이상이 아니고 5만원을 초과하면 에러가 발생한다`(amount: Int) {
+        assertThrows<IllegalArgumentException> {
+            PurchaseAmount(amount)
+        }
+    }
+
 }
