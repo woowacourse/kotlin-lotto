fee083dac3729172771ab741140cb0d1b4c60869
hyunji1203 <hj27720@gmail.com>
2023-02-14 16:48:09 +0900

    test(LottoTest): 로또가 6개의 숫자로 잘 생성되었는지 확인 테스트 추가

---
+++ src/test/kotlin/LottoTest.kt
@@ -0,0 +1,13 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
+
+class LottoTest {
+    @Test
+    fun `로또가 6개의 숫자로 잘 생성되었는지 확인`(){
+        val lotto = Lotto(listOf(1,2,3,4,5,6))
+        assertThat(lotto.numbers).isEqualTo(listOf(1,2,3,4,5,6))
+    }
+
+}
