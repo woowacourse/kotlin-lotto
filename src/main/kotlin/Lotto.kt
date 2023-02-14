98fb570ddb6c19aaef58c7490ea8bec3c1ef8778
EmilyCh0 <emily9803@ewhain.net>
2023-02-14 16:38:13 +0900

    feat: 로또 번호의 개수 검증

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -5,9 +5,11 @@ class Lotto(numbers: List<Int>) {
                 number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
             }
         ) { NUMBER_RANGE_ERROR }
+        require(numbers.size == NUMBER_COUNT)
     }
 
     companion object {
+        private const val NUMBER_COUNT = 6
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
         private const val NUMBER_RANGE_ERROR = "[ERROR] 번호는 1 이상 45 이하입니다."
--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -16,4 +16,18 @@ class LottoTest {
             Lotto(listOf(1, 45, 10, 11, 12, 13))
         }
     }
+
+    @Test
+    fun `로또 번호는 6개가 아닌 경우 예외가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 6개인 경우`() {
+        assertDoesNotThrow {
+            Lotto(listOf(1, 45, 10, 11, 12, 13))
+        }
+    }
 }
