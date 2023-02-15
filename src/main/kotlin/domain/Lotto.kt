ed5724abda8b61c11df596efde0403337a1b98f6
hyunji1203 <hj27720@gmail.com>
2023-02-15 14:05:00 +0900

    refactor[Lotto]: LottoNumber 객체 변환

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -1,18 +1,16 @@
 package domain
 
-class Lotto(val numbers: List<Int>?) {
+class Lotto(val numbers: List<LottoNumber>?) {
 
     init {
         require(numbers != null) { INPUT_LOTTO_NULL_ERROR_MESSAGE }
         require(numbers.size == 6) { INPUT_LOTTO_SIZE_ERROR_MESSAGE }
-        require(numbers.toSet().size == 6) { INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE }
-        require(numbers.filter { number -> number in 1..45 }.size == 6) { INPUT_LOTTO_RANGE_ERROR_MESSAGE }
+        require(numbers.map { it.number }.toSet().size == 6) { INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE }
     }
 
     companion object {
         const val INPUT_LOTTO_NULL_ERROR_MESSAGE = "당첨 번호가 입력되지 않았습니다."
         const val INPUT_LOTTO_SIZE_ERROR_MESSAGE = "당첨 번호가 6개가 아닙니다"
         const val INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE = "당첨 번호가 중복되었습니다."
-        const val INPUT_LOTTO_RANGE_ERROR_MESSAGE = "당첨 번호의 범위는 1에서 45 사이의 숫자여야 합니다."
     }
 }
--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,13 +1,24 @@
 import domain.Lotto
+import domain.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
+
+    private val lotto = Lotto(
+        listOf(
+            LottoNumber(1),
+            LottoNumber(2),
+            LottoNumber(3),
+            LottoNumber(4),
+            LottoNumber(5),
+            LottoNumber(6)
+        )
+    )
     @Test
     fun `로또가 6개의 숫자로 잘 생성되었는지 확인`() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        assertThat(lotto.numbers).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.numbers!!.map { it.number }).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
     }
 
     @Test
@@ -17,21 +28,34 @@ class LottoTest {
 
     @Test
     fun `길이가 6이 아닌 경우`() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        assertThat(lotto.numbers?.size).isEqualTo(6)
+        assertThrows<IllegalArgumentException> {
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                    LottoNumber(7)
+                )
+            )
+        }
     }
 
     @Test
     fun `중복된 번호가 존재하는 경우`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
-        }
-    }
-
-    @Test
-    fun `로또 번호가 1-45 사이의 숫자가 아닌 경우`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 60))
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(5)
+                )
+            )
         }
     }
 }
