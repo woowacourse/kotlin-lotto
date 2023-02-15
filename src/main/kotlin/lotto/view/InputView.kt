655d20fbd8301c650eafccefc3b8779614c41826
Doyeop Kim <dygames@naver.com>
2023-02-15 13:59:44 +0900

    feat(InputView): 지난주 당첨 번호 입력

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,21 +2,23 @@ package lotto.view
 
 import lotto.entity.InputMoney
 import lotto.entity.LottoPrice
+import lotto.entity.WinNumber
 
 class InputView {
     fun inputMoney(lottoPrice: LottoPrice): InputMoney {
         val input = readln()
-
-        require(input.toIntOrNull() != null) {
-            "숫자로만 이루어져야 합니다"
-        }
-
+        require(input.toIntOrNull() != null) { "숫자로만 이루어져야 합니다" }
         val purchaseMoney = InputMoney(input.toInt())
-
-        require(lottoPrice.value <= purchaseMoney.value) {
-            "로또 1장의 가격보다 큰 숫자를 입력해야 합니다"
-        }
-
+        require(lottoPrice.value <= purchaseMoney.value) { "로또 1장의 가격보다 큰 숫자를 입력해야 합니다" }
         return purchaseMoney
+    }
+
+    fun winNumber(): WinNumber {
+        val input = readln()
+        require(input.contains(",")) { "숫자는 콤마로 구별되어야 합니다" }
+        val splittedInput = input.split(",").map { it.trim() }
+        require(splittedInput.all { it.toIntOrNull() != null }) { "구분된 입력은 숫자로만 이루어져야 합니다" }
+        require(splittedInput.all { (1..45).contains(it.toInt()) }) { "구분된 입력은 1에서 45 사이의 숫자여야 합니다" }
+        return WinNumber(splittedInput.map { it.toInt() })
     }
 }
