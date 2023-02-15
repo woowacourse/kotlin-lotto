0e3402b91be14b5e7a5350cc7c92e0dc484c7772
otter <a33397397@gmail.com>
2023-02-15 22:01:06 +0900

    feat: 당첨 로또가 보너스 번호를 가지는 기능 구현

--- src/main/kotlin/lotto/domain/WinningLottery.kt
+++ src/main/kotlin/lotto/domain/WinningLottery.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
 class WinningLottery(
-    val lottery: Lottery
+    val lottery: Lottery,
+    val bonusNumber: LotteryNumber
 )
--- src/test/kotlin/lotto/domain/WinningLotteryTest.kt
+++ src/test/kotlin/lotto/domain/WinningLotteryTest.kt
@@ -16,8 +16,27 @@ class WinningLotteryTest {
                 LotteryNumber(45)
             )
         )
-        val winningLottery = WinningLottery(lottery)
+        val bonusNumber = LotteryNumber(10)
+        val winningLottery = WinningLottery(lottery, bonusNumber)
 
         assertThat(winningLottery.lottery.numbers).containsAll(lottery.numbers)
+    }
+
+    @Test
+    fun `보너스 번호를 가진다`() {
+        val lottery = Lottery(
+            listOf(
+                LotteryNumber(1),
+                LotteryNumber(10),
+                LotteryNumber(20),
+                LotteryNumber(30),
+                LotteryNumber(40),
+                LotteryNumber(45)
+            )
+        )
+        val bonusNumber = LotteryNumber(8)
+        val winningLottery = WinningLottery(lottery, bonusNumber)
+
+        assertThat(winningLottery.bonusNumber).isEqualTo(LotteryNumber(8))
     }
 }
