35ced807a4cde8f7db4a772feb737a2c0c1bc253
Sujin Kim <sujinkim919@gmail.com>
2023-02-15 22:18:15 +0900

    refactor: 코딩 컨벤션에 맞추어 클래스 내 순서를 수정하였다.
    - 초기화 블록 다음에 부 생성자가 오도록 하였다.

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -4,11 +4,11 @@ import lotto.view.ERROR_DUPLICATE_NUMBER
 
 class WinningLotto(val winningNumbers: Lotto, val bonusNumber: Int) {
 
-    constructor(winningNumbers: List<Int>, bonusNumber: Int) : this(Lotto(winningNumbers), bonusNumber)
-
     init {
         require(hasNoDuplicateNumber()) { ERROR_DUPLICATE_NUMBER }
     }
+
+    constructor(winningNumbers: List<Int>, bonusNumber: Int) : this(Lotto(winningNumbers), bonusNumber)
 
     fun hasNoDuplicateNumber(): Boolean {
         return !winningNumbers.lotto.contains(bonusNumber)
