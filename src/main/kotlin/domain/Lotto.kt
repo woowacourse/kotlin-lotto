64b7221feba22b794762c5f811f0e48e4ecc7e59
krrong <khk9664@naver.com>
2023-02-15 14:01:47 +0900

    style(Lotto): 코틀린 컨벤션 유지

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -9,10 +9,10 @@ class Lotto(private val _numbers: Set<Int>) {
 
     init {
         require(_numbers.size == LOTTO_SIZE) { ERROR_LOTTO_SIZE }
-        require(_numbers.all { number -> number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE } ) { ERROR_LOTTO_RANGE }
+        require(_numbers.all { number -> number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE }) { ERROR_LOTTO_RANGE }
     }
 
-    companion object{
+    companion object {
         private const val LOTTO_SIZE = 6
         private const val ERROR_LOTTO_SIZE = "로또 번호는 6개여야 합니다."
         private const val ERROR_LOTTO_RANGE = "로또 번호는 1이상 45이하여야 합니다."
--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -1,23 +1,23 @@
 package domain
 
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
     fun `개수가 6개면 로또 생성`() {
-        val lotto = Lotto(setOf(1,2,3,4,5,6))
+        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
         assertThat(lotto.numbers.size).isEqualTo(6)
     }
 
     @Test
     fun `개수가 6개가 아니라면 exception 발생`() {
-        assertThrows<IllegalArgumentException> { Lotto(setOf(1,2,3,4,5)) }
+        assertThrows<IllegalArgumentException> { Lotto(setOf(1, 2, 3, 4, 5)) }
     }
 
     @Test
     fun `번호가 범위안에 들어오지 않으면 exception 발생`() {
-        assertThrows<IllegalArgumentException> { Lotto(setOf(1,2,3,4,5,47)) }
+        assertThrows<IllegalArgumentException> { Lotto(setOf(1, 2, 3, 4, 5, 47)) }
     }
 }
