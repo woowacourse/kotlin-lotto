5c84fa5e2bc2873df4ddfab19e33a789e2cd2fac
hyunji1203 <hj27720@gmail.com>
2023-02-15 15:30:18 +0900

    test[BonusNumberTest]: 보너스 번호가 null일 경우 예외 테스트 추가

--- src/test/kotlin/BonusNumberTest.kt
+++ src/test/kotlin/BonusNumberTest.kt
@@ -1,6 +1,7 @@
 import domain.BonusNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class BonusNumberTest {
 
@@ -8,5 +9,12 @@ class BonusNumberTest {
     fun `보너스 번호가 생성되었는지 확인`() {
         val bonusNumber = BonusNumber(3)
         assertThat(bonusNumber.number).isEqualTo(3)
+    }
+
+    @Test
+    fun `보너스 번호가 null일 경우`() {
+        assertThrows<IllegalArgumentException> {
+            BonusNumber(null)
+        }
     }
 }
