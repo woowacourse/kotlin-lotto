c7f8b4e11df509d973ceccd55216fb3200c3a37a
otter <a33397397@gmail.com>
2023-02-15 14:02:38 +0900

    feat: 로또 번호 생성시 1 이상 45 이하의 숫자가 아니라면 에러가 발생하는 기능 구현

--- src/main/kotlin/lotto/model/LotteryNumber.kt
+++ src/main/kotlin/lotto/model/LotteryNumber.kt
@@ -2,4 +2,19 @@ package lotto.model
 
 class LotteryNumber(
     val number: Int
-)
+) {
+    init {
+        checkNumberBoundary()
+    }
+
+    private fun checkNumberBoundary() {
+        require(number in LOTTERY_NUMBER_LOWER_BOUNDARY..LOTTERY_NUMBER_UPPER_BOUNDARY) {
+            "로또 번호는 1 이상 45 이하의 숫자여야 합니다."
+        }
+    }
+
+    companion object {
+        private const val LOTTERY_NUMBER_LOWER_BOUNDARY = 1
+        private const val LOTTERY_NUMBER_UPPER_BOUNDARY = 45
+    }
+}
--- src/test/kotlin/lotto/model/LotteryNumberTest.kt
+++ src/test/kotlin/lotto/model/LotteryNumberTest.kt
@@ -1,6 +1,8 @@
 package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -10,5 +12,21 @@ class LotteryNumberTest {
     fun `번호를 하나 가진다`(number: Int) {
         val lotteryNumber = LotteryNumber(number)
         assertThat(lotteryNumber.number).isEqualTo(number)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `번호는 1 이상 45 이하의 숫자가 아니라면 에러가 발생한다`(number: Int) {
+        assertThrows<IllegalArgumentException> {
+            LotteryNumber(number)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 45])
+    fun `번호는 1 이상 45 이하의 숫자면 에러가 발생하지 않는다`(number: Int) {
+        assertDoesNotThrow {
+            LotteryNumber(number)
+        }
     }
 }
