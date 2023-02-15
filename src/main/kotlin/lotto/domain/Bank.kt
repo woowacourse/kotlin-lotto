a585c4fc79cc948de42d732d2b2b1f03cd742bd2
changhwan <chang5hustle@gmail.com>
2023-02-15 18:04:24 +0900

    feat(lotto): 보너스 번호 당첨 여부를 구하는 기능 구현 및 테스트

--- src/main/kotlin/lotto/domain/Bank.kt
+++ src/main/kotlin/lotto/domain/Bank.kt
@@ -1,6 +1,9 @@
 package lotto.domain
 
 class Bank {
-    fun countMatchMainLottoNumber(lotto: Lotto, winningLotto: WinningLotto): Int =
+    fun countMatchedMainLottoNumber(lotto: Lotto, winningLotto: WinningLotto): Int =
         lotto.lottoNumbers.count { lottoNumber -> lottoNumber.value in winningLotto.mainLottoNumbers.map { it.value } }
+
+    fun checkMatchedBonusLottoNumber(lotto: Lotto, winningLotto: WinningLotto): Boolean =
+        winningLotto.bonusLottoNumber.value in lotto.lottoNumbers.map { lottoNumber -> lottoNumber.value }
 }
--- src/test/kotlin/lotto/domain/BankTest.kt
+++ src/test/kotlin/lotto/domain/BankTest.kt
@@ -7,7 +7,7 @@ class BankTest {
     @Test
     fun `로또 번호와 당첨번호가 6개 같다(6을 반환)`() {
         assertThat(
-            Bank().countMatchMainLottoNumber(
+            Bank().countMatchedMainLottoNumber(
                 Lotto(
                     listOf(
                         LottoNumber(1),
@@ -31,5 +31,34 @@ class BankTest {
                 ),
             ),
         ).isEqualTo(6)
+    }
+
+    @Test
+    fun `보너스 번호가 당첨되었다`() {
+        assertThat(
+            Bank().checkMatchedBonusLottoNumber(
+                Lotto(
+                    listOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(7),
+                        LottoNumber(6),
+                    ),
+                ),
+                WinningLotto(
+                    listOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(6),
+                    ),
+                    LottoNumber(7),
+                ),
+            ),
+        ).isTrue
     }
 }
