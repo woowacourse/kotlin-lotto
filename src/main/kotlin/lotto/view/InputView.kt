ce537298c2501c3989cb292026498e32e1e146a3
whk06061 <gju06061@naver.com>
2023-02-15 16:30:40 +0900

    feat: 당첨 번호와 보너스 볼을 입력받는다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,23 @@
 package lotto.view
 
+import lotto.domain.Lotto
 import lotto.exception.Validator
 
 object InputView {
     fun readInputMoney(): Int {
         val input = readln()
         Validator.checkInputMoney(input)
+        return input.toInt()
+    }
+
+    fun readInputWinningLotto(): Lotto {
+        val input = readln()
+        return Lotto(input.split(",").map { it.toInt() })
+    }
+
+    fun readInputBonusNumber(): Int {
+        val input = readln()
+        Validator.checkInputBonusNumber(input)
         return input.toInt()
     }
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,7 +5,9 @@ import lotto.domain.Lotto
 object OutputView {
 
     private const val INPUT_MONEY_PROMPT = "구입금액을 입력해 주세요."
-    const val LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다."
+    private const val LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다."
+    private const val INPUT_WINNING_NUMBERS_PROMPT = "지난 주 당첨 번호를 입력해 주세요."
+    private const val INPUT_BONUS_NUMBER_PROMPT = "보너스 볼을 입력해 주세요."
 
     fun printInputMoneyPrompt() {
         println(INPUT_MONEY_PROMPT)
@@ -21,5 +23,13 @@ object OutputView {
 
     private fun printLotto(lotto: Lotto) {
         println("[${lotto.numbers.joinToString(", ")}]")
+    }
+
+    fun printInputWinningNumbersPrompt() {
+        println(INPUT_WINNING_NUMBERS_PROMPT)
+    }
+
+    fun printInputBonusNumberPrompt() {
+        println(INPUT_BONUS_NUMBER_PROMPT)
     }
 }
