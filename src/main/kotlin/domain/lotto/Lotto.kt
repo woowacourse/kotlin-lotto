5ce0145f92ac97446b3af48bc3d794892db039c5
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-15 15:20:34 +0900

    feat: 로또 생성시 로또 번호가 6개인지 검증하는 로직 구현

--- src/main/kotlin/domain/lotto/Lotto.kt
+++ src/main/kotlin/domain/lotto/Lotto.kt
@@ -1,3 +1,12 @@
 package domain.lotto
 
-class Lotto(private val numbers: List<LottoNumber>)
+class Lotto(private val numbers: List<LottoNumber>) {
+    init {
+        require(numbers.size == LOTTO_SIZE) { ERROR_MESSAGE_INVALID_LOTTO_SIZE }
+    }
+
+    companion object {
+        private const val LOTTO_SIZE = 6
+        private const val ERROR_MESSAGE_INVALID_LOTTO_SIZE = "[ERROR] 로또 번호 6개이어야 합니다."
+    }
+}
--- src/test/kotlin/domain/lotto/LottoTest.kt
+++ src/test/kotlin/domain/lotto/LottoTest.kt
@@ -1,6 +1,7 @@
 package domain.lotto
 
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -14,14 +15,28 @@ class LottoTest {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("provideFiveLottoNumbers")
+    fun `로또 번호가 5개 일 때, 로또 객체 생성시, IllegalArgumentException이 발생한다`(lottoNumbers: List<LottoNumber>) {
+        assertThrows<IllegalArgumentException> {
+            Lotto(lottoNumbers)
+        }
+    }
+
     companion object {
         @JvmStatic
         fun provideSixLottoNumbers(): List<Arguments> =
             listOf(
                 Arguments.of(
-                    (1..6).map { number ->
-                        LottoNumber(number)
-                    }
+                    (1..6).map { number -> LottoNumber(number) }
+                )
+            )
+
+        @JvmStatic
+        fun provideFiveLottoNumbers(): List<Arguments> =
+            listOf(
+                Arguments.of(
+                    (1..5).map { number -> LottoNumber(number) }
                 )
             )
     }
