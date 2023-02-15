e15e8390ed98e13acf40821accf09c02d3bd6c7d
whk06061 <gju06061@naver.com>
2023-02-15 15:15:40 +0900

    fix: 로또 번호를 정렬한다.

--- src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
@@ -2,6 +2,6 @@ package lotto.domain
 
 class RandomNumberGenerator : LottoNumberGenerator {
     override fun generate(): List<Int> {
-        return (1..45).toList().shuffled().slice(0..5)
+        return (1..45).toList().shuffled().slice(0..5).sorted()
     }
 }
