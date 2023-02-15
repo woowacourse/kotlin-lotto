378a8a1872423c085edbc054f652a60b7d661413
s9hn <tnghqn@naver.com>
2023-02-15 15:54:40 +0900

    [FEAT(RankTest)] 당첨번호 & 보너스번호 일치 갯수에 따른 등수를 확인한다

---
+++ src/main/kotlin/domain/Rank.kt
@@ -0,0 +1,24 @@
+package domain
+
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+
+    companion object {
+        fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank? {
+            return values().find {rank ->
+               check(countOfMatch,matchBonus,rank)
+            }
+        }
+
+        fun check(countOfMatch: Int,matchBonus: Boolean,rank: Rank):Boolean{
+            if(countOfMatch ==5 && !matchBonus) return rank.winningMoney == 1_500_000
+            return  countOfMatch == rank.countOfMatch
+        }
+    }
+}
---
+++ src/test/kotlin/domain/RankTest.kt
@@ -0,0 +1,17 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RankTest {
+    @Test
+    fun `당첨번호 등수를 확인한다`(){
+        //given
+        val countOfMatch = 5
+        val matchBonus = false
+
+        val actual = Rank.valueOf(countOfMatch,matchBonus)
+
+        assertThat(actual).isEqualTo(Rank.THIRD)
+    }
+}
