41cce16dfa9b7374ff2058c0d76a9198e38fde98
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-15 16:05:03 +0900

    feat: 로또 중복 검증 기능 구현

--- src/main/kotlin/domain/lotto/Lotto.kt
+++ src/main/kotlin/domain/lotto/Lotto.kt
@@ -2,11 +2,25 @@ package domain.lotto
 
 class Lotto(private val numbers: List<LottoNumber>) {
     init {
+        validateLottoNumbers()
+    }
+
+    private fun validateLottoNumbers() {
+        validateLottoNumbersSize()
+        validateLottoNumbersDuplication()
+    }
+
+    private fun validateLottoNumbersSize() {
         require(numbers.size == LOTTO_SIZE) { ERROR_MESSAGE_INVALID_LOTTO_SIZE }
+    }
+
+    private fun validateLottoNumbersDuplication() {
+        check(numbers.distinctBy { it.value }.size == LOTTO_SIZE) { ERROR_MESSAGE_DUPLICATED_LOTTO_NUMBER }
     }
 
     companion object {
         private const val LOTTO_SIZE = 6
         private const val ERROR_MESSAGE_INVALID_LOTTO_SIZE = "[ERROR] 로또 번호 6개이어야 합니다."
+        private const val ERROR_MESSAGE_DUPLICATED_LOTTO_NUMBER = "[ERROR] 로또 번호는 중복되지 않아야 합니다."
     }
 }
--- src/test/kotlin/domain/lotto/LottoTest.kt
+++ src/test/kotlin/domain/lotto/LottoTest.kt
@@ -23,21 +23,49 @@ class LottoTest {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("provideDistinctSixLottoNumbers")
+    fun `중복되지 않는 로또 번호가 6개 주어졌을 때, 로또 객체 생성시, 예외가 발생하지 않는다`(lottoNumbers: List<LottoNumber>) {
+        assertDoesNotThrow {
+            Lotto(lottoNumbers)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDuplicatedSixLottoNumbers")
+    fun `중복되는 로또 번호가 6개 주어졌을 때, 로또 객체 생성시, IllegalStateException이 발생한다`(lottoNumbers: List<LottoNumber>) {
+        assertThrows<IllegalStateException> {
+            Lotto(lottoNumbers)
+        }
+    }
+
     companion object {
         @JvmStatic
         fun provideSixLottoNumbers(): List<Arguments> =
             listOf(
-                Arguments.of(
-                    (1..6).map { number -> LottoNumber(number) }
-                )
+                Arguments.of((1..6).map { number -> LottoNumber(number) })
             )
 
         @JvmStatic
         fun provideFiveLottoNumbers(): List<Arguments> =
             listOf(
-                Arguments.of(
-                    (1..5).map { number -> LottoNumber(number) }
-                )
+                Arguments.of((1..5).map { number -> LottoNumber(number) })
+            )
+
+        @JvmStatic
+        fun provideDistinctSixLottoNumbers(): List<Arguments> =
+            listOf(
+                Arguments.of((1..6).map { number -> LottoNumber(number) }),
+                Arguments.of(listOf(1, 3, 5, 7, 8, 9).map { number -> LottoNumber(number) }),
+                Arguments.of(listOf(2, 4, 6, 8, 10, 12).map { number -> LottoNumber(number) })
+            )
+
+        @JvmStatic
+        fun provideDuplicatedSixLottoNumbers(): List<Arguments> =
+            listOf(
+                Arguments.of(listOf(1, 1, 2, 3, 4, 5).map { number -> LottoNumber(number) }),
+                Arguments.of(listOf(1, 1, 1, 1, 1, 1).map { number -> LottoNumber(number) }),
+                Arguments.of(listOf(1, 4, 13, 13, 41, 41).map { number -> LottoNumber(number) })
             )
     }
 }
