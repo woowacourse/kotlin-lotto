dfe7bbf0dc33b8b8198807d9680f9d6c0d64ce0e
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 16:47:05 +0900

    feat: 당첨 번호를 입력받기

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -1,10 +1,17 @@
 object InputView {
 
     private const val PURCHASE_MONEY_REQUEST_MESSAGE = "구입금액을 입력해 주세요."
-
-    fun requestPurchaseMoney(): Int? {
+    private const val CATCH_NUMBER_REQUEST_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+    fun requestPurchaseMoney(): String {
         println(PURCHASE_MONEY_REQUEST_MESSAGE)
-        return readln().toIntOrNull()
+
+        return readln()
+    }
+
+    fun requestCatchNumbers(): String{
+        println(CATCH_NUMBER_REQUEST_MESSAGE)
+
+        return readln()
     }
 
 }
