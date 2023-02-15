846a9666dacbc1320da98240d913d0b0fab53773
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 13:56:38 +0900

    remove: LottoGenerator 로직 삭제

--- src/main/kotlin/LottoGenerator.kt
+++ src/main/kotlin/LottoGenerator.kt
@@ -1,20 +1,3 @@
-class LottoGenerator(
-    money: Int,
-    private val lottoPrice: Int = LOTTO_PRICE
-) {
-    init {
-        require(money % lottoPrice == ZERO_MONEY) {
-            moneyError(lottoPrice)
-        }
-        require(money > ZERO_MONEY) {
-            NEGATIVE_MONEY_ERROR
-        }
-    }
+class LottoGenerator() {
 
-    companion object {
-        private const val LOTTO_PRICE = 1000
-        private const val ZERO_MONEY = 0
-        private const val NEGATIVE_MONEY_ERROR = "[ERROR] 음수는 입력하실 수 없습니다."
-        private fun moneyError(lottoPrice: Int): String = "[ERROR] %s원 단위로 입력해주세요.".format(lottoPrice)
-    }
 }
--- src/test/kotlin/LottoGeneratorTest.kt
+++ src/test/kotlin/LottoGeneratorTest.kt
@@ -1,25 +1,3 @@
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
-
 class LottoGeneratorTest {
 
-    @Test
-    fun `구입 금액은 1000원 단위가 아닌 경우`() {
-        assertThrows<IllegalArgumentException> {
-            LottoGenerator(2500)
-        }
-    }
-
-    @Test
-    fun `구입 금액이 1000원인 경우`() {
-        assertDoesNotThrow { LottoGenerator(3000) }
-    }
-
-    @Test
-    fun `구입 금액이 음수인 경우`() {
-        assertThrows<IllegalArgumentException> {
-            LottoGenerator(-1000)
-        }
-    }
 }
