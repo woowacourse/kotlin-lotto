23789b43fd8ad63a23565355ce54b0f2497e586a
hyunji1203 <hj27720@gmail.com>
2023-02-15 16:20:26 +0900

    test[LottoTest]: 랜덤으로 생성된 로또가 6개의 숫자로 이루어졌는지 확인하는 테스트 추가

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,4 +1,5 @@
 import domain.Lotto
+import domain.RandomLottoGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,6 +12,12 @@ class LottoTest {
     @Test
     fun `로또가 6개의 숫자로 잘 생성되었는지 확인`() {
         assertThat(lotto.numbers).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+    }
+
+    @Test
+    fun `랜덤으로 생성된 로또가 6개의 숫자로 이루어졌는지 확인`() {
+        val lotto = RandomLottoGenerator().generateLotto()
+        assertThat(lotto.numbers!!.size).isEqualTo(6)
     }
 
     @Test
@@ -43,5 +50,10 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(listOf(1, 12, 55, 44, 23, 65))
         }
+    }
+
+    @Test
+    fun `로또 번호가 몇 개 일치하는 지 확인`() {
+        val testLotto = Lotto(listOf(1, 2, 4, 6, 7, 8))
     }
 }
