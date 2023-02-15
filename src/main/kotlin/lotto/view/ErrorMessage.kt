99ea6780e187716b4906be15e7b3bec7b73fa2f6
Sujin Kim <sujinkim919@gmail.com>
2023-02-15 18:13:54 +0900

    feat: 입력 관련 오류 메시지 상수 파일 작성

---
+++ src/main/kotlin/lotto/view/ErrorMessage.kt
@@ -0,0 +1,6 @@
+package lotto.view
+
+const val ERROR_NULL_OR_BLANK = "공백을 입력하셨습니다. 다시 입력해주세요 !"
+const val ERROR_NOT_POSITIVE_NUMBER = "양의 정수를 입력해주세요."
+const val ERROR_SIZE_OF_LOTTO = "로또 번호 숫자가 맞지 않습니다."
+const val ERROR_DUPLICATE_NUMBER = "중복된 숫자가 있습니다."
