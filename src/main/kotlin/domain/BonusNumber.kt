a65a165d47a9a3a9eee1230308f001c762ea3461
hyunji1203 <hj27720@gmail.com>
2023-02-15 15:58:13 +0900

    feat[BonusNumber]: 보너스 번호가 1에서 45 사이의 값이 아닌 경우 예외 처리 추가

--- src/main/kotlin/domain/BonusNumber.kt
+++ src/main/kotlin/domain/BonusNumber.kt
@@ -1,3 +1,12 @@
 package domain
 
-class BonusNumber(val number: Int?)
+class BonusNumber(val number: Int?) {
+
+    init {
+        require(number in 1..45) { BONUS_NUMBER_RANGE_ERROR }
+    }
+
+    companion object {
+        const val BONUS_NUMBER_RANGE_ERROR = "보너스 번호는 1에서 45 사이여야 합니다."
+    }
+}
