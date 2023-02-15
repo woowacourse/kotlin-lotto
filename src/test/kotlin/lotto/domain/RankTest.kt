b739d75ea73d512427c19994d73444c5b38a64c2
Gloria <ippnsj@gmail.com>
2023-02-16 01:42:10 +0900

    test: 일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려주는 기능 테스트 추가

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -1,11 +1,13 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
-    @Test
-    fun `일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려준다`() {
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND)
+    @ParameterizedTest
+    @CsvSource("6,false,FIRST", "5,true,SECOND", "5,false,THIRD", "4,true,FOURTH", "3,false,FIFTH", "2,true,MISS")
+    fun `일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려준다`(countOfMatch: Int, matchBonus: Boolean, expected: Rank) {
+        assertThat(Rank.valueOf(countOfMatch, matchBonus)).isEqualTo(expected)
     }
 }
