6de3b1cb87f7adf32ea9f4cbeae8eb665d993aa9
krrong <khk9664@naver.com>
2023-02-15 17:02:58 +0900

    refactor(LottoStatistics): 불필요한 중복 삭제, 중복 테스트코드 단일화

--- src/main/kotlin/domain/LottoStatistics.kt
+++ src/main/kotlin/domain/LottoStatistics.kt
@@ -2,8 +2,9 @@ package domain
 
 class LottoStatistics(val winningLotto: WinningLotto) {
     fun compareNumbers(numbers: Set<Int>): Int {
+        val winningNumbers = winningLotto.getWinningNumbers()
         return numbers.filter { number ->
-            winningLotto.getWinningNumbers().contains(number)
+            winningNumbers.contains(number)
         }.size
     }
 }
--- src/test/kotlin/domain/LottoStatisticsTest.kt
+++ src/test/kotlin/domain/LottoStatisticsTest.kt
@@ -1,18 +1,33 @@
 package domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class LottoStatisticsTest {
-    @Test
-    fun `당첨 번호와 일치하는 숫자의 개수를 찾는다`() {
-        val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
+    @ParameterizedTest(name = "{1}개가 일치하는 경우")
+    @MethodSource("provideLottoAndMatchCount")
+    fun `당첨 번호와 일치하는 숫자의 개수를 찾는다`(lotto: Lotto, expected: Int) {
         val winningNumbers = setOf(1, 2, 3, 4, 5, 6)
         val bonusNumber = 7
         val winningLotto = WinningLotto(Lotto(winningNumbers), bonusNumber)
         val lottoStatistics = LottoStatistics(winningLotto)
-        val matchOfCount = lottoStatistics.compareNumbers(lotto.numbers)
+        val actual = lottoStatistics.compareNumbers(lotto.numbers)
 
-        assertThat(matchOfCount).isEqualTo(6)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideLottoAndMatchCount(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments(Lotto(setOf(1, 2, 3, 4, 5, 6)), 6),
+                Arguments.arguments(Lotto(setOf(1, 2, 3, 4, 5, 7)), 5),
+                Arguments.arguments(Lotto(setOf(1, 2, 3, 4, 8, 7)), 4),
+                Arguments.arguments(Lotto(setOf(1, 2, 3, 9, 8, 7)), 3),
+            )
+        }
     }
 }
