ba9dcc4e860c163532c2dd5ef5b8bd3782d0ebe6
whk06061 <gju06061@naver.com>
2023-02-15 16:32:56 +0900

    feat: 오류 발생시 입력을 다시 받는다.

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -1,14 +1,52 @@
 package lotto.controller
 
+import lotto.domain.Lotto
 import lotto.domain.LottoGenerator
+import lotto.domain.WinningNumbers
 import lotto.view.InputView
 import lotto.view.OutputView
 
 class Controller {
     fun start() {
-        OutputView.printInputMoneyPrompt()
-        val count = InputView.readInputMoney() / 1000
+        val lottoNumbers = initializeLotto()
+        val winningNumbers = readBonusNumber(readWinningLotto())
+    }
+
+    private fun initializeLotto(): List<Lotto> {
+        val count = readInputMoney() / 1000
         OutputView.printLottoCountMessage(count)
-        OutputView.printLottoNumbers(LottoGenerator.generate(count))
+        val lottoNumbers = LottoGenerator.generate(count)
+        OutputView.printLottoNumbers(lottoNumbers)
+        return lottoNumbers
+    }
+
+    private fun readInputMoney(): Int {
+        return try {
+            OutputView.printInputMoneyPrompt()
+            InputView.readInputMoney()
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] ${e.message}")
+            readInputMoney()
+        }
+    }
+
+    private fun readWinningLotto(): Lotto {
+        return try {
+            OutputView.printInputWinningNumbersPrompt()
+            InputView.readInputWinningLotto()
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] ${e.message}")
+            readWinningLotto()
+        }
+    }
+
+    private fun readBonusNumber(winningLotto: Lotto): WinningNumbers {
+        return try {
+            OutputView.printInputBonusNumberPrompt()
+            WinningNumbers(winningLotto, InputView.readInputBonusNumber())
+        } catch (e: IllegalArgumentException) {
+            println("[ERROR] ${e.message}")
+            readBonusNumber(winningLotto)
+        }
     }
 }
