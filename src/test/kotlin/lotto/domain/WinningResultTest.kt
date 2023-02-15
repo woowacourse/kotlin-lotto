fcc0ff42e0994eb69efa63e91360a60d026ee725
otter <a33397397@gmail.com>
2023-02-16 06:03:11 +0900

    refactor: 테스트 코드에서 잘못된 인자를 전달하는 것을 수정

--- src/test/kotlin/lotto/domain/WinningResultTest.kt
+++ src/test/kotlin/lotto/domain/WinningResultTest.kt
@@ -7,14 +7,14 @@ class WinningResultTest {
 
     @Test
     fun `일치 등수의 개수를 기록한다`() {
-        val winningResult = WinningResult(mutableListOf(1, 0, 0, 0, 0))
-        assertThat(winningResult.countMatchRanks).isEqualTo(mutableListOf(1, 0, 0, 0, 0))
+        val winningResult = WinningResult(mutableListOf(1, 0, 0, 0, 0, 0))
+        assertThat(winningResult.countMatchRanks).isEqualTo(mutableListOf(1, 0, 0, 0, 0, 0))
     }
 
     @Test
     fun `등수를 받아 카운트한다`() {
-        val winningResult = WinningResult(mutableListOf(1, 0, 0, 0, 0))
+        val winningResult = WinningResult(mutableListOf(1, 0, 0, 0, 0, 0))
         winningResult.countRank(Rank.SECOND)
-        assertThat(winningResult.countMatchRanks).isEqualTo(listOf(1, 1, 0, 0, 0))
+        assertThat(winningResult.countMatchRanks).isEqualTo(listOf(1, 1, 0, 0, 0, 0))
     }
 }
