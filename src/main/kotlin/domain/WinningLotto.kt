f6ce62036f0d5708ada3bd0b675c43296aa588cc
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-15 15:12:45 +0900

    feat (WinningLotto): 로또를 받으면 맞는 개수를 반환하고 보너스 번호가 있는지 확인한다.

--- src/main/kotlin/domain/WinningLotto.kt
+++ src/main/kotlin/domain/WinningLotto.kt
@@ -7,6 +7,10 @@ class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
 
     constructor(numbers: List<Int>, bonusNumber: Int) : this(Lotto.create(numbers), LottoNumber(bonusNumber))
 
+    fun getCountOfMatch(anyLotto: Lotto): Int = anyLotto.count { lotto.contains(it) }
+
+    fun matchBonus(anyLotto: Lotto): Boolean = anyLotto.contains(bonusNumber)
+
     companion object {
         private const val ERROR_DUPLICATE_BONUS_NUMBER = "보너스 번호는 당첨 번호와 중복될 수 없습니다. \n잘못된 값 : %s, %s"
     }
--- src/test/kotlin/domain/WinningLottoTest.kt
+++ src/test/kotlin/domain/WinningLottoTest.kt
@@ -20,4 +20,24 @@ class WinningLottoTest {
             .isThrownBy { WinningLotto(numbers, bonusNumber) }
             .withMessage("보너스 번호는 당첨 번호와 중복될 수 없습니다. \n잘못된 값 : ${Lotto.create(numbers)}, ${LottoNumber(bonusNumber)}")
     }
+
+    @Test
+    fun `당첨 번호는 로또에 대해 맞는 개수를 반환할 수 있다`() {
+        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
+        val lotto = Lotto.create(listOf(1, 2, 3, 4, 5, 6))
+
+        val result = winningLotto.getCountOfMatch(lotto)
+
+        assertThat(result).isEqualTo(6)
+    }
+
+    @Test
+    fun `당첨 번호는 로또에 대해 보너스 번호 매치 여부를 반환할 수 있다`() {
+        val winningLotto = WinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
+        val lotto = Lotto.create(listOf(1, 2, 3, 4, 5, 7))
+
+        val result = winningLotto.matchBonus(lotto)
+
+        assertThat(result).isTrue
+    }
 }
