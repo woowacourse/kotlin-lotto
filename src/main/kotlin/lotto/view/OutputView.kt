7a7ba50b22674c24439b318e80b338071ef6de22
Sujin Kim <sujinkim919@gmail.com>
2023-02-15 19:18:43 +0900

    feat: print 관련 상수를 담은 동반객체를 생성한다.

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,4 +1,20 @@
 package lotto.view
 
 class OutputView {
+
+    companion object {
+        private const val INSERT_MONEY = "구입금액을 입력해 주세요."
+        private const val INSERT_WINNING_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
+        private const val INSERT_BONUS_BALL = "보너스 볼을 입력해 주세요."
+
+        private const val BUY_MESSAGE = "개를 구매했습니다."
+        private const val WINNING_STATISTICS = "당첨 통계"
+        private const val DIVIDER = "---------"
+        private const val MATCH_STANDARD = "%d개 일치"
+        private const val MATCH_STANDARD_WITH_BONUS = ", 보너스볼 일치"
+        private const val MATCH_MONEY = "(%d원)"
+        private const val MATCH_COUNT = "- %d개"
+        private const val EARNING_RATE = "총 수익률은 %f입니다."
+        private const val LOSING_MONEY = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+    }
 }
