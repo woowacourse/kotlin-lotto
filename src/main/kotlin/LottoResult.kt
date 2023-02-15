9e627ed7c5071ccd61d25571ecfbcd4746edcc2e
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 16:28:42 +0900

    feat: 수익률 계산

--- src/main/kotlin/LottoResult.kt
+++ src/main/kotlin/LottoResult.kt
@@ -1,8 +1,8 @@
-enum class LottoResult {
-    FIRST,
-    SECOND,
-    THIRD,
-    FORTH,
-    FIFTH,
-    MISS
+enum class LottoResult(val prizeMoney: Int) {
+    FIRST(2_000_000_000),
+    SECOND(30_000_000),
+    THIRD(1_500_000),
+    FORTH(50_000),
+    FIFTH(5_000),
+    MISS(0)
 }
---
+++ src/test/kotlin/ProfitCalculatorTest.kt
@@ -0,0 +1,17 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ProfitCalculatorTest {
+
+    @Test
+    fun `수익률을 계산한다`() {
+        val profitCalculator = ProfitCalculator()
+
+        assertThat(
+            profitCalculator.getProfit(
+                PurchaseMoney(14000),
+                listOf(LottoResult.FIFTH)
+            )
+        ).isEqualTo(0.35)
+    }
+}
