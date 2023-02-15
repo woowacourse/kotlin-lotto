4579295d2dba680d4fd390b3780207d9518db07c
Sujin Kim <sujinkim919@gmail.com>
2023-02-15 17:20:31 +0900

    feat: 당첨 번호에 보너스 볼과 중복된 번호가 있는 경우 false를 반환한다.

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -1,5 +1,10 @@
 package lotto.model
 
 class WinningLotto(val winningNumbers: Lotto, val bonusNumber: Int) {
+
     constructor(winningNumbers: List<Int>, bonusNumber: Int) : this(Lotto(winningNumbers), bonusNumber)
+
+    fun hasNoDuplicateNumber(): Boolean {
+        return !winningNumbers.lotto.contains(bonusNumber)
+    }
 }
