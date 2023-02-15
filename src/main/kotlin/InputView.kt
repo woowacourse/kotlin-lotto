26e014fbe28874f87cafc63593e86d73cffb5156
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 16:51:08 +0900

    refactor: 당첨 번호 입력 리턴 타입 변경

--- src/main/kotlin/InputView.kt
+++ src/main/kotlin/InputView.kt
@@ -2,16 +2,16 @@ object InputView {
 
     private const val PURCHASE_MONEY_REQUEST_MESSAGE = "구입금액을 입력해 주세요."
     private const val CATCH_NUMBER_REQUEST_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+    private const val TOKENIZER = ","
     fun requestPurchaseMoney(): String {
         println(PURCHASE_MONEY_REQUEST_MESSAGE)
 
         return readln()
     }
 
-    fun requestCatchNumbers(): String{
+    fun requestCatchNumbers(): List<String> {
         println(CATCH_NUMBER_REQUEST_MESSAGE)
 
-        return readln()
+        return readln().split(TOKENIZER)
     }
-
 }
