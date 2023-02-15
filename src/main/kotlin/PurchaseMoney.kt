eb8e8d6a14f316fba06d75c0dd88bde64ac0e12d
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 15:14:57 +0900

    feat: 구입 금액 0원보다 큰지 검증

--- src/main/kotlin/PurchaseMoney.kt
+++ src/main/kotlin/PurchaseMoney.kt
@@ -1,4 +1,14 @@
 class PurchaseMoney(
     val money: Int
 ) {
+    init {
+        require(money > ZERO) {
+            MONEY_RANGE_ERROR
+        }
+    }
+
+    companion object {
+        private const val ZERO = 0
+        private const val MONEY_RANGE_ERROR = "[ERROR] 구입 금액은 0원보다 커야합니다."
+    }
 }
