8f64c03de642b9edc6c6df1845237ca944a13124
Doyeop Kim <dygames@naver.com>
2023-02-15 16:21:07 +0900

    fix(OutputView): format 함수가 제대로 작동하지 않던 문제 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -7,12 +7,12 @@ import kotlin.math.floor
 
 object OutputView {
     const val MESSAGE_INPUT_MONEY = "구입금액을 입력해 주세요."
-    const val MESSAGE_PURCHASE_COUNT = "개를 구매했습니다."
+    const val MESSAGE_PURCHASE_COUNT = "%d개를 구매했습니다."
     const val MESSAGE_WIN_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
     const val MESSAGE_BONUS = "보너스 볼을 입력해 주세요."
 
-    fun printMessage(message: String, vararg args: Any) {
-        println(String.format(message, args))
+    fun printMessage(message: String, vararg args: Any?) {
+        println(String.format(message, *args))
     }
 
     fun winStatisticsResult(winStatistics: WinStatistics, winStatisticsFormatter: WinStatisticsFormatter) {
