1d83a879cc2b30fa19bee4cbba139805c33d687d
hyunji1203 <hj27720@gmail.com>
2023-02-15 15:02:12 +0900

    test[LottosTest]: 금액에 맞는 로또 생성 개수 확인 테스트 추가

--- src/test/kotlin/LottosTest.kt
+++ src/test/kotlin/LottosTest.kt
@@ -1,5 +1,6 @@
 import domain.Lotto
 import domain.Lottos
+import domain.RandomLottoGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -13,6 +14,12 @@ class LottosTest {
     @Test
     fun `로또 리스트가 생성되는지 확인`() {
         val lottos = Lottos(testLottos)
+        assertThat(lottos.lottos.size).isEqualTo(4)
+    }
+
+    @Test
+    fun `금액에 맞게 로또 리스트가 생성되는지 확인`() {
+        val lottos = RandomLottoGenerator().generateLottos(10000)
         assertThat(lottos.lottos.size).isEqualTo(10)
     }
 }
