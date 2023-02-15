43e5c1232e824f859ecde8258fdf1c86cd6770cf
EmilyCh0 <emily9803@ewhain.net>
2023-02-15 15:37:20 +0900

    feat: 전체 당첨 결과 확인

--- src/main/kotlin/Lottos.kt
+++ src/main/kotlin/Lottos.kt
@@ -1,2 +1,6 @@
 class Lottos(private val lottos: List<Lotto>) {
+
+    fun getTotalLottoResults(winningNumbers: WinningNumbers) = lottos.map { lotto ->
+        lotto.getLottoResult(winningNumbers)
+    }
 }
