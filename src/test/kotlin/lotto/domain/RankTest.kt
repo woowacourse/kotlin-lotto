865472fa801b1f68c745d4f530fd2502887ae0c2
otter <a33397397@gmail.com>
2023-02-16 05:08:35 +0900

    test: 테스트케이스 추가 일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려주는 테스트케이스를 추가함

--- src/test/kotlin/lotto/domain/RankTest.kt
+++ src/test/kotlin/lotto/domain/RankTest.kt
@@ -1,11 +1,23 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
-    @Test
-    fun `일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려준다`() {
-        assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND)
+    @ParameterizedTest
+    @CsvSource(
+        "6, false, FIRST",
+        "5, true, SECOND",
+        "5, false, THIRD",
+        "4, false, FOURTH",
+        "3, false, FIFTH",
+        "3, true, FIFTH",
+        "2, false, MISS",
+        "1, false, MISS",
+        "0, false, MISS",
+    )
+    fun `일치하는 개수와 보너스번호 매치 여부를 알려주면 등수를 알려준다`(countOfMatch: Int, matchBonus: Boolean, expected: Rank) {
+        assertThat(Rank.valueOf(countOfMatch, matchBonus)).isEqualTo(expected)
     }
 }
