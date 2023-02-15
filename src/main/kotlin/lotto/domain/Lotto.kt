6799bbf0263816237cb8af7ab15e99da23c64438
whk06061 <gju06061@naver.com>
2023-02-15 16:32:20 +0900

    fix: 로또 생성시 에러 메시지를 수정한다.

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -2,9 +2,9 @@ package lotto.domain
 
 class Lotto(val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
-        require(numbers.all { it in 1..45 })
-        require(numbers.distinct().size == 6)
+        require(numbers.size == 6) { LOTTO_SIZE_ERROR }
+        require(numbers.all { it in 1..45 }) { LOTTO_RANGE_ERROR }
+        require(numbers.distinct().size == 6) { LOTTO_DUPLICATE_ERROR }
     }
 
     fun countMatchingNumbers(winningNumbers: List<Int>): Int {
@@ -13,5 +13,11 @@ class Lotto(val numbers: List<Int>) {
 
     fun checkMatchingBonusNumber(bonusNumber: Int): Boolean {
         return numbers.contains(bonusNumber)
+    }
+
+    companion object {
+        private const val LOTTO_SIZE_ERROR = "로또 번호는 여섯 개여야 합니다."
+        private const val LOTTO_RANGE_ERROR = "로또 번호는 1과 45 사이여야 합니다."
+        private const val LOTTO_DUPLICATE_ERROR = "로또 번호는 중복되면 안됩니다."
     }
 }
