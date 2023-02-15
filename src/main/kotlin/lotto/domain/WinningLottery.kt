19078c6bfd7c906226f1f160389c0b4c1fdb0bcc
otter <a33397397@gmail.com>
2023-02-15 22:13:08 +0900

    feat: 당첨 로또 번호와 보너스 번호가 중복되면 에러가 발생하는 기능 구현

--- src/main/kotlin/lotto/domain/WinningLottery.kt
+++ src/main/kotlin/lotto/domain/WinningLottery.kt
@@ -3,4 +3,16 @@ package lotto.domain
 class WinningLottery(
     val lottery: Lottery,
     val bonusNumber: LotteryNumber
-)
+) {
+    init {
+        checkBonusNumberDuplicate()
+    }
+
+    private fun checkBonusNumberDuplicate() {
+        require(!lottery.numbers.contains(bonusNumber)) { BONUS_NUMBER_DUPLICATE_ERROR }
+    }
+
+    companion object {
+        private const val BONUS_NUMBER_DUPLICATE_ERROR = "보너스 번호는 당첨 번호와 중복될 수 없습니다."
+    }
+}
--- src/test/kotlin/lotto/domain/WinningLotteryTest.kt
+++ src/test/kotlin/lotto/domain/WinningLotteryTest.kt
@@ -2,6 +2,7 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class WinningLotteryTest {
     @Test
@@ -16,7 +17,7 @@ class WinningLotteryTest {
                 LotteryNumber(45)
             )
         )
-        val bonusNumber = LotteryNumber(10)
+        val bonusNumber = LotteryNumber(5)
         val winningLottery = WinningLottery(lottery, bonusNumber)
 
         assertThat(winningLottery.lottery.numbers).containsAll(lottery.numbers)
@@ -38,5 +39,22 @@ class WinningLotteryTest {
         val winningLottery = WinningLottery(lottery, bonusNumber)
 
         assertThat(winningLottery.bonusNumber).isEqualTo(LotteryNumber(8))
+    }
+
+    @Test
+    fun `당첨 로또 번호와 보너스 번호가 중복되면 에러가 발생한다`() {
+        val lottery = Lottery(
+            listOf(
+                LotteryNumber(1),
+                LotteryNumber(10),
+                LotteryNumber(20),
+                LotteryNumber(30),
+                LotteryNumber(40),
+                LotteryNumber(45)
+            )
+        )
+        val bonusNumber = LotteryNumber(10)
+
+        assertThrows<IllegalArgumentException> { WinningLottery(lottery, bonusNumber) }
     }
 }
