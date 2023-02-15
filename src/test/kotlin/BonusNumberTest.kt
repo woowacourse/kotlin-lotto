a4e5be78dbb442012181dc566f9cf4da070bb176
hyunji1203 <hj27720@gmail.com>
2023-02-15 15:57:24 +0900

    test[BonusNumberTest]: 보너스 번호가 1에서 45 사이의 값이 아닌 경우 예외 처리 기능 테스트 추가

--- src/test/kotlin/BonusNumberTest.kt
+++ src/test/kotlin/BonusNumberTest.kt
@@ -2,6 +2,8 @@ import domain.BonusNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class BonusNumberTest {
 
@@ -15,6 +17,14 @@ class BonusNumberTest {
     fun `보너스 번호가 null일 경우`() {
         assertThrows<IllegalArgumentException> {
             BonusNumber(null)
+        }
+    }
+
+    @ValueSource(ints = [0, 46, -1])
+    @ParameterizedTest
+    fun `보너스 번호가 1에서 45 사이의 값이 아닌 경우`(number: Int) {
+        assertThrows<IllegalArgumentException> {
+            BonusNumber(number)
         }
     }
 }
