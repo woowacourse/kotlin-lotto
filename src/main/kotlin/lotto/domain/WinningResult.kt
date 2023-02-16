4086e5f2a8451699d3846d73e81e352a4b11feaf
changhwan <chang5hustle@gmail.com>
2023-02-16 17:42:39 +0900

    fix(lotto): winningResult 반환 오류 수정

--- src/main/kotlin/lotto/domain/WinningResult.kt
+++ src/main/kotlin/lotto/domain/WinningResult.kt
@@ -2,15 +2,23 @@ package lotto.domain
 
 import lotto.constant.Rank
 
-class WinningResult(val map: Map<Rank, Int>) {
+class WinningResult(private val map: Map<Rank, Int>) {
 
     override fun toString(): String {
         return map.keys.reversed().drop(1).joinToString(
             separator = WINNING_RESULT_TO_STRING_SEPARATOR,
-        ) { EACH_RANK_SCRIPT.format(it.matchCount, it.prizeMoney, map[it]) }
+        ) { findRankFormat(it) }
+    }
+
+    private fun findRankFormat(rank: Rank): String {
+        if (rank == Rank.SECOND) {
+            return SECOND_RANK_SCRIPT.format(rank.matchCount, rank.prizeMoney, map[rank])
+        }
+        return EACH_RANK_SCRIPT.format(rank.matchCount, rank.prizeMoney, map[rank])
     }
 
     companion object {
+        private const val SECOND_RANK_SCRIPT = "%d개 일치, 보너스 볼 일치(%d원)- %d개"
         private const val EACH_RANK_SCRIPT = "%d개 일치 (%d원)- %d개"
         private const val WINNING_RESULT_TO_STRING_SEPARATOR = "\n"
 
