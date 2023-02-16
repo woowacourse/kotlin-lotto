2a0e5f407609d4759fc98267d09d72494cbf51b8
no1msh <no1msh1217@gmail.com>
2023-02-16 17:38:58 +0900

    refactor: 메서드 분리

--- src/main/kotlin/domain/Bank.kt
+++ src/main/kotlin/domain/Bank.kt
@@ -2,23 +2,28 @@ package domain
 
 object Bank {
     fun getWinStatistics(winningResult: WinningResult): WinStatistics {
-        val winStatistics = WinStatistics(
-            mutableMapOf<Rank, Int>(
-                Rank.FIRST to 0,
-                Rank.SECOND to 0,
-                Rank.THIRD to 0,
-                Rank.FOURTH to 0,
-                Rank.FIFTH to 0
-        )
-        )
+        val winStatistics = initWinStatistics()
+        setWinStatistics(winningResult, winStatistics)
 
+        return winStatistics
+    }
+
+    private fun setWinStatistics(winningResult: WinningResult, winStatistics: WinStatistics){
         winningResult.comparingResults.forEach {
             val rank = Rank.valueOf(it.matchedCount, it.isBonusMatched)
             winStatistics.plusValue(rank,1)
         }
-
-        return winStatistics
     }
+
+    private fun initWinStatistics() = WinStatistics(
+        mutableMapOf<Rank, Int>(
+            Rank.FIRST to 0,
+            Rank.SECOND to 0,
+            Rank.THIRD to 0,
+            Rank.FOURTH to 0,
+            Rank.FIFTH to 0
+        )
+    )
 
     fun getEarningRate(totalPrize: Money, spendMoney: Money): Double {
         return totalPrize.amount / spendMoney.amount.toDouble()
