07fbefa72c8327780856eec774af2de5ba5b148f
hyunji1203 <hj27720@gmail.com>
2023-02-16 11:16:36 +0900

    feat[WinningResult]: 당첨금액과 입력금액에 대한 수익률 반환하는 기능 추가

--- src/main/kotlin/domain/WinningResult.kt
+++ src/main/kotlin/domain/WinningResult.kt
@@ -18,4 +18,8 @@ class WinningResult {
     fun getWinningMoney(): Int {
         return result.map { it.key.winningMoney * it.value }.sum()
     }
+
+    fun calculateYield(money: Money): Double {
+        return getWinningMoney() / money.price!!.toDouble()
+    }
 }
