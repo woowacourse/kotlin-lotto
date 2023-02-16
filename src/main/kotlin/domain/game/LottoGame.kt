333bb526ed02e6cb917b56e6356c4a4d9f348a17
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-16 17:49:15 +0900

    feat: LottoGame 클래스 기능 구현

---
+++ src/main/kotlin/domain/game/LottoGame.kt
@@ -0,0 +1,47 @@
+package domain.game
+
+import domain.lotto.PurchasedLotto
+import domain.lotto.WinningLotto
+import domain.lotto.number.LottoNumber
+import domain.rank.Rank
+import kotlin.math.floor
+
+class LottoGame(
+    private val lottoMachine: LottoMachine
+) {
+    private lateinit var winningLotto: WinningLotto
+    private var bonusNumber: LottoNumber? = null
+
+    fun initWinningLottoNumbers(_lottoNumbers: List<Int>, _bonusNumber: Int) {
+        bonusNumber = LottoNumber(_bonusNumber)
+        winningLotto = WinningLotto(_lottoNumbers.map { LottoNumber(it) }, bonusNumber!!)
+    }
+
+    fun purchaseLottos(money: Int): List<PurchasedLotto> = lottoMachine.purchaseLottos(money)
+
+    fun matchLottos(purchasedLottos: List<PurchasedLotto>): Map<Rank, Int> {
+        val matchResult = mutableMapOf<Rank, Int>()
+        purchasedLottos.forEach {
+            val rank = it.matchLotto(winningLotto, bonusNumber!!)
+            val originCount = matchResult.getOrDefault(rank, 0)
+
+            matchResult[rank] = originCount + 1
+        }
+
+        return matchResult
+    }
+
+    fun calculateIncomeRate(matchResult: Map<Rank, Int>, investMoney: Int): Double {
+        val income = calculateIncome(matchResult)
+        return floor((income / investMoney.toDouble()) * 100) / 100
+    }
+
+    private fun calculateIncome(matchResult: Map<Rank, Int>): Long {
+        var income = 0L
+
+        matchResult.entries.forEach { (rank, count) ->
+            income += (rank.winningMoney * count)
+        }
+        return income
+    }
+}
