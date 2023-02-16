9e45c62d1cd1d04bc2fb3353d2a3fdd6945f0f39
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-16 10:39:34 +0900

    refactor: BonusNumber 포함여부 검증을 WinningLotto 생성시 하도록 변경

--- src/main/kotlin/domain/lotto/WinningLotto.kt
+++ src/main/kotlin/domain/lotto/WinningLotto.kt
@@ -1,6 +1,10 @@
 package domain.lotto
 
-class WinningLotto(numbers: List<LottoNumber>) : Lotto(numbers) {
+class WinningLotto(numbers: List<LottoNumber>, bonusNumber: BonusNumber) : Lotto(numbers) {
+    init {
+        check(!this.contains(bonusNumber)) { ERROR_MESSAGE_CONTAINED_BONUS_NUMBER }
+    }
+
     fun matchLotto(purchasedLotto: PurchasedLotto, bonusNumber: BonusNumber): Pair<Int, Boolean> {
         val countOfMatch = matchPurchasedLotto(purchasedLotto)
         val matchBonus = matchBonusNumber(purchasedLotto, bonusNumber)
--- src/test/kotlin/domain/lotto/WinningLottoTest.kt
+++ src/test/kotlin/domain/lotto/WinningLottoTest.kt
@@ -18,6 +18,28 @@ class WinningLottoTest {
         Assertions.assertThat(expected).isEqualTo(matchedResult)
     }
 
+    @ParameterizedTest
+    @MethodSource("provideWinningLottoAndNotContainedBonusNumber")
+    fun `당첨번호와 6개의 당첨번호에 포함되지 않는 보너스 번호가 주어졌을 때, LottoTicket 객체 생성시, 예외가 발생하지 않는다`(
+        lottoNumbers: List<LottoNumber>,
+        bonusNumber: BonusNumber
+    ) {
+        assertDoesNotThrow {
+            WinningLotto(lottoNumbers, bonusNumber)
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideWinningLottoAndContainedBonusNumber")
+    fun `당첨번호와 6개의 당첨번호에 포함되는 보너스 번호가 주어졌을 때, WinningLotto 객체 생성시, IllegalStateException이 발생한다`(
+        lottoNumbers: List<LottoNumber>,
+        bonusNumber: BonusNumber
+    ) {
+        assertThrows<IllegalStateException> {
+            WinningLotto(lottoNumbers, bonusNumber)
+        }
+    }
+
     companion object {
         @JvmStatic
         fun provideWinningLottoAndPurchasedLottoAndMatchedResult(): List<Arguments> =
@@ -41,35 +63,63 @@ class WinningLottoTest {
                     Pair(5, false)
                 ),
                 Arguments.of(
-                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }),
+                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }, BonusNumber(10)),
                     BonusNumber(10),
                     PurchasedLotto(listOf(2, 4, 6, 8, 44, 45).map { number -> LottoNumber(number) }),
                     Pair(4, false)
                 ),
                 Arguments.of(
-                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }),
+                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }, BonusNumber(10)),
                     BonusNumber(10),
                     PurchasedLotto(listOf(2, 4, 6, 43, 44, 45).map { number -> LottoNumber(number) }),
                     Pair(3, false)
                 ),
                 Arguments.of(
-                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }),
+                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }, BonusNumber(10)),
                     BonusNumber(10),
                     PurchasedLotto(listOf(2, 4, 42, 43, 44, 45).map { number -> LottoNumber(number) }),
                     Pair(2, false)
                 ),
                 Arguments.of(
-                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }),
+                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }, BonusNumber(10)),
                     BonusNumber(10),
                     PurchasedLotto(listOf(2, 41, 42, 43, 44, 45).map { number -> LottoNumber(number) }),
                     Pair(1, false)
                 ),
                 Arguments.of(
-                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }),
+                    WinningLotto(listOf(2, 4, 6, 8, 12, 14).map { number -> LottoNumber(number) }, BonusNumber(10)),
                     BonusNumber(10),
                     PurchasedLotto(listOf(2, 4, 6, 10, 44, 45).map { number -> LottoNumber(number) }),
                     Pair(3, true)
                 ),
+            )
+
+        @JvmStatic
+        fun provideWinningLottoAndNotContainedBonusNumber(): List<Arguments> =
+            listOf(
+                Arguments.of((1..6).map { number -> LottoNumber(number) }, BonusNumber(7)),
+                Arguments.of(
+                    listOf(1, 3, 5, 7, 8, 9).map { number -> LottoNumber(number) },
+                    BonusNumber(45)
+                ),
+                Arguments.of(
+                    listOf(2, 4, 6, 8, 10, 12).map { number -> LottoNumber(number) },
+                    BonusNumber(3)
+                )
+            )
+
+        @JvmStatic
+        fun provideWinningLottoAndContainedBonusNumber(): List<Arguments> =
+            listOf(
+                Arguments.of((1..6).map { number -> LottoNumber(number) }, BonusNumber(6)),
+                Arguments.of(
+                    listOf(1, 3, 5, 7, 8, 9).map { number -> LottoNumber(number) },
+                    BonusNumber(8)
+                ),
+                Arguments.of(
+                    listOf(2, 4, 6, 8, 10, 12).map { number -> LottoNumber(number) },
+                    BonusNumber(12)
+                )
             )
     }
 }
