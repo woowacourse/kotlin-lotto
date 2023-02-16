aa442c922d003fe716c043e8e92ca2bf2a3b1cf2
Gloria <ippnsj@gmail.com>
2023-02-16 16:32:29 +0900

    feat: 당첨 결과를 계산하는 컨트롤러 기능 구현

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -4,8 +4,11 @@ import lotto.domain.LotteriesGenerator
 import lotto.domain.Lottery
 import lotto.domain.LotteryNumber
 import lotto.domain.LotteryNumberGenerator
+import lotto.domain.NumberOfRank
 import lotto.domain.PurchaseAmount
+import lotto.domain.Rank
 import lotto.domain.WinningLottery
+import lotto.domain.WinningResultCalculator
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -15,20 +18,48 @@ class Controller(
     private val outputView: OutputView
 ) {
     fun run() {
-        publishLotteries()
+        val purchase = getPurchase()
+        val lotteries = publishLotteries(purchase)
+        outputView.printInterval()
+
         val winningLottery = getWinningLottery()
+        outputView.printInterval()
+
+        val counter = getRankCounter(lotteries, winningLottery)
+        val profit = getProfit(purchase, counter.calculateTotalPrize())
     }
 
-    private fun publishLotteries() {
-        val money = PurchaseAmount(inputView.readPurchaseAmount())
-        val lotteries = LotteriesGenerator().generate(generator, money.getPurchaseQuantity())
+    private fun getPurchase(): PurchaseAmount {
+        return PurchaseAmount(inputView.readPurchaseAmount())
+    }
+
+    private fun publishLotteries(purchase: PurchaseAmount): List<Lottery> {
+        val lotteries = LotteriesGenerator().generate(generator, purchase.getPurchaseQuantity())
         outputView.printLotteries(lotteries)
-        outputView.printInterval()
+        return lotteries
     }
 
     private fun getWinningLottery(): WinningLottery {
         val winningNumbers = Lottery(inputView.readWinningNumbers().map { LotteryNumber(it) })
         val bonusNumber = LotteryNumber(inputView.readBonusNumber())
         return WinningLottery(winningNumbers, bonusNumber)
+    }
+
+    private fun getRankCounter(lotteries: List<Lottery>, winningLottery: WinningLottery): NumberOfRank {
+        val counter = NumberOfRank()
+        lotteries.forEach {
+            counter.increaseNumber(
+                Rank.valueOf(
+                    it.countMatches(winningLottery.lottery),
+                    it.containBonusNumber(winningLottery.bonusNumber)
+                )
+            )
+        }
+        return counter
+    }
+
+    private fun getProfit(purchase: PurchaseAmount, prize: Long): Double {
+        val calculator = WinningResultCalculator()
+        return calculator.calculateProfit(purchase, prize)
     }
 }
