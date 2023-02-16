c43f25ab252526b8615428d81caf9f50ed4d2f8b
s9hn <tnghqn@naver.com>
2023-02-16 15:41:48 +0900

    [FEAT(InputView)] 입력금액 널 에러처리

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,11 +1,18 @@
 package view
 
 private const val SEPARATOR = ","
-private const val INPUT_NULL = "[ERROR] null 값이 들어왔습니다."
+private const val INPUT_NULL = "[ERROR] null 값이 들어왔습니다. \n다시 입력해주세요"
 
 class InputView {
     fun inputMoney(): Int {
-        return readLine()?.toIntOrNull() ?: throw IllegalArgumentException(INPUT_NULL)
+        val input = runCatching {
+            readLine()?.toInt()
+        }.getOrNull()
+        if (input == null) {
+            println(INPUT_NULL)
+            return inputMoney()
+        }
+        return input
     }
 
     fun inputWinningNumbers(): List<String> {
