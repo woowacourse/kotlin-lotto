ed70e657c57878ae1a7a07c3572651ff690358e9
Doyeop Kim <dygames@naver.com>
2023-02-16 17:44:55 +0900

    test(LottoGeneratorTest): BeforeAll 적용

--- src/test/kotlin/lotto/model/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/model/LottoGeneratorTest.kt
@@ -2,26 +2,34 @@ package lotto.model
 
 import lotto.entity.Game
 import lotto.entity.Lotto
+import lotto.entity.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 internal class LottoGeneratorTest {
-    private lateinit var lottoGenerator: LottoGenerator
+    companion object {
+        private lateinit var lottoGenerator: LottoGenerator
 
-    @BeforeAll
-    fun init() {
-        val game = Game(
-            listOf(
-                Lotto(listOf(1, 2, 3, 4, 5, 6)),
+        @BeforeAll
+        @JvmStatic
+        fun init() {
+            val game = Game(
+                listOf(
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                )
             )
-        )
-        lottoGenerator = SequentialLottoNumberGenerator(game)
+            lottoGenerator = SequentialLottoNumberGenerator(game)
+        }
     }
 
     @Test
     fun `생성된 로또는 1,2,3,4,5,6이다`() {
         val lotto = lottoGenerator.generate()
-        assertThat(lotto.numbers == setOf(1, 2, 3, 4, 5, 6)).isTrue
+        val lottoNumbers = lotto.numbers.toList()
+
+        val targetLottoNumbers =
+            listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6))
+        assertThat(lottoNumbers).isEqualTo(targetLottoNumbers)
     }
 }
