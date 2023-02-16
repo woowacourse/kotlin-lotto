1ce6b26d17c70172a3cf4a0e49728f0dba4b8086
Doyeop Kim <dygames@naver.com>
2023-02-16 11:36:20 +0900

    fix(Lotto): Lotto와 WinNumber의 로직이 중복되어 WinNumber 삭제

--- src/main/kotlin/lotto/entity/Lotto.kt
+++ src/main/kotlin/lotto/entity/Lotto.kt
@@ -1,7 +1,14 @@
 package lotto.entity
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(val numbers: Set<LottoNumber>) {
+    init {
+        require(numbers.size == LOTTO_COUNT) { ERROR_MESSAGE_WIN_NUMBER_IS_SIX }
+        require(numbers.intersect(numbers).size == LOTTO_COUNT) { ERROR_MESSAGE_DUPLICATED_NUMBER }
+    }
+
     companion object {
+        private const val ERROR_MESSAGE_WIN_NUMBER_IS_SIX = "당첨 번호는 6개여야 합니다"
+        private const val ERROR_MESSAGE_DUPLICATED_NUMBER = "당첨 번호는 서로 중복될 수 없습니다"
         const val MINIMUM_LOTTO_NUMBER = 1
         const val MAXIMUM_LOTTO_NUMBER = 45
         const val LOTTO_MINIMUM_RANGE = 0
