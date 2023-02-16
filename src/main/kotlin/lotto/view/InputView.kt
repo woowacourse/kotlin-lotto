f34765ccbe771d6fcfa5fb64d9ee33c7e6a7983c
otter <a33397397@gmail.com>
2023-02-16 15:10:15 +0900

    feat: 입력 요청시 요청 문구를 출력하는 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,18 +7,27 @@ import lotto.domain.PurchaseAmount
 class InputView {
 
     fun readPurchaseAmount(): PurchaseAmount {
+        println(REQUIRE_MESSAGE_PURCHASE_AMOUNT)
         val input = readln()
         return PurchaseAmount(input.toInt())
     }
 
     fun readLottery(): Lottery {
+        println(REQUIRE_MESSAGE_WINNING_LOTTERY)
         val input = readln()
         val lotteryNumbers = input.split(", ").map { LotteryNumber(it.toInt()) }
         return Lottery(lotteryNumbers)
     }
 
     fun readBonusNumber(): LotteryNumber {
+        println(REQUIRE_MESSAGE_BONUS_NUMBER)
         val input = readln()
         return LotteryNumber(input.toInt())
+    }
+
+    companion object {
+        private const val REQUIRE_MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요."
+        private const val REQUIRE_MESSAGE_WINNING_LOTTERY = "지난 주 당첨 번호를 입력해 주세요."
+        private const val REQUIRE_MESSAGE_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     }
 }
