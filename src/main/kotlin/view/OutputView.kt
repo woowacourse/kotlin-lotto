0cbe166b3b758d91cf7a2981c9ef8f57e1d4d07f
hyunji1203 <hj27720@gmail.com>
2023-02-16 13:27:54 +0900

    feat(OutputView): 로또 몇 개를 구매했는지 출력하는 기능 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -5,7 +5,12 @@ class OutputView {
         println(OUTPUT_MONEY_MESSAGE)
     }
 
+    fun outputLottoSizeMessage(size: Int) {
+        println(size.toString() + OUTPUT_LOTTO_SIZE_MESSAGE)
+    }
+
     companion object {
         const val OUTPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
+        const val OUTPUT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다."
     }
 }
