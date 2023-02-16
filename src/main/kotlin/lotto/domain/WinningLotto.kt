4ed97f78e8f4fcb172c3faaaa380c4807533c06b
changhwan <chang5hustle@gmail.com>
2023-02-16 17:36:48 +0900

    feat(lotto): 중복 로또번호 오류 메시지 추가

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -14,13 +14,14 @@ class WinningLotto(val mainLottoNumbers: List<LottoNumber>, val bonusLottoNumber
     }
 
     private fun validateDuplicateLottoNumbers() {
-        require(combineLottoNumbers().distinctBy { lottoNumber -> lottoNumber.value }.size == TOTAL_LOTTO_SIZE)
+        require(combineLottoNumbers().distinctBy { lottoNumber -> lottoNumber.value }.size == TOTAL_LOTTO_SIZE) { LOTTO_DUPLICATE_ERROR }
     }
 
     private fun combineLottoNumbers(): List<LottoNumber> = mainLottoNumbers + bonusLottoNumber
 
     companion object {
         private const val LOTTO_SIZE_ERROR = "로또 숫자의 개수가 올바르지 않습니다."
+        private const val LOTTO_DUPLICATE_ERROR = "로또 메인 번호와 보너스 번호에 중복이 있습니다."
         private const val TOTAL_LOTTO_SIZE = LOTTO_SIZE + BONUS_LOTTO_NUMBER_SIZE
     }
 }
