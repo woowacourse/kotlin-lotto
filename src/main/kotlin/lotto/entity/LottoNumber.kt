e0d254beb93852f4dccfd6d98fb29250fabec256
Doyeop Kim <dygames@naver.com>
2023-02-16 11:52:45 +0900

    feat(LottoNumber): Lotto 번호를 개별으로 관리하는 LottoNumber 생성

---
+++ src/main/kotlin/lotto/entity/LottoNumber.kt
@@ -0,0 +1,11 @@
+package lotto.entity
+
+class LottoNumber(val value: Int) {
+    init {
+        require(value in (Lotto.MINIMUM_LOTTO_NUMBER..Lotto.MAXIMUM_LOTTO_NUMBER)) { ERROR_MESSAGE_RANGE_1_TO_45 }
+    }
+
+    companion object {
+        const val ERROR_MESSAGE_RANGE_1_TO_45 = "당첨 번호는 1에서 45 사이의 숫자여야 합니다"
+    }
+}
