83ef75265664660d5d828ca7cbbcd5f29225d648
changhwan <chang5hustle@gmail.com>
2023-02-16 14:15:26 +0900

    feat(lotto): 딩첨 결과 출력 및 수익률 출력 함수 작성

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,7 +5,8 @@ object OutputView {
     private const val GET_MAIN_LOTTO_NUMBERS_SCRIPT = "지난 주 당첨 번호를 입력해 주세요."
     private const val GET_BONUS_LOTTO_NUMBER_SCRIPT = "보너스 볼을 입력해 주세요."
     private const val WINNING_STATS_SCRIPT = "당첨 통계\n---------"
-
+    private const val PURCHASE_COUNT_SCRIPT = "%d개를 구매했습니다."
+    private const val YIELD_RATE_SCRIPT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
     fun printGetPurchaseMoneyScript() {
         println(GET_PURCHASE_MONEY_SCRIPT)
     }
@@ -20,5 +21,27 @@ object OutputView {
 
     fun printWinningStatsScript() {
         println(WINNING_STATS_SCRIPT)
+    }
+
+    fun printWinningResult(winningResult: String) {
+        println(winningResult)
+    }
+
+    fun printPurchaseResult(lottoBunch: String, purchaseCount: Int) {
+        printPurchaseCount(purchaseCount)
+        printLottoBunch(lottoBunch)
+    }
+
+    fun printYieldRate(yieldRate: Double) {
+        println(YIELD_RATE_SCRIPT.format(yieldRate))
+    }
+
+    private fun printLottoBunch(lottoBunch: String) {
+        println(lottoBunch)
+        println()
+    }
+
+    private fun printPurchaseCount(purchaseCount: Int) {
+        println(PURCHASE_COUNT_SCRIPT.format(purchaseCount))
     }
 }
