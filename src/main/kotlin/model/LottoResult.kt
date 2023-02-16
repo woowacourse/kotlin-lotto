29c36c71b41f0671ccbba18e8c7176a90c5ece46
s9hn <tnghqn@naver.com>
2023-02-16 17:50:07 +0900

    [REFACTOR(LottoResult)] 상수 익명객체변수 추가

--- src/main/kotlin/model/LottoResult.kt
+++ src/main/kotlin/model/LottoResult.kt
@@ -6,22 +6,23 @@ class LottoResult {
     var result: HashMap<Rank, Int> = hashMapOf()
 
     fun plusRankCount(rank: Rank, lottoResult: HashMap<Rank, Int>) {
-        lottoResult[rank] = (lottoResult[rank] ?: 0) + 1
+        lottoResult[rank] = (lottoResult[rank] ?: NULL) + 1
     }
 
     fun getProfitRate(result: HashMap<Rank, Int>): Double {
-        var profit = 0.00
-        var countOfTicket = 0.00
+        var profit = DOUBLE_ZERO
+        var countOfTicket = DOUBLE_ZERO
         result.forEach { (rank, count) ->
             profit += (rank.winningMoney * count)
             countOfTicket += count
         }
 
-        return (profit / (countOfTicket * 1000))
+        return (profit / (countOfTicket * ONE_LOTTO_PRICE))
     }
 
     companion object {
-        private const val COUNT_ONE = 1
-        private const val RESULT_CONTAINER_NULL_ERROR = "[ERROR] 비정상적인 오류가 발생했습니다."
+        private const val NULL = 0
+        private const val DOUBLE_ZERO = 0.00
+        private const val ONE_LOTTO_PRICE = 1000
     }
 }
