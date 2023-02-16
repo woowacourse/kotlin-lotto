fbf23012a5ecb6b123bb38827e1f31a94db2ba80
hyunji1203 <hj27720@gmail.com>
2023-02-16 13:35:41 +0900

    feat(OutputView): 생성된 로또를 출력하는 기능 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,5 +1,8 @@
 package view
 
+import domain.Lotto
+import domain.Lottos
+
 class OutputView {
     fun outputMoneyMessage() {
         println(OUTPUT_MONEY_MESSAGE)
@@ -9,8 +12,20 @@ class OutputView {
         println(size.toString() + OUTPUT_LOTTO_SIZE_MESSAGE)
     }
 
+    fun outputLottos(lottos: Lottos) {
+        lottos.lottos.forEach { lotto -> outputLotto(lotto) }
+        println()
+    }
+
+    fun outputLotto(lotto: Lotto) {
+        println(lotto.numbers!!.joinToString(prefix = PREFIX_MARK, separator = SEPERATOR_MARK, postfix = POSTFIX_MARK))
+    }
+
     companion object {
         const val OUTPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
         const val OUTPUT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다."
+        const val PREFIX_MARK = "["
+        const val SEPERATOR_MARK = ", "
+        const val POSTFIX_MARK = "]"
     }
 }
