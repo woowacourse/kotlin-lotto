87560f0d0da983e7e832e4472917f9a07537b421
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-16 15:29:24 +0900

    feat: 주어진 금액만큼 PurchasedLotto 리스트를 반환하는 기능 구현

---
+++ src/main/kotlin/domain/game/LottoGame.kt
@@ -0,0 +1,22 @@
+package domain.game
+
+import domain.lotto.Lotto
+import domain.lotto.PurchasedLotto
+import domain.lotto.number.LottoNumber
+import util.common.divideByThousand
+import util.common.generateDistinctRandomNumbers
+
+class LottoGame {
+    fun purchaseLottos(money: Int): List<PurchasedLotto> = mutableListOf<PurchasedLotto>().apply {
+        val lottoCount = money.divideByThousand()
+
+        repeat(lottoCount) {
+            add(purchaseLotto())
+        }
+    }
+
+    private fun purchaseLotto(): PurchasedLotto =
+        PurchasedLotto((LottoNumber.MIN_LOTTO_NUMBER..LottoNumber.MAX_LOTTO_NUMBER)
+            .generateDistinctRandomNumbers(Lotto.LOTTO_SIZE)
+            .map { LottoNumber(it) })
+}
---
+++ src/test/kotlin/domain/lotto/game/LottoGameTest.kt
@@ -0,0 +1,38 @@
+package domain.lotto.game
+
+import domain.game.LottoGame
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoGameTest {
+    private lateinit var lottoGame: LottoGame
+
+    @BeforeEach
+    fun setUp() {
+        lottoGame = LottoGame()
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["10000,10", "100,0", "12500, 12", "0,0"])
+    fun `0이상의 구매금액이 주어졌을 때, purchaseLotto 호출시, 구매금액을 1000으로 나눈 몫만큼 로또를 반환한다`(money: Int, expected: Int) {
+        val actual = lottoGame.purchaseLottos(money).size
+        assertEquals(expected, actual)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, -1000, -20000])
+    fun `0미만의 구매금액이 주어졌을 때, purchaseLotto 호출시, IllegalArgumentException이 발생한다`(money: Int) {
+        assertThrows<IllegalArgumentException> {
+            lottoGame.purchaseLottos(money)
+        }
+    }
+
+    @Test
+    fun `구매금액이 주어졌을 때, buyLotto 호출시, 구매금액을 1000으로 나눈 몫만큼 로또 리스트를 반환한다`() {
+    }
+}
