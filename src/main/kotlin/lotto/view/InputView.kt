8b28782c43ba15cddc4265639e3199fb7737b384
Gloria <ippnsj@gmail.com>
2023-02-16 15:15:02 +0900

    feat: 지난 주 당첨 번호 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -9,23 +9,40 @@ class InputView {
         while (!correctInput) {
             println(PURCHASE_AMOUNT_GUIDE)
             input = readLine()
-            correctInput = isInteger(input)
+            correctInput = isInteger(input, PURCHASE_AMOUNT_TYPE_ERROR)
         }
         return input.toInt()
+    }
+
+    fun readWinningNumbers(): List<Int> {
+        var correctInput = false
+        var inputs = listOf<String>()
+        while (!correctInput) {
+            println(WINNING_NUMBERS_GUIDE)
+            val input = readLine()
+            inputs = input.split(", ")
+            correctInput = isIntegerList(inputs, WINNING_NUMBERS_TYPE_ERROR)
+        }
+        return inputs.map { it.toInt() }
     }
 
     private fun readLine(): String {
         return readln()
     }
 
-    private fun isInteger(input: String): Boolean {
+    private fun isInteger(input: String, message: String): Boolean {
         return try {
-            requireNotNull(input.toIntOrNull()) { PURCHASE_AMOUNT_TYPE_ERROR }
+            requireNotNull(input.toIntOrNull()) { message }
             true
         } catch (e: IllegalArgumentException) {
             printError(e.message ?: "")
             false
         }
+    }
+
+    private fun isIntegerList(inputs: List<String>, message: String): Boolean {
+        val correctCount = inputs.map { isInteger(it, message) }.count { it }
+        return correctCount == inputs.size
     }
 
     private fun printError(message: String) {
@@ -36,5 +53,7 @@ class InputView {
         private const val ERROR_HEADER = "[ERROR]"
         private const val PURCHASE_AMOUNT_GUIDE = "구입금액을 입력해 주세요."
         private const val PURCHASE_AMOUNT_TYPE_ERROR = "구매금액은 정수여야 합니다."
+        private const val WINNING_NUMBERS_GUIDE = "지난 주 당첨 번호를 입력해 주세요."
+        private const val WINNING_NUMBERS_TYPE_ERROR = "당첨 번호는 정수여야 합니다."
     }
 }
