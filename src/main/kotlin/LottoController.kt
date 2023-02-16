9417ef8efc30cdf9e9c8aea9e501f8eb86bf63a3
no1msh <no1msh1217@gmail.com>
2023-02-16 17:39:31 +0900

    feat: controller 구현

---
+++ src/main/kotlin/LottoController.kt
@@ -0,0 +1,52 @@
+import domain.*
+import view.InputView
+import view.OutputView
+import view.UI
+
+class LottoController {
+
+    private val lottoGenerator: LottoGenerator = RandomLottoGenerator()
+
+    fun startLottoGame() {
+        val spendMoney = getMoney()
+        val lottoCount = LottoVendingMachine.getLottoCount(spendMoney)
+        OutputView.printPurchasedLottoCount(lottoCount)
+        val lottoBundle = LottoVendingMachine.getLottoBundle(lottoCount = lottoCount, lottoGenerator = lottoGenerator)
+        OutputView.printPurchasedLotto(lottoBundle)
+
+        produceResult(lottoBundle, spendMoney)
+    }
+
+    private fun produceResult(lottoBundle: LottoBundle, spendMoney: Money) {
+        val winningResult = Judgement.compareLottoBundle(getWinningNumbers(), lottoBundle)
+        val winStatistics = Bank.getWinStatistics(winningResult)
+
+        OutputView.printWinStatistics(winStatistics)
+        OutputView.printEarningRate(winStatistics.getTotalIncome(), spendMoney)
+    }
+
+    private fun getWinningNumbers(): WinningNumbers{
+        val winningLotto = getWinningLotto()
+        val bonusNumber = getBonusNumber()
+        return WinningNumbers(winningLotto, bonusNumber)
+    }
+
+    private fun getMoney(): Money {
+        UI.printRequestMoney()
+        val money = InputView.inputMoney()
+        return Money.create(money)
+    }
+
+    private fun getWinningLotto(): Lotto {
+        UI.printRequestWinningNumbers()
+        val winningNumbers = InputView.inputWinningNumbers()
+        return Lotto(winningNumbers)
+    }
+
+    private fun getBonusNumber(): LottoNumber {
+        UI.printRequestBonusNumber()
+        val bonusNumber = InputView.inputBonusNumber()
+        return LottoNumber.create(bonusNumber)
+    }
+
+}
