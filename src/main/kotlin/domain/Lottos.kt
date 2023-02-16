f49d78df308858b76c349f5d4d2d96c4c189a4ab
hyunji1203 <hj27720@gmail.com>
2023-02-16 10:40:55 +0900

    feat(Lottos): 당첨 결과 반환하는 기능 추가

--- src/main/kotlin/domain/Lottos.kt
+++ src/main/kotlin/domain/Lottos.kt
@@ -1,3 +1,13 @@
 package domain
 
-class Lottos(val lottos: List<Lotto>)
+class Lottos(val lottos: List<Lotto>) {
+    fun matchLottos(winningLotto: Lotto, bonusNumber: BonusNumber): WinningResult {
+        var winningResult = WinningResult()
+
+        lottos.forEach { lotto ->
+            var rank = lotto.matchLotto(winningLotto, bonusNumber)
+            winningResult.setWinnigResult(rank)
+        }
+        return winningResult
+    }
+}
