e652396ea7f5fa4e2e72d55fd8af0bfe1a981dd4
changhwan <chang5hustle@gmail.com>
2023-02-16 11:08:57 +0900

    feat(lotto): 개별 로또 출력 기능 작성

--- src/main/kotlin/lotto/domain/Lotto.kt
+++ src/main/kotlin/lotto/domain/Lotto.kt
@@ -16,6 +16,8 @@ class Lotto(val lottoNumbers: List<LottoNumber>) {
         require(lottoNumbers.distinctBy { it.value }.size == LOTTO_SIZE) { DUPLICATE_LOTTO_NUMBER_ERROR }
     }
 
+    override fun toString(): String = lottoNumbers.map { lottoNumber -> lottoNumber.value }.toString()
+
     companion object {
         private const val LOTTO_SIZE_ERROR = "로또 숫자의 개수가 올바르지 않습니다."
         private const val DUPLICATE_LOTTO_NUMBER_ERROR = "로또 숫자는 중복되면 안됩니다."
