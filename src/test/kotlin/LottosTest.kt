023a304fd9990ce1c34e591ddf49a59be794dd35
hyunji1203 <hj27720@gmail.com>
2023-02-16 10:40:08 +0900

    test[LottosTest]: 전체 당첨 결과 확인하는 테스트 추가

--- src/test/kotlin/LottosTest.kt
+++ src/test/kotlin/LottosTest.kt
@@ -1,6 +1,9 @@
+import domain.BonusNumber
 import domain.Lotto
 import domain.Lottos
+import domain.Money
 import domain.RandomLottoGenerator
+import domain.Rank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -8,7 +11,8 @@ class LottosTest {
 
     private val testLottos = listOf(
         Lotto(listOf(1, 2, 3, 4, 5, 6)),
-        Lotto(listOf(1, 2, 4, 6, 34, 45)), Lotto(listOf(3, 5, 15, 23, 33, 43)),
+        Lotto(listOf(1, 2, 4, 6, 34, 45)),
+        Lotto(listOf(3, 5, 15, 23, 33, 43)),
         Lotto(listOf(4, 6, 15, 18, 24, 35))
     )
     @Test
@@ -19,7 +23,19 @@ class LottosTest {
 
     @Test
     fun `금액에 맞게 로또 리스트가 생성되는지 확인`() {
-        val lottos = RandomLottoGenerator().generateLottos(10000)
+        val lottos = RandomLottoGenerator().generateLottos(Money(10000))
         assertThat(lottos.lottos.size).isEqualTo(10)
+    }
+
+    @Test
+    fun `전체 당첨 결과 확인`() {
+        val testLottos = Lottos(testLottos)
+        val winningLotto = Lotto(listOf(1, 2, 3, 4, 6, 10))
+        val bonusNumber = BonusNumber(45)
+        assertThat(testLottos.matchLottos(winningLotto, bonusNumber).result[Rank.FIRST]).isEqualTo(0)
+        assertThat(testLottos.matchLottos(winningLotto, bonusNumber).result[Rank.SECOND]).isEqualTo(0)
+        assertThat(testLottos.matchLottos(winningLotto, bonusNumber).result[Rank.THIRD]).isEqualTo(1)
+        assertThat(testLottos.matchLottos(winningLotto, bonusNumber).result[Rank.FOURTH]).isEqualTo(1)
+        assertThat(testLottos.matchLottos(winningLotto, bonusNumber).result[Rank.FIFTH]).isEqualTo(0)
     }
 }
