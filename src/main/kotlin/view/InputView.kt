618bb327ededc4f6d947dab2cc56df208de8d2f5
krrong <khk9664@naver.com>
2023-02-16 15:55:36 +0900

    fix(InputView): 입력한 값을 반환하지 않아 재귀에서 while문으로 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,38 +2,29 @@ package view
 
 class InputView : InputViewInterface {
     override fun getMoney(): Int {
-        println("구입 금액을 입력해 주세요.")
-        val money = readln()
-        var result = 0
-        runCatching { result = money.toInt() }
-            .onSuccess { return result }
-            .onFailure {
-                result = getMoney()
-            }
-        return result
+        while (true) {
+            println("구입 금액을 입력해 주세요.")
+            val money = readln()
+            runCatching { money.toInt() / 1000 }
+                .onSuccess { return it }
+        }
     }
 
     override fun getNumbers(): Set<Int> {
-        println("지난 주 당첨 번호를 입력해 주세요.")
-        val numbers = readln()
-        var result = setOf<Int>()
-        runCatching { result = numbers.split(",").map { it.toInt() }.toSet() }
-            .onSuccess { return result }
-            .onFailure {
-                result = getNumbers()
-            }
-        return result
+        while (true) {
+            println("지난 주 당첨 번호를 입력해 주세요.")
+            val numbers = readln()
+            runCatching { numbers.split(",").map { it.trim().toInt() }.toSet() }
+                .onSuccess { return it }
+        }
     }
 
     override fun getBonusNumber(): Int {
-        println("보너스 볼을 입력해 주세요.")
-        val bonusNumber = readln()
-        var result = 0
-        runCatching { result = bonusNumber.toInt() }
-            .onSuccess { return result }
-            .onFailure {
-                result = getBonusNumber()
-            }
-        return result
+        while (true) {
+            println("보너스 볼을 입력해 주세요.")
+            val bonusNumber = readln()
+            runCatching { bonusNumber.toInt() }
+                .onSuccess { return it }
+        }
     }
 }
