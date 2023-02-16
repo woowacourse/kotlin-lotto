403774fc0bf9eda8a9acdf784dcf5ba9ee0155e1
hyunji1203 <hj27720@gmail.com>
2023-02-16 10:42:19 +0900

    feat(WinningResult): 당첨 결과를 저장하는 클래스 생성

---
+++ src/main/kotlin/domain/WinningResult.kt
@@ -0,0 +1,17 @@
+package domain
+
+class WinningResult {
+
+    val result = mutableMapOf(
+        Rank.FIFTH to 0,
+        Rank.FOURTH to 0,
+        Rank.THIRD to 0,
+        Rank.SECOND to 0,
+        Rank.FIRST to 0,
+        Rank.MISS to 0
+    )
+
+    fun setWinnigResult(rank: Rank) {
+        result.put(rank, result.getOrDefault(rank, 0) + 1)
+    }
+}
