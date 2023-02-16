7a15cdc3e23748afd4d7ac468d1defb67146f656
Doyeop Kim <dygames@naver.com>
2023-02-16 15:49:29 +0900

    refactor(RandomLottoGenerator): 1~45의 숫자를 담은 List를 멤버 변수로 캐싱해둠

--- src/main/kotlin/lotto/model/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/model/RandomLottoGenerator.kt
@@ -3,8 +3,9 @@ package lotto.model
 import lotto.entity.Lotto
 
 class RandomLottoGenerator : LottoGenerator {
+    private val numbers = (Lotto.MINIMUM_LOTTO_NUMBER..Lotto.MAXIMUM_LOTTO_NUMBER).toList()
+
     override fun generate(): Lotto {
-        val numbers = (Lotto.MINIMUM_LOTTO_NUMBER..Lotto.MAXIMUM_LOTTO_NUMBER).toList()
         val shuffledNumbers = numbers.shuffled()
         return Lotto(shuffledNumbers.slice(Lotto.LOTTO_MINIMUM_RANGE until Lotto.LOTTO_COUNT).sorted())
     }
