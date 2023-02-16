9438bd03c0c66c74bf4e248933e9eda1e33097a5
no1msh <no1msh1217@gmail.com>
2023-02-16 17:39:15 +0900

    feat: 부생성자 구현

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -2,6 +2,8 @@ package domain
 
 class Lotto(val lottoNumbers : Set<LottoNumber>) {
 
+    constructor(lottoNumbers : List<Int>) : this(lottoNumbers.map { LottoNumber.create(it) }.toSet())
+
     init {
         require(lottoNumbers.size == 6){"$PREFIX 로또번호는 6개여야합니다."}
     }
