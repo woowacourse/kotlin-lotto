fc2ae9aec47659db1ac28f6c73116a1a1f4b529c
hyunji1203 <hj27720@gmail.com>
2023-02-16 11:15:54 +0900

    test[WinningResultTest]: 구입금액이 10000원일 때 4등, 5등 한번 씩 당첨됐을 경우 수익률 확인하는 테스트 추가

--- src/test/kotlin/WinnigResultTest.kt
+++ src/test/kotlin/WinnigResultTest.kt
@@ -1,3 +1,4 @@
+import domain.Money
 import domain.Rank
 import domain.WinningResult
 import org.assertj.core.api.Assertions.assertThat
@@ -11,5 +12,13 @@ class WinnigResultTest {
         winningresult.setWinnigResult(Rank.FIRST)
         winningresult.setWinnigResult(Rank.FOURTH)
         assertThat(winningresult.getWinningMoney()).isEqualTo(2000050000)
+    }
+
+    @Test
+    fun `구입금액이 10000원일 때 4등, 5등 한번 씩 당첨됐을 경우 수익률 확인`() {
+        val winningResult = WinningResult()
+        winningResult.setWinnigResult(Rank.FOURTH)
+        winningResult.setWinnigResult(Rank.FIFTH)
+        assertThat(winningResult.calculateYield(Money(10000))).isEqualTo(5.5)
     }
 }
