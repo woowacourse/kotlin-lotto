91c9d2fae13daee10db375f816a6c0d29c3d00b9
EmilyCh0 <emily9803@ewhain.net>
2023-02-16 14:50:24 +0900

    refactor: numbersRange 검증 메서드 분리

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -1,11 +1,11 @@
 class Lotto(val numbers: Set<Int>) {
     init {
-        require(
-            numbers.all { number ->
-                number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
-            }
-        ) { NUMBER_RANGE_ERROR }
-        require(numbers.size == NUMBER_COUNT)
+        require(numbers.checkNumbersRange()) {
+            NUMBER_RANGE_ERROR
+        }
+        require(numbers.size == NUMBER_COUNT) {
+            NUMBER_COUNT_ERROR
+        }
     }
 
     fun getLottoResult(winningNumbers: WinningNumbers): LottoResult {
@@ -16,12 +16,20 @@ class Lotto(val numbers: Set<Int>) {
     }
 
     private fun Set<Int>.getMatchCount(winningNumbers: WinningNumbers) =
-        this.count { winningNumbers.catchNumbers.contains(it) }
+        this.count { number ->
+            winningNumbers.catchNumbers.contains(number)
+        }
+
+    private fun Set<Int>.checkNumbersRange() =
+        this.all { number ->
+            number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
+        }
 
     companion object {
         private const val NUMBER_COUNT = 6
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
         private const val NUMBER_RANGE_ERROR = "[ERROR] 번호는 1 이상 45 이하입니다."
+        private const val NUMBER_COUNT_ERROR = "[ERROR] 로또 번호는 6개의 숫자로 이루어져야 합니다."
     }
 }
