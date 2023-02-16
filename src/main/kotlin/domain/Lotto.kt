5647bca881a01a28d09a6efa294c4a97c49fe391
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-17 00:38:13 +0900

    refactor (Lotto): 필요에 맞는 함수로 변경

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -7,9 +7,8 @@ class Lotto(private val numbers: Set<LottoNumber>) : Set<LottoNumber> by numbers
         require(numbers.size == NUMBER_SIZE) { ERROR_NUMBER_SIZE.format(numbers.size) }
     }
 
-    override fun toString(): String {
-        return numbers.map { it.number }.toString()
-    }
+    override fun toString(): String = numbers.joinToString("")
+
     companion object {
         const val NUMBER_SIZE = 6
         private const val ERROR_NUMBER_SIZE = "로또 번호의 개수는 6개여야 합니다. \n 잘못된 값 : %d"
