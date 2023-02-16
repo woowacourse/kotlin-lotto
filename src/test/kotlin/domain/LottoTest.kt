85a9a2ca12b17dc37599e17d433af5adf2e4c401
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-17 01:26:33 +0900

    refactor (Test): 테스트의 출력결과를 알 수 이게 변경

--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -14,15 +14,19 @@ class LottoTest {
 
     @Test
     fun `숫자가 6개가 아니면 에러가 발생한다`() {
+        val intArray = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+
         assertThatIllegalArgumentException()
-            .isThrownBy { Lotto(1, 24, 34, 35, 37, 45, 22) }
-            .withMessage("로또 번호의 개수는 6개여야 합니다. \n 잘못된 값 : 7")
+            .isThrownBy { Lotto(*intArray) }
+            .withMessage("로또 번호의 개수는 6개여야 합니다. \n 잘못된 값 : ${intArray.size}")
     }
 
     @Test
     fun `중복된 숫자 6개를 이용해 로또를 생성하면 에러가 발생한다`() {
+        val intArray = intArrayOf(1, 1, 1, 1, 1, 2)
+
         assertThatIllegalArgumentException()
             .isThrownBy { Lotto(1, 1, 1, 1, 1, 2) }
-            .withMessage("로또 번호의 개수는 6개여야 합니다. \n 잘못된 값 : 2")
+            .withMessage("로또 번호의 개수는 6개여야 합니다. \n 잘못된 값 : ${intArray.toSet().size}")
     }
 }
