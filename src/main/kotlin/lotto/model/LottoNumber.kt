1ab734d11931ffa1c31c5b84b64028e673f993b1
Sujin Kim <sujinkim919@gmail.com>
2023-02-16 11:29:40 +0900

    fix: 코드 변경에 따라 로또 번호가 비교되지 않는 부분을 해결하였다.
    - LottoNumber가 비교 가능한 객체가 되도록 하였다. (equals)

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -2,9 +2,17 @@ package lotto.model
 
 import lotto.view.ERROR_OUT_OF_RANGE
 
-class LottoNumber(val number: Int) {
+class LottoNumber(val number: Int): Comparable<LottoNumber> {
 
     init {
         require(number in 1..45) { ERROR_OUT_OF_RANGE }
+    }
+
+    override fun compareTo(other: LottoNumber): Int {
+        return number.compareTo(other.number)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return number == (other as LottoNumber).number
     }
 }
