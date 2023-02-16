abb757c74398a65597368e1409f9d267aba673ff
hyunji1203 <hj27720@gmail.com>
2023-02-16 14:25:38 +0900

    feat(OutputView): 당첨 결과를 출력하는 기능 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,6 +2,8 @@ package view
 
 import domain.Lotto
 import domain.Lottos
+import domain.Rank
+import domain.WinningResult
 
 class OutputView {
     fun outputMoneyMessage() {
@@ -29,6 +31,16 @@ class OutputView {
         println(OUTPUT_BONUS_NUMBER_MESSAGE)
     }
 
+    fun outputWinningResult(winningResult: WinningResult) {
+        println(OUTPUT_WINNING_RESULT_MESSAGE)
+        println(SEPERATE_MESSAGE)
+
+        val rankResult = listOf(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)
+        rankResult.forEach {
+            println(it.toString() + winningResult.result[it] + "개")
+        }
+    }
+
     companion object {
         const val OUTPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
         const val OUTPUT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다."
@@ -37,5 +49,7 @@ class OutputView {
         const val POSTFIX_MARK = "]"
         const val OUTPUT_WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
         const val OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
+        const val OUTPUT_WINNING_RESULT_MESSAGE = "당첨 통계"
+        const val SEPERATE_MESSAGE = "---------"
     }
 }
