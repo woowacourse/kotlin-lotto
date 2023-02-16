093e4da5e6656c96661d8e569729e2304a73539c
hyunji1203 <hj27720@gmail.com>
2023-02-16 13:41:51 +0900

    feat(OutputView): 보너스 번호 입력하는 메시지 출력 기능 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -25,6 +25,10 @@ class OutputView {
         println(OUTPUT_WINNING_LOTTO_MESSAGE)
     }
 
+    fun outputBonusNumberMessage() {
+        println(OUTPUT_BONUS_NUMBER_MESSAGE)
+    }
+
     companion object {
         const val OUTPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
         const val OUTPUT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다."
@@ -32,5 +36,6 @@ class OutputView {
         const val SEPERATOR_MARK = ", "
         const val POSTFIX_MARK = "]"
         const val OUTPUT_WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+        const val OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
     }
 }
