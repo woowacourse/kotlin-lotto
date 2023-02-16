a5d75be6a1b3ff31fcc7aec3702685d2e92b8971
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-17 01:44:39 +0900

    refactor (RankTest): CSVsource 활용하여 테스트 구현

--- src/test/kotlin/domain/RankTest.kt
+++ src/test/kotlin/domain/RankTest.kt
@@ -3,17 +3,19 @@ package domain
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RankTest {
 
     @ParameterizedTest
-    @MethodSource("getCountOfMatchAndMatchBonus")
+    @CsvSource(
+        value = [
+            "6 : false : FIRST", "5 : true : SECOND", "5 : false : THIRD", "4 : false : FOURTH", "3 : false : FIFTH", "2 : false : MISS",
+        ],
+        delimiter = ':',
+    )
     fun `번호 매치 개수와 보너스 번호 매치 여부를 받으면 그에 맞는 랭크를 반환할 수 있다`(countOfMatch: Int, matchBonus: Boolean, rank: Rank) {
         val result = Rank.valueOf(countOfMatch, matchBonus)
 
@@ -34,13 +36,4 @@ class RankTest {
             .isThrownBy { Rank.valueOf(6, true) }
             .withMessage("숫자가 6개 맞으면 보너스 번호가 매치될 수 없습니다.")
     }
-
-    private fun getCountOfMatchAndMatchBonus() = listOf(
-        Arguments.of(6, false, Rank.FIRST),
-        Arguments.of(5, true, Rank.SECOND),
-        Arguments.of(5, false, Rank.THIRD),
-        Arguments.of(4, false, Rank.FOURTH),
-        Arguments.of(3, false, Rank.FIFTH),
-        Arguments.of(2, false, Rank.MISS),
-    )
 }
