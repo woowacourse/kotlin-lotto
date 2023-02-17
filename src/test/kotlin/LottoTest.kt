f924e8dcefd96d4abe69c407d4384f365d9a83f3
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-17 17:53:00 +0900

    test : Lotto가 WinningNumber와 비교하면서 로또 당첨결과를 확인하는지 테스트

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,10 +1,14 @@
+import domain.model.LottoResult
+import domain.model.WinningNumbers
 import domain.model.lotto.Lotto
 import domain.model.lotto.LottoNumber
+import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
+
 
     @Test
     fun `로또의 번호가 6개인 경우`() {
@@ -37,5 +41,132 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(numbers)
         }
+    }
+
+    @Test
+    fun `로또의 번호가 6개가 일치하는 경우 (1등)`() {
+        //given
+        val numbers = setOf(
+            LottoNumber.from(1),
+            LottoNumber.from(2),
+            LottoNumber.from(3),
+            LottoNumber.from(4),
+            LottoNumber.from(5),
+            LottoNumber.from(6),
+        )
+        val lotto = Lotto(numbers)
+
+        //when
+        val actual = lotto.getLottoResult(
+            WinningNumbers(lotto, LottoNumber.from(7))
+        )
+
+        //then
+        assertThat(actual).isEqualTo(LottoResult.FIRST)
+    }
+
+    @Test
+    fun `로또의 번호가 5개가 일치하고 보너스 번호가 일치하는 경우 (2등)`() {
+        //given
+        val lotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(6)
+            )
+        )
+
+        val winningLotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(7)
+            )
+        )
+        val bonusNumber = LottoNumber.from(6)
+
+        val winningNumbers = WinningNumbers(winningLotto, bonusNumber)
+
+        //when
+        val actual = lotto.getLottoResult(winningNumbers)
+
+        //then
+        assertThat(actual).isEqualTo(LottoResult.SECOND)
+    }
+
+    @Test
+    fun `로또의 번호가 5개가 일치하고 보너스 번호가 일치하지 않는 경우 (3등)`() {
+        //given
+        val lotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(6)
+            )
+        )
+
+        val winningLotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(7)
+            )
+        )
+        val bonusNumber = LottoNumber.from(8)
+
+        val winningNumbers = WinningNumbers(winningLotto, bonusNumber)
+
+        //when
+        val actual = lotto.getLottoResult(winningNumbers)
+
+        //then
+        assertThat(actual).isEqualTo(LottoResult.THIRD)
+    }
+
+    @Test
+    fun `로또의 번호가 3개가 일치하는 경우 (5등)`() {
+        //given
+        val lotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(6),
+            )
+        )
+
+        val winningLotto = Lotto(
+            setOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(10),
+                LottoNumber.from(11),
+                LottoNumber.from(12),
+            )
+        )
+        val bonusNumber = LottoNumber.from(8)
+
+        val winningNumbers = WinningNumbers(winningLotto, bonusNumber)
+
+        //when
+        val actual = lotto.getLottoResult(winningNumbers)
+
+        //then
+        assertThat(actual).isEqualTo(LottoResult.FIFTH)
     }
 }
