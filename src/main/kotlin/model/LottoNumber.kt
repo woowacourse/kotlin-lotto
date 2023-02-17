74825fc5c625d998a9076bb936c70439f19e3976
chws0508 <chws0508@naver.com>
2023-02-17 15:01:03 +0900

    refactor : 데이터 관리를 위해 미리 1~45 의 instance 를 만들어서 꺼내 쓰는 형식으로 바꿈

--- src/main/kotlin/model/LottoNumber.kt
+++ src/main/kotlin/model/LottoNumber.kt
@@ -1,15 +1,21 @@
 package model
 
-class LottoNumber(val number: Int) {
+data class LottoNumber private constructor(val number: Int) {
 
     init {
-        require(number in range) { LOTTO_NUMBER_RANGE_ERROR_MESSAGE }
+        require(number in MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER) { LOTTO_NUMBER_RANGE_ERROR_MESSAGE }
     }
 
     companion object {
         private const val MINIMUM_LOTTO_NUMBER = 1
         private const val MAXIMUM_LOTTO_NUMBER = 45
-        private val range = MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER
         private const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호가 1~45가 아닙니다"
+
+        private val NUMBERS: Map<Int, LottoNumber> =
+            (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith(::LottoNumber)
+
+        fun from(value: Int): LottoNumber {
+            return NUMBERS[value] ?: throw IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE)
+        }
     }
 }
--- src/test/kotlin/model/LottoNumberTest.kt
+++ src/test/kotlin/model/LottoNumberTest.kt
@@ -9,7 +9,7 @@ class LottoNumberTest {
     @CsvSource("46", "0")
     fun `로또 번호가 1이상 45이하가 아니면 에러를 발생시킨다`(number: Int) {
         assertThrows<IllegalArgumentException> {
-            LottoNumber(number)
+            LottoNumber.from(number)
         }
     }
 }
