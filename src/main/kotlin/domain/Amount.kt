03ecf9e3c3e7cb62817704a98ac85edbe0f5cb9f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-17 15:26:21 +0900

    refactor (Amount): 원시값 래핑하기

---
+++ src/main/kotlin/domain/Amount.kt
@@ -0,0 +1,14 @@
+package domain
+
+class Amount private constructor(val value: Int) {
+    companion object {
+        private const val MINIMUM_AMOUNT = 1000
+        private const val MAXIMUM_AMOUNT = 100000
+        private const val ERROR_CREATE_COUNT = "구매 할 수 있는 금액은 1000원 이상 100000원 이하입니다.\n잘못된 값: %d"
+        private val AMOUNTS: MutableMap<Int, Amount> = mutableMapOf()
+        fun from(value: Int): Amount {
+            require(value in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) { ERROR_CREATE_COUNT.format(value) }
+            return AMOUNTS.getOrPut(value) { Amount(value) }
+        }
+    }
+}
--- src/main/kotlin/domain/LottoStore.kt
+++ src/main/kotlin/domain/LottoStore.kt
@@ -4,18 +4,11 @@ class LottoStore(
     private val lottoGenerator: LottoGenerator,
 ) {
 
-    fun buyLotto(amount: Int): List<Lotto> {
-        require(amount in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) { ERROR_CREATE_COUNT.format(amount) }
-        val lottoFactory = LottoFactory(lottoGenerator)
-        return lottoFactory.create(getCount(amount))
-    }
+    fun buyLotto(amount: Int): List<Lotto> = LottoFactory(lottoGenerator).create(getCount(Amount.from(amount)))
 
-    private fun getCount(amount: Int): Int = amount / LOTTO_PRICE
+    private fun getCount(amount: Amount): Int = amount.value / LOTTO_PRICE
 
     companion object {
-        private const val MINIMUM_AMOUNT = 1000
-        private const val MAXIMUM_AMOUNT = 100000
         const val LOTTO_PRICE = 1000
-        private const val ERROR_CREATE_COUNT = "구매 할 수 있는 금액은 1000원 이상 100000원 이하입니다.\n잘못된 값: %d"
     }
 }
