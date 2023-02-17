7808642558d3b4ab4c26712154999aa685265c5d
chws0508 <chws0508@naver.com>
2023-02-17 14:52:56 +0900

    refactor : ParameterizedTest 로 테스트

--- src/test/kotlin/model/LottoNumberTest.kt
+++ src/test/kotlin/model/LottoNumberTest.kt
@@ -1,18 +1,15 @@
 package model
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoNumberTest {
-
-    @Test
-    fun `당첨번호와 보너스번호는 1~45이다`() {
-        // given
-        val lottoNumber = 46
-
-        // then
+    @ParameterizedTest
+    @CsvSource("46", "0")
+    fun `로또 번호가 1이상 45이하가 아니면 에러를 발생시킨다`(number: Int) {
         assertThrows<IllegalArgumentException> {
-            LottoNumber(lottoNumber)
+            LottoNumber(number)
         }
     }
 }
