61a3f8ab1e524f331a8bf91ed4ade3c6f90466f6
mendel <rhthrhrl0@naver.com>
2023-02-17 16:18:36 +0900

    test(PurchaseLottoMoney): PurchaseLottoMoney 의 로또 구입 개수 반환에 대한 테스트 코드 추가

--- src/test/kotlin/domain/PurchaseMoneyTest.kt
+++ src/test/kotlin/domain/PurchaseMoneyTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -16,5 +17,11 @@ class PurchaseMoneyTest {
     fun `1000원 이상의 금액이면 에러를 반환하지 않는다`() {
         assertDoesNotThrow { PurchaseLottoMoney(1000) }
         assertDoesNotThrow { PurchaseLottoMoney(1500) }
+    }
+
+    @Test
+    fun `로또 구입 금액으로 구매 가능한 최대 로또의 개수가 일치하는지 검사`() {
+        assertThat(PurchaseLottoMoney(1500).purchaseCount).isEqualTo(1)
+        assertThat(PurchaseLottoMoney(2000).purchaseCount).isEqualTo(2)
     }
 }
