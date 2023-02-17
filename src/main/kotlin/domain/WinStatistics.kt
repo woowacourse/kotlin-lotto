d34d30fb85f0f41dd00f7816ffd6e146f75fcd14
briandr97 <briandr@naver.com>
2023-02-17 17:40:00 +0900

    refactor(WinStatistics): [리뷰 반영] mutable 타입 프로퍼티를 생성자에 넣으면 예상치 못하게 밖에서 값을 변경할 수 있다.
     - immutable한 값을 받아 init블록에서 mutable하게 바꿔 프로퍼티에 저장한다.
     - 이렇게 하면 생성자 프로퍼티에서 무조건 val, var로 선언해야하는 data class를 사용할 수 없으므로 class로 변환하고 equals와 hashCode를 오버라이드해었다.
     - equals와 hashCode를 직접 구현해야하는줄 알았는데 ide가 알아서 구현해줘서 놀랐다.

--- src/main/kotlin/domain/WinStatistics.kt
+++ src/main/kotlin/domain/WinStatistics.kt
@@ -1,19 +1,37 @@
 package domain
 
-data class WinStatistics(private val _rankCount: MutableMap<Rank, Int>) {
+class WinStatistics(_rankCount: Map<Rank, Int>) {
+    private val _rankCount: MutableMap<Rank, Int>
     val rankCount: Map<Rank, Int> get() = _rankCount
+
+    init {
+        this._rankCount = _rankCount.toMutableMap()
+    }
 
     fun plusValue(key: Rank, value: Int) {
         _rankCount[key] = (_rankCount[key] ?: 0).plus(value)
     }
 
     fun getTotalIncome(): Money {
-        var total: Long = 0L
+        return Money(
+            rankCount
+                .map { it.key.winningMoney * it.value.toLong() }
+                .sum(),
+        )
+    }
 
-        rankCount.forEach {
-            total += it.key.winningMoney * it.value.toLong()
-        }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-        return Money(total)
+        other as WinStatistics
+
+        if (rankCount != other.rankCount) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return rankCount.hashCode()
     }
 }
