2205c86117c49f654e707ca5e7990bccf020f035
mendel <rhthrhrl0@naver.com>
2023-02-17 14:37:44 +0900

    refactor(LottoNumber): LottoNumber에 팩토리 패턴 적용시켜서 객체 생성 비용 감소 및 동일성 제공

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -3,12 +3,16 @@ package domain
 import common.MAXIMUM_LOTTO_RANGE
 import common.MINIMUM_LOTTO_RANGE
 
-class LottoNumber(private val number: Int) {
-    init {
-        require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { ERROR_LOTTO_RANGE }
-    }
+class LottoNumber private constructor(val number: Int) {
 
     companion object {
         private const val ERROR_LOTTO_RANGE = "로또의 숫자는 ${MINIMUM_LOTTO_RANGE}에서 ${MAXIMUM_LOTTO_RANGE}사이의 숫자여야 합니다."
+        private val lottoNumbers: Map<Int, LottoNumber> =
+            (MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE).associateWith { LottoNumber(it) }
+
+        fun from(number: Int): LottoNumber {
+            require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { ERROR_LOTTO_RANGE }
+            return lottoNumbers[number]!!
+        }
     }
 }
--- src/test/kotlin/domain/LottoNumberTest.kt
+++ src/test/kotlin/domain/LottoNumberTest.kt
@@ -1,5 +1,6 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -8,14 +9,19 @@ import java.lang.IllegalArgumentException
 class LottoNumberTest {
     @Test
     fun `로또는 1에서 45 사이의 숫자다`() {
-        assertDoesNotThrow { LottoNumber(1) }
-        assertDoesNotThrow { LottoNumber(45) }
-        assertDoesNotThrow { LottoNumber(10) }
+        assertDoesNotThrow { LottoNumber.from(1) }
+        assertDoesNotThrow { LottoNumber.from(45) }
+        assertDoesNotThrow { LottoNumber.from(10) }
     }
 
     @Test
     fun `로또는 1에서 45 사이의 숫자가 아니면 에러발생한다`() {
-        assertThrows<IllegalArgumentException> { LottoNumber(0) }
-        assertThrows<IllegalArgumentException> { LottoNumber(46) }
+        assertThrows<IllegalArgumentException> { LottoNumber.from(0) }
+        assertThrows<IllegalArgumentException> { LottoNumber.from(46) }
+    }
+
+    @Test
+    fun `LottoNumber의 동일성 검사`() {
+        assertThat(LottoNumber.from(17)).isEqualTo(LottoNumber.from(17))
     }
 }
