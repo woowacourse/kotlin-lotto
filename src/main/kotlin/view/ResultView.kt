1c64961430c1a5149f9954733d3846c0aedef190
tmdgh1592 <tmdgh1592@naver.com>
2023-02-18 00:43:22 +0900

    refactor: 로또 게임 결과 수익을 출력을 Rank의 values() 메서드를 이용하여 출력하도록 변경

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -17,24 +17,30 @@ class ResultView {
         println(WINNING_RATE_TITLE)
         println(DIVIDER)
 
-        println(RANK_PRINT_FORMAT.format(Rank.FIFTH.countOfMatch, Rank.FIFTH.winningMoney, resultRank[Rank.FIFTH] ?: DEFAULT_MATCHED_SIZE))
-        println(RANK_PRINT_FORMAT.format(Rank.FOURTH.countOfMatch, Rank.FOURTH.winningMoney, resultRank[Rank.FOURTH] ?: DEFAULT_MATCHED_SIZE))
-        println(RANK_PRINT_FORMAT.format(Rank.THIRD.countOfMatch, Rank.THIRD.winningMoney, resultRank[Rank.THIRD] ?: DEFAULT_MATCHED_SIZE))
-        println(RANK_PRINT_FORMAT.format(Rank.SECOND.countOfMatch, Rank.SECOND.winningMoney, resultRank[Rank.SECOND] ?: DEFAULT_MATCHED_SIZE))
-        println(RANK_PRINT_FORMAT.format(Rank.FIRST.countOfMatch, Rank.FIRST.winningMoney, resultRank[Rank.FIRST] ?: DEFAULT_MATCHED_SIZE))
+        Rank.values().slice(0 until Rank.values().size - 1).reversed().forEach { rank ->
+            println(
+                RANK_PRINT_FORMAT.format(
+                    rank.countOfMatch,
+                    rank.winningMoney,
+                    resultRank[rank] ?: DEFAULT_MATCHED_SIZE
+                )
+            )
+        }
+
         print(TOTAL_INCOME_RATE_FORMAT.format(incomeRate))
-        if (incomeRate < 0.0) println(LOSS_MESSAGE)
+        if (incomeRate < LOSS_RATE_STANDARD) println(LOSS_MESSAGE)
     }
 
     companion object {
         private const val PURCHASED_LOTTO_SIZE_FORMAT = "%d개를 구매했습니다."
 
-        private const val WINNING_RATE_TITLE = "\n당첨 통계"
+        private const val WINNING_RATE_TITLE = "당첨 통계"
         private const val DIVIDER = "---------"
         private const val RANK_PRINT_FORMAT = "%d개 일치 (%d원) - %d개"
         private const val TOTAL_INCOME_RATE_FORMAT = "총 수익률은 %.2f입니다."
         private const val LOSS_MESSAGE = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
 
         private const val DEFAULT_MATCHED_SIZE = 0
+        private const val LOSS_RATE_STANDARD = 1.0F
     }
 }
