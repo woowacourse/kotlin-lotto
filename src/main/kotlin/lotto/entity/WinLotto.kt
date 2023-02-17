a06fc0b2b18fddc8d0d07236b3278dab6c402f05
Doyeop Kim <dygames@naver.com>
2023-02-17 18:06:58 +0900

    feat(WinLotto): 에러 메시지 입력값 표시

--- src/main/kotlin/lotto/entity/WinLotto.kt
+++ src/main/kotlin/lotto/entity/WinLotto.kt
@@ -2,10 +2,17 @@ package lotto.entity
 
 class WinLotto(val winNumber: Lotto, val bonus: LottoNumber) {
     init {
-        require(!winNumber.numbers.contains(bonus)) { ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER }
+        require(!winNumber.numbers.contains(bonus)) {
+            String.format(
+                ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER,
+                winNumber.numbers.toString(),
+                bonus
+            )
+        }
     }
 
     companion object {
-        private const val ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER = "보너스 번호와 당첨 번호는 중복될 수 없습니다"
+        private const val ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER =
+            "보너스 번호와 당첨 번호는 중복될 수 없습니다. 입력된 당첨 번호 : %s, 보너스 번호 : %d"
     }
 }
