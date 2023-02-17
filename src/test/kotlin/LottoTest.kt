2bab5490f99ce08e056b15fec8d8b57d89595cbe
EmilyCh0 <emily9803@ewhain.net>
2023-02-17 16:19:27 +0900

    test: 불필요한 테스트 제거

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -7,20 +7,6 @@ import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
-    @Test
-    fun `로또 번호는 1 이상 45 이하가 아닌 경우 예외가 발생한다`() {
-        val errorMessage = assertThrows<IllegalArgumentException> {
-            Lotto(setOf(1, 2, 3, 4, 5, 50))
-        }.message
-        assertThat(errorMessage).isEqualTo("[ERROR] 번호는 1 이상 45 이하입니다.")
-    }
-
-    @Test
-    fun `로또 번호는 1 이상 45 이하인 경우`() {
-        assertDoesNotThrow {
-            Lotto(setOf(1, 45, 10, 11, 12, 13))
-        }
-    }
 
     @Test
     fun `로또 번호는 6개가 아닌 경우 예외가 발생한다`() {
