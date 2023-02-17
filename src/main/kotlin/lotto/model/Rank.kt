4042e9850e435e2605f6bf5efa275bfc5d0ef8fb
Doyeop Kim <dygames@naver.com>
2023-02-17 18:30:12 +0900

    fix(Rank, LottoWinStatisticsFormatter): Rank의 countOfMatch를 IntRange에서 Int로 변경 그에 따라 determineSingle함수에서 MISS 검사하게 변경

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -3,27 +3,36 @@ package lotto.model
 import lotto.entity.Lotto
 import lotto.entity.WinLotto
 
-enum class Rank(val countOfMatch: IntRange, val needBonus: Boolean, val winningMoney: Int) {
-    FIRST(6..6, false, 2_000_000_000),
-    SECOND(5..5, true, 30_000_000),
-    THIRD(5..5, false, 1_500_000),
-    FOURTH(4..4, false, 50_000),
-    FIFTH(3..3, false, 5_000),
-    MISS(0..2, false, 0);
+enum class Rank(val countOfMatch: Int, val needBonus: Boolean, val winningMoney: Int) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0);
 
     companion object {
+        private const val ERROR_DETERMINED_RANK_IS_EMPTY = "등수는 반드시 하나 판별되어야 합니다. 판별된 등수 개수 : %d"
+
         fun determine(lotto: Lotto, winLotto: WinLotto): Rank {
-            return Rank.values().filter {
+            val determinedRank = Rank.values().filter {
                 determineSingle(it, lotto, winLotto)
-            }[0]
+            }
+            check(determinedRank.size == 1) {
+                String.format(ERROR_DETERMINED_RANK_IS_EMPTY, determinedRank.size)
+            }
+            return determinedRank[0]
         }
 
         private fun determineSingle(condition: Rank, lotto: Lotto, winLotto: WinLotto): Boolean {
-            var match = lotto.matchLottoNumberCount(winLotto.winNumber) in condition.countOfMatch
+            val countOfMatch = lotto.matchLottoNumberCount(winLotto.winNumber)
+            var isMatch = countOfMatch == condition.countOfMatch
+            if (condition == MISS && countOfMatch in 0..2)
+                return true
             if (condition.needBonus && !lotto.isMatchBonus(winLotto.bonus)) {
-                match = false
+                isMatch = false
             }
-            return match
+            return isMatch
         }
     }
 }
