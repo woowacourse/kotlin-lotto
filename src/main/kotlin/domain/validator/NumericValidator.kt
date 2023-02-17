44a4e7592099c5b8d454b59337d6496844f6ed50
EmilyCh0 <emily9803@ewhain.net>
2023-02-17 20:49:58 +0900

    refactor: NumericValidator 불필요한 코드 제거

--- src/main/kotlin/domain/validator/NumericValidator.kt
+++ src/main/kotlin/domain/validator/NumericValidator.kt
@@ -9,11 +9,6 @@ class NumericValidator {
         return input.toInt()
     }
 
-    private fun String.isNumeric(): Boolean =
-        this.chars().allMatch { char ->
-            Character.isDigit(char)
-        }
-
     companion object {
         private const val NUMERIC_ERROR = "[ERROR] 숫자가 아닌 입력은 허용하지 않습니다."
     }
