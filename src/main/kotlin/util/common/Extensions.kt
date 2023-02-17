a37b292a586ca86210ae6c44045dd6172c6b7237
tmdgh1592 <tmdgh1592@naver.com>
2023-02-18 01:21:44 +0900

    refactor: Extensions에서 divideByThousand 함수 제거

--- src/main/kotlin/util/common/Extensions.kt
+++ src/main/kotlin/util/common/Extensions.kt
@@ -1,9 +1,3 @@
 package util.common
 
 fun IntRange.generateDistinctRandomNumbers(count: Int): List<Int> = shuffled().take(count)
-
-fun Int.divideByThousand(): Int {
-    require(this >= 0) { "[ERROR] 입력값은 0이상이어야 합니다. " }
-
-    return this.div(1000)
-}
