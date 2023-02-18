49739f59fbfc02caaa0d60d3dd11711168f43a1e
krrong <khk9664@naver.com>
2023-02-18 23:18:00 +0900

    refactor(LottoSeller): 로또 한 장을 구매하는 함수 삭제

--- src/main/kotlin/domain/LottoSeller.kt
+++ src/main/kotlin/domain/LottoSeller.kt
@@ -1,10 +1,6 @@
 package domain
 
 class LottoSeller(private val lottoMachine: LottoMachine = RandomLottoMachine()) {
-    fun sellLotto(): Lotto {
-        return lottoMachine.create()
-    }
-
     fun sellLottos(count: Int): Ticket {
         return Ticket(List(count) { lottoMachine.create() })
     }
--- src/test/kotlin/domain/LottoSellerTest.kt
+++ src/test/kotlin/domain/LottoSellerTest.kt
@@ -5,15 +5,6 @@ import org.junit.jupiter.api.Test
 
 class LottoSellerTest {
     @Test
-    fun `로또 한 장을 발급한다`() {
-        val numberGenerator = TestLottoMachine(listOf(1, 2, 3, 4, 5, 6))
-        val lottoSeller = LottoSeller(numberGenerator)
-        val lotto = lottoSeller.sellLotto()
-        val expected = listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }
-        assertThat(lotto.numbers).containsAll(expected)
-    }
-
-    @Test
     fun `입력받은 개수만큼 로또를 발급한다`() {
         // given
         val numberGenerator = TestLottoMachine(listOf(1, 2, 3, 4, 5, 6))
