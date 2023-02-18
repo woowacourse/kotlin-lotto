2940231ba234a1a2ddbbb44a3783868efd8d43e9
SangHyun Moon <no1msh1217@gmail.com>
2023-02-18 21:45:30 +0900

    refactor: 생성자를 막아놓으면서 못쓰는 init 블록 대체 메서드 구현, create 메서드 테스트

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -2,18 +2,20 @@ package domain
 
 @JvmInline
 value class LottoNumber private constructor(val number: Int) {
-    init {
-        require(number in LOTTO_NUMBER_RANGE) { "$PREFIX 로또 넘버는 1~45여야합니다." }
-    }
 
     companion object {
-        const val PREFIX = "[Error]"
+        private const val PREFIX = "[Error]"
         private const val LOTTO_NUMBER_MINIMUM = 1
         private const val LOTTO_NUMBER_MAXIMUM = 45
-        val LOTTO_NUMBER_RANGE = LOTTO_NUMBER_MINIMUM..LOTTO_NUMBER_MAXIMUM
+        private val LOTTO_NUMBER_RANGE = LOTTO_NUMBER_MINIMUM..LOTTO_NUMBER_MAXIMUM
 
         fun create(number: Int): LottoNumber {
+            validateNumber(number)
             return LottoNumber(number)
+        }
+
+        private fun validateNumber(number: Int) {
+            require(number in LOTTO_NUMBER_RANGE) { "$PREFIX 로또 넘버는 1~45여야합니다." }
         }
     }
 }
--- src/test/kotlin/domain/LottoNumberTest.kt
+++ src/test/kotlin/domain/LottoNumberTest.kt
@@ -1,11 +1,24 @@
 package domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
-import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
+
+    @ValueSource(ints = [1, 2, 3, 4, 5])
+    @ParameterizedTest
+    fun `create를 사용하여 인자로 들어간 int값으로 LottoNumber를 만든다`(input: Int) {
+        // given
+
+        // when
+        val lottoNumber = LottoNumber.create(input)
+
+        // then
+        assertThat(lottoNumber.number).isEqualTo(input)
+    }
+
     @ValueSource(ints = [-1, 0, 46, 47])
     @ParameterizedTest
     fun `로또 번호가 1~45 밖에 있으면 에러 발생`(number: Int) {
@@ -16,18 +29,10 @@ class LottoNumberTest {
         // then
         assertThatIllegalArgumentException()
             .isThrownBy { LottoNumber.create(number) }
-            .withMessageContaining("${LottoNumber.PREFIX} 로또 넘버는 1~45여야합니다.")
+            .withMessageContaining("$PREFIX 로또 넘버는 1~45여야합니다.")
     }
 
-    @ValueSource(ints = [1, 2, 44, 45])
-    @ParameterizedTest
-    fun `로또 번호는 1~45 안에 있어야한다`(number: Int) {
-        // given
-
-        // when
-        val lottoNumber: LottoNumber = LottoNumber.create(number)
-
-        // then
-        assertDoesNotThrow { lottoNumber }
+    companion object {
+        private const val PREFIX = "[Error]"
     }
 }
