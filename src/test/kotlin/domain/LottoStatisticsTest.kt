b4b36fd1d075130538c3b821be0f88ca1fba5481
krrong <khk9664@naver.com>
2023-02-18 19:52:53 +0900

    refactor(LottoStatisticsTest): MethodSource 삭제, given으로 명시

--- src/test/kotlin/domain/LottoStatisticsTest.kt
+++ src/test/kotlin/domain/LottoStatisticsTest.kt
@@ -2,17 +2,13 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import java.lang.reflect.Method
-import java.util.stream.Stream
 
 class LottoStatisticsTest {
-    @ParameterizedTest(name = "{1}개가 일치하는 경우")
-    @MethodSource("provideLottoAndMatchCount")
-    fun `당첨 번호와 일치하는 숫자의 개수를 찾는다`(lotto: Lotto, expected: Int) {
+    @Test
+    fun `당첨 번호와 4개의 숫자가 일치하는 경우`() {
         // given
+        val lotto = Lotto(listOf(1, 2, 3, 4, 7, 8).map { number -> LottoNumber(number) }.toSet())
         val winningNumbers = listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()
         val bonusNumber = LottoNumber(7)
         val winningLotto = WinningLotto(Lotto(winningNumbers), bonusNumber)
@@ -27,13 +23,13 @@ class LottoStatisticsTest {
         val actual = method?.invoke(lottoStatistics, lotto)
 
         // then
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo(4)
     }
 
-    @ParameterizedTest(name = "{1}인 경우")
-    @MethodSource("provideLottoAndBonusMatchResult")
-    fun `보너스 번호와 일치하는지 여부를 확인한다`(lotto: Lotto, expected: Boolean) {
+    @Test
+    fun `보너스 번호와 일치하는 경우`() {
         // given
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toSet())
         val winningNumbers = listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()
         val bonusNumber = LottoNumber(7)
         val winningLotto = WinningLotto(Lotto(winningNumbers), bonusNumber)
@@ -48,13 +44,13 @@ class LottoStatisticsTest {
         val actual = method?.invoke(lottoStatistics, lotto)
 
         // then
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo(true)
     }
 
-    @ParameterizedTest(name = "{1}인 경우")
-    @MethodSource("provideLottoAndRankMatchResult")
-    fun `단일 로또를 넘겨 받아서 당첨 결과를 반환한다`(lotto: Lotto, expected: Rank) {
+    @Test
+    fun `단일 로또를 넘겨 받아서 당첨 결과를 반환한다`() {
         // given
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet())
         val winningNumbers = listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()
         val bonusNumber = LottoNumber(7)
         val winningLotto = WinningLotto(Lotto(winningNumbers), bonusNumber)
@@ -69,11 +65,12 @@ class LottoStatisticsTest {
         val actual = method?.invoke(lottoStatistics, lotto)
 
         // then
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo(Rank.FIRST)
     }
 
     @Test
     fun `Ticket을 넘겨 받아서 당첨 번호와 비교한다`() {
+        // given
         val ticket = Ticket(
             listOf(
                 Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()),
@@ -86,8 +83,11 @@ class LottoStatisticsTest {
         val bonusNumber = LottoNumber(13)
         val winningLotto = WinningLotto(Lotto(winningNumber), bonusNumber)
         val lottoStatistics = LottoStatistics(winningLotto)
+
+        // when
         val result: Map<Rank, Int> = lottoStatistics.compareTicket(ticket)
 
+        // then
         assertThat(result[Rank.FIRST]).isEqualTo(2)
         assertThat(result[Rank.SECOND]).isEqualTo(1)
         assertThat(result[Rank.THIRD]).isEqualTo(1)
@@ -108,63 +108,12 @@ class LottoStatisticsTest {
         val bonusNumber = LottoNumber(13)
         val winningLotto = WinningLotto(Lotto(winningNumber), bonusNumber)
         val lottoStatistics = LottoStatistics(winningLotto)
-        val result = lottoStatistics.calculateProfitToString(winResult)
         val expected = "0.35"
+
+        // when
+        val result = lottoStatistics.calculateProfitToString(winResult)
 
         // then
         assertThat(result).isEqualTo(expected)
-    }
-
-    companion object {
-        @JvmStatic
-        fun provideLottoAndMatchCount(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.arguments(Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()), 6),
-                Arguments.arguments(Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toSet()), 5),
-                Arguments.arguments(Lotto(listOf(1, 2, 3, 4, 8, 7).map { number -> LottoNumber(number) }.toSet()), 4),
-                Arguments.arguments(Lotto(listOf(1, 2, 3, 9, 8, 7).map { number -> LottoNumber(number) }.toSet()), 3),
-            )
-        }
-
-        @JvmStatic
-        fun provideLottoAndBonusMatchResult(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()),
-                    false
-                ),
-                Arguments.arguments(Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toSet()), true)
-            )
-        }
-
-        @JvmStatic
-        fun provideLottoAndRankMatchResult(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 4, 5, 6).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.FIRST
-                ),
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 4, 5, 7).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.SECOND
-                ),
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 4, 5, 10).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.THIRD
-                ),
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 4, 10, 11).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.FOURTH
-                ),
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 3, 11, 12, 45).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.FIFTH
-                ),
-                Arguments.arguments(
-                    Lotto(listOf(1, 2, 11, 12, 15, 45).map { number -> LottoNumber(number) }.toSet()),
-                    Rank.MISS
-                ),
-            )
-        }
     }
 }
