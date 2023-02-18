dea50afbe9779b7b5c293b8c62ff5231c989d4bf
krrong <khk9664@naver.com>
2023-02-19 00:54:21 +0900

    refactor(LottoStatisticsTest): private 함수 테스트가 아닌 기능 테스트로 변경

--- src/test/kotlin/domain/LottoStatisticsTest.kt
+++ src/test/kotlin/domain/LottoStatisticsTest.kt
@@ -2,71 +2,10 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.lang.reflect.Method
 
 class LottoStatisticsTest {
     @Test
-    fun `당첨 번호와 4개의 숫자가 일치하는 경우`() {
-        // given
-        val lotto = Lotto(1, 2, 3, 4, 7, 8)
-        val bonusNumber = LottoNumber(7)
-        val winningLotto = WinningLotto(Lotto(1, 2, 3, 4, 5, 6), bonusNumber)
-        val lottoStatistics = LottoStatistics(winningLotto)
-
-        val method = lottoStatistics.javaClass.declaredMethods.find {
-            it.name == "getCountOfMatch"
-        }
-        method?.isAccessible = true
-
-        // when
-        val actual = method?.invoke(lottoStatistics, lotto)
-
-        // then
-        assertThat(actual).isEqualTo(4)
-    }
-
-    @Test
-    fun `보너스 번호와 일치하는 경우`() {
-        // given
-        val lotto = Lotto(1, 2, 3, 4, 5, 7)
-        val bonusNumber = LottoNumber(7)
-        val winningLotto = WinningLotto(Lotto(1, 2, 3, 4, 5, 6), bonusNumber)
-        val lottoStatistics = LottoStatistics(winningLotto)
-
-        val method: Method? = lottoStatistics.javaClass.declaredMethods.find {
-            it.name == "isBonusNumberMatch"
-        }
-        method?.isAccessible = true
-
-        // when
-        val actual = method?.invoke(lottoStatistics, lotto)
-
-        // then
-        assertThat(actual).isEqualTo(true)
-    }
-
-    @Test
-    fun `단일 로또를 넘겨 받아서 당첨 결과를 반환한다`() {
-        // given
-        val lotto = Lotto(1, 2, 3, 4, 5, 6)
-        val bonusNumber = LottoNumber(7)
-        val winningLotto = WinningLotto(Lotto(1, 2, 3, 4, 5, 6), bonusNumber)
-        val lottoStatistics = LottoStatistics(winningLotto)
-
-        val method: Method? = lottoStatistics.javaClass.declaredMethods.find {
-            it.name == "getRank"
-        }
-        method?.isAccessible = true
-
-        // when
-        val actual = method?.invoke(lottoStatistics, lotto)
-
-        // then
-        assertThat(actual).isEqualTo(Rank.FIRST)
-    }
-
-    @Test
-    fun `Ticket을 넘겨 받아서 당첨 번호와 비교한다`() {
+    fun `Ticket을 넘겨 받아서 당첨 결과를 확인한다`() {
         // given
         val ticket = Ticket(
             listOf(
