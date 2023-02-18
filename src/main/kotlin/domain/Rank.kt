c33ff7938cd6eeb9d9f7230fa04f25f9429d6201
hyemdooly <hyemdooly@gmail.com>
2023-02-18 18:36:06 +0900

    refactor(Rank): valueOf함수 일관적인 코드로 수정

--- src/main/kotlin/domain/Rank.kt
+++ src/main/kotlin/domain/Rank.kt
@@ -1,12 +1,12 @@
 package domain
 
-enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(5, 1_500_000),
-    FOURTH(4, 50_000),
-    FIFTH(3, 5_000),
-    MISS(0, 0),
+enum class Rank(val countOfMatch: Int, val mustHaveBonus: Boolean, val winningMoney: Int) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0),
     ;
 
     companion object {
@@ -19,13 +19,11 @@ enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
                 )
             }
             require(countOfMatch != max || matchBonus.not()) { ERROR_IMPOSSIBLE_CASE }
-            return when (countOfMatch) {
-                6 -> FIRST
-                5 -> if (matchBonus) SECOND else THIRD
-                4 -> FOURTH
-                3 -> FIFTH
-                else -> MISS
-            }
+
+            val matchCount = values().filter { it.countOfMatch == countOfMatch }
+            if (matchCount.size > 1) return matchCount.filter { it.mustHaveBonus == matchBonus }[0]
+            if (matchCount.isEmpty()) return MISS
+            return matchCount[0]
         }
 
         private const val ERROR_COUNT_OF_MATCH_RANGE = "맞는 숫자의 개수는 0 이상 6 이하여야 합니다.\n잘못된 값: %d"
