1adc65715b325c1aa7ab2e30b3148dd668aea94c
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-18 16:43:08 +0900

    refactor: 입력된 금액에 맞게 로또를 생성해주는 기능 리팩토링

--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -3,15 +3,14 @@ package domain
 class RandomLottoGenerator : LottoGenerator {
 
     override fun generateLottos(money: Money): Lottos {
-        val count = money.price / LOTTO_PRICE
+        val count = money.getLottoCount()
         val lottos = mutableListOf<Lotto>()
         repeat(count) { lottos.add(generateLotto()) }
         return Lottos(lottos)
     }
 
     fun generateLotto(): Lotto {
-        val lotto = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).shuffled().subList(0, LOTTO_LIMIT_SIZE)
-        return Lotto(lotto.sorted())
+        return Lotto((LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).shuffled().subList(0, LOTTO_LIMIT_SIZE).map { LottoNumber.from(it) })
     }
 
     companion object {
--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -2,6 +2,8 @@ import domain.Money
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class MoneyTest {
 
@@ -23,5 +25,14 @@ class MoneyTest {
         assertThrows<IllegalArgumentException> {
             Money(925)
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["10000,10", "12000,12", "50000,50"])
+    fun `입력된 금액으로 로또를 몇 개 살 수 있는지 확인`(money: Int, count: Int) {
+        val money = Money(money)
+        val actual = money.getLottoCount()
+
+        assertThat(actual).isEqualTo(count)
     }
 }
