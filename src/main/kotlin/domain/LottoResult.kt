850729d305dddb8a5f64231c5910e971210b516e
hyemdooly <hyemdooly@gmail.com>
2023-02-18 17:06:25 +0900

    refactor(LottoResult): 가독성 수정

--- src/main/kotlin/domain/LottoResult.kt
+++ src/main/kotlin/domain/LottoResult.kt
@@ -7,14 +7,12 @@ data class LottoResult(private val result: Map<Rank, Int>) : Map<Rank, Int> by r
         require(result.values.sum() > 0) { ERROR_COUNT_OF_LOTTO_AT_LEAST_ONE }
     }
 
-    fun getRateOfReturn(): Double =
-        floor(
-            result.map {
-                it.key.winningMoney.toDouble() * it.value / (
-                    result.values.sum() * LottoStore.LOTTO_PRICE
-                    )
-            }.sum() * 100
-        ) / 100
+    fun getRateOfReturn(): Double {
+        val total = result.values.sum() * LottoStore.LOTTO_PRICE
+        return floor(result.map { getSumMoney(it).toDouble() / total }.sum() * 100) / 100
+    }
+
+    private fun getSumMoney(entry: Map.Entry<Rank, Int>): Int = entry.key.winningMoney * entry.value
 
     companion object {
         private const val ERROR_COUNT_OF_LOTTO_AT_LEAST_ONE = "적어도 하나 이상의 복권을 넣어야합니다."
