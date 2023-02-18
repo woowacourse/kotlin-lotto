e6c5f67aecc03b0e40bb7c691114ea2b5f61510c
hyemdooly <hyemdooly@gmail.com>
2023-02-18 17:14:12 +0900

    refactor(LottoNumber): 팩토리 함수 구현하여 객체를 재사용하도록 수정

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -10,7 +10,8 @@ data class LottoNumber(val number: Int) {
     companion object {
         private const val MINIMUM_NUMBER = 1
         private const val MAXIMUM_NUMBER = 45
-
         private const val ERROR_NUMBER_IN_RANGE = "로또 숫자는 1부터 45사이어야합니다. \n 잘못된 값: %d"
+        private val NUMBERS = (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith { LottoNumber(it) }
+        fun from(number: Int): LottoNumber = NUMBERS[number] ?: LottoNumber(number)
     }
 }
--- src/test/kotlin/domain/LottoNumberTest.kt
+++ src/test/kotlin/domain/LottoNumberTest.kt
@@ -10,12 +10,12 @@ class LottoNumberTest {
     @ValueSource(ints = [1, 45])
     @ParameterizedTest
     fun `1부터 45까지 번호를 받아 생성할 수 있다`(int: Int) {
-        assertDoesNotThrow { LottoNumber(int) }
+        assertDoesNotThrow { LottoNumber.from(int) }
     }
 
     @ValueSource(ints = [0, 46])
     @ParameterizedTest
     fun `1부터 45까지 이외에 숫자를 받으면 에러가 발생한다`(int: Int) {
-        assertThrows<IllegalArgumentException> { LottoNumber(int) }
+        assertThrows<IllegalArgumentException> { LottoNumber.from(int) }
     }
 }
