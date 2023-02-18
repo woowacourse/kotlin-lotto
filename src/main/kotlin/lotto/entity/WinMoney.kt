a358ba344f5267712cbc0e32cb3fd82fd1a51919
DYGames <dygames@naver.com>
2023-02-18 20:19:20 +0900

    feat(WinMoney): WinMoney는 생성자로도 생성 될 수 있게 변경

--- src/main/kotlin/lotto/entity/WinMoney.kt
+++ src/main/kotlin/lotto/entity/WinMoney.kt
@@ -1,6 +1,6 @@
 package lotto.entity
 
-class WinMoney private constructor(value: Int) : Money(value) {
+class WinMoney (value: Int) : Money(value) {
     companion object {
         fun from(winStatistics: WinStatistics): WinMoney =
             WinMoney(winStatistics.value.sumOf { it.winningMoney.value })
