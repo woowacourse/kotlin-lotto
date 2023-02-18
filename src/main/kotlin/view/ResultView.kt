f1f708af3a89e9380b0448adc3bb073d4ef5f229
krrong <khk9664@naver.com>
2023-02-18 23:50:17 +0900

    refactor(ResultView): 자료형의 의미를 분명히 알 수 있도록 파라미터명 변경

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -3,15 +3,15 @@ package view
 import domain.Rank
 
 class ResultView : ResultViewInterface {
-    override fun printResult(statisticsResult: Map<Rank, Int>, profit: String) {
-        printStatistics(statisticsResult)
+    override fun printResult(winningCountBy: Map<Rank, Int>, profit: String) {
+        printStatistics(winningCountBy)
         printProfit(profit)
     }
 
-    override fun printStatistics(statisticsResult: Map<Rank, Int>) {
+    override fun printStatistics(winningCountBy: Map<Rank, Int>) {
         println("당첨 통계\n---------")
         Rank.values().reversed().forEach { rank ->
-            val winningCount = statisticsResult[rank]!!
+            val winningCount = winningCountBy[rank]!!
             printWinningCount(rank, winningCount)
         }
     }
@@ -20,10 +20,10 @@ class ResultView : ResultViewInterface {
         println("총 수익률은 ${profit}입니다.")
     }
 
-    private fun printBonus(rank: Rank) = if (rank == Rank.SECOND) ", 보너스 볼 일치" else " "
+    private fun isMatch(rank: Rank) = if (rank == Rank.SECOND) ", 보너스 볼 일치" else " "
 
     private fun printWinningCount(rank: Rank, winningCount: Int) {
         if (rank != Rank.MISS)
-            println("${rank.countOfMatch}개 일치${printBonus(rank)}(${rank.winningMoney}원)- ${winningCount}개")
+            println("${rank.countOfMatch}개 일치${isMatch(rank)}(${rank.winningMoney}원)- ${winningCount}개")
     }
 }
--- src/main/kotlin/view/ResultViewInterface.kt
+++ src/main/kotlin/view/ResultViewInterface.kt
@@ -3,8 +3,8 @@ package view
 import domain.Rank
 
 interface ResultViewInterface {
-    fun printResult(statisticsResult: Map<Rank, Int>, profit: String)
+    fun printResult(winningCountBy: Map<Rank, Int>, profit: String)
 
-    fun printStatistics(statisticsResult: Map<Rank, Int>)
+    fun printStatistics(winningCountBy: Map<Rank, Int>)
     fun printProfit(profit: String)
 }
