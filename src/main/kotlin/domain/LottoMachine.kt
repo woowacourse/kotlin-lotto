5a2731b8158fc536d8257741ca9c67ab6b73d284
krrong <khk9664@naver.com>
2023-02-19 01:00:01 +0900

    refactor(LottoNumber): 1과 45를 LottoNumber가 가지도록 변경

--- src/main/kotlin/domain/LottoMachine.kt
+++ src/main/kotlin/domain/LottoMachine.kt
@@ -1,8 +1,5 @@
 package domain
 
-import common.MAXIMUM_LOTTO_RANGE
-import common.MINIMUM_LOTTO_RANGE
-
 interface LottoMachine {
-    fun create(start: Int = MINIMUM_LOTTO_RANGE, end: Int = MAXIMUM_LOTTO_RANGE): Lotto
+    fun create(start: Int = LottoNumber.valueOfMinRange(), end: Int = LottoNumber.valueOfMaxRange()): Lotto
 }
