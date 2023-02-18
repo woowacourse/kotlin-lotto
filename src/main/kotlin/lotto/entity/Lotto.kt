c69328384d84609b240052051f203d601ec26ddf
DYGames <dygames@naver.com>
2023-02-18 18:31:54 +0900

    fix(Lotto): Set끼리의 중복을 검사하는 불필요한 코드 제거

--- src/main/kotlin/lotto/entity/Lotto.kt
+++ src/main/kotlin/lotto/entity/Lotto.kt
@@ -5,7 +5,6 @@ import lotto.model.Rank
 class Lotto(val numbers: Set<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_COUNT) { String.format(ERROR_MESSAGE_WIN_NUMBER_IS_SIX, numbers.size) }
-        require(numbers.intersect(numbers).size == LOTTO_COUNT) { ERROR_MESSAGE_DUPLICATED_NUMBER }
     }
 
     constructor(numbers: List<Int>) : this(numbers.map { LottoNumber.from(it) }.toSet())
@@ -27,7 +26,6 @@ class Lotto(val numbers: Set<LottoNumber>) {
 
     companion object {
         private const val ERROR_MESSAGE_WIN_NUMBER_IS_SIX = "로또 번호는 6개여야 합니다. 입력된 로또 번호 개수는 %d 입니다."
-        private const val ERROR_MESSAGE_DUPLICATED_NUMBER = "로또 번호는 서로 중복될 수 없습니다"
         const val MINIMUM_LOTTO_NUMBER = 1
         const val MAXIMUM_LOTTO_NUMBER = 45
         const val LOTTO_COUNT = 6
