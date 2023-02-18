d316aa1c6f629391dd433e8ed5f9391371419e93
chws0508 <chws0508@naver.com>
2023-02-18 20:37:26 +0900

    refactor : LottoMachineTest 테스트 구체화

--- src/test/kotlin/domain/LottoMachineTest.kt
+++ src/test/kotlin/domain/LottoMachineTest.kt
@@ -1,15 +1,31 @@
 package domain
 
+import model.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoMachineTest {
 
     @Test
-    fun `1~45의 랜덤번호 6개를 추출한다`() {
+    fun `generator가 (1,2,3,4,5,6)인 리스트를 생성했을 때 로또 번호가(1,2,3,4,5,6)인 로또가 생성된다`() {
         // when
-        val actual = ShuffledNumberGenerator().generate()
+        val actual = LottoMachine(TestLottoNumbersGenerator()).generateLotto().lottoNumbers
+        // then
+        val expected = listOf(
+            LottoNumber.from(1),
+            LottoNumber.from(2),
+            LottoNumber.from(3),
+            LottoNumber.from(4),
+            LottoNumber.from(5),
+            LottoNumber.from(6),
+        )
 
-        assertThat(actual.size).isEqualTo(6)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    class TestLottoNumbersGenerator() : NumbersGenerator {
+        override fun generate(): List<Int> {
+            return listOf(1, 2, 3, 4, 5, 6)
+        }
     }
 }
