7634e4e0cd821353309d20c2f1d496f5d00deb3b
hyunji1203 <hj27720@gmail.com>
2023-02-19 01:45:11 +0900

    refactor(RandomLottoGenerator): 상수화

--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -9,9 +9,9 @@ class RandomLottoGenerator : LottoGenerator {
     }
 
     private fun generateLotto(): Lotto {
-        val randomNumbers = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).shuffled().subList(0, LOTTO_LIMIT_SIZE).sorted()
-        val test = randomNumbers.map { LottoNumber.from(it) }
-        return Lotto(test)
+        val randomNumbers = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).shuffled().subList(LOTTO_MIN_SIZE, LOTTO_LIMIT_SIZE).sorted()
+        val lotto = randomNumbers.map { LottoNumber.from(it) }
+        return Lotto(lotto)
     }
 
     companion object {
@@ -19,5 +19,6 @@ class RandomLottoGenerator : LottoGenerator {
         const val LOTTO_LIMIT_SIZE = 6
         const val LOTTO_MIN_NUMBER = 1
         const val LOTTO_MAX_NUMBER = 45
+        const val LOTTO_MIN_SIZE = 0
     }
 }
