f7f919072435c55664d8b9be4370aa6f4ac7173b
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-19 20:37:57 +0900

    refactor: 1000원 단위의 돈 검증을 PurchaseMoney 클래스에서 하도록 수정

--- src/main/kotlin/domain/model/PurchaseMoney.kt
+++ src/main/kotlin/domain/model/PurchaseMoney.kt
@@ -7,11 +7,20 @@ class PurchaseMoney(
         require(money > ZERO) {
             MONEY_RANGE_ERROR
         }
+        require(money.validateMoneyUnit()){
+            MONEY_UNIT_ERROR
+        }
     }
+
+    private fun Int.validateMoneyUnit() = this % LOTTO_PRICE == 0
+
+    fun getLottosCountToPurchase(): Int = money / LOTTO_PRICE
 
     companion object {
         private const val ZERO = 0
+        private const val LOTTO_PRICE = 1000
 
+        private const val MONEY_UNIT_ERROR = "[ERROR] 천원 단위의 돈을 입력해주세요."
         private const val MONEY_RANGE_ERROR = "[ERROR] 구입 금액은 0원보다 커야합니다."
     }
 }
--- src/test/kotlin/PurchaseMoneyTest.kt
+++ src/test/kotlin/PurchaseMoneyTest.kt
@@ -1,8 +1,23 @@
 import domain.model.PurchaseMoney
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class PurchaseMoneyTest {
+
+    @Test
+    fun `1000원 단위의 돈인 경우`(){
+        assertDoesNotThrow{
+            PurchaseMoney(15000)
+        }
+    }
+
+    @Test
+    fun `1000원 단위의 돈이 아닌 경우 예외가 발생`() {
+        assertThrows<IllegalArgumentException> {
+            PurchaseMoney(15500)
+        }
+    }
 
     @Test
     fun `0보다 작은 금액의 돈을 입력한 경우`() {
