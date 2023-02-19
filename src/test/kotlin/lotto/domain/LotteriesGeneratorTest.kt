3849ee18a4a37da61e5768578f61ce838e4a182e
Gloria <ippnsj@gmail.com>
2023-02-19 22:52:02 +0900

    refactor: 한 번만 실행되는 테스트 어노테이션 및 구조 변경

--- src/test/kotlin/lotto/domain/LotteriesGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/LotteriesGeneratorTest.kt
@@ -1,22 +1,24 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.api.Test
 
 class LotteriesGeneratorTest {
-    @ParameterizedTest
-    @MethodSource("generateLotteries")
-    fun `구입 로또 개수만큼 로또를 발행한다`(
-        numbers: MutableList<List<Int>>,
-        expectedLottery1: List<LotteryNumber>,
-        expectedLottery2: List<LotteryNumber>,
-        testName: String
-    ) {
-        println(testName)
-
+    @Test
+    fun `구입 로또 개수만큼 로또를 2개 발행한다`() {
+        val numbers = mutableListOf(
+            listOf(1, 3, 5, 7, 9, 11),
+            listOf(45, 24, 33, 10, 5, 15)
+        )
         val generator = LotteriesGenerator()
+        val expectedLottery1 = listOf(
+            LotteryNumber(1), LotteryNumber(3), LotteryNumber(5),
+            LotteryNumber(7), LotteryNumber(9), LotteryNumber(11)
+        )
+        val expectedLottery2 = listOf(
+            LotteryNumber(5), LotteryNumber(10), LotteryNumber(15),
+            LotteryNumber(24), LotteryNumber(33), LotteryNumber(45)
+        )
 
         val lotteries = generator.generate(2) {
             numbers.removeAt(0)
@@ -24,28 +26,5 @@ class LotteriesGeneratorTest {
 
         assertThat(lotteries[0].numbers).containsExactlyInAnyOrderElementsOf(expectedLottery1)
         assertThat(lotteries[1].numbers).containsExactlyInAnyOrderElementsOf(expectedLottery2)
-    }
-
-    companion object {
-        @JvmStatic
-        fun generateLotteries(): Array<Arguments> {
-            return arrayOf(
-                Arguments.of(
-                    mutableListOf(
-                        listOf(1, 3, 5, 7, 9, 11),
-                        listOf(45, 24, 33, 10, 5, 15)
-                    ),
-                    listOf(
-                        LotteryNumber(1), LotteryNumber(3), LotteryNumber(5),
-                        LotteryNumber(7), LotteryNumber(9), LotteryNumber(11)
-                    ),
-                    listOf(
-                        LotteryNumber(5), LotteryNumber(10), LotteryNumber(15),
-                        LotteryNumber(24), LotteryNumber(33), LotteryNumber(45)
-                    ),
-                    "로또 2개를 발행한다"
-                )
-            )
-        }
     }
 }
