ec87dfa8dade6a31571ed23963e87c72009df2bc
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-19 22:55:40 +0900

    feat: 당첨 로또를 생성하는 WinningLottoGenerator 클래스 생성

--- src/main/kotlin/domain/lottogenerator/WinningLottoGenerator.kt
+++ src/main/kotlin/domain/lottogenerator/WinningLottoGenerator.kt
@@ -3,7 +3,7 @@ package domain.lottogenerator
 import domain.model.WinningLotto
 import domain.model.lotto.LottoNumber
 
-class WinningNumbersGenerator : ManualLottoGenerator() {
+class WinningLottoGenerator : ManualLottoGenerator() {
 
     fun generateWinningLotto(InputNumbers: List<Int>, bonusNumber: Int): WinningLotto =
         WinningLotto(generate(InputNumbers), LottoNumber.from(bonusNumber))
--- src/test/kotlin/lottogenerator/WinningLottoGeneratorTest.kt
+++ src/test/kotlin/lottogenerator/WinningLottoGeneratorTest.kt
@@ -1,4 +1,24 @@
 package lottogenerator
 
+import domain.lottogenerator.WinningLottoGenerator
+import domain.model.WinningLotto
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
 class WinningLottoGeneratorTest {
+
+    lateinit var winningLottoGenerator: WinningLottoGenerator
+
+    @BeforeEach
+    fun setUp(){
+        winningLottoGenerator = WinningLottoGenerator()
+    }
+
+    @Test
+    fun `1등 로또를 생성하기`(){
+        val winningLotto = winningLottoGenerator.generateWinningLotto(listOf(1,2,3,4,5,6), 7)
+
+        assertThat(winningLotto).isInstanceOf(WinningLotto::class.java)
+    }
 }
