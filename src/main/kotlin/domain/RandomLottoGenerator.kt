6d608c1f16a5ee6bd833a9bca0fe0477efeb2429
hyunji1203 <hj27720@gmail.com>
2023-02-19 17:26:34 +0900

    refactor(RandommLottoGenerator): money 함수 사용

--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -2,7 +2,7 @@ package domain
 
 class RandomLottoGenerator : LottoGenerator {
     override fun generateLottos(money: Money): Lottos {
-        val count = money.price / LOTTO_PRICE
+        val count = money.lottoCount(money.price)
         val lottos = mutableListOf<Lotto>()
         repeat(count) { lottos.add(generateLotto()) }
         return Lottos(lottos)
@@ -15,7 +15,6 @@ class RandomLottoGenerator : LottoGenerator {
     }
 
     companion object {
-        const val LOTTO_PRICE = 1000
         const val LOTTO_LIMIT_SIZE = 6
         const val LOTTO_MIN_NUMBER = 1
         const val LOTTO_MAX_NUMBER = 45
