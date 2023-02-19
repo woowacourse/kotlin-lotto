73fa5da8834b59201a3620cdcaa5dfd157f0c817
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-19 20:46:41 +0900

    refactor: 함수 리턴타입 나타내기

--- src/main/kotlin/domain/LottoSeller.kt
+++ src/main/kotlin/domain/LottoSeller.kt
@@ -2,15 +2,16 @@ package domain
 
 import domain.lottogenerator.AutomaticLottoGenerator
 import domain.lottogenerator.ManualLottoGenerator
+import domain.model.lotto.Lotto
 
 class LottoSeller(
     private val automaticLottoGenerator: AutomaticLottoGenerator = AutomaticLottoGenerator(),
     private val manualLottoGenerator: ManualLottoGenerator = ManualLottoGenerator(),
 ) {
 
-    fun sellAutoMaticLottos(autoMaticLottosCount: Int) =
+    fun sellAutoMaticLottos(autoMaticLottosCount: Int): List<Lotto> =
         List(autoMaticLottosCount) { automaticLottoGenerator.generate() }
 
-    fun sellManualLotto(inputManualNumbers: List<Int>) =
+    fun sellManualLotto(inputManualNumbers: List<Int>): Lotto =
         manualLottoGenerator.generate(inputManualNumbers)
 }
