807075897d71418d8559f649275ecaf353131ae5
hyunji1203 <hj27720@gmail.com>
2023-02-19 20:03:08 +0900

    refactor(Rank): UI 기능 분리

--- src/main/kotlin/domain/Rank.kt
+++ src/main/kotlin/domain/Rank.kt
@@ -2,19 +2,11 @@ package domain
 
 enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
     FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000) {
-        override fun toString(): String {
-            return RANK_BONUS_MESSAGE.format(countOfMatch, winningMoney)
-        }
-    },
+    SECOND(5, 30_000_000),
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0);
-
-    override fun toString(): String {
-        return RANK_MESSAGE.format(countOfMatch, winningMoney)
-    }
 
     companion object {
         fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank? {
@@ -32,8 +24,5 @@ enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
                 false -> THIRD
             }
         }
-
-        const val RANK_MESSAGE = "%d개 일치 (%d원)- "
-        const val RANK_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원)- "
     }
 }
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -26,10 +26,22 @@ class OutputView {
     fun outputWinningResult(winningResult: WinningResult) {
         println(OUTPUT_WINNING_RESULT_MESSAGE)
         println(SEPARATE_MESSAGE)
+        winningCatalog(winningResult)
+    }
 
+    private fun winningCatalog(winningResult: WinningResult) {
+        val catalog = StringBuilder()
         val rankResult = listOf(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)
         rankResult.forEach {
-            println(it.toString() + winningResult.result[it] + "개")
+            catalog.append(catalogText(it, winningResult) + "\n")
+        }
+        println(catalog)
+    }
+    private fun catalogText(rankResult: Rank, winningResult: WinningResult): String {
+        return when (rankResult) {
+            Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.FIRST -> RANK_MESSAGE.format(rankResult.countOfMatch, rankResult.winningMoney) + winningResult.result[rankResult] + "개"
+            Rank.SECOND -> RANK_BONUS_MESSAGE.format(rankResult.countOfMatch, rankResult.winningMoney) + winningResult.result[rankResult] + "개"
+            Rank.MISS -> ""
         }
     }
 
@@ -44,6 +56,8 @@ class OutputView {
         const val POSTFIX_MARK = "]"
         const val OUTPUT_WINNING_RESULT_MESSAGE = "당첨 통계"
         const val SEPARATE_MESSAGE = "---------"
+        const val RANK_MESSAGE = "%d개 일치 (%d원)- "
+        const val RANK_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치(%d원)- "
         const val YIELD_MESSAGE = "총 수익률은 %.2f입니다."
     }
 }
