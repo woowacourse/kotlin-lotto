c23fec73cb58bc3503729015b29d5828c26ab686
Gloria <ippnsj@gmail.com>
2023-02-19 19:09:01 +0900

    refactor: Factory Function 으로 Fake Constructor 생성
    테스트 코드에서의 반복을 줄이기 위함

--- src/test/kotlin/lotto/domain/WinningLotteryTest.kt
+++ src/test/kotlin/lotto/domain/WinningLotteryTest.kt
@@ -4,19 +4,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
+private fun Lottery(vararg numbers: Int): Lottery {
+    return Lottery(numbers.map(::LotteryNumber))
+}
+
 class WinningLotteryTest {
     @Test
     fun `당첨 로또를 가진다`() {
-        val lottery = Lottery(
-            listOf(
-                LotteryNumber(1),
-                LotteryNumber(10),
-                LotteryNumber(20),
-                LotteryNumber(30),
-                LotteryNumber(40),
-                LotteryNumber(45)
-            )
-        )
+        val lottery = Lottery(1, 10, 20, 30, 40, 45)
         val bonusNumber = LotteryNumber(5)
         val winningLottery = WinningLottery(lottery, bonusNumber)
         val expected = listOf(
@@ -33,16 +28,7 @@ class WinningLotteryTest {
 
     @Test
     fun `보너스 번호를 가진다`() {
-        val lottery = Lottery(
-            listOf(
-                LotteryNumber(1),
-                LotteryNumber(10),
-                LotteryNumber(20),
-                LotteryNumber(30),
-                LotteryNumber(40),
-                LotteryNumber(45)
-            )
-        )
+        val lottery = Lottery(1, 10, 20, 30, 40, 45)
         val bonusNumber = LotteryNumber(8)
         val winningLottery = WinningLottery(lottery, bonusNumber)
 
@@ -51,16 +37,7 @@ class WinningLotteryTest {
 
     @Test
     fun `당첨 로또 번호와 보너스 번호가 중복되면 에러가 발생한다`() {
-        val lottery = Lottery(
-            listOf(
-                LotteryNumber(1),
-                LotteryNumber(10),
-                LotteryNumber(20),
-                LotteryNumber(30),
-                LotteryNumber(40),
-                LotteryNumber(45)
-            )
-        )
+        val lottery = Lottery(1, 10, 20, 30, 40, 45)
         val bonusNumber = LotteryNumber(10)
 
         assertThrows<IllegalArgumentException> { WinningLottery(lottery, bonusNumber) }
