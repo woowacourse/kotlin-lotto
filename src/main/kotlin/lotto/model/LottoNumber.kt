42485dd42ea4cfb582d76ce41c93aea85e38f4c7
Sujin Kim <sujinkim919@gmail.com>
2023-02-19 15:40:56 +0900

    refactor: hash code를 추가하고, equals를 자동 생성되는 함수로 변경하였다.

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -2,18 +2,25 @@ package lotto.model
 
 import lotto.view.ERROR_OUT_OF_RANGE
 
-class LottoNumber(val number: Int) : Comparable<LottoNumber> {
+class LottoNumber(val number: Int) {
 
     init {
         require(number in START_LOTTO_RANGE..END_LOTTO_RANGE) { ERROR_OUT_OF_RANGE }
     }
 
-    override fun compareTo(other: LottoNumber): Int {
-        return number.compareTo(other.number)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as LottoNumber
+
+        if (number != other.number) return false
+
+        return true
     }
 
-    override fun equals(other: Any?): Boolean {
-        return number == (other as LottoNumber).number
+    override fun hashCode(): Int {
+        return number
     }
 
     companion object {
