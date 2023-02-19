9cbbf7f96628c96f0940491abc48d2cffd3e6b1a
hyemdooly <hyemdooly@gmail.com>
2023-02-19 20:32:10 +0900

    refactor(Amount): minus 연산 추가

--- src/main/kotlin/domain/Amount.kt
+++ src/main/kotlin/domain/Amount.kt
@@ -6,6 +6,8 @@ data class Amount(private val amount: Int) {
         require(amount in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) { ERROR_CREATE_COUNT.format(amount) }
     }
     operator fun div(number: Int): Number = amount / number
+    operator fun minus(number: Int): Int = amount - number
+
     companion object {
         private const val MINIMUM_AMOUNT = 1000
         private const val MAXIMUM_AMOUNT = 100000
