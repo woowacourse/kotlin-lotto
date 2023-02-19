642b6bc2ab1784ebd21cee881d4dbb37e6f4169c
SangHyun Moon <no1msh1217@gmail.com>
2023-02-19 12:05:30 +0900

    refactor: 결합도 낮춤, 테스트 코드 추가

--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -2,8 +2,15 @@ package domain
 
 class RandomLottoGenerator : LottoGenerator {
     override fun generate(): Lotto {
-        val randomNumbers: List<Int> = LottoNumber.LOTTO_NUMBER_RANGE.shuffled().take(6).sorted()
+        val randomNumbers: List<Int> = LOTTO_NUMBER_RANGE.shuffled().take(LOTTO_NUMBER_COUNT).sorted()
         val lottoNumbers: Set<LottoNumber> = randomNumbers.map { LottoNumber.create(it) }.toSet()
         return Lotto(lottoNumbers)
+    }
+
+    companion object {
+        private const val LOTTO_NUMBER_MINIMUM = 1
+        private const val LOTTO_NUMBER_MAXIMUM = 45
+        private const val LOTTO_NUMBER_COUNT = 6
+        private val LOTTO_NUMBER_RANGE = LOTTO_NUMBER_MINIMUM..LOTTO_NUMBER_MAXIMUM
     }
 }
---
+++ src/test/kotlin/domain/RandomLottoGeneratorTest.kt
@@ -0,0 +1,21 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class RandomLottoGeneratorTest {
+
+    private val randomLottoGenerator = RandomLottoGenerator()
+
+    @Test
+    fun `gererate는 로또번호 6개를 생성해준다`() {
+        // given
+        val expected = 6
+
+        // when
+        val actual: Lotto = randomLottoGenerator.generate()
+
+        // then
+        assertThat(actual.lottoNumbers.size).isEqualTo(expected)
+    }
+}
