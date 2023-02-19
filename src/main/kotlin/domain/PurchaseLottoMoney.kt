991159615b3ef85158b215f00ecede0ce82e3abf
mendel <rhthrhrl0@naver.com>
2023-02-20 01:39:03 +0900

    refactor(PurchaseLottoMoney): 에러문구 상수의 접근제한자 private 으로 수정 및 에러문구 내용 수정

--- src/main/kotlin/domain/PurchaseLottoMoney.kt
+++ src/main/kotlin/domain/PurchaseLottoMoney.kt
@@ -5,11 +5,12 @@ class PurchaseLottoMoney(val money: Int) {
         get() = money / ONE_LOTTO_MONEY
 
     init {
-        require(money >= ONE_LOTTO_MONEY) { ERROR_LOTTO_PURCHASE_MONEY }
+        require(money >= ONE_LOTTO_MONEY) { ERROR_LOTTO_PURCHASE_MONEY.format(money) }
     }
 
     companion object {
-        const val ONE_LOTTO_MONEY = 1000
-        const val ERROR_LOTTO_PURCHASE_MONEY = "로또를 구입하기 위해서는 최소한 ${ONE_LOTTO_MONEY}원 이상의 금액이여야 합니다."
+        const val ONE_LOTTO_MONEY = 1000 // LottoStatistics 의 yield 메서드에서도 쓰이고 있음.
+        private const val ERROR_LOTTO_PURCHASE_MONEY =
+            "[ERROR] 현재의 로또 구입 금액은 %d, 로또를 구입하기 위해서는 최소한 ${ONE_LOTTO_MONEY}원 이상의 금액이여야 합니다."
     }
 }
