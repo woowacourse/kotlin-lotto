a2e85a84977260498bd58b7f72d3502965827b5a
SangHyun Moon <no1msh1217@gmail.com>
2023-02-19 12:00:25 +0900

    refactor: WinStatistics rankCount 초기화 방식 변경, 기존 Bank에 있던 메서드들 가져옴

--- src/main/kotlin/domain/WinStatistics.kt
+++ src/main/kotlin/domain/WinStatistics.kt
@@ -1,19 +1,25 @@
 package domain
 
-data class WinStatistics(private val _rankCount: MutableMap<Rank, Int>) {
-    val rankCount: Map<Rank, Int> get() = _rankCount
+class WinStatistics(winningResult: WinningResult) {
 
-    fun plusValue(key: Rank, value: Int) {
-        _rankCount[key] = (_rankCount[key] ?: 0).plus(value)
+    private val _rankCount = Rank.values().associateWith { 0 }.toMutableMap()
+    val rankCount: Map<Rank, Int>
+        get() = _rankCount.toMap()
+
+    init {
+        winningResult.comparingResults.forEach {
+            val rank = Rank.valueOf(it.matchedCount, it.isBonusMatched)
+            _rankCount[rank] = _rankCount[rank]?.plus(1)
+                ?: throw IllegalArgumentException("잘못된 Rank 참조입니다.")
+        }
     }
 
-    fun getTotalIncome(): Money {
+    fun getTotalIncome(): Long {
         var total: Long = 0L
-
-        rankCount.forEach {
+        _rankCount.forEach {
             total += it.key.winningMoney * it.value.toLong()
         }
 
-        return Money.create(total)
+        return total
     }
 }
--- src/test/kotlin/domain/WinStatisticsTest.kt
+++ src/test/kotlin/domain/WinStatisticsTest.kt
@@ -8,21 +8,50 @@ class WinStatisticsTest {
     fun `당첨통계를 통해 총 수익이 얼마인지 반환한다`() {
         // given
         val winStatistics: WinStatistics = WinStatistics(
-            mutableMapOf(
-                Rank.FIRST to 2,
-                Rank.SECOND to 1,
-                Rank.THIRD to 2,
-                Rank.FOURTH to 0,
-                Rank.FIFTH to 0,
+            WinningResult(
+                listOf(
+                    ComparingResult(6, false),
+                    ComparingResult(6, false),
+                    ComparingResult(5, true),
+                    ComparingResult(5, false),
+                    ComparingResult(5, false),
+                )
             )
         )
 
-        val expected: Money = Money.create(4033000000)
+        val expected: Long = 4033000000
 
         // when
         val actual = winStatistics.getTotalIncome()
 
         // then
         assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `당첨 통계를 계산한다`() {
+        // given
+        val winningResult: WinningResult = WinningResult(
+            listOf(
+                ComparingResult(6, false),
+                ComparingResult(5, true),
+                ComparingResult(5, false),
+                ComparingResult(5, false),
+            )
+        )
+        val expected: Map<Rank, Int> = mapOf(
+            Rank.FIRST to 1,
+            Rank.SECOND to 1,
+            Rank.THIRD to 2,
+            Rank.FOURTH to 0,
+            Rank.FIFTH to 0,
+            Rank.MISS to 0
+        )
+
+        // when
+        val actual: WinStatistics = WinStatistics(winningResult)
+
+        // then
+        assertThat(actual.rankCount).isEqualTo(expected)
     }
 }
