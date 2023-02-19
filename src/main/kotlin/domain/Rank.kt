08d3eeab0e2efc1f9a3523026c86a178c1d55137
SangHyun Moon <no1msh1217@gmail.com>
2023-02-19 12:02:35 +0900

    refactor: checkSame 메서드 이름 변경, 세부적인 테스트 추가

--- src/main/kotlin/domain/Rank.kt
+++ src/main/kotlin/domain/Rank.kt
@@ -3,27 +3,27 @@ package domain
 enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
     FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000) {
-        override fun checkSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
-            return super.checkSame(countOfMatch, matchBonus) and matchBonus
+        override fun isSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
+            return super.isSame(countOfMatch, matchBonus) and matchBonus
         }
     },
     THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0) {
-        override fun checkSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
+        override fun isSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
             return false
         }
-    }, ;
+    };
 
-    open fun checkSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
+    open fun isSame(countOfMatch: Int, matchBonus: Boolean): Boolean {
         return this.countOfMatch == countOfMatch
     }
 
     companion object {
         fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
             return values().find {
-                it.checkSame(countOfMatch, matchBonus)
+                it.isSame(countOfMatch, matchBonus)
             } ?: MISS
         }
     }
--- src/test/kotlin/domain/RankTest.kt
+++ src/test/kotlin/domain/RankTest.kt
@@ -2,10 +2,26 @@ package domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class RankTest {
+
+    @ValueSource(booleans = [false, true])
+    @ParameterizedTest
+    fun `보너스 번호 일치 여부와 관계 없이 모든 번호를 맞추면 1등이다`(isBonusMatch: Boolean) {
+        // given
+        val countMatch: Int = 6
+
+        // when
+        val actual = Rank.valueOf(countMatch, isBonusMatch)
+
+        // then
+        assertThat(actual).isEqualTo(Rank.FIRST)
+    }
+
     @Test
-    fun `번호를 맞춘 개 수에 따라 알맞은 등수를 반환한다`() {
+    fun `5개의 번호와 보너스 번호를 맞추면 2등이다`() {
         // given
         val countOfMatch: Int = 5
         val matchBonus: Boolean = true
@@ -15,5 +31,31 @@ class RankTest {
 
         // then
         assertThat(actual).isEqualTo(Rank.SECOND)
+    }
+
+    @Test
+    fun `5개의 번호를 맞추고 보너스 번호를 못맞추면 3등이다`() {
+        // given
+        val countOfMatch: Int = 5
+        val matchBonus: Boolean = false
+
+        // when
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        // then
+        assertThat(actual).isEqualTo(Rank.THIRD)
+    }
+
+    @ValueSource(booleans = [true, false])
+    @ParameterizedTest
+    fun `2개의 번호를 맞추면 보너스 번호 일치여부와 상관 없이 MISS 이다`(isBonusMatch: Boolean) {
+        // given
+        val countOfMatch: Int = 2
+
+        // when
+        val actual = Rank.valueOf(countOfMatch, isBonusMatch)
+
+        // then
+        assertThat(actual).isEqualTo(Rank.MISS)
     }
 }
