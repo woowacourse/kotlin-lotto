c241930dc91b8130eac05224c2d7cc2d8254ce10
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-19 18:31:59 +0900

    feat: 자동으로 로또를 생성하기

---
+++ src/main/kotlin/domain/AutomaticLottoGenerator.kt
@@ -0,0 +1,24 @@
+package domain
+
+import domain.model.lotto.LottoNumber
+
+class AutomaticLottoGenerator {
+
+    fun generate(
+        numberGenerator: () -> Set<LottoNumber> = {
+            (MINIMUM_NUMBER..MAXIMUM_NUMBER).drawLottoNumbers()
+        }
+    ): Set<LottoNumber> {
+        return numberGenerator()
+    }
+
+    companion object {
+        private const val MINIMUM_NUMBER = 1
+        private const val MAXIMUM_NUMBER = 45
+        private const val ZERO = 0
+        private const val NUMBER_COUNT = 6
+
+        private fun IntRange.drawLottoNumbers() =
+            this.shuffled().subList(ZERO, NUMBER_COUNT).sorted().map { number -> LottoNumber.from(number) }.toSet()
+    }
+}
---
+++ src/test/kotlin/numbergeneratortest/AutomaticLottoGeneratorTest.kt
@@ -0,0 +1,25 @@
+package numbergeneratortest
+
+import domain.AutomaticLottoGenerator
+import domain.model.lotto.Lotto
+import domain.model.lotto.LottoNumber
+import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class AutomaticLottoGeneratorTest {
+
+    lateinit var automaticLottoGenerator: AutomaticLottoGenerator
+
+    @BeforeEach
+    fun setUp(){
+        automaticLottoGenerator = AutomaticLottoGenerator()
+    }
+
+    @Test
+    fun `자동으로 로또 번호를 생성하기`(){
+        val lotto = automaticLottoGenerator.generate()
+
+        assertThat(lotto).isInstanceOf(Lotto::class.java)
+    }
+}
