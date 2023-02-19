3ee29e9aef48654f57d3b5d9327d95708ed399c3
whk06061 <gju06061@naver.com>
2023-02-19 23:16:37 +0900

    refactor: 하드코딩 대신 상수를 사용한다.

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -7,7 +7,7 @@ class LottoNumber private constructor(val number: Int) {
         const val MAXIMUM_LOTTO_NUMBER = 45
         private val NUMBERS: Map<Int, LottoNumber> =
             (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).associateWith(::LottoNumber)
-        private const val LOTTO_NUMBER_RANGE_ERROR = "로또 번호는 1과 45 사이여야 합니다."
+        private const val LOTTO_NUMBER_RANGE_ERROR = "로또 번호는 ${MINIMUM_LOTTO_NUMBER}과 $MAXIMUM_LOTTO_NUMBER 사이여야 합니다."
 
         fun from(value: Int): LottoNumber {
             return NUMBERS[value] ?: throw IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR)
