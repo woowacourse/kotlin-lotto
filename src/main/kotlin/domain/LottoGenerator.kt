f1ead127e09d2bd13d5e3b34961f7eeef8553688
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-19 18:03:34 +0900

    feat: 수동으로 로또 번호를 생성하기

--- src/main/kotlin/domain/LottoGenerator.kt
+++ src/main/kotlin/domain/LottoGenerator.kt
@@ -5,29 +5,8 @@ import domain.model.lotto.Lotto
 import domain.model.lotto.LottoNumber
 
 class LottoGenerator(
-    private val numberGenerator: () -> Set<LottoNumber> = {
-        (MINIMUM_NUMBER..MAXIMUM_NUMBER).drawLottoNumbers()
-    }
+   automaticLottoNumbersGenerator: AutomaticLottoNumbersGenerator = AutomaticLottoNumbersGenerator(),
+   manualLottoNumbersGenerator: ManualLottoNumbersGenerator = ManualLottoNumbersGenerator()
 ) {
 
-    fun generateLottosAutomatically(count: Int): List<Lotto> =
-        List(count) { Lotto(numberGenerator()) }
-
-    fun generateLottosManually(lottoNumbers: Set<LottoNumber>): Lotto =
-        Lotto(lottoNumbers)
-
-    private fun PurchaseMoney.getNumberOfLottos(): Int = this.money / LOTTO_PRICE
-
-    companion object {
-        private const val MINIMUM_NUMBER = 1
-        private const val MAXIMUM_NUMBER = 45
-        private const val NUMBER_COUNT = 6
-        private const val LOTTO_PRICE = 1000
-        private const val ZERO = 0
-
-        private const val NUMBER_UNIT_ERROR = "[ERROR] 천원 단위로 입력해주세요."
-
-        private fun IntRange.drawLottoNumbers() =
-            this.shuffled().subList(ZERO, NUMBER_COUNT).sorted().map { number -> LottoNumber.from(number) }.toSet()
-    }
 }
--- src/main/kotlin/domain/ManualLottoNumbersGenerator.kt
+++ src/main/kotlin/domain/ManualLottoNumbersGenerator.kt
@@ -1,4 +1,10 @@
 package domain
 
+import domain.model.lotto.LottoNumber
+
 class ManualLottoNumbersGenerator {
+
+    fun generate(InputNumbers: List<Int>): Set<LottoNumber> = InputNumbers.map { number ->
+        LottoNumber.from(number)
+    }.toSet()
 }
