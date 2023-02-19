db2339c414fb6c57f13ec307b5133ae4b1fcdba7
mendel <rhthrhrl0@naver.com>
2023-02-19 15:05:57 +0900

    refactor(WinningLotto): WinningLotto의 불필요한 메소드인 getWinningNumbers 제거

--- src/main/kotlin/domain/WinningLotto.kt
+++ src/main/kotlin/domain/WinningLotto.kt
@@ -8,8 +8,6 @@ class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
         require(!lotto.numbers.contains(bonusNumber)) { ERROR_CONTAIN_BONUS_NUMBER_IN_LOTTO }
     }
 
-    fun getWinningNumbers(): Set<LottoNumber> = lotto.numbers
-
     companion object {
         const val ERROR_CONTAIN_BONUS_NUMBER_IN_LOTTO = "로또 번호와 보너스 번호는 같을 수 없습니다."
     }
