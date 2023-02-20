bcccad7b81a92455182820c3509fde7a8bd190e9
Doyeop Kim <dygames@naver.com>
2023-02-20 17:20:56 +0900

    refactor(RandomLottoGenerator): Lotto 랜덤 생성시 LottoNumber에서 사용되는 모든 LottoNumber를 가져와 shuffle하게 변경

--- src/main/kotlin/lotto/model/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/model/RandomLottoGenerator.kt
@@ -1,12 +1,11 @@
 package lotto.model
 
 import lotto.entity.Lotto
+import lotto.entity.LottoNumber
 
 class RandomLottoGenerator : LottoGenerator {
-    private val numbers = (Lotto.MINIMUM_LOTTO_NUMBER..Lotto.MAXIMUM_LOTTO_NUMBER).toList()
-
     override fun generate(): Lotto {
-        val shuffledNumbers = numbers.shuffled()
-        return Lotto(shuffledNumbers.take(Lotto.LOTTO_COUNT).sorted())
+        val shuffledNumbers = LottoNumber.lottoNumberCache.shuffled()
+        return Lotto(shuffledNumbers.take(Lotto.LOTTO_COUNT).sortedBy { it.value }.toSet())
     }
 }
