3ef2d67bda36584361450dc3c4ddc51cc92f7a5c
tmdgh1592 <tmdgh1592@naver.com>
2023-02-20 13:17:02 +0900

    refactor: LottoNumber 클래스에서 from() 메서드 호출시 1에서 45까지 캐싱한 객체 반환하도록 변경

--- src/main/kotlin/domain/lotto/number/LottoNumber.kt
+++ src/main/kotlin/domain/lotto/number/LottoNumber.kt
@@ -19,7 +19,11 @@ value class LottoNumber(val value: Int) {
         private const val ERROR_MESSAGE_LOTTO_NUMBER_OUT_OF_RANGE = "$ERROR_PREFIX 로또 번호는 1이상 45이하여야 합니다."
         private const val ERROR_MESSAGE_NOT_NUMERIC_LOTTO_NUMBER = "$ERROR_PREFIX 로또 번호는 숫자여야 합니다."
 
-        fun from(number: String): LottoNumber =
-            LottoNumber(requireNotNull(number.toIntOrNull()) { ERROR_MESSAGE_NOT_NUMERIC_LOTTO_NUMBER })
+        private val NUMBERS: Map<Int, LottoNumber> = (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER).associateWith(::LottoNumber)
+
+        fun from(number: String): LottoNumber {
+            val lottoNumber = requireNotNull(number.toIntOrNull()) { ERROR_MESSAGE_NOT_NUMERIC_LOTTO_NUMBER }
+            return requireNotNull(NUMBERS[lottoNumber]) { ERROR_MESSAGE_LOTTO_NUMBER_OUT_OF_RANGE }
+        }
     }
 }
