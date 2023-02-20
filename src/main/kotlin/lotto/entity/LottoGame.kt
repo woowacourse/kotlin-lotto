d581d7c4971bdee9d7d9a1292b1c4088ceba80c2
Doyeop Kim <dygames@naver.com>
2023-02-20 15:49:05 +0900

    refactor(LottoGame): 당첨 통계를 LottoGame에서 만들게 변경

--- src/main/kotlin/lotto/entity/LottoGame.kt
+++ src/main/kotlin/lotto/entity/LottoGame.kt
@@ -1,8 +1,11 @@
 package lotto.entity
 
 import lotto.model.LottoGenerator
+import lotto.model.Rank
 
 class LottoGame private constructor(val value: List<Lotto>) {
+    fun makeWinStatistics(winLotto: WinLotto): WinStatistics = WinStatistics(value.map { Rank.determine(it, winLotto) })
+
     companion object {
         fun from(lottoCount: Int, lottoGenerator: LottoGenerator): LottoGame {
             return LottoGame(
