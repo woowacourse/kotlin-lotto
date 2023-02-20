4dd5bd646e3787b98cda3e0d9ee8e858b1b8520a
hyunji1203 <hj27720@gmail.com>
2023-02-21 00:22:19 +0900

    refactor(WinningResult): 함수 클래스 이동

--- src/main/kotlin/domain/WinningResult.kt
+++ src/main/kotlin/domain/WinningResult.kt
@@ -1,18 +1,6 @@
 package domain
 
-class WinningResult {
-    var result = mutableMapOf(
-        Rank.FIFTH to 0,
-        Rank.FOURTH to 0,
-        Rank.THIRD to 0,
-        Rank.SECOND to 0,
-        Rank.FIRST to 0
-    )
-        private set
-
-    fun setWinnigResult(rank: Rank) {
-        if (!rank.equals(Rank.MISS)) result.put(rank, result.getOrDefault(rank, 0) + 1)
-    }
+class WinningResult(val result: Map<Rank, Int>) {
 
     fun getWinningMoney(): Int {
         return result.map { it.key.winningMoney * it.value }.sum()
--- src/test/kotlin/WinnigResultTest.kt
+++ src/test/kotlin/WinnigResultTest.kt
@@ -9,9 +9,7 @@ class WinnigResultTest {
     @Test
     fun `1등 한번 4등 한번 당첨되었을 때 당첨금이 20억 5만원인지 확인`() {
         // given
-        val winningresult = WinningResult()
-        winningresult.setWinnigResult(Rank.FIRST) // 1등 당첨
-        winningresult.setWinnigResult(Rank.FOURTH) // 2등 당첨
+        val winningresult = WinningResult(mapOf(Rank.FIRST to 1, Rank.FOURTH to 1)) // 1등, 4등 당첨
 
         // when
         val totalMoney = winningresult.getWinningMoney()
@@ -24,10 +22,7 @@ class WinnigResultTest {
     fun `구입금액이 10000원일 때 4등, 5등 한번 씩 당첨됐을 경우 수익률 확인`() {
         // given
         // 구입금액 : 10000원
-        val money = 10000
-        val winningResult = WinningResult()
-        winningResult.setWinnigResult(Rank.FOURTH) // 4등 당첨
-        winningResult.setWinnigResult(Rank.FIFTH) // 5등 당첨
+        val winningResult = WinningResult(mapOf(Rank.FOURTH to 1, Rank.FIFTH to 1)) // 4등 5등 당첨
 
         // when
         val yield = winningResult.calculateYield(Money(10000))
