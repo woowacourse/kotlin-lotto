f6125eb65b29953fac4430e17e810e3a491c355f
inseonyun <yuninseon@naver.com>
2023-02-20 14:09:58 +0900

    refactor(ProfitRateCalculator): 불필요한 인터페이스 제거

--- src/main/kotlin/lotto/model/LottoProfitRateCalculator.kt
+++ src/main/kotlin/lotto/model/LottoProfitRateCalculator.kt
@@ -4,8 +4,8 @@ import lotto.entity.ProfitRate
 import lotto.entity.PurchaseMoney
 import lotto.entity.WinStatistics
 
-class LottoProfitRateCalculator(private val purchaseMoney: PurchaseMoney, private val winStatistics: WinStatistics) : ProfitRateCalculator {
-    override fun calculate(): ProfitRate {
+class LottoProfitRateCalculator(private val purchaseMoney: PurchaseMoney, private val winStatistics: WinStatistics) {
+    fun calculate(): ProfitRate {
         val winMoney = winStatistics.value.sumOf { it.winningMoney }
         return ProfitRate(winMoney / purchaseMoney.value.toFloat())
     }
--- src/main/kotlin/lotto/model/ProfitRateCalculator.kt
+++
@@ -1,7 +0,0 @@
-package lotto.model
-
-import lotto.entity.ProfitRate
-
-interface ProfitRateCalculator {
-    fun calculate(): ProfitRate
-}
