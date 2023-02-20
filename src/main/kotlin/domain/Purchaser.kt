3d518333dc3695de1aaf858bca460f7f94b134d2
briandr97 <briandr@naver.com>
2023-02-20 14:29:33 +0900

    refactor(Purchaser): 프로퍼티 네이밍 변경

--- src/main/kotlin/domain/Purchaser.kt
+++ src/main/kotlin/domain/Purchaser.kt
@@ -3,9 +3,9 @@ package domain
 import domain.lotto.LottoBundle
 import domain.lotto.generator.LottoVendingMachine
 
-class Purchaser(val purchasedMoney: Money) {
-    val numberOfPurchasedLotto: Int = (purchasedMoney / LOTTO_PRICE).toInt()
-    val purchasedLottoBundle: LottoBundle = LottoVendingMachine.getLottoBundle(numberOfPurchasedLotto)
+class Purchaser(val spentMoney: Money) {
+    val purchasedLottoCount: Int = (spentMoney / LOTTO_PRICE).toInt()
+    val purchasedLottoBundle: LottoBundle = LottoVendingMachine.getLottoBundle(purchasedLottoCount)
 
     companion object {
         private val LOTTO_PRICE: Money = Money(1000)
