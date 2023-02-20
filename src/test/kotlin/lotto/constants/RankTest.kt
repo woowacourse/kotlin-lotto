eb0c1d67a603837dd40a26b4cf160f07eb40916f
Yerin <myheaven_b@naver.com>
2023-02-20 15:30:59 +0900

    refactor(RankTest): 해당 패키지로 파일 경로 이동

--- src/test/kotlin/lotto/constants/RankTest.kt
+++ src/test/kotlin/lotto/constants/RankTest.kt
@@ -1,6 +1,5 @@
-package lotto.domain
+package lotto.constants
 
-import lotto.constants.Rank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
