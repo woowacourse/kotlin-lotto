633c88c72927d53408b146e9def79481b4f222e7
SangHyun Moon <no1msh1217@gmail.com>
2023-02-20 18:00:10 +0900

    refactor: ComparingResult 유효성 검증 추가 및 테스트 코드 추가

--- src/main/kotlin/domain/ComparingResult.kt
+++ src/main/kotlin/domain/ComparingResult.kt
@@ -3,4 +3,17 @@ package domain
 data class ComparingResult(
     val matchedCount: Int,
     val isBonusMatched: Boolean
-)
+) {
+    init {
+        require(matchedCount in MATCHED_RANGE) {
+            "[Error] 잘못된 값 : $matchedCount matchedCount는 $MATCHED_COUNT_MINIMUM 부터 $MATCHED_COUNT_MAXIMUM 까지 가능합니다."
+        }
+        require(!(matchedCount == 6 && isBonusMatched)) { "[Error] 당첨번호 6개가 같다면 보너스 번호가 일치할 수 없습니다." }
+    }
+
+    companion object {
+        private const val MATCHED_COUNT_MINIMUM = 0
+        private const val MATCHED_COUNT_MAXIMUM = 6
+        private val MATCHED_RANGE = MATCHED_COUNT_MINIMUM..MATCHED_COUNT_MAXIMUM
+    }
+}
