8dc232fc595354a56c362e9c8b11c53751f021a6
Gloria <ippnsj@gmail.com>
2023-02-20 17:29:30 +0900

    refactor: List Builder 활용
    repeat 대신 List 의 constructor-like function 활용

--- src/main/kotlin/lotto/domain/LotteriesGenerator.kt
+++ src/main/kotlin/lotto/domain/LotteriesGenerator.kt
@@ -2,11 +2,7 @@ package lotto.domain
 
 class LotteriesGenerator {
     fun generate(numberOfLottery: Int, numberGenerator: LotteryNumbersGenerator): List<Lottery> {
-        val lotteries = mutableListOf<Lottery>()
-        repeat(numberOfLottery) {
-            lotteries.add(generateLottery(numberGenerator))
-        }
-        return lotteries
+        return List(numberOfLottery) { generateLottery(numberGenerator) }
     }
 
     private fun generateLottery(numbersGenerator: LotteryNumbersGenerator): Lottery {
