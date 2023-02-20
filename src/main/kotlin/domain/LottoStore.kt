50f2beedea7c23357d19c7b7cb7256f301188db9
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-21 00:41:12 +0900

    Refactor(LottoStore): 비슷한 역할 클래스끼리 통합

--- src/main/kotlin/domain/LottoStore.kt
+++ src/main/kotlin/domain/LottoStore.kt
@@ -13,14 +13,24 @@ class LottoStore(
 
     fun buyAutoLotto(money: Money): List<Lotto> {
         require(money.value >= LOTTO_PRICE) { ERROR_INVALID_MONEY.format(money.value) }
-        return LottoFactory(lottoGenerator).createLottos(getCount(money))
+        return createLottos(getCount(money))
     }
+
+    fun createLottos(count: Int): List<Lotto> {
+        require(count in MINIMUM_COUNT..MAXIMUM_COUNT) { ERROR_CREATE_COUNT.format(count) }
+        return List(count) { createLotto() }
+    }
+
+    private fun createLotto(): Lotto = lottoGenerator.generateLotto()
 
     private fun getCount(money: Money): Int = money.value / LOTTO_PRICE
 
     companion object {
         const val LOTTO_PRICE = 1000
+        private const val MINIMUM_COUNT = 1
+        private const val MAXIMUM_COUNT = 100
         private const val ERROR_INVALID_COUNT = "구입 금액보다 많은 로또를 구매할 수 없습니다.\n잘못된 값: %d"
         private const val ERROR_INVALID_MONEY = "최소 금액보다 적은 돈으로 로또를 구매할 수 없습니다.\n잘못된 값: %d"
+        private const val ERROR_CREATE_COUNT = "한 번에 생성할 수 있는 로또 개수는 1개 이상 100개 이하입니다.\n잘못된 값: %d"
     }
 }
--- src/test/kotlin/domain/LottoStoreTest.kt
+++ src/test/kotlin/domain/LottoStoreTest.kt
@@ -3,6 +3,8 @@ package domain
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoStoreTest {
     private val lottoStore = LottoStore(RandomLottoGenerator)
@@ -35,5 +37,25 @@ class LottoStoreTest {
         Assertions.assertThatIllegalArgumentException()
             .isThrownBy { lottoStore.buyAutoLotto(money) }
             .withMessage("최소 금액보다 적은 돈으로 로또를 구매할 수 없습니다.\n잘못된 값: 999")
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 100])
+    fun `1개 이상 100개 이하의 개수만큼 로또를 생성할 수 있다`(count: Int) {
+        val lottoStore = LottoStore(RandomLottoGenerator)
+
+        val result = lottoStore.createLottos(count)
+
+        assertThat(result.size).isEqualTo(count)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0, 101])
+    fun `로또를 생성할 때 생성 개수가 1개 이상 100개 이하가 아니면 에러가 발생한다`(count: Int) {
+        val lottoStore = LottoStore(RandomLottoGenerator)
+
+        Assertions.assertThatIllegalArgumentException()
+            .isThrownBy { lottoStore.createLottos(count) }
+            .withMessage("한 번에 생성할 수 있는 로또 개수는 1개 이상 100개 이하입니다.\n잘못된 값: $count")
     }
 }
