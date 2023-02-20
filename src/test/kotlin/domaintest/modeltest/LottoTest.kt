9159f90bf0ed7f9a7e0f0a8625578099e97c0490
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-20 15:06:09 +0900

    refactor: generator클래스들을 이용하여 test 상황에서 Lotto나 WinningLotto 인스턴스를 생성하는 것을 간소화

--- src/test/kotlin/domaintest/modeltest/LottoTest.kt
+++ src/test/kotlin/domaintest/modeltest/LottoTest.kt
@@ -1,208 +1,91 @@
 package domaintest.modeltest
 
+import domain.lottogenerator.ManualLottoGenerator
+import domain.lottogenerator.WinningLottoGenerator
 import domain.model.LottoResult
-import domain.model.WinningLotto
-import domain.model.lotto.Lotto
-import domain.model.lotto.LottoNumber
 import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
 
+    private lateinit var manualLottoGenerator: ManualLottoGenerator
+    private lateinit var winningLottoGenerator: WinningLottoGenerator
+
+    @BeforeEach
+    fun setUp() {
+        manualLottoGenerator = ManualLottoGenerator()
+        winningLottoGenerator = WinningLottoGenerator()
+    }
+
     @Test
     fun `로또의 번호가 6개인 경우`() {
-        val numbers = setOf(
-            LottoNumber.from(1),
-            LottoNumber.from(2),
-            LottoNumber.from(3),
-            LottoNumber.from(4),
-            LottoNumber.from(5),
-            LottoNumber.from(6)
-        )
-
         assertDoesNotThrow {
-            Lotto(numbers)
+            manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
         }
     }
 
     @Test
     fun `로또의 번호가 6개가 아닌 경우 예외가 발생`() {
-        val numbers = setOf(
-            LottoNumber.from(1),
-            LottoNumber.from(2),
-            LottoNumber.from(3),
-            LottoNumber.from(4),
-            LottoNumber.from(5),
-            LottoNumber.from(6),
-            LottoNumber.from(7)
-        )
-
         assertThrows<IllegalArgumentException> {
-            Lotto(numbers)
+            manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 
     @Test
     fun `로또의 번호가 6개가 일치하는 경우 (1등)`() {
-        // given
-        val numbers = setOf(
-            LottoNumber.from(1),
-            LottoNumber.from(2),
-            LottoNumber.from(3),
-            LottoNumber.from(4),
-            LottoNumber.from(5),
-            LottoNumber.from(6),
-        )
-        val catchLotto = Lotto(numbers)
+        val catchLotto = manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
 
-        // when
         val actual = catchLotto.getLottoResult(
-            WinningLotto(catchLotto, LottoNumber.from(7))
+            winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
         )
 
-        // then
         assertThat(actual).isEqualTo(LottoResult.FIRST)
     }
 
     @Test
     fun `로또의 번호가 5개가 일치하고 보너스 번호가 일치하는 경우 (2등)`() {
-        // given
-        val lotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            )
+        val lotto = manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
+
+        val actual = lotto.getLottoResult(
+            winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 4, 5, 8), 6)
         )
 
-        val catchLotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(7)
-            )
-        )
-        val bonusNumber = LottoNumber.from(6)
-
-        val winningLotto = WinningLotto(catchLotto, bonusNumber)
-
-        // when
-        val actual = lotto.getLottoResult(winningLotto)
-
-        // then
         assertThat(actual).isEqualTo(LottoResult.SECOND)
     }
 
     @Test
     fun `로또의 번호가 5개가 일치하고 보너스 번호가 일치하지 않는 경우 (3등)`() {
-        // given
-        val lotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            )
+        val lotto = manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
+
+        val actual = lotto.getLottoResult(
+            winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 4, 5, 8), 7)
         )
 
-        val catchLotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(7)
-            )
-        )
-        val bonusNumber = LottoNumber.from(8)
-
-        val winningLotto = WinningLotto(catchLotto, bonusNumber)
-
-        // when
-        val actual = lotto.getLottoResult(winningLotto)
-
-        // then
         assertThat(actual).isEqualTo(LottoResult.THIRD)
     }
 
     @Test
     fun `로또의 번호가 4개가 일치하는 경우 (4등)`() {
-        // given
-        val lotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            )
+        val lotto = manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
+
+        val actual = lotto.getLottoResult(
+            winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 4, 41, 42), 43)
         )
 
-        val catchLotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(9),
-                LottoNumber.from(7)
-            )
-        )
-        val bonusNumber = LottoNumber.from(8)
-
-        val winningLotto = WinningLotto(catchLotto, bonusNumber)
-
-        // when
-        val actual = lotto.getLottoResult(winningLotto)
-
-        // then
         assertThat(actual).isEqualTo(LottoResult.FORTH)
     }
 
     @Test
     fun `로또의 번호가 3개가 일치하는 경우 (5등)`() {
-        // given
-        val lotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6),
-            )
+        val lotto = manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6))
+
+        val actual = lotto.getLottoResult(
+            winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 35, 41, 42), 43)
         )
 
-        val catchLotto = Lotto(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(10),
-                LottoNumber.from(11),
-                LottoNumber.from(12),
-            )
-        )
-        val bonusNumber = LottoNumber.from(8)
-
-        val winningLotto = WinningLotto(catchLotto, bonusNumber)
-
-        // when
-        val actual = lotto.getLottoResult(winningLotto)
-
-        // then
         assertThat(actual).isEqualTo(LottoResult.FIFTH)
     }
 }
--- src/test/kotlin/domaintest/modeltest/PurchasedLottosTest.kt
+++ src/test/kotlin/domaintest/modeltest/PurchasedLottosTest.kt
@@ -1,67 +1,37 @@
 package domaintest.modeltest
 
+import domain.lottogenerator.ManualLottoGenerator
+import domain.lottogenerator.WinningLottoGenerator
 import domain.model.LottoResult
-import domain.model.WinningLotto
-import domain.model.lotto.Lotto
-import domain.model.lotto.LottoNumber
 import domain.model.lotto.PurchasedLottos
 import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class PurchasedLottosTest {
+
+    private lateinit var manualLottoGenerator: ManualLottoGenerator
+    private lateinit var winningLottoGenerator: WinningLottoGenerator
+
+    @BeforeEach
+    fun setUp() {
+        manualLottoGenerator = ManualLottoGenerator()
+        winningLottoGenerator = WinningLottoGenerator()
+    }
 
     @Test
     fun `구입한 로또의 전체 당첨 결과를 확인하기`() {
         val purchasedLottos = PurchasedLottos(
             lottos = listOf(
-                Lotto(
-                    setOf(
-                        LottoNumber.from(1),
-                        LottoNumber.from(2),
-                        LottoNumber.from(3),
-                        LottoNumber.from(4),
-                        LottoNumber.from(5),
-                        LottoNumber.from(6)
-                    )
-                ),
-                Lotto(
-                    setOf(
-                        LottoNumber.from(1),
-                        LottoNumber.from(2),
-                        LottoNumber.from(3),
-                        LottoNumber.from(4),
-                        LottoNumber.from(5),
-                        LottoNumber.from(7)
-                    )
-                ),
-                Lotto(
-                    setOf(
-                        LottoNumber.from(1),
-                        LottoNumber.from(2),
-                        LottoNumber.from(3),
-                        LottoNumber.from(4),
-                        LottoNumber.from(7),
-                        LottoNumber.from(9)
-                    )
-                )
+                manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 6)),
+                manualLottoGenerator.generate(listOf(1, 2, 3, 4, 5, 7)),
+                manualLottoGenerator.generate(listOf(1, 2, 3, 4, 7, 8))
             )
         )
 
         assertThat(
             purchasedLottos.getTotalLottoResults(
-                winningLotto = WinningLotto(
-                    catchLotto = Lotto(
-                        setOf(
-                            LottoNumber.from(1),
-                            LottoNumber.from(2),
-                            LottoNumber.from(3),
-                            LottoNumber.from(4),
-                            LottoNumber.from(5),
-                            LottoNumber.from(6)
-                        )
-                    ),
-                    bonusNumber = LottoNumber.from(7)
-                )
+                winningLottoGenerator.generateWinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
             )
         ).isEqualTo(
             listOf(
