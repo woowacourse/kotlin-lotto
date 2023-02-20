62da208e65259b93dde550304b34696497f52183
Sujin Kim <sujinkim919@gmail.com>
2023-02-20 15:25:02 +0900

    refactor: value class 활용

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -2,22 +2,8 @@ package lotto.model
 
 import lotto.view.ERROR_OUT_OF_RANGE
 
-class LottoNumber private constructor(val number: Int) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LottoNumber
-
-        if (number != other.number) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return number
-    }
+@JvmInline
+value class LottoNumber private constructor(val number: Int) {
 
     companion object {
         private const val START_LOTTO_RANGE = 1
