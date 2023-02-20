ee917aabe2a65ee084a7cd6fe40f9bf9b536f07d
Doyeop Kim <dygames@naver.com>
2023-02-20 15:54:33 +0900

    refactor(WinLotto, Rank): WinLotto에서 Rank를 판별하도록 책임 이동

--- src/main/kotlin/lotto/entity/WinLotto.kt
+++ src/main/kotlin/lotto/entity/WinLotto.kt
@@ -6,24 +6,29 @@ class WinLotto(private val winNumber: Lotto, private val bonus: LottoNumber) {
     init {
         require(!winNumber.numbers.contains(bonus)) {
             String.format(
-                ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER,
-                winNumber.toString(),
-                bonus.value
+                ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER, winNumber.toString(), bonus.value
             )
         }
     }
 
-    fun determineLottoRank(condition: Rank, lotto: Lotto): Boolean {
+    fun determineRank(lotto: Lotto): Rank {
         val countOfMatch = lotto.matchLottoNumberCount(winNumber)
-        if (condition == Rank.MISS && countOfMatch in 0..2)
-            return true
-        if (condition.needBonus && !lotto.isMatchBonus(bonus))
-            return false
-        return countOfMatch == condition.countOfMatch
+        val isMatchBonus = lotto.isMatchBonus(bonus)
+        val determinedRank = Rank.values().filter { determineSingleRank(it, countOfMatch, isMatchBonus) }
+        check(determinedRank.isNotEmpty()) {
+            String.format(ERROR_DETERMINED_RANK_IS_EMPTY)
+        }
+        return determinedRank[0]
+    }
+
+    private fun determineSingleRank(rank: Rank, countOfMatch: Int, isMatchBonus: Boolean): Boolean {
+        if (rank == Rank.MISS && countOfMatch in 0..2) return true
+        if (rank == Rank.SECOND && !isMatchBonus) return false
+        return rank.countOfMatch == countOfMatch
     }
 
     companion object {
-        const val ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER =
-            "보너스 번호와 당첨 번호는 중복될 수 없습니다. 입력된 당첨 번호 : %s, 보너스 번호 : %d"
+        const val ERROR_MESSAGE_DUPLICATED_BONUS_NUMBER = "보너스 번호와 당첨 번호는 중복될 수 없습니다. 입력된 당첨 번호 : %s, 보너스 번호 : %d"
+        private const val ERROR_DETERMINED_RANK_IS_EMPTY = "등수 판별에 실패하였습니다."
     }
 }
--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -1,28 +1,12 @@
 package lotto.model
 
-import lotto.entity.Lotto
 import lotto.entity.Money
-import lotto.entity.WinLotto
 
-enum class Rank(val countOfMatch: Int, val needBonus: Boolean, val winningMoney: Money) {
-    FIRST(6, false, Money(2_000_000_000)),
-    SECOND(5, true, Money(30_000_000)),
-    THIRD(5, false, Money(1_500_000)),
-    FOURTH(4, false, Money(50_000)),
-    FIFTH(3, false, Money(5_000)),
-    MISS(0, false, Money(0));
-
-    companion object {
-        private const val ERROR_DETERMINED_RANK_IS_EMPTY = "등수 판별에 실패하였습니다."
-
-        fun determine(lotto: Lotto, winLotto: WinLotto): Rank {
-            val determinedRank = Rank.values().filter {
-                winLotto.determineLottoRank(it, lotto)
-            }
-            check(determinedRank.isNotEmpty()) {
-                String.format(ERROR_DETERMINED_RANK_IS_EMPTY)
-            }
-            return determinedRank[0]
-        }
-    }
+enum class Rank(val countOfMatch: Int, val winningMoney: Money) {
+    FIRST(6, Money(2_000_000_000)),
+    SECOND(5, Money(30_000_000)),
+    THIRD(5, Money(1_500_000)),
+    FOURTH(4, Money(50_000)),
+    FIFTH(3, Money(5_000)),
+    MISS(0, Money(0));
 }
