1cce0ffd798f048377e3c02cff06265ae68b390d
hyunji1203 <hj27720@gmail.com>
2023-02-20 21:26:58 +0900

    refactor(LottoNumberTest): 테스트 명 명료화

--- src/test/kotlin/LottoNumberTest.kt
+++ src/test/kotlin/LottoNumberTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.assertThrows
 
 class LottoNumberTest {
     @Test
-    fun `로또 숫자의 범위를 벗어나는 경우 예외 처리 확인`() {
+    fun `로또 숫자는 1에서 45 사이 이외의 숫자로 생성될 수 없다`() {
         assertThrows<IllegalArgumentException> { LottoNumber.from(46) }
     }
 }
