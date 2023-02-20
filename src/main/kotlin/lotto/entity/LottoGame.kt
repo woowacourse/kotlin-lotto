7dfee94ac4c836b70aaf2c8a7ba3fde4c302ab64
Doyeop Kim <dygames@naver.com>
2023-02-20 17:13:40 +0900

    refactor(LottoGame, WinStatistics): 객체에 적절한 역할 부여

--- src/main/kotlin/lotto/entity/LottoGame.kt
+++ src/main/kotlin/lotto/entity/LottoGame.kt
@@ -1,13 +1,13 @@
 package lotto.entity
 
 import lotto.model.LottoGenerator
-import lotto.model.Rank
 
 class LottoGame private constructor(val value: List<Lotto>) {
-    fun makeWinStatistics(winLotto: WinLotto): WinStatistics = WinStatistics(value.map { Rank.determine(it, winLotto) })
+    fun makeWinStatistics(winLotto: WinLotto): WinStatistics = WinStatistics(value.map { winLotto.determineRank(it) })
 
     companion object {
-        fun from(lottoCount: Int, lottoGenerator: LottoGenerator): LottoGame {
+        fun from(purchaseMoney: PurchaseMoney, lottoGenerator: LottoGenerator): LottoGame {
+            val lottoCount = purchaseMoney.calculateLottoCount()
             return LottoGame(
                 (0 until lottoCount).map {
                     lottoGenerator.generate()
--- src/main/kotlin/lotto/entity/WinStatistics.kt
+++ src/main/kotlin/lotto/entity/WinStatistics.kt
@@ -2,9 +2,6 @@ package lotto.entity
 
 import lotto.model.Rank
 
-class WinStatistics private constructor(val value: List<Rank>) {
-    companion object {
-        fun from(lottoGame: LottoGame, winLotto: WinLotto): WinStatistics =
-            WinStatistics(lottoGame.value.map { Rank.determine(it, winLotto) })
-    }
+class WinStatistics(val value: List<Rank>) {
+    fun calculateWinMoney(): Money = Money(value.sumOf { it.winningMoney.value })
 }
