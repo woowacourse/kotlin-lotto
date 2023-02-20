91908762c606b3ae61a4e8769159f24ba40e7df8
chws0508 <chws0508@naver.com>
2023-02-20 15:39:49 +0900

    feat : 총 로또 개수 구하는 기능 구현

--- src/main/kotlin/PaymentMoney.kt
+++ src/main/kotlin/PaymentMoney.kt
@@ -1,10 +1,15 @@
 class PaymentMoney(val value: Int) {
+
     init {
-        require(value > MINIMUM_MONEY) { PAYMENT_MONEY_MINIMUM_ERROR_MESSAGE }
+        require(value > ONE_LOTTO_PRICE) { PAYMENT_MONEY_MINIMUM_ERROR_MESSAGE }
+    }
+
+    fun getTotalLottoCount(): Int {
+        return this.value / ONE_LOTTO_PRICE
     }
 
     companion object {
         const val PAYMENT_MONEY_MINIMUM_ERROR_MESSAGE = "[ERROR] 구입 금액이 1000을 넘지 않습니다."
-        const val MINIMUM_MONEY = 1000
+        const val ONE_LOTTO_PRICE = 1000
     }
 }
--- src/test/kotlin/PaymentMoneyTest.kt
+++ src/test/kotlin/PaymentMoneyTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -7,9 +8,25 @@ class PaymentMoneyTest() {
     @ParameterizedTest
     @CsvSource("500", "300", "100", "900")
     fun `구입 금액이 1000원 이상이 아니라면 예외를 발생한다`(money: Int) {
+        // given
         val exception = assertThrows<IllegalArgumentException> {
             PaymentMoney(money)
         }
+
+        // then
         assertEquals(PaymentMoney.PAYMENT_MONEY_MINIMUM_ERROR_MESSAGE, exception.message)
+    }
+
+    @Test
+    fun `구입 금액이 14000원이면 총 로또 개수는 14개이다`() {
+        // given
+        val money = 14000
+
+        // when
+        val actual = PaymentMoney(14000).getTotalLottoCount()
+
+        // then
+        val expected = 14
+        assertEquals(actual, expected)
     }
 }
