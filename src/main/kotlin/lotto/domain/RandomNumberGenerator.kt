95751ea589e3ecedc84b826e4a90031583d04a98
Yerin <myheaven_b@naver.com>
2023-02-20 17:29:55 +0900

    refactor: 중복된 일을 하는 함수 제거

--- src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
+++ src/main/kotlin/lotto/domain/RandomNumberGenerator.kt
@@ -2,7 +2,7 @@ package lotto.domain
 
 class RandomNumberGenerator : LottoNumberGenerator {
     override fun generate(): List<LottoNumber> {
-        return (LottoNumber.MINIMUM_LOTTO_NUMBER..LottoNumber.MAXIMUM_LOTTO_NUMBER).toList().shuffled()
+        return (LottoNumber.MINIMUM_LOTTO_NUMBER..LottoNumber.MAXIMUM_LOTTO_NUMBER).shuffled()
             .slice(0 until Lotto.LOTTO_SIZE)
             .sorted().map { number -> LottoNumber(number) }
     }
