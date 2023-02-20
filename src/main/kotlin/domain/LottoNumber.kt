1d3b76478ab321d11af50306b635ad8941bdd7e1
hyunji1203 <hj27720@gmail.com>
2023-02-20 16:18:02 +0900

    refactor(RandomLottoGenerator): 상수 값 변경

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -7,8 +7,8 @@ data class LottoNumber private constructor(private val value: Int) {
     }
 
     companion object {
-        private const val MINIMUM_NUMBER = 1
-        private const val MAXIMUM_NUMBER = 45
+        const val MINIMUM_NUMBER = 1
+        const val MAXIMUM_NUMBER = 45
         private const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 1에서 45 사이여야 합니다."
         private val NUMBERS: Map<Int, LottoNumber> = (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith(::LottoNumber)
 
--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -6,15 +6,13 @@ class RandomLottoGenerator : LottoGenerator {
     }
 
     private fun generateLotto(): Lotto {
-        val randomNumbers = (LOTTO_MIN_NUMBER..LOTTO_MAX_NUMBER).shuffled().subList(LOTTO_MIN_SIZE, LOTTO_LIMIT_SIZE).sorted()
+        val randomNumbers = (LottoNumber.MINIMUM_NUMBER..LottoNumber.MAXIMUM_NUMBER).shuffled().subList(LOTTO_MIN_SIZE, LOTTO_LIMIT_SIZE).sorted()
         val lotto = randomNumbers.map { LottoNumber.from(it) }
         return Lotto(lotto)
     }
 
     companion object {
         const val LOTTO_LIMIT_SIZE = 6
-        const val LOTTO_MIN_NUMBER = 1
-        const val LOTTO_MAX_NUMBER = 45
         const val LOTTO_MIN_SIZE = 0
     }
 }
