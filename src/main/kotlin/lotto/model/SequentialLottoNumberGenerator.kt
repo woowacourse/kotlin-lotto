c973e8a53a3d40ae2322162c67f356990efb0e5f
Doyeop Kim <dygames@naver.com>
2023-02-20 22:14:13 +0900

    fix(SequentialLottoNumberGenerator): 로또 순차 생성시 index를 증가시키지 않던 버그 수정

--- src/main/kotlin/lotto/model/SequentialLottoNumberGenerator.kt
+++ src/main/kotlin/lotto/model/SequentialLottoNumberGenerator.kt
@@ -3,6 +3,6 @@ package lotto.model
 import lotto.entity.Lotto
 
 class SequentialLottoNumberGenerator(private val lottos: List<Lotto>) : LottoGenerator {
-    private val index: Int = 0
-    override fun generate(): Lotto = lottos[index]
+    private var index: Int = 0
+    override fun generate(): Lotto = lottos[index++]
 }
