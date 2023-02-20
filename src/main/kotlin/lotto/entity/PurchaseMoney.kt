e6f0c68be72011fc4a4e96faba90309a52d06ba1
Doyeop Kim <dygames@naver.com>
2023-02-20 17:22:06 +0900

    refactor(PurchaseMoney): 구매할 로또 개수를 구매 금액에서 계산하게 변경

--- src/main/kotlin/lotto/entity/PurchaseMoney.kt
+++ src/main/kotlin/lotto/entity/PurchaseMoney.kt
@@ -1,11 +1,11 @@
 package lotto.entity
 
-import lotto.controller.World
-
 class PurchaseMoney(value: Int) : Money(value) {
     init {
         require(value >= LOTTO_PRICE) { String.format(ERROR_MESSAGE_PURCHASE_LESS_THAN_LOTTO, value) }
     }
+
+    fun calculateLottoCount(): Int = value / LOTTO_PRICE
 
     companion object {
         const val ERROR_MESSAGE_PURCHASE_LESS_THAN_LOTTO = "구입 금액이 로또 가격보다 적습니다. 입력된 구입 금액은 %d원 입니다."
