047c34068ab52c0276abbf7b426fd142f90db7db
otter <a33397397@gmail.com>
2023-02-20 15:23:37 +0900

    refactor: 테스트명 상세화

--- src/test/kotlin/lotto/domain/LotteryNumberTest.kt
+++ src/test/kotlin/lotto/domain/LotteryNumberTest.kt
@@ -9,23 +9,23 @@ import org.junit.jupiter.params.provider.ValueSource
 class LotteryNumberTest {
     @ParameterizedTest
     @ValueSource(ints = [1, 45])
-    fun `번호를 하나 가진다`(number: Int) {
+    fun `1 이상 45 이하의 로또 번호를 생성할 수 있다`(number: Int) {
         val lotteryNumber = LotteryNumber(number)
         assertThat(lotteryNumber.number).isEqualTo(number)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [0, 46])
-    fun `번호는 1 이상 45 이하의 숫자가 아니라면 에러가 발생한다`(number: Int) {
-        assertThrows<IllegalArgumentException> {
-            LotteryNumber(number)
-        }
     }
 
     @ParameterizedTest
     @ValueSource(ints = [1, 45])
     fun `번호는 1 이상 45 이하의 숫자면 에러가 발생하지 않는다`(number: Int) {
         assertDoesNotThrow {
+            LotteryNumber(number)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `번호는 1 이상 45 이하의 숫자가 아니라면 에러가 발생한다`(number: Int) {
+        assertThrows<IllegalArgumentException> {
             LotteryNumber(number)
         }
     }
--- src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/domain/PurchaseAmountTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class PurchaseAmountTest {
     @ParameterizedTest
     @ValueSource(ints = [1000, 50000])
-    fun `구입금액을 가진다`(amount: Int) {
+    fun `1000원 이상 50000 이하의 구입금액을 생성할 수 있다`(amount: Int) {
         val purchaseAmount = PurchaseAmount(amount)
         assertThat(purchaseAmount.amount).isEqualTo(amount)
     }
