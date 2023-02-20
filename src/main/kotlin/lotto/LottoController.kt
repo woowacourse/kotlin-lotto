608b07454b40a25f434ab6ad6dd7a9450d046b27
changhwan <chang5hustle@gmail.com>
2023-02-20 16:26:52 +0900

    refactor(lotto): InputView로 형변환 로직 이동 및 입력을 위한 대사출력 InputView로 이동

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -5,9 +5,7 @@ import lotto.domain.Lotto
 import lotto.domain.LottoBunch
 import lotto.domain.LottoNumber
 import lotto.domain.PurchaseMoney
-import lotto.domain.Validator
 import lotto.domain.Validator.ERROR_INPUT_HANDLER
-import lotto.domain.Validator.ERROR_NOT_NUMBER
 import lotto.domain.WinningLotto
 import lotto.domain.WinningResult
 import lotto.domain.factory.LottoFactory
@@ -35,49 +33,26 @@ class LottoController(private val lottoFactory: LottoFactory) {
 
     private fun getPurchaseMoney(): PurchaseMoney {
         return runCatching {
-            OutputView.printGetPurchaseMoneyScript()
-            getVerifiedPurchaseMoney(InputView.getUserInput())
+            PurchaseMoney(InputView.getPurchaseMoney())
         }.getOrElse { error ->
             inputErrorHandler(error, ::getPurchaseMoney) as PurchaseMoney
         }
     }
 
-    private fun getVerifiedPurchaseMoney(input: String): PurchaseMoney {
-        require(Validator.isNumber(input)) { ERROR_NOT_NUMBER }
-
-        return PurchaseMoney(input.toInt())
-    }
-
     private fun getMainLottoNumber(): List<LottoNumber> {
         return kotlin.runCatching {
-            OutputView.printGetMainLottoNumbersScript()
-            getVerifiedMainLottoNumber(InputView.getUserInput())
+            InputView.getMainLottoNumbers().map { LottoNumber(it) }
         }.getOrElse { error ->
             inputErrorHandler(error, ::getMainLottoNumber) as List<LottoNumber>
         }
     }
 
-    private fun getVerifiedMainLottoNumber(input: String): List<LottoNumber> {
-        val lottoNumbers = input.split(",").map { number -> number.trim() }
-
-        lottoNumbers.forEach { require(Validator.isNumber(it)) { ERROR_NOT_NUMBER } }
-
-        return lottoNumbers.map { LottoNumber(it.toInt()) }
-    }
-
     private fun getBonusLottoNumber(): LottoNumber {
         return runCatching {
-            OutputView.printGetBonusLottoNumberScript()
-            getVerifiedBonusLottoNumber(InputView.getUserInput())
+            LottoNumber(InputView.getBonusLottoNumber())
         }.getOrElse { error ->
             inputErrorHandler(error, ::getBonusLottoNumber) as LottoNumber
         }
-    }
-
-    private fun getVerifiedBonusLottoNumber(input: String): LottoNumber {
-        require(Validator.isNumber(input)) { ERROR_NOT_NUMBER }
-
-        return LottoNumber(input.toInt())
     }
 
     fun getWinningLotto(): WinningLotto {
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,8 +2,23 @@ package lotto.view
 
 object InputView {
     private const val ERROR_WRONG_INPUT = "입력값으로 Null 이 입력되었습니다."
+    private const val GET_PURCHASE_MONEY_SCRIPT = "구입금액을 입력해 주세요."
+    private const val GET_MAIN_LOTTO_NUMBERS_SCRIPT = "지난 주 당첨 번호를 입력해 주세요."
+    private const val GET_BONUS_LOTTO_NUMBER_SCRIPT = "보너스 볼을 입력해 주세요."
 
-    fun getUserInput(): String {
-        return readlnOrNull()?.trim() ?: throw IllegalStateException(ERROR_WRONG_INPUT)
+    fun getPurchaseMoney(): Int {
+        println(GET_PURCHASE_MONEY_SCRIPT)
+        return readlnOrNull()?.trim()?.toInt() ?: throw IllegalStateException(ERROR_WRONG_INPUT)
+    }
+
+    fun getMainLottoNumbers(): List<Int> {
+        println(GET_MAIN_LOTTO_NUMBERS_SCRIPT)
+        return readlnOrNull()?.trim()?.split(",")?.map { number -> number.trim().toInt() }
+            ?: throw IllegalStateException(ERROR_WRONG_INPUT)
+    }
+
+    fun getBonusLottoNumber(): Int {
+        println(GET_BONUS_LOTTO_NUMBER_SCRIPT)
+        return readlnOrNull()?.trim()?.toInt() ?: throw IllegalStateException(ERROR_WRONG_INPUT)
     }
 }
