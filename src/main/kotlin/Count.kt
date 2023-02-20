b06a797e6c665e8c79ef4509352198f53a02ad61
chws0508 <chws0508@naver.com>
2023-02-20 16:12:21 +0900

    feat : 로또 개수가 음수라면 예외를 발생하는 기능 구현

---
+++ src/main/kotlin/Count.kt
@@ -0,0 +1,10 @@
+class Count(val value: Int) {
+    init {
+        require(value >= MINIMUM_NON_NEGATIVE_NUMBER) { COUNT_NEGATIVE_ERROR_MESSAGE }
+    }
+
+    companion object {
+        const val COUNT_NEGATIVE_ERROR_MESSAGE = "[ERROR] 갯수는 음수가 될 수 없습니다."
+        const val MINIMUM_NON_NEGATIVE_NUMBER = 0
+    }
+}
---
+++ src/test/kotlin/CountTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class CountTest {
+    @ParameterizedTest
+    @CsvSource("-1", "-10", "-20")
+    fun `개수가 음수라면 예외를 발생한다`(value: Int) {
+        val exception = assertThrows<IllegalArgumentException> { Count(value) }
+        assertEquals(Count.COUNT_NEGATIVE_ERROR_MESSAGE, exception.message)
+    }
+}
