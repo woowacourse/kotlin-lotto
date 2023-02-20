f06e0cd72e2fbdce868227aadd5e0544c76e9f85
briandr97 <briandr@naver.com>
2023-02-20 15:07:39 +0900

    refactor(LottoVendingMachineTest): 테스트 코드의 가독성을 위한 코드 정리

--- src/test/kotlin/domain/lotto/LottoVendingMachineTest.kt
+++ src/test/kotlin/domain/lotto/LottoVendingMachineTest.kt
@@ -10,16 +10,7 @@ class LottoVendingMachineTest {
     fun `로또 개수만큼 발행`() {
         // given
         val lottoCount: Int = 5
-        val lotto = Lotto(
-            setOf(
-                LottoNumber.of(1),
-                LottoNumber.of(2),
-                LottoNumber.of(3),
-                LottoNumber.of(4),
-                LottoNumber.of(5),
-                LottoNumber.of(6),
-            ),
-        )
+        val lotto = Lotto(1, 2, 3, 4, 5, 6)
         // when
         val lottoBundle = LottoVendingMachine.getLottoBundle(lottoCount) { lotto }
 
