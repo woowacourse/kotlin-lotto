336f111506733af2eca42ebfdc3026286eea0583
Doyeop Kim <dygames@naver.com>
2023-02-20 17:16:00 +0900

    refactor(Lotto, LottoNumber): LottoNumber 생성시 45개의 숫자를 캐싱해둠

--- src/main/kotlin/lotto/entity/LottoNumber.kt
+++ src/main/kotlin/lotto/entity/LottoNumber.kt
@@ -1,25 +1,15 @@
 package lotto.entity
 
 data class LottoNumber private constructor(val value: Int) {
-    init {
-        require(value in (Lotto.MINIMUM_LOTTO_NUMBER..Lotto.MAXIMUM_LOTTO_NUMBER)) {
-            String.format(
-                ERROR_MESSAGE_RANGE_1_TO_45,
-                value
-            )
-        }
-    }
-
     companion object {
         const val ERROR_MESSAGE_RANGE_1_TO_45 = "당첨 번호는 1에서 45 사이의 숫자여야 합니다. 입력된 당첨 번호는 %d 입니다."
-        private val lottoNumberCache = mutableMapOf<Int, LottoNumber>()
+        val lottoNumberCache = Lotto.LOTTO_RANGE.map { LottoNumber(it) }
 
         fun from(value: Int): LottoNumber {
-            if (lottoNumberCache.containsKey(value)) {
-                return lottoNumberCache[value]!!
+            require(value in Lotto.LOTTO_RANGE) {
+                String.format(ERROR_MESSAGE_RANGE_1_TO_45, value)
             }
-            lottoNumberCache[value] = LottoNumber(value)
-            return lottoNumberCache[value]!!
+            return lottoNumberCache[value]
         }
     }
 }
