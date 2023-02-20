74771181d0ac6eb954d64bcabeafcb4d74580484
hyunji1203 <hj27720@gmail.com>
2023-02-20 13:50:37 +0900

    refactor(Lotto): 에러 메세지 이름 변경

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -3,8 +3,8 @@ package domain
 class Lotto(val numbers: List<LottoNumber>) {
 
     init {
-        require(numbers.size == 6) { INPUT_LOTTO_SIZE_ERROR_MESSAGE }
-        require(numbers.toSet().size == 6) { INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE }
+        require(numbers.size == 6) { LOTTO_SIZE_ERROR_MESSAGE }
+        require(numbers.toSet().size == 6) { LOTTO_DUPLICATE_ERROR_MESSAGE }
     }
 
     fun countMatchNumber(lotto: Lotto): Int {
@@ -19,7 +19,7 @@ class Lotto(val numbers: List<LottoNumber>) {
         Rank.valueOf(countMatchNumber(lotto), hasBonusNumber(bonusNumber))
 
     companion object {
-        const val INPUT_LOTTO_SIZE_ERROR_MESSAGE = "당첨 번호가 6개가 아닙니다"
-        const val INPUT_LOTTO_DUPLICATE_ERROR_MESSAGE = "당첨 번호가 중복되었습니다."
+        const val LOTTO_SIZE_ERROR_MESSAGE = "당첨 번호가 6개가 아닙니다"
+        const val LOTTO_DUPLICATE_ERROR_MESSAGE = "당첨 번호가 중복되었습니다."
     }
 }
