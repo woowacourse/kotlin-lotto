cc32d4877a20367e218f5079ba1e814eda556bb6
pingu244 <69796976+pingu244@users.noreply.github.com>
2023-02-20 17:14:47 +0900

    refactor: getUserLotto 수정에 따라 인자를 추가하였다.

--- src/test/kotlin/lotto/domain/LottoControllerTest.kt
+++ src/test/kotlin/lotto/domain/LottoControllerTest.kt
@@ -1,5 +1,6 @@
 package lotto.domain
 
+import lotto.model.Lotto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -7,6 +8,7 @@ class LottoControllerTest {
 
     @Test
     fun `구매한 장 수 만큼 로또를 발급한다`() {
-        assertThat(LottoController().getUserLotto(5).lotto.size).isEqualTo(5)
+        val manualLotto = listOf(Lotto(1, 2, 3, 4, 5, 6))
+        assertThat(LottoController().getUserLotto(manualLotto, 4).lotto.size).isEqualTo(5)
     }
 }
