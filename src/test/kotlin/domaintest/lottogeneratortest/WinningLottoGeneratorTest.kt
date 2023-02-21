081d5404ad59577ca1dfdf5f206e9ac848c7efae
boogieWoogie <kimjinuk1999@gmail.com>
2023-02-21 15:04:05 +0900

    test: WinningLottoGenerator가 생성한 winningLotto의 catchLotto가 특정 로또와의 LottoNumber와 모두 똑같을때 정말 1등이 판단이 되는지 테스트

--- src/test/kotlin/domaintest/lottogeneratortest/WinningLottoGeneratorTest.kt
+++ src/test/kotlin/domaintest/lottogeneratortest/WinningLottoGeneratorTest.kt
@@ -1,6 +1,8 @@
 package domaintest.lottogeneratortest
 
+import domain.lottogenerator.ManualLottoGenerator
 import domain.lottogenerator.WinningLottoGenerator
+import domain.model.LottoResult
 import domain.model.WinningLotto
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -11,6 +13,11 @@ class WinningLottoGeneratorTest {
     fun `1등 로또를 생성하기`() {
         val winningLotto = WinningLottoGenerator().generateWinningLotto(listOf(1, 2, 3, 4, 5, 6), 7)
 
-        assertThat(winningLotto).isInstanceOf(WinningLotto::class.java)
+        val lotto = ManualLottoGenerator().generate(listOf(1, 2, 3, 4, 5, 6))
+
+        val actual =
+            LottoResult.valueOf(lotto.getMatchCount(winningLotto.catchLotto), lotto.contains(winningLotto.bonusNumber))
+
+        assertThat(actual).isEqualTo(LottoResult.FIRST)
     }
 }
