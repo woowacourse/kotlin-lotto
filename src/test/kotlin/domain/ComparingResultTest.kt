09bdb86bc726012b2422ce37effc2ece7f2821d1
SangHyun Moon <no1msh1217@gmail.com>
2023-02-21 16:27:12 +0900

    refactor: 불필요한 검증 및 테스트 제거

--- src/test/kotlin/domain/ComparingResultTest.kt
+++ src/test/kotlin/domain/ComparingResultTest.kt
@@ -20,19 +20,4 @@ class ComparingResultTest {
         val comparingResult = ComparingResult(matchedCount, true)
         assertThat(comparingResult.matchedCount).isEqualTo(matchedCount)
     }
-
-    @ValueSource(ints = [-1, 7, 8])
-    @ParameterizedTest
-    fun `맞춘 개수가 0~6이 아니라면 예외처리`(matchedCount: Int) {
-        assertThatIllegalArgumentException()
-            .isThrownBy { ComparingResult(matchedCount, false) }
-            .withMessageContaining("[Error] 잘못된 값 : $matchedCount matchedCount는 0 부터 6 까지 가능합니다.")
-    }
-
-    @Test
-    fun `당첨번호 6개와 같을 때 보너스 번호가 일치할 수 없다`() {
-        assertThatIllegalArgumentException()
-            .isThrownBy { ComparingResult(6, true) }
-            .withMessageContaining("[Error] 당첨번호 6개가 같다면 보너스 번호가 일치할 수 없습니다.")
-    }
 }
