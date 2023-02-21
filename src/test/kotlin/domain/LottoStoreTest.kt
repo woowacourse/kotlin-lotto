fd70c6bebe8dad7210b4bc015e4332d8e3776265
re4rk <37167652+re4rk@users.noreply.github.com>
2023-02-22 02:00:26 +0900

    Refactor(LottoStoreTest): TestLottoGenerator 생성으로 예측할 수 있도록 수정

--- src/test/kotlin/domain/LottoStoreTest.kt
+++ src/test/kotlin/domain/LottoStoreTest.kt
@@ -3,28 +3,40 @@ package domain
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class LottoStoreTest {
     private val lottoStore = LottoStore(RandomLottoGenerator)
+
+    inner class TestLottoGenerator : LottoGenerator {
+        private var lotateTickets = listOf(0, 1, 2, 3, 4, 5)
+        override fun generateLotto(): Lotto {
+            lotateTickets = lotateTickets.map { (it + 1) % 45 }
+            return Lotto(*lotateTickets.toIntArray())
+        }
+    }
 
     @Test
     fun `원하는 번호들의 수동 로또들을 구매한다`() {
         val money = Money(2000)
         val lottoTickets: LottoTickets = lottoStore.buyManualLotto(money.count, intArrayOf(1, 2, 3, 4, 5, 6), intArrayOf(1, 11, 22, 33, 44, 45))
-        assertThat(lottoTickets.size).isEqualTo(2)
-        assertThat(lottoTickets[0].toList().map { it.number }).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
-        assertThat(lottoTickets[1].toList().map { it.number }).isEqualTo(listOf(1, 11, 22, 33, 44, 45))
+        assertAll({
+            assertThat(lottoTickets.size).isEqualTo(2)
+            assertThat(lottoTickets[0].toList().map { it.number }).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+            assertThat(lottoTickets[1].toList().map { it.number }).isEqualTo(listOf(1, 11, 22, 33, 44, 45))
+        })
     }
 
     @Test
     fun `자동 로또들을 구매한다`() {
-        val money = Money(100000)
+        val money = Money(2000)
+        val lottoStore = LottoStore(TestLottoGenerator())
         val lottoTickets: LottoTickets = lottoStore.buyAutoLotto(money.count)
-        assertThat(lottoTickets.size).isEqualTo(100)
-        lottoTickets.forEach { lotto ->
-            assertThat(lotto.toList()).hasSize(6)
-            lotto.toList().forEach { assertThat(it.number).isBetween(1, 45) }
-        }
+        assertAll({
+            assertThat(lottoTickets.size).isEqualTo(2)
+            assertThat(lottoTickets[0].toList().map { it.number }).isEqualTo(listOf(1, 2, 3, 4, 5, 6))
+            assertThat(lottoTickets[1].toList().map { it.number }).isEqualTo(listOf(2, 3, 4, 5, 6, 7))
+        })
     }
 
     @Test
