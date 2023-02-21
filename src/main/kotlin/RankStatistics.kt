4d21493e9f141eba9b1a4b966e474c67ba28f352
chws0508 <chws0508@naver.com>
2023-02-21 16:11:53 +0900

    feat : 등수 통계 기능 구현

---
+++ src/main/kotlin/RankStatistics.kt
@@ -0,0 +1,26 @@
+class RankStatistics(val moeny: Int) : PaymentMoney(moeny) {
+    private val rankCount = HashMap<Rank, Int>()
+
+    fun updateRankCount(rank: Rank) {
+        this.rankCount[rank] = (this.rankCount[rank] ?: INITIALIZE_TO_ZERO) + PLUS_ONE
+    }
+
+    fun getRankCount(rank: Rank): Int {
+        val count = runCatching { return this.rankCount.getValue(rank) }.getOrNull()
+        return count ?: INITIALIZE_TO_ZERO
+    }
+
+    fun getProfitRate(): Double {
+        var profit = INITIALIZE_TO_DOUBLE_ZERO
+        this.rankCount.forEach { (rank, count) ->
+            profit += rank.winningMoney * count
+        }
+        return profit / moeny
+    }
+
+    companion object{
+        const val INITIALIZE_TO_ZERO = 0
+        const val INITIALIZE_TO_DOUBLE_ZERO = 0.00
+        const val PLUS_ONE = 1
+    }
+}
