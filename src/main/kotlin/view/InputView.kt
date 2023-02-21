3f7906b024b53f235a010bab7f4221676631f42b
mendel <rhthrhrl0@naver.com>
2023-02-22 01:06:19 +0900

    refactor(InputView): 논리적 오류가 아니면 null 반환값을 활용해서 재입력 받도록 적용

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -9,21 +9,37 @@ class InputView : InputViewInterface {
     override fun getMoney(): PurchaseLottoMoney {
         while (true) {
             println(INPUT_MESSAGE_FOR_PURCHASE_MONEY)
-            val money = readln()
-            runCatching { PurchaseLottoMoney(money.toInt()) }
+            val money = validateMoney(readln().toIntOrNull()) ?: continue
+            runCatching { PurchaseLottoMoney(money) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
     }
 
+    private fun validateMoney(money: Int?): Int? {
+        if (money == null) {
+            println(ERROR_MONEY_TYPE_CONVERT)
+            return null
+        }
+        return money
+    }
+
     override fun getManualCount(purchaseLottoMoney: PurchaseLottoMoney): LottoPurchaseInfo {
+        println(INPUT_MESSAGE_MANUAL_LOTTO_COUNT)
         while (true) {
-            println(INPUT_MESSAGE_MANUAL_LOTTO_COUNT)
-            val manualCount = readln().toIntOrNull() ?: Int.MAX_VALUE
+            val manualCount = validateManualCount(readln().toIntOrNull()) ?: continue
             runCatching { LottoPurchaseInfo(purchaseLottoMoney, manualCount) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
+    }
+
+    private fun validateManualCount(count: Int?): Int? {
+        if (count == null) {
+            println(ERROR_MANUAL_COUNT_TYPE_CONVERT)
+            return null
+        }
+        return count
     }
 
     override fun getManualNumbers(purchaseInfo: LottoPurchaseInfo): List<Lotto> {
@@ -35,31 +51,55 @@ class InputView : InputViewInterface {
 
     private fun getManualNumber(): Lotto {
         while (true) {
-            val lotto = readln().split(",").map { it.trim().toIntOrNull() ?: -1 }
+            val lotto = validateManualNumber(readln().split(",").map { it.trim().toIntOrNull() }) ?: continue
             runCatching { Lotto(lotto.map { LottoNumber.from(it) }.toSet()) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
     }
 
+    private fun validateManualNumber(lotto: List<Int?>): List<Int>? {
+        if (lotto.contains(null)) {
+            println(ERROR_MANUAL_NUMBERS_FORMAT_IS_INCORRECT)
+            return null
+        }
+        return lotto.map { it!! }
+    }
+
     override fun getWinningNumbers(): Lotto {
+        println(INPUT_MESSAGE_FOR_WINNING_NUMBERS)
         while (true) {
-            println(INPUT_MESSAGE_FOR_WINNING_NUMBERS)
-            val numbers = readln()
-            runCatching { Lotto(numbers.split(",").map { LottoNumber.from(it.trim().toInt()) }.toSet()) }
+            val numbers = validateWinningNumbers(readln().split(",").map { it.trim().toIntOrNull() }) ?: continue
+            runCatching { Lotto(numbers.map { LottoNumber.from(it) }.toSet()) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
     }
 
+    private fun validateWinningNumbers(numbers: List<Int?>): List<Int>? {
+        if (numbers.contains(null)) {
+            println(ERROR_MANUAL_NUMBERS_FORMAT_IS_INCORRECT)
+            return null
+        }
+        return numbers.map { it!! }
+    }
+
     override fun getBonusNumber(): LottoNumber {
+        println(INPUT_MESSAGE_FOR_BONUS_NUMBER)
         while (true) {
-            println(INPUT_MESSAGE_FOR_BONUS_NUMBER)
-            val bonusNumber = readln()
-            runCatching { LottoNumber.from(bonusNumber.toInt()) }
+            val bonusNumber = validateBonusNumber(readln().toIntOrNull()) ?: continue
+            runCatching { LottoNumber.from(bonusNumber) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
+    }
+
+    private fun validateBonusNumber(bonusNumber: Int?): Int? {
+        if (bonusNumber == null) {
+            println(ERROR_BONUS_NUMBER_TYPE_CONVERT)
+            return null
+        }
+        return bonusNumber
     }
 
     companion object {
@@ -68,5 +108,10 @@ class InputView : InputViewInterface {
         private const val INPUT_MESSAGE_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
         private const val INPUT_MESSAGE_FOR_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val INPUT_MESSAGE_FOR_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
+
+        private const val ERROR_MONEY_TYPE_CONVERT = "[ERROR] 금액을 숫자로 다시 입력해주세요."
+        private const val ERROR_MANUAL_COUNT_TYPE_CONVERT = "[ERROR] 수동 발급 개수를 숫자로 다시 입력해주세요."
+        private const val ERROR_MANUAL_NUMBERS_FORMAT_IS_INCORRECT = "[ERROR] 콤마로 숫자를 구분해서 다시 입력해주세요."
+        private const val ERROR_BONUS_NUMBER_TYPE_CONVERT = "[ERROR] 보너스 넘버를 숫자로 다시 입력해주세요."
     }
 }
