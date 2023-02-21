bdd2bc135863de8e0d63d4e09ebdf9d209739e15
krrong <khk9664@naver.com>
2023-02-21 16:10:54 +0900

    refactor(InputView): while(true) 반복문 재귀로 변경

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,29 +2,35 @@ package view
 
 class InputView : InputViewInterface {
     override fun getMoney(): Int {
-        while (true) {
+        return runCatching {
             println("구입 금액을 입력해 주세요.")
             val money = readln()
-            runCatching { money.toInt() / 1000 }
-                .onSuccess { return it }
+            money.toInt()
+        }.getOrElse { error ->
+            println(error)
+            getMoney()
         }
     }
 
     override fun getNumbers(): Set<Int> {
-        while (true) {
+        return runCatching {
             println("지난 주 당첨 번호를 입력해 주세요.")
-            val numbers = readln()
-            runCatching { numbers.split(",").map { it.trim().toInt() }.toSet() }
-                .onSuccess { return it }
+            val numbers = readln().trim()
+            numbers.split(",").map { it.trim().toInt() }.toSet()
+        }.getOrElse { error ->
+            println(error)
+            getNumbers()
         }
     }
 
     override fun getBonusNumber(): Int {
-        while (true) {
+        return runCatching {
             println("보너스 볼을 입력해 주세요.")
             val bonusNumber = readln()
-            runCatching { bonusNumber.toInt() }
-                .onSuccess { return it }
+            bonusNumber.toInt()
+        }.getOrElse { error ->
+            println(error)
+            getBonusNumber()
         }
     }
 }
