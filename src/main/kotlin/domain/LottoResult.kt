9d79c6f837bae4021f3626a81cf1dc0ae4a979ae
ki960213 <ki960213@github.com>
2023-02-21 15:28:28 +0900

    refactor(LottoResult): LottoResult의 정적 팩토리 메서드 리팩터링

--- src/main/kotlin/domain/LottoResult.kt
+++ src/main/kotlin/domain/LottoResult.kt
@@ -13,15 +13,14 @@ data class LottoResult(private val result: Map<Rank, Int>) {
 
     companion object {
         private const val ERROR_COUNT_OF_LOTTO_AT_LEAST_ONE = "적어도 하나 이상의 복권을 넣어야합니다."
-        fun of(lottos: List<Lotto>, winningLotto: WinningLotto): LottoResult {
-            val result = Rank.values().associateWith { 0 }.toMutableMap()
-
-            lottos.forEach {
-                val rank = Rank.valueOf(winningLotto.getCountOfMatch(it), winningLotto.matchBonus(it))
-                result[rank] = (result[rank] ?: 0) + 1
-            }
-
-            return LottoResult(result)
-        }
+        fun of(lottos: List<Lotto>, winningLotto: WinningLotto): LottoResult =
+            LottoResult(
+                Rank.values().associateWith { 0 }.toMutableMap().apply {
+                    lottos.forEach {
+                        val rank = Rank.valueOf(winningLotto.getCountOfMatch(it), winningLotto.matchBonus(it))
+                        this[rank] = (this[rank] ?: 0) + 1
+                    }
+                },
+            )
     }
 }
