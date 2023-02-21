6d57593d2e50d232f1e416f937891d5d42f32c85
mendel <rhthrhrl0@naver.com>
2023-02-21 23:54:27 +0900

    feature(InputView): 수동 로또 구매 수 입력받는 기능과 수동 로또 번호 입력받는 기능 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,6 +2,7 @@ package view
 
 import domain.Lotto
 import domain.LottoNumber
+import domain.LottoPurchaseInfo
 import domain.PurchaseLottoMoney
 
 class InputView : InputViewInterface {
@@ -10,6 +11,32 @@ class InputView : InputViewInterface {
             println(INPUT_MESSAGE_FOR_PURCHASE_MONEY)
             val money = readln()
             runCatching { PurchaseLottoMoney(money.toInt()) }
+                .onSuccess { return it }
+                .onFailure { println(it.message) }
+        }
+    }
+
+    override fun getManualCount(purchaseLottoMoney: PurchaseLottoMoney): LottoPurchaseInfo {
+        while (true) {
+            println(INPUT_MESSAGE_MANUAL_LOTTO_COUNT)
+            val manualCount = readln().toIntOrNull() ?: Int.MAX_VALUE
+            runCatching { LottoPurchaseInfo(purchaseLottoMoney, manualCount) }
+                .onSuccess { return it }
+                .onFailure { println(it.message) }
+        }
+    }
+
+    override fun getManualNumbers(purchaseInfo: LottoPurchaseInfo): List<Lotto> {
+        println(INPUT_MESSAGE_MANUAL_LOTTO_NUMBERS)
+        val lottos = mutableListOf<Lotto>()
+        repeat(purchaseInfo.manualCount) { lottos.add(getManualNumber()) }
+        return lottos
+    }
+
+    private fun getManualNumber(): Lotto {
+        while (true) {
+            val lotto = readln().split(",").map { it.trim().toIntOrNull() ?: -1 }
+            runCatching { Lotto(lotto.map { LottoNumber.from(it) }.toSet()) }
                 .onSuccess { return it }
                 .onFailure { println(it.message) }
         }
@@ -37,6 +64,8 @@ class InputView : InputViewInterface {
 
     companion object {
         private const val INPUT_MESSAGE_FOR_PURCHASE_MONEY = "구입 금액을 입력해 주세요."
+        private const val INPUT_MESSAGE_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요."
+        private const val INPUT_MESSAGE_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
         private const val INPUT_MESSAGE_FOR_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val INPUT_MESSAGE_FOR_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     }
--- src/main/kotlin/view/InputViewInterface.kt
+++ src/main/kotlin/view/InputViewInterface.kt
@@ -2,10 +2,13 @@ package view
 
 import domain.Lotto
 import domain.LottoNumber
+import domain.LottoPurchaseInfo
 import domain.PurchaseLottoMoney
 
 interface InputViewInterface {
     fun getMoney(): PurchaseLottoMoney
+    fun getManualCount(purchaseLottoMoney: PurchaseLottoMoney): LottoPurchaseInfo
+    fun getManualNumbers(purchaseInfo: LottoPurchaseInfo): List<Lotto>
     fun getWinningNumbers(): Lotto
     fun getBonusNumber(): LottoNumber
 }
