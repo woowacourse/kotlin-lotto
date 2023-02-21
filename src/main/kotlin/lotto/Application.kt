13c3bbb3f76238f0b12caaee1ab7356550fa3d3a
whk06061 <gju06061@naver.com>
2023-02-21 15:43:17 +0900

    refactor: 전역적인 논리 오류를 잡아주는 함수를 추가한다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,6 +3,9 @@ package lotto
 import lotto.controller.Controller
 
 fun main() {
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        println(e.message)
+    }
     val controller = Controller()
     controller.start()
 }
