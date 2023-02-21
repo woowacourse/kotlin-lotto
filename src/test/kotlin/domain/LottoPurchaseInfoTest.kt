d761881978be2f4cb82cc34ea0de9bf75dbf936b
mendel <rhthrhrl0@naver.com>
2023-02-21 22:17:45 +0900

    test(LottoPurchaseInfoTest): 수동발급의 개수가 구매 가능 개수의 범위 안에 들어오면 에러가 발생하지 않는지 검증하는 테스트 코드 추가

--- src/test/kotlin/domain/LottoPurchaseInfoTest.kt
+++ src/test/kotlin/domain/LottoPurchaseInfoTest.kt
@@ -1,6 +1,7 @@
 package domain
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalArgumentException
 
@@ -17,5 +18,12 @@ class LottoPurchaseInfoTest() {
         assertThrows<IllegalArgumentException> {
             LottoPurchaseInfo(PurchaseLottoMoney(14000), -1)
         }
+    }
+
+    @Test
+    fun `수동 구매 개수가 구매 가능 개수의 범위 안에 들어오면 에러 발생하지 않음`() {
+        assertDoesNotThrow { LottoPurchaseInfo(PurchaseLottoMoney(14000), 14) }
+        assertDoesNotThrow { LottoPurchaseInfo(PurchaseLottoMoney(14000), 0) }
+        assertDoesNotThrow { LottoPurchaseInfo(PurchaseLottoMoney(14000), 10) }
     }
 }
