196645b67121145fda15368024c0c04d8f9e5b91
hyemdooly <hyemdooly@gmail.com>
2023-02-21 14:36:30 +0900

    refactor(OutputView): 출력 문구 상수화

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -6,46 +6,61 @@ import domain.Rank
 
 class OutputView {
     fun outputGetAmount() {
-        println("구입금액을 입력해 주세요.")
+        println(GET_AMOUNT)
     }
 
     fun outputGetCount() {
-        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        println(GET_COUNT)
     }
 
     fun outputGetManualLottos() {
-        println("수동으로 구매할 번호를 입력해 주세요.")
+        println(GET_MANUAL_LOTTOS)
     }
 
     fun outputGetWinningNumbers() {
-        println("지난 주 당첨 번호를 입력해 주세요.")
+        println(GET_WINNING_NUMBERS)
     }
 
     fun outputGetBonusNumber() {
-        println("보너스 볼을 입력해 주세요.")
+        println(GET_BONUS_NUMBER)
     }
 
     fun outputLottos(manual: List<Lotto>, auto: List<Lotto>) {
-        println("수동으로 ${manual.size}장, 자동으로 ${auto.size}개를 구매했습니다.")
+        println(NOTICE_LOTTOS.format(manual.size, auto.size))
         (manual + auto).forEach { println(it.toList()) }
         println()
     }
 
     fun outputResult(lottoResult: LottoResult) {
-        println("\n당첨 통계")
-        println("---------")
+        println("\n" + NOTICE_RESULT)
+        println(DOTTED_LINE)
         for (value in Rank.values().reversed()) {
             if (value == Rank.MISS) continue
             if (value.mustHaveBonus) {
-                println("%d개 일치, 보너스 볼 일치 (%s)원 - %d개".format(value.countOfMatch, value.winningMoney, lottoResult[value]))
+                println(RESULT_HAVE_BONUS.format(value.countOfMatch, value.winningMoney, lottoResult[value]))
                 continue
             }
-            println("%d개 일치 (%s)원 - %d개".format(value.countOfMatch, value.winningMoney, lottoResult[value]))
+            println(RESULT_BASIC.format(value.countOfMatch, value.winningMoney, lottoResult[value]))
         }
-        println("총 수익률은 ${lottoResult.getRateOfReturn()}입니다.")
+        println(RESULT_RATE_OF_RETURN.format(lottoResult.getRateOfReturn()))
     }
 
     fun outputError(e: String?) {
-        println(e ?: "알 수 없는 에러입니다.")
+        println(e ?: UNKNOWN_ERROR)
+    }
+
+    companion object {
+        private const val GET_AMOUNT = "구입금액을 입력해 주세요."
+        private const val GET_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
+        private const val GET_MANUAL_LOTTOS = "수동으로 구매할 번호를 입력해 주세요."
+        private const val GET_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
+        private const val GET_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
+        private const val NOTICE_LOTTOS = "수동으로 %d장, 자동으로 %d개를 구매했습니다."
+        private const val NOTICE_RESULT = "당첨 통계"
+        private const val DOTTED_LINE = "---------"
+        private const val RESULT_HAVE_BONUS = "%d개 일치, 보너스 볼 일치 (%s)원 - %d개"
+        private const val RESULT_BASIC = "%d개 일치 (%s)원 - %d개"
+        private const val RESULT_RATE_OF_RETURN = "총 수익률은 %.2f입니다."
+        private const val UNKNOWN_ERROR = "알 수 없는 에러입니다."
     }
 }
