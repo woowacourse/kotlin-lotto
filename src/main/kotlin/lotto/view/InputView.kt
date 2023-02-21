e5a476941abc86a325fa06a87b678e4e517f4774
vrexpert <vrexpert2302@gmail.com>
2023-02-21 20:56:14 +0900

    feat(lotto): 수동 로또 개수 입력 및 수동 로또 입력 기능 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,7 +1,11 @@
 package lotto.view
 
+import lotto.utils.inputNullHandler
+
 object InputView {
     private const val GET_PURCHASE_MONEY_SCRIPT = "구입금액을 입력해 주세요."
+    private const val GET_MANUAL_LOTTO_COUNT_SCRIPT = "수동으로 구매할 로또 수를 입력해 주세요."
+    private const val GET_MANUAL_LOTTO_NUMBER_LINES_SCRIPT = "수동으로 구매할 번호를 입력해 주세요."
     private const val GET_MAIN_LOTTO_NUMBERS_SCRIPT = "지난 주 당첨 번호를 입력해 주세요."
     private const val GET_BONUS_LOTTO_NUMBER_SCRIPT = "보너스 볼을 입력해 주세요."
     private const val MAIN_LOTTO_NUMBERS_DELIMITER = ','
@@ -10,17 +14,34 @@ object InputView {
 
     fun getPurchaseMoney(): Int {
         println(GET_PURCHASE_MONEY_SCRIPT)
-        return readln().trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_NUMBER)
+        return readln().trim().toIntOrNull() ?: inputNullHandler(ERROR_NOT_NUMBER, getPurchaseMoney())
     }
 
-    fun getMainLottoNumber(): List<Int> {
+    fun getManualNumberLines(): List<List<Int>> {
+        println(GET_MANUAL_LOTTO_NUMBER_LINES_SCRIPT)
+        return List(getManualLottoCount()) { getLottoNumbers() }
+    }
+
+    fun getMainLottoNumbers(): List<Int> {
         println(GET_MAIN_LOTTO_NUMBERS_SCRIPT)
-        return readln().split(MAIN_LOTTO_NUMBERS_DELIMITER)
-            .map { it.trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_COMMA_NUMBER) }
+        return getLottoNumbers()
+    }
+
+    private fun getLottoNumbers(): List<Int> {
+        val lottoNumbers = readln().split(MAIN_LOTTO_NUMBERS_DELIMITER)
+            .map {
+                it.trim().toIntOrNull() ?: return inputNullHandler(ERROR_NOT_COMMA_NUMBER, getLottoNumbers())
+            }
+        return lottoNumbers
+    }
+
+    private fun getManualLottoCount(): Int {
+        println(GET_MANUAL_LOTTO_COUNT_SCRIPT)
+        return readln().trim().toIntOrNull() ?: inputNullHandler(ERROR_NOT_NUMBER, getManualLottoCount())
     }
 
     fun getBonusLottoNumber(): Int {
         println(GET_BONUS_LOTTO_NUMBER_SCRIPT)
-        return readln().trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_NUMBER)
+        return readln().trim().toIntOrNull() ?: inputNullHandler(ERROR_NOT_NUMBER, getBonusLottoNumber())
     }
 }
