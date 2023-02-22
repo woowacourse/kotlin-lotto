03d1e3b47b1ed3e3dc1a4f5c7748ccbdcadc2968
s9hn <tnghqn@naver.com>
2023-02-22 21:15:32 +0900

    [REFACTOR(RankTest)] RankTest 수정

--- src/test/kotlin/domain/RankTest.kt
+++ src/test/kotlin/domain/RankTest.kt
@@ -1,17 +1,78 @@
 package domain
 
+import domain.Rank.FIFTH
+import domain.Rank.FIRST
+import domain.Rank.FOURTH
+import domain.Rank.MISS
+import domain.Rank.SECOND
+import domain.Rank.THIRD
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RankTest {
     @Test
-    fun `당첨번호 등수를 확인한다`() {
+    fun `1등인지 확인한다`() {
         // given
-        val countOfMatch = 1
+        val countOfMatch = 6
         val matchBonus = false
 
         val actual = Rank.valueOf(countOfMatch, matchBonus)
 
-        assertThat(actual).isEqualTo(Rank.MISS)
+        assertThat(actual).isEqualTo(FIRST)
+    }
+
+    @Test
+    fun `2등인지 확인한다`() {
+        // given
+        val countOfMatch = 5
+        val matchBonus = true
+
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        assertThat(actual).isEqualTo(SECOND)
+    }
+
+    @Test
+    fun `3등인지 확인한다`() {
+        // given
+        val countOfMatch = 5
+        val matchBonus = false
+
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        assertThat(actual).isEqualTo(THIRD)
+    }
+
+    @Test
+    fun `4등인지 확인한다`() {
+        // given
+        val countOfMatch = 4
+        val matchBonus = false
+
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        assertThat(actual).isEqualTo(FOURTH)
+    }
+
+    @Test
+    fun `5등인지 확인한다`() {
+        // given
+        val countOfMatch = 3
+        val matchBonus = false
+
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        assertThat(actual).isEqualTo(FIFTH)
+    }
+
+    @Test
+    fun `꼴등인지 확인한다`() {
+        // given
+        val countOfMatch = 1
+        val matchBonus = true
+
+        val actual = Rank.valueOf(countOfMatch, matchBonus)
+
+        assertThat(actual).isEqualTo(MISS)
     }
 }
