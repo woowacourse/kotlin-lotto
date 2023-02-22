fd89b39a8838c39df9ae2baaa4aed272dff5b031
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-22 17:53:27 +0900

    feat: 수동로또를 생산하는 기능 구현

--- src/main/kotlin/domain/LottoMachine.kt
+++ src/main/kotlin/domain/LottoMachine.kt
@@ -1,9 +1,11 @@
 package domain
 
 class LottoMachine(val lottoGenerator: LottoGenerator) {
-
-    fun generateLottos(money: Money): List<Lotto> {
-        val count = money.getLottoCount()
+    fun generateAutoLottos(count: Int): List<Lotto> {
         return (1..count).map { lottoGenerator.generateLotto() }
+    }
+
+    fun generateManaulLottos(lottos: List<String>): List<Lotto> {
+        return lottos.map { Lotto(it.trim().split(",").map { LottoNumber.from(it.toInt()) }) }
     }
 }
