dff958c607bbb8af9ef6ed23b7aefcc02e3581dd
otter <a33397397@gmail.com>
2023-02-22 16:47:56 +0900

    refactor: LotteryNumber class 개선
    - class에서 data class로 변경해 equals(), hashCode(), toString()의 불필요해지는 메서드들을 줄임
    - number 맴버변수를 toInt()로 외부에서 사용할 수 있도록 변경함 (데 팍토)

--- src/main/kotlin/lotto/domain/LotteryNumber.kt
+++ src/main/kotlin/lotto/domain/LotteryNumber.kt
@@ -1,35 +1,18 @@
 package lotto.domain
 
-class LotteryNumber(
-    val number: Int
+data class LotteryNumber(
+    private val number: Int
 ) {
     init {
         checkNumberBoundary()
     }
 
+    fun toInt(): Int = number
+
     private fun checkNumberBoundary() {
         require(number in LOTTERY_NUMBER_LOWER_BOUNDARY..LOTTERY_NUMBER_UPPER_BOUNDARY) {
             "로또 번호는 1 이상 45 이하의 숫자여야 합니다."
         }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LotteryNumber
-
-        if (number != other.number) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return number
-    }
-
-    override fun toString(): String {
-        return "$number"
     }
 
     companion object {
