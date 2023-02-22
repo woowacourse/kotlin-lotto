76940eeb51e65f2a2f98de934106a8ed5283f3ae
EmilyCh0 <emily9803@ewhain.net>
2023-02-22 22:07:43 +0900

    test: WinningNumbers 생성 메서드 분리

--- src/test/kotlin/WinningNumbersTest.kt
+++ src/test/kotlin/WinningNumbersTest.kt
@@ -7,126 +7,58 @@ import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class WinningNumbersTest {
+
+    private fun generateWinningNumbers(catchNumbers: List<Int>, bonusNumber: Int): WinningNumbers {
+        return WinningNumbers(catchNumbers.map { LottoNumber.from(it) }.toSet(), LottoNumber.from(bonusNumber))
+    }
+
     @Test
     fun `당첨 번호가 6개가 아닌 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            WinningNumbers(
-                setOf(
-                    LottoNumber.from(1),
-                    LottoNumber.from(2),
-                    LottoNumber.from(3),
-                    LottoNumber.from(4),
-                    LottoNumber.from(5),
-                    LottoNumber.from(6),
-                    LottoNumber.from(7)
-                ),
-                LottoNumber.from(8)
-            )
+            generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6, 7), 8)
         }
     }
 
     @Test
     fun `당첨 번호가 6개인 경우`() {
         assertDoesNotThrow {
-            WinningNumbers(
-                setOf(
-                    LottoNumber.from(1),
-                    LottoNumber.from(2),
-                    LottoNumber.from(3),
-                    LottoNumber.from(4),
-                    LottoNumber.from(5),
-                    LottoNumber.from(6)
-                ),
-                LottoNumber.from(7)
-            )
+            generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
         }
     }
 
     @Test
     fun `보너스 번호가 당첨 번호와 중복된 경우 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            WinningNumbers(
-                setOf(
-                    LottoNumber.from(1),
-                    LottoNumber.from(2),
-                    LottoNumber.from(3),
-                    LottoNumber.from(4),
-                    LottoNumber.from(5),
-                    LottoNumber.from(6)
-                ),
-                LottoNumber.from(3)
-            )
+            generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 3)
         }
     }
 
     @Test
     fun `보너스 번호가 당첨 번호와 중복되지 않은 경우`() {
         assertDoesNotThrow {
-            WinningNumbers(
-                setOf(
-                    LottoNumber.from(1),
-                    LottoNumber.from(2),
-                    LottoNumber.from(3),
-                    LottoNumber.from(4),
-                    LottoNumber.from(5),
-                    LottoNumber.from(6)
-                ),
-                LottoNumber.from(7)
-            )
+            generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
         }
     }
 
     @Test
     fun `당첨 번호와 일치하는 번호의 개수를 구한다`() {
-        val winningNumber = WinningNumbers(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            ),
-            LottoNumber.from(7)
-        )
+        val winningNumber = generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
         val lotto = Lotto.create(listOf(1, 2, 3, 7, 8, 9))
         assertThat(
-            winningNumber.getMatchCount(
-                lotto.numbers
-            )
+            winningNumber.getMatchCount(lotto.numbers)
         ).isEqualTo(3)
     }
 
     @Test
     fun `로또 번호가 보너스 번호를 포함하는 경우`() {
-        val winningNumber = WinningNumbers(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            ),
-            LottoNumber.from(7)
-        )
+        val winningNumber = generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 7)
         val lotto = Lotto.create(listOf(1, 2, 3, 7, 8, 9))
         assertThat(winningNumber.checkMatchBonus(lotto.numbers)).isEqualTo(true)
     }
 
     @Test
     fun `로또 번호가 보너스 번호를 포함하지 않는 경우`() {
-        val winningNumber = WinningNumbers(
-            setOf(
-                LottoNumber.from(1),
-                LottoNumber.from(2),
-                LottoNumber.from(3),
-                LottoNumber.from(4),
-                LottoNumber.from(5),
-                LottoNumber.from(6)
-            ),
-            LottoNumber.from(10)
-        )
+        val winningNumber = generateWinningNumbers(listOf(1, 2, 3, 4, 5, 6), 10)
         val lotto = Lotto.create(listOf(1, 2, 3, 7, 8, 9))
         assertThat(winningNumber.checkMatchBonus(lotto.numbers)).isEqualTo(false)
     }
