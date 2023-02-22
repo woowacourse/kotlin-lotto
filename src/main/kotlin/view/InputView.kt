cf57640aa416d590b0226a1e9a3d5d0d276102d2
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-02-22 22:01:48 +0900

    feat: 입력값에서 논리적인 오류가 아닌 예외는 재입력 받도록 수정

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -10,19 +10,37 @@ class InputView(
 ) {
     fun inputPurchasingMoney(): Money {
         println(PURCHASING_MONEY_INPUT_MESSAGE)
-        return Money(inputValidator.validateNumeric(readln().trim()))
+        lateinit var result: Money
+        kotlin.runCatching {
+            inputValidator.validateNumeric(readln().trim())
+        }.onSuccess {
+            result = Money(it)
+        }.onFailure {
+            println(INPUT_REPEAT)
+            return inputPurchasingMoney()
+        }
+        return result
     }
 
     fun inputManualLottoCount(): Int {
         println(PURCHASING_MANUAL_LOTTO_COUNT)
-        return readln().toInt()
+        var result: Int = 0
+        kotlin.runCatching {
+            inputValidator.validateNumeric(readln().trim())
+        }.onSuccess {
+            result = it
+        }.onFailure {
+            println(INPUT_REPEAT)
+            return inputManualLottoCount()
+        }
+        return result
     }
 
     fun inputManualLottos(manualLottoCount: Int): List<Lotto> {
         println(PURCHASING_MANUAL_LOTTO)
         val manualLottos = mutableListOf<Lotto>()
         repeat(manualLottoCount) {
-            val manualLotto = readln().trim().split(",")
+            val manualLotto = inputLottoNumbers() ?: return inputManualLottos(manualLottoCount)
             manualLottos.add(Lotto(manualLotto.map { LottoNumber(it.toInt()) }))
         }
         return manualLottos
@@ -30,12 +48,34 @@ class InputView(
 
     fun inputLastWeekWinningNumbers(): List<Int> {
         println(LAST_WEEK_WINNING_NUMBERS_INPUT_MESSAGE)
-        return inputValidator.validateWinningNumbersIsNumeric(readln().split(WINNING_NUMBER_DELIMITER))
+        return inputLottoNumbers() ?: inputLastWeekWinningNumbers()
+    }
+
+    private fun inputLottoNumbers(): List<Int>? {
+        lateinit var result: List<Int>
+        kotlin.runCatching {
+            inputValidator.validateWinningNumbersIsNumeric(readln().split(WINNING_NUMBER_DELIMITER))
+        }.onSuccess {
+            result = it
+        }.onFailure {
+            println(INPUT_REPEAT)
+            return null
+        }
+        return result
     }
 
     fun inputBonusBallNumber(): Int {
         println(BONUS_BALL_NUMBERS_INPUT_MESSAGE)
-        return inputValidator.validateNumeric(readln().trim())
+        var result: Int = 0
+        kotlin.runCatching {
+            inputValidator.validateNumeric(readln().trim())
+        }.onSuccess {
+            result = it
+        }.onFailure {
+            println(INPUT_REPEAT)
+            return inputBonusBallNumber()
+        }
+        return result
     }
 
     companion object {
@@ -44,6 +84,7 @@ class InputView(
         private const val PURCHASING_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요."
         private const val LAST_WEEK_WINNING_NUMBERS_INPUT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
         private const val BONUS_BALL_NUMBERS_INPUT_MESSAGE = "보너스 볼을 입력해 주세요."
+        private const val INPUT_REPEAT = "다시 입력해주세요."
 
         private const val WINNING_NUMBER_DELIMITER = ","
     }
