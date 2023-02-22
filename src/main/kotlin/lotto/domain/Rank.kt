9afb0f164cc2cf97f9337a9add03b62616d1fb43
Gloria <ippnsj@gmail.com>
2023-02-22 17:57:24 +0900

    refactor: 불필요한 멤버 변수 삭제

--- src/main/kotlin/lotto/domain/Rank.kt
+++ src/main/kotlin/lotto/domain/Rank.kt
@@ -1,12 +1,12 @@
 package lotto.domain
 
-enum class Rank(val countOfMatch: Int, val winningMoney: Int, val description: String) {
-    FIRST(6, 2_000_000_000, "6개 일치"),
-    SECOND(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
-    THIRD(5, 1_500_000, "5개 일치"),
-    FOURTH(4, 50_000, "4개 일치"),
-    FIFTH(3, 5_000, "3개 일치"),
-    MISS(0, 0, "일치 없음");
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
     fun calculatePrize(count: Int): Long {
         return winningMoney.toLong() * count
