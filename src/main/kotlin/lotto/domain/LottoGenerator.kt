e05a0218bdfe4b41921271108b0fcce8c804d36c
whk06061 <gju06061@naver.com>
2023-02-22 17:53:15 +0900

    refactor: LottoGenerator의 불필요한 자동 로또 생성 기능을 없앤다.

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -3,13 +3,9 @@ package lotto.domain
 object LottoGenerator {
     private val randomNumberGenerator = RandomNumberGenerator()
 
-    fun generateAuto(count: Int): List<Lotto> {
+    fun generate(count: Int): List<Lotto> {
         val lottoNumbers = mutableListOf<Lotto>()
         repeat(count) { lottoNumbers.add(Lotto(randomNumberGenerator.generate())) }
         return lottoNumbers
-    }
-
-    fun generateManual(numbers: List<Int>): Lotto {
-        return Lotto(numbers.map { LottoNumber.from(it) })
     }
 }
