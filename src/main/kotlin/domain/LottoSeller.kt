5582f15d7618dbf868f84ebf8d34aa80719d55aa
krrong <khk9664@naver.com>
2023-02-22 17:53:47 +0900

    feat(LottoSeller): 수동로또 발급기능 구현

--- src/main/kotlin/domain/LottoSeller.kt
+++ src/main/kotlin/domain/LottoSeller.kt
@@ -4,4 +4,12 @@ class LottoSeller(private val lottoMachine: LottoMachine = LottoVendingMachine()
     fun sellRandomLottos(count: Int): Ticket {
         return Ticket(List(count) { lottoMachine.createRandomLotto() })
     }
+
+    private fun sellManualLotto(numbers: Set<Int>): Lotto {
+        return lottoMachine.createManualLotto(numbers)
+    }
+
+    fun sellManualLottos(lottoNumbers: List<Set<Int>>): Ticket {
+        return Ticket(lottoNumbers.map { sellManualLotto(it) })
+    }
 }
--- src/test/kotlin/domain/LottoSellerTest.kt
+++ src/test/kotlin/domain/LottoSellerTest.kt
@@ -5,14 +5,30 @@ import org.junit.jupiter.api.Test
 
 class LottoSellerTest {
     @Test
-    fun `입력받은 개수만큼 로또를 발급한다`() {
+    fun `입력받은 개수만큼 랜덤 로또를 발급한다`() {
         // given
-
         val lottoMachine = LottoVendingMachine()
         val lottoSeller = LottoSeller(lottoMachine)
 
         // when
         val actual = lottoSeller.sellRandomLottos(2)
+
+        // then
+        assertThat(actual.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `입력받은 번호로 수동 로또를 발급한다`() {
+        // given
+        val lottoMachine = LottoVendingMachine()
+        val lottoSeller = LottoSeller(lottoMachine)
+        val lottoNumbers = listOf(
+            setOf(1, 2, 3, 4, 5, 6),
+            setOf(3, 4, 5, 6, 7, 8)
+        )
+
+        // when
+        val actual = lottoSeller.sellManualLottos(lottoNumbers)
 
         // then
         assertThat(actual.size).isEqualTo(2)
