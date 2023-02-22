b370019429642074e9f85f89ee03f72fbfec7d44
chws0508 <chws0508@naver.com>
2023-02-22 14:52:18 +0900

    feat : 로또를 생성하는 기능 구현

---
+++ src/main/kotlin/LottoMachine.kt
@@ -0,0 +1,10 @@
+class LottoMachine(val count: Count, val lottoGenerator: LottoGenerator) {
+
+    fun generateLotto(): Lotto {
+        return lottoGenerator.generateLotto()
+    }
+
+    fun generateLotteries(): List<Lotto> {
+        return List(count.value) { generateLotto() }
+    }
+}
---
+++ src/test/kotlin/LottoMachineTest.kt
@@ -0,0 +1,68 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest() {
+    @Test
+    fun `generator가 (1,2,3,4,5,6)인 리스트를 생성했을 때 로또 번호가(1,2,3,4,5,6)인 로또가 생성된다`() {
+        // when
+        val actual = LottoMachine(Count(14), TestLottoNumbersGenerator()).generateLotto().numbers
+        // then
+        val expected = Lotto(
+            listOf(
+                LottoNumber(1),
+                LottoNumber(2),
+                LottoNumber(3),
+                LottoNumber(4),
+                LottoNumber(5),
+                LottoNumber(6),
+            )
+        ).numbers
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `count가 14이면, 자동 로또가 14개 생성된다`() {
+        // when
+        val actual = LottoMachine(Count(14), TestLottoNumbersGenerator()).generateLotteries().size
+        // then
+        val expected = 14
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `generator가 (1,2,3,4,5,6)인 생성할 때, 모든 로또는 (1,2,3,4,5,6)이다`() {
+        // given
+        val lotteries = LottoMachine(Count(14), TestLottoNumbersGenerator()).generateLotteries()
+        // then
+        val expected = Lotto(
+            listOf(
+                LottoNumber(1),
+                LottoNumber(2),
+                LottoNumber(3),
+                LottoNumber(4),
+                LottoNumber(5),
+                LottoNumber(6),
+            )
+        )
+        lotteries.forEach { lotto ->
+            assertThat((lotto.numbers)).isEqualTo(expected)
+        }
+    }
+
+    class TestLottoNumbersGenerator() : LottoGenerator {
+        override fun generateLotto(): Lotto {
+            return Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                )
+            )
+        }
+    }
+}
