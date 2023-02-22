ff6405d4baac04c10ad47630910263c2f20fc0b4
hyunji1203 <hj27720@gmail.com>
2023-02-23 02:57:48 +0900

    refactor(LottoNumber): 함수 스타일 변경

--- src/main/kotlin/domain/LottoNumber.kt
+++ src/main/kotlin/domain/LottoNumber.kt
@@ -12,14 +12,10 @@ class LottoNumber private constructor(private val value: Int) {
         private const val LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 1에서 45 사이여야 합니다."
         private val NUMBERS: Map<Int, LottoNumber> = (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith(::LottoNumber)
 
-        fun from(value: Int): LottoNumber {
-            return NUMBERS[value] ?: throw IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE)
-        }
-        fun getMinNumber(): Int {
-            return MINIMUM_NUMBER
-        }
-        fun getMaxNumber(): Int {
-            return MAXIMUM_NUMBER
-        }
+        fun from(value: Int): LottoNumber = NUMBERS[value] ?: throw IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE)
+
+        fun getMinNumber(): Int = MINIMUM_NUMBER
+
+        fun getMaxNumber(): Int = MAXIMUM_NUMBER
     }
 }
