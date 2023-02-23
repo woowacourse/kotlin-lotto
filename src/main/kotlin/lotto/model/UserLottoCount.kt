c2bf7f3b30615e4d58ffef94a7bf91515e00f667
Sujin Kim <sujinkim919@gmail.com>
2023-02-23 14:01:39 +0900

    refactor: UserLottoCount 생성자 수정

--- src/main/kotlin/lotto/model/UserLottoCount.kt
+++ src/main/kotlin/lotto/model/UserLottoCount.kt
@@ -17,13 +17,5 @@ class UserLottoCount(
 
     companion object {
         private const val LOTTO_PRICE: Int = 1000
-
-        /*        fun create(money: Int, numberOfPassive: Int): UserLottoCount {
-                    val number = money / LOTTO_PRICE
-                    if (number < numberOfPassive) {
-                        throw IllegalArgumentException(ERROR_NOT_ENOUGH_MONEY)
-                    }
-                    return UserLottoCount(number, numberOfPassive)
-                }*/
     }
 }
