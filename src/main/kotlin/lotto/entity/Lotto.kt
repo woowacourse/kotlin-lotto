3f29c967797ff2cc61dc10f5696fe25a1ee4406d
DYGames <dygames@naver.com>
2023-02-24 01:09:03 +0900

    refactor: LottoNumber에 관련된 상수들을 LottoNumber로 이동

--- src/main/kotlin/lotto/entity/Lotto.kt
+++ src/main/kotlin/lotto/entity/Lotto.kt
@@ -16,8 +16,5 @@ class Lotto(val numbers: Set<LottoNumber>) {
     companion object {
         const val ERROR_MESSAGE_WIN_NUMBER_IS_SIX = "로또 번호는 6개여야 합니다. 입력된 로또 번호 개수는 %d 입니다."
         const val LOTTO_NUMBER_COUNT = 6
-        private const val MINIMUM_LOTTO_NUMBER = 1
-        private const val MAXIMUM_LOTTO_NUMBER = 45
-        val LOTTO_RANGE = (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER)
     }
 }
--- src/main/kotlin/lotto/entity/LottoNumber.kt
+++ src/main/kotlin/lotto/entity/LottoNumber.kt
@@ -2,11 +2,14 @@ package lotto.entity
 
 data class LottoNumber private constructor(val value: Int) {
     companion object {
-        const val ERROR_MESSAGE_RANGE_1_TO_45 = "당첨 번호는 1에서 45 사이의 숫자여야 합니다. 입력된 당첨 번호는 %d 입니다."
-        val lottoNumberCache = Lotto.LOTTO_RANGE.map { LottoNumber(it) }
+        private const val MINIMUM_LOTTO_NUMBER = 1
+        private const val MAXIMUM_LOTTO_NUMBER = 45
+        private val LOTTO_RANGE = (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER)
+        private const val ERROR_MESSAGE_RANGE_1_TO_45 = "당첨 번호는 1에서 45 사이의 숫자여야 합니다. 입력된 당첨 번호는 %d 입니다."
+        val lottoNumberCache = LOTTO_RANGE.map { LottoNumber(it) }
 
         fun from(value: Int): LottoNumber {
-            require(value in Lotto.LOTTO_RANGE) { ERROR_MESSAGE_RANGE_1_TO_45.format(value) }
+            require(value in LOTTO_RANGE) { ERROR_MESSAGE_RANGE_1_TO_45.format(value) }
             return lottoNumberCache[value - 1]
         }
     }
