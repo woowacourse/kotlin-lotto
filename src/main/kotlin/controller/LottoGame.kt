99692fec4d804149ef1e44de387fba33b262189f
hyunji1203 <hj27720@gmail.com>
2023-02-23 17:50:28 +0900

    feat: 논리 오류 처리

--- src/main/kotlin/controller/LottoGame.kt
+++ src/main/kotlin/controller/LottoGame.kt
@@ -21,26 +21,43 @@ class LottoGame {
     }
 
     private fun getMoney(): Money {
-        val money = input.inputMoney()
-        return money
+        return runCatching {
+            val money = input.inputMoney()
+            return money
+        }.onFailure {
+            println(INPUT_MONEY_ERROR_MESSAGE)
+        }.getOrDefault(getMoney())
     }
 
     private fun makeLottos(money: Money): Lottos {
-        val count = input.InputManualLottoCount()
-        val manualLottos = LottoMaker().makeManualLottos(input.inputManualLottoNumber(count).map { LottoMaker().wrapLotto(it) })
-        val autoLottos = LottoMaker().makeAutoLottos(money.lottoCount() - count)
-        output.outputLottoSizeMessage(count, money.lottoCount() - count)
-        output.outputLottos(Lottos(manualLottos + autoLottos))
-        return Lottos(manualLottos + autoLottos)
+        return runCatching {
+            val count = input.InputManualLottoCount()
+            val manualLottos = LottoMaker().makeManualLottos(input.inputManualLottoNumber(count).map { LottoMaker().wrapLotto(it) })
+            val autoLottos = LottoMaker().makeAutoLottos(money.lottoCount() - count)
+            output.outputLottoSizeMessage(count, money.lottoCount() - count)
+            output.outputLottos(Lottos(manualLottos + autoLottos))
+            return Lottos(manualLottos + autoLottos)
+        }.onFailure {
+            println(INPUT_LOTTO_ERROR_MESSAGE)
+        }.getOrDefault(makeLottos(money))
     }
 
     private fun startGame(lottos: Lottos): WinningResult {
-        val winningNumber = WinningNumber(LottoMaker().wrapLotto(input.inputWinningLotto()), input.inputBonusNumber())
-        return lottos.matchLottos(winningNumber)
+        return runCatching {
+            val winningNumber = WinningNumber(LottoMaker().wrapLotto(input.inputWinningLotto()), input.inputBonusNumber())
+            return lottos.matchLottos(winningNumber)
+        }.onFailure {
+            println(INPUT_LOTTO_ERROR_MESSAGE)
+        }.getOrDefault(startGame(lottos))
     }
 
     private fun endGame(winningResult: WinningResult, money: Money) {
         output.outputWinningResult(winningResult)
         output.outputYield(winningResult.calculateYield(money))
+    }
+
+    companion object {
+        private const val INPUT_MONEY_ERROR_MESSAGE = "[ERROR] 금액은 1000원으로 나누어떨어지는 양수여야 합니다."
+        private const val INPUT_LOTTO_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복되지 않은 6개의 수여야합니다."
     }
 }
