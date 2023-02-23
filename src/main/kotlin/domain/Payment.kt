938879f4702d7fe442f172413bcd2c4ba513c487
SangHyun Moon <no1msh1217@gmail.com>
2023-02-23 13:39:11 +0900

    refactor: 불필요한 nullable 제거

--- src/main/kotlin/domain/Payment.kt
+++ src/main/kotlin/domain/Payment.kt
@@ -1,15 +1,14 @@
 package domain
 
-class Payment(nullableAmount: Long?) {
-    val amount: Long
+class Payment(val amount: Long) {
 
     init {
-        if (nullableAmount == null) throw NumberFormatException("[Error] 1000 단위의 숫자로 값을 입력해주세요.")
-        amount = nullableAmount
         require((amount % 1000L == 0L) and (amount != 0L)) { "[Error] $amount 받은 돈이 1000원 단위여야합니다." }
     }
 
-    constructor(amount: String) : this(amount.toLongOrNull())
+    constructor(amount: String) : this(
+        amount.toLongOrNull() ?: throw NumberFormatException("[Error] 1000 단위의 숫자로 값을 입력해주세요.")
+    )
 
     fun calculateLottoCount(): Int {
         return (amount / LOTTO_PRICE).toInt()
