1cc5f4e1421334b5e3f8edce0549e88e19d680d0
hyunji1203 <hj27720@gmail.com>
2023-02-23 14:43:33 +0900

    refactor(RandomLottoGenerator): 스트림 함수 사용

--- src/main/kotlin/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/domain/RandomLottoGenerator.kt
@@ -6,13 +6,12 @@ class RandomLottoGenerator : LottoGenerator {
     }
 
     private fun generateLotto(): Lotto {
-        val randomNumbers = (LottoNumber.getMinNumber()..LottoNumber.getMaxNumber()).shuffled().subList(LOTTO_MIN_SIZE, LOTTO_LIMIT_SIZE).sorted()
+        val randomNumbers = (LottoNumber.getMinNumber()..LottoNumber.getMaxNumber()).shuffled().take(LOTTO_SIZE).sorted()
         val lotto = randomNumbers.map { LottoNumber.from(it) }
         return Lotto(lotto)
     }
 
     companion object {
-        const val LOTTO_LIMIT_SIZE = 6
-        const val LOTTO_MIN_SIZE = 0
+        const val LOTTO_SIZE = 6
     }
 }
