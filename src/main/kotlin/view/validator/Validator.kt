e99a577fe9339e3e7c2990ab4023dcb74e8bc18f
mendel <rhthrhrl0@naver.com>
2023-02-24 02:54:45 +0900

    feature(Validator): 스트링에서 정수 배열로 형변환이 가능한지 검증하고 된다면 정수 배열 반환하는 기능 추가

--- src/main/kotlin/view/validator/Validator.kt
+++ src/main/kotlin/view/validator/Validator.kt
@@ -8,5 +8,15 @@ object Validator {
         return number
     }
 
+    fun validateConvertToIntList(input: String, delimiter: String): List<Int>? {
+        val numbers = input.split(delimiter).map { it.trim().toIntOrNull() }
+        if (numbers.contains(null)) {
+            println(ERROR_CONVERT_INT_LIST.format(delimiter))
+            return null
+        }
+        return numbers.map { it!! }
+    }
+
     private const val ERROR_CONVERT_INT = "[ERROR] 숫자로 다시 입력해주세요."
+    private const val ERROR_CONVERT_INT_LIST = "[ERROR] %s로 숫자를 구분해서 다시 입력해주세요."
 }
--- src/test/kotlin/view/validator/ValidatorTest.kt
+++ src/test/kotlin/view/validator/ValidatorTest.kt
@@ -17,4 +17,16 @@ class ValidatorTest {
         val expected = null
         assertThat(result).isEqualTo(expected)
     }
+
+    @Test
+    fun `스트링에서 정수 배열로 형변환이 가능한지 검증하고 된다면 정수 배열 반환`() {
+        val result = Validator.validateConvertToIntList("1,2,3", ",")
+        assertThat(result).containsExactly(1, 2, 3)
+    }
+
+    @Test
+    fun `스트링에서 정수 배열로 형변환이 안된다면 null 반환`() {
+        val result = Validator.validateConvertToIntList("1,a,3", ",")
+        assertThat(result).isEqualTo(null)
+    }
 }
