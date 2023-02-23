d41299e1ac86e6ebc463a5526abc0e2559c4e66d
Sujin Kim <sujinkim919@gmail.com>
2023-02-23 15:53:49 +0900

    refactor: UserLottoCount 생성자와 프로퍼티 이름 수정

--- src/main/kotlin/lotto/model/UserLottoCount.kt
+++ src/main/kotlin/lotto/model/UserLottoCount.kt
@@ -4,15 +4,15 @@ import lotto.view.ERROR_NOT_ENOUGH_MONEY
 
 class UserLottoCount(
     money: Int,
-    val passive: Int
+    val manual: Int
 ) {
     val all: Int
     val auto: Int
-        get() = all - passive
+        get() = all - manual
 
     init {
         all = money / LOTTO_PRICE
-        require(all >= passive) { ERROR_NOT_ENOUGH_MONEY }
+        require(all >= manual) { ERROR_NOT_ENOUGH_MONEY }
     }
 
     companion object {
