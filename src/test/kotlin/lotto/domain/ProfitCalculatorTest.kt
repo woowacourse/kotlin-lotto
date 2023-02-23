f5d402c8cf6b5ca265c8321e3459ef8083947fb6
Gloria <ippnsj@gmail.com>
2023-02-23 16:01:12 +0900

    refactor: MethodSource 대신 CsvSource 사용

--- src/test/kotlin/lotto/domain/ProfitCalculatorTest.kt
+++ src/test/kotlin/lotto/domain/ProfitCalculatorTest.kt
@@ -2,65 +2,38 @@ package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class ProfitCalculatorTest {
-    @ParameterizedTest(name = "{0}")
-    @MethodSource("calculateProfit")
-    fun `수익률을 계산한다`(testName: String, amount: Int, numbers: Map<Rank, Int>, expected: Double) {
+    @ParameterizedTest(name = "구입 금액이 {0}원이고 1등 {1}명, 2등 {2}명, 3등 {3}명, 4등 {4}명, 5등 {5}명이면 수익률은 {7}이다")
+    @CsvSource(
+        "14000, 0, 0, 0, 0, 1, 5, 0.35",
+        "14000, 1, 0, 0, 0, 0, 4, 142857.14",
+        "1000, 0, 0, 0, 0, 1, 2, 5.00"
+    )
+    fun `수익률을 계산한다`(
+        amount: Int,
+        first: Int,
+        second: Int,
+        third: Int,
+        fourth: Int,
+        fifth: Int,
+        miss: Int,
+        expected: Double
+    ) {
         val money = PurchaseAmount(amount)
+        val numbers = mapOf(
+            Rank.FIRST to first,
+            Rank.SECOND to second,
+            Rank.THIRD to third,
+            Rank.FOURTH to fourth,
+            Rank.FIFTH to fifth,
+            Rank.MISS to miss
+        )
         val counter = RankCounter(numbers)
 
         val actual = ProfitCalculator.calculate(money, counter.calculateTotalPrize())
 
         assertThat(actual).isEqualTo(expected)
-    }
-
-    companion object {
-        @JvmStatic
-        fun calculateProfit(): Array<Arguments> {
-            return arrayOf(
-                Arguments.of(
-                    "구입 금액이 14000원이고 5등이면 수익률은 0.35다",
-                    14000,
-                    mapOf(
-                        Rank.FIRST to 0,
-                        Rank.SECOND to 0,
-                        Rank.THIRD to 0,
-                        Rank.FOURTH to 0,
-                        Rank.FIFTH to 1,
-                        Rank.MISS to 5
-                    ),
-                    0.35
-                ),
-                Arguments.of(
-                    "구입 금액이 14000원이고 1등이면 수익률은 142857.14다",
-                    14000,
-                    mapOf(
-                        Rank.FIRST to 1,
-                        Rank.SECOND to 0,
-                        Rank.THIRD to 0,
-                        Rank.FOURTH to 0,
-                        Rank.FIFTH to 0,
-                        Rank.MISS to 4
-                    ),
-                    142857.14
-                ),
-                Arguments.of(
-                    "구입 금액이 1000원이고 5등이면 수익률은 5.00이다",
-                    1000,
-                    mapOf(
-                        Rank.FIRST to 0,
-                        Rank.SECOND to 0,
-                        Rank.THIRD to 0,
-                        Rank.FOURTH to 0,
-                        Rank.FIFTH to 1,
-                        Rank.MISS to 2
-                    ),
-                    5.00
-                )
-            )
-        }
     }
 }
