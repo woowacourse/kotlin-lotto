b5aee42c17e52def6018e9074cc632f9cc579ddf
hyunji1203 <hj27720@gmail.com>
2023-02-23 15:33:02 +0900

    refactor(Lottos): 함수 분리

--- src/main/kotlin/domain/Lottos.kt
+++ src/main/kotlin/domain/Lottos.kt
@@ -5,11 +5,14 @@ class Lottos(val lottos: List<Lotto>) : List<Lotto> by lottos {
         val winningResult =
             mutableListOf(Rank.FIRST, Rank.SECOND, Rank.THIRD, Rank.FOURTH, Rank.FIFTH, Rank.MISS).associateWith { 0 }
                 .toMutableMap()
+        return WinningResult(matchEachLotto(winningNumber, winningResult).toMap())
+    }
+
+    fun matchEachLotto(winningNumber: WinningNumber, winningResult: MutableMap<Rank, Int>): MutableMap<Rank, Int> {
         lottos.forEach { lotto ->
             val rank = lotto.matchLotto(winningNumber.lotto, winningNumber.bonusNumber)
-            println(rank)
             if (rank != Rank.MISS) winningResult[rank!!] = winningResult.getOrDefault(rank, 0) + 1
         }
-        return WinningResult(winningResult.toMap())
+        return winningResult
     }
 }
