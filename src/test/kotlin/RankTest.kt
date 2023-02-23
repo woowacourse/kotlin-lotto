37772e677fd0923b7e8f910f18dfbf61f40280ec
hyunji1203 <hj27720@gmail.com>
2023-02-23 15:50:29 +0900

    refactor(RankTest): 테스트 케이스 추가

--- src/test/kotlin/RankTest.kt
+++ src/test/kotlin/RankTest.kt
@@ -13,6 +13,24 @@ class RankTest {
     }
 
     @Test
+    fun `3개의 숫자가 맞으면 5등이다`() {
+        // given
+        val rank = Rank.valueOf(3, false)
+
+        // then
+        assertThat(rank).isEqualTo(Rank.FIFTH) // 5등
+    }
+
+    @Test
+    fun `4개의 숫자가 맞으면 4등이다`() {
+        // given
+        val rank = Rank.valueOf(4, false)
+
+        // then
+        assertThat(rank).isEqualTo(Rank.FOURTH) // 4등
+    }
+
+    @Test
     fun `5개의 숫자가 맞고 보너스번호가 맞지 않으면 3등이다`() {
         // given
         val rank = Rank.valueOf(5, false)
@@ -28,5 +46,14 @@ class RankTest {
 
         // then
         assertThat(rank).isEqualTo(Rank.SECOND) // 2등
+    }
+
+    @Test
+    fun `6개의 숫자가 맞으면 1등이다`() {
+        // given
+        val rank = Rank.valueOf(6, false)
+
+        // then
+        assertThat(rank).isEqualTo(Rank.FIRST) // 1등
     }
 }
