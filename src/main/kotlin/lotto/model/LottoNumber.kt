b2d215a9939ee99e4364fdbd679f8ee4c2088bc2
Sujin Kim <sujinkim919@gmail.com>
2023-02-23 16:36:00 +0900

    test: 범위를 벗어난 로또 숫자에 대한 오류 테스트 추가

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -10,7 +10,7 @@ value class LottoNumber private constructor(private val number: Int) {
     companion object {
         private const val START_LOTTO_RANGE: Int = 1
         private const val END_LOTTO_RANGE: Int = 45
-        val RANGE_LOTTO_NUMBER: IntRange = START_LOTTO_RANGE..END_LOTTO_RANGE
+        private val RANGE_LOTTO_NUMBER: IntRange = START_LOTTO_RANGE..END_LOTTO_RANGE
 
         private val NUMBERS: Map<Int, LottoNumber> = RANGE_LOTTO_NUMBER.associateWith(::LottoNumber)
 
