48af8b9b2b22f82edd582955b836809ff20d1e65
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-02-23 17:06:32 +0900

    feat: 로또 개수 출력하는 기능 추가

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,7 +1,6 @@
 package view
 
 import domain.Lotto
-import domain.Money
 import domain.Rank
 import domain.WinningResult
 
@@ -10,10 +9,9 @@ class OutputView {
         println(OUTPUT_MONEY_MESSAGE)
     }
 
-    fun outputLottoSizeMessage(money: Money) {
-        println((money.getLottoCount()).toString() + OUTPUT_LOTTO_SIZE_MESSAGE)
+    fun outputLottoSizeMessage(manualLottoCount: Int, autoLottoCount: Int) {
+        println(OUTPUT_LOTTO_SIZE_MESSAGE.format(manualLottoCount, autoLottoCount))
     }
-
     fun outputLottos(lottos: List<Lotto>) {
         println(StringBuilder().getLottos(lottos))
     }
@@ -30,7 +28,7 @@ class OutputView {
     }
 
     private fun outputLotto(lotto: Lotto): String {
-        return lotto.numbers.map { it.number }
+        return lotto.map { it.number }
             .joinToString(prefix = PREFIX_MARK, separator = SEPERATOR_MARK, postfix = POSTFIX_MARK)
     }
 
@@ -40,6 +38,13 @@ class OutputView {
 
     fun outputBonusNumberMessage() {
         println(OUTPUT_BONUS_NUMBER_MESSAGE)
+    }
+
+    fun outputManualLottoCountMessage() {
+        println(OUTPUT_MANUAL_LOTTO_COUNT_MESSAGE)
+    }
+    fun outputManualLottosMessage() {
+        println(OUTPUT_MANUAL_LOTTOS_MESSAGE)
     }
 
     fun outputWinningResult(winningResult: WinningResult) {
@@ -64,12 +69,14 @@ class OutputView {
 
     companion object {
         const val OUTPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요."
-        const val OUTPUT_LOTTO_SIZE_MESSAGE = "개를 구매했습니다."
+        const val OUTPUT_LOTTO_SIZE_MESSAGE = "수동으로 %d장, 자동으로 %d개를 구매했습니다."
         const val PREFIX_MARK = "["
         const val SEPERATOR_MARK = ", "
         const val POSTFIX_MARK = "]"
         const val OUTPUT_WINNING_LOTTO_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
         const val OUTPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
+        const val OUTPUT_MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요."
+        const val OUTPUT_MANUAL_LOTTOS_MESSAGE = "수동으로 구매할 번호를 입력해 주세요."
         const val OUTPUT_WINNING_RESULT_MESSAGE = "당첨 통계"
         const val SEPERATE_MESSAGE = "---------"
         const val YIELD_MESSAGE = "총 수익률은 %.2f입니다."
