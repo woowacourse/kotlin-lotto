6647802eefd30b898eeb411c17564f8d8007cc5f
hyemdooly <hyemdooly@gmail.com>
2023-02-23 18:12:01 +0900

    feat(LottoStore): amount가 아닌 count를 사용하도록 수정

--- src/main/kotlin/domain/LottoStore.kt
+++ src/main/kotlin/domain/LottoStore.kt
@@ -1,9 +1,9 @@
 package domain
 
 class LottoStore {
-    fun buyAutoLotto(amount: Amount): List<Lotto> {
+    fun buyAutoLotto(count: Count): List<Lotto> {
         val lottoFactory = LottoFactory(RandomNumberGenerator())
-        return lottoFactory.create(getCount(amount))
+        return lottoFactory.create(count)
     }
 
     fun buyManualLotto(lottos: List<String>): List<Lotto> {
@@ -11,8 +11,6 @@ class LottoStore {
             Lotto.create(it.split(",").map { it.toInt() })
         }
     }
-
-    private fun getCount(amount: Amount): Int = (amount.amount / LOTTO_PRICE).toInt()
 
     companion object {
         const val LOTTO_PRICE = 1000
