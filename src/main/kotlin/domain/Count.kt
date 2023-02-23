236d3dedef241affe64b666be3ef86bd693e3511
hyemdooly <hyemdooly@gmail.com>
2023-02-23 18:09:42 +0900

    refactor(Count): 생성자 변경, 유효값 검증 이동, minusToInt 구현

--- src/main/kotlin/domain/Count.kt
+++ src/main/kotlin/domain/Count.kt
@@ -1,17 +1,15 @@
 package domain
 
-import domain.LottoStore.Companion.LOTTO_PRICE
-
-data class Count private constructor(val count: Int) {
-    constructor(count: Int, amount: Amount) : this(count) {
+data class Count(private val count: Int) {
+    init {
         require(count >= 0) { ERROR_MINUS.format(count) }
-        require((amount.amount / LOTTO_PRICE) >= count) { ERROR_NUMBER_RANGE.format(count) }
     }
 
     fun toInt() = count
 
+    fun minusToInt(number: Int): Int = number - count
+
     companion object {
         private const val ERROR_MINUS = "0 이상의 숫자를 입력해야 합니다.\n잘못된 값 : %d"
-        private const val ERROR_NUMBER_RANGE = "구입 가능한 로또 장수를 초과합니다.\n잘못된 값 : %d"
     }
 }
