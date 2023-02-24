6d252336d35e7b938299e2a59a91eb2c744bfb45
hyunji1203 <hj27720@gmail.com>
2023-02-25 01:11:51 +0900

    refactor: 함수 단순화

--- src/main/kotlin/domain/Lottos.kt
+++ src/main/kotlin/domain/Lottos.kt
@@ -2,17 +2,10 @@ package domain
 
 class Lottos(val lottos: List<Lotto>) : List<Lotto> by lottos {
     fun matchLottos(winningNumber: WinningNumber): WinningResult {
-        val winningResult =
-            mutableListOf(Rank.FIRST, Rank.SECOND, Rank.THIRD, Rank.FOURTH, Rank.FIFTH, Rank.MISS).associateWith { 0 }
-                .toMutableMap()
-        return WinningResult(matchEachLotto(winningNumber, winningResult).toMap())
-    }
-
-    fun matchEachLotto(winningNumber: WinningNumber, winningResult: MutableMap<Rank, Int>): MutableMap<Rank, Int> {
-        lottos.forEach { lotto ->
-            val rank = lotto.matchLotto(winningNumber.lotto, winningNumber.bonusNumber)
-            if (rank != Rank.MISS) winningResult[rank!!] = winningResult.getOrDefault(rank, 0) + 1
+        val winningResult = Rank.values().associateWith {
+            rank ->
+            lottos.count { rank == it.matchLotto(winningNumber.lotto, winningNumber.bonusNumber) }
         }
-        return winningResult
+        return WinningResult(winningResult)
     }
 }
