70b61df4156b09bade019e7f71195e439fffab51
changhwan <chang5hustle@gmail.com>
2023-02-24 18:20:02 +0900

    feat(lotto): 수동 구입 로또 개수 입력 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,12 @@
 package lotto.view
 
 object InputView {
+    private const val ERROR_NOT_NUMBER = "숫자 이외의 입력이 들어갔거나 올바르지 않은 입력입니다."
     private const val ERROR_WRONG_INPUT = "입력값으로 Null 이 입력되었습니다."
     private const val GET_PURCHASE_MONEY_SCRIPT = "구입금액을 입력해 주세요."
     private const val GET_MAIN_LOTTO_NUMBERS_SCRIPT = "지난 주 당첨 번호를 입력해 주세요."
     private const val GET_BONUS_LOTTO_NUMBER_SCRIPT = "보너스 볼을 입력해 주세요."
-    private const val ERROR_NOT_NUMBER = "숫자 이외의 입력이 들어갔거나 올바르지 않은 입력입니다."
+    private const val GET_MANUAL_PURCHASE_COUNT_SCRIPT = "수동으로 구매할 로또 수를 입력해 주세요."
 
     fun getPurchaseMoney(): Int {
         return runCatching {
@@ -31,6 +32,16 @@ object InputView {
     fun getBonusLottoNumber(): Int {
         return runCatching {
             println(GET_BONUS_LOTTO_NUMBER_SCRIPT)
+            readlnOrNull()?.trim()?.toInt() ?: throw IllegalStateException(ERROR_WRONG_INPUT)
+        }.getOrElse {
+            println(ERROR_NOT_NUMBER)
+            getBonusLottoNumber()
+        }
+    }
+
+    fun getManualPurchaseCount(): Int {
+        return runCatching {
+            println(GET_MANUAL_PURCHASE_COUNT_SCRIPT)
             readlnOrNull()?.trim()?.toInt() ?: throw IllegalStateException(ERROR_WRONG_INPUT)
         }.getOrElse {
             println(ERROR_NOT_NUMBER)
