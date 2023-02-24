a302257f049ddf8cb9320c3c99334145b86d7f66
changhwan <chang5hustle@gmail.com>
2023-02-24 16:53:48 +0900

    refactor(lotto): WinningResult getPrizeMoney관련 오류 메시지 추가

--- src/main/kotlin/lotto/domain/WinningResult.kt
+++ src/main/kotlin/lotto/domain/WinningResult.kt
@@ -6,9 +6,11 @@ class WinningResult private constructor(val rankTable: Map<Rank, Int>) {
         rankTable.keys.fold(0) { acc, rank -> acc + getPrizeMoney(rank) }
 
     private fun getPrizeMoney(rank: Rank): Int =
-        rank.prizeMoney * (rankTable[rank] ?: throw IllegalStateException())
+        rank.prizeMoney * (rankTable[rank] ?: throw IllegalStateException(INAPPROPRIATE_RANK_ERROR))
 
     companion object {
+        private const val INAPPROPRIATE_RANK_ERROR = "올바르지 않은 rank 값이 포함되었습니다."
+
         fun from(ranks: List<Rank>): WinningResult {
             val value = Rank.values().map { rank -> rank to ranks.count { it == rank } }
 
