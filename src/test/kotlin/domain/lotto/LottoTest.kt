79dd0ca469364e48b006c086554d12de3d225d5d
briandr97 <briandr@naver.com>
2023-02-24 23:59:07 +0900

    refactor(LottoTest): parameterizedTest로 변경

--- src/test/kotlin/domain/lotto/LottoTest.kt
+++ src/test/kotlin/domain/lotto/LottoTest.kt
@@ -1,31 +1,30 @@
 package domain.lotto
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotest.matchers.throwable.shouldHaveMessage
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import util.PREFIX
 
 class LottoTest {
-    @Test
-    fun `로또 번호가 6개가 아니라면 에러 발생 (5개일때)`() {
+    @MethodSource("provideNumbers")
+    @ParameterizedTest
+    fun `로또 번호가 6개가 아니라면 에러 발생 (5개일때)`(numbers: List<Int>) {
         // given
 
         // when
-        val exception = runCatching { Lotto(1, 2, 3, 4, 5) }.exceptionOrNull()
 
         // then
-        assertThat(exception).isInstanceOf(IllegalArgumentException::class.java)
-        assertThat(exception).hasMessageContaining("$PREFIX 로또번호는 6개여야합니다.")
+        assertThrows<IllegalArgumentException> { Lotto(numbers) }
+            .shouldHaveMessage("$PREFIX 로또번호는 6개여야합니다.")
     }
 
-    @Test
-    fun `로또 번호가 6개가 아니라면 에러 발생 (7개일때)`() {
-        // given
-
-        // when
-        val exception = runCatching { Lotto(1, 2, 3, 4, 5, 6, 7) }.exceptionOrNull()
-
-        // then
-        assertThat(exception).isInstanceOf(IllegalArgumentException::class.java)
-        assertThat(exception).hasMessageContaining("$PREFIX 로또번호는 6개여야합니다.")
+    companion object {
+        @JvmStatic
+        fun provideNumbers(): List<Arguments> = listOf(
+            Arguments.of(listOf(1, 2, 3, 4, 5)),
+            Arguments.of(listOf(1, 2, 3, 4, 5, 6, 7)),
+        )
     }
 }
