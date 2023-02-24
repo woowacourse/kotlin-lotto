3b17fb299f86d71451d825ecccc3a267b48efa16
ki960213 <ki960213@github.com>
2023-02-24 13:34:04 +0900

    refactor(Money): Money 클래스를 data class에서 value class로 변경

--- src/main/kotlin/domain/Money.kt
+++ src/main/kotlin/domain/Money.kt
@@ -1,6 +1,7 @@
 package domain
 
-data class Money(val amount: Int) : Comparable<Money> {
+@JvmInline
+value class Money(private val amount: Int) : Comparable<Money> {
 
     init {
         require(amount in MINIMUM_AMOUNT..MAXIMUM_AMOUNT) { ERROR_MONEY_AMOUNT }
@@ -14,10 +15,6 @@ data class Money(val amount: Int) : Comparable<Money> {
     companion object {
         private const val MINIMUM_AMOUNT = 0
         private const val MAXIMUM_AMOUNT = 100000
-        private const val MINIMUM_AMOUNT_LENGTH = 1
-        private const val MAXIMUM_AMOUNT_LENGTH = 6
         private const val ERROR_MONEY_AMOUNT = "돈은 ${MINIMUM_AMOUNT}원 이상 ${MAXIMUM_AMOUNT}원 이하의 값으로만 존재할 수 있습니다."
-        private const val ERROR_MONEY_STRING =
-            "돈을 문자열로 생성할 때 모든 문자는 숫자이고 길이는 $MINIMUM_AMOUNT_LENGTH 이상 $MAXIMUM_AMOUNT_LENGTH 이하여야 합니다."
     }
 }
