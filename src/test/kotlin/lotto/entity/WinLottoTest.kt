b05bfe5fab6b71fc64ffc65fe2f2a4687f9f934c
Doyeop Kim <dygames@naver.com>
2023-02-24 18:22:37 +0900

    refactor: 등수 검사 테스트에서 CsvSource를 사용하도록 변경

--- src/test/kotlin/lotto/entity/WinLottoTest.kt
+++ src/test/kotlin/lotto/entity/WinLottoTest.kt
@@ -5,9 +5,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import org.junit.jupiter.params.provider.CsvSource
 
 class WinLottoTest {
     @Test
@@ -19,39 +17,32 @@ class WinLottoTest {
         // when
         val thrown = assertThrows<IllegalArgumentException> { WinLotto(winNumber, bonus) }
         val except = "보너스 번호와 당첨 번호는 중복될 수 없습니다. 입력된 당첨 번호 : %s, 보너스 번호 : %d".format(
-            winNumber.toString(),
-            bonus.value
+            winNumber.toString(), bonus.value
         )
 
         // then
         assertThat(thrown.message).isEqualTo(except)
     }
 
-    @MethodSource("makeWinStatisticsSource")
     @ParameterizedTest(name = "당첨 번호 : {0}, 결과 : {1}")
-    fun `로또 번호를 비교하여 등수를 검사`(winLotto: WinLotto, exceptRank: Map<Rank, Int>) {
+    @CsvSource(
+        value = ["1,2,3,4,5,6:45:0", "1,2,3,4,5,7:6:1", "1,2,3,4,5,7:45:2", "1,2,3,4,7,8:45:3", "1,2,3,7,8,9:45:4", "1,2,7,8,9,10:45:5"],
+        delimiter = ':'
+    )
+    fun `로또 번호를 비교하여 등수를 검사`(winNumber: String, bonus: Int, exceptRank: Int) {
         // given
+        val splittedWinNumber = winNumber.split(',')
+        val instantiatedWinNumber = Lotto(splittedWinNumber.map { it.toInt() })
+        val instantiatedBonus = LottoNumber.from(bonus)
+        val winLotto = WinLotto(instantiatedWinNumber, instantiatedBonus)
         val lottos = listOf(Lotto(listOf(1, 2, 3, 4, 5, 6)))
         val purchasedLottos = PurchasedLottos(lottos)
+        val except = mapOf(Rank.values()[exceptRank] to 1)
 
         // when
         val rank = winLotto.makeWinStatistics(purchasedLottos).value
 
         // then
-        assertThat(rank).isEqualTo(exceptRank)
-    }
-
-    companion object {
-        @JvmStatic
-        fun makeWinStatisticsSource(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), LottoNumber.from(45)), mapOf(Rank.FIRST to 1)),
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 3, 4, 5, 7)), LottoNumber.from(6)), mapOf(Rank.SECOND to 1)),
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 3, 4, 5, 7)), LottoNumber.from(45)), mapOf(Rank.THIRD to 1)),
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 3, 4, 7, 8)), LottoNumber.from(45)), mapOf(Rank.FOURTH to 1)),
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 3, 7, 8, 9)), LottoNumber.from(45)), mapOf(Rank.FIFTH to 1)),
-                Arguments.arguments(WinLotto(Lotto(listOf(1, 2, 7, 8, 9, 10)), LottoNumber.from(45)), mapOf(Rank.MISS to 1))
-            )
-        }
+        assertThat(rank).isEqualTo(except)
     }
 }
