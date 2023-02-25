4652695c6781e2ba8992ebda7476bb4a28fca9c1
inseonyun <yuninseon@naver.com>
2023-02-25 17:59:34 +0900

    refactor(WinStatistics): 우승 상금 합계 함수 구현

--- src/main/kotlin/lotto/entity/WinStatistics.kt
+++ src/main/kotlin/lotto/entity/WinStatistics.kt
@@ -2,4 +2,6 @@ package lotto.entity
 
 import lotto.model.Rank
 
-class WinStatistics(val value: List<Rank>)
+class WinStatistics(val value: List<Rank>) {
+    fun sumWiningMoney(): Long = value.sumOf { it.winningMoney }
+}
--- src/main/kotlin/lotto/model/LottoProfitRateCalculator.kt
+++ src/main/kotlin/lotto/model/LottoProfitRateCalculator.kt
@@ -6,7 +6,7 @@ import lotto.entity.WinStatistics
 
 class LottoProfitRateCalculator(private val purchaseMoney: PurchaseMoney, private val winStatistics: WinStatistics) {
     fun calculate(): ProfitRate {
-        val winMoney = winStatistics.value.sumOf { it.winningMoney }
+        val winMoney = winStatistics.sumWiningMoney()
         return ProfitRate(winMoney / purchaseMoney.value.toFloat())
     }
 }
