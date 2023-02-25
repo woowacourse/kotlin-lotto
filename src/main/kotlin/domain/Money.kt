a3e9856ccd2893aa8161931e49de7c64cd5d34a8
krrong <khk9664@naver.com>
2023-02-25 16:55:00 +0900

    refactor(Money): 로또 한 장의 가격을 Money가 가지도록 수정

--- src/main/kotlin/domain/Money.kt
+++ src/main/kotlin/domain/Money.kt
@@ -3,14 +3,15 @@ package domain
 @JvmInline
 value class Money(val money: Int) {
     init {
-        require(money % 1000 == 0 && money != 0) { ERROR_MINIMUM_MONEY }
+        require(money % EACH_LOTTO_PRICE == 0 && money != 0) { ERROR_MINIMUM_MONEY }
     }
 
-    fun divide(divisor: Int): Int {
-        return money / divisor
+    fun makeCount(): Int {
+        return money / EACH_LOTTO_PRICE
     }
 
     companion object {
         const val ERROR_MINIMUM_MONEY = "금액을 1000원 단위로 입력해주세요."
+        private const val EACH_LOTTO_PRICE = 1000
     }
 }
