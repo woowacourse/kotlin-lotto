f134b5ba840adf467f4a14451f4d3c906a3773b4
Gloria <ippnsj@gmail.com>
2023-02-26 15:12:36 +0900

    refactor: 로또 개수 음수 아닌지 확인하는 기능 이동

--- src/main/kotlin/lotto/domain/Receipt.kt
+++ src/main/kotlin/lotto/domain/Receipt.kt
@@ -1,25 +1,21 @@
 package lotto.domain
 
 class Receipt(val purchase: PurchaseAmount, val manual: TicketCount) {
-    val auto = TicketCount(purchase.count - manual.count)
+    var auto: TicketCount
 
     init {
         checkCount()
+        auto = TicketCount(purchase.count - manual.count)
     }
 
     private fun checkCount() {
-        require(manual.count >= 0) {
-            "${MANUAL_QUANTITY_ERROR_MESSAGE}\n" +
-                "오류값 : ${manual.count}"
-        }
         require(manual.count <= purchase.count) {
-            "${MANUAL_QUANTITY_OVERFLOW_ERROR_MESSAGE}\n" +
+            "${MANUAL_COUNT_OVERFLOW_ERROR_MESSAGE}\n" +
                 "오류값 : ${manual.count}"
         }
     }
 
     companion object {
-        private const val MANUAL_QUANTITY_ERROR_MESSAGE = "수동 로또 개수는 음수일 수 없습니다."
-        private const val MANUAL_QUANTITY_OVERFLOW_ERROR_MESSAGE = "구입금액보다 수동 로또 개수가 더 많습니다."
+        private const val MANUAL_COUNT_OVERFLOW_ERROR_MESSAGE = "구입금액보다 수동 로또 개수가 더 많습니다."
     }
 }
--- src/main/kotlin/lotto/domain/TicketCount.kt
+++ src/main/kotlin/lotto/domain/TicketCount.kt
@@ -1,3 +1,18 @@
 package lotto.domain
 
-class TicketCount(val count: Int)
+class TicketCount(val count: Int) {
+    init {
+        checkCount()
+    }
+
+    private fun checkCount() {
+        require(count >= 0) {
+            "${COUNT_ERROR_MESSAGE}\n" +
+                "오류값 : $count"
+        }
+    }
+
+    companion object {
+        private const val COUNT_ERROR_MESSAGE = "로또 개수는 음수일 수 없습니다."
+    }
+}
