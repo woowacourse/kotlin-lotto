5b6331ab80f120fea700395e80b735489f36edd5
inseonyun <yuninseon@naver.com>
2023-02-26 18:08:44 +0900

    refactor(PurchaseMoney): 수동 로또 구매 수 입력 검증 로직 추가

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,7 +3,6 @@ package lotto.view
 import lotto.entity.Lotto
 import lotto.entity.LottoCount
 import lotto.entity.LottoNumber
-import lotto.entity.LottoPrice
 import lotto.entity.Lottos
 import lotto.entity.PurchaseMoney
 import lotto.entity.WinLotto
@@ -36,16 +35,14 @@ class InputView {
     fun getManualLottoCount(purchaseMoney: PurchaseMoney): LottoCount {
         return tryAndRerun {
             println(MESSAGE_INPUT_MANUAL_LOTTO_COUNT)
-            LottoCount(readManualLottoCount(purchaseMoney.value))
+            LottoCount(readManualLottoCount(purchaseMoney))
         } as LottoCount
     }
 
-    private fun readManualLottoCount(purchaseMoney: Int): Int {
+    private fun readManualLottoCount(purchaseMoney: PurchaseMoney): Int {
         val input = readln()
         require(input.toIntOrNull() != null) { ERROR_MESSAGE_ONLY_NUMBER }
-        if (input.toInt() != 0)
-            require((purchaseMoney / (input.toInt() * LottoPrice.DEFAULT_LOTTO_PRICE) > 0)) { ERROR_MESSAGE_AVAILABLE_COUNT }
-        return input.toInt()
+        return purchaseMoney.checkManualLottoCount(input.toInt())
     }
 
     fun getManualLottos(manualLottoCount: LottoCount): Lottos {
@@ -87,7 +84,6 @@ class InputView {
         private const val MESSAGE_WIN_NUMBER = "지난 주 당첨 번호를 입력해 주세요."
         private const val MESSAGE_BONUS = "보너스 볼을 입력해 주세요."
         private const val ERROR_MESSAGE_ONLY_NUMBER = "숫자로만 이루어져야 합니다"
-        private const val ERROR_MESSAGE_AVAILABLE_COUNT = "구매 가능한 개수여야 합니다"
         private const val ERROR_MESSAGE_SPLIT_BY_COMMA = "숫자는 콤마로 구별되어야 합니다"
         private const val ERROR_MESSAGE_SPLIT_ONLY_NUMBER = "구분된 입력은 숫자로만 이루어져야 합니다"
     }
