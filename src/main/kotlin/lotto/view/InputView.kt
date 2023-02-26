7b5ca2c46141dd9ea089ead2d949d21fde80f7e6
whk06061 <gju06061@naver.com>
2023-02-26 21:45:05 +0900

    refactor: 틀린 형식 입력시 에러 메시지 출력 기능을 추가하고 재입력 로직을 수정한다.

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,26 +1,60 @@
 package lotto.view
 
 object InputView {
+    private const val INPUT_NOT_INT_ERROR = "숫자를 입력해주세요."
+    private const val INPUT_SPLIT_ERROR = "콤마로 구분된 숫자를 입력해주세요."
+
     fun readInputMoney(): Int {
-        return readln().toIntOrNull() ?: readInputMoney()
+        var input: String
+        var pass = true
+        do {
+            input = readln()
+            printErrorMessageIfNotPass(pass, INPUT_NOT_INT_ERROR)
+            pass = input.toIntOrNull() != null
+        } while (!pass)
+        return input.toInt()
     }
 
     fun readInputLottoNumber(): List<Int> {
-        val input = readln().split(",").map {
-            it.trim()
-        }
-        val lottoNumbers = input.map {
-            it.toIntOrNull()
-        }
-        if (lottoNumbers.contains(null)) readInputLottoNumber()
+        var input: List<String>
+        var lottoNumbers: List<Int?>
+        var pass = true
+        do {
+            printErrorMessageIfNotPass(pass, INPUT_SPLIT_ERROR)
+            input = readln().split(",").map {
+                it.trim()
+            }
+            lottoNumbers = input.map {
+                it.toIntOrNull()
+            }
+            pass = !lottoNumbers.contains(null)
+        } while (!pass)
         return input.map { it.toInt() }
     }
 
     fun readInputBonusNumber(): Int {
-        return readln().toIntOrNull() ?: readInputBonusNumber()
+        var input: String
+        var pass = true
+        do {
+            input = readln()
+            printErrorMessageIfNotPass(pass, INPUT_NOT_INT_ERROR)
+            pass = input.toIntOrNull() != null
+        } while (!pass)
+        return input.toInt()
     }
 
     fun readInputManualLottoCount(): Int {
-        return readln().toIntOrNull() ?: readInputManualLottoCount()
+        var input: String
+        var pass = true
+        do {
+            input = readln()
+            printErrorMessageIfNotPass(pass, INPUT_NOT_INT_ERROR)
+            pass = input.toIntOrNull() != null
+        } while (!pass)
+        return input.toInt()
+    }
+
+    private fun printErrorMessageIfNotPass(pass: Boolean, message: String) {
+        if (!pass) OutputView.printErrorMessage(message)
     }
 }
