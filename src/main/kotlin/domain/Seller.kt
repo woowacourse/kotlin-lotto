bd4dde1074d1d84153e2a5dce45bbebd7a4b9ee5
s9hn <tnghqn@naver.com>
2023-02-27 02:53:17 +0900

    [REFACTOR(Seller)] 본 클래스에서 로또리스트를 반환하도록 수정

--- src/main/kotlin/domain/Seller.kt
+++ src/main/kotlin/domain/Seller.kt
@@ -3,9 +3,10 @@ package domain
 import model.Count
 import model.Payment
 
-class Seller(private val payment: Payment) {
+class Seller(private val payment: Payment, private val lottoMachine: LottoMachine) {
 
     fun getLottoCount(): Count = Count(payment.money / ONE_TICKET_PRICE)
+    fun getLotto() = lottoMachine.makeLotto(getLottoCount())
 
     companion object {
         private const val ONE_TICKET_PRICE = 1000
