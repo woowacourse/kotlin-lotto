9f01142e545498080924dccfbca1e3b2e9bed295
whk06061 <gju06061@naver.com>
2023-02-26 18:50:13 +0900

    refactor: LottoMoney 생성 조건을 변경한다.

--- src/main/kotlin/lotto/domain/model/LottoMoney.kt
+++ src/main/kotlin/lotto/domain/model/LottoMoney.kt
@@ -3,13 +3,11 @@ package lotto.domain.model
 class LottoMoney(val amount: Int) {
 
     init {
-        require(amount > 0) { MONEY_NEGATIVE_NUMBER_ERROR }
-        require(amount % MONEY_UNIT == 0) { MONEY_UNIT_ERROR }
+        require(amount >= MONEY_UNIT) { MONEY_MINIMUM_ERROR }
     }
 
     companion object {
-        private const val MONEY_NEGATIVE_NUMBER_ERROR = "금액은 양수여야 합니다."
-        private const val MONEY_UNIT_ERROR = "금액은 1000원 단위여야 합니다."
+        const val MONEY_MINIMUM_ERROR = "최소 금액은 1000원입니다."
         const val MONEY_UNIT = 1000
     }
 }
