b168972eaf365a226eb23790d86788911ea285b0
whk06061 <gju06061@naver.com>
2023-02-26 17:26:41 +0900

    refactor: 전역적으로 예외를 잡아주는 로직을 추가한다.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,6 +3,10 @@ package lotto
 import lotto.controller.Controller
 
 fun main() {
+    Thread.setDefaultUncaughtExceptionHandler { _, e ->
+        println(Controller.ERROR_PREFIX + e.message)
+        e.printStackTrace()
+    }
     val controller = Controller()
     controller.start()
 }
