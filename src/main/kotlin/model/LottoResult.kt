78569eca7aeda6d379a1cb4b8006d31c85568026
s9hn <tnghqn@naver.com>
2023-02-27 02:54:11 +0900

    [REFACTOR(LottoResult)] 해당 클래스가 생성될 때, 로또 결과를 초기화 하도록 변경

--- src/main/kotlin/model/LottoResult.kt
+++ src/main/kotlin/model/LottoResult.kt
@@ -2,12 +2,15 @@ package model
 
 import domain.Rank
 
-class LottoResult {
+class LottoResult(
+    private val bundleOfLotto: List<Lotto>,
+    private val winningNumber: WinningNumber,
+) {
     private val _result: MutableMap<Rank, Int> = mutableMapOf()
-    val result: Map<Rank, Int> get() = _result
+    val result: Map<Rank, Int> get() = _result.toMap()
 
-    fun updateRankCount(rank: Rank) {
-        _result[rank] = (_result[rank] ?: NULL) + 1
+    init {
+        readyLottoResult()
     }
 
     fun getProfitRate(): Double {
@@ -20,6 +23,24 @@ class LottoResult {
 
         return (profit / (countOfTicket * ONE_LOTTO_PRICE))
     }
+
+    private fun readyLottoResult() {
+        bundleOfLotto.forEach { lotto ->
+            val matchOfCount = compareLottoNumber(lotto, winningNumber.lotto)
+            val isMatchBonus = checkBonusNumber(lotto, winningNumber.bonusNumber)
+            val rank = Rank.valueOf(matchOfCount, isMatchBonus)
+
+            updateRankCount(rank)
+        }
+    }
+
+    private fun updateRankCount(rank: Rank) {
+        _result[rank] = (_result[rank] ?: NULL) + 1
+    }
+
+    private fun compareLottoNumber(lotto: Lotto, winningNumber: Lotto) = lotto.getMatchOfNumber(winningNumber)
+
+    private fun checkBonusNumber(lotto: Lotto, winningNumber: LottoNumber) = lotto.isMatchBonus(winningNumber)
 
     companion object {
         private const val NULL = 0
