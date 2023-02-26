ff2ea7a2b4b418a3d58df13746317525f9548eb2
whk06061 <gju06061@naver.com>
2023-02-26 18:52:09 +0900

    refactor: 수동 로또 개수 예외 메시지 출력 코드를 추가한다.

--- src/main/kotlin/lotto/domain/ManualLottoCountValidator.kt
+++ src/main/kotlin/lotto/domain/ManualLottoCountValidator.kt
@@ -1,11 +1,14 @@
 package lotto.domain
 
+import lotto.controller.Controller
+
 object ManualLottoCountValidator {
 
     private const val LOTTO_COUNT_NEGATIVE_ERROR = "구매할 수동 로또 금액이 투입 금액보다 큽니다."
 
     fun checkAvailable(manualLottoCount: Int, totalLottoCount: Int): Boolean {
         if (manualLottoCount <= totalLottoCount) return true
+        println(Controller.ERROR_PREFIX + LOTTO_COUNT_NEGATIVE_ERROR)
         return false
     }
 }
