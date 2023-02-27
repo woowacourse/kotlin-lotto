727ea2a627d545ab8363af2d3f8b294b0f67559c
SangHyun Moon <no1msh1217@gmail.com>
2023-02-27 14:23:46 +0900

    refactor: 부생성자 this() 구문 함수분리

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -7,13 +7,15 @@ class Lotto(val lottoNumbers: List<LottoNumber>) {
         require(lottoNumbers.size == 6) { "[Error] 로또번호는 서로 다른 숫자 6개여야합니다." }
     }
 
-    constructor(lottoNumber: String) : this(
-        lottoNumber.split(SEPARATOR)
-            .map { LottoNumber.of(it) }
-            .sortedBy { it.number }
-    )
+    constructor(lottoNumber: String) : this(convertLotto(lottoNumber))
 
     companion object {
         private const val SEPARATOR = ", "
+
+        private fun convertLotto(lottoNumber: String): List<LottoNumber> {
+            return lottoNumber.split(SEPARATOR)
+                .map { LottoNumber.of(it) }
+                .sortedBy { it.number }
+        }
     }
 }
