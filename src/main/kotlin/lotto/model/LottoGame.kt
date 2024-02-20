55ce22f6b25b03fe561298f75431bc176f6cd761
JunJangE <wnswkd486@gmail.com>
2024-02-20 16:53:40 +0900

    feat: LottoGame 클래스의 로또 생성 기능
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

--- src/main/kotlin/lotto/model/LottoGame.kt
+++ src/main/kotlin/lotto/model/LottoGame.kt
@@ -1,9 +1,19 @@
 package lotto.model
 
-class LottoGame(val lottos: List<Lotto>, val winningLotto: Lotto, val bonusNumber: String) {
+class LottoGame(lottos: List<Lotto> = emptyList(), val winningLotto: Lotto, val bonusNumber: String) {
+    var lottos: List<Lotto> = lottos
+        private set
+
     init {
         require(bonusNumber.toIntOrNull() in 1..45)
         require(bonusNumber !in winningLotto.numbers)
+    }
+
+    fun createLottos(count: Int) {
+        lottos =
+            List(count) {
+                Lotto((1..45).shuffled().take(6).sorted().map { it.toString() })
+            }
     }
 
     fun calculateResult(): Map<Rank, Int> {
