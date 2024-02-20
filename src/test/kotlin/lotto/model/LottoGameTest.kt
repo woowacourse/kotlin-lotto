2039d33eadd6440351771559672d7ffd6d00e71b
JunJangE <wnswkd486@gmail.com>
2024-02-20 15:54:29 +0900

    test: LottoGame calculateResult 테스트
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

--- src/test/kotlin/lotto/model/LottoGameTest.kt
+++ src/test/kotlin/lotto/model/LottoGameTest.kt
@@ -17,4 +17,17 @@ class LottoGameTest {
         }
     }
 
+    @Test
+    fun `5개의 당첨 번호가 일치하고 보너스 번호가 일치하지 않으면 3등이고, 일치하면 2등이다`() {
+        val numbers =
+            listOf(
+                Lotto(numbers = listOf("1", "2", "3", "4", "5", "6")),
+                Lotto(numbers = listOf("1", "2", "3", "4", "5", "8")),
+            )
+        val winningNumbers = Lotto(numbers = listOf("1", "2", "3", "4", "5", "7"))
+        val lottoGame = LottoGame(numbers, winningNumbers, "8")
+        val result = lottoGame.calculateResult()
+
+        assertThat(result).isEqualTo(mapOf(Rank.THIRD to 1, Rank.SECOND to 1))
+    }
 }
