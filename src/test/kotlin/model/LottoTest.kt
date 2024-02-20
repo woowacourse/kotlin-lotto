3b48826694e70ac154d35639de03765ca3db7c81
somin <salth0601@gmail.com>
2024-02-20 17:39:36 +0900

    test: 고유한 로또 번호에 대한 유효성 검사 테스트 추가
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/test/kotlin/model/LottoTest.kt
+++ src/test/kotlin/model/LottoTest.kt
@@ -1,5 +1,6 @@
 package model
 
+import model.Lotto.Companion.DUPLICATED_NUMBER_EXCEPTION_MESSAGE
 import model.Lotto.Companion.LOTTO_NUMBER_SIZE
 import model.Lotto.Companion.NUMBER_SIZE_EXCEPTION_MESSAGE
 import org.assertj.core.api.Assertions.assertThat
@@ -10,6 +11,12 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class LottoTest {
 
+    @Test
+    fun `로또 번호가 고유한 6개일 때 예외를 던지지 않는다`() {
+        val lottoNumbers = List(6) { LottoNumber(it + 1) }
+        assertThat(lottoNumbers.size).isEqualTo(LOTTO_NUMBER_SIZE)
+    }
+
     @ParameterizedTest
     @ValueSource(ints = [5, 7])
     fun `로또 번호가 6개가 아닐 때 예외를 던진다`(size: Int) {
@@ -19,8 +26,9 @@ class LottoTest {
     }
 
     @Test
-    fun `로또 번호가 6개일 때 예외를 던지지 않는다`() {
-        val lottoNumbers = List(6) { LottoNumber(it + 1) }
-        assertThat(lottoNumbers.size).isEqualTo(LOTTO_NUMBER_SIZE)
+    fun `로또 번호가 서로 중복될 때 예외를 던진다`() {
+        val lottoNumbers = List(6) { LottoNumber(1) }
+        val exception = assertThrows<IllegalArgumentException> { Lotto(lottoNumbers) }
+        assertThat(exception.message).isEqualTo(DUPLICATED_NUMBER_EXCEPTION_MESSAGE)
     }
 }
