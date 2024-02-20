186b0e99335b6c9833283919bf5e295de31b4bb1
Gimun Kim <kmkim7575@gmail.com>
2024-02-20 17:57:08 +0900

    test: 로또에 들어갈 수 있는 숫자 개수에 대한 테스트 케이스 추가

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,3 +1,29 @@
-class LottoTest
+import model.LottoNumber
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 
-class Lotto()
+class LottoTest {
+    @Test
+    fun `로또 하나에 들어가는 숫자가 6개가 아닌 경우, 예외를 발생시킨다`() {
+        // given
+        val nums = intArrayOf(1, 2, 3, 4, 5)
+        val nums2 = intArrayOf(1, 2, 3, 4, 5, 6, 7)
+        // then
+        assertAll(
+            { assertThrows<IllegalArgumentException> { Lotto(*nums) } },
+            { assertThrows<IllegalArgumentException> { Lotto(*nums2) } },
+        )
+    }
+
+    @Test
+    fun `로또 하나에 들어가는 숫자는 6개다`() {
+        val nums = intArrayOf(1, 2, 3, 4, 5, 6)
+        assertThat(nums).hasSize(6)
+    }
+}
+
+class Lotto(private val numbers: List<LottoNumber>) {
+    constructor(vararg nums: Int) : this(nums.map { LottoNumber(it) })
+}
