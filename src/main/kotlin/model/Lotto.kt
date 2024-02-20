dbdffe603834b7fb470852dcf7baf9c1f7583a47
Gimun Kim <kmkim7575@gmail.com>
2024-02-20 18:11:31 +0900

    feat: Lotto 객체에 중복된 로또 숫자 입력 예외 발생 추가

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -3,6 +3,7 @@ package model
 class Lotto(private val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_NUM_SIZE) { EXCEPTION_LOTTO_NUM_SIZE }
+        require(numbers.distinct().size == LOTTO_NUM_SIZE) { EXCEPTION_DUPLICATED_LOTTO_NUM }
     }
 
     constructor(vararg nums: Int) : this(nums.map { LottoNumber(it) })
@@ -10,5 +11,6 @@ class Lotto(private val numbers: List<LottoNumber>) {
     companion object {
         private const val LOTTO_NUM_SIZE = 6
         private const val EXCEPTION_LOTTO_NUM_SIZE = "로또 숫자의 개수는 ${LOTTO_NUM_SIZE}만 가능합니다."
+        private const val EXCEPTION_DUPLICATED_LOTTO_NUM = "로또 숫자는 중복되어서는 안됩니다."
     }
 }
