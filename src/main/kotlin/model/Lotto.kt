adb035ef116f27e4d62f1ed4f6e6c45455b6e541
hevton <hevton22@gmail.com>
2024-02-20 18:10:26 +0900

    feat(Lotto): 로또 클래스 기능 구현
    - 로또 번호 유효성 검증(로또 번호는 6개, 로또 번호 중복 불가)
    - 당첨 번호 일치 갯수 구하기
    - 보너스 볼 일치 여부 확인
    에 대한 단위 테스트 완료

---
+++ src/main/kotlin/model/Lotto.kt
@@ -0,0 +1,21 @@
+package model
+
+class Lotto(private val numbers: List<Int>) {
+    init {
+        require(validateCount(numbers)) { EXCEPTION_INVALID_COUNT }
+        require(validateDuplicate(numbers)) { EXCEPTION_DUPLICATED_NUMBER }
+    }
+
+    private fun validateCount(numbers: List<Int>) = numbers.size == 6
+
+    private fun validateDuplicate(numbers: List<Int>) = numbers.size == numbers.toSet().size
+
+    fun getCountOfMatch(numbers: List<Int>) = this.numbers.intersect(numbers).size
+
+    fun hasBonus(bonus: Int) = numbers.contains(bonus)
+
+    companion object {
+        const val EXCEPTION_INVALID_COUNT = "로또 번호는 6개여야 합니다"
+        const val EXCEPTION_DUPLICATED_NUMBER = "로또 번호에 중복이 없어야 합니다"
+    }
+}
