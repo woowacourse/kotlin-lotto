80592bc4207acf44caf1a97b8d74f8e695bb4b5a
Gimun Kim <kmkim7575@gmail.com>
2024-02-20 16:19:07 +0900

    test: 로또에 들어갈 숫자에 대한 테스트 케이스 추가

--- src/test/kotlin/LottoNumberTest.kt
+++ src/test/kotlin/LottoNumberTest.kt
@@ -1,1 +1,23 @@
-class LottoNumberTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoNumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = [1, 45])
+    fun `로또 번호가 허용 범위 이내에 존재할 때 생성된다`(number: Int) {
+        assertThat(LottoNumber(number).number).isEqualTo(number)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 46])
+    fun `로또 번호가 허용 범위 이내에 존재하지 않을 때, 예외를 발생시킴`(number: Int) {
+        assertThrows<IllegalArgumentException> {
+            LottoNumber(number)
+        }
+    }
+}
+
+@JvmInline
+value class LottoNumber(val number: Int)
