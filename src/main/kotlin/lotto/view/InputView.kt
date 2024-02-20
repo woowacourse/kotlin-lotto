ad0bd75957412b8b74096660bfa69c62b6422a27
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-20 17:31:50 +0900

    feat: 각종 입력 문구 출력

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,18 @@
+package lotto.view
+
+import lotto.util.Constant
+fun insertCostMessage(){
+    println(Constant.REQUEST_PURCHASE_PRICE)
+}
+
+fun purchaseCountMessage(){
+    println(Constant.PURCHASE_LOTTO_COUNT)
+}
+
+fun insertWinNumbers(){
+    println(Constant.REQUEST_WINNING_NUM)
+}
+
+fun insertBonusNumbers(){
+    println(Constant.REQUEST_BONUS_NUM)
+}
