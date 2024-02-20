d75470fbb8697f3d0ebb64aa78620cd70a50a74b
JunJangE <wnswkd486@gmail.com>
2024-02-20 15:54:41 +0900

    feat: LottoGame calculateResult
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

--- src/main/kotlin/lotto/model/LottoGame.kt
+++ src/main/kotlin/lotto/model/LottoGame.kt
@@ -5,4 +5,12 @@ class LottoGame(val lottos: List<Lotto>, val winningLotto: Lotto, val bonusNumbe
         require(bonusNumber.toIntOrNull() in 1..45)
         require(bonusNumber !in winningLotto.numbers)
     }
+
+    fun calculateResult(): Map<Rank, Int> {
+        return lottos.map { lotto ->
+            val countOfMatch = lotto.numbers.intersect(winningLotto.numbers.toSet()).size
+            val matchBonus = bonusNumber in lotto.numbers
+            Rank.valueOf(countOfMatch, matchBonus)
+        }.groupingBy { it }.eachCount()
+    }
 }
