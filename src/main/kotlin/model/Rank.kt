364985f7b61d952c39d608ecf3d28647191fd20b
songpink <jejeje04@naver.com>
2024-02-20 20:49:50 +0900

    feat(Rank): 당첨 번호 개수와 보너스 번호 존재 여부로 등수 판별하는 기능 구현

---
+++ src/main/kotlin/model/Rank.kt
@@ -0,0 +1,23 @@
+package model
+
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    companion object {
+        fun decideRank(countOfMatch: Int, matchBonus: Boolean): Rank {
+            return when {
+                countOfMatch == 6 -> FIRST
+                countOfMatch == 5 && matchBonus -> SECOND
+                countOfMatch == 5 && !matchBonus -> THIRD
+                countOfMatch == 4 -> FOURTH
+                countOfMatch == 3 -> FIFTH
+                else -> MISS
+            }
+        }
+    }
+}
