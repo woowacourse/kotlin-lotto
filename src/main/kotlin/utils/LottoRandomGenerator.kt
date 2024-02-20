d1ffccac5248ebd82a012e8e2e8b3b8ad1c11604
hevton <hevton22@gmail.com>
2024-02-20 17:18:53 +0900

    refactor(LottoRandomGenerator): 랜덤 생성 방식 변경
    - 이전: Random 클래스 사용
    - 이후: shuffled()와 sorted() 사용

--- src/main/kotlin/utils/LottoRandomGenerator.kt
+++ src/main/kotlin/utils/LottoRandomGenerator.kt
@@ -1,26 +1,14 @@
 package utils
 
-import kotlin.random.Random
-
 object LottoRandomGenerator : Generator {
     private const val MIN_LOTTO_NUMBER = 1
     private const val MAX_LOTTO_NUMBER = 45
     private const val LOTTO_COUNT = 6
 
-    override fun generate(): List<Int> {
-        val lottoNumbers = mutableListOf<Int>()
-
-        while (lottoNumbers.size < LOTTO_COUNT) {
-            makeLottoNumbers(lottoNumbers)
-        }
-
-        return lottoNumbers
-    }
-
-    private fun makeLottoNumbers(lottoNumbers: MutableList<Int>) {
-        val randomNumber = Random.nextInt(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
-        if (!lottoNumbers.contains(randomNumber)) {
-            lottoNumbers.add(randomNumber)
-        }
-    }
+    override fun generate(): List<Int> =
+        (MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER)
+            .toList()
+            .shuffled()
+            .take(LOTTO_COUNT)
+            .sorted()
 }
