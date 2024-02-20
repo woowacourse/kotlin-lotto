abadb57d5d654d8887d4b07993fe8c43d0c37eec
hevton <hevton22@gmail.com>
2024-02-20 20:14:55 +0900

    test(BonusTest): 보너스 볼 유효성 검증 테스트 구현

---
+++ src/test/kotlin/model/BonusTest.kt
@@ -0,0 +1,33 @@
+package model
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class BonusTest {
+    private val defaultLottoNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+    @ParameterizedTest
+    @ValueSource(strings = ["pang", "hannah", "a12", "%@"])
+    fun `숫자가 아니라면 예외 발생`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Bonus(input, defaultLottoNumbers)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["46", "0", "100"])
+    fun `1 ~ 45 범위의 숫자가 아니라면 예외 발생`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Bonus(input, defaultLottoNumbers)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2", "3"])
+    fun `사용자가 입력한 로또번호에 포함된다면 예외 발생`(input: String) {
+        assertThrows<IllegalArgumentException> {
+            Bonus(input, defaultLottoNumbers)
+        }
+    }
+}
