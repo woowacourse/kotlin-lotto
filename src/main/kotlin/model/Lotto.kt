06d1d43d8cd3f83219f66d3076ed0e564c1c1c97
somin <salth0601@gmail.com>
2024-02-20 17:40:12 +0900

    feat: 고유한 로또 번호에 대한 유효성 검사 기능 구현
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -3,10 +3,12 @@ package model
 class Lotto(val lottoNumbers: List<LottoNumber>) {
     init {
         require(lottoNumbers.size == LOTTO_NUMBER_SIZE) { NUMBER_SIZE_EXCEPTION_MESSAGE }
+        require(lottoNumbers.toSet().size == LOTTO_NUMBER_SIZE) { DUPLICATED_NUMBER_EXCEPTION_MESSAGE }
     }
 
     companion object {
         const val LOTTO_NUMBER_SIZE = 6
         const val NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 번호는 ${LOTTO_NUMBER_SIZE}개여야 합니다."
+        const val DUPLICATED_NUMBER_EXCEPTION_MESSAGE = "로또 번호끼리는 중복되면 안됩니다."
     }
 }
