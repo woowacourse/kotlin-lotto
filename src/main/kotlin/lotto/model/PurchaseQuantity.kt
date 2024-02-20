7b16b286bf8b8b9dc4e220ffbea470db19e05014
gaeun <geun5744@gmail.com>
2024-02-20 17:05:36 +0900

    feat: 로또 구입 금액 검증 기능 구현

--- src/main/kotlin/lotto/model/PurchaseQuantity.kt
+++ src/main/kotlin/lotto/model/PurchaseQuantity.kt
@@ -1,7 +1,21 @@
 package lotto.model
 
-class PurchaseQuantity(purchasePrice: Int) {
+class PurchaseQuantity private constructor(purchasePrice: Int) {
 
     val amount: Int = purchasePrice / 1_000
 
+    companion object {
+        fun create(purchasePrice: String): PurchaseQuantity {
+            require(purchasePrice.isValidDigit() && purchasePrice.isMoreThanMin() && purchasePrice.divideByLottoPrice()) {
+                "올바른 구매 금액을 입력해 주세요."
+            }
+            return PurchaseQuantity(purchasePrice.toInt())
+        }
+
+        private fun String.isValidDigit() = toIntOrNull() != null
+
+        private fun String.isMoreThanMin() = toInt() >= 1000
+
+        private fun String.divideByLottoPrice() = toInt() % 1000 == 0
+    }
 }
--- src/test/kotlin/lotto/service/ServiceTest.kt
+++ src/test/kotlin/lotto/service/ServiceTest.kt
@@ -2,15 +2,28 @@ package lotto.service
 
 import lotto.model.PurchaseQuantity
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 class ServiceTest {
 
     @ParameterizedTest
     @CsvSource("1000,1", "10000,10")
-    fun `구매할 수 있는 로또의 개수 계산한다`(purchasePrice: Int, amount: Int) {
-        val purchaseQuantity = PurchaseQuantity(purchasePrice)
+    fun `구매할 수 있는 로또의 개수 계산한다`(purchasePrice: String, amount: Int) {
+        val purchaseQuantity = PurchaseQuantity.create(purchasePrice)
         assertThat(purchaseQuantity.amount).isEqualTo(amount)
     }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = ["0", "-1000", "1000100", "1000abc", "2200000000"])
+    fun `로또의 구입 금액을 검증한다`(purchasePrice: String) {
+        assertThrows<IllegalArgumentException> {
+            PurchaseQuantity.create(purchasePrice)
+        }
+    }
+
 }
