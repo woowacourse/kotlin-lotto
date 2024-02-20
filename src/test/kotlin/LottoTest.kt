3c9cdb20c8f8a52659f8c48b80a65a516a2bcfad
Gimun Kim <kmkim7575@gmail.com>
2024-02-20 17:22:23 +0900

    test : Lotto 가격 로직 삭제

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,11 +1,3 @@
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+class LottoTest
 
-class LottoTest {
-    @Test
-    fun `로또 가격을 설정할 수 있다`() {
-        assertThat(Lotto(1000).price).isEqualTo(1000)
-    }
-}
-
-class Lotto(val price: Int)
+class Lotto()
