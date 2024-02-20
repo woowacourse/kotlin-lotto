9441a61430ea195c83cb60fdd67ac98c8f9c0a18
sh1mj1 <7wlgns@gmail.com>
2024-02-20 21:53:13 +0900

    feat(LotteryGenerator): generate a lottery instance
     - select 6 shuffled numbers in range of 1 ~ 45
     - the numbers are sorted

---
+++ src/main/kotlin/model/LotteryGenerator.kt
@@ -0,0 +1,17 @@
+package model
+
+class LotteryGenerator {
+    fun generate(): Lottery =
+        Lottery.from(
+            (MIN_LOTTERY_NUMBER..MAX_LOTTERY_NUMBER)
+                .shuffled()
+                .take(LOTTERY_NUMBER_COUNT)
+                .sorted()
+        )
+
+    companion object {
+        private const val MIN_LOTTERY_NUMBER = 1
+        private const val MAX_LOTTERY_NUMBER = 45
+        private const val LOTTERY_NUMBER_COUNT = 6
+    }
+}
---
+++ src/test/kotlin/model/LotteryGeneratorTest.kt
@@ -0,0 +1,15 @@
+package model
+
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class LotteryGeneratorTest {
+    private val lotteryGenerator = LotteryGenerator()
+
+    @RepeatedTest(1_000)
+    fun `6자리의 중복되지 않은 번호의 로또를 생성한다`() {
+        assertDoesNotThrow {
+            println(lotteryGenerator.generate())
+        }
+    }
+}
