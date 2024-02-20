e746af3ff994cd45211c567a711c12b405e6e07a
JunJangE <wnswkd486@gmail.com>
2024-02-20 17:22:18 +0900

    refactor: 하드코딩 제거
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,12 +2,15 @@ package lotto.model
 
 class Lotto(val numbers: List<String>) {
     init {
-        require(numbers.distinct().size == LOTTO_SIZE)
-        require(numbers.all { it.toIntOrNull() in LOTTO_NUMBER_RANGE })
+        require(numbers.distinct().size == LOTTO_SIZE) { LOTTO_SIZE_ERROR_MESSAGE }
+        require(numbers.all { it.toIntOrNull() in LOTTO_NUMBER_RANGE }) { LOTTO_RANGE_ERROR_MESSAGE }
     }
 
     companion object {
         const val LOTTO_SIZE = 6
         val LOTTO_NUMBER_RANGE: IntRange = 1..45
+
+        const val LOTTO_SIZE_ERROR_MESSAGE = "로또의 숫자들은 중복되지 않는 6개입니다."
+        const val LOTTO_RANGE_ERROR_MESSAGE = "로또의 숫자들은 1부터 45까지의 숫자로 구성되어야 합니다."
     }
 }
--- src/main/kotlin/lotto/model/LottoGame.kt
+++ src/main/kotlin/lotto/model/LottoGame.kt
@@ -5,14 +5,14 @@ class LottoGame(lottos: List<Lotto> = emptyList(), val winningLotto: Lotto, val 
         private set
 
     init {
-        require(bonusNumber.toIntOrNull() in 1..45)
-        require(bonusNumber !in winningLotto.numbers)
+        require(bonusNumber.toIntOrNull() in LOTTO_NUMBER_RANGE) { BONUS_NUMBER_RANGE_ERROR_MESSAGE }
+        require(bonusNumber !in winningLotto.numbers) { BONUS_NUMBER_DUPLICATED_ERROR_MESSAGE }
     }
 
     fun createLottos(count: Int) {
         lottos =
             List(count) {
-                Lotto((1..45).shuffled().take(6).sorted().map { it.toString() })
+                Lotto((LOTTO_NUMBER_RANGE).shuffled().take(LOTTO_SIZE).sorted().map { it.toString() })
             }
     }
 
@@ -22,5 +22,13 @@ class LottoGame(lottos: List<Lotto> = emptyList(), val winningLotto: Lotto, val 
             val matchBonus = bonusNumber in lotto.numbers
             Rank.valueOf(countOfMatch, matchBonus)
         }.groupingBy { it }.eachCount()
+    }
+
+    companion object {
+        val LOTTO_SIZE = 6
+        val LOTTO_NUMBER_RANGE: IntRange = 1..45
+
+        const val BONUS_NUMBER_RANGE_ERROR_MESSAGE = "보너스 숫자는 1부터 45까지의 숫자로 구성되어야 합니다."
+        const val BONUS_NUMBER_DUPLICATED_ERROR_MESSAGE = "보너스 숫자는 당첨 번호와 중복되면 안됩니다."
     }
 }
