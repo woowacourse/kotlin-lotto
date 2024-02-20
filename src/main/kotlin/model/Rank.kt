be705a37cdd96d7c35b369cfa041356c427fd718
hevton <hevton22@gmail.com>
2024-02-20 19:43:07 +0900

    feat(Rank): 번호 매칭 갯수에 따라 당첨 결과 판별 구현

---
+++ src/main/kotlin/model/Rank.kt
@@ -0,0 +1,27 @@
+package model
+
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0),
+    ;
+
+    companion object {
+        fun valueOf(
+            countOfMatch: Int,
+            matchBonus: Boolean,
+        ): Rank {
+            return when {
+                countOfMatch == FIRST.countOfMatch -> FIRST
+                countOfMatch == SECOND.countOfMatch && matchBonus -> SECOND
+                countOfMatch == THIRD.countOfMatch -> THIRD
+                countOfMatch == FOURTH.countOfMatch -> FOURTH
+                countOfMatch == FIFTH.countOfMatch -> FIFTH
+                else -> MISS
+            }
+        }
+    }
+}
