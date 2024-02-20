7fc9fb75ae8b62373f366da900740e2c818b9ff1
Gimun Kim <kmkim7575@gmail.com>
2024-02-20 20:45:00 +0900

    feat: 로또 번호를 생성하기 위한 인터페이스와 자동 로또 번호 생성기 클래스 추가

---
+++ src/main/kotlin/model/NumbersGenerator.kt
@@ -0,0 +1,9 @@
+package model
+
+fun interface NumbersGenerator {
+    fun generate(size: Int): List<Int>
+}
+
+class RandomNumberGenerator : NumbersGenerator {
+    override fun generate(size: Int): List<Int> = (1..45).shuffled().take(size)
+}
