d45c899b5b558d0d9badd3e6066c6151fa8f86e6
somin <salth0601@gmail.com>
2024-02-21 17:24:27 +0900

    feat: 로또의 보너스 넘버 포함 여부 확인 기능 구현
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/main/kotlin/model/LottoNumberMatcher.kt
+++ src/main/kotlin/model/LottoNumberMatcher.kt
@@ -4,4 +4,8 @@ class LottoNumberMatcher {
     fun matchCount(targetLotto: Lotto, winningLotto: Lotto): Int {
         return (targetLotto.numbers.intersect(winningLotto.numbers.toSet())).size
     }
+
+    fun bonusCount(targetLotto: Lotto, bonusNumber: LottoNumber): Boolean {
+        return targetLotto.numbers.contains(bonusNumber)
+    }
 }
