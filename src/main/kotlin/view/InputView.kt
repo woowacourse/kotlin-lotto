3da9c35a23c292f22078e56bfebdf95b67c36cab
songpink <jejeje04@naver.com>
2024-02-21 15:31:45 +0900

    feat(OutputView): 당첨번호를 입력받는 기능 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -6,5 +6,9 @@ class InputView {
         return readln().toInt()
     }
 
+    fun getWinningTicket(): List<Int> {
+        println("지난 주 당첨 번호를 입력해 주세요.")
+        return readln().split(",").map { it.trim().toInt() }
+    }
 
 }
