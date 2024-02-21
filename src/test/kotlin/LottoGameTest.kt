7575918f503fab11a3de2c26d1e407f66121043b
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-21 16:18:57 +0900

    refect: TestCode REFECTORING

--- src/test/kotlin/LottoGameTest.kt
+++ src/test/kotlin/LottoGameTest.kt
@@ -1,51 +1,61 @@
-import lotto.model.*
+import lotto.model.Lotto
+import lotto.model.LottoPrize
+import lotto.model.UserPrize
+import lotto.model.WinningNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoGameTest {
+    private val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
+    private val winningNumber = WinningNumber(
+        lotto = lotto,
+        bonusNumber = 5
+    )
+
     @Test
     fun `보너스 매치`() {
-        assertThat(matchBonusNumber(setOf(1,2,3,4,5,6),5)).isTrue()
+        assertThat(lotto.matchBonusNumber(winningNumber)).isTrue()
     }
+
     @Test
     fun `당첨 계산`() {
-        assertThat(matchCount(setOf(1,2,3,4,5,6), setOf(1,2,3,4,5,6))).isEqualTo(6)
+        assertThat(lotto.matchCount(winningNumber)).isEqualTo(6)
     }
 
     @Test
-    fun `당첨금 계산 (보너스 당첨)`(){
-        assertThat(findRanking(0,true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(1,true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(2,true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(3,true)).isEqualTo(LottoPrize.FIFTH)
-        assertThat(findRanking(4,true)).isEqualTo(LottoPrize.FOURTH)
-        assertThat(findRanking(5,true)).isEqualTo(LottoPrize.SECOND)
-        assertThat(findRanking(6,true)).isEqualTo(LottoPrize.FIRST)
+    fun `당첨금 계산 (보너스 당첨)`() {
+        assertThat(lotto.findRanking(0, true)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(1, true)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(2, true)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(3, true)).isEqualTo(LottoPrize.FIFTH)
+        assertThat(lotto.findRanking(4, true)).isEqualTo(LottoPrize.FOURTH)
+        assertThat(lotto.findRanking(5, true)).isEqualTo(LottoPrize.SECOND)
+        assertThat(lotto.findRanking(6, true)).isEqualTo(LottoPrize.FIRST)
     }
 
     @Test
-    fun `당첨금 계산 (보너스 미당첨)`(){
-        assertThat(findRanking(0,false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(1,false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(2,false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(findRanking(3,false)).isEqualTo(LottoPrize.FIFTH)
-        assertThat(findRanking(4,false)).isEqualTo(LottoPrize.FOURTH)
-        assertThat(findRanking(5,false)).isEqualTo(LottoPrize.THIRD)
-        assertThat(findRanking(6,false)).isEqualTo(LottoPrize.FIRST)
+    fun `당첨금 계산 (보너스 미당첨)`() {
+        assertThat(lotto.findRanking(0, false)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(1, false)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(2, false)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lotto.findRanking(3, false)).isEqualTo(LottoPrize.FIFTH)
+        assertThat(lotto.findRanking(4, false)).isEqualTo(LottoPrize.FOURTH)
+        assertThat(lotto.findRanking(5, false)).isEqualTo(LottoPrize.THIRD)
+        assertThat(lotto.findRanking(6, false)).isEqualTo(LottoPrize.FIRST)
     }
 
     @Test
-    fun `수익 계산`(){
-        assertThat(0).isEqualTo(prizeCalculate(mapOf(LottoPrize.BOOM to 1)))
-        assertThat(5_000).isEqualTo(prizeCalculate(mapOf(LottoPrize.FIFTH to 1)))
-        assertThat(50_000).isEqualTo(prizeCalculate(mapOf(LottoPrize.FOURTH to 1)))
-        assertThat(1_500_000).isEqualTo(prizeCalculate(mapOf(LottoPrize.THIRD to 1)))
-        assertThat(30_000_000).isEqualTo(prizeCalculate(mapOf(LottoPrize.SECOND to 1)))
-        assertThat(2_000_000_000).isEqualTo(prizeCalculate(mapOf(LottoPrize.FIRST to 1)))
+    fun `수익 계산`() {
+        assertThat(0).isEqualTo(UserPrize(mapOf(LottoPrize.BOOM to 1)).prizeCalculate())
+        assertThat(5_000).isEqualTo(UserPrize(mapOf(LottoPrize.FIFTH to 1)).prizeCalculate())
+        assertThat(50_000).isEqualTo(UserPrize(mapOf(LottoPrize.FOURTH to 1)).prizeCalculate())
+        assertThat(1_500_000).isEqualTo(UserPrize(mapOf(LottoPrize.THIRD to 1)).prizeCalculate())
+        assertThat(30_000_000).isEqualTo(UserPrize(mapOf(LottoPrize.SECOND to 1)).prizeCalculate())
+        assertThat(2_000_000_000).isEqualTo(UserPrize(mapOf(LottoPrize.FIRST to 1)).prizeCalculate())
     }
 
     @Test
-    fun `수익률 계산`(){
-        assertThat(0.357).isEqualTo(prizeRateCalculate(5000,14))
+    fun `수익률 계산`() {
+        assertThat(0.357).isEqualTo(UserPrize(mapOf(LottoPrize.FIRST to 1)).prizeRateCalculate(5000, 14))
     }
 }
--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,31 +1,32 @@
 import lotto.model.Lotto
-import net.bytebuddy.asm.Advice.OffsetMapping.Factory.Illegal
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.lang.IllegalArgumentException
 
 class LottoTest {
     @Test
     fun `로또 번호 클래스`() {
         assertThat(Lotto(setOf(1, 2, 3, 4, 5, 6)).getNumbers()).isEqualTo((setOf(1, 2, 3, 4, 5, 6)))
     }
+
     @Test
     fun `로또 번호 개수는 6개 초과`() {
-        assertThrows<IllegalArgumentException>{
-            Lotto(setOf(1,2,3,4,5,6,7))
+        assertThrows<IllegalArgumentException> {
+            Lotto(setOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
+
     @Test
     fun `로또 번호 개수는 6개 미만`() {
-        assertThrows<IllegalArgumentException>{
-            Lotto(setOf(1,2,3,4,5))
+        assertThrows<IllegalArgumentException> {
+            Lotto(setOf(1, 2, 3, 4, 5))
         }
     }
+
     @Test
-    fun `로또 번호 1~45 사이`(){
-        assertThrows<IllegalArgumentException>{
-            Lotto(setOf(0,2,3,4,5,46))
+    fun `로또 번호 1~45 사이`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(setOf(0, 2, 3, 4, 5, 46))
         }
     }
 }
