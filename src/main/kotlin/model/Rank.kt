64d22d8fc86078dcc7c94318daf58a084be900d1
somin <salth0601@gmail.com>
2024-02-21 16:20:43 +0900

    feat: 일치 개수와 보너스 매치 여부에 따른 랭크 반환 기능 구현
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/main/kotlin/model/Rank.kt
+++ src/main/kotlin/model/Rank.kt
@@ -7,4 +7,23 @@ enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
     FOURTH(4, 50_000),
     FIFTH(3, 5_000),
     MISS(0, 0);
+
+    companion object {
+        fun valueOf(countOfMatch: Int, matchBonus: Boolean): Rank {
+            return Rank.entries.find {
+                when (countOfMatch) {
+                    FIRST.countOfMatch, FOURTH.countOfMatch, FIFTH.countOfMatch -> it.countOfMatch == countOfMatch
+                    SECOND.countOfMatch, THIRD.countOfMatch -> isSecondOrThird(matchBonus, it)
+                    else -> it.winningMoney == MISS.winningMoney
+                }
+            } ?: MISS
+        }
+
+        private fun isSecondOrThird(matchBonus: Boolean, it: Rank) =
+            if (matchBonus) {
+                it.winningMoney == SECOND.winningMoney
+            } else {
+                it.winningMoney == THIRD.winningMoney
+            }
+    }
 }
