562bbe5a313f45cba9c122ee69f40b4d48173389
Gimun Kim <kmkim7575@gmail.com>
2024-02-21 15:55:32 +0900

    feat : 로또넘버는 오름차순 유효성 검사 로직 추가

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -4,7 +4,7 @@ data class Lotto(private val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_NUM_SIZE) { EXCEPTION_LOTTO_NUM_SIZE }
         require(numbers.distinct().size == LOTTO_NUM_SIZE) { EXCEPTION_DUPLICATED_LOTTO_NUM }
-        // TODO sorted 검증문 추가
+        require(numbers.sortedBy { it.number } == numbers) { EXCEPTION_SORTED_LOTTO_NUMS }
     }
 
     constructor(vararg nums: Int) : this(nums.map { LottoNumber(it) })
@@ -17,5 +17,6 @@ data class Lotto(private val numbers: List<LottoNumber>) {
         private const val LOTTO_NUM_SIZE = 6
         private const val EXCEPTION_LOTTO_NUM_SIZE = "로또 숫자의 개수는 ${LOTTO_NUM_SIZE}만 가능합니다."
         private const val EXCEPTION_DUPLICATED_LOTTO_NUM = "로또 숫자는 중복되어서는 안됩니다."
+        private const val EXCEPTION_SORTED_LOTTO_NUMS = "로또 숫자는 오름차순으로 정렬되어야합니다."
     }
 }
