72f7f68283ee4b84cbcf2fc574a4e0871c359e6e
songpink <jejeje04@naver.com>
2024-02-21 15:25:34 +0900

    feat(OutputView): 구입한 로또 목록을 출력하는 기능 구현

--- src/test/kotlin/model/LottoTicketTest.kt
+++ src/test/kotlin/model/LottoTicketTest.kt
@@ -1,20 +1,19 @@
 package model
 
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 
-class LottoNumbersTest {
+class LottoTicketTest {
     @Test
     fun `구매한 로또 번호 6개를 저장한다`() {
         assertThrows<IllegalArgumentException>{
-            LottoNumbers(listOf(1,2,3,4,5,6,7))
+            LottoTicket(listOf(1,2,3,4,5,6,7))
         }
     }
     @Test
     fun `로또 번호는 1 ~ 45 이어야 한다`() {
         assertThrows<IllegalArgumentException>{
-            LottoNumbers(listOf(0,99,377,422,511,642))
+            LottoTicket(listOf(0,99,377,422,511,642))
         }
     }
 }
