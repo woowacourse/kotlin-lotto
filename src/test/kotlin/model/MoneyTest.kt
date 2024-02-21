24cd504fd44d8dcb7c9cb12d6546c7f5643b4d56
somin <salth0601@gmail.com>
2024-02-21 13:51:18 +0900

    test: 구입 금액이 천원 단위인지 유효성 검사 테스트 추가
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/test/kotlin/model/MoneyTest.kt
+++ src/test/kotlin/model/MoneyTest.kt
@@ -1,7 +1,9 @@
 package model
 
+import model.Money.Companion.INVALID_UNIT_EXCEPTION_MESSAGE
 import model.Money.Companion.MINIMUM_VALUE_EXCEPTION_MESSAGE
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -14,5 +16,12 @@ class MoneyTest {
         val exception = assertThrows<IllegalArgumentException> { Money(value) }
 
         assertThat(exception.message).isEqualTo(MINIMUM_VALUE_EXCEPTION_MESSAGE)
+    }
+
+    @Test
+    fun `구입 금액이 1000원 단위가 아니면 예외를 던진다 `() {
+        val exception = assertThrows<IllegalArgumentException> { Money(1234) }
+
+        assertThat(exception.message).isEqualTo(INVALID_UNIT_EXCEPTION_MESSAGE)
     }
 }
