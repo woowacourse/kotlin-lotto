50d328b6907626992d3eb6df4a84d8706b5277f5
JunJangE <wnswkd486@gmail.com>
2024-02-21 15:13:43 +0900

    test: LottoGameTest에서 LottoAnalyzerTest로 클래스명 수정
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

--- src/test/kotlin/lotto/model/LottoAnalyzerTest.kt
+++ src/test/kotlin/lotto/model/LottoAnalyzerTest.kt
@@ -2,11 +2,8 @@ package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
-class LottoGameTest {
+class LottoAnalyzerTest {
     @Test
     fun `5개의 당첨 번호가 일치하고 보너스 번호가 일치하지 않으면 3등이고, 일치하면 2등이다`() {
         val numbers =
@@ -15,9 +12,9 @@ class LottoGameTest {
                 Lotto(numbers = listOf("1", "2", "3", "4", "5", "8")),
             )
         val winningNumbers = Lotto(numbers = listOf("1", "2", "3", "4", "5", "7"))
-        val lottoGame = LottoGame(numbers, winningNumbers, "8")
-        val result = lottoGame.calculateResult()
+        val lottoGame = LottoAnalyzer(numbers, DrawResult(winningNumbers, "8"))
+        val lottoResult = lottoGame.calculateResult()
 
-        assertThat(result).isEqualTo(mapOf(Rank.THIRD to 1, Rank.SECOND to 1))
+        assertThat(lottoResult).isEqualTo(LottoResult(mapOf(Rank.THIRD to 1, Rank.SECOND to 1)))
     }
 }
