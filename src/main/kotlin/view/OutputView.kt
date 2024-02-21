b8fadc19ccd3ce248098a4468ea28fbb0a745114
songpink <jejeje04@naver.com>
2024-02-21 15:54:18 +0900

    feat(OutputView): 로또 당첨 통계를 출력하는 기능 구현

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,6 +1,7 @@
 package view
 
 import model.LottoTicket
+import model.Rank
 
 class OutputView {
     fun printLottoCount(lottoCount: Int){
@@ -11,5 +12,17 @@ class OutputView {
         lottoTickets.forEach{
             println(it.lottoTicket.sorted())
         }
+    }
+
+    fun printWinningChart(winningChart: Map<Rank, Int>){
+        println("3개 일치 (5000원)- ${winningChart[Rank.FIFTH]}개")
+        println("4개 일치 (50000원)- ${winningChart[Rank.FOURTH]}개")
+        println("5개 일치 (1500000원)- ${winningChart[Rank.THIRD]}개")
+        println("5개 일치, 보너스 볼 일치(30000000원) - ${winningChart[Rank.SECOND]}개")
+        println("6개 일치 (2000000000원)- ${winningChart[Rank.FIRST]}개")
+    }
+
+    fun printWinningRate(winningRate: Float) {
+        println("총 수익률은 ${winningRate}입니다.")
     }
 }
