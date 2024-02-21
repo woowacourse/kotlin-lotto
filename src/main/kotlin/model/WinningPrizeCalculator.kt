5c4c5f3af88a86e32bc345ac37b653f3bb5ce8a4
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-02-21 19:06:52 +0900

    feat: 수익률 계산 기능 구현

--- src/main/kotlin/model/WinningPrizeCalculator.kt
+++ src/main/kotlin/model/WinningPrizeCalculator.kt
@@ -1,6 +1,11 @@
 package lotto.model
 
 object WinningPrizeCalculator {
-    //
+    fun calculateProfitAmount(winningResult: Map<WinningRank, Int>) = winningResult.entries.sumOf {
+        it.key.prize * (it.value)
+    }
 
+    fun calculateProfitRate(purchaseAmount:Int, profitAmount:Int):Double{
+        return (profitAmount/purchaseAmount).toDouble()
+    }
 }
