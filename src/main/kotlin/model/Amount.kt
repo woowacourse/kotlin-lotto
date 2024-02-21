db840956d7446770efe49f1865b22fb0b2300a78
hevton <hevton22@gmail.com>
2024-02-21 15:28:33 +0900

    refactor(Amount): 구입 금액 네이밍 변경 & 로또 티겟 가격 상수화
    - 이전: price
    - 이후: money

--- src/main/kotlin/model/Amount.kt
+++ src/main/kotlin/model/Amount.kt
@@ -1,22 +1,23 @@
 package model
 
-class Amount(val price: Int) {
+class Amount(val money: Int) {
     constructor(input: String) : this(
-        input.toIntOrNull() ?: throw IllegalArgumentException(EXCEPTION_IS_NOT_NUMBER)
+        input.toIntOrNull() ?: throw IllegalArgumentException(EXCEPTION_IS_NOT_NUMBER),
     )
 
     init {
-        require(validateIsPositive(price)) { EXCEPTION_IS_NOT_POSITIVE }
-        require(validatePriceIsThousand(price)) { EXCEPTION_IS_NOT_THOUSAND_UNIT }
+        require(validateIsPositive(money)) { EXCEPTION_IS_NOT_POSITIVE }
+        require(validatePriceIsThousand(money)) { EXCEPTION_IS_NOT_THOUSAND_UNIT }
     }
 
-    private fun validatePriceIsThousand(price: Int) = price % 1000 == 0
+    private fun validatePriceIsThousand(money: Int) = money % LOTTO_TICKET_PRICE == 0
 
-    private fun validateIsPositive(price: Int) = price > 0
+    private fun validateIsPositive(money: Int) = money > 0
 
     companion object {
         const val EXCEPTION_IS_NOT_NUMBER = "숫자만 입력하셔야 합니다"
         const val EXCEPTION_IS_NOT_POSITIVE = "양수를 입력하셔야 합니다"
         const val EXCEPTION_IS_NOT_THOUSAND_UNIT = "로또 한 장의 가격은 천원 단위여야 합니다"
+        const val LOTTO_TICKET_PRICE = 1000
     }
 }
