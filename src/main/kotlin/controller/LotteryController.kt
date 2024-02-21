f73e4a6935f4e6d49fcefcaacecde8360c3fcd2d
sh1mj1 <7wlgns@gmail.com>
2024-02-21 22:49:56 +0900

    feat(LotteryController): Implement whole service

---
+++ src/main/kotlin/controller/LotteryController.kt
@@ -0,0 +1,41 @@
+package controller
+
+import model.*
+import view.InputView
+import view.OutputView
+import java.math.BigDecimal
+
+class LotteryController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+    private val lotteryResultEvaluator: LotteryResultEvaluator = LotteryResultEvaluator(),
+    private val profit: Profit = Profit()
+) {
+
+    fun start() {
+        val purchaseAmount = Money.from(inputView.readPurchaseAmount())
+        val lotterySeller = LotterySeller(purchaseAmount)
+        val lotteryCount = lotterySeller.getLotteryCount()
+
+        val lotteryGenerator = LotteryGenerator()
+
+        val lotteries = Lotteries(List(lotteryCount) { lotteryGenerator.generate() })
+        outputView.showPurchasedLotteries(lotteries)
+
+        val winningNumbers = Lottery.fromInput(inputView.readWinningNumbers())
+        val bonusNumber = LotteryNumber.bonusNumber(winningNumbers, inputView.readBonusNumber())
+
+        val winningResult = lotteryResultEvaluator.evaluate(lotteries, winningNumbers, bonusNumber)
+        outputView.showWinningResult(winningResult)
+
+        var totalPrize = Money(BigDecimal(0))
+        for (result in winningResult.result) {
+            totalPrize += (result.key.winningPrize * result.value)
+        }
+
+        val profitRate = profit.calculateRate(purchaseAmount, totalPrize)
+
+        outputView.showProfitRate(profitRate, ProfitStatusDecider.decide(purchaseAmount, totalPrize))
+    }
+
+}
