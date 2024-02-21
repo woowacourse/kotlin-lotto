d300a1b101d6c94cc45bb01886483d332b5c3604
songpink <jejeje04@naver.com>
2024-02-21 15:07:38 +0900

    refactor(LottoTicket): 네이밍을 더욱 명확하게 수정

--- src/main/kotlin/model/LottoTicket.kt
+++ src/main/kotlin/model/LottoTicket.kt
@@ -1,8 +1,10 @@
 package model
 
-class LottoNumbers (val lottoTicket: List<Int>) {
+class LottoTicket (val lottoTicket: List<Int>) {
     init {
         require(lottoTicket.size == 6)
         require(this.lottoTicket.all {it in 1..45})
     }
+
+
 }
