9cfa63fa481cd4dbc3e58d98a38e8a70d2e0b8ac
JoYehyun <jo990714@naver.com>
2024-02-21 18:44:27 +0900

    test: 로또 번호 객체에 대한 유효성 테스트
    Co-authored-by: Junyoung-WON <wjyo60@gmail.com>

---
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -0,0 +1,13 @@
+package lotto.model
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoNumberTest  {
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "46", "a"])
+    fun `로또 번호는 1~45 사이의 6개의 서로 다른 자연수를 갖지 않으면 오류를 발생시킨다`(input: String) {
+        assertThrows<IllegalArgumentException> { LottoNumber(input) }
+    }
+}
