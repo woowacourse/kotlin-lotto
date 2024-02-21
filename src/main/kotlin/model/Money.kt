84e362647e3f75c0554d89abba9d58087bf49473
somin <salth0601@gmail.com>
2024-02-21 13:51:44 +0900

    feat: 구입 금액이 천원 단위인지 유효성 검사 기능 구현
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/main/kotlin/model/Money.kt
+++ src/main/kotlin/model/Money.kt
@@ -2,10 +2,13 @@ package model
 
 class Money(val value: Int) {
     init {
-        require(value >= 1000) { MINIMUM_VALUE_EXCEPTION_MESSAGE }
+        require(value >= LOTTO_PRICE) { MINIMUM_VALUE_EXCEPTION_MESSAGE }
+        require(value % LOTTO_PRICE == 0) { INVALID_UNIT_EXCEPTION_MESSAGE }
     }
 
     companion object {
-        const val MINIMUM_VALUE_EXCEPTION_MESSAGE = "구입금액은 1000원 이상이어야 합니다."
+        const val LOTTO_PRICE = 1000
+        const val MINIMUM_VALUE_EXCEPTION_MESSAGE = "구입 금액은 ${LOTTO_PRICE}원 이상이어야 합니다."
+        const val INVALID_UNIT_EXCEPTION_MESSAGE = "구입 금액은 ${LOTTO_PRICE}원 단위여야 합니다."
     }
 }
