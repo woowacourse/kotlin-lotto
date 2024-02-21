75009037c9dd04374db40f0c6b326cb293aa43fc
sh1mj1 <7wlgns@gmail.com>
2024-02-21 22:09:56 +0900

    feat(Money): add factory method
    - this method creates Money instance from input(String)

--- src/main/kotlin/model/Money.kt
+++ src/main/kotlin/model/Money.kt
@@ -10,4 +10,17 @@ data class Money(val amount: BigDecimal) : Comparable<Money> {
     operator fun div(other: Money): Double = this.amount.divide(other.amount, 2, RoundingMode.HALF_UP).toDouble()
     operator fun rem(other: Money): Money = Money(this.amount.rem(other.amount))
     override fun compareTo(other: Money): Int = (amount - other.amount).toInt()
+
+    companion object {
+        fun from(input: String): Money {
+            val amount = input.toIntOrNull()
+            requireNotNull(amount) { ERROR_INVALID_PURCHASE_AMOUNT }
+            require(amount >= 0) { ERROR_MINUS_PURCHASE_AMOUNT }
+
+            return Money(BigDecimal(input))
+        }
+
+        private const val ERROR_INVALID_PURCHASE_AMOUNT = "제대로 된 금액을 입력해야 합니다."
+        private const val ERROR_MINUS_PURCHASE_AMOUNT = "0 이상의 금액을 입력해야 합니다."
+    }
 }
