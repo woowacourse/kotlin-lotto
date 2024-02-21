10b452c67cbaa9639604bf0316ebeace8ae07b0b
sh1mj1 <7wlgns@gmail.com>
2024-02-21 15:27:26 +0900

    feat(WinningRank): add none entry
     - none entry was added to represent a result of matching 0~2 numbers
     - entry order was changed in descending order

--- src/main/kotlin/model/WinningRank.kt
+++ src/main/kotlin/model/WinningRank.kt
@@ -3,13 +3,15 @@ package model
 import java.math.BigDecimal
 
 enum class WinningRank(
-    matchNumbers: Int,
+    val matchNumbers: Int,
     bonusNumberMatch: Boolean,
     winningPrize: Money,
 ) {
-    FIFTH(3, false, Money(BigDecimal(5_000))),
-    FOURTH(4, false, Money(BigDecimal(50_000))),
-    THIRD(5, false, Money(BigDecimal(1_500_000))),
-    SECOND(5, true, Money(BigDecimal(30_000_000))),
     FIRST(6, false, Money(BigDecimal(2_000_000_000))),
+    SECOND(5, true, Money(BigDecimal(30_000_000))),
+    THIRD(5, false, Money(BigDecimal(1_500_000))),
+    FOURTH(4, false, Money(BigDecimal(50_000))),
+    FIFTH(3, false, Money(BigDecimal(5_000))),
+    // NONE 의 matchNumbers 의 0은 편의상 적은 값으로 실제값과 다를 수 있음. (0,1,2 의 경우)
+    NONE(0,false, Money(BigDecimal(0))),
 }
