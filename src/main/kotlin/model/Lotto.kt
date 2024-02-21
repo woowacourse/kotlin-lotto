81d7dd723335434344270ae011a427999e03ba50
songpink <jejeje04@naver.com>
2024-02-21 15:07:01 +0900

    feat(Lotto): 로또 시스템 클래스 구현

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -1,8 +1,47 @@
 package model
 
-class Lotto(val purchasePrice: Int) {
-    fun getLottoCount(): Int{
-        return purchasePrice/1000
+class Lotto(
+    private val purchasePrice: Int,
+    private val winningTicket: LottoTicket,
+    private val bonusNumber: Int,
+) {
+    fun getUserTickets(): List<LottoTicket> {
+        val userTickets: MutableList<LottoTicket> = mutableListOf()
+        repeat(getLottoCount()) {
+            userTickets.add(makeUserTicket())
+        }
+        return userTickets
+    }
+
+    fun makeUserTicket() = LottoTicket((1..45).shuffled().take(6))
+
+    fun getLottoCount(): Int = purchasePrice / 1000
+
+    fun countMatchNumber(userTicket: LottoTicket): Int {
+        return winningTicket.lottoTicket.intersect(userTicket.lottoTicket.toSet()).size
+    }
+
+    fun getRankList(): List<Rank> = getUserTickets().map {
+        val num = countMatchNumber(it)
+        val hasBonusNumber = bonusIsInTicket(it)
+        Rank.decideRank(num, hasBonusNumber)
+    }
+
+    fun calculateWinningPrize(): Int {
+        val rankList = getRankList()
+        val winningPrize = rankList.sumOf{
+            it.winningMoney
+        }
+        return winningPrize
+    }
+
+    fun calculateWinningRate(): Float {
+        val winningPrize = calculateWinningPrize()
+        return winningPrize.toFloat()/purchasePrice
+    }
+
+    fun bonusIsInTicket(userTicket: LottoTicket): Boolean {
+        return userTicket.lottoTicket.contains(bonusNumber)
     }
 
 }
