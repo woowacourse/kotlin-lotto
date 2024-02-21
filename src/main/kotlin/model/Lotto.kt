e2f6d5ec4ce7d3dc3318bb479a0b50d52e80e24b
Gimun Kim <kmkim7575@gmail.com>
2024-02-21 15:47:25 +0900

    feat : countMatch, contains 구현

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -4,9 +4,14 @@ data class Lotto(private val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_NUM_SIZE) { EXCEPTION_LOTTO_NUM_SIZE }
         require(numbers.distinct().size == LOTTO_NUM_SIZE) { EXCEPTION_DUPLICATED_LOTTO_NUM }
+        // TODO sorted 검증문 추가
     }
 
     constructor(vararg nums: Int) : this(nums.map { LottoNumber(it) })
+
+    fun countMatch(other: Lotto): Int = (other.numbers intersect numbers).size
+
+    operator fun contains(num: LottoNumber): Boolean = num in numbers
 
     companion object {
         private const val LOTTO_NUM_SIZE = 6
