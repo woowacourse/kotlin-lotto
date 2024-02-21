4cc650dab3b7da698588ec1c330028cacc025c59
JunJangE <wnswkd486@gmail.com>
2024-02-21 15:21:17 +0900

    feat: LottoGameController 생성
    Co-Authored-By: jaeyeongjo <124424829+jaeyeongjo@users.noreply.github.com>

---
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -0,0 +1,41 @@
+package lotto.controller
+
+import lotto.model.DrawResult
+import lotto.model.Lotto
+import lotto.model.LottoAnalyzer
+import lotto.model.LottoMachine
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoGameController {
+    fun start() {
+        val lottos = buyLottos()
+        val drawResult = lottoDraw()
+        matchResult(lottos, drawResult)
+    }
+
+    private fun buyLottos(): List<Lotto> {
+        val price = InputView.readPrice()
+        val lottoMachine = LottoMachine(price)
+        val lottos = lottoMachine.createLottos()
+        OutputView.printLottos(lottos)
+        return lottos
+    }
+
+    private fun lottoDraw(): DrawResult {
+        val winningNumber = InputView.readWinningNumbers()
+        val winningLotto = Lotto(winningNumber.split(",").map { it.trim() })
+        val bonusNumber = InputView.readBonusNumber()
+
+        return DrawResult(winningLotto, bonusNumber)
+    }
+
+    private fun matchResult(
+        lottos: List<Lotto>,
+        drawResult: DrawResult,
+    ) {
+        val lottoAnalyzer = LottoAnalyzer(lottos, drawResult)
+        val lottoResult = lottoAnalyzer.calculateResult()
+        OutputView.printResult(lottoResult)
+    }
+}
