68ef05bdccc5bffeb1e86fcc63e418584045753a
somin <salth0601@gmail.com>
2024-02-21 17:24:01 +0900

    test: 로또의 보너스 넘버 포함 여부 테스트 추가
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

--- src/test/kotlin/model/LottoNumberMatcherTest.kt
+++ src/test/kotlin/model/LottoNumberMatcherTest.kt
@@ -1,6 +1,7 @@
 package model
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -8,11 +9,28 @@ import java.util.stream.Stream
 
 class LottoNumberMatcherTest {
 
+    private val lottoNumberMatcher = LottoNumberMatcher()
+    private val targetLotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) })
+
+    @Test
+    fun `로또가 보너스 넘버를 포함하면 true를 반환 한다`() {
+        val bonusNumber = LottoNumber(1)
+        val actual = lottoNumberMatcher.bonusCount(targetLotto, bonusNumber)
+
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `로또가 보너스 넘버를 포함하면 false를 반환 한다`() {
+        val bonusNumber = LottoNumber(7)
+        val actual = lottoNumberMatcher.bonusCount(targetLotto, bonusNumber)
+
+        assertThat(actual).isFalse()
+    }
+
     @ParameterizedTest
     @MethodSource("lottoNumbers, expectedCount")
     fun `두 로또의 일치 숫자 갯수를 반환 한다`(lottoNumbers: List<Int>, expectedCount: Int) {
-        val lottoNumberMatcher = LottoNumberMatcher()
-        val targetLotto = Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) })
         val winningLotto = Lotto(lottoNumbers.map { LottoNumber(it) })
         val actual = lottoNumberMatcher.matchCount(targetLotto, winningLotto)
 
