46b083fcc95e939b90379d6be2887ad4e01c7518
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-22 10:50:23 +0900

    feat: 보너스 Class 검증

--- src/main/kotlin/lotto/model/WinningNumber.kt
+++ src/main/kotlin/lotto/model/WinningNumber.kt
@@ -1,9 +1,14 @@
 package lotto.model
 
+import lotto.util.Constant
+
 class WinningNumber(
     private val lotto: Lotto,
     private val bonusNumber: Int,
 ) {
+    init {
+        require(bonusNumber in Constant.LOTTO_NUM_RANGE)
+    }
 
     fun getWinning(): Lotto {
         return lotto
