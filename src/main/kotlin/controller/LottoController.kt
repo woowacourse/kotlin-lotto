c83d557b09bf56430c5735ae6d35c1ae47780487
hevton <hevton22@gmail.com>
2024-02-22 11:13:35 +0900

    feat(InputView): 보너스 볼 입력 기능 구현

--- src/main/kotlin/controller/LottoController.kt
+++ src/main/kotlin/controller/LottoController.kt
@@ -1,6 +1,7 @@
 package controller
 
 import model.Amount
+import model.Bonus
 import model.Lotto
 import model.LottoStore
 import utils.RandomLottoGenerationStrategy
@@ -8,17 +9,19 @@ import view.InputView
 import view.OutputView
 
 class LottoController {
+
     fun start() {
         val amount = readAmount()
         val userLottos = purchaseLottos(amount)
         printPurchasedLottos(userLottos)
 
         val winningNumbers = readWinningNumbers()
-
-
+        val bonus = readBonus(winningNumbers)
     }
 
     private fun readAmount() = Amount(InputView.readAmount())
+
+    private fun readBonus(winningNumbers: Lotto) = Bonus(InputView.readBonus(), winningNumbers)
 
     private fun readWinningNumbers() = Lotto.fromInput(InputView.readWinningNumbers())
 
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -3,6 +3,7 @@ package view
 object InputView {
     private const val HEADER_READ_AMOUNT = "구입금액을 입력해 주세요."
     private const val HEADER_READ_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
+    private const val HEADER_READ_BONUS = "보너스 볼을 입력해 주세요."
 
     fun readAmount(): String {
         println(HEADER_READ_AMOUNT)
@@ -11,6 +12,11 @@ object InputView {
 
     fun readWinningNumbers(): String {
         println(HEADER_READ_WINNING_NUMBERS)
+        return readln()
+    }
+
+    fun readBonus(): String {
+        println(HEADER_READ_BONUS)
         return readln()
     }
 }
