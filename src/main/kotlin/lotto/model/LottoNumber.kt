92b970a435624f1ca7ed6235b3d603e02732863a
JunJangE <wnswkd486@gmail.com>
2024-02-22 13:36:24 +0900

    feat: LottoNumber 구입 금액 숫자 유효성 검사 추가

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -6,9 +6,11 @@ data class LottoNumber(val number: Int) {
     companion object {
         private val LOTTO_NUMBER_RANGE: IntRange = 1..45
 
+        private const val LOTTO_TYPE_ERROR_MESSAGE = "로또는 숫자여야만 합니다."
         private const val LOTTO_RANGE_ERROR_MESSAGE = "로또의 숫자들은 1부터 45까지의 숫자로 구성되어야 합니다."
 
         fun from(strNumber: String): LottoNumber {
+            require(strNumber.toIntOrNull() != null) { LOTTO_TYPE_ERROR_MESSAGE }
             require(strNumber.toIntOrNull() in LOTTO_NUMBER_RANGE) { LOTTO_RANGE_ERROR_MESSAGE }
             return LottoNumber(strNumber.toInt())
         }
