3167f49cc97c60e977f0d6628e67e62ec5ad5992
jaeyeongjo <nos05075@naver.com>
2024-02-22 13:53:23 +0900

    test: WinningBundleTest 수정

--- src/test/kotlin/lotto/model/WinningBundleTest.kt
+++ src/test/kotlin/lotto/model/WinningBundleTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class WinningBundleTest {
     @ParameterizedTest
-    @CsvSource("7", "1")
+    @CsvSource("6", "1")
     fun `로또 게임에서 보너스 번호는 당첨 번호와 중복되면 안된다`(bonusNumber: String) {
         val winningLotto = Lotto((1..6).map { LottoNumber(it) })
 
