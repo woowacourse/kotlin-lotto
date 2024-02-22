fb9fdf85602eccd2e62ab8d884e8eacc6c9f31bb
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-22 11:07:22 +0900

    refect: Input 함수 객체 생성

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -13,14 +13,14 @@ class Controller {
 
     fun run() {
         insertCostMessage()
-        val charge = inputCharge()
+        val charge = Veilfier.inputCharge()
         val count = charge / Constant.LOTTO_PRICE.toInt()
         purchaseCountMessage(count)
-        val lottos = makeLottos(count) ?: return
+        val lottos = makeLottos(count)
         insertWinNumbers()
-        val winning = inputWinning()
+        val winning = Veilfier.inputWinning()
         insertBonusNumbers()
-        val bonusNumber = inputBonusNumber()
+        val bonusNumber = Veilfier.inputBonusNumber()
 
         val winningNumber = WinningNumber(
             lotto = winning,
@@ -29,57 +29,16 @@ class Controller {
         val prize = lottos.matchlottos(winningNumber)
         winningStatistics()
         outputWinningNumber(prize)
-        calculationOfYield(prize,charge.toDouble())
+        calculationOfYield(prize, charge.toDouble())
     }
 
-    private fun makeLottos(count: Int): Lottos? {
-        return try {
-            Lottos(
-                lottos = List(count) {
-                    val lotto = lottoGenerator.generateLotto()
-                    showLotto(lotto)
-                    lotto
-                }
-            )
-        } catch (e: IllegalArgumentException) {
-            println(e)
-            return null
-        }
-    }
-
-    private fun inputCharge(): Int {
-        return try {
-            readlnOrNull()?.toIntOrNull()
-                ?: throw (IllegalArgumentException("잘못 된 입력 값입니다."))
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            inputCharge()
-        }
-    }
-
-    private fun inputWinning(): Lotto {
-        return try {
-            Lotto(
-                readlnOrNull()
-                    ?.split(SEPARATOR)
-                    ?.map {
-                        it.trim().toIntOrNull() ?: throw (IllegalArgumentException("로또 넘버는 숫자를 입력해야 합니다."))
-                    }?.toSet()
-                    ?: throw (IllegalArgumentException("잘못 된 로또 값입니다."))
-            )
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            inputWinning()
-        }
-    }
-
-    private fun inputBonusNumber(): Int {
-        return try {
-            readlnOrNull()?.toIntOrNull()
-                ?: throw (IllegalArgumentException("잘못 된 입력 값입니다."))
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            inputBonusNumber()
-        }
+    private fun makeLottos(count: Int): Lottos {
+        return Lottos(
+            lottos = List(count) {
+                val lotto = lottoGenerator.generateLotto()
+                showLotto(lotto)
+                lotto
+            }
+        )
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,7 +7,6 @@ const val REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요."
 const val PURCHASE_LOTTO_COUNT = "%d개를 구매했습니다."
 const val REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요."
 const val REQUEST_BONUS_NUM = "보너스 번호를 입력해 주세요."
-const val SEPARATOR = ","
 
 fun insertCostMessage() {
     println(REQUEST_PURCHASE_PRICE)
