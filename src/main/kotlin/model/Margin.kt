0db05e9dcfe64069937be1521842e4876f0ff12f
somin <salth0601@gmail.com>
2024-02-22 11:19:28 +0900

    feat: 총 상금 계산 기능 구현
    Co-authored-by: haeum808 <159797292+haeum808@users.noreply.github.com>

---
+++ src/main/kotlin/model/Margin.kt
@@ -0,0 +1,3 @@
+package model
+
+data class Margin(val rate: Double)
--- src/main/kotlin/model/MarginCalculator.kt
+++ src/main/kotlin/model/MarginCalculator.kt
@@ -1,7 +1,15 @@
 package model
 
-class MarginCalculator {
+object MarginCalculator {
 
-    fun calculate(winningPrize: Money, purchaseAmount: Money): Double =
-        winningPrize.amount * 1000 / purchaseAmount.amount / 1000.0
+    fun calculateTotalPrize(result: LottoDrawingResult): Money {
+        return Money(
+            result.statistics.entries.sumOf { (rank, count) ->
+                rank.winningMoney * count.toLong()
+            }
+        )
+    }
+
+    fun calculateMarginRate(winningPrize: Money, purchaseAmount: Money): Margin =
+        Margin(winningPrize.amount * 1000 / purchaseAmount.amount / 1000.0)
 }
