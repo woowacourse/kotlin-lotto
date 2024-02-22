281f5bd5f0a18c6885240512e92d7bb6b85fcaee
JunJangE <wnswkd486@gmail.com>
2024-02-22 11:05:25 +0900

    rafactor: message 상수화

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,18 +1,22 @@
 package lotto.view
 
 object InputView {
+    private const val PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
+    private const val LAST_WEEK_WINNING_NUMBERS_MESSAGE = "지난 주 당첨번호를 입력해 주세요."
+    private const val BONUS_BALL_MESSAGE = "보너스 볼을 입력해 주세요."
+
     fun readPrice(): String {
-        println("구입금액을 입력해 주세요.")
+        println(PURCHASE_AMOUNT_MESSAGE)
         return readlnOrNull().orEmpty()
     }
 
     fun readWinningNumbers(): List<String> {
-        println("지난 주 당첨번호를 입력해 주세요.")
+        println(LAST_WEEK_WINNING_NUMBERS_MESSAGE)
         return readlnOrNull().orEmpty().split(",").map { it.trim() }
     }
 
     fun readBonusNumber(): String {
-        println("보너스 볼을 입력해 주세요.")
+        println(BONUS_BALL_MESSAGE)
         return readlnOrNull().orEmpty()
     }
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,11 +5,14 @@ import lotto.model.LottoResult
 import lotto.model.Rank
 
 object OutputView {
-    fun printLottos(lottos: List<Lotto>) {
-        println("${lottos.size}개를 구매했습니다.")
-        lottos.forEach { lotto ->
-            println(lotto.lottoNumbers)
-        }
+    private const val LOTTO_SIZE_MESSAGE = "%d개를 구매했습니다."
+    private const val WINNING_STATISTICS_MESSAGE = "당첨 통계\n---------\n"
+    private const val TOTAL_RETURN_MESSAGE = "총 수익률은 %.2f입니다."
+    private const val WINNING_BONUS_MESSAGE = "%d개 일치, 보너스 볼 일치(%d)- %d개"
+    private const val WINNING_MESSAGE = "%d개 일치 (%d)- %d개"
+
+    private const val EMPTY_COUNT = 0
+
     fun printLottoBundle(lottoBundle: LottoBundle) {
         println(LOTTO_SIZE_MESSAGE.format(lottoBundle.lottos.size))
         println(lottoBundle)
@@ -17,16 +20,15 @@ object OutputView {
     }
 
     fun printResult(lottoResult: LottoResult) {
-        println("당첨 통계")
-        println("---------")
+        println(WINNING_STATISTICS_MESSAGE)
         Rank.entries.reversed().forEach { rank ->
-            val count = lottoResult.static[rank] ?: 0
+            val count = lottoResult.static[rank] ?: EMPTY_COUNT
             when (rank) {
                 Rank.MISS -> Unit
-                Rank.SECOND -> println("${rank.countOfMatch}개 일치, 보너스 볼 일치(${rank.winningMoney})- ${count}개")
-                else -> println("${rank.countOfMatch}개 일치 (${rank.winningMoney})- ${count}개")
+                Rank.SECOND -> println(WINNING_BONUS_MESSAGE.format(rank.countOfMatch, rank.winningMoney, count))
+                else -> println(WINNING_MESSAGE.format(rank.countOfMatch, rank.winningMoney, count))
             }
         }
-        println("총 수익률은 ${String.format("%.2f", lottoResult.getProfitRate())}입니다.")
+        println(TOTAL_RETURN_MESSAGE.format(lottoResult.getProfitRate()))
     }
 }
