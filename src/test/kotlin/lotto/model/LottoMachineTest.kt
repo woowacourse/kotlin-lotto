2ee2942980ab1de3cf79c786caa37dfa297909ca
jaeyeongjo <nos05075@naver.com>
2024-02-22 13:51:23 +0900

    test: LottoMachineTest 테스트 케이스 추가 및 불필요한 테스트 케이스 제거

--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -1,23 +1,15 @@
 package lotto.model
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class LottoMachineTest {
     @ParameterizedTest
-    @CsvSource("-1", "한글")
+    @CsvSource("-1", "한글", "999")
     fun `구입 금액은 자연수이면서 1000 이상이다`(price: String) {
         assertThrows<IllegalArgumentException> {
             LottoMachine(price)
         }
-    }
-
-    @Test
-    fun `1000으로 1장의 티켓을 받는다`() {
-        val lottoMachine = LottoMachine("1000")
-        assertThat(lottoMachine.lottoCounts()).isEqualTo(1)
     }
 }
