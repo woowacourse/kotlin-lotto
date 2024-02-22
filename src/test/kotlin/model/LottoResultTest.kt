090ce8262b3af2a1a9563336d3eeb2935d2ecbd2
songpink <jejeje04@naver.com>
2024-02-22 13:08:27 +0900

    test(LottoResultTest): LottoResult 로직 테스트 구현

--- src/test/kotlin/model/LottoResultTest.kt
+++ src/test/kotlin/model/LottoResultTest.kt
@@ -1,7 +1,6 @@
 package model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
