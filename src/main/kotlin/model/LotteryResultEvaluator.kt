8cbc5b609369295f67596f1aea1a0fdeeb484cf3
Hogu59 <dbstjdguswkd@naver.com>
2024-02-22 14:28:45 +0900

    refactactor(LotteryResultEvaluator): change magic numbers
    change magic numbers to constants of companion object

--- src/main/kotlin/model/LotteryResultEvaluator.kt
+++ src/main/kotlin/model/LotteryResultEvaluator.kt
@@ -11,20 +11,25 @@ class LotteryResultEvaluator {
 
         lotteries.lotteries.forEach {
             val rank = lotteryRankEvaluator.evaluate(it, winningLottery, bonusNumber)
-            winningResult[rank] = winningResult[rank]!! + 1
+            winningResult[rank] = winningResult[rank]!! + PLUS_WINNING_COUNT
         }
 
         return WinningResult(winningResult.toMap())
     }
 
     companion object {
-        private val DEFAULT_WINNING_RESULT = mutableMapOf(
-            WinningRank.FIRST to 0,
-            WinningRank.SECOND to 0,
-            WinningRank.THIRD to 0,
-            WinningRank.FOURTH to 0,
-            WinningRank.FIFTH to 0,
-            WinningRank.NONE to 0,
-        )
+        private val DEFAULT_WINNING_RESULT: MutableMap<WinningRank, Int>
+            get() =
+                mutableMapOf(
+                    WinningRank.FIRST to DEFAULT_WINNING_COUNT,
+                    WinningRank.SECOND to DEFAULT_WINNING_COUNT,
+                    WinningRank.THIRD to DEFAULT_WINNING_COUNT,
+                    WinningRank.FOURTH to DEFAULT_WINNING_COUNT,
+                    WinningRank.FIFTH to DEFAULT_WINNING_COUNT,
+                    WinningRank.NONE to DEFAULT_WINNING_COUNT,
+                )
+
+        private const val DEFAULT_WINNING_COUNT = 0
+        private const val PLUS_WINNING_COUNT = 1
     }
 }
