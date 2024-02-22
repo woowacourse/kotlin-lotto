10d96fdbfd253a23b8bcd62a068ec4bbab9ce5fc
jaeyeongjo <nos05075@naver.com>
2024-02-22 13:35:43 +0900

    refactor: lottoCounts함수와 createLottos함수 병합

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -7,15 +7,15 @@ class LottoMachine(private val price: String) {
         require(price.toIntOrNull()?.let { it >= LottoConstant.PRICE } == true) { PRICE_ERROR_MESSAGE }
     }
 
-    fun createLottos() =
-        List(lottoCounts()) {
+    fun createLottos() {
+        val count = price.toInt() / LottoConstant.PRICE
+        List(count) {
             Lotto(
                 (LottoConstant.NUMBER_RANGE).shuffled().take(LottoConstant.SIZE).sorted()
                     .map { LottoNumber(it) },
             )
         }
-
-    fun lottoCounts(): Int = price.toInt() / LottoConstant.PRICE
+    }
 
     companion object {
         private const val PRICE_ERROR_MESSAGE = "구입 금액은 자연수이면서 1000 이상이여야 합니다."
