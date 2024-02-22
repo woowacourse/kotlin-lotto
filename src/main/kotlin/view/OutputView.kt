cf0692efe7d534c6bfbdd456c6ee134e46c01236
songpink <jejeje04@naver.com>
2024-02-22 11:45:56 +0900

    refactor(OutputView): OutputView에서 null처리 책임을 LottoResult로 이동

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,28 +1,29 @@
 package view
 
+import model.LottoResult
 import model.LottoTicket
 import model.Rank
 
 class OutputView {
-    fun printLottoCount(lottoCount: Int){
+    fun printLottoCount(lottoCount: Int) {
         println("${lottoCount}개를 구매했습니다")
     }
 
-    fun printLottoTickets(lottoTickets: List<LottoTicket>){
-        lottoTickets.forEach{
+    fun printLottoTickets(lottoTickets: List<LottoTicket>) {
+        lottoTickets.forEach {
             println(it.lottoTicket.sorted())
         }
     }
 
-    fun printWinningChart(winningChart: Map<Rank, Int>){
-        println("3개 일치 (5,000원)- ${winningChart[Rank.FIFTH]?: 0}개")
-        println("4개 일치 (50,000원)- ${winningChart[Rank.FOURTH]?: 0}개")
-        println("5개 일치 (1,500,000원)- ${winningChart[Rank.THIRD]?: 0}개")
-        println("5개 일치, 보너스 볼 일치(30,000,000원) - ${winningChart[Rank.SECOND]?: 0}개")
-        println("6개 일치 (2,000,000,000원)- ${winningChart[Rank.FIRST]?: 0}개")
+    fun printWinningChart(lottoResult: LottoResult) {
+        println("3개 일치 (${Rank.FIFTH.winningMoney}원)- ${lottoResult.getNum(Rank.FIFTH)}개")
+        println("4개 일치 (${Rank.FOURTH.winningMoney}원)- ${lottoResult.getNum(Rank.FOURTH)}개")
+        println("5개 일치 (${Rank.THIRD.winningMoney}원)- ${lottoResult.getNum(Rank.THIRD)}개")
+        println("5개 일치, 보너스 볼 일치(${Rank.SECOND.winningMoney}원) - ${lottoResult.getNum(Rank.SECOND)}개")
+        println("6개 일치 (${Rank.FIRST.winningMoney}원)- ${lottoResult.getNum(Rank.FIRST)}개")
     }
 
     fun printWinningRate(winningRate: Float) {
-        println("총 수익률은 ${winningRate}입니다.")
+        println("총 수익률은 %.2f입니다.".format(winningRate))
     }
 }
