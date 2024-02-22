3bc14c34e05baa5852668c2eb6ee026ead02b262
songpink <jejeje04@naver.com>
2024-02-22 13:08:04 +0900

    test(LottoResultTest): LottoResult 로직 테스트 구현

---
+++ src/test/kotlin/model/LottoResultTest.kt
@@ -0,0 +1,33 @@
+package model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class LottoResultTest {
+    @Test
+    fun `Map의 내부의 value에 null이 있을 경우 0을 반환한다`() {
+        val rankMap = emptyMap<Rank, Int>()
+        val lottoResult = LottoResult(rankMap)
+        val actual = lottoResult.getNum(Rank.THIRD)
+        assertThat(actual).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "FIRST, 1",
+        "SECOND, 2",
+        "THIRD, 3",
+        "FOURTH, 4",
+        "FIFTH, 5",
+    )
+    fun `Map의 내부의 value가 null이 아닐 경우 제대로 반환한다`(rank: Rank, expected: Int) {
+        val rankMap: MutableMap<Rank, Int> = mutableMapOf()
+        rankMap[rank] = expected
+        val lottoResult = LottoResult(rankMap)
+        val actual = lottoResult.getNum(rank)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
