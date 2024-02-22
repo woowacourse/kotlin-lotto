c00b83665681d7840485b470386f2a3371e982a0
songpink <jejeje04@naver.com>
2024-02-22 11:14:49 +0900

    test(LottoPurchaseTest): Lotto의 기능 분리에 따른 테스트 코드 네이밍의 수정

--- src/test/kotlin/model/LottoPurchaseTest.kt
+++ src/test/kotlin/model/LottoPurchaseTest.kt
@@ -1,17 +1,15 @@
 package model
 
-import model.Lotto
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class LottoTest {
+class LottoPurchaseTest {
     @ParameterizedTest
     @ValueSource(ints = [1000, 5000, 14000, 35000])
     fun `구입 금액 만큼 로또를 산다 (개수)`(purchased: Int) {
-        val actual = Lotto(14000)
-        assertThat(actual.getLottoCount()).isEqualTo(14)
+        val actual = LottoPurchase(14000)
+        assertThat(actual.makeLottoCount()).isEqualTo(14)
     }
 
 
