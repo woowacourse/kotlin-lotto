adb789f3e1fcc107449aa2be73196c34db50fb34
gaeun <geun5744@gmail.com>
2024-02-22 15:08:01 +0900

    fix: 검증 로직 수정

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -3,7 +3,7 @@ package lotto.model
 class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
 
     init {
-        require(lotto.contains(bonusNumber)) {
+        require(!lotto.contains(bonusNumber)) {
             "당첨 번호와 중복되지 않는 보너스 번호를 입력해 주세요."
         }
     }
