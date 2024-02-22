5e9e70072b961a4ef74ba1506252be1c78120fa9
Hogu59 <dbstjdguswkd@naver.com>
2024-02-22 14:31:22 +0900

    refactor(OutputView): change string to constant
    change string to constant and move it in to companion object

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -1,13 +1,18 @@
 package view
 
-import model.*
+import model.Lotteries
+import model.Lottery
+import model.ProfitRate
+import model.ProfitStatus
+import model.WinningRank
+import model.WinningResult
 import java.text.DecimalFormat
 
 class OutputView {
     fun showPurchasedLotteries(lotteries: Lotteries) {
         val amount = lotteries.lotteries.size
 
-        println("${amount}개를 구매했습니다.")
+        println("$amount" + GUIDE_PURCHASED)
         lotteries.lotteries.forEach { lottery ->
             showPurchasedLottery(lottery)
         }
@@ -15,7 +20,13 @@ class OutputView {
     }
 
     private fun showPurchasedLottery(lottery: Lottery) =
-        println("[${lottery.lotteryNumbers.joinToString(", ") { it.number.toString() }}]")
+        println(
+            "$OPENING_SQUARE_BRACKET${
+                lottery.lotteryNumbers.joinToString(
+                    COMMA_WITH_SPACE,
+                ) { it.number.toString() }
+            }$CLOSING_SQUARE_BRACKET",
+        )
 
     fun showWinningResult(winningResult: WinningResult) {
         println(HEADER_WINNING_RESULT)
@@ -28,25 +39,44 @@ class OutputView {
         }
     }
 
-    private fun printMatchStatus(result: Map.Entry<WinningRank, Int>) = println(
-        "${result.key.matchNumbers}개 일치 ${if (result.key.bonusNumberMatch) ", 보너스 볼 일치" else ""} ${
-            MONEY_FORMAT.format(
-                result.key.winningPrize.amount.toInt()
-            )
-        }- ${result.value}개"
-    )
+    private fun printMatchStatus(result: Map.Entry<WinningRank, Int>) =
+        println(
+            "${result.key.matchNumbers}$MATCH_NUMBERS ${if (result.key.bonusNumberMatch) BONUS_BALL_MATCH else EMPTY_LINE} ${
+                MONEY_FORMAT.format(
+                    result.key.winningPrize.amount.toInt(),
+                )
+            }$DASH ${result.value}$COUNT",
+        )
 
-    fun showProfitRate(profitRate: ProfitRate, profitStatus: ProfitStatus) {
-        print("총 수익률은 ${profitRate.rate}입니다.")
-        println("(기준이 1이기 때문에 결과적으로 ${profitStatus.status}이라는 의미임)")
+    fun showProfitRate(
+        profitRate: ProfitRate,
+        profitStatus: ProfitStatus,
+    ) {
+        print("$TOTAL_PROFIT_RATE ${profitRate.rate}$ENDING_MENTION.")
+        println("${OPENING_ROUND_BRACKET}$AS_RESULT_MENTION ${profitStatus.status}${MEANING_MENTION}$CLOSING_ROUND_BRACKET")
     }
-
 
     companion object {
         private val MONEY_FORMAT = DecimalFormat("(#,###원)")
+        private const val GUIDE_PURCHASED = "개를 구매했습니다."
+        private const val COMMA_WITH_SPACE = ", "
+        private const val OPENING_SQUARE_BRACKET = "["
+        private const val CLOSING_SQUARE_BRACKET = "]"
+        private const val OPENING_ROUND_BRACKET = "([)"
+        private const val CLOSING_ROUND_BRACKET = ")"
+        private const val MATCH_NUMBERS = "개 일치"
+        private const val BONUS_BALL_MATCH = ", 보너스 볼 일치"
+        private const val EMPTY_LINE = ""
+        private const val DASH = "-"
+        private const val COUNT = "개"
+        private const val TOTAL_PROFIT_RATE = "총 수익률은"
+        private const val ENDING_MENTION = "입니다."
+        private const val AS_RESULT_MENTION = "기준이 1이기 때문에 결과적으로"
+        private const val MEANING_MENTION = "이라는 의미임"
 
-        private val HEADER_WINNING_RESULT = """당첨 통계
-            |---------""".trimMargin()
+        private val HEADER_WINNING_RESULT =
+            """당첨 통계
+            |---------
+            """.trimMargin()
     }
-
 }
