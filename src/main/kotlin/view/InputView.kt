ef9aecbb2e6e2afecc493eafdf08798186f26fb9
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-02-22 11:14:55 +0900

    feat(InputView): 사용자 입력 기능 구현

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,6 +5,12 @@ class InputView {
     fun readPurchaseAmount(): String =
         readlnOrNull()?.trim() ?: throw IllegalArgumentException(ERROR_EMPTY_INPUT_MESSAGE)
 
+    fun readWinningNumbers(): String =
+        readlnOrNull()?.trim() ?: throw IllegalArgumentException(ERROR_EMPTY_INPUT_MESSAGE)
+
+    fun readWinningBonusNumber(): String =
+        readlnOrNull()?.trim() ?: throw IllegalArgumentException(ERROR_EMPTY_INPUT_MESSAGE)
+
     companion object {
         private const val ERROR_PREFIX = "[ERROR] "
         const val ERROR_EMPTY_INPUT_MESSAGE = "${ERROR_PREFIX}입력값이 없습니다."
