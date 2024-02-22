2275579af377936d4fec61588e8cc244e6cb0544
gaeun <geun5744@gmail.com>
2024-02-22 14:55:07 +0900

    feat: 당첨 통계 및 수익률 출력 구현

--- src/main/kotlin/lotto/constants/StringConstants.kt
+++ src/main/kotlin/lotto/constants/StringConstants.kt
@@ -11,4 +11,10 @@ object StringConstants {
     const val INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
 
     const val OUTPUT_PURCHASE_COUNT = "%d개를 구매했습니다."
+    const val OUTPUT_MATCHING_COUNT = "%d개 일치 (%d원)- %d개"
+    const val OUTPUT_MATCHING_COUNT_BONUS = "%d개 일치, 보너스 볼 일치(%d원)- %d개"
+    const val OUTPUT_WINNING_STATICS = "당첨 통계"
+    const val OUTPUT_DIVIDER = "---------"
+    const val OUTPUT_PROFIT_RATIO = "총 수익률은 %f입니다."
+
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,9 +1,13 @@
 package lotto.view
 
+import lotto.constants.LottoPrize
 import lotto.constants.StringConstants
+import lotto.constants.StringConstants.OUTPUT_PROFIT_RATIO
 import lotto.model.LottoStore
 
 object OutputView {
+
+    private const val DEFAULT_MATCHING_COUNT = 0
 
     fun printPurchaseLotto(lottoStore: LottoStore) {
         println(StringConstants.OUTPUT_PURCHASE_COUNT.format(lottoStore.lottos.size))
@@ -12,5 +16,28 @@ object OutputView {
         }
         println()
     }
+
+    fun printWinningStatistics(prizeCount: Map<LottoPrize, Int>, profitRatio: Double) {
+        println(StringConstants.OUTPUT_WINNING_STATICS)
+        println(StringConstants.OUTPUT_DIVIDER)
+        LottoPrize.values().forEach { lottoPrize ->
+            val matchingCount = prizeCount.getOrDefault(lottoPrize, DEFAULT_MATCHING_COUNT)
+            println(provideMatchingMessage(lottoPrize, matchingCount))
+        }
+    }
+
+    fun printProfitRatio(profitRatio: Double) =
+        println(OUTPUT_PROFIT_RATIO.format(profitRatio))
+
+    private fun provideMatchingMessage(lottoPrize: LottoPrize, matchingCount: Int) =
+        if (lottoPrize == LottoPrize.SECOND) {
+            StringConstants.OUTPUT_MATCHING_COUNT_BONUS.format(
+                lottoPrize.matchingCount,
+                lottoPrize.amount,
+                matchingCount
+            )
+        } else {
+            StringConstants.OUTPUT_MATCHING_COUNT.format(lottoPrize.matchingCount, lottoPrize.amount, matchingCount)
+        }
 
 }
