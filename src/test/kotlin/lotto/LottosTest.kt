55173d3d4ead723c130cfff9c437eb876e8e1f7b
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-02-22 14:58:21 +0900

    refactor(LottosTest): 로또 생성 함수 분리

--- src/test/kotlin/lotto/LottosTest.kt
+++ src/test/kotlin/lotto/LottosTest.kt
@@ -1,6 +1,5 @@
 package lotto
 
-import lotto.model.LottoNumberGenerator
 import lotto.model.LottoNumbers
 import lotto.model.Lottos
 import lotto.model.WinningRank
@@ -10,12 +9,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottosTest {
+    private fun setUpLotto(vararg numbers: Int): Lotto = Lotto(LottoNumbers.lottoNumbersOf(*numbers))
+
     @Test
     fun `Lottos에 추가한 Lotto가 있는지 테스트`() {
         val lottos = Lottos()
-        val lottoNumberGenerator = LottoNumberGenerator()
-        val testLotto1 = Lotto(lottoNumberGenerator.generate())
-        val testLotto2 = Lotto(lottoNumberGenerator.generate())
+        val testLotto1 = setUpLotto(1, 2, 3, 4, 5, 6)
+        val testLotto2 = setUpLotto(11, 12, 13, 14, 15, 16)
         lottos.add(testLotto1)
         lottos.add(testLotto2)
         assertThat(lottos.lottos).usingRecursiveComparison().isEqualTo(
@@ -26,10 +26,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 1등`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)),
+                setUpLotto(1, 2, 3, 4, 5, 6),
                 LottoNumber(10)
             )
         ).isEqualTo(mapOf(WinningRank.FIRST to 1))
@@ -38,10 +38,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 2등`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 7)),
+                setUpLotto(1, 2, 3, 4, 5, 7),
                 LottoNumber(6)
             )
         ).isEqualTo(mapOf(WinningRank.SECOND to 1))
@@ -50,10 +50,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 3등`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 7)),
+                setUpLotto(1, 2, 3, 4, 5, 7),
                 LottoNumber(10)
             )
         ).isEqualTo(mapOf(WinningRank.THIRD to 1))
@@ -62,10 +62,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 4등`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 15, 25)),
+                setUpLotto(1, 2, 3, 4, 15, 25),
                 LottoNumber(10)
             )
         ).isEqualTo(mapOf(WinningRank.FOURTH to 1))
@@ -74,10 +74,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 5등`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 14, 15, 17)),
+                setUpLotto(1, 2, 3, 14, 15, 17),
                 LottoNumber(40)
             )
         ).isEqualTo(mapOf(WinningRank.FIFTH to 1))
@@ -86,10 +86,10 @@ class LottosTest {
     @Test
     fun `결과 계산 테스트 - 꽝`() {
         val lottos = Lottos()
-        lottos.add(Lotto(LottoNumbers.lottoNumbersOf(1, 2, 3, 4, 5, 6)))
+        lottos.add(setUpLotto(1, 2, 3, 4, 5, 6))
         assertThat(
             lottos.winningResult(
-                Lotto(LottoNumbers.lottoNumbersOf(11, 22, 33, 34, 35, 37)),
+                setUpLotto(11, 22, 33, 34, 35, 37),
                 LottoNumber(17)
             )
         ).isEqualTo(mapOf(WinningRank.NONE to 1))
--- src/test/kotlin/lotto/TestNumberGenerator.kt
+++
@@ -1,8 +0,0 @@
-package lotto
-
-import lotto.model.LottoNumbers
-import lotto.model.NumberGenerator
-
-class TestNumberGenerator : NumberGenerator {
-    override fun generate() = LottoNumbers.lottoNumbersOf(1,2,3,4,5,6)
-}
