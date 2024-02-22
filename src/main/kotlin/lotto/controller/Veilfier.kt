884a1829d5d5025d0b4d496a85ee218a832e1cf9
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-22 11:33:18 +0900

    refect: LottoGameTest 리펙

--- src/main/kotlin/lotto/controller/Veilfier.kt
+++ src/main/kotlin/lotto/controller/Veilfier.kt
@@ -3,12 +3,14 @@ package lotto.controller
 import lotto.model.Lotto
 
 const val SEPARATOR = ","
-const val WRONG_INPUT="잘못 된 입력 값입니다."
-const val NEED_NUMBER="로또 넘버는 숫자를 입력해야 합니다."
+const val WRONG_INPUT = "잘못 된 입력 값입니다."
+const val NEED_NUMBER = "로또 넘버는 숫자를 입력해야 합니다."
+
 object Veilfier {
     fun inputCharge(): Int {
         return try {
-            readlnOrNull()?.toIntOrNull()
+            readlnOrNull()
+                ?.toIntOrNull()
                 ?: throw (IllegalArgumentException(WRONG_INPUT))
         } catch (e: IllegalArgumentException) {
             println(e.message)
--- src/test/kotlin/LottoGameTest.kt
+++ src/test/kotlin/LottoGameTest.kt
@@ -1,47 +1,60 @@
-import lotto.model.Lotto
-import lotto.model.LottoPrize
-import lotto.model.UserPrize
-import lotto.model.WinningNumber
+import lotto.model.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LottoGameTest {
-    private val lotto = Lotto(setOf(1, 2, 3, 4, 5, 6))
-    private val winningNumber = WinningNumber(
-        lotto = lotto,
-        bonusNumber = 5
+    private val lottos = Lottos(
+        lottos = listOf(
+            Lotto(setOf(12, 11, 7, 8, 9, 10)),
+            Lotto(setOf(1, 11, 7, 8, 9, 10)),
+            Lotto(setOf(1, 2, 7, 8, 9, 10)),
+            Lotto(setOf(1, 2, 3, 7, 8, 9)),
+            Lotto(setOf(1, 2, 3, 4, 7, 8)),
+            Lotto(setOf(1, 2, 3, 4, 5, 8)),
+            Lotto(setOf(1, 2, 3, 4, 5, 7)),
+            Lotto(setOf(1, 2, 3, 4, 5, 6))
+        )
+    )
+    private val winningNumberCorrect = WinningNumber(
+        lotto = Lotto(setOf(1,2,3,4,5,6)),
+        bonusNumber = 7
+    )
+    private val winningNumberWrong= WinningNumber(
+        lotto = Lotto(setOf(1,2,3,4,5,6)),
+        bonusNumber = 45
     )
 
     @Test
     fun `보너스 매치`() {
-        assertThat(lotto.matchBonusNumber(winningNumber)).isTrue()
+        assertThat(lottos.getLottos()[0].matchBonusNumber(winningNumberCorrect)).isTrue()
     }
 
     @Test
     fun `당첨 계산`() {
-        assertThat(lotto.matchCount(winningNumber)).isEqualTo(6)
+        assertThat(lottos.getLottos()[8].matchCount(winningNumberCorrect)).isEqualTo(6)
+        assertThat(lottos.getLottos()[8].matchCount(winningNumberWrong)).isEqualTo(6)
     }
 
     @Test
     fun `당첨금 계산 (보너스 당첨)`() {
-        assertThat(lotto.findRanking(0, true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(1, true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(2, true)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(3, true)).isEqualTo(LottoPrize.FIFTH)
-        assertThat(lotto.findRanking(4, true)).isEqualTo(LottoPrize.FOURTH)
-        assertThat(lotto.findRanking(5, true)).isEqualTo(LottoPrize.SECOND)
-        assertThat(lotto.findRanking(6, true)).isEqualTo(LottoPrize.FIRST)
+        assertThat(lottos.getLottos()[0].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[1].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[2].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[3].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.FIFTH)
+        assertThat(lottos.getLottos()[4].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.FOURTH)
+        assertThat(lottos.getLottos()[6].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.SECOND)
+        assertThat(lottos.getLottos()[7].findRanking(winningNumberCorrect)).isEqualTo(LottoPrize.FIRST)
     }
 
     @Test
     fun `당첨금 계산 (보너스 미당첨)`() {
-        assertThat(lotto.findRanking(0, false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(1, false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(2, false)).isEqualTo(LottoPrize.BOOM)
-        assertThat(lotto.findRanking(3, false)).isEqualTo(LottoPrize.FIFTH)
-        assertThat(lotto.findRanking(4, false)).isEqualTo(LottoPrize.FOURTH)
-        assertThat(lotto.findRanking(5, false)).isEqualTo(LottoPrize.THIRD)
-        assertThat(lotto.findRanking(6, false)).isEqualTo(LottoPrize.FIRST)
+        assertThat(lottos.getLottos()[0].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[1].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[2].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.BOOM)
+        assertThat(lottos.getLottos()[3].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.FIFTH)
+        assertThat(lottos.getLottos()[4].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.FOURTH)
+        assertThat(lottos.getLottos()[5].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.THIRD)
+        assertThat(lottos.getLottos()[7].findRanking(winningNumberWrong)).isEqualTo(LottoPrize.FIRST)
     }
 
     @Test
@@ -56,6 +69,6 @@ class LottoGameTest {
 
     @Test
     fun `수익률 계산`() {
-        assertThat(0.357).isEqualTo(UserPrize(mapOf(LottoPrize.FIRST to 1)).prizeRateCalculate(5000, 14))
+        assertThat(0.35714285714285715).isEqualTo(UserPrize(mapOf(LottoPrize.FIRST to 1)).prizeRateCalculate(5000, 14000.0))
     }
 }
