316de3351ebccfd49f15c6713750a364eecc71a9
Gimun Kim <kmkim7575@gmail.com>
2024-02-22 13:26:45 +0900

    chore: 발행된 로또 목록 출력 후와 당첨 통계 타이틀 출력 전에 개행 추가

--- src/main/kotlin/view/ConsoleLottoGameOutputView.kt
+++ src/main/kotlin/view/ConsoleLottoGameOutputView.kt
@@ -7,13 +7,15 @@ import model.Rank
 class ConsoleLottoGameOutputView : LottoGameOutputView {
     override fun showPurchasedLottie(lottie: List<Lotto>) {
         println(MESSAGE_PURCHASED_LOTTIE_COUNT.format(lottie.size))
-        lottie.forEach { println(it.numbers) }
+        lottie.forEach { println(it.numbers.map { it.number }) }
+        println()
     }
 
     override fun showGameResult(
         results: List<LottoGameResult.RankResult>,
         rate: Double,
     ) {
+        println(MESSAGE_TITLE)
         showRankState(results)
         showEarningRate(rate)
     }
@@ -36,7 +38,7 @@ class ConsoleLottoGameOutputView : LottoGameOutputView {
 
     private fun formatRankResult(rankResult: LottoGameResult.RankResult): String {
         val (rank, count) = rankResult
-        return "${rank.countOfMatch}개 일치, ${formatBonusMessage(rankResult)}(${rank.winningMoney}원)- $count"
+        return "${rank.countOfMatch}개 일치, ${formatBonusMessage(rankResult)}(${rank.winningMoney}원)- ${count}개\n"
     }
 
     private fun formatBonusMessage(rankResult: LottoGameResult.RankResult): String {
@@ -44,8 +46,19 @@ class ConsoleLottoGameOutputView : LottoGameOutputView {
     }
 
     companion object {
+        private const val MESSAGE_TITLE = "\n당첨 통계\n---------"
         private const val MESSAGE_PURCHASED_LOTTIE_COUNT = "%d개를 구매했습니다."
         private const val MESSAGE_EARNING_RATE = "총 수익률은 %.2f입니다."
         private const val MESSAGE_LOSS = "(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
     }
+}
+
+fun main() {
+    println(
+        buildString {
+            append(1)
+            append("\n\n")
+            append(2)
+        },
+    )
 }
