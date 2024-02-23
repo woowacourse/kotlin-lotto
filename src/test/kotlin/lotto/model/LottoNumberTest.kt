8a18ada6cd827010e5085a94a0ed21349b59e99e
JoYehyun <jo990714@naver.com>
2024-02-23 15:56:43 +0900

    test: LottoNumber 테스트 추가

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -1,5 +1,6 @@
 package lotto.model
 
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -7,7 +8,13 @@ import org.junit.jupiter.params.provider.ValueSource
 class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(strings = ["0", "46", "a"])
-    fun `로또 번호는 1~45 사이의 6개의 서로 다른 자연수를 갖지 않으면 오류를 발생시킨다`(input: String) {
+    fun `로또 번호는 1~45 사이의 자연수를 갖지 않으면 오류를 발생시킨다`(input: String) {
         assertThrows<IllegalArgumentException> { LottoNumber(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "45", "20"])
+    fun `로또 번호는 1~45 사이의 자연수를 갖는다`(input: String) {
+        assertDoesNotThrow { LottoNumber(input) }
     }
 }
