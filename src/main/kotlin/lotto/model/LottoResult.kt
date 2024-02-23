0bf2d021c250b1673a9047d55fb9632a842c08bf
jaeyeongjo <nos05075@naver.com>
2024-02-23 15:08:25 +0900

    refactor: LottoResult의 WinningStatics변수 이름 변경

--- src/main/kotlin/lotto/model/LottoResult.kt
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -1,16 +1,16 @@
 package lotto.model
 
-data class LottoResult(private val winningStatistics: Map<Rank, Int>) {
+data class LottoResult(private val winningCountsByRank: Map<Rank, Int>) {
     fun getProfitRate(): Double {
         val totalProfit =
-            winningStatistics.entries.sumOf { (rank, count) ->
+            winningCountsByRank.entries.sumOf { (rank, count) ->
                 rank.winningMoney * count
             }
-        val totalCount = winningStatistics.entries.sumOf { (_, count) -> count }
+        val totalCount = winningCountsByRank.entries.sumOf { (_, count) -> count }
         return totalProfit.toDouble() / (totalCount * Lotto.LOTTO_PRICE)
     }
 
     fun getWinningCountBy(rank: Rank): Int {
-        return winningStatistics[rank] ?: 0
+        return winningCountsByRank[rank] ?: 0
     }
 }
