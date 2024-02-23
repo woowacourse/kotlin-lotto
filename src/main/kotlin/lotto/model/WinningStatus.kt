a29e431be42f7eafc6cae5799d62b54606d3d9c4
JoYehyun <jo990714@naver.com>
2024-02-23 15:30:17 +0900

    refactor: 티켓값 상수화

--- src/main/kotlin/lotto/model/WinningStatus.kt
+++ src/main/kotlin/lotto/model/WinningStatus.kt
@@ -1,10 +1,11 @@
 package lotto.model
 
+import lotto.model.LottoMachine.TICKET_PRICE
 import lotto.util.WinningRank
 
 data class WinningStatus(val resultStatus: Map<WinningRank, Int>) {
     fun getEarningRate(): Double {
         val total: Long = resultStatus.entries.sumOf { it.key.winningMoney * it.value }
-        return (total / (resultStatus.values.sum() * 1000).toDouble())
+        return (total / (resultStatus.values.sum() * TICKET_PRICE).toDouble())
     }
 }
