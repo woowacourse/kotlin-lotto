733ba7e2fad46575804f8507bb7e35d17a3c5d46
hxeyexn <kck85075@kumoh.ac.kr>
2024-02-23 21:00:48 +0900

    refactor: 로또 한장의 가격을 내부 상수에서 주 생성자로 변경
    - 로또 한장의 가격을 내부 상수에 의존하지 않고 유연하게 대처

--- src/main/kotlin/utils/RandomTicketGenerationStrategy.kt
+++ src/main/kotlin/utils/RandomTicketGenerationStrategy.kt
@@ -5,10 +5,13 @@ import model.Amount
 import model.Lottery
 import model.LotteryNumber
 
-class RandomTicketGenerationStrategy(private val amount: Amount) : TicketGenerationStrategy {
+class RandomTicketGenerationStrategy(
+    private val amount: Amount,
+    private val lottoTicketPrice: Int = 1_000,
+) : TicketGenerationStrategy {
     override fun issueTicket(): Ticket {
         val lotteries = mutableListOf<Lottery>()
-        repeat(amount.money / LOTTO_TICKET_PRICE) {
+        repeat(amount.money / lottoTicketPrice) {
             lotteries.add(Lottery(generateNumbers()))
         }
         return Ticket(lotteries, amount)
@@ -28,6 +31,5 @@ class RandomTicketGenerationStrategy(private val amount: Amount) : TicketGenerat
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
         private const val LOTTO_COUNT = 6
-        private const val LOTTO_TICKET_PRICE = 1000
     }
 }
