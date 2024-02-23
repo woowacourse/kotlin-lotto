711e5480201996cb2b487c245a5d74df23404003
jinuemong <jinwoo3661@naver.com>
2024-02-23 14:02:28 +0900

    chore: 코드 포맷 수정

--- src/main/kotlin/lotto/util/Constant.kt
+++ src/main/kotlin/lotto/util/Constant.kt
@@ -1,7 +1,7 @@
 package lotto.util
 
 object Constant {
-    val LOTTO_NUM_RANGE=(1..45)
-    const val LOTTO_LEN=6
-    const val LOTTO_PRICE=1000.0
+    val LOTTO_NUM_RANGE = (1..45)
+    const val LOTTO_LEN = 6
+    const val LOTTO_PRICE = 1000.0
 }
