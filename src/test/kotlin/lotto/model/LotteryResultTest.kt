a00997c48013758abf10a8031800a18a6e814522
JoYehyun <jo990714@naver.com>
2024-02-23 15:55:42 +0900

    test: 로또 결과 생성 테스트

---
+++ src/test/kotlin/lotto/model/LotteryResultTest.kt
@@ -0,0 +1,40 @@
+package lotto.model
+
+import lotto.util.WinningRank
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LotteryResultTest {
+    @Test
+    fun `내가 구매한 로또와 당첨 결과를 비교하여 당첨 현황을 반환한다`() {
+        val winningNumbers =
+            Lotto(setOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)))
+        val bonusNumber = LottoNumber(7)
+        val mine =
+            listOf(
+                Lotto(
+                    setOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(6),
+                    ),
+                ),
+                Lotto(
+                    setOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(7),
+                    ),
+                ),
+            )
+        val expected = mapOf(WinningRank.FIRST to 1, WinningRank.SECOND to 1)
+        val actual = LotteryResult(winningNumbers, bonusNumber).generateWinningStatus(mine)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
