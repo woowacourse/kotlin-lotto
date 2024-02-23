6bc1050798260e8cfa99c2dd3f798177bc31e078
Junyoung-WON <wjyo60@gmail.com>
2024-02-23 15:12:28 +0900

    test: FOURTH와 FIFTH에 대한 테스트 케이스 추가

--- src/test/kotlin/lotto/util/WinningRankTest.kt
+++ src/test/kotlin/lotto/util/WinningRankTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class WinningRankTest {
     @ParameterizedTest
-    @CsvSource("6,False,FIRST", "5,True,SECOND", "5,False,THIRD", "2,False,MISS")
+    @CsvSource("6,False,FIRST", "5,True,SECOND", "5,False,THIRD", "4,True,FOURTH", "3,False,FIFTH", "2,False,MISS")
     fun `당첨 번호 일치 개수에 따른 등수 반환`(
         matchCount: Int,
         matchBonus: Boolean,
