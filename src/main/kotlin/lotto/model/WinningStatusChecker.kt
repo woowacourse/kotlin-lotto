b42bf50425948b959524682ea8e19b3ac15345cb
Junyoung-WON <wjyo60@gmail.com>
2024-02-23 15:27:07 +0900

    refactor: 로또 숫자 적중 시 증가 크기와 기본 맞춤 횟수를 상수로 활용

--- src/main/kotlin/lotto/model/WinningStatusChecker.kt
+++ src/main/kotlin/lotto/model/WinningStatusChecker.kt
@@ -7,17 +7,22 @@ class WinningStatusChecker(results: List<WinningRank>) {
 
     init {
         results.forEach { rank ->
-            status[rank] = status.getOrDefault(rank, 0) + 1
+            status[rank] = status.getOrDefault(rank, DEFAULT_MATCH_COUNT) + INCREASING_COUNT
         }
     }
 
     override fun toString(): String =
         WinningRank.entries.filter { it != WinningRank.MISS }.joinToString("\n") {
-            "${WinningRank.formatByRank(it)} - ${status.getOrDefault(it, 0)}개"
+            "${WinningRank.formatByRank(it)} - ${status.getOrDefault(it, DEFAULT_MATCH_COUNT)}개"
         }
 
     fun getEarningRate(): Double {
         val total: Long = status.entries.sumOf { it.key.winningMoney * it.value }
-        return (total / (status.values.sum() * 1000).toDouble())
+        return (total / (status.values.sum() * LottoMachine.TICKET_PRICE).toDouble())
+    }
+
+    companion object {
+        const val DEFAULT_MATCH_COUNT = 0
+        const val INCREASING_COUNT = 1
     }
 }
