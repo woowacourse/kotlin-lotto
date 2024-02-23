be050a9fc662078710f111d87abb20e080ef63ef
JoYehyun <jo990714@naver.com>
2024-02-23 13:36:02 +0900

    refactor: 로또와 당첨번호 비교 후 결과 생성 로직 위치 수정

--- src/main/kotlin/lotto/controller/LottoGameController.kt
+++ src/main/kotlin/lotto/controller/LottoGameController.kt
@@ -4,7 +4,6 @@ import lotto.model.LotteryResult
 import lotto.model.Lotto
 import lotto.model.LottoMachine
 import lotto.model.WinningStatusChecker
-import lotto.util.WinningRank
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -22,13 +21,7 @@ class LottoGameController {
 
     private fun makeResult(lottoTickets: List<Lotto>) {
         val lotteryResult = LotteryResult(Lotto(InputView.getWinningNumbers()), InputView.getBonusNumber())
-        val winningResult =
-            lottoTickets.map {
-                WinningRank.convert(
-                    it.checkWinningNumbers(lotteryResult.winning),
-                    it.checkBonusNumbers(lotteryResult.bonusNumber),
-                )
-            }
+        val winningResult = lotteryResult.getWinningResult(lottoTickets)
         val winningStatusChecker = WinningStatusChecker(winningResult)
 
         OutputView.printWinningStatus(winningStatusChecker.toString())
--- src/main/kotlin/lotto/model/LotteryResult.kt
+++ src/main/kotlin/lotto/model/LotteryResult.kt
@@ -1,7 +1,18 @@
 package lotto.model
 
+import lotto.util.WinningRank
+
 data class LotteryResult(val winning: Lotto, val bonusNumber: LottoNumber) {
     init {
-        require(!winning.checkBonusNumbers(bonusNumber)) { "보너스 숫자는 당첨 번호와 중복될 수 없습니다." }
+        require(!winning.compareBonusNumbers(bonusNumber)) { "보너스 숫자는 당첨 번호와 중복될 수 없습니다." }
+    }
+
+    fun getWinningResult(lottoTickets: List<Lotto>): List<WinningRank> {
+        return lottoTickets.map {
+            WinningRank.convert(
+                it.matchWinningNumbers(winning),
+                it.compareBonusNumbers(bonusNumber),
+            )
+        }
     }
 }
