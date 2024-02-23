3a53bc157f177e5fae566868c9da261681769651
songpink <jejeje04@naver.com>
2024-02-23 16:39:54 +0900

    refactor(InputView): 프롬프트 메시지 상수화

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -2,17 +2,23 @@ package view
 
 class InputView {
     fun getPurchasePrice(): Int {
-        println("구입금액을 입력해 주세요.")
+        println(PURCHASE_AMOUNT_PROMPT_MESSAGE)
         return readln().toInt()
     }
 
     fun getWinningTicket(): List<Int> {
-        println("지난 주 당첨 번호를 입력해 주세요.")
+        println(WINNING_NUMBER_PROMPT_MESSAGE)
         return readln().split(",").map { it.trim().toInt() }
     }
 
     fun getBonusNumber(): Int {
-        println("보너스 볼을 입력해 주세요.")
+        println(BONUS_BALL_PROMPT_MESSAGE)
         return readln().toInt()
+    }
+
+    companion object {
+        const val PURCHASE_AMOUNT_PROMPT_MESSAGE = "구입금액을 입력해 주세요."
+        const val WINNING_NUMBER_PROMPT_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+        const val BONUS_BALL_PROMPT_MESSAGE = "보너스 볼을 입력해 주세요."
     }
 }
