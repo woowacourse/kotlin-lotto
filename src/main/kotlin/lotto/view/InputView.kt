059a04c33d07b6f8cff581dee3b25d1228a1ab72
jinuemong <jinwoo3661@naver.com>
2024-02-23 14:32:53 +0900

    refactor: 입출력 뷰의 역할 명확하게 하도록 분리

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,31 +1,19 @@
 package lotto.view
 
-import lotto.model.Lotto
-
-
 const val REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요."
-const val PURCHASE_LOTTO_COUNT = "%d개를 구매했습니다."
 const val REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요."
 const val REQUEST_BONUS_NUM = "보너스 번호를 입력해 주세요."
 
-fun insertCostMessage() {
+fun inputCostMessage() {
     println(REQUEST_PURCHASE_PRICE)
 }
 
-fun purchaseCountMessage(count: Int) {
-    println(PURCHASE_LOTTO_COUNT.format(count))
-}
-
-fun showLotto(lotto: Lotto) {
-    println(lotto.getNumbers().sorted())
-}
-
-fun insertWinNumbers() {
+fun inputWinNumbers() {
     println()
     println(REQUEST_WINNING_NUM)
 }
 
-fun insertBonusNumbers() {
+fun inputBonusNumbers() {
     println()
     println(REQUEST_BONUS_NUM)
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,10 +1,21 @@
 package lotto.view
 
+import lotto.model.Lotto
 import lotto.model.LottoPrize
 import lotto.model.UserPrize
 
+const val PURCHASE_LOTTO_COUNT = "%d개를 구매했습니다."
 const val START_RESULT = "당첨 통계\n—--------"
 const val YIELD_RESULT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+
+fun purchaseCountMessage(count: Int) {
+    println(PURCHASE_LOTTO_COUNT.format(count))
+}
+
+fun showLotto(lotto: Lotto) {
+    println(lotto.getNumbers().sorted())
+}
+
 fun winningStatistics() {
     println()
     println(START_RESULT)
