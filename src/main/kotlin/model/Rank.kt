304465c456c525778e9c0b2e275624debc2b0586
kkosang <eiwoqp77@gmail.com>
2024-02-23 17:00:08 +0900

    refactor: Rank enum에서 일치 횟수 상수화

--- src/main/kotlin/model/Rank.kt
+++ src/main/kotlin/model/Rank.kt
@@ -2,13 +2,20 @@ package model
 
 import util.Constant
 
+private const val MATCH_COUNT_FIRST = 6
+private const val MATCH_COUNT_SECOND = 5
+private const val MATCH_COUNT_THIRD = 5
+private const val MATCH_COUNT_FOURTH = 4
+private const val MATCH_COUNT_FIFTH = 3
+private const val MATCH_COUNT_MISS = 0
+
 enum class Rank(val countOfMatch: Int, val winningAmount: Int) {
-    FIRST(6, Constant.FIRST_RANK_WINNING_MONEY),
-    SECOND(5, Constant.SECOND_RANK_WINNING_MONEY),
-    THIRD(5, Constant.THIRD_RANK_WINNING_MONEY),
-    FOURTH(4, Constant.FOURTH_RANK_WINNING_MONEY),
-    FIFTH(3, Constant.FIFTH_RANK_WINNING_MONEY),
-    MISS(0, Constant.MISS_WINNING_MONEY),
+    FIRST(MATCH_COUNT_FIRST, Constant.FIRST_RANK_WINNING_MONEY),
+    SECOND(MATCH_COUNT_SECOND, Constant.SECOND_RANK_WINNING_MONEY),
+    THIRD(MATCH_COUNT_THIRD, Constant.THIRD_RANK_WINNING_MONEY),
+    FOURTH(MATCH_COUNT_FOURTH, Constant.FOURTH_RANK_WINNING_MONEY),
+    FIFTH(MATCH_COUNT_FIFTH, Constant.FIFTH_RANK_WINNING_MONEY),
+    MISS(MATCH_COUNT_MISS, Constant.MISS_WINNING_MONEY),
     ;
 
     companion object {
@@ -17,10 +24,10 @@ enum class Rank(val countOfMatch: Int, val winningAmount: Int) {
             bonusMatched: Boolean,
         ): Rank {
             return when (countOfMatch) {
-                6 -> FIRST
-                5 -> SECOND.takeIf { bonusMatched } ?: THIRD
-                4 -> FOURTH
-                3 -> FIFTH
+                MATCH_COUNT_FIRST -> FIRST
+                MATCH_COUNT_SECOND -> SECOND.takeIf { bonusMatched } ?: THIRD
+                MATCH_COUNT_FOURTH -> FOURTH
+                MATCH_COUNT_FIFTH -> FIFTH
                 else -> MISS
             }
         }
