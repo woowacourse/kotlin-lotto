cf0638469d05b39201b3829ba7cda20eabb85d9b
songpink <jejeje04@naver.com>
2024-02-23 22:17:19 +0900

    test(LottoResultTest): 두 테스트케이스 통합
    Rank에 대한 당첨 개수를 잘 반환하는지를 테스트하는 코드이므로 하나로 통합하되 개수가 0인 케이스를 추가해 null이 아닌 0이 제대로 반환되는지를 테스트하였습니다.

--- src/test/kotlin/model/LottoResultTest.kt
+++ src/test/kotlin/model/LottoResultTest.kt
@@ -1,28 +1,19 @@
 package model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class LottoResultTest {
-    @Test
-    fun `Map의 내부의 value에 null이 있을 경우 0을 반환한다`() {
-        val rankMap = emptyMap<Rank, Int>()
-        val lottoResult = LottoResult(rankMap)
-        val actual = lottoResult.getNum(Rank.THIRD)
-        assertThat(actual).isEqualTo(0)
-    }
-
     @ParameterizedTest
     @CsvSource(
-        "FIRST, 1",
+        "FIRST, 0",
         "SECOND, 2",
-        "THIRD, 3",
+        "THIRD, 1",
         "FOURTH, 4",
-        "FIFTH, 5",
+        "FIFTH, 2",
     )
-    fun `Map의 내부의 value가 null이 아닐 경우 제대로 반환한다`(
+    fun `Map에서 Rank에 대한 당첨 개수를 반환한다`(
         rank: Rank,
         expected: Int,
     ) {
