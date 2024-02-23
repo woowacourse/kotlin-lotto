975d23b0eed62c9cd6cc3d0610cd772a234dfdad
JoYehyun <jo990714@naver.com>
2024-02-23 16:02:09 +0900

    test: 당첨 등수 반환 테스트 파라미터 추가

--- src/test/kotlin/lotto/util/WinningRankTest.kt
+++ src/test/kotlin/lotto/util/WinningRankTest.kt
@@ -6,7 +6,15 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class WinningRankTest {
     @ParameterizedTest
-    @CsvSource("6,False,FIRST", "5,True,SECOND", "5,False,THIRD", "2,False,MISS")
+    @CsvSource(
+        "6,False,FIRST",
+        "5,True,SECOND",
+        "5,False,THIRD",
+        "4,False,FOURTH",
+        "3,True,FIFTH",
+        "2,False,MISS",
+        "1,False,MISS",
+    )
     fun `당첨 번호 일치 개수에 따른 등수 반환`(
         matchCount: Int,
         matchBonus: Boolean,
