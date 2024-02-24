14896fe1af6faa39221ff276906009553063ce8a
songpink <jejeje04@naver.com>
2024-02-24 21:02:07 +0900

    test(LottoPurchaseTest): 테스트코드 내 로직이 없도록 수정

--- src/test/kotlin/lotto/model/LottoPurchaseTest.kt
+++ src/test/kotlin/lotto/model/LottoPurchaseTest.kt
@@ -2,13 +2,20 @@ package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoPurchaseTest {
     @ParameterizedTest
-    @ValueSource(ints = [1000, 5000, 14000, 35000])
-    fun `구입 금액 만큼 로또를 산다 (개수)`(purchased: Int) {
-        val actual = LottoPurchase(purchased)
-        assertThat(actual.makeUserTickets().size).isEqualTo(purchased / LottoPurchase.PRICE_OF_LOTTO_TICKET)
+    @CsvSource(
+        "5000, 5",
+        "1000, 1",
+        "15000, 15",
+    )
+    fun `구입 금액 만큼의 개수만큼 로또를 산다`(
+        purchasedAmount: Int,
+        purchasedCount: Int,
+    ) {
+        val lottoPurchase = LottoPurchase(purchasedAmount)
+        assertThat(lottoPurchase.makeUserTickets().size).isEqualTo(purchasedCount)
     }
 }
