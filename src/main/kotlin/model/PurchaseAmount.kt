2a0a42ddd1c9b67564de8949042caa20b74d6bf3
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-02-25 00:29:54 +0900

    refactor: PurchaseAmount 로또 가격 외부 주입"

--- src/main/kotlin/model/PurchaseAmount.kt
+++ src/main/kotlin/model/PurchaseAmount.kt
@@ -1,6 +1,8 @@
 package lotto.model
 
-class PurchaseAmount() {
+import lotto.constants.GameConstant.PURCHASE_UNIT
+
+class PurchaseAmount(private val ticketPrice: Int = PURCHASE_UNIT) {
     fun getAmount(inputMoney: String): Int {
         val amount =
             inputMoney.toIntOrNull()
@@ -10,13 +12,12 @@ class PurchaseAmount() {
     }
 
     private fun validateNumberRange(amount: Int) {
-        require(amount >= PURCHASE_UNIT) { ERROR_NUMBER_RANGE_MESSAGE }
+        require(amount >= ticketPrice) { ERROR_NUMBER_RANGE_MESSAGE }
     }
 
     companion object {
         private const val ERROR_PREFIX = "[ERROR] "
         private const val ERROR_INPUT_TYPE_MESSAGE = "${ERROR_PREFIX}숫자만 입력 가능합니다."
-        private const val ERROR_NUMBER_RANGE_MESSAGE = "${ERROR_PREFIX}1000원 이상의 값만 입력 가능합니다."
-        private const val PURCHASE_UNIT = 1000
+        private const val ERROR_NUMBER_RANGE_MESSAGE = "${ERROR_PREFIX}${PURCHASE_UNIT}원 이상의 값만 입력 가능합니다."
     }
 }
--- src/test/kotlin/lotto/PurchaseAmountTest.kt
+++ src/test/kotlin/lotto/PurchaseAmountTest.kt
@@ -1,12 +1,15 @@
 package lotto
 
 import lotto.model.PurchaseAmount
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class PurchaseAmountTest {
-    private val purchaseAmount = PurchaseAmount()
+    private val purchaseUnit = 1000
+    private val purchaseUnitString = purchaseUnit.toString()
+    private val purchaseAmount = PurchaseAmount(purchaseUnit)
 
     @ParameterizedTest
     @ValueSource(strings = ["", "채123채", "!000", "10o0", "1ㅇㅇㅇ"])
@@ -18,5 +21,10 @@ class PurchaseAmountTest {
     @ValueSource(strings = ["-1000", "0", "100", "999"])
     fun `구입금액이 1000이상이 아닌경우 예외가 발생한다`(invalidAmount: String) {
         assertThrows<IllegalArgumentException> { purchaseAmount.getAmount(invalidAmount) }
+    }
+
+    @Test
+    fun `구입금액이 1000이상인 경우 예외가 발생하지 않는다`() {
+        assertThrows<IllegalArgumentException> { purchaseAmount.getAmount(purchaseUnitString) }
     }
 }
