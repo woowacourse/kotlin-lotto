dec597ff950cac1af66f5c97744cea02713c08a6
JunJangE <wnswkd486@gmail.com>
2024-02-24 18:46:33 +0900

    feat: LottoBundle append 기능

--- src/main/kotlin/lotto/model/LottoBundle.kt
+++ src/main/kotlin/lotto/model/LottoBundle.kt
@@ -2,4 +2,6 @@ package lotto.model
 
 data class LottoBundle(val lottos: List<Lotto>) {
     override fun toString(): String = lottos.joinToString("\n")
+
+    fun append(lottoBundle: LottoBundle): LottoBundle = copy(lottos = lottos + lottoBundle.lottos)
 }
