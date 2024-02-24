03697953d835d99c1bd0765e05babb977ee5024a
Hogu59 <dbstjdguswkd@naver.com>
2024-02-25 05:07:34 +0900

    test(lotteries): add one more test
    name of previous test was ambiguous.
    so test name was changed and one more test was added.

--- src/test/kotlin/model/LotteriesTest.kt
+++ src/test/kotlin/model/LotteriesTest.kt
@@ -2,10 +2,23 @@ package model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class LotteriesTest {
     @Test
-    fun `로또 여러 장을 저장할 수 있다`() {
+    fun `2장 이상의 로또를 저장할 수 있다`() {
+        assertDoesNotThrow {
+            Lotteries(
+                listOf(
+                    Lottery.of(1, 2, 3, 4, 5, 6),
+                    Lottery.of(1, 2, 3, 4, 5, 6),
+                ),
+            )
+        }
+    }
+
+    @Test
+    fun `로또가 2장 저장되면 lotteries의 크기는 2이다`() {
         val lotteries =
             Lotteries(
                 listOf(
