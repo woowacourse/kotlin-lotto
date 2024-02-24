1f0253ae314cd12d350072ca587ec39a33861598
somin <salth0601@gmail.com>
2024-02-24 19:47:43 +0900

    test: 테스트 이름 수정

--- src/test/kotlin/lotto/domain/CashierTest.kt
+++ src/test/kotlin/lotto/domain/CashierTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 
 class CashierTest {
     @Test
-    fun name() {
+    fun `입력된 금액을 계산해 적절한 티켓 수량으로 반환하는지 테스트`() {
         val result = Cashier().toTicketQuantity(Money(10000))
         assertThat(result).isEqualTo(10)
     }
