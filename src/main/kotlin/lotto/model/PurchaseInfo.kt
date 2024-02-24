54c45c707138832ceb833d87c79b8b6e29cf3be7
kimhm0728 <kimhm0728@naver.com>
2024-02-24 17:34:31 +0900

    test: 구매 금액 유효성 검사 테스트

--- src/main/kotlin/lotto/model/PurchaseInfo.kt
+++ src/main/kotlin/lotto/model/PurchaseInfo.kt
@@ -4,15 +4,13 @@ class PurchaseInfo(val price: Int) {
     val amount: Int
 
     init {
-        require(price.isMoreThanMin() && price.divideByLottoPrice()) { INVALID_DIVIDE_LOTTO_PRICE }
+        require(price.divideByLottoPrice()) { INVALID_DIVIDE_LOTTO_PRICE }
         amount = price / Lotto.PRICE
     }
-
-    private fun Int.isMoreThanMin() = this >= Lotto.PRICE
 
     private fun Int.divideByLottoPrice() = this % Lotto.PRICE == 0
 
     companion object {
-        private const val INVALID_DIVIDE_LOTTO_PRICE = "${Lotto.PRICE} 단위로 입력해 주세요."
+        private const val INVALID_DIVIDE_LOTTO_PRICE = "${Lotto.PRICE}원 단위로 입력해 주세요."
     }
 }
--- src/test/kotlin/lotto/PurchaseInfoTest.kt
+++ src/test/kotlin/lotto/PurchaseInfoTest.kt
@@ -2,28 +2,34 @@ package lotto
 
 import lotto.model.PurchaseInfo
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.ValueSource
 
 class PurchaseInfoTest {
     @ParameterizedTest
-    @CsvSource("1000, 1", "10000, 10")
-    fun `구매할 수 있는 로또의 개수 계산한다`(
-        purchasePrice: Int,
-        amount: Int,
-    ) {
-        val purchaseInfo = PurchaseInfo(purchasePrice)
-        Assertions.assertThat(purchaseInfo.amount).isEqualTo(amount)
-    }
-
-    @ParameterizedTest
-    @EmptySource
     @ValueSource(strings = ["0", "-1000", "1000100", "100", "2200000000"])
-    fun `로또의 구입 금액을 검증한다`(purchasePrice: Int) =
+    fun `구매 금액이 잘못된 경우 예외가 발생한다`(purchasePrice: Int) =
         assertThrows<IllegalArgumentException> {
             PurchaseInfo(purchasePrice)
         }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1000", "14000", "30000"])
+    fun `구매 금액이 올바른 경우 예외가 발생하지 않는다`(purchasePrice: Int) =
+        assertDoesNotThrow {
+            PurchaseInfo(purchasePrice)
+        }
+
+    @ParameterizedTest
+    @CsvSource("1000, 1", "10000, 10")
+    fun `구매할 수 있는 로또의 개수를 계산한다`(
+        purchasePrice: Int,
+        expected: Int,
+    ) {
+        val actual = PurchaseInfo(purchasePrice).amount
+        Assertions.assertThat(actual).isEqualTo(expected)
+    }
 }
