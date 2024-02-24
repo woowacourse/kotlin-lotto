e7be1482465b5da17f73943d8c3ce8013c46d198
songpink <jejeje04@naver.com>
2024-02-24 21:07:06 +0900

    test(LottoTicketTest): 로직에 맞지 않는 테스트코드 제거

--- src/test/kotlin/lotto/model/UserLottoTicketTest.kt
+++ src/test/kotlin/lotto/model/UserLottoTicketTest.kt
@@ -10,11 +10,4 @@ class UserLottoTicketTest {
             UserLottoTicket(listOf(1, 2, 3, 4, 5, 6, 7).map { LottoNumber((it)) })
         }
     }
-
-    @Test
-    fun `로또 번호는 1 ~ 45 이어야 한다`() {
-        assertThrows<IllegalArgumentException> {
-            UserLottoTicket(listOf(0, 99, 377, 422, 511, 642).map { LottoNumber((it)) })
-        }
-    }
 }
