58b54f8edc0d8c06e6b6b385b53add0f3ace477d
aprilgom <aprilgom@gmail.com>
2024-02-24 16:19:28 +0900

    refactor(ConsoleInputView): 메시지 상수화

--- src/main/kotlin/view/ConsoleInputView.kt
+++ src/main/kotlin/view/ConsoleInputView.kt
@@ -2,17 +2,23 @@ package view
 
 class ConsoleInputView : InputView {
     override fun getPurchasePrice(): Int {
-        println("구입금액을 입력해 주세요.")
+        println(GET_PURCHASE_PRICE_MESSAGE)
         return readln().toInt()
     }
 
     override fun getWinningTicket(): List<Int> {
-        println("지난 주 당첨 번호를 입력해 주세요.")
+        println(GET_WINNING_TICKET_MESSAGE)
         return readln().split(",").map { it.trim().toInt() }
     }
 
     override fun getBonusNumber(): Int {
-        println("보너스 볼을 입력해 주세요.")
+        println(GET_BONUS_NUMBER_MESSAGE)
         return readln().toInt()
+    }
+
+    companion object {
+        private const val GET_PURCHASE_PRICE_MESSAGE = "구입금액을 입력해 주세요."
+        private const val GET_WINNING_TICKET_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+        private const val GET_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
     }
 }
