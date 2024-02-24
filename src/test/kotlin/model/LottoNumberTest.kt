f221a11d29eaed29445c7eae9c0c1bd7a976a8ab
songpink <jejeje04@naver.com>
2024-02-24 15:44:23 +0900

    test(LottoNumberTest): 로또 번호가 허용 범위를 벗어나면 예외를 발생시키는지 테스트 구현

---
+++ src/test/kotlin/model/LottoNumberTest.kt
@@ -0,0 +1,15 @@
+package model
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoNumberTest {
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0, 46, 100])
+    fun `로또 번호가 허용 범위를 벗어나면 예외를 발생시킨다`(num: Int) {
+        assertThrows<IllegalArgumentException> {
+            LottoNumber(num)
+        }
+    }
+}
