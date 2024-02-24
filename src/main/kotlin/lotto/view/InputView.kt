a208b01d78741c70f5d5ba3c644bb7c3a758f701
jaeyeongjo <nos05075@naver.com>
2024-02-25 00:28:34 +0900

    refactor: InputView 개선

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,21 +6,18 @@ object InputView {
     fun reedBuyPrice(): Int {
         println("구입금액을 입력해 주세요.")
         val input = readlnOrNull().orEmpty()
-        val buyPrice = input.toIntOrNull() ?: INVALID_INPUT
-        return buyPrice
+        return input.toIntOrNull() ?: INVALID_INPUT
     }
 
     fun readLottoWinningNumbers(): List<Int> {
         println("지난 주 당첨번호를 입력해 주세요.")
         val input = readlnOrNull().orEmpty()
-        val winningNumbers = input.split(",").map { it.toIntOrNull() ?: INVALID_INPUT }
-        return winningNumbers
+        return input.split(",").map { it.toIntOrNull() ?: INVALID_INPUT }
     }
 
     fun readLottoBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
         val input = readlnOrNull().orEmpty()
-        val bonusNumber = input.toIntOrNull() ?: INVALID_INPUT
-        return bonusNumber
+        return input.toIntOrNull() ?: INVALID_INPUT
     }
 }
