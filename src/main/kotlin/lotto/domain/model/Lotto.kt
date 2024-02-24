e200afc5d95cc089e22f674da4ada26a4e0927ce
somin <salth0601@gmail.com>
2024-02-24 22:09:32 +0900

    feat: Lotto 클래스에 확장 함수 toLotto 추가 - Lotto 객체 생성 시 오름차 순 정렬 - 로또 사이즈 만큼 슬라이스

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -1,12 +1,17 @@
 package lotto.domain.model
 
-class Lotto(val numbers: List<LottoNumber>) {
+class Lotto(numbers: List<LottoNumber>) {
+    val numbers: List<LottoNumber> = numbers.sortedBy { it.value }
+
     init {
         require(numbers.size == LOTTO_NUMBER_SIZE) { NUMBER_SIZE_EXCEPTION_MESSAGE }
         require(numbers.toSet().size == LOTTO_NUMBER_SIZE) { DUPLICATED_NUMBER_EXCEPTION_MESSAGE }
     }
 
     companion object {
+        fun List<Int>.toLotto(): Lotto =
+            this.take(LOTTO_NUMBER_SIZE).map { LottoNumber(it) }.run { Lotto(this) }
+
         private const val LOTTO_NUMBER_SIZE = 6
         private const val NUMBER_SIZE_EXCEPTION_MESSAGE = "로또 번호는 ${LOTTO_NUMBER_SIZE}개여야 합니다."
         private const val DUPLICATED_NUMBER_EXCEPTION_MESSAGE = "로또 번호끼리는 중복되면 안됩니다."
