4a76b2f07fb7a21bca1d875dc2cba7a9d76ec251
jaeyeongjo <nos05075@naver.com>
2024-02-24 21:55:29 +0900

    refactor: LottoNumber 클래스 개선
    value class로 전환

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,36 +1,21 @@
 package lotto.model
 
-class LottoNumber private constructor(val number: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LottoNumber
-
-        if (number != other.number) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return number
-    }
-
-    override fun toString(): String {
-        return number.toString()
-    }
+@JvmInline
+value class LottoNumber private constructor(private val number: Int) {
+    override fun toString(): String = number.toString()
 
     companion object {
-        private const val LOTTO_RANGE_ERROR_MESSAGE = "로또의 숫자들은 1부터 45까지의 숫자로 구성되어야 합니다."
         private const val MIN_LOTTO_NUMBER = 1
         private const val MAX_LOTTO_NUMBER = 45
+        private const val LOTTO_RANGE_ERROR_MESSAGE =
+            "로또의 숫자들은 ${MIN_LOTTO_NUMBER}부터 ${MAX_LOTTO_NUMBER}까지의 숫자로 구성되어야 합니다."
+
         val LOTTO_NUMBER_RANGE = MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER
 
-        private val cache = (LOTTO_NUMBER_RANGE).associateWith { LottoNumber(it) }
+        private val cache = LOTTO_NUMBER_RANGE.associateWith { LottoNumber(it) }
 
         fun of(number: Int): LottoNumber {
-            require(number in LOTTO_NUMBER_RANGE) { LOTTO_RANGE_ERROR_MESSAGE }
-            return cache[number]!!
+            return cache[number] ?: throw IllegalArgumentException(LOTTO_RANGE_ERROR_MESSAGE)
         }
     }
 }
