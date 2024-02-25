cd749e65e893d69b581c7e10f68c3d9677540abf
JunJangE <wnswkd486@gmail.com>
2024-02-25 11:37:58 +0900

    refactor: 출력 문구 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,8 +5,7 @@ import lotto.model.LottoBundle
 import lotto.model.LottoResult
 
 object OutputView {
-    private const val LOTTO_SIZE_MESSAGE = "%d개를 구매했습니다."
-    private const val WINNING_STATISTICS_MESSAGE = "당첨 통계\n---------\n"
+    private const val WINNING_STATISTICS_MESSAGE = "\n당첨 통계\n---------"
     private const val TOTAL_RETURN_MESSAGE = "총 수익률은 %.2f입니다."
 
     fun printLottoBundle(lottoBundle: LottoBundle) {
