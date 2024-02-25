3283cc8320131c4395106cce4535ff34c931fefd
haeum808 <haeumueah@gmail.com>
2024-02-25 17:00:58 +0900

    refactor: Money의 예외 메시지에 얼마를 넣어서 예외가 나왔는지 확인 할 수 있게 변경

--- src/main/kotlin/domain/model/Money.kt
+++ src/main/kotlin/domain/model/Money.kt
@@ -2,11 +2,14 @@ package domain.model
 
 data class Money(val amount: Long) {
     init {
-        require(amount >= ZERO) { MINIMUM_VALUE_EXCEPTION_MESSAGE }
+        require(amount >= MINIMUM_AMOUNT) { "${amount}원은 안됩니다. ${MINIMUM_AMOUNT}원 이상이어야 합니다." }
+    }
+
+    operator fun compareTo(other: Money): Int {
+        return this.amount.compareTo(other.amount)
     }
 
     companion object {
-        const val ZERO = 0
-        const val MINIMUM_VALUE_EXCEPTION_MESSAGE = " ${ZERO}원 이상이어야 합니다."
+        private const val MINIMUM_AMOUNT = 0L
     }
 }
--- src/test/kotlin/domain/model/MoneyTest.kt
+++ src/test/kotlin/domain/model/MoneyTest.kt
@@ -1,18 +1,16 @@
 package domain.model
 
-import domain.model.Money.Companion.MINIMUM_VALUE_EXCEPTION_MESSAGE
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class MoneyTest {
+    @Test
+    fun `돈이 0원 미만이면 예외를 던지는지`() {
+        val amount = -1L
 
-    @ParameterizedTest
-    @ValueSource(longs = [-1, -999])
-    fun `구입 금액이 0원 미만이면 예외를 던진다`(value: Long) {
-        val exception = assertThrows<IllegalArgumentException> { Money(value) }
+        val exception = assertThrows<IllegalArgumentException> { Money(amount) }
 
-        assertThat(exception.message).isEqualTo(MINIMUM_VALUE_EXCEPTION_MESSAGE)
+        assertThat(exception.message).isEqualTo("-1원은 안됩니다. 0원 이상이어야 합니다.")
     }
 }
