f8218fe87e889fd40367c4c22228988052b7c0e3
murjune <sis9221302@gmail.com>
2024-02-25 14:37:44 +0900

    refactor : MoneyTest 매직 넘버 상수화

--- src/test/kotlin/model/MoneyTest.kt
+++ src/test/kotlin/model/MoneyTest.kt
@@ -8,13 +8,18 @@ class MoneyTest {
     @Test
     fun `Money는 음수일 수 없다`() {
         assertThrows<IllegalArgumentException> {
-            Money(-1)
+            Money(MINUS_MONEY_AMOUNT)
         }
     }
 
     @Test
     fun `Money를 Money로 나누면 정수 몫이 나온다`() {
-        val count = Money(10) / Money(10)
+        val count = Money(GENERAL_MONEY_AMOUNT) / Money(GENERAL_MONEY_AMOUNT)
         assertThat(count).isEqualTo(1)
+    }
+
+    private companion object {
+        const val MINUS_MONEY_AMOUNT = -1
+        const val GENERAL_MONEY_AMOUNT = 1_000
     }
 }
