9e3cb1a3878c98ed46f82f20b636a25dd426ac83
dpcks0509 <dpcks0509@gmail.com>
2024-02-25 22:37:12 +0900

    refactor: winningAmount 상수 선언 해제

--- src/main/kotlin/model/Rank.kt
+++ src/main/kotlin/model/Rank.kt
@@ -1,14 +1,12 @@
 package model
 
-import util.Constant
-
 enum class Rank(val countOfMatch: Int, val winningAmount: Int) {
-    FIRST(6, Constant.FIRST_RANK_WINNING_MONEY),
-    SECOND(5, Constant.SECOND_RANK_WINNING_MONEY),
-    THIRD(5, Constant.THIRD_RANK_WINNING_MONEY),
-    FOURTH(4, Constant.FOURTH_RANK_WINNING_MONEY),
-    FIFTH(3, Constant.FIFTH_RANK_WINNING_MONEY),
-    MISS(0, Constant.MISS_WINNING_MONEY), ;
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0), ;
 
     companion object {
         fun getRank(
--- src/main/kotlin/util/Constant.kt
+++ src/main/kotlin/util/Constant.kt
@@ -5,10 +5,4 @@ object Constant {
     const val LOTTO_START_RANGE = 1
     const val LOTTO_END_RANGE = 45
     const val LOTTO_SIZE = 6
-    const val FIRST_RANK_WINNING_MONEY = 2_000_000_000
-    const val SECOND_RANK_WINNING_MONEY = 30_000_000
-    const val THIRD_RANK_WINNING_MONEY = 1_500_000
-    const val FOURTH_RANK_WINNING_MONEY = 50_000
-    const val FIFTH_RANK_WINNING_MONEY = 5_000
-    const val MISS_WINNING_MONEY = 0
 }
