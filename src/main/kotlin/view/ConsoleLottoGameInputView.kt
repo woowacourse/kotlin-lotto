2ffdcb5342d03df90209e26339409a0b801bc9ea
Gimun Kim <kmkim7575@gmail.com>
2024-02-25 13:23:23 +0900

    chore: 사용자가 아무런 값 없이 입력을 수행하였을 때의 예외 메시지 구체화

--- src/main/kotlin/view/ConsoleLottoGameInputView.kt
+++ src/main/kotlin/view/ConsoleLottoGameInputView.kt
@@ -25,8 +25,9 @@ class ConsoleLottoGameInputView : LottoGameInputView {
         private const val MESSAGE_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
         private const val DELIMITER = ","
-        private const val EXCEPTION_PURCHASE_EXPENSE = "로또 구매 금액은 정수여야 합니다."
-        private const val EXCEPTION_WINNING_NUMBER = "당첨 번호는 정수여야 합니다."
-        private const val EXCEPTION_BONUS_NUMBER = "보너스 번호는 정수여야 합니다."
+        private const val EXCEPTION_NULL = "을(를) 입력하지 않았습니다."
+        private const val EXCEPTION_PURCHASE_EXPENSE = "로또 구매 금액$EXCEPTION_NULL"
+        private const val EXCEPTION_WINNING_NUMBER = "당첨 번호$EXCEPTION_NULL"
+        private const val EXCEPTION_BONUS_NUMBER = "보너스 번호$EXCEPTION_NULL"
     }
 }
