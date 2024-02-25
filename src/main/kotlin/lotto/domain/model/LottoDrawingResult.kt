47f80ea1e950f25333f65b3ef06e4871fab68d2a
somin <salth0601@gmail.com>
2024-02-25 12:52:52 +0900

    refactor: LottoDrawingResult 생성을 위한 toLottoDrawingResult 확장 함수 구현

--- src/main/kotlin/lotto/domain/model/LottoDrawingResult.kt
+++ src/main/kotlin/lotto/domain/model/LottoDrawingResult.kt
@@ -1,3 +1,10 @@
 package lotto.domain.model
 
-data class LottoDrawingResult(val statistics: Map<Rank, Int>)
+data class LottoDrawingResult(private val input: Map<Rank, Int>) {
+    val statistics: Map<Rank, Int> = Rank.entries.associateWith { rank -> input.getOrDefault(rank, DEFAULT_COUNT) }
+
+    companion object {
+        fun List<Rank>.toLottoDrawingResult() = LottoDrawingResult(this.groupingBy { rank -> rank }.eachCount())
+        private const val DEFAULT_COUNT = 0
+    }
+}
