a4f5dd3291c02f7b7a24ed91c7cd82c5559eac1a
murjune <sis9221302@gmail.com>
2024-02-26 16:39:41 +0900

    fix : BonusNumber Test

--- src/test/kotlin/model/BonusLottoNumberTest.kt
+++ src/test/kotlin/model/BonusLottoNumberTest.kt
@@ -8,7 +8,15 @@ class BonusLottoNumberTest {
     fun `보너스 넘버는 우승 로또 번호와 중복될 수 없다`() {
         // given
         val lottoNumber: LottoNumber = GeneralLottoNumber(6)
-        val winningLotto = Lotto(1, 2, 3, 4, 5, 6)
+        val winningLotto =
+            listOf(
+                GeneralLottoNumber(1),
+                GeneralLottoNumber(2),
+                GeneralLottoNumber(3),
+                GeneralLottoNumber(4),
+                GeneralLottoNumber(5),
+                GeneralLottoNumber(6),
+            )
         // when
         assertThrows<IllegalArgumentException> {
             BonusLottoNumber.of(lottoNumber, winningLotto)
