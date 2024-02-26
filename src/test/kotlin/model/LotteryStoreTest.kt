c025d430a88b1795861096ad958d550f6fcdb886
hevton <hevton22@gmail.com>
2024-02-26 17:35:26 +0900

    test(LotteryStoreTest): 수동/자동 구매를 함께 진행하여 티켓을 발행하는 전략

--- src/test/kotlin/model/LotteryStoreTest.kt
+++ src/test/kotlin/model/LotteryStoreTest.kt
@@ -1,10 +1,12 @@
 package model
 
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import utils.ExplicitTicketGenerationStrategy
+import utils.MixedTicketGenerationStrategy
 import utils.RandomTicketGenerationStrategy
 
 class LotteryStoreTest {
@@ -16,6 +18,28 @@ class LotteryStoreTest {
     ) {
         val lotteryStore = LotteryStore().setStrategy(RandomTicketGenerationStrategy(Amount.fromInput(input)))
         assertThat(lotteryStore.issueTicket().userLotteries.size).isEqualTo(count)
+    }
+
+    @Test
+    fun `수동 갯수를 지정했을 때, 로또가 잘 생성되는지 테스트`() {
+        val explicitAmount = Amount.fromInput("10000")
+        val explicitNumbers =
+            listOf(
+                "1, 2, 3, 4, 5, 6",
+                "7, 8, 9, 10, 11, 12",
+            )
+        val lotteryPurchasePattern = LotteryPurchasePattern.ofManual(explicitAmount, "2")
+        val lotteryStore =
+            LotteryStore().setStrategy(
+                MixedTicketGenerationStrategy(
+                    explicitAmount,
+                    explicitNumbers,
+                    lotteryPurchasePattern,
+                ),
+            )
+
+        val ticket = lotteryStore.issueTicket()
+        ticket.userLotteries.size shouldBe 10
     }
 
     @Test
