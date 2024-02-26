cf248072b9db9e7e2ca521cbb361e1b26bfc4ef4
somin <salth0601@gmail.com>
2024-02-26 14:20:45 +0900

    refactor: LottoGenerator 인터페이스 기본 구현, 불필요한 메서드 삭제

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -1,16 +1,7 @@
 package lotto.domain
 
 import lotto.domain.model.Lotto
-import lotto.domain.model.Lotto.Companion.toLotto
-import lotto.domain.model.LottoNumber.Companion.MAXIMUM_LOTTO_NUMBER
-import lotto.domain.model.LottoNumber.Companion.MINIMUM_LOTTO_NUMBER
 
 interface LottoGenerator {
-
-    fun make(start: Int = MINIMUM_LOTTO_NUMBER, endInclusive: Int = MAXIMUM_LOTTO_NUMBER): Lotto {
-        val numbers = (start..endInclusive).toList()
-        return manipulate(numbers).toLotto()
-    }
-
-    fun manipulate(numbers: List<Int>): List<Int>
+    fun make(): Lotto
 }
--- src/main/kotlin/lotto/domain/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/domain/RandomLottoGenerator.kt
@@ -1,7 +1,17 @@
 package lotto.domain
 
+import lotto.domain.model.Lotto
+import lotto.domain.model.Lotto.Companion.toLotto
+
 class RandomLottoGenerator : LottoGenerator {
-    override fun manipulate(numbers: List<Int>): List<Int> {
-        return numbers.shuffled()
+    private val lottoNumbers = (MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).toList()
+    override fun make(): Lotto {
+        return lottoNumbers.shuffled().take(LOTTO_SIZE).toLotto()
+    }
+
+    companion object {
+        const val MINIMUM_LOTTO_NUMBER = 1
+        const val MAXIMUM_LOTTO_NUMBER = 45
+        const val LOTTO_SIZE = 6
     }
 }
