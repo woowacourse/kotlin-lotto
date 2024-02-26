42188dcb19264750ab41681b26faaa40a2b5f159
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-26 15:16:24 +0900

    refect: Enum class 변화로 인한  상금 테스트코드 리펙토링

--- src/test/kotlin/LottoPrizeTest.kt
+++ src/test/kotlin/LottoPrizeTest.kt
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.Test
 
 class LottoPrizeTest {
     @Test
-    fun `당첨금 Enum Class`() {
-        assertThat(LottoPrize.FIFTH.getDetails())
-            .isEqualTo(Triple(3, 5_000, "3개 일치 (5,000원) -"))
-        assertThat(LottoPrize.FOURTH.getDetails())
-            .isEqualTo(Triple(4, 50_000, "4개 일치 (50,000원) -"))
-        assertThat(LottoPrize.THIRD.getDetails())
-            .isEqualTo(Triple(5, 1_500_000, "5개 일치 (1,500,000원) -"))
-        assertThat(LottoPrize.SECOND.getDetails())
-            .isEqualTo(Triple(5, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) -"))
-        assertThat(LottoPrize.FIRST.getDetails())
-            .isEqualTo(Triple(6, 2_000_000_000, "6개 일치 (2,000,000,000원) -"))
+    fun `당첨금 getMatchNumber Enum Class`() {
+        assertThat(LottoPrize.FIFTH.getMatchNumbers()).isEqualTo(3)
+        assertThat(LottoPrize.FOURTH.getMatchNumbers()).isEqualTo(4)
+        assertThat(LottoPrize.THIRD.getMatchNumbers()).isEqualTo(5)
+        assertThat(LottoPrize.SECOND.getMatchNumbers()).isEqualTo(5)
+        assertThat(LottoPrize.FIRST.getMatchNumbers()).isEqualTo(6)
+    }
+
+    @Test
+    fun `당첨금 getPrice Enum Class`() {
+        assertThat(LottoPrize.FIFTH.getPrice()).isEqualTo(5_000)
+        assertThat(LottoPrize.FOURTH.getPrice()).isEqualTo(50_000)
+        assertThat(LottoPrize.THIRD.getPrice()).isEqualTo(1_500_000)
+        assertThat(LottoPrize.SECOND.getPrice()).isEqualTo(30_000_000)
+        assertThat(LottoPrize.FIRST.getPrice()).isEqualTo(2_000_000_000)
     }
 }
