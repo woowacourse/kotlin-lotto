4e616f7ad6430bf7770a0b6ee3287cddecedb1e4
jaeyeongjo <nos05075@naver.com>
2024-02-26 16:58:02 +0900

    test: 테스트 코드 가독성 좋게 개선

--- src/test/kotlin/lotto/model/AutoLottoMachineTest.kt
+++ src/test/kotlin/lotto/model/AutoLottoMachineTest.kt
@@ -5,91 +5,25 @@ import org.junit.jupiter.api.Test
 
 class AutoLottoMachineTest {
     @Test
-    fun `사전에 정의된 범위로 로또를 생성한다`() {
-        val fixedNumbers =
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-            )
-        val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
-        val autoBuyedLottos =
-            AutoLottoMachine.createLottos(LottoBuyBudget(Lotto.PRICE_PER_LOTTO), fixedLottoNumbersGenerator).first()
-        val expectedLotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        assertThat(autoBuyedLottos).isEqualTo(expectedLotto)
-    }
+    fun `로또 한장의 가격이 5,000원일 때 2만원을 투입하면 총 4장의 로또를 사전에 약속된 숫자들로 발급하고 예산은 0이된다`() {
+        val lottoBuyBudget = LottoBuyBudget(20000, 5000)
 
-    @Test
-    fun `로또 한장의 가격이 5,000원일 때 2만원을 투입하면 총 4장의 로또를 발급한다`() {
-        val fixedNumbers =
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(7, 8, 9, 10, 11, 12),
-                listOf(14, 15, 16, 17, 18, 19),
-                listOf(21, 22, 23, 24, 25, 26),
-            )
+        val fixedNumbers = (1..24 step 6).map { (it..it + 5).toList() }
         val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
-        val autoBuyedLottos = AutoLottoMachine.createLottos(LottoBuyBudget(20000, 5000), fixedLottoNumbersGenerator)
-        assertThat(autoBuyedLottos).isEqualTo(
-            listOf(
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(7, 8, 9, 10, 11, 12),
-                Lotto.of(14, 15, 16, 17, 18, 19),
-                Lotto.of(21, 22, 23, 24, 25, 26),
-            ),
-        )
-    }
-
-    @Test
-    fun `로또 한장의 가격이 1,000원일 때 1만원을 투입하면 총 10장의 로또를 발급한다`() {
-        val fixedNumbers =
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(7, 8, 9, 10, 11, 12),
-                listOf(14, 15, 16, 17, 18, 19),
-                listOf(21, 22, 23, 24, 25, 26),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(7, 8, 9, 10, 11, 12),
-                listOf(14, 15, 16, 17, 18, 19),
-                listOf(21, 22, 23, 24, 25, 26),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(7, 8, 9, 10, 11, 12),
-            )
-        val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
-        val autoBuyedLottos = AutoLottoMachine.createLottos(LottoBuyBudget(10000, 1000), fixedLottoNumbersGenerator)
-        assertThat(autoBuyedLottos).isEqualTo(
-            listOf(
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(7, 8, 9, 10, 11, 12),
-                Lotto.of(14, 15, 16, 17, 18, 19),
-                Lotto.of(21, 22, 23, 24, 25, 26),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(7, 8, 9, 10, 11, 12),
-                Lotto.of(14, 15, 16, 17, 18, 19),
-                Lotto.of(21, 22, 23, 24, 25, 26),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(7, 8, 9, 10, 11, 12),
-            ),
-        )
-    }
-
-    @Test
-    fun `현재 예산으로 살 수 있는 만큼 로또를 생성한다 그 후 예산이 0이 된다`() {
-        val fixedNumbers =
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-            )
-        val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
-        val lottoBuyBudget = LottoBuyBudget(10000, 1000)
         val autoBuyedLottos = AutoLottoMachine.createLottos(lottoBuyBudget, fixedLottoNumbersGenerator)
-        assertThat(autoBuyedLottos.size).isEqualTo(10)
+        assertThat(autoBuyedLottos).isEqualTo(fixedNumbers.map { Lotto(it.map(LottoNumber::of)) })
+
+        assertThat(lottoBuyBudget.availableFunds).isEqualTo(0)
+    }
+
+    @Test
+    fun `로또 한장의 가격이 1,000원일 때 7천을 투입하면 총 7장의 로또를 사전에 약속된 숫자들로 발급하고 예산은 0이된다`() {
+        val lottoBuyBudget = LottoBuyBudget(7000, 1000)
+
+        val fixedNumbers = (1..42 step 6).map { (it..it + 5).toList() }
+        val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
+        val autoBuyedLottos = AutoLottoMachine.createLottos(lottoBuyBudget, fixedLottoNumbersGenerator)
+        assertThat(autoBuyedLottos).isEqualTo(fixedNumbers.map { Lotto(it.map(LottoNumber::of)) })
         assertThat(lottoBuyBudget.availableFunds).isEqualTo(0)
     }
 }
--- src/test/kotlin/lotto/model/ManualLottoMachineTest.kt
+++ src/test/kotlin/lotto/model/ManualLottoMachineTest.kt
@@ -26,13 +26,8 @@ class ManualLottoMachineTest {
 
     @Test
     fun `수동으로 입력된 숫자들로 로또가 생성되고 생성된 로또의 가격이 예산에서 차감된다`() {
-        val fixedNumbers =
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(7, 8, 9, 10, 11, 12),
-                listOf(13, 14, 15, 16, 17, 18),
-                listOf(19, 20, 21, 22, 23, 24),
-            )
+        val fixedNumbers = (1..24 step 6).map { (it..it + 5).toList() }
+
         val fixedLottoNumbersGenerator = FixedLottoNumbersGenerator(fixedNumbers)
 
         val lottoBuyBudget = LottoBuyBudget(20000, 1000)
@@ -41,12 +36,7 @@ class ManualLottoMachineTest {
             manualLottoMachine.createLottos(fixedLottoNumbersGenerator)
 
         Assertions.assertThat(manualBuyedLottos).isEqualTo(
-            listOf(
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(7, 8, 9, 10, 11, 12),
-                Lotto.of(13, 14, 15, 16, 17, 18),
-                Lotto.of(19, 20, 21, 22, 23, 24),
-            ),
+            fixedNumbers.map { Lotto(it.map(LottoNumber::of)) },
         )
         Assertions.assertThat(lottoBuyBudget.availableFunds).isEqualTo(16000)
     }
