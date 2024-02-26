759cc522765cfef208a9186619dc28e46bd083ce
jinuemong <jinwoo3661@naver.com>
2024-02-26 14:36:39 +0900

    fix: 출력 오류 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -6,8 +6,8 @@ import lotto.controller.inputWinning
 import lotto.model.LottoNumber
 
 const val REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요."
-const val REQUEST_WINNING_NUM = "당첨 번호를 입력해 주세요."
-const val REQUEST_BONUS_NUM = "보너스 번호를 입력해 주세요."
+const val REQUEST_WINNING_NUM = "지난 주 당첨 번호를 입력해 주세요."
+const val REQUEST_BONUS_NUM = "보너스 볼을 입력해 주세요."
 
 fun inputCostMessage(): Int {
     println(REQUEST_PURCHASE_PRICE)
