80ef72c40327909e7696b5bb53df1bd16c5be351
Gimun Kim <kmkim7575@gmail.com>
2024-02-26 11:47:50 +0900

    feat: 수동 로또 기능과 관련된 입력 기능 추가

--- src/main/kotlin/view/ConsoleLottoGameInputView.kt
+++ src/main/kotlin/view/ConsoleLottoGameInputView.kt
@@ -7,23 +7,43 @@ class ConsoleLottoGameInputView : LottoGameInputView {
             ?: throw IllegalArgumentException(EXCEPTION_NULL.format(EXCEPTION_PURCHASE_EXPENSE))
     }
 
+    override fun inputManualPurchaseCount(): Int {
+        println(MESSAGE_INPUT_MANUAL_COUNT)
+        return readln().toIntOrNull()
+            ?: throw IllegalArgumentException(EXCEPTION_NULL.format(EXCEPTION_MANUAL_LOTTO_COUNT))
+    }
+
+    override fun inputManualLotteries(count: Int): List<List<Int>> {
+        println(MESSAGE_INPUT_MANUAL_LOTTO_NUMBERS)
+        return mutableListOf<List<Int>>().apply {
+            repeat(count) { add(inputLotteryNumbers(EXCEPTION_MANUAL_LOTTO_NUMBERS)) }
+        }.toList()
+    }
+
     override fun inputWinningNumbers(): List<Int> {
         println(MESSAGE_INPUT_WINNING_NUMBERS)
+        return inputLotteryNumbers(EXCEPTION_WINNING_NUMBER)
+    }
+
+    override fun inputLotteryNumbers(message: String): List<Int> {
         val winningNumbers =
             readln().split(DELIMITER).map {
                 it.trim().toIntOrNull()
-                    ?: throw IllegalArgumentException(EXCEPTION_NULL.format(EXCEPTION_WINNING_NUMBER))
+                    ?: throw IllegalArgumentException(EXCEPTION_NULL.format(message))
             }
         return winningNumbers
     }
 
     override fun inputBonusNumber(): Int {
         println(MESSAGE_INPUT_BONUS_NUMBER)
-        return readln().toIntOrNull() ?: throw IllegalArgumentException(EXCEPTION_NULL.format(EXCEPTION_BONUS_NUMBER))
+        return readln().toIntOrNull()
+            ?: throw IllegalArgumentException(EXCEPTION_NULL.format(EXCEPTION_BONUS_NUMBER))
     }
 
     companion object {
         private const val MESSAGE_INPUT_PURCHASE_EXPENSE = "구입금액을 입력해 주세요."
+        private const val MESSAGE_INPUT_MANUAL_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
+        private const val MESSAGE_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요."
         private const val MESSAGE_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
         private const val DELIMITER = ","
@@ -31,5 +51,7 @@ class ConsoleLottoGameInputView : LottoGameInputView {
         private const val EXCEPTION_PURCHASE_EXPENSE = "로또 구매 금액"
         private const val EXCEPTION_WINNING_NUMBER = "당첨 번호"
         private const val EXCEPTION_BONUS_NUMBER = "보너스 번호"
+        private const val EXCEPTION_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또의 수"
+        private const val EXCEPTION_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 로또"
     }
 }
--- src/main/kotlin/view/LottoGameInputView.kt
+++ src/main/kotlin/view/LottoGameInputView.kt
@@ -3,6 +3,12 @@ package view
 interface LottoGameInputView {
     fun inputPurchaseExpense(): Int
 
+    fun inputManualPurchaseCount(): Int
+
+    fun inputManualLotteries(count: Int): List<List<Int>>
+
+    fun inputLotteryNumbers(message: String): List<Int>
+
     fun inputWinningNumbers(): List<Int>
 
     fun inputBonusNumber(): Int
