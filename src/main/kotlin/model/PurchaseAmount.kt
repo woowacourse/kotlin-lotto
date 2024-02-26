acba5a3a7c90d87ac6362f66d7da2d430089faf4
Namyunsuk <yunsuk0328@naver.com>
2024-02-26 17:01:20 +0900

    feat(PurchaseAmount): 수동으로 구매할 로또 수에 대한 유효성 검증 및 저장 기능 구현

--- src/main/kotlin/model/PurchaseAmount.kt
+++ src/main/kotlin/model/PurchaseAmount.kt
@@ -1,14 +1,17 @@
 package lotto.model
 
-class PurchaseAmount(val money: Int) {
+class PurchaseAmount(val money: Int, val numberOfManualLottos: Int) {
     init {
         validateNumberRange(money)
     }
 
-    constructor(money: String) : this(validateInputMoney(money))
+    constructor(money: String, numberOfManualLottos: String) : this(
+        validateInputMoney(money),
+        validateNumberOfManualLottos(numberOfManualLottos),
+    )
 
-    fun getNumberOfLottos(): Int {
-        return money / PURCHASE_UNIT
+    fun getNumberOfAutoLottos(): Int {
+        return (money / PURCHASE_UNIT) - numberOfManualLottos
     }
 
     companion object {
@@ -16,11 +19,19 @@ class PurchaseAmount(val money: Int) {
         private const val ERROR_INPUT_TYPE_MESSAGE = "${ERROR_PREFIX}숫자만 입력 가능합니다."
         private const val ERROR_NUMBER_RANGE_MESSAGE = "${ERROR_PREFIX}1000원 이상의 값만 입력 가능합니다."
         private const val PURCHASE_UNIT = 1000
+        private const val ERROR_MANUAL_NUMBER_RANGE_MESSAGE = "0이상의 수를 입력해주세요"
+        private const val MINIMUM_MANUAL_NUMBER_RANGE = 0
 
         private fun validateInputMoney(inputMoney: String): Int {
             validateIsDigit(inputMoney)
             validateNumberRange(inputMoney.toInt())
             return inputMoney.toInt()
+        }
+
+        private fun validateNumberOfManualLottos(numberOfManualLottos: String): Int {
+            validateIsDigit(numberOfManualLottos)
+            validateManualNumberRange(numberOfManualLottos.toInt())
+            return numberOfManualLottos.toInt()
         }
 
         private fun validateIsDigit(inputMoney: String) {
@@ -29,6 +40,10 @@ class PurchaseAmount(val money: Int) {
 
         private fun validateNumberRange(amount: Int) {
             require(amount >= PURCHASE_UNIT) { ERROR_NUMBER_RANGE_MESSAGE }
+        }
+
+        private fun validateManualNumberRange(numberOfManualLottos: Int) {
+            require(numberOfManualLottos >= MINIMUM_MANUAL_NUMBER_RANGE) { ERROR_MANUAL_NUMBER_RANGE_MESSAGE }
         }
     }
 }
