8c09bcf5765f3d64dcc192db763aec6bbf9ec2ab
김예찬 <yechan@gim-yechan-ui-noteubug.local>
2024-02-26 14:40:07 +0900

    refect: require 내부 메세지 추가

--- src/main/kotlin/lotto/model/WinningNumber.kt
+++ src/main/kotlin/lotto/model/WinningNumber.kt
@@ -5,7 +5,9 @@ class WinningNumber(
     private val bonusNumber: LottoNumber,
 ) {
     init {
-        require(!lotto.contains(bonusNumber))
+        require(!lotto.contains(bonusNumber)) {
+            "당첨번호에 들어가있는걸 보너스 볼로 지정할 수 없습니다."
+        }
     }
 
     fun getWinning(): Lotto {
