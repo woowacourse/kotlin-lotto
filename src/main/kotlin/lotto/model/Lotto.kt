565b620042a3459d360d791b2338f34446b8c16c
JoYehyun <jo990714@naver.com>
2024-02-27 14:30:52 +0900

    refactor: 로또 객체 코드 개선

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,13 +1,13 @@
 package lotto.model
 
-data class Lotto(private val numbers: Set<LottoNumber>) {
+data class Lotto(val numbers: Set<LottoNumber>) {
     init {
         require(numbers.size == NUMBER_COUNT) { "로또 번호는 중복되지 않는 6개의 수로 구성되어야 합니다." }
     }
 
     fun countMatchingNumbers(winningNumbers: Lotto): Int = numbers.intersect(winningNumbers.numbers).size
 
-    fun hasMatchingBonusNumbers(bonusNumber: LottoNumber): Boolean = bonusNumber in numbers
+    fun hasMatchingBonusNumbers(bonusNumber: LottoNumber): Boolean = numbers.contains(bonusNumber)
 
     override fun toString(): String = numbers.toList().sortedBy { it.number }.toString()
 
