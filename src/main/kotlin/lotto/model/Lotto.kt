609c578649e8d610a0595d6255127b67c0b92ec5
kimhm0728 <kimhm0728@naver.com>
2024-02-27 14:54:36 +0900

    refactor: Lotto 유효성 검증 로직 init으로 이동

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,6 +1,15 @@
 package lotto.model
 
 class Lotto private constructor(val numbers: List<LottoNumber>) {
+    init {
+        require(numbers.isValidSize()) { INVALID_SIZE }
+        require(numbers.isNotDuplicate()) { INVALID_DUPLICATION }
+    }
+
+    private fun List<LottoNumber>.isValidSize() = size == SIZE
+
+    private fun List<LottoNumber>.isNotDuplicate() = distinct().size == SIZE
+
     fun getMatchingCount(otherLotto: Lotto) = otherLotto.numbers.intersect(numbers).size
 
     fun contains(lottoNumber: LottoNumber) = numbers.contains(lottoNumber)
@@ -12,13 +21,7 @@ class Lotto private constructor(val numbers: List<LottoNumber>) {
         private const val INVALID_DUPLICATION = "중복되지 않는 로또 번호를 입력해 주세요."
 
         fun create(numbers: List<Int>): Lotto {
-            require(numbers.isValidSize()) { INVALID_SIZE }
-            require(numbers.isNotDuplicate()) { INVALID_DUPLICATION }
             return Lotto(numbers.sorted().map { LottoNumber(it) })
         }
-
-        private fun List<Int>.isValidSize() = size == SIZE
-
-        private fun List<Int>.isNotDuplicate() = distinct().size == SIZE
     }
 }
