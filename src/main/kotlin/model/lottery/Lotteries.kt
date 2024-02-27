a6ed319233800952a98b2c32f4f8e17e7204a7f7
sh1mj1 <7wlgns@gmail.com>
2024-02-27 17:59:37 +0900

    feat(Lotteries): 로또 뭉치의 최종 당첨 결과를 구한다

--- src/main/kotlin/model/lottery/Lotteries.kt
+++ src/main/kotlin/model/lottery/Lotteries.kt
@@ -1,5 +1,33 @@
 package model.lottery
 
+import WinningLottery
+import model.Quantity
+import model.WinningRank
+import model.WinningResult
+
 data class Lotteries(val lotteries: List<Lottery>) {
     operator fun plus(other: Lotteries): Lotteries = Lotteries(this.lotteries + other.lotteries)
+
+    fun evaluate(winningLottery: WinningLottery): WinningResult {
+        val winningResult = DEFAULT_WINNING_RESULT
+
+        lotteries.forEach {
+            val rank = winningLottery.evaluate(it)
+            winningResult[rank] = winningResult[rank]!! + Quantity(1)
+        }
+
+        return WinningResult(winningResult.toMap())
+    }
+
+    companion object {
+        private val DEFAULT_WINNING_RESULT =
+            mutableMapOf(
+                WinningRank.FIRST to Quantity(0),
+                WinningRank.SECOND to Quantity(0),
+                WinningRank.THIRD to Quantity(0),
+                WinningRank.FOURTH to Quantity(0),
+                WinningRank.FIFTH to Quantity(0),
+                WinningRank.NONE to Quantity(0),
+            )
+    }
 }
