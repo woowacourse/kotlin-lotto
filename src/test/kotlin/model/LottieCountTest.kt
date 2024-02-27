feecf711b2a08e860ebdd2f88f91b4e627c59df1
murjune <sis9221302@gmail.com>
2024-02-27 13:57:13 +0900

    test : Lotto count를 검사하고 반환하는 로직 검사

---
+++ src/test/kotlin/model/LottieCountTest.kt
@@ -0,0 +1,31 @@
+package model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class LottieCountTest {
+    @Test
+    fun `buying 금액으로 구매할 로또의 개수를 반환해준다`() {
+        // given
+        val purchaseExpense = Money(3_000)
+        val lottoPrice = Money(1_000)
+        val expected = 3
+        // when
+        val actual = purchaseExpense / lottoPrice
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `로또 count 보다 많은 로또 티켓 수는 발급할 수 없다`() {
+        // given
+        val lottoCount = 4
+        val purchaseExpense = Money(5_000)
+        val lottoPrice = Money(1_000)
+        // when
+        assertThrows<IllegalArgumentException> {
+            if (lottoCount < purchaseExpense / lottoPrice) throw IllegalArgumentException()
+        }
+    }
+}
