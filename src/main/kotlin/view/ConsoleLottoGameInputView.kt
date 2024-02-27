52b39fd0ec0dcf3019b97ea2a159006a1dd21691
murjune <sis9221302@gmail.com>
2024-02-27 17:50:46 +0900

    chore: 구매할 번호 입력 메세지 변경

--- src/main/kotlin/view/ConsoleLottoGameInputView.kt
+++ src/main/kotlin/view/ConsoleLottoGameInputView.kt
@@ -37,7 +37,7 @@ class ConsoleLottoGameInputView : LottoGameInputView {
     companion object {
         private const val MESSAGE_INPUT_PURCHASE_EXPENSE = "구입금액을 입력해 주세요."
         private const val MESSAGE_INPUT_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
-        private const val MESSAGE_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 로또 번호를 입력해 주세요."
+        private const val MESSAGE_INPUT_MANUAL_LOTTO_NUMBERS = "수동으로 구매할 번호를 입력해 주세요."
         private const val MESSAGE_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
         private const val DELIMITER = ","
