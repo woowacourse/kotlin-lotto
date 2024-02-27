63023f01e57f7acd9e64271bcfc7f75fec30a0c9
Junyoung-WON <wjyo60@gmail.com>
2024-02-27 20:11:58 +0900

    feat: 수동으로 발행할 로또의 개수 입력

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -11,6 +11,13 @@ object InputView {
         return input.toInt()
     }
 
+    fun getManualTicketCounts(): Int {
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+        val input = readln()
+        ValidationUtils.validateManualCounts(input)
+        return input.toInt()
+    }
+
     fun getWinningNumbers(): List<LottoNumber> {
         println("지난 주 당첨 번호를 입력해 주세요.")
         val input = readln()
