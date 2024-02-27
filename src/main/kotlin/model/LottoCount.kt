6e3658d25abc73ef793d66898051725be92c2a79
murjune <sis9221302@gmail.com>
2024-02-27 19:15:42 +0900

    feat : LottoCount 에서 로또 개수 세는 로직 변경

--- src/main/kotlin/model/LottoCount.kt
+++ src/main/kotlin/model/LottoCount.kt
@@ -1,8 +1,21 @@
 package model
 
 @JvmInline
-value class LottoCount(val amount: Int) {
+value class LottoCount private constructor(val amount: Int) {
     init {
         require(amount > 0) { "amount 는 0보다 커야함" }
+    }
+
+    companion object {
+        @JvmStatic
+        fun of(
+            count: Int,
+            lottoPrice: Money,
+            cost: Money,
+        ): LottoCount {
+            val totalLottiePrice = lottoPrice * count
+            check(cost >= (totalLottiePrice)) { "lottoPrice * count = $totalLottiePrice 는 cost = $cost 보다 클 수 없다 " }
+            return LottoCount(cost / totalLottiePrice)
+        }
     }
 }
