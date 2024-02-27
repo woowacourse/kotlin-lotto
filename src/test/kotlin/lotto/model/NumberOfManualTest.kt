b23a752afc7fecd61a88f247015bd9b8fc8787dd
Junyoung-WON <wjyo60@gmail.com>
2024-02-27 18:02:10 +0900

    test: 수동 발행 로또의 개수가 0개 이상인지 검증하는 테스트

--- src/test/kotlin/lotto/model/NumberOfManualTest.kt
+++ src/test/kotlin/lotto/model/NumberOfManualTest.kt
@@ -1,17 +1,26 @@
 package lotto.model
 
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class NumberOfManualTest {
     @ParameterizedTest
-    @CsvSource("1000:2", "3000:5", "10000:11", delimiter = ':')
-    fun `수동으로 발행할 로또의 개수는 구매한 로또 개수를 넘길 수 없다`(
-        purchaseAmounts: Int,
+    @CsvSource("0:2", "3:3", "10:11", delimiter = ':')
+    fun `수동으로 발행할 로또의 개수는 0 이상의 정수여야 한다`(
         manualCounts: Int,
+        totalCounts: Int,
     ) {
-        val ticketCounts = NumberOfTickets(purchaseAmounts).counts
-        assertThrows<IllegalArgumentException> { NumberOfManual(manualCounts, ticketCounts) }
+        assertDoesNotThrow { NumberOfManual(manualCounts, totalCounts) }
+    }
+
+    @ParameterizedTest
+    @CsvSource("2:1", "5:3", "11:10", delimiter = ':')
+    fun `수동으로 발행할 로또의 개수는 구매한 로또 개수를 넘길 수 없다`(
+        manualCounts: Int,
+        totalCounts: Int,
+    ) {
+        assertThrows<IllegalArgumentException> { NumberOfManual(manualCounts, totalCounts) }
     }
 }
