ea7b982797bc15473e8809c4fdcfdff111340812
murjune <sis9221302@gmail.com>
2024-02-27 19:00:01 +0900

    refactor : LottoCount 불필요한 함수들 삭제

--- src/main/kotlin/model/LottoCount.kt
+++ src/main/kotlin/model/LottoCount.kt
@@ -5,14 +5,4 @@ value class LottoCount(val amount: Int) {
     init {
         require(amount > 0) { "amount 는 0보다 커야함" }
     }
-
-    operator fun compareTo(o: LottoCount) = amount - o.amount
-
-    operator fun plus(o: LottoCount) = LottoCount(amount + o.amount)
-
-    operator fun minus(o: LottoCount) = LottoCount(amount - o.amount)
-
-    companion object {
-        val MAX = LottoCount(Int.MAX_VALUE)
-    }
 }
