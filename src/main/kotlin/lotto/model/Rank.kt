cbb3567675a2745865e1870cf691045a0a818be7
songpink <jejeje04@naver.com>
2024-02-27 19:43:02 +0900

    refactor(Rank): index대신 ordinal을 사용하는 것으로 변경

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -1,13 +1,13 @@
 package lotto.model
 
-enum class Rank(val index: Int, val countOfMatch: Int, val winningMoney: Int) {
-    FIRST(0, 6, 2_000_000_000),
-    SECOND(1, 5, 30_000_000),
-    THIRD(2, 5, 1_500_000),
-    FOURTH(3, 4, 50_000),
-    FIFTH(4, 3, 5_000),
-    SIXTH(5, 2, 1_000),
-    MISS(6, 0, 0), ;
+enum class Rank(val countOfMatch: Int, val winningMoney: Int) {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    SIXTH(2, 1_000),
+    MISS(0, 0), ;
 
     companion object {
         val bonusNeededRank = SECOND
@@ -19,7 +19,7 @@ enum class Rank(val index: Int, val countOfMatch: Int, val winningMoney: Int) {
             val rankEntries = enumValues<Rank>()
             return when (countOfMatch) {
                 bonusNeededRank.countOfMatch ->
-                    if (matchBonus) bonusNeededRank else rankEntries[bonusNeededRank.index + 1]
+                    if (matchBonus) bonusNeededRank else rankEntries[bonusNeededRank.ordinal + 1]
 
                 else -> entries.find { it.countOfMatch == countOfMatch } ?: MISS
             }
