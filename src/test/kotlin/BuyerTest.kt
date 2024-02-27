a8fd7af3585611f0a43dcf26a8beef4279833ccd
kkosang <eiwoqp77@gmail.com>
2024-02-28 00:52:46 +0900

    refactor: BuyerTest의 테스트 수정

--- src/test/kotlin/BuyerTest.kt
+++ src/test/kotlin/BuyerTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 class BuyerTest {
     @ParameterizedTest
     @CsvSource("8000,8", "1000,1", "15000,15")
-    fun `발행한 로또 수량 계산`(
+    fun `구매 금액 1000원당, 로또를 1장씩 구매할 수 있다`(
         purchaseAmount: Int,
         numberOfLotto: Int,
     ) {
