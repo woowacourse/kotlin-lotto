c97d2037b26dca01168d66518c014ea339c8b6ad
Junyoung-WON <wjyo60@gmail.com>
2024-02-27 20:13:10 +0900

    feat: 수동으로 발행할 로또 개수에 대한 입력 검증

--- src/main/kotlin/lotto/util/ValidationUtils.kt
+++ src/main/kotlin/lotto/util/ValidationUtils.kt
@@ -4,4 +4,8 @@ object ValidationUtils {
     fun validatePurchaseAmount(input: String) {
         requireNotNull(input.toIntOrNull()) { "구입 금액은 숫자로 입력해주세요." }
     }
+
+    fun validateManualCounts(input: String) {
+        requireNotNull(input.toIntOrNull()) { "수동으로 발행할 개수를 숫자로 입력해 주세요." }
+    }
 }
