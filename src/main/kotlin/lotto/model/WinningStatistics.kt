31a4502911ceba63677b530c2019246f3db5bbe9
kimhm0728 <kimhm0728@naver.com>
2024-02-28 00:22:51 +0900

    refactor: 전체 수익 금액 구하는 기능 분리

--- src/main/kotlin/lotto/model/WinningStatistics.kt
+++ src/main/kotlin/lotto/model/WinningStatistics.kt
@@ -2,14 +2,19 @@ package lotto.model
 
 class WinningStatistics(private val statistics: Map<LottoPrize, Int>) {
     fun calculateProfitRatio(purchasePrice: Int): ProfitRatio {
-        val totalPrizeAmount =
-            statistics
-                .map { (lottoPrize, count) -> lottoPrize.amount * count.toLong() }
-                .sum()
-        return ProfitRatio(totalPrizeAmount.toDouble() / purchasePrice)
+        val totalPrizeAmount = calculateTotalPrizeAmount()
+        return ProfitRatio(totalPrizeAmount / purchasePrice.toDouble())
     }
 
-    operator fun get(lottoPrize: LottoPrize) = statistics.getOrDefault(lottoPrize, DEFAULT_LOTTO_PRIZE_COUNT)
+    private fun calculateTotalPrizeAmount(): Long {
+        return statistics
+            .map { (lottoPrize, count) -> lottoPrize.amount * count.toLong() }
+            .sum()
+    }
+
+    operator fun get(lottoPrize: LottoPrize): Int {
+        return statistics.getOrDefault(lottoPrize, DEFAULT_LOTTO_PRIZE_COUNT)
+    }
 
     companion object {
         private const val DEFAULT_LOTTO_PRIZE_COUNT = 0
