2ee5bfcc3048eb6aaea41d7471c18e9c1d9cff8c
songpink <jejeje04@naver.com>
2024-02-28 00:30:46 +0900

    fix(Rank): 보너스번호 검증이 필요한 랭킹을 리스트로 저장하도록 변경
    보너스번호 검증이 필요한 랭킹이 여러개로 바뀔 수 있으므로 리스트로 여러개 넣을 수 있게 하였음(테스트 용도로 5등, 6등 당첨조건도 변경함)

--- src/test/kotlin/lotto/model/LottoWinningRankTest.kt
+++ src/test/kotlin/lotto/model/LottoWinningRankTest.kt
@@ -47,10 +47,10 @@ class LottoWinningRankTest {
     }
 
     @Test
-    fun `번호 3개 일치하면 5등이다`() {
+    fun `번호 3개 일치하고 보너스번호 있으면 5등이다`() {
         val userTicket = UserLottoTicket(listOf(1, 2, 3, 42, 43, 44).map { LottoNumber(it) })
         val userTickets = listOf(userTicket)
-        val bonusNumber = 45
+        val bonusNumber = 44
         val lottoWinningRank = LottoWinningRank(winningNumbers, LottoNumber(bonusNumber))
         val rankMap = lottoWinningRank.makeRankMap(userTickets)
         Assertions.assertThat(rankMap[Rank.FIFTH]).isEqualTo(1)
