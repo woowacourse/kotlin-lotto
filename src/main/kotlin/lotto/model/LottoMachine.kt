3dc4d0dad5cbffdf83d0f0b202b64da3ca24a691
Junyoung-WON <wjyo60@gmail.com>
2024-02-28 14:02:18 +0900

    refactor: issueTicket의 인자로 Lotto 리스트를 받아오도록 리팩터링

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -6,22 +6,14 @@ class LottoMachine(val ticketCounts: TicketCounts) {
     private val randomNumberGenerate =
         NumberGenerate { LottoNumber.NUMBER_RANGE.shuffled().take(Lotto.NUMBER_COUNT).sorted() }
 
-    fun issueTickets(manualLotto: List<List<Int>> = listOf()): List<Lotto> {
+    fun issueTickets(manualLotto: List<Lotto> = listOf()): List<Lotto> {
         val tickets = mutableListOf<Lotto>()
         if (ticketCounts.numberOfManual.counts > NumberOfManual.MIN_NUMBER_OF_MANUAL) {
-            tickets.addAll(issueManualTickets(manualLotto))
+            tickets.addAll(manualLotto)
         }
         tickets.addAll(
             issueAutomaticTickets(ticketCounts.numberOfTickets.counts - ticketCounts.numberOfManual.counts),
         )
-        return tickets
-    }
-
-    fun issueManualTickets(manualLotto: List<List<Int>>): List<Lotto> {
-        val tickets = mutableListOf<Lotto>()
-        repeat(ticketCounts.numberOfManual.counts) { idx ->
-            tickets.add(Lotto(manualLotto[idx].map { LottoNumber(it) }))
-        }
         return tickets
     }
 
