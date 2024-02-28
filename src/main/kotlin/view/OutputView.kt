5ea5423788c0a7053228d5d98eef1798e27bf66e
hxeyexn <kck85075@kumoh.ac.kr>
2024-02-29 00:09:57 +0900

    feat(OutputView): 구매한 로또 갯수 출력 형식 수정
    - 이전: 자동 갯수만 출력
    - 이후: 수동, 자동 로또 갯수 출력

--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -5,7 +5,7 @@ import model.Rank
 import model.WinningResult
 
 object OutputView {
-    private const val LOTTO_PURCHASED = "%d개를 구매했습니다."
+    private const val LOTTO_PURCHASED = "\n수동으로 %d장, 자동으로 %d개를 구매했습니다."
     private const val LOTTERY_DELIMITER = ", "
     private const val HEADER_STATS =
         "\n" +
@@ -13,7 +13,9 @@ object OutputView {
             "-----------"
 
     fun printTicketInfo(ticket: Ticket) {
-        println(LOTTO_PURCHASED.format(ticket.userLotteries.size))
+        val userLotteries = ticket.userLotteries
+
+        println(LOTTO_PURCHASED.format(userLotteries.count { it.isManual }, userLotteries.count { !it.isManual }))
         ticket.userLotteries.forEach { lottery ->
             print("[${lottery.toString(LOTTERY_DELIMITER)}]\n")
         }
