49f55107753bdc7c038a4398aa7b3d351cc8228c
haeum808 <haeum808@gmail.com>
2024-02-28 21:00:13 +0900

    chore: LottoMachine의 매직 넘버 상수화

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -10,8 +10,8 @@ class LottoMachine(
         get() = _lottoes
 
     init {
-        require(money >= Money(1000)) { "구입금액은 ${money.amount}보다 큰 ${1000}원 이상이어야 합니다." }
-        require(money.amount - manual * 1000 >= 0) { "수동 로또 ${manual}개를 ${money.amount}원으로 살 수 없습니다." }
+        require(money >= Money(LOTTO_PRICE)) { "구입금액은 ${money.amount}보다 큰 ${LOTTO_PRICE}원 이상이어야 합니다." }
+        require(money.amount - manual * LOTTO_PRICE >= 0) { "수동 로또 ${manual}개를 ${money.amount}원으로 살 수 없습니다." }
     }
 
     fun makeManualLotto(lottoNumbers: Lotto) {
@@ -23,5 +23,9 @@ class LottoMachine(
             val randomNumbers = (1..45).shuffled().take(6).sorted()
             _lottoes.add(Lotto(randomNumbers))
         }
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000L
     }
 }
