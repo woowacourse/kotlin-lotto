a9c3714032695a75de79c12a20849ea00ebd532d
hevton <hevton22@gmail.com>
2024-02-28 16:25:13 +0900

    feat(LotteryPurchasePattern): 수동 입력 갯수는 0 이상의 수여야 한다

--- src/main/kotlin/model/LotteryPurchasePattern.kt
+++ src/main/kotlin/model/LotteryPurchasePattern.kt
@@ -3,7 +3,8 @@ package model
 class LotteryPurchasePattern(val manualLottoCount: Int, val autoLottoCount: Int) {
     companion object {
         private const val LOTTERY_TICKET_PRICE = 1000
-        const val EXCEPTION_INSUFFICIENT_FUNDS = "금액보다 구매하려는 갯수가 더 많습니다"
+        private const val EXCEPTION_INSUFFICIENT_FUNDS = "금액보다 구매하려는 갯수가 더 많습니다"
+        private const val EXCEPTION_IS_NOT_POSITIVE = "0 이상의 수를 입력하셔야 합니다"
 
         fun ofManual(
             amount: Amount,
@@ -11,13 +12,18 @@ class LotteryPurchasePattern(val manualLottoCount: Int, val autoLottoCount: Int)
         ): LotteryPurchasePattern {
             val availableTicketCount = amount.money / LOTTERY_TICKET_PRICE
 
-            return manualInput.toInt().validateFund(amount.money).run {
+            return manualInput.toInt().validatePositive().validateFund(amount.money).run {
                 LotteryPurchasePattern(this, availableTicketCount - this)
             }
         }
 
         private fun String.toInt(): Int {
             return this.toIntOrNull() ?: throw IllegalArgumentException(Amount.EXCEPTION_IS_NOT_NUMBER)
+        }
+
+        private fun Int.validatePositive(): Int {
+            require(this >= 0) { EXCEPTION_IS_NOT_POSITIVE }
+            return this
         }
 
         private fun Int.validateFund(money: Int): Int {
