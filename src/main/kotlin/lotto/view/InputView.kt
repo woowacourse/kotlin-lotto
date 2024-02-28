9f2f6c38bdea5733a19ea2fdf91edd3d9c945680
haeum808 <haeum808@gmail.com>
2024-02-28 20:21:16 +0900

    feat: 수동으로 구매할 로또 수 입력 받는 기능 및 수동으로 구매할 번호 입력 받는 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -10,6 +10,23 @@ class InputView {
         }
     }
 
+    fun readManualLottoAmount(): Int {
+        return try {
+            println("\n수동으로 구매할 로또 수를 입력해 주세요.")
+            readln().toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(INVALID_FORMAT_EXCEPTION_MESSAGE)
+        }
+    }
+
+    fun readManualLottos(): List<Int> {
+        return try {
+            readln().split(",").map { it.trim().toInt() }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException(INVALID_FORMAT_EXCEPTION_MESSAGE)
+        }
+    }
+
     fun readWinningNumbers(): List<Int> {
         return try {
             println("\n지난 주 당첨 번호를 입력해 주세요.")
