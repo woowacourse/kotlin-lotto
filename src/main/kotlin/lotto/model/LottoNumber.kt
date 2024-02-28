a1cd9013b092bd0fd0787db15ca4fe45b4bb39dd
kimhm0728 <kimhm0728@naver.com>
2024-02-28 18:53:01 +0900

    rename: LottoNumber의 인스턴스를 저장하는 맵 이름 변경

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -7,15 +7,14 @@ class LottoNumber private constructor(private val number: Int) {
         const val MIN_RANGE = 1
         const val MAX_RANGE = 45
         private const val INVALID_RANGE = "${MIN_RANGE}부터 $MAX_RANGE 사이의 숫자를 입력해 주세요."
-        private val lottoNumbers = mutableMapOf<Int, LottoNumber>()
+        private val NUMBERS = mutableMapOf<Int, LottoNumber>()
 
         fun from(number: Int): LottoNumber {
             require(number in MIN_RANGE..MAX_RANGE) { INVALID_RANGE }
-            if (!lottoNumbers.contains(number)) {
-                lottoNumbers[number] = LottoNumber(number)
+            if (!NUMBERS.contains(number)) {
+                NUMBERS[number] = LottoNumber(number)
             }
-
-            return lottoNumbers[number] ?: throw IllegalArgumentException()
+            return NUMBERS[number] ?: throw IllegalArgumentException()
         }
     }
 }
