b8091069b22dbe2ee2218b4147cf01d153cea8da
gaeun <geun5744@gmail.com>
2024-02-29 21:50:39 +0900

    feat: 수동으로 구매할 로또 수 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -13,6 +13,14 @@ class InputView {
             purchasePrice.toInt()
         }
 
+    fun readManualLottoCount(): Int =
+        retryWhileNoException {
+            println(INPUT_MANUAL_LOTTO_COUNT)
+            val manualLottoCount = readln()
+            isPositiveDigit(manualLottoCount)
+            manualLottoCount.toInt()
+        }
+
     fun readWinningLottoNumbers(): Lotto =
         retryWhileNoException {
             println(INPUT_WINNING_LOTTO_NUMBERS)
@@ -44,7 +52,7 @@ class InputView {
             INVALID_LOTTO_NUMBER
         }
 
-    private fun isPositiveDigit(it: String) = it.toIntOrNull() != null && it.toInt() > MINIMUM_DIGIT
+    private fun isPositiveDigit(it: String) = it.toIntOrNull() != null && it.toInt() >= MINIMUM_DIGIT
 
     companion object {
         private const val INVALID_PURCHASE_PRICE = "올바른 금액을 입력해 주세요."
@@ -52,6 +60,7 @@ class InputView {
         private const val INVALID_BONUS_NUMBER = "올바른 보너스 번호를 입력해 주세요."
         private const val LOTTO_NUMBER_DELIMITER = ", "
         private const val INPUT_PURCHASE_PRICE = "구입금액을 입력해 주세요."
+        private const val INPUT_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
         private const val INPUT_WINNING_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
         private const val INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
 
