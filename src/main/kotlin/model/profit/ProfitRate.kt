205562bd43e6aa1bee597f219efaded6086994b4
sh1mj1 <7wlgns@gmail.com>
2024-02-29 21:24:36 +0900

    feat(ProfitRate): 수익률을 통해 수익 상태를 계산한다
    - 수익률이 1 초과면 이득, 1 미만이면 손해, 1 이면 본전이다

--- src/main/kotlin/model/profit/ProfitRate.kt
+++ src/main/kotlin/model/profit/ProfitRate.kt
@@ -1,3 +1,10 @@
 package model.profit
 
-data class ProfitRate(val rate: Double)
+data class ProfitRate(val rate: Double) {
+    fun decideProfitStatus(): ProfitStatus =
+        when {
+            rate > 1.0 -> ProfitStatus.GAIN
+            rate < 1.0 -> ProfitStatus.LOSS
+            else -> ProfitStatus.EVEN
+        }
+}
