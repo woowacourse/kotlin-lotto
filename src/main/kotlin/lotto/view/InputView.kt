b5a21e44197b3e9b098d0ca978e8ad9882a00010
김진우 <jinwoo3661@naver.com>
2024-02-29 17:26:11 +0900

    refactor: util 패키지를 제거하고 각 뷰에 맞는 기능으로 분리

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,26 +1,52 @@
 package lotto.view
 
-import lotto.controller.inputBonus
-import lotto.controller.inputCharge
-import lotto.controller.inputWinning
-import lotto.model.LottoNumber
+import lotto.model.user.UserInputVerifier
+import lotto.model.BonusNumber
+import lotto.model.Lotto
+import lotto.model.user.UserEvent
 
-const val REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요."
-const val REQUEST_WINNING_NUM = "지난 주 당첨 번호를 입력해 주세요."
-const val REQUEST_BONUS_NUM = "보너스 볼을 입력해 주세요."
+private const val REQUEST_PURCHASE_PRICE = "구입금액을 입력해 주세요."
+private const val REQUEST_WINNING_NUM = "지난 주 당첨 번호를 입력해 주세요."
+private const val REQUEST_BONUS_NUM = "보너스 볼을 입력해 주세요."
+private const val REQUEST_MANUAL_NUM = "수동으로 구매할 로또 수를 입력해 주세요."
+private const val REQUEST_LOTTO_NUM = "수동으로 구매할 번호를 입력해 주세요."
 
 fun inputCostMessage(): Int {
     println(REQUEST_PURCHASE_PRICE)
-    return inputCharge(readlnOrNull()) ?: inputCostMessage()
+    return when (val event = UserInputVerifier.inputCharge(readlnOrNull())) {
+        is UserEvent.PurchaseEvent.Success -> event.price
+        else -> inputCostMessage()
+    }
 }
 
-fun inputWinNumbers(): LottoNumber {
+fun inputManualNumber(lottoCount: Int): Int {
+    println(REQUEST_MANUAL_NUM)
+    return when (val event = UserInputVerifier.inputManualCount(readlnOrNull(), lottoCount)) {
+        is UserEvent.ManualEvent.Success -> event.count
+        else -> inputManualNumber(lottoCount)
+    }
+}
+
+fun inputManualLottoNumbers() {
+    println(REQUEST_LOTTO_NUM)
+}
+
+fun inputWinningLottoNumbers() {
     println(REQUEST_WINNING_NUM)
-    return inputLottoNumbers(readlnOrNull()) ?: inputWinNumbers()
 }
 
-fun inputBonusNumber(): Int {
+fun inputLottoNumbers(): Lotto {
+    return when (val event = UserInputVerifier.inputLottoNumbers(readlnOrNull())) {
+        is UserEvent.LottoEvent.Success -> event.lotto
+        else -> inputLottoNumbers()
+    }
+}
+
+fun inputBonusNumber(winningLotto: Lotto): BonusNumber {
     println(REQUEST_BONUS_NUM)
-    return inputBonus(readlnOrNull()) ?: inputBonusNumber()
+    return when (val event = UserInputVerifier.inputBonus(readlnOrNull(), winningLotto)) {
+        is UserEvent.BonusEvent.Success -> event.bonusNumber
+        else -> inputBonusNumber(winningLotto)
+    }
 }
 
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -2,18 +2,23 @@ package lotto.view
 
 import lotto.model.Lotto
 import lotto.model.LottoPrize
-import lotto.util.LottoMessage
 
-const val PURCHASE_LOTTO_COUNT = "%d개를 구매했습니다."
-const val START_RESULT = "당첨 통계\n—--------"
-const val YIELD_RESULT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+private const val PURCHASE_LOTTO_COUNT = "수동으로 %d장, 자동으로 %d개를 구매했습니다."
+private const val START_RESULT = "당첨 통계\n—--------"
+private const val YIELD_RESULT = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)"
+private const val LOTTO_BOOM = "꽝"
+private const val LOTTO_FIFTH = "3개 일치 (5,000원) -"
+private const val LOTTO_FOURTH = "4개 일치 (50,000원) -"
+private const val LOTTO_THIRD = "5개 일치 (1,500,000원) -"
+private const val LOTTO_SECOND = "5개 일치, 보너스 볼 일치 (30,000,000원) -"
+private const val LOTTO_FIRST = "6개 일치 (2,000,000,000원) -"
 
 fun outputNewLine() {
     println()
 }
 
-fun outputPurchaseCount(count: Int) {
-    println(PURCHASE_LOTTO_COUNT.format(count))
+fun outputPurchaseCount(manualCount: Int, autoCount: Int) {
+    println(PURCHASE_LOTTO_COUNT.format(manualCount, autoCount))
 }
 
 fun outputLottos(lottos: List<Lotto>) {
@@ -45,11 +50,11 @@ private fun outputLotto(lotto: Lotto) {
 
 private fun getLottoMessage(prize: LottoPrize): String {
     return when (prize) {
-        LottoPrize.BOOM -> LottoMessage.BOOM
-        LottoPrize.FIFTH -> LottoMessage.FIFTH
-        LottoPrize.FOURTH -> LottoMessage.FOURTH
-        LottoPrize.THIRD -> LottoMessage.THIRD
-        LottoPrize.SECOND -> LottoMessage.SECOND
-        LottoPrize.FIRST -> LottoMessage.FIRST
+        LottoPrize.BOOM -> LOTTO_BOOM
+        LottoPrize.FIFTH -> LOTTO_FIFTH
+        LottoPrize.FOURTH -> LOTTO_FOURTH
+        LottoPrize.THIRD -> LOTTO_THIRD
+        LottoPrize.SECOND -> LOTTO_SECOND
+        LottoPrize.FIRST -> LOTTO_FIRST
     }
 }
