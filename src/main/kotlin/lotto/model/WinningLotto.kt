ef8223c0bfb9106285a58344f2ee1bd0f5d56da3
gaeun <geun5744@gmail.com>
2024-02-29 14:44:29 +0900

    fix: calculatePrizeCount 파라미터 수정 및 calculateProfitRatio 메서드 이동

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -1,5 +1,7 @@
 package lotto.model
 
+import kotlin.math.floor
+
 class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
     init {
         require(!lotto.contains(bonusNumber)) {
@@ -7,11 +9,26 @@ class WinningLotto(val lotto: Lotto, val bonusNumber: LottoNumber) {
         }
     }
 
-    fun calculatePrizeCount(lottoStore: LottoStore) =
-        lottoStore.lottos
+    fun calculatePrizeCount(lottos: List<Lotto>) =
+        lottos
             .map { it.compare(lotto, bonusNumber) }
             .groupBy { it }
             .mapValues { it.value.size }
+
+    fun calculateProfitRatio(
+        lottos: List<Lotto>,
+        purchasePrice: Int,
+    ): Double {
+        val totalPrizeAmount =
+            calculatePrizeCount(lottos)
+                .map { (lottoPrize, count) ->
+                    lottoPrize.amount * count.toLong()
+                }.sum()
+
+        return (totalPrizeAmount.toDouble() / purchasePrice).roundTwoDecimal()
+    }
+
+    private fun Double.roundTwoDecimal() = floor(this * 100) / 100
 
     companion object {
         private const val INVALID_DUPLICATE_BONUS_NUMBER = "당첨 번호와 중복되지 않는 보너스 번호를 입력해 주세요."
