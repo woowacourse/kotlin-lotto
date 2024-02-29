675511dfc1b8cfd6bef53c5f0c07ca0092af17dd
Gimun Kim <kmkim7575@gmail.com>
2024-02-29 13:49:36 +0900

    refactor: 자동으로 랜덤 넘버로 이뤄진 자동 로또 리스트를 만들어주는 로직을  정수의 리스트를 로또 리스트로 변환해주는 비즈니스 로직으로 변경

--- src/main/kotlin/controller/LottoGameController.kt
+++ src/main/kotlin/controller/LottoGameController.kt
@@ -18,20 +18,27 @@ class LottoGameController(
 ) {
     fun startLottoGame() {
         val purchaseExpense: Money = getPurchaseExpense()
-        val lottie: List<Lotto> = purchaseLottie(purchaseExpense)
-        val winningLotto = getWinningLotto()
-        val bonusLottoNumber = createBonusLottoNumber(winningLotto)
-        val lottoGameResult = LottoGameResult(bonusLottoNumber, winningLotto, lottie)
-        displayLottoResult(lottoGameResult, purchaseExpense)
+        var availableCount = Count.fromMoney(purchaseExpense)
+        val manualCount = getManualCount()
+//        val manualLotteries = lottoGameInputView.inputManualLotteries(manualCount.value)
+//        availableCount -= manualCount
+//        val autoLotteries = lottoGenerator.generate()
+//        val lottie: List<Lotto> = purchaseLottie(purchaseExpense)
+//        val winningLotto = getWinningLotto()
+//        val bonusLottoNumber = createBonusLottoNumber(winningLotto)
+//        val lottoGameResult = LottoGameResult(bonusLottoNumber, winningLotto, lottie)
+//        displayLottoResult(lottoGameResult, purchaseExpense)
     }
 
     private fun getPurchaseExpense(): Money = exceptionHandler.handleInputValue(lottoGameInputView::inputPurchaseExpense)
 
-    private fun purchaseLottie(purchaseExpense: Money): List<Lotto> =
-        exceptionHandler.handleInputValue {
-            val lotteries = lottoGenerator.generate(Count.fromMoney(purchaseExpense))
-            lotteries.also(lottoGameOutputView::showPurchasedLottie)
-        }
+//    private fun purchaseLottie(purchaseExpense: Money): List<Lotto> =
+//        exceptionHandler.handleInputValue {
+//            val lotteries = lottoGenerator.generate(Count.fromMoney(purchaseExpense))
+//            lotteries.also(lottoGameOutputView::showPurchasedLottie)
+//        }
+
+    private fun getManualCount(): Count = exceptionHandler.handleInputValue(lottoGameInputView::inputManualPurchaseCount)
 
     private fun getWinningLotto(): Lotto = exceptionHandler.handleInputValue(lottoGameInputView::inputWinningNumbers)
 
--- src/main/kotlin/model/LottoGenerator.kt
+++ src/main/kotlin/model/LottoGenerator.kt
@@ -1,20 +1,11 @@
 package model
 
 fun interface LottoGenerator {
-    fun generate(count: Count): List<Lotto>
+    fun generate(lotteryNumbers: List<List<Int>>): List<Lotto>
 }
 
-class AutoLottoGenerator(
-    private val numbersGenerator: NumbersGenerator = RandomNumbersGenerator(),
-) : LottoGenerator {
-    override fun generate(count: Count): List<Lotto> {
-        return List(count.value) {
-            val lottoNums = numbersGenerator.generate(LOTTO_NUMBER_SIZE).toIntArray()
-            Lotto(*lottoNums)
-        }
-    }
-
-    companion object {
-        private const val LOTTO_NUMBER_SIZE = 6
+class AutoLottoGenerator : LottoGenerator {
+    override fun generate(lotteryNumbers: List<List<Int>>): List<Lotto> {
+        return lotteryNumbers.map { Lotto(*it.toIntArray()) }
     }
 }
