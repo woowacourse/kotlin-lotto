4ded3476f629bb601c3ffb877f6570203ba2925a
gaeun <geun5744@gmail.com>
2024-03-01 21:48:08 +0900

    refactor: inputView 함수 세분화

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -3,43 +3,35 @@ package lotto.controller
 import lotto.model.Lotto
 import lotto.model.LottoCount
 import lotto.model.Ticket
-import lotto.model.WinningLotto
-import lotto.view.InputView
+import lotto.view.LottoCountInputView
+import lotto.view.ManualLottoInputView
 import lotto.view.OutputView
+import lotto.view.WinningLottoInputView
 
 class LottoController(
-    private val inputView: InputView = InputView(),
+    private val lottoCountInputView: LottoCountInputView = LottoCountInputView(),
+    private val manualLottoInputView: ManualLottoInputView = ManualLottoInputView(),
+    private val winningLottoInputView: WinningLottoInputView = WinningLottoInputView(),
     private val outputView: OutputView = OutputView(),
 ) {
     fun run() {
-        val purchasePrice = inputView.readPurchasePrice()
-        val manualLottoCount = inputView.readManualLottoCount()
-
-        val lottoCount = LottoCount(purchasePrice, 1_000, manualLottoCount)
+        val lottoCount = lottoCountInputView.readLottoCount()
         val ticket = Ticket(lottoCount)
 
-        val lottos = issueLottos(lottoCount, ticket)
+        val manualLottoNumbers = manualLottoInputView.readManualLottoNumbers(lottoCount.manualCount)
+        val lottos = ticket.issueLottos(manualLottoNumbers)
+        outputView.printPurchaseLotto(lottoCount, lottos)
 
-        val winningLotto = readWinningLotto()
+        printResult(lottos, lottoCount)
+    }
+
+    private fun printResult(
+        lottos: List<Lotto>,
+        lottoCount: LottoCount,
+    ) {
+        val winningLotto = winningLottoInputView.readWinningLotto()
 
         outputView.printWinningResult(winningLotto.calculatePrizeCount(lottos))
         outputView.printWinningRatio(winningLotto.calculateProfitRatio(lottos, lottoCount.purchasePrice))
-    }
-
-    private fun readWinningLotto(): WinningLotto {
-        val winningLottoNumbers = inputView.readWinningLottoNumbers()
-        val bonusNumber = inputView.readBonusNumber()
-
-        return WinningLotto(winningLottoNumbers, bonusNumber)
-    }
-
-    private fun issueLottos(
-        lottoCount: LottoCount,
-        ticket: Ticket,
-    ): List<Lotto> {
-        val manualLottoNumbers = inputView.readManualLottoNumbers(lottoCount.manualCount)
-        val lottos = ticket.issueLottos(manualLottoNumbers)
-        outputView.printPurchaseLotto(lottoCount, lottos)
-        return lottos
     }
 }
---
+++ src/main/kotlin/lotto/view/LottoCountInputView.kt
@@ -0,0 +1,38 @@
+package lotto.view
+
+import lotto.model.LottoCount
+import lotto.view.util.retryWhileNoException
+
+class LottoCountInputView {
+    fun readLottoCount(): LottoCount {
+        val purchasePrice = readPurchasePrice()
+        val manualLottoCount = readManualLottoCount()
+        return LottoCount(purchasePrice, 1000, manualLottoCount)
+    }
+
+    private fun readPurchasePrice(): Int =
+        retryWhileNoException {
+            println("구입금액을 입력해 주세요.")
+            val purchasePrice = readln()
+            validatePositiveDigit(purchasePrice)
+            purchasePrice.toInt()
+        }.also { println() }
+
+    private fun readManualLottoCount(): Int =
+        retryWhileNoException {
+            println("수동으로 구매할 로또 수를 입력해 주세요.")
+            val manualLottoCount = readln()
+            validatePositiveDigit(manualLottoCount)
+            manualLottoCount.toInt()
+        }.also { println() }
+
+    private fun validatePositiveDigit(it: String) {
+        require(it.toIntOrNull() != null && it.toInt() >= MINIMUM_DIGIT) {
+            "올바른 금액을 입력해 주세요."
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_DIGIT = 0
+    }
+}
