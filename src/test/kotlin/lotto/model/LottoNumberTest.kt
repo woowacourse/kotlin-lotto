0767bca3dd610f89470935e7b7ec588c249194a0
JoYehyun <jo990714@naver.com>
2024-03-01 15:10:48 +0900

    refactor: LottoNumber 값 재활용 테스트

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -1,7 +1,9 @@
 package lotto.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -16,5 +18,14 @@ class LottoNumberTest {
     @ValueSource(ints = [1, 45, 20])
     fun `로또 번호는 1~45 사이의 자연수를 갖는다`(input: Int) {
         assertNotNull(LottoNumber.valueOf(input))
+    }
+
+    @Test
+    fun `로또 번호 생성 캐싱 테스트`() {
+        val firstGenerated = LottoNumber.valueOf(1)
+        val secondGenerated = LottoNumber.valueOf(1)
+
+        assertThat(firstGenerated).isEqualTo(secondGenerated)
+        assertThat(firstGenerated).isSameAs(secondGenerated)
     }
 }
