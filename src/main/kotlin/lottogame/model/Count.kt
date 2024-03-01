1421c6db71f71b54e26b46576575754a5ca7d622
murjune <sis9221302@gmail.com>
2024-03-02 07:11:37 +0900

    fix : Count 범위, test

--- src/main/kotlin/lottogame/model/Count.kt
+++ src/main/kotlin/lottogame/model/Count.kt
@@ -3,14 +3,15 @@ package lottogame.model
 @JvmInline
 value class Count(val amount: Int) {
     init {
-        require(amount > 0) { EXCEPTION_COUNT_AMOUNT }
+        require(amount >= 0) { EXCEPTION_COUNT_AMOUNT }
     }
 
     companion object {
+        private const val MIN_COUNT = 0
         private const val EXCEPTION_COUNT_AMOUNT = "count 는 0보다 커야함"
 
         fun createOrNull(count: Int): Count? {
-            if (count < 0) return null
+            if (count < MIN_COUNT) return null
             return Count(count)
         }
     }
--- src/test/kotlin/lottogame/model/CountTest.kt
+++ src/test/kotlin/lottogame/model/CountTest.kt
@@ -1,13 +1,16 @@
 package lottogame.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class CountTest {
-    @ParameterizedTest
-    @ValueSource(ints = [-1, 0])
-    fun `Count 는 0 보다 작으면 null 반환`(count: Int) {
-        assertThat(Count.createOrNull(0)).isNull()
+    @Test
+    fun `Count 는 0 보다 작으면 null 반환`() {
+        assertAll(
+            { assertThat(Count.createOrNull(-1)).isNull() },
+            { assertDoesNotThrow { Count.createOrNull(0) } },
+        )
     }
 }
