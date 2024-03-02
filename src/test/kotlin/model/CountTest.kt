fd34604457483070d87eb9ce13c2e98d150901cb
Hogu59 <dbstjdguswkd@naver.com>
2024-03-03 07:01:08 +0900

    refactor(countTest): apply test fixture

--- src/test/kotlin/model/CountTest.kt
+++ src/test/kotlin/model/CountTest.kt
@@ -7,20 +7,21 @@ import java.math.BigDecimal
 class CountTest() {
     @Test
     fun `로또 구매 장 수를 저장한다`() {
-        assertThat(Count(3).amount).isEqualTo(3)
+        assertThat(COUNT_3.amount).isEqualTo(3)
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 구매가 가능한지 판별한다`() {
-        assertThat(Count(3).isPurchasable(money)).isTrue
+        assertThat(COUNT_3.isPurchasable(MONEY_14000)).isTrue
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 자동 로또 구매 장 수를 계산한다`() {
-        assertThat(Count(3).getAutoCount(money)).isEqualTo(11)
+        assertThat(COUNT_3.getAutoCount(MONEY_14000)).isEqualTo(11)
     }
 
     companion object {
-        private val money = Money(BigDecimal(14000))
+        private val COUNT_3 = Count(3)
+        private val MONEY_14000 = Money(BigDecimal(14000))
     }
 }
