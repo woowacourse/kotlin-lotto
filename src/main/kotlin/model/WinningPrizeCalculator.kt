cf167c29bc311bec18bb3605c5cb955d80962704
Namyunsuk <yunsuk0328@naver.com>
2024-03-02 15:51:53 +0900

    refactor: 수익률 계산 시 필요한 총 상금 액수구하는 로직을 모델 내부에서 처리하도록 수정

--- src/main/kotlin/model/WinningPrizeCalculator.kt
+++ src/main/kotlin/model/WinningPrizeCalculator.kt
@@ -1,15 +1,16 @@
 package lotto.model
 
 object WinningPrizeCalculator {
-    fun calculateProfitAmount(winningResult: Map<WinningRank, Int>): Int =
+    fun calculateProfitRate(
+        purchaseAmount: Int,
+        winningResult: Map<WinningRank, Int>,
+    ): Double {
+        val profitAmount = calculateProfitAmount(winningResult)
+        return (profitAmount / purchaseAmount).toDouble()
+    }
+
+    private fun calculateProfitAmount(winningResult: Map<WinningRank, Int>): Int =
         winningResult.entries.sumOf {
             it.key.prize * (it.value)
         }.toInt()
-
-    fun calculateProfitRate(
-        purchaseAmount: Int,
-        profitAmount: Int,
-    ): Double {
-        return (profitAmount / purchaseAmount).toDouble()
-    }
 }
