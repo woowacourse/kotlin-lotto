bc866f9c96342a2042f36cc21208a7a84d5a85a2
JoYehyun <jo990714@naver.com>
2024-03-03 01:52:07 +0900

    refactor: 유효하지 않는 입력값 상수화

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,28 +3,30 @@ package lotto.view
 import lotto.util.ValidationUtils
 
 object InputView {
+    private const val INVALID_INPUT_VALUE = -1
+
     fun getPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
         val input = readln()
-        return input.toIntOrNull() ?: -1
+        return input.toIntOrNull() ?: INVALID_INPUT_VALUE
     }
 
     fun getWinningNumbers(): Set<Int> {
         println("지난 주 당첨 번호를 입력해 주세요.")
         val input = readln()
         input.split(",").forEach { ValidationUtils.validateLottoNumber(it.trim()) }
-        return input.split(",").map { it.trim().toIntOrNull() ?: -1 }.toSet()
+        return input.split(",").map { it.trim().toIntOrNull() ?: INVALID_INPUT_VALUE }.toSet()
     }
 
     fun getBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
         val input = readln()
-        return input.toIntOrNull() ?: -1
+        return input.toIntOrNull() ?: INVALID_INPUT_VALUE
     }
 
     fun getNumberOfManualLotto(): Int {
         println("수동으로 구매할 로또 수를 입력해 주세요.")
-        return readln().toIntOrNull() ?: -1
+        return readln().toIntOrNull() ?: INVALID_INPUT_VALUE
     }
 
     fun getManualLottoNumbers(count: Int): List<Set<Int>> {
@@ -32,7 +34,7 @@ object InputView {
         return List(count) {
             val input = readln().split(",")
             input.forEach { ValidationUtils.validateLottoNumber(it.trim()) }
-            input.map { it.trim().toIntOrNull() ?: -1 }.toSet()
+            input.map { it.trim().toIntOrNull() ?: INVALID_INPUT_VALUE }.toSet()
         }
     }
 }
