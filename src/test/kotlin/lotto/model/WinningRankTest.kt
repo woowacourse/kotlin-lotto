6164ab12e4d6ed90bf8f7b4c1227f8f791f89aea
JoYehyun <jo990714@naver.com>
2024-03-02 23:13:53 +0900

    chore: 당첨 등수 테스트 패키지 변경

--- src/test/kotlin/lotto/model/WinningRankTest.kt
+++ src/test/kotlin/lotto/model/WinningRankTest.kt
@@ -1,6 +1,5 @@
-package lotto.util
+package lotto.model
 
-import lotto.model.WinningRank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
