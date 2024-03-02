3f281784e4da2ddaf539dfc18a72aed72284a2db
Namyunsuk <yunsuk0328@naver.com>
2024-03-02 16:19:48 +0900

    feat: 수익률 계산 시에도 총 상금에 대한 유효섬 검증을 하도록 구현

--- src/main/kotlin/model/WinningPrizeCalculator.kt
+++ src/main/kotlin/model/WinningPrizeCalculator.kt
@@ -1,6 +1,10 @@
 package lotto.model
 
 object WinningPrizeCalculator {
+    private const val ERROR_PREFIX = "[ERROR] "
+    private const val PURCHASE_UNIT = 1000
+    private const val ERROR_NUMBER_RANGE_MESSAGE = "${ERROR_PREFIX}1000원 이상의 값만 입력 가능합니다."
+
     fun calculateProfitRate(
         purchaseAmount: Int,
         winningResult: Map<WinningRank, Int>,
@@ -13,4 +17,8 @@ object WinningPrizeCalculator {
         winningResult.entries.sumOf {
             it.key.prize * (it.value)
         }.toInt()
+
+    private fun validatePurchaseAmountRange(purchaseAmount: Int) {
+        require(purchaseAmount >= PURCHASE_UNIT) { ERROR_NUMBER_RANGE_MESSAGE }
+    }
 }
