fdb5712f074823b00a81bf0ceab827aa9ddd8b1f
JoYehyun <jo990714@naver.com>
2024-03-03 01:08:26 +0900

    refactor: contains 활용

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -2,14 +2,14 @@ package lotto.model
 
 class WinningLotto(private val winning: Lotto, private val bonusNumber: LottoNumber) {
     init {
-        require(!winning.hasMatchingBonusNumbers(bonusNumber)) { "보너스 숫자는 당첨 번호와 중복될 수 없습니다." }
+        require(bonusNumber !in winning) { "보너스 숫자는 당첨 번호와 중복될 수 없습니다." }
     }
 
     fun generateWinningStatus(lottoTickets: List<Lotto>): Map<WinningRank, Int> =
-        lottoTickets.map {
+        lottoTickets.map { lotto ->
             WinningRank.determineRank(
-                it.countMatchingNumbers(winning),
-                it.hasMatchingBonusNumbers(bonusNumber),
+                lotto.countMatchingNumbers(winning),
+                bonusNumber in lotto,
             )
         }.groupingBy { it }.eachCount()
 }
