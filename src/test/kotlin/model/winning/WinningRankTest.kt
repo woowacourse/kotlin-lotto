9329d25b56adf6cf6c01aef1b1d4d337f8fc8cdc
sh1mj1 <7wlgns@gmail.com>
2024-03-02 22:45:14 +0900

    test(WinningRankTest): 여러 케이스에서 당첨 순위를 구하는 테스트를 추가한다

--- src/test/kotlin/model/winning/WinningRankTest.kt
+++ src/test/kotlin/model/winning/WinningRankTest.kt
@@ -2,17 +2,30 @@ package model.winning
 
 import model.Quantity
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class WinningRankTest {
-    @Test
-    fun `일치하는 수와 보너스 번호 일치 여부로 당첨 순위를 구한다`() {
-        val expectedWinningRank = WinningRank.FIFTH
-        val actualWinningRank =
-            WinningRank.of(
-                numbersMatchQuantity = Quantity(3),
-                bonusNumberMatch = true,
-            )
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "6:false:FIRST", "6:true:FIRST",
+            "5:true:SECOND", "5:false:THIRD",
+            "4:true:FOURTH", "4:false:FOURTH",
+            "3:true:FIFTH", "3:false:FIFTH",
+            "2:true:NONE", "2:false:NONE",
+            "1:true:NONE", "1:false:NONE",
+            "0:true:NONE", "0:false:NONE",
+        ],
+        delimiter = ':',
+    )
+    fun `일치하는 수와 보너스 번호 일치 여부로 당첨 순위를 구한다`(
+        numbersMatchCount: Int,
+        bonusNumberMatch: Boolean,
+        expectedRank: String,
+    ) {
+        val actualWinningRank = WinningRank.of(Quantity(numbersMatchCount), bonusNumberMatch)
+        val expectedWinningRank = WinningRank.valueOf(expectedRank)
 
         assertThat(actualWinningRank).isEqualTo(expectedWinningRank)
     }
