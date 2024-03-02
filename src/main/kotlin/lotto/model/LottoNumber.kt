d9d714390b7a34b763fac89ef6ef41cbb3ec8c1f
JoYehyun <jo990714@naver.com>
2024-03-03 01:20:28 +0900

    refactor: LottoNumber 유효성 검증 로직 이동

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,13 +1,15 @@
 package lotto.model
 
 class LottoNumber private constructor(private val number: Int) {
+    init {
+        require(number in NUMBER_RANGE) { "로또 번호의 범위는 1~45 사이의 자연수입니다." }
+    }
+
     override fun toString(): String = number.toString()
 
     companion object {
         val NUMBER_RANGE = (1..45)
         private val CACHED_LOTTO_NUMBERS = NUMBER_RANGE.associateWith(::LottoNumber)
-
-        private fun validationNumber(number: Int) = require(number in NUMBER_RANGE) { "로또 번호의 범위는 1~45 사이의 자연수입니다." }
 
         fun valueOf(number: Int): LottoNumber? {
             return CACHED_LOTTO_NUMBERS[number]
