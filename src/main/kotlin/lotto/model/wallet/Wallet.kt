cf57bfc54f3be13b51da068f326dd446e05c6ce3
jinuemong <jinwoo3661@naver.com>
2024-03-02 22:45:01 +0900

    refactor: 구매 금액 입력 기능 분리

---
+++ src/main/kotlin/lotto/model/wallet/Wallet.kt
@@ -0,0 +1,29 @@
+package lotto.model.wallet
+
+import lotto.model.user.UserException
+import lotto.model.Lotto
+
+class Wallet(private val price: Int) {
+
+    fun getPrice(): Int {
+        return price
+    }
+
+    fun getLottoCount(): Int {
+        return price / Lotto.LOTTO_PRICE.toInt()
+    }
+
+    companion object {
+        fun checkPurchasePrice(price: Int): WalletEvent {
+            return runCatching {
+                if (price == 0 || price < Lotto.LOTTO_PRICE) throw WalletEvent.checkWallet(WalletEvent.InvalidPrice)
+                WalletEvent.Success(Wallet(price))
+            }.getOrElse { exception ->
+                when(exception){
+                    is UserException.WalletException -> exception.walletEvent
+                    else -> WalletEvent.UnknownError
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/lotto/model/wallet/WalletEvent.kt
@@ -0,0 +1,24 @@
+package lotto.model.wallet
+
+import lotto.model.user.UserException
+import lotto.model.user.UserException.WalletException
+
+sealed interface WalletEvent {
+    data class Success(val wallet: Wallet) : WalletEvent
+    data object InvalidDataType : WalletEvent
+    data object InvalidPrice : WalletEvent
+    data object UnknownError : WalletEvent
+
+    companion object {
+        fun checkWallet(event: WalletEvent): WalletException {
+            return when (event) {
+                is Success -> WalletException(UserException.SUCCESS,event)
+                is InvalidDataType -> WalletException(UserException.INVALID_DATA_TYPE,event)
+                is InvalidPrice -> WalletException(INVALID_PRICE,event)
+                is UnknownError -> WalletException(UserException.UNKNOWN_ERROR,event)
+            }
+        }
+
+        const val INVALID_PRICE = "구입 금액이 올바르지 않습니다."
+    }
+}
