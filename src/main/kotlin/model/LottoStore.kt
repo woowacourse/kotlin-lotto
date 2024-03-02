904d43aa95bb5af1aedb7b1f63d039e2d1fe0ba0
Namyunsuk <yunsuk0328@naver.com>
2024-03-02 16:27:57 +0900

    refactor: LottoStore를 object로 변경

--- src/main/kotlin/model/LottoStore.kt
+++ src/main/kotlin/model/LottoStore.kt
@@ -2,21 +2,19 @@ package lotto.model
 
 import model.Lotto
 
-class LottoStore {
-    companion object {
-        fun generateAutoLottos(
-            numberOfAutoLottos: Int,
-            lottoNumberGenerator: NumberGenerator,
-        ): Lottos {
-            val lottoBundle = mutableListOf<Lotto>()
-            repeat(numberOfAutoLottos) {
-                lottoBundle.add(Lotto(lottoNumberGenerator.generate()))
-            }
-            return Lottos(lottoBundle)
+object LottoStore {
+    fun generateAutoLottos(
+        numberOfAutoLottos: Int,
+        lottoNumberGenerator: NumberGenerator,
+    ): Lottos {
+        val lottoBundle = mutableListOf<Lotto>()
+        repeat(numberOfAutoLottos) {
+            lottoBundle.add(Lotto(lottoNumberGenerator.generate()))
         }
+        return Lottos(lottoBundle)
+    }
 
-        fun generateManualLottos(lottoBundle: List<Lotto>): Lottos {
-            return Lottos(lottoBundle)
-        }
+    fun generateManualLottos(lottoBundle: List<Lotto>): Lottos {
+        return Lottos(lottoBundle)
     }
 }
