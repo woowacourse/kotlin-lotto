e92c4687f5044f4881206442a05f714e5bb2ec34
Junyoung-WON <wjyo60@gmail.com>
2024-03-02 16:24:07 +0900

    refactor: 수동 로또 발행 기능을 따로 분리 및 인자로 들어오는 로또의 자료형 변경
    LottoMachine의 로또 발행은 '수동'과 '자동' 발행 두 가지 기능으로 구분할 수 있음.

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -6,14 +6,22 @@ class LottoMachine(val ticketCounts: TicketCounts) {
     private val randomNumberGenerate =
         NumberGenerate { LottoNumber.NUMBER_RANGE.shuffled().take(Lotto.NUMBER_COUNT).sorted() }
 
-    fun issueTickets(manualLotto: List<Lotto> = listOf()): List<Lotto> {
+    fun issueTickets(manualLotto: List<String> = listOf()): List<Lotto> {
         val tickets = mutableListOf<Lotto>()
         if (ticketCounts.numberOfManual.counts > NumberOfManual.MIN_NUMBER_OF_MANUAL) {
-            tickets.addAll(manualLotto)
+            tickets.addAll(issueManualTickets(manualLotto))
         }
-        tickets.addAll(
-            issueAutomaticTickets(ticketCounts.getAutomaticTicketCounts()),
-        )
+        tickets.addAll(issueAutomaticTickets(ticketCounts.getAutomaticTicketCounts()))
+        return tickets
+    }
+
+    fun issueManualTickets(manualLotto: List<String>): List<Lotto> {
+        val tickets = mutableListOf<Lotto>()
+        repeat(manualLotto.size) { idx ->
+            tickets.add(
+                Lotto(manualLotto[idx].split(",").map { LottoNumber(it.trim()) }),
+            )
+        }
         return tickets
     }
 
