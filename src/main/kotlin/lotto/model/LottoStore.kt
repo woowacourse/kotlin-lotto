943f5e3df8b54b755a9aba29c3b7fe443381846d
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-03 08:44:32 +0900

    refactor: WinningPrizeCalculator 삭제

--- src/main/kotlin/lotto/model/LottoStore.kt
+++ src/main/kotlin/lotto/model/LottoStore.kt
@@ -18,8 +18,17 @@ class LottoStore(private val lottoNumberGenerator: NumberGenerator) {
         }
     }
 
-    fun getWinningResult(winningLotto: WinningLotto): Map<WinningRank, Int> {
-        return lottos.winningResult(winningLotto)
+    private fun calculateProfitAmount(winningResult: Map<WinningRank, Int>): Int =
+        winningResult.entries.sumOf {
+            it.key.prize * (it.value)
+        }.toInt()
+
+    fun calculateProfitRate(
+        purchaseAmount: Int,
+        winningResult: Map<WinningRank, Int>,
+    ): Double {
+        val profitAmount = calculateProfitAmount(winningResult)
+        return (profitAmount / purchaseAmount).toDouble()
     }
 
     companion object {
