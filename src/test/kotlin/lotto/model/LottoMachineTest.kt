e9b5e833e269aa61bb100407d3da5c5f59319b4d
kimhm0728 <kimhm0728@naver.com>
2024-03-04 00:46:35 +0900

    test: 테스트 코드 중복 제거

--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -1,48 +1,41 @@
 package lotto.model
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoMachineTest {
-    private val manualLottoMachine =
-        ManualLottoMachine(
-            listOf(
-                listOf(1, 2, 3, 4, 5, 6),
-                listOf(1, 2, 3, 4, 5, 6),
-            ),
-        )
-    private val manualLottos = manualLottoMachine.generate(2)
-    private val automaticlottoMachine =
-        object : LottoMachine {
-            override fun generate(lottoSize: Int): List<Lotto> {
-                return List(lottoSize) { Lotto.create(listOf(1, 2, 3, 4, 5, 6)) }
-            }
+    @ParameterizedTest
+    @MethodSource("로또 발행에 대한 테스트 데이터")
+    fun `로또의 번호를 임의로 지정해주면 해당 번호로 로또가 발행된다`(
+        lottoMachine: LottoMachine,
+        lottoSize: Int,
+    ) {
+        val lottos = lottoMachine.generate(lottoSize)
+        assertThat(lottos).allMatch { lotto ->
+            lottoNumbers.all { LottoNumber.from(it) in lotto }
         }
-    private val automaticLottos = automaticlottoMachine.generate(3)
-
-    @ParameterizedTest
-    @ValueSource(ints = [1, 2, 3, 4, 5, 6])
-    fun `자동 로또의 번호를 임의로 지정해주면 해당 번호로 로또가 발행된다`(number: Int) {
-        assertThat(automaticLottos).allMatch { LottoNumber.from(number) in it }
-    }
-
-    @Test
-    fun `지정해준 자동 로또의 개수만큼 로또가 발행된다`() {
-        val actual = automaticLottos.size
-        assertThat(actual).isEqualTo(3)
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1, 2, 3, 4, 5, 6])
-    fun `수동 로또 번호를 임의로 지정해주면 해당 번호로 로또가 발행된다`(number: Int) {
-        assertThat(manualLottos).allMatch { LottoNumber.from(number) in it }
+    @MethodSource("로또 발행에 대한 테스트 데이터")
+    fun `지정해준 로또의 개수만큼 로또가 발행된다`(
+        lottoMachine: LottoMachine,
+        lottoSize: Int,
+    ) {
+        val actual = lottoMachine.generate(lottoSize).size
+        assertThat(actual).isEqualTo(lottoSize)
     }
 
-    @Test
-    fun `지정해준 수동 로또의 개수만큼 로또가 발행된다`() {
-        val actual = manualLottos.size
-        assertThat(actual).isEqualTo(2)
+    companion object {
+        private val lottoNumbers = listOf(1, 2, 3, 4, 5, 6)
+
+        @JvmStatic
+        fun `로또 발행에 대한 테스트 데이터`() =
+            listOf(
+                Arguments.of(ManualLottoMachine(listOf(lottoNumbers, lottoNumbers)), 2),
+                Arguments.of(LottoMachine { lottoSize -> List(lottoSize) { Lotto.create(lottoNumbers) } }, 5),
+            )
     }
 }
