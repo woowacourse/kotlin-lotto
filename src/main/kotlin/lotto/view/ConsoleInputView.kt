80b48a03583ea88c44e62dc988ca9f391bf541ab
aprilgom <aprilgom@gmail.com>
2024-03-03 23:25:19 +0900

    feat(ConsoleInputView): 숫자 입력이 올바른 숫자가 아닐 시 재입력 처리 구현

--- src/main/kotlin/lotto/view/ConsoleInputView.kt
+++ src/main/kotlin/lotto/view/ConsoleInputView.kt
@@ -7,14 +7,21 @@ import lotto.model.Money
 class ConsoleInputView : InputView {
     override fun getPurchasePrice(): Money {
         println(GET_PURCHASE_PRICE_MESSAGE)
-        val money = readln().toLong()
+        val money: Long =
+            repeatUntilGetValidInput(INVALID_PRICE_ERROR) {
+                readln().toLongOrNull()
+            }
         require(money > 0L)
         return Money(money)
     }
 
     override fun getManualCount(): Int {
         println(GET_MANUAL_COUNT_MESSAGE)
-        return readln().toInt()
+        val manualCount: Int =
+            repeatUntilGetValidInput(INVALID_MANUAL_COUNT_ERROR) {
+                readln().toIntOrNull()
+            }
+        return manualCount
     }
 
     override fun getManualLottoTickets(count: Int): List<LottoTicket> {
@@ -28,17 +35,53 @@ class ConsoleInputView : InputView {
     }
 
     private fun getLottoTicket(): LottoTicket {
-        val numbers =
-            readln()
-                .trim()
-                .split(",")
-                .map { LottoNumber.of(it.trim().toInt()) }
+        val intNumbers =
+            repeatUntilGetValidInputList(INVALID_LOTTO_TICKET_NUMBER) {
+                readln()
+                    .trim()
+                    .split(",")
+                    .map { it.trim().toIntOrNull() }
+            }
+        val numbers = intNumbers.map { LottoNumber.of(it) }
         return LottoTicket(numbers)
     }
 
     override fun getBonusNumber(): LottoNumber {
         println(GET_BONUS_NUMBER_MESSAGE)
-        return LottoNumber.of(readln().toInt())
+        val bonusNumber: Int =
+            repeatUntilGetValidInput(INVALID_BONUS_NUMBER) {
+                readln().toIntOrNull()
+            }
+        return LottoNumber.of(bonusNumber)
+    }
+
+    private fun <T> repeatUntilGetValidInput(
+        errorMsg: String,
+        inputFunction: () -> T?,
+    ): T {
+        var result: T?
+        do {
+            result = inputFunction()
+            if (result == null) {
+                println(errorMsg)
+            }
+        } while (result == null)
+        return result
+    }
+
+    private fun <T> repeatUntilGetValidInputList(
+        errorMsg: String,
+        inputFunction: () -> List<T?>,
+    ): List<T> {
+        var result: List<T?>
+        do {
+            result = inputFunction()
+            val condition = result.any { it == null }
+            if (condition) {
+                println(errorMsg)
+            }
+        } while (condition)
+        return result.filterNotNull()
     }
 
     companion object {
@@ -47,5 +90,10 @@ class ConsoleInputView : InputView {
         private const val GET_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
         private const val GET_MANUAL_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요."
         private const val GET_MANUAL_TICKET_MESSAGE = "수동으로 구매할 번호를 입력해 주세요."
+
+        private const val INVALID_PRICE_ERROR = "갸격을 잘못 입력하셨습니다."
+        private const val INVALID_MANUAL_COUNT_ERROR = "횟수를 잘못 입력하셨습니다."
+        private const val INVALID_LOTTO_TICKET_NUMBER = "로또 번호를 잘못 입력하셨습니다."
+        private const val INVALID_BONUS_NUMBER = "보너스 번호를 잘못 입력하셨습니다."
     }
 }
