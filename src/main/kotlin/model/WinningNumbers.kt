66ff01e5a700d846ea6e17dd81d9a630d80c9ead
Namyunsuk <yunsuk0328@naver.com>
2024-03-04 00:32:18 +0900

    feat: 당첨번호와 보너스번호가 중복된 경우 예외처리 기능 구현

--- src/main/kotlin/model/WinningNumbers.kt
+++ src/main/kotlin/model/WinningNumbers.kt
@@ -5,13 +5,24 @@ import model.LottoNumber
 
 class WinningNumbers(val winningLotto: Lotto, val bonusNumber: LottoNumber) {
     companion object {
+        private const val ERROR_PREFIX = "[ERROR] "
+        private const val ERROR_DUPLICATE_WINNING_NUMBERS = "${ERROR_PREFIX}당첨번호와 보너스 번호는 중복될 수 없습니다."
+
         fun of(
             winningLottoValue: List<String>,
             bonusNumberValue: String,
         ): WinningNumbers {
+            validateDuplicateWinningNumbers(winningLottoValue, bonusNumberValue)
             val winningLotto = Lotto(LottoNumbers(winningLottoValue.map { LottoNumber(it) }))
             val bonusNumber = LottoNumber(bonusNumberValue)
             return WinningNumbers(winningLotto, bonusNumber)
+        }
+
+        private fun validateDuplicateWinningNumbers(
+            winningLottoValue: List<String>,
+            bonusNumberValue: String,
+        ) {
+            require(winningLottoValue.all { it != bonusNumberValue }) { ERROR_DUPLICATE_WINNING_NUMBERS }
         }
     }
 }
