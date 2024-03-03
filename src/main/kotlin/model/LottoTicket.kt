932ccaa0ff6526056e8ba4d830bd05ba03dc0fa4
aprilgom <aprilgom@gmail.com>
2024-03-03 19:54:30 +0900

    style(LottoTicket): 코딩 컨벤션 준수

--- src/main/kotlin/model/LottoTicket.kt
+++ src/main/kotlin/model/LottoTicket.kt
@@ -16,6 +16,7 @@ class LottoTicket(
     companion object {
         const val SIZE = 6
         private const val NUMBER_DUPLICATED_OR_INSUFFICIENT = "로또 번호가 모자라거나 중복됨."
+
         fun from(intLottoNumbers: List<Int>) = LottoTicket(intLottoNumbers.map { LottoNumber.of(it) })
     }
 }
