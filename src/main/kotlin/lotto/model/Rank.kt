993358b82e1d1aae5b9e83b21852f3a176b7ac70
kkosang <eiwoqp77@gmail.com>
2024-03-04 00:24:15 +0900

    refactor: Rank 클래스 수정

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -1,7 +1,5 @@
 package lotto.model
 
-import lotto.util.MoneyConstants
-
 private const val MATCH_COUNT_FIRST = 6
 private const val MATCH_COUNT_SECOND = 5
 private const val MATCH_COUNT_THIRD = 5
@@ -10,12 +8,12 @@ private const val MATCH_COUNT_FIFTH = 3
 private const val MATCH_COUNT_MISS = 0
 
 enum class Rank(val countOfMatch: Int, val winningAmount: Int) {
-    FIRST(MATCH_COUNT_FIRST, MoneyConstants.FIRST_RANK_WINNING_MONEY),
-    SECOND(MATCH_COUNT_SECOND, MoneyConstants.SECOND_RANK_WINNING_MONEY),
-    THIRD(MATCH_COUNT_THIRD, MoneyConstants.THIRD_RANK_WINNING_MONEY),
-    FOURTH(MATCH_COUNT_FOURTH, MoneyConstants.FOURTH_RANK_WINNING_MONEY),
-    FIFTH(MATCH_COUNT_FIFTH, MoneyConstants.FIFTH_RANK_WINNING_MONEY),
-    MISS(MATCH_COUNT_MISS, MoneyConstants.MISS_WINNING_MONEY),
+    FIRST(MATCH_COUNT_FIRST, 2_000_000_000),
+    SECOND(MATCH_COUNT_SECOND, 30_000_000),
+    THIRD(MATCH_COUNT_THIRD, 1_500_000),
+    FOURTH(MATCH_COUNT_FOURTH, 50_000),
+    FIFTH(MATCH_COUNT_FIFTH, 5_000),
+    MISS(MATCH_COUNT_MISS, 0),
     ;
 
     companion object {
@@ -23,13 +21,9 @@ enum class Rank(val countOfMatch: Int, val winningAmount: Int) {
             countOfMatch: Int,
             bonusMatched: Boolean,
         ): Rank {
-            return when (countOfMatch) {
-                MATCH_COUNT_FIRST -> FIRST
-                MATCH_COUNT_SECOND -> SECOND.takeIf { bonusMatched } ?: THIRD
-                MATCH_COUNT_FOURTH -> FOURTH
-                MATCH_COUNT_FIFTH -> FIFTH
-                else -> MISS
-            }
+            return enumValues<Rank>().firstOrNull {
+                it.countOfMatch == countOfMatch && (it != SECOND || bonusMatched)
+            } ?: MISS
         }
 
         fun getRankByOrdinal(ordinal: Int): Rank {
