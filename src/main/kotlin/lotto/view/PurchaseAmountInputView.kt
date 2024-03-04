26373f9cf553774592669b9f68fe76d265273d68
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-03-04 22:19:19 +0900

    fix: 필요없는 코드 삭제

--- src/main/kotlin/lotto/view/PurchaseAmountInputView.kt
+++ src/main/kotlin/lotto/view/PurchaseAmountInputView.kt
@@ -6,21 +6,6 @@ object PurchaseAmountInputView {
     private const val ERROR_PREFIX = "[ERROR]"
     private const val ERROR_EMPTY_INPUT_MESSAGE = "입력값이 없습니다."
 
-    fun readPurchaseAmount1(purchaseUnit: Int): PurchaseAmount {
-        while (true) {
-            println("구입금액을 입력해 주세요.")
-            val result =
-                runCatching {
-                    val input = readlnOrNull() ?: throw IllegalArgumentException(ERROR_EMPTY_INPUT_MESSAGE)
-                    PurchaseAmount(input, purchaseUnit)
-                }
-
-            result.onSuccess { return it }
-
-            result.onFailure { println("$ERROR_PREFIX ${it.message}") }
-        }
-    }
-
     fun readPurchaseAmount(purchaseUnit: Int): PurchaseAmount? {
         println("구입금액을 입력해 주세요.")
         val result =
