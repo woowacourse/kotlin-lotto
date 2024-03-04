3ef4fa92772905c2aaf439da662c15c84a036ae8
songpink <jejeje04@naver.com>
2024-03-04 15:17:10 +0900

    refactor(ManualLottoCount): 에러메시지 명확하게 수정

--- src/main/kotlin/lotto/model/ManualLottoCount.kt
+++ src/main/kotlin/lotto/model/ManualLottoCount.kt
@@ -3,6 +3,6 @@ package lotto.model
 @JvmInline
 value class ManualLottoCount(val num: Int) {
     init {
-        require(num >= 0) { "[ERROR] 0이상의 정수이어야 함" }
+        require(num >= 0) { "[ERROR] 수동로또개수는 0이상의 정수이어야 함" }
     }
 }
