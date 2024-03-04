c7a74af093b5ba479e3c7c84939e7dfc48e7d2a4
Hogu59 <dbstjdguswkd@naver.com>
2024-03-04 17:45:05 +0900

    refactor(countTest): change constant to value
    change for visibility

--- src/test/kotlin/model/CountTest.kt
+++ src/test/kotlin/model/CountTest.kt
@@ -2,26 +2,20 @@ package model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 
 class CountTest() {
     @Test
     fun `로또 구매 장 수를 저장한다`() {
-        assertThat(COUNT_3.amount).isEqualTo(3)
+        assertThat(Count(3).amount).isEqualTo(3)
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 구매가 가능한지 판별한다`() {
-        assertThat(COUNT_3.isPurchasable(MONEY_14000)).isTrue
+        assertThat(Count(3).isPurchasable(Money.from(14000))).isTrue
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 자동 로또 구매 장 수를 계산한다`() {
-        assertThat(COUNT_3.getAutoCount(MONEY_14000)).isEqualTo(11)
-    }
-
-    companion object {
-        private val COUNT_3 = Count(3)
-        private val MONEY_14000 = Money(BigDecimal(14000))
+        assertThat(Count(3).getAutoCount(Money.from(14000))).isEqualTo(11)
     }
 }
