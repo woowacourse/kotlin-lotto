945bb6d6ae6c151d3b36e1f5729da24fd7b63a29
haeum808 <haeum808@gmail.com>
2024-03-04 12:57:36 +0900

    refactor: TreeSet -> Set 구현체 대신에 인터페이스 사용으로 변경

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -2,10 +2,10 @@ package lotto.model
 
 import java.util.*
 
-data class Lotto(val numbers: TreeSet<LottoNumber>) {
+data class Lotto(val numbers: Set<LottoNumber>) {
 
-    constructor(vararg numbers: Int) : this(TreeSet(numbers.map { LottoNumber.from(it) }))
-    constructor(numbers: List<Int>) : this(TreeSet(numbers.map { LottoNumber.from(it) }))
+    constructor(vararg numbers: Int) : this(TreeSet(numbers.sorted().map { LottoNumber.from(it) }))
+    constructor(numbers: List<Int>) : this(TreeSet(numbers.sorted().map { LottoNumber.from(it) }))
 
     init {
         require(numbers.size == LOTTO_SIZE) { "입력된 로또 숫자는 ${numbers.size}개입니다. 로또 숫자는 고유한 ${LOTTO_SIZE}개여야 합니다." }
