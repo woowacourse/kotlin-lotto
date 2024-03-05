d2ea9fafdf6d241859f0b4533bafa860acbc0ae6
Hogu59 <dbstjdguswkd@naver.com>
2024-03-06 01:16:30 +0900

    refactor(countTest): use factory method to make instance.

--- src/test/kotlin/model/CountTest.kt
+++ src/test/kotlin/model/CountTest.kt
@@ -2,20 +2,21 @@ package model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class CountTest() {
     @Test
     fun `로또 구매 장 수를 저장한다`() {
-        assertThat(Count(3).amount).isEqualTo(3)
+        assertThat(Count.from(3, Money.from(14000)).amount).isEqualTo(3)
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 구매가 가능한지 판별한다`() {
-        assertThat(Count(3).isPurchasable(Money.from(14000))).isTrue
+        assertDoesNotThrow { (Count.from(3, Money.from(14000))) }
     }
 
     @Test
     fun `구입금액과 수동 구매 로또 장 수의 입력을 받아 자동 로또 구매 장 수를 계산한다`() {
-        assertThat(Count(3).getAutoCount(Money.from(14000))).isEqualTo(11)
+        assertThat(Count.from(3, Money.from(14000)).getAutoCount(Money.from(14000))).isEqualTo(11)
     }
 }
