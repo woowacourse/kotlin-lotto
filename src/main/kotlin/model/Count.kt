d91989dfc1197ad039f56815a7ca2bb49d44a817
Hogu59 <dbstjdguswkd@naver.com>
2024-03-06 01:15:00 +0900

    refactor(count): develop factory method, remove unused function.
    private constructor applied.
    unused function `isPurchasable` was deleted.
    one more validation was added to factory method.

--- src/main/kotlin/model/Count.kt
+++ src/main/kotlin/model/Count.kt
@@ -1,19 +1,22 @@
 package model
 
-// private constructor issue
 @JvmInline
-value class Count(val amount: Int) {
-    fun isPurchasable(money: Money): Boolean = money.amount.toInt() >= amount * LOTTO_PRICE
-
+value class Count private constructor(val amount: Int) {
     fun getAutoCount(money: Money): Count = Count((money.amount.toInt() - amount * LOTTO_PRICE) / LOTTO_PRICE)
 
     companion object {
         private const val LOTTO_PRICE: Int = 1000
 
         private const val ERROR_LESS_THAN_ZERO = "0 이상의 값을 입력해주셔야 합니다."
+        private const val ERROR_UNAFFORDABLE = "구입금액으로 구매 불가능한 갯수입니다."
 
-        fun from(amount: Int): Count? {
+        fun from(
+            amount: Int,
+            money: Money,
+        ): Count {
             require(amount >= 0) { ERROR_LESS_THAN_ZERO }
+            require(money.amount.toInt() >= amount * LOTTO_PRICE) { ERROR_UNAFFORDABLE }
+
             return Count(amount)
         }
     }
