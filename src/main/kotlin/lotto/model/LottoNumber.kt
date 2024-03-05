13664c526c7439370e327d82d8d9bc9d6b010cee
kkosang <eiwoqp77@gmail.com>
2024-03-05 22:07:33 +0900

    refactor: LottoNumber 클래스 수정

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,30 +1,17 @@
 package lotto.model
 
-class LottoNumber(val number: Int) {
+@JvmInline
+value class LottoNumber(val number: Int) {
     init {
         require(number in MIN_LOTTO_NUMBER..MAX_LOTTO_NUMBER) {
-            throw IllegalArgumentException(INVALID_LOTTO_NUMBER_OUT_OF_RANGE)
+            INVALID_LOTTO_NUMBER_OUT_OF_RANGE
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is LottoNumber) return false
-        return this.number == other.number
-    }
-
-    override fun hashCode(): Int {
-        return number.hashCode()
-    }
-
-    override fun toString(): String {
-        return number.toString()
-    }
-
     companion object {
-        private const val MIN_LOTTO_NUMBER = 1
-        private const val MAX_LOTTO_NUMBER = 45
-        const val INVALID_LOTTO_NUMBER_OUT_OF_RANGE =
-            ("로또 번호가 $MIN_LOTTO_NUMBER~$MAX_LOTTO_NUMBER 사이의 숫자가 아닌 경우 예외처리한다.")
+        private const val MIN_LOTTO_NUMBER: Int = 1
+        private const val MAX_LOTTO_NUMBER: Int = 45
+        private const val INVALID_LOTTO_NUMBER_OUT_OF_RANGE: String =
+            "로또 번호가 $MIN_LOTTO_NUMBER~$MAX_LOTTO_NUMBER 사이의 숫자가 아닌 경우 예외처리한다."
     }
 }
--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -1,23 +1,17 @@
 package lotto.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
     @ParameterizedTest
-    @ValueSource(strings = ["1", "2", "45", "34"])
-    fun `올바른 로또 번호에 대해서, 에러를 던지지 않아야한다`(number: String) {
-        assertDoesNotThrow { LottoNumber(number.toIntOrNull() ?: 0) }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["apple", "12fj", "ㅇㄹ"])
-    fun `숫자로 변환 할 수 없는 로또 번호에 대해서, 에러를 던져야한다`(number: String) {
-        assertThrows<IllegalArgumentException> {
-            LottoNumber(number.toIntOrNull() ?: 0)
-        }
+    @ValueSource(ints = [1, 2, 45, 34])
+    fun `올바른 로또 번호에 대해서, 에러를 던지지 않아야한다`(number: Int) {
+        assertDoesNotThrow { LottoNumber(number) }
     }
 
     @ParameterizedTest
@@ -26,5 +20,10 @@ class LottoNumberTest {
         assertThrows<IllegalArgumentException> {
             LottoNumber(number)
         }
+    }
+
+    @Test
+    fun `로또 번호 생성시 같은지 확인한다`() {
+        assertThat(LottoNumber(1)).isEqualTo(LottoNumber(1))
     }
 }
