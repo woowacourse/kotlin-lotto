f4f17a8ac612064dcebca11839db4ceb62e275a9
kkosang <eiwoqp77@gmail.com>
2024-03-05 22:16:12 +0900

    refactor: WinningStatistics 클래스 수정

--- src/test/kotlin/lotto/model/WinningStatisticsTest.kt
+++ src/test/kotlin/lotto/model/WinningStatisticsTest.kt
@@ -7,7 +7,7 @@ class WinningStatisticsTest {
     @Test
     fun `로또 수익률 계산 확인`() {
         val purchaseAmount = 14_000
-        val result = listOf(WinningStatistic(Pair(Rank.FIFTH, 1)))
+        val result = mapOf(Rank.FIFTH to 1)
         val winningStatistics = WinningStatistics(result)
 
         val actual = winningStatistics.calculateRateOfReturn(purchaseAmount)
