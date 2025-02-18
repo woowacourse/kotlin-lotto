6479c006d4b138ed979c0c58b58f382142a81a40
지원 <cheerfulboys@naver.com>
2025-02-18 15:46:52 +0900

    feat: 로또 개수를 반환하는 기능 추가

---
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -0,0 +1,18 @@
+package lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest {
+    @Test
+    fun `로또 1장의 가격은 1000원이다`() {
+        val lottoMachine = LottoMachine()
+        assertThat(lottoMachine.price).isEqualTo(1_000)
+    }
+
+    @Test
+    fun `구입 금액을 넣으면 몇 개의 로또를 구매했는지 알 수 있다`() {
+        val lottoMachine = LottoMachine()
+        assertThat(lottoMachine.getLottoCount(1_000)).isEqualTo(1)
+    }
+}
