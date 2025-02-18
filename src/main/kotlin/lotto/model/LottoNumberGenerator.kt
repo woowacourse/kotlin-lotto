e760de2d12484da3e24ba5c9468e963857ca4291
junseo511 <prime54@g.skku.edu>
2025-02-18 16:53:41 +0900

    test(LottoNumberGenerator): 불필요한 객체 제거

--- src/main/kotlin/lotto/model/LottoNumberGenerator.kt
+++
@@ -1,7 +0,0 @@
-package lotto.model
-
-class LottoNumberGenerator {
-    fun getLottoNumbers(): List<Int> = List(6) { getLottoNumber() }
-
-    private fun getLottoNumber(): Int = (1..45).random()
-}
