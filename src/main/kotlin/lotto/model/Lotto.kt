f097f2985e3231dd58d27ed3cbdffeb39f6d9b3d
junseo511 <prime54@g.skku.edu>
2025-02-18 15:58:35 +0900

    feat(Lotto): 로또 숫자 범위 검증 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -3,5 +3,8 @@ package lotto.model
 class Lotto(numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또는 6개의 번호만 가질 수 있습니다." }
+        numbers.forEach { number ->
+            require(number in 1..45) { "[ERROR] 로또 번호의 범위는 1 이상 45 이하여야 합니다." }
+        }
     }
 }
