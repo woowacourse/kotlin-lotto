f96d661fbf655c62c52ee16ed840b36cb661f4ea
chanho0908 <uni070@naver.com>
2025-02-18 16:17:23 +0900

    test : 로또 번호와 보너스 번호 중복 검사 기능x테스트 추가

---
+++ src/test/kotlin/WinningLottoTest.kt
@@ -0,0 +1,15 @@
+import lotto.Lotto
+import lotto.WinningLotto
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class WinningLottoTest {
+    @Test
+    fun `로또 번호와 보너스 번호가 중복되면 예외가 발생한다`() {
+        val lotto = Lotto(listOf(1, 3, 4, 5, 6, 7))
+        Assertions.assertThatThrownBy {
+            WinningLotto(lotto, 6)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 보너스 번호는 로또 번호는 중복될 수 없습니다.")
+    }
+}
