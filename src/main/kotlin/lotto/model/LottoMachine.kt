35c2200471cbd8f2b35623e5917812c4641e100d
junseo511 <prime54@g.skku.edu>
2025-02-18 15:32:14 +0900

    feat(LottoMachine): 로또 머신 객체에 0원 이상 검증 추가

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -2,6 +2,7 @@ package lotto.model
 
 class LottoMachine(amount: Int) {
     init {
-        require(amount % 1000 == 0) { "[ERROR] 1,000원 단위의 금액으로 입력해 주세요." }
+        require(amount >= 0) { "[ERROR] 0원 이상의 금액으로 입력해 주세요. 입력값: $amount" }
+        require(amount % 1000 == 0) { "[ERROR] 1,000원 단위의 금액으로 입력해 주세요. 입력값: $amount" }
     }
 }
