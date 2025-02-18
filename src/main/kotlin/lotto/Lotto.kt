961debe78071a24b070779ea6d05cf53e0ff2fc6
gahyunkim <mapledt001@naver.com>
2025-02-18 15:44:48 +0900

    feat: Lotto 숫자 범위 지정 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,10 +1,9 @@
 package lotto
 
-class Lotto() {
-    val lottoNums = listOf(1, 2, 3, 4, 5, 6)
-
+class Lotto(val lottoNums: List<Int>) {
     init {
         require(lottoNums.size == DEFAULT_LOTTO_SIZE) { "로또는 6개의 숫자를 가져야 합니다" }
+        require(lottoNums.all { it in 1..45 }) { "로또 번호는 1에서 45까지의 숫자이다" }
     }
 
     companion object {
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,11 +2,26 @@ package lotto
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
     fun `로또는 6개의 숫자를 가진다`() {
-        val lotto = Lotto()
+        val nums = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(nums)
         assertThat(lotto.lottoNums.size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호는 1~45 숫자 사이에 해당한다`() {
+        val nums = listOf(1, 2, 3, 4, 5, 6)
+        val lotto = Lotto(nums)
+        assertThat(lotto.lottoNums.all { it in 1..45 }).isTrue()
+    }
+
+    @Test
+    fun `로또 번호는 1~45 숫자 사이에 없을 경우 예외가 발생한다`() {
+        val nums = listOf(1, 2, 3, 4, 5, 46)
+        assertThrows<IllegalArgumentException> { Lotto(nums) }
     }
 }
