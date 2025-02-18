d01e33835728f22336e0d4af14d91aaf342f35de
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-18 17:38:53 +0900

    refactor: `보너스 번호는 1부터 45 사이의 숫자여야 한다` add case

--- src/test/kotlin/lotto/WinningLottoTest.kt
+++ src/test/kotlin/lotto/WinningLottoTest.kt
@@ -17,6 +17,7 @@ class WinningLotto(
 class WinningLottoTest {
     @Test
     fun `보너스 번호는 1부터 45 사이의 숫자여야 한다`() {
+        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(1, 2, 3, 4, 5, 6), 0) }
         assertThrows<IllegalArgumentException> { WinningLotto(Lotto(1, 2, 3, 4, 5, 6), 46) }
     }
 }
