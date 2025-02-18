595fdbd46bcb0c6e5a747c222d05752d1a460114
moondev03 <moondev03@gmail.com>
2025-02-18 18:34:10 +0900

    feat: 보너스 번호 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -13,13 +13,20 @@ class InputView {
     fun readWinningNumbers(): List<Int> {
         println(READ_WINNING_NUMBERS_MESSAGE)
         val winningNumbersInput = scanner.nextLine().split(WINNING_NUMBERS_DELIMITER)
-        return winningNumbersInput.map { requireNotNull(it.toIntOrNull()) { INVALID_WINNING_NUMBER_MESSAGE } }
+        return winningNumbersInput.map { requireNotNull(it.toIntOrNull()) { INVALID_NUMBER_MESSAGE } }
+    }
+
+    fun readBonusNumber(): Int {
+        println(READ_BONUS_NUMBER_MESSAGE)
+        val bonusNumberInput = scanner.nextLine()
+        return requireNotNull(bonusNumberInput.toIntOrNull()) { INVALID_NUMBER_MESSAGE }
     }
 
     private companion object {
         const val WINNING_NUMBERS_DELIMITER = ','
         const val READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
         const val READ_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
-        const val INVALID_WINNING_NUMBER_MESSAGE = "[ERROR] 숫자만 입력해 주세요"
+        const val READ_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요."
+        const val INVALID_NUMBER_MESSAGE = "[ERROR] 숫자만 입력해 주세요."
     }
 }
