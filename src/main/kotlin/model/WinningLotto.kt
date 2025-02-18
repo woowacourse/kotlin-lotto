0931e336ea16fa4b75db02d0a993eeebef24cae3
이지윤 <02leejiyun@naver.com>
2025-02-18 16:54:47 +0900

    feat: 로또 번호와 당첨 로또의 일치하는 번호의 개수를 구하는 기능 구현

--- src/main/kotlin/model/WinningLotto.kt
+++ src/main/kotlin/model/WinningLotto.kt
@@ -1,10 +1,16 @@
 package model
 
 class WinningLotto(
-    winningNumbers: Lotto,
+    private val winningNumbers: Lotto,
     bonusNumber: LottoNumber,
 ) {
     init {
         require(!winningNumbers.lottoNumbers.map { it.number }.contains(bonusNumber.number))
+    }
+
+    fun getCountOfMatch(lotto: Lotto): Int {
+        val winningNumbers = winningNumbers.lottoNumbers.map { it.number }.toSet()
+        val lottoNumbers = lotto.lottoNumbers.map { it.number }.toSet()
+        return winningNumbers.intersect(lottoNumbers).size
     }
 }
--- src/test/kotlin/WinningLottoTest.kt
+++ src/test/kotlin/WinningLottoTest.kt
@@ -1,6 +1,7 @@
 import model.Lotto
 import model.LottoNumber
 import model.WinningLotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
@@ -24,5 +25,15 @@ class WinningLottoTest {
             val bonusNumber = LottoNumber(1)
             WinningLotto(winningNumbers, bonusNumber)
         }
+    }
+
+    @Test
+    fun `당첨 로또 번호와 일치하는 번호의 개수를 구한다`() {
+        val lottoNumbers = (1..6).map { LottoNumber(it) }
+        val winningNumbers = Lotto(lottoNumbers)
+        val bonusNumber = LottoNumber(45)
+        val winningLotto = WinningLotto(winningNumbers, bonusNumber)
+        val lotto = Lotto(lottoNumbers)
+        assertThat(winningLotto.getCountOfMatch(lotto)).isEqualTo(6)
     }
 }
