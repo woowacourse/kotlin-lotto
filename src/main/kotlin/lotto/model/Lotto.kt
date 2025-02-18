e2c19807cc7a02f8bd8d9a5866880daab1e622b3
junseo511 <prime54@g.skku.edu>
2025-02-18 17:39:37 +0900

    refactor(Lotto): 줄바꿈 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,6 +1,8 @@
 package lotto.model
 
-class Lotto(numbers: List<Int>) {
+class Lotto(
+    numbers: List<Int>,
+) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또는 6개의 번호만 가질 수 있습니다." }
         numbers.forEach { number ->
