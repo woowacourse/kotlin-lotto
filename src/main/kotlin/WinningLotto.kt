d1bbeb68d627952fd4686aa9d9090656423d8f16
chanho0908 <uni070@naver.com>
2025-02-18 16:18:09 +0900

    feat : 당첨 로또 보너스 번호, 당첨 번호 중복 유효성 검사 추가

---
+++ src/main/kotlin/WinningLotto.kt
@@ -0,0 +1,15 @@
+package lotto
+
+class WinningLotto(
+    lotto: Lotto,
+    bonusNumber: Int,
+) {
+    init {
+        require(bonusNumber !in lotto.numbers) { DUPLICATED_BONUS_NUMBER }
+    }
+
+    companion object {
+        const val ERROR = "[ERROR]"
+        const val DUPLICATED_BONUS_NUMBER = "$ERROR 보너스 번호는 로또 번호는 중복될 수 없습니다."
+    }
+}
