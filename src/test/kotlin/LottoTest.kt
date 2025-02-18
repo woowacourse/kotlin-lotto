85d6313ad03d3b11e96bd5d00b5a55fd66ef3538
chanho0908 <uni070@naver.com>
2025-02-18 15:21:13 +0900

    test : 로또 번호 범위 테스트 추가

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,5 +1,6 @@
 import lotto.Lotto
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,5 +13,15 @@ class LottoTest {
             Lotto(values)
         }.isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("[ERROR] 로또 번호는 6개 입니다.")
+    }
+
+    @ValueSource(strings = ["1,56,2,3,4,5", "45,56,67,87,23,1"])
+    @ParameterizedTest
+    fun `로또 번호는 1 부터 45 사이가 아니면 예외가 발생한다`(value: String){
+        val values = value.split(",").map { it.toInt() }
+        Assertions.assertThatThrownBy {
+            Lotto(values)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 로또 번호는 1부터 45 사이입니다.")
     }
 }
