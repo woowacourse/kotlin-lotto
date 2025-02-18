d4942b368309f9ae8999aecfc120e9da3cb581d4
moondev03 <moondev03@gmail.com>
2025-02-18 18:20:18 +0900

    feat: 구매한 로또 번호 출력 기능 구현

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,6 +5,10 @@ class OutputView {
         println(PRINT_PURCHASE_LOTTO_COUNT_FORMAT.format(purchaseLottoCount))
     }
 
+    fun printPurchaseLottoNumbers(lottoNumbers: List<Int>) {
+        println(lottoNumbers)
+    }
+
     private companion object {
         const val PRINT_PURCHASE_LOTTO_COUNT_FORMAT = "%s개 구매했습니다."
     }
