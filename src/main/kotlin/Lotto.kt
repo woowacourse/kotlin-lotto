771f1e94eef4814268fd0296b427508057c6f2ea
지원 <cheerfulboys@naver.com>
2025-02-18 15:22:09 +0900

    feat: 로또 가격 설정

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -1,3 +1,8 @@
 class Lotto {
-    val number: Int = (1..45).random()
+    val price: Int = LOTTO_PRICE
+    val numbers: List<Int> = (1..45).shuffled().take(6)
+
+    companion object {
+        const val LOTTO_PRICE: Int = 1_000
+    }
 }
--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,11 +1,22 @@
-import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
-
+import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
     fun `로또 번호는 1~45 사이이다`() {
         val lotto = Lotto()
-        assertThat(lotto.number).isIn(1..45)
+        assertThat(lotto.numbers.all { it in 1..45 }).isTrue()
+    }
+
+    @Test
+    fun `로또 번호는 중복되지 않는 6개의 숫자를 갖는다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 1장의 가격은 1000원이다`() {
+        val lotto = Lotto()
+        assertThat(lotto.price).isEqualTo(1_000)
     }
 }
