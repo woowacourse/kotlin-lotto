688e9c9e80646092f870ea7850701c3aeda8744e
moondev03 <moondev03@gmail.com>
2025-02-18 18:30:18 +0900

    feat: 당첨 번호 입력 기능 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -10,7 +10,16 @@ class InputView {
         return scanner.nextInt()
     }
 
+    fun readWinningNumbers(): List<Int> {
+        println(READ_WINNING_NUMBERS_MESSAGE)
+        val winningNumbersInput = scanner.nextLine().split(WINNING_NUMBERS_DELIMITER)
+        return winningNumbersInput.map { requireNotNull(it.toIntOrNull()) { INVALID_WINNING_NUMBER_MESSAGE } }
+    }
+
     private companion object {
+        const val WINNING_NUMBERS_DELIMITER = ','
         const val READ_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요."
+        const val READ_WINNING_NUMBERS_MESSAGE = "지난 주 당첨 번호를 입력해 주세요."
+        const val INVALID_WINNING_NUMBER_MESSAGE = "[ERROR] 숫자만 입력해 주세요"
     }
 }
