3d8185dfd3893ae7babe813399c96194f0e6d412
Leeyerin0210 <lyl8661@naver.com>
2025-02-18 15:21:16 +0900

    feat: 로또 객체 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,5 +1,11 @@
 package lotto
 
-class Lotto() {
-    val number = (1..45).random()
+class Lotto {
+    val numbers: List<Int> = (MINIMUM_LOTTO_VALUE..MAXIMUM_LOTTO_VALUE).shuffled().take(LOTTO_NUMBER_AMOUNT).sorted()
+
+    companion object {
+        private const val MINIMUM_LOTTO_VALUE = 1
+        private const val MAXIMUM_LOTTO_VALUE = 45
+        private const val LOTTO_NUMBER_AMOUNT = 6
+    }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,12 +1,32 @@
 package lotto
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class LottoTest {
     @Test
     fun `로또 번호는 1~45 사이이다`() {
         val lotto = Lotto()
-        assertThat(lotto.number in 1..45)
+        assertTrue(lotto.numbers.min() >= 1)
+        assertTrue(lotto.numbers.max() <= 45)
+    }
+
+    @Test
+    fun `로또 1매에는 6개의 로또 번호가 존재한다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호는 중복되지 않는다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.distinct().size).isEqualTo(6)
+    }
+
+    @Test
+    fun `로또 번호는 오름차순으로 정렬되어 있다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.sorted()).isEqualTo(lotto.numbers)
     }
 }
