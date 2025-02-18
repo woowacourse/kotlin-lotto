d497b7ce36a6b1cf41ec535f7dd312b554af9e9d
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-18 17:05:14 +0900

    refactor: make Lottos String constant

--- src/test/kotlin/lotto/LottosTest.kt
+++ src/test/kotlin/lotto/LottosTest.kt
@@ -13,11 +13,14 @@ class Lottos(
             price: Int,
             vararg lottos: Lotto,
         ): Lottos {
-            require(price >= Lotto.LOTTO_PRICE) { "최소 구입 금액은 ${Lotto.LOTTO_PRICE}원입니다." }
+            require(price >= Lotto.LOTTO_PRICE) { ERROR_MESSAGE_NOT_ENOUGH_MONEY }
             val count = price / Lotto.LOTTO_PRICE
-            require(count == lottos.size) { "구입 금액과 로또의 개수가 일치하지 않습니다." }
+            require(count == lottos.size) { ERROR_MESSAGE_NOT_MATCHED_MONEY_AND_LOTTOS_COUNT }
             return Lottos(*lottos)
         }
+
+        private const val ERROR_MESSAGE_NOT_ENOUGH_MONEY = "최소 구입 금액은 ${Lotto.LOTTO_PRICE}원입니다."
+        private const val ERROR_MESSAGE_NOT_MATCHED_MONEY_AND_LOTTOS_COUNT = "구입 금액과 로또의 개수가 일치하지 않습니다."
     }
 }
 
