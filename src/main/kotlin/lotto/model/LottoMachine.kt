d5e639c04d91e8e0c9e0d2b3add75132881e1148
junseo511 <prime54@g.skku.edu>
2025-02-18 16:52:44 +0900

    feat(LottoMachine): 구입 금액만큼 로또 갯수 반환 로직 추가

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -1,8 +1,12 @@
 package lotto.model
 
-class LottoMachine(amount: Int) {
+class LottoMachine(private val amount: Int) {
     init {
         require(amount >= 0) { "[ERROR] 0원 이상의 금액으로 입력해 주세요. 입력값: $amount" }
         require(amount % 1000 == 0) { "[ERROR] 1,000원 단위의 금액으로 입력해 주세요. 입력값: $amount" }
+    }
+
+    fun getLottoQuantity(): Int {
+        return amount / 1000
     }
 }
