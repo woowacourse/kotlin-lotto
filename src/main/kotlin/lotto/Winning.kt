af0c2c78b6f0ddc1df3558fb04928d60f0f5b130
gahyunkim <mapledt001@naver.com>
2025-02-18 16:38:27 +0900

    feat: Lotto 당첨 번호 기능 구현

---
+++ src/main/kotlin/lotto/Winning.kt
@@ -0,0 +1,10 @@
+package lotto
+
+class Winning(
+    val winningLotto: Lotto,
+    val bounusNumber: Int,
+) {
+    init {
+        require(winningLotto.lottoNums.contains(bounusNumber)) { "보너스 볼 번호는 당첨 번호와 중복될 수 없습니다." }
+    }
+}
