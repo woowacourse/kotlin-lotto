efee32e51f80119a6a9a50e2fbcd55a89ada00b8
chanho0908 <uni070@naver.com>
2025-02-18 15:21:53 +0900

    feat : 로또 번호 범위 유효성 검사 추가

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -3,11 +3,13 @@ package lotto
 class Lotto(numbers: List<Int>) {
     init {
         require(numbers.size == LOTTO_SIZE) { INVALID_LOTTO_SIZE }
+        require(numbers.all { it in (1..45) }) { INVALID_LOTTO_NUMBERS }
     }
 
     companion object {
         const val ERROR = "[ERROR]"
         const val LOTTO_SIZE = 6
         const val INVALID_LOTTO_SIZE = "$ERROR 로또 번호는 6개 입니다."
+        const val INVALID_LOTTO_NUMBERS = "$ERROR 로또 번호는 1부터 45 사이입니다."
     }
 }
