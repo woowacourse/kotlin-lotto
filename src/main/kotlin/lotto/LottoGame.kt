eb57ed89831f13f658374bf41e18b7425c7ec0d9
지원 <cheerfulboys@naver.com>
2025-02-18 17:04:07 +0900

    feat: 보너스 볼 일치 여부를 확인하는 기능 추가

--- src/main/kotlin/lotto/LottoGame.kt
+++ src/main/kotlin/lotto/LottoGame.kt
@@ -1,7 +1,22 @@
 package lotto
 
-class LottoGame(private val winningNumber: List<Int>) {
-    fun getSameNumberCount(lottoNumber: List<Int>): Int {
-        return winningNumber.intersect(lottoNumber).size
+import lotto.util.Rank
+
+class LottoGame(private val winningNumber: List<Int>, private val bonusNumber: Int) {
+    fun getSameNumberCount(lottoNumber: List<Int>): Rank {
+        return when (winningNumber.intersect(lottoNumber).size) {
+            6 -> Rank.FIRST
+            5 -> checkBonusNumber(lottoNumber)
+            4 -> Rank.FOURTH
+            3 -> Rank.FIFTH
+            else -> Rank.NONE
+        }
+    }
+
+    fun checkBonusNumber(number: List<Int>): Rank {
+        if (number.contains(bonusNumber)) {
+            return Rank.SECOND
+        }
+        return Rank.THIRD
     }
 }
--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,28 +1,19 @@
 package lotto.domain
 
+import lotto.Lotto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
-    fun `로또는 중복되지 않는 6개의 숫자를 갖는다`() {
-        val lotto = Lotto(listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)).toSet())
-        assertThat(lotto.getSortedLotto().size).isEqualTo(6)
+    fun `로또 번호는 1~45 사이이다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.all { it in 1..45 }).isTrue()
     }
 
     @Test
-    fun `로또는 중복된 숫자를 가지면 오류가 발생한다`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(LottoNumber(1), LottoNumber(1), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)).toSet())
-        }
-    }
-
-    @Test
-    fun `로또 번호는 오름차순으로 정렬된다`() {
-        val lotto = Lotto(listOf(LottoNumber(7), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)).toSet())
-        assertThat(
-            lotto.getSortedLotto(),
-        ).isEqualTo(listOf(LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6), LottoNumber(7)))
+    fun `로또 번호는 중복되지 않는 6개의 숫자를 갖는다`() {
+        val lotto = Lotto()
+        assertThat(lotto.numbers.size).isEqualTo(6)
     }
 }
