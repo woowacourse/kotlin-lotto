b40ad9c646b83b567e1d54af6d8a104ac2998993
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-19 19:50:47 +0900

    feat: OutputView 클래스 구현

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,10 +1,19 @@
 package lotto.view
 
+import lotto.model.LottoStore
+import lotto.model.RandomLottoGenerator
 import java.lang.String.format
 
 class OutputView {
     fun printLottoCount(number: Int) {
         println(format(OUTPUT_LOTTO_COUNT_MESSAGE, number))
+    }
+
+    fun printLottoBundle(input: String) {
+        val lottoBundle = LottoStore().getTickets(input, RandomLottoGenerator())
+        lottoBundle.forEach { lotto ->
+            println(lotto.numbers)
+        }
     }
 
     companion object {
