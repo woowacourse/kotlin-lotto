6a91f4c3c74462d7006963fdd3d4e35cefe6ef60
junseo511 <prime54@g.skku.edu>
2025-02-19 16:08:03 +0900

    test(LottoTest): 해당되는 당첨 번호에 맞는 로또 랭크 반환 테스트 추가

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -1,8 +1,10 @@
 package lotto.model
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoTest {
     @Test
@@ -11,19 +13,41 @@ class LottoTest {
         assertThrows<IllegalArgumentException> { Lotto(numbers) }
     }
 
-    @Test
-    fun `당첨 번호와 기존 로또가 동시에 보유하고 있는 번호 갯수를 반환한다`() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        val winningNumbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+    @ParameterizedTest
+    @MethodSource("lottoCasesForRankTest")
+    fun `각 로또는 해당되는 당첨 번호에 맞게 랭크가 반환된다`(matchCase: Pair<List<Int>, Rank>) {
+        val (lottoNumbers, expectedRank) = matchCase
+        val lotto = Lotto(lottoNumbers)
 
-        Assertions.assertEquals(6, lotto.compareWinningNumbers(winningNumbers))
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+
+        val rank = lotto.getRank(winningNumbers, bonusNumber)
+
+        assertEquals(expectedRank, rank)
     }
 
     @Test
-    fun `기존 로또가 보너스 번호를 보유하고 있는지 여부를 반환한다`() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        val bonusNumber = 1
+    fun `보너스 번호가 일치하면서 5개의 번호가 동일한 경우 2등을 반환한다`() {
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
 
-        Assertions.assertTrue(lotto.isHaveBonusNumber(bonusNumber))
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val rank = lotto.getRank(winningNumbers, bonusNumber)
+
+        assertEquals(Rank.SECOND, rank)
+    }
+
+    companion object {
+        @JvmStatic
+        fun lottoCasesForRankTest(): List<Pair<List<Int>, Rank>> {
+            return listOf(
+                listOf(1, 2, 3, 4, 5, 6) to Rank.FIRST,
+                listOf(1, 2, 3, 4, 5, 8) to Rank.THIRD,
+                listOf(1, 2, 3, 4, 9, 10) to Rank.FOURTH,
+                listOf(1, 2, 3, 8, 9, 10) to Rank.FIFTH,
+                listOf(1, 2, 8, 9, 10, 11) to Rank.MISS,
+            )
+        }
     }
 }
