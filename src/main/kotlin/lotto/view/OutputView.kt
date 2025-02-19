b1176f04bd9a535c00079f51f97438d01ee5435f
지원 <cheerfulboys@naver.com>
2025-02-19 16:43:06 +0900

    feat: 로또, 당첨 통계, 수익률 출력하는 OutputView 기능 추가

---
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -0,0 +1,60 @@
+package lotto.view
+
+import lotto.domain.Lotto
+import lotto.util.Rank
+
+class OutputView {
+    fun printPurchasedLottos(
+        count: Int,
+        lottos: List<Lotto>,
+    ) {
+        println(MESSAGE_BUY_LOTTO.format(count))
+        lottos.forEach { lotto ->
+            println(lotto)
+        }
+    }
+
+    fun printWinningStats(winningStats: Map<Rank, Int>) {
+        println(MESSAGE_WINNING_STATS)
+        for ((state, count) in winningStats) {
+            printWinningStat(state, count)
+        }
+    }
+
+    private fun printWinningStat(
+        state: Rank,
+        count: Int,
+    ) {
+        if (state.countOfMatch == 2) {
+            printWinningStatWithBonusBall(state, count)
+        } else {
+            printWinningStatWIthNoBonusBall(state, count)
+        }
+    }
+
+    private fun printWinningStatWIthNoBonusBall(
+        state: Rank,
+        count: Int,
+    ) {
+        println(MESSAGE_MATCH_COUNT.format(state.countOfMatch, state.price, count))
+    }
+
+    private fun printWinningStatWithBonusBall(
+        state: Rank,
+        count: Int,
+    ) {
+        println(MESSAGE_MATCH_COUNT_WITH_BONUS_BALL.format(state.countOfMatch, state.price, count))
+    }
+
+    fun printProfit(profit: Double) {
+        println(MESSAGE_PROFIT.format(profit))
+    }
+
+    companion object {
+        const val MESSAGE_BUY_LOTTO = "%d개를 구매했습니다."
+        const val MESSAGE_WINNING_STATS = "당첨 통계\n---------"
+        const val MESSAGE_MATCH_COUNT = "%d개 일치 (%d원)- %d개"
+        const val MESSAGE_MATCH_COUNT_WITH_BONUS_BALL = "%d개 일치, 보너스 볼 일치(%d원)- %d개"
+        const val MESSAGE_PROFIT = "총 수익률은 %lf입니다."
+    }
+}
