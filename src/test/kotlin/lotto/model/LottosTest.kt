d792f4c1a677d90094ea256c5d642191ae42faaa
junseo511 <prime54@g.skku.edu>
2025-02-19 17:13:49 +0900

    test(LottosTest): 각 랭크별 당첨된 로또 개수 반환 테스트 추가

--- src/test/kotlin/lotto/model/LottosTest.kt
+++ src/test/kotlin/lotto/model/LottosTest.kt
@@ -1,3 +1,35 @@
 package lotto.model
 
-class LottosTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class LottosTest {
+    @Test
+    fun `각 랭크별 당첨된 로또 개수를 정확하게 반환한다`() {
+        val lottos =
+            listOf(
+                Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                Lotto(listOf(1, 2, 3, 4, 5, 7)),
+                Lotto(listOf(1, 2, 3, 4, 5, 8)),
+                Lotto(listOf(1, 2, 3, 4, 8, 9)),
+                Lotto(listOf(1, 2, 3, 8, 9, 10)),
+                Lotto(listOf(8, 9, 10, 11, 12, 13)),
+            )
+
+        val winningNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val bonusNumber = 7
+
+        val expectedResult =
+            mapOf(
+                Rank.FIRST to 1,
+                Rank.SECOND to 1,
+                Rank.THIRD to 1,
+                Rank.FOURTH to 1,
+                Rank.FIFTH to 1,
+                Rank.MISS to 1,
+            )
+        val actualResult = Lottos(lottos).countLottoByRank(winningNumbers, bonusNumber)
+
+        Assertions.assertEquals(expectedResult, actualResult)
+    }
+}
