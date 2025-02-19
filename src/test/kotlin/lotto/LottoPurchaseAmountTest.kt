f46cd8c12975c2911972fd280626dacb07a4165b
yrsel <xxknu88@gmail.com>
2025-02-19 15:54:10 +0900

    feat: 로또 구입 금액 범위 예외 처리 구현

--- src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
+++ src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
@@ -6,7 +6,15 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class LottoPurchaseAmountTest {
     @ParameterizedTest
-    @ValueSource(ints = [0, 999, 1001, 1234, 10100])
+    @ValueSource(ints = [-1_000, 0, 999, 100_001])
+    fun `구입 금액은 최소 1,000원 이상 최대 100,000원 이하가 아닐 경우 예외를 발생한다`(money: Int) {
+        assertThatIllegalArgumentException()
+            .isThrownBy { LottoPurchaseAmount(money) }
+            .withMessage("구입 금액은 최소 1,000원 이상 최대 100,000원 이하 입니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1_100, 10_100])
     fun `로또 구입 금액은 1,000원 단위가 아닌 경우 예외를 발생한다`(money: Int) {
         assertThatIllegalArgumentException()
             .isThrownBy { LottoPurchaseAmount(money) }
