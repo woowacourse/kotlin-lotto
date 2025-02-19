1ccc4e6befdb1513acd671a95e4e56635211412a
junseo511 <prime54@g.skku.edu>
2025-02-19 17:45:02 +0900

    test(LottoMachineTest): 로또의 당첨 결과에 맞는 수익률 반환 테스트 추가

--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -30,4 +30,25 @@ class LottoMachineTest {
 
         Assertions.assertEquals(5, lottos.lottos.size)
     }
+
+    @Test
+    fun `로또의 당첨 결과에 맞는 수익률을 소수점 둘째 자리까지 반환한다`() {
+        val winningResult =
+            mapOf(
+                Rank.FIRST to 1,
+                Rank.SECOND to 0,
+                Rank.THIRD to 0,
+                Rank.FOURTH to 0,
+                Rank.FIFTH to 1,
+                Rank.MISS to 100,
+            )
+
+        val totalAmount = winningResult.values.size * 1000
+        val lottoMachine = LottoMachine(totalAmount)
+
+        val totalProfit = (Rank.FIRST.winningMoney + Rank.FIFTH.winningMoney).toFloat()
+        val expectedProfitRate = (totalProfit / totalAmount.toFloat())
+
+        Assertions.assertEquals(expectedProfitRate, lottoMachine.getProfitRate(winningResult))
+    }
 }
