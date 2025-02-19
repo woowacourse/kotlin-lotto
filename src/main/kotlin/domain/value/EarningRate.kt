4d7b55baff6493cac7954e268a33f11e70fadbd5
이지윤 <02leejiyun@naver.com>
2025-02-19 16:47:54 +0900

    fix: 수익률 검증 로직 수정

--- src/main/kotlin/domain/value/EarningRate.kt
+++ src/main/kotlin/domain/value/EarningRate.kt
@@ -4,11 +4,10 @@ class EarningRate(
     val rate: Double,
 ) {
     init {
-        require(rate in MINIMUM_EARNING_RATE..MAXIMUM_EARNING_RATE)
+        require(rate >= MINIMUM_EARNING_RATE)
     }
 
     companion object {
         private const val MINIMUM_EARNING_RATE = 0.0
-        private const val MAXIMUM_EARNING_RATE = 1.0
     }
 }
--- src/test/kotlin/EarningRateTest.kt
+++ src/test/kotlin/EarningRateTest.kt
@@ -7,16 +7,16 @@ import java.lang.IllegalArgumentException
 
 class EarningRateTest {
     @ParameterizedTest
-    @ValueSource(doubles = [0.0, 0.1, 0.5, 1.0])
-    fun `수익률은 0 이상 1 이하인 실수이다`(rate: Double) {
+    @ValueSource(doubles = [0.0, 0.1, 1.0, 1000.0])
+    fun `수익률은 0 이상의 실수이다`(rate: Double) {
         assertDoesNotThrow {
             EarningRate(rate)
         }
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = [-0.1, 1.1, 2.0])
-    fun `수익률이 0 이상 1 이하가 아니면 예외가 발생한다`(rate: Double) {
+    @ValueSource(doubles = [-0.1, -1.0, -1000.0])
+    fun `수익률이 0 이상이 아니면 예외가 발생한다`(rate: Double) {
         assertThrows<IllegalArgumentException> {
             EarningRate(rate)
         }
