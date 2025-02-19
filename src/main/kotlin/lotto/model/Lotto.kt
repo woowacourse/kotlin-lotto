8bdcc9703d51cc1b26563fdb6195739c2a803861
junseo511 <prime54@g.skku.edu>
2025-02-19 14:26:27 +0900

    feat(Lotto): 로또의 우승 번호 일치 갯수를 반환하는 로직 추가

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,9 +4,24 @@ class Lotto(
     numbers: List<Int>,
 ) {
     init {
+        validateLottoNumbersCount(numbers)
+        validateLottoNumbersRange(numbers)
+    }
+
+    private fun validateLottoNumbersCount(numbers: List<Int>) {
         require(numbers.size == 6) { "[ERROR] 로또는 6개의 번호만 가질 수 있습니다." }
+    }
+
+    private fun validateLottoNumbersRange(numbers: List<Int>) {
         numbers.forEach { number ->
             require(number in 1..45) { "[ERROR] 로또 번호의 범위는 1 이상 45 이하여야 합니다." }
         }
+    }
+
+    fun compareWinningNumbers(winningNumbers: List<Int>): Int {
+        validateLottoNumbersRange(winningNumbers)
+        validateLottoNumbersCount(winningNumbers)
+
+        return numbers.count { existNumber -> winningNumbers.contains(existNumber) }
     }
 }
