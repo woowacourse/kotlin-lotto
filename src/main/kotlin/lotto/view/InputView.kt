3778dae59ff31c52f57397c2a4d6cb23126b5c1b
yrsel <xxknu88@gmail.com>
2025-02-19 21:51:46 +0900

    feat: 로또 당첨 번호 입력 함수 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -3,7 +3,13 @@ package lotto.view
 class InputView {
     fun readLottoPurchaseAmount(): Int = readln().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER)
 
+    fun readWinningLottoNumbers(): List<Int> =
+        readln()
+            .split(LOTTO_NUMBER_DELIMITER)
+            .map { number -> number.trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER) }
+
     companion object {
         private const val ERROR_NOT_A_NUMBER = "숫자만 입력 가능합니다."
+        private const val LOTTO_NUMBER_DELIMITER = ","
     }
 }
