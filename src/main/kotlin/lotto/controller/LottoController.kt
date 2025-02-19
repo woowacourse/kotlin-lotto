59c208fc1706d5cbff48bb27b545b3a744616bd7
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-19 16:27:45 +0900

    feat: implement LottoController

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,14 +1,45 @@
 package lotto.controller
 
+import lotto.domain.Lotto
 import lotto.domain.LottoResult
+import lotto.domain.Lottos
+import lotto.domain.WinningLotto
+import lotto.view.View
 
-object LottoController {
-    fun getProfitRate(
-        lottoResults: List<LottoResult>,
-        price: Int,
-    ): Double {
-        val profit: Long = lottoResults.sumOf { lottoResult: LottoResult -> lottoResult.prizeAmount.toLong() }
-        val profitRate = profit / price.toDouble()
-        return profitRate
+class LottoController {
+    private val view = View
+    lateinit var winningLotto: WinningLotto
+    lateinit var boughtLottos: Lottos
+
+    fun buyLottos() {
+        val price: Int = view.readPrice()
+        val lottoNumbers: List<List<Int>> = List(price / Lotto.LOTTO_PRICE) { makeRandomNumbers(Lotto.LOTTO_SIZE) }
+        val lottos: Lottos =
+            Lottos.buy(
+                price = price,
+                *(
+                    lottoNumbers.map { lottoNumber: List<Int> -> Lotto(*lottoNumber.toIntArray()) }
+                ).toTypedArray(),
+            )
+        boughtLottos = lottos
+        view.showLottos(lottos)
     }
+
+    fun readWinningLotto() {
+        winningLotto = view.readWinningLotto()
+    }
+
+    fun showResult() {
+        val lottoResults: List<LottoResult> = boughtLottos.lottos.map { lotto -> LottoResult.from(winningLotto, lotto) }
+        view.showResult(lottoResults = lottoResults, profitRate = lottoResults.getProfitRate)
+    }
+
+    private fun makeRandomNumbers(size: Int): List<Int> = (Lotto.LOTTO_NUMBER_MIN..Lotto.LOTTO_NUMBER_MAX).shuffled().subList(0, size)
+
+    private val List<LottoResult>.getProfitRate: Double
+        get() {
+            val profit: Long = this.sumOf { lottoResult: LottoResult -> lottoResult.prizeAmount.toLong() }
+            val profitRate = profit / (this.size * Lotto.LOTTO_PRICE).toDouble()
+            return profitRate
+        }
 }
--- src/main/kotlin/lotto/view/View.kt
+++ src/main/kotlin/lotto/view/View.kt
@@ -1,31 +1,25 @@
 package lotto.view
 
 import lotto.domain.Lotto
+import lotto.domain.LottoResult
 import lotto.domain.Lottos
 import lotto.domain.WinningLotto
+import kotlin.math.floor
 
 object View {
-    fun readPriceInput() {
+    fun readPrice(): Int {
         println(MESSAGE_ENTER_PRICE)
         val input: String = readLine() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT_STATE)
         val price: Int = input.toIntOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_INPUT_NOT_A_NUMBER)
+        return price
         val lottoCount: Int = price / Lotto.LOTTO_PRICE
-
         println("${lottoCount}개를 구매했습니다.")
-        val lottoNumbers: List<List<Int>> = List(lottoCount) { makeRandomNumbers(Lotto.LOTTO_SIZE) }
-        val lottos: Lottos =
-            Lottos.buy(
-                price = price,
-                *(
-                    lottoNumbers.map { lottoNumber: List<Int> -> Lotto(*lottoNumber.toIntArray()) }
-                ).toTypedArray(),
-            )
+    }
+
+    fun showLottos(lottos: Lottos) {
         lottos.lottos.forEach { lotto: Lotto ->
             println(lotto.numbers.sorted())
         }
-    }
-
-    fun requestWinningLotto() {
     }
 
     fun readWinningLotto(): WinningLotto {
@@ -46,13 +40,34 @@ object View {
         return WinningLotto(wonLotto, bonusNumber)
     }
 
-    private fun makeRandomNumbers(size: Int): List<Int> = (Lotto.LOTTO_NUMBER_MIN..Lotto.LOTTO_NUMBER_MAX).shuffled().subList(0, size)
+    fun showResult(
+        lottoResults: List<LottoResult>,
+        profitRate: Double,
+    ) {
+        println(MESSAGE_RESULT_HEADER)
+        println("3개 일치 (5000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FIFTH }}개")
+        println("4개 일치 (50000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FOURTH }}개")
+        println("5개 일치 (1500000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.THIRD }}개")
+        println("5개 일치, 보너스 볼 일치(30000000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.SECOND }}개")
+        println("6개 일치 (2000000000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FIRST }}개")
+        println("총 수익률은 ${floor(profitRate * 100 / 100)}입니다.(기준이 1이기 때문에 결과적으로 ${makeProfitRateDescription(profitRate)} 의미임)")
+    }
+
+    private fun makeProfitRateDescription(profitRate: Double): String =
+        when {
+            profitRate < 1.0 -> "손해라는"
+            profitRate == 1.0 -> "본전이라는"
+            profitRate > 1.0 -> "이득이라는"
+            else -> throw IllegalStateException(ERROR_MESSAGE_ILLEGAL_STATE)
+        }
 
     private const val LOTTO_NUMBERS_DELIMITER = ","
     private const val MESSAGE_ENTER_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
     private const val MESSAGE_ENTER_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     private const val MESSAGE_ENTER_PRICE = "구입금액을 입력해 주세요."
     private const val MESSAGE_INVALID_WINNING_NUMBERS = "양식에 맞게 입력해주세요. (1, 2, 3, 4, 5, 6)"
+    private const val MESSAGE_RESULT_HEADER = "당첨 통계\n---------"
     private const val ERROR_MESSAGE_INVALID_INPUT_STATE = "정상적으로 입력되지 않았습니다."
     private const val ERROR_MESSAGE_INPUT_NOT_A_NUMBER = "숫자를 입력해주세요."
+    private const val ERROR_MESSAGE_ILLEGAL_STATE = "연산에 실패했습니다."
 }
