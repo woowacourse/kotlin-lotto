334871b24fbe20eb4996318c7a51d61354e86764
gahyunkim <mapledt001@naver.com>
2025-02-19 15:58:03 +0900

    feat: 메인 로직 구현

---
+++ src/main/kotlin/lotto/LottoController.kt
@@ -0,0 +1,12 @@
+package lotto
+
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun run() {
+    }
+}
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,3 +1,7 @@
 package lotto.view
 
-object OutputView
+object OutputView {
+    fun printLottoAmount(amount: String) {
+        println("${amount.toInt()}개를 구매했습니다.")
+    }
+}
