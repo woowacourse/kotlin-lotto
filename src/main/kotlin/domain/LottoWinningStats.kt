9255e319ec34283bd9ba92747ca6dfa22fe3440f
이지윤 <02leejiyun@naver.com>
2025-02-19 17:10:01 +0900

    feat: 당첨 정보를 바탕으로 수익률 계산

--- src/main/kotlin/domain/LottoWinningStats.kt
+++ src/main/kotlin/domain/LottoWinningStats.kt
@@ -1,19 +1,39 @@
 package domain
 
 import Rank
+import constants.LottoConstants
+import domain.value.EarningRate
 
 class LottoWinningStats(
     private val winningLotto: WinningLotto,
     private val lottos: List<Lotto>,
 ) {
-    private val winningStats: MutableMap<Rank, Int> =
+    val winningStats: MutableMap<Rank, Int> =
         Rank.entries.associateWith { 0 }.toMutableMap()
 
-    fun getLottoRanks(): Map<Rank, Int> {
+    init {
+        initializeLottoRanks()
+    }
+
+    fun calculateEarningRate(): EarningRate {
+        val purchaseAmount = lottos.size * LottoConstants.LOTTO_PRICE
+        val winningAmount = calculateWinningAmount()
+        val rate = winningAmount.toDouble() / purchaseAmount
+        return EarningRate(rate)
+    }
+
+    private fun initializeLottoRanks() {
         lottos.forEach {
             val rank = winningLotto.getRank(it)
             winningStats[rank] = (winningStats[rank]?.plus(1)) ?: 1
         }
-        return winningStats
+    }
+
+    private fun calculateWinningAmount(): Int {
+        var result = 0
+        winningStats.forEach { (rank, count) ->
+            result += rank.winningMoney * count
+        }
+        return result
     }
 }
--- src/test/kotlin/LottoWinningStatsTest.kt
+++ src/test/kotlin/LottoWinningStatsTest.kt
@@ -2,7 +2,7 @@ import domain.Lotto
 import domain.LottoWinningStats
 import domain.WinningLotto
 import domain.value.LottoNumber
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -23,9 +23,7 @@ class LottoWinningStatsTest {
         val lottos = listOf(Lotto(firstRankNumbers))
         val lottoWinningStats = LottoWinningStats(winningLotto, lottos)
 
-        Assertions
-            .assertThat(lottoWinningStats.getLottoRanks()[Rank.FIRST])
-            .isEqualTo(1)
+        assertThat(lottoWinningStats.winningStats[Rank.FIRST]).isEqualTo(1)
     }
 
     @Test
@@ -35,8 +33,17 @@ class LottoWinningStatsTest {
         val lottos = listOf(Lotto(firstRankNumbers), Lotto(missRankNumbers))
         val lottoWinningStats = LottoWinningStats(winningLotto, lottos)
 
-        Assertions
-            .assertThat(lottoWinningStats.getLottoRanks()[Rank.MISS])
-            .isEqualTo(1)
+        assertThat(lottoWinningStats.winningStats[Rank.MISS]).isEqualTo(1)
+    }
+
+    @Test
+    fun `당첨 수익률을 계산한다`() {
+        val thirdRankNumbers = (4..9).map { LottoNumber(it) }
+        val missRankNumbers = (11..16).map { LottoNumber(it) }
+        val lottos = mutableListOf(Lotto(thirdRankNumbers))
+        repeat(13) { lottos.add(Lotto(missRankNumbers)) }
+
+        val winningStats = LottoWinningStats(winningLotto, lottos)
+        assertThat(winningStats.calculateEarningRate().rate).isEqualTo(0.36)
     }
 }
