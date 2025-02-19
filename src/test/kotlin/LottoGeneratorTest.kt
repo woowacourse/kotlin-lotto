806b6a72f5e671b9d4a4c3488751b384d4a50012
chanho0908 <uni070@naver.com>
2025-02-19 15:44:03 +0900

    test : 구매한 로또 개수 테스트 추가

---
+++ src/test/kotlin/LottoGeneratorTest.kt
@@ -0,0 +1,12 @@
+import lotto.domain.LottoGenerator
+import lotto.domain.model.PurchasePrice
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoGeneratorTest {
+    @Test
+    fun `구입 금액이 2천원이면 로또를 2개 발행한다`() {
+        val money = PurchasePrice(2000)
+        assertThat(LottoGenerator(money).makeLotto().values.size).isEqualTo(2)
+    }
+}
