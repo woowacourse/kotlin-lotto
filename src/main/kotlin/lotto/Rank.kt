1acda9ffd8d6a7b4bc82afdc5ab61c88df1e11f1
yrsel <xxknu88@gmail.com>
2025-02-19 15:14:23 +0900

    feat: 로또 등수 열거객체 구현

---
+++ src/main/kotlin/lotto/Rank.kt
@@ -0,0 +1,14 @@
+package lotto
+
+enum class Rank(
+    private val matchCount: Int,
+    private val isMatchedBonus: Boolean,
+    private val prizeMoney: Int,
+) {
+    FIRST(6, false, 2_000_000_000),
+    SECOND(5, true, 30_000_000),
+    THIRD(5, false, 1_500_000),
+    FOURTH(4, false, 50_000),
+    FIFTH(3, false, 5_000),
+    MISS(0, false, 0),
+}
