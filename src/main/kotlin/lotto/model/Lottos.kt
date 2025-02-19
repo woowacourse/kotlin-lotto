99d58e7731951b04c3c6e20d5432066d2bf8805f
junseo511 <prime54@g.skku.edu>
2025-02-19 17:14:54 +0900

    feat(Lottos): 각 랭크별 당첨된 로또 개수 반환 로직 추가

--- src/main/kotlin/lotto/model/Lottos.kt
+++ src/main/kotlin/lotto/model/Lottos.kt
@@ -2,4 +2,18 @@ package lotto.model
 
 class Lottos(
     val lottos: List<Lotto>,
-)
+) {
+    fun countLottoByRank(
+        winningNumbers: List<Int>,
+        bonusNumber: Int,
+    ): Map<Rank, Int> {
+        val countResult = Rank.entries.associateWith { 0 }.toMutableMap()
+
+        lottos.forEach { lotto ->
+            val rank = lotto.getRank(winningNumbers, bonusNumber)
+            countResult[rank] = countResult.getValue(rank) + 1
+        }
+
+        return countResult
+    }
+}
