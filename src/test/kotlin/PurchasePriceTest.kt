20c8f3960605c91339b45c3d3c699d676ab8cbef
chanho0908 <uni070@naver.com>
2025-02-19 15:16:41 +0900

    test : 구입 금액 유효성 검사 테스트 추가

---
+++ src/test/kotlin/PurchasePriceTest.kt
@@ -0,0 +1,22 @@
+import lotto.domain.model.PurchasePrice
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class PurchasePriceTest {
+
+    @Test
+    fun `구입 금액이 0 이하면 예외가 발생한다`(){
+        Assertions.assertThatThrownBy {
+            PurchasePrice(0)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 천원 이상 입력해주세요.")
+    }
+
+    @Test
+    fun `구입 금액이 천원 단위가 아니면 예외가 발생한다`(){
+        Assertions.assertThatThrownBy {
+            PurchasePrice(10001)
+        }.isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("[ERROR] 천원 단위로 입력해주세요.")
+    }
+}
