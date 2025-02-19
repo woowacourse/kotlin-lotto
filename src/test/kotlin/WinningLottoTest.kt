91ee55ad44ea1f37ab77bb5c1ef1e3487e89b444
chanho0908 <uni070@naver.com>
2025-02-19 15:45:38 +0900

    refactor : 로또 자료구조 변경

--- src/test/kotlin/WinningLottoTest.kt
+++ src/test/kotlin/WinningLottoTest.kt
@@ -2,11 +2,12 @@ import lotto.Lotto
 import lotto.WinningLotto
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import java.util.TreeSet
 
 class WinningLottoTest {
     @Test
     fun `로또 번호와 보너스 번호가 중복되면 예외가 발생한다`() {
-        val lotto = Lotto(listOf(1, 3, 4, 5, 6, 7))
+        val lotto = Lotto(TreeSet(setOf(1, 3, 4, 5, 6, 7)))
         Assertions.assertThatThrownBy {
             WinningLotto(lotto, 6)
         }.isInstanceOf(IllegalArgumentException::class.java)
