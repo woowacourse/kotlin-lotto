67632b8d6bbe8ab76a7c6335a6aa9baf77d63461
chanho0908 <uni070@naver.com>
2025-02-20 18:55:23 +0900

    test : 로또 수익률 테스트 함수 수정

--- src/test/kotlin/domain/model/LottoResultTest.kt
+++ src/test/kotlin/domain/model/LottoResultTest.kt
@@ -5,17 +5,14 @@ import org.junit.jupiter.api.Test
 
 class LottoResultTest {
     @Test
-    fun `Lotto 결과에 따른 수익률 구하기`() {
+    fun `Lotto 당첨 결과에 따른 수익률 테스트`() {
         val profitRate =
             LottoResult(
-                mutableMapOf(
-                    Rank.FIRST to 1,
-                    Rank.SECOND to 2,
-                    Rank.THIRD to 3,
-                    Rank.FOURTH to 4,
+                mapOf(
+                    Rank.FOURTH to 1,
                 ),
-            ).getProfitRate(PurchasePrice(1000000))
+            ).getProfitRate(PurchasePrice(11_000_0))
 
-        assertThat(profitRate).isEqualTo("2064.70")
+        assertThat(profitRate).isEqualTo("0.45")
     }
 }
