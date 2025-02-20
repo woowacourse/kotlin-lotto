eb886ea667da0e12b71daa2cf888cd185aaae729
cucumber99 <dlsguq1@gmail.com>
2025-02-20 15:00:17 +0900

    test: 로또 번호 테스트 추가
    로또 번호 객체가 생성되는 케이스를 테스트합니다.

--- src/test/kotlin/lotto/model/LottoNumberTest.kt
+++ src/test/kotlin/lotto/model/LottoNumberTest.kt
@@ -1,13 +1,22 @@
 package lotto.model
 
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
     @ParameterizedTest
-    @ValueSource(ints = [0, 1, 46, 47])
-    fun `로또 번호가 1~45가 아닌 경우 예외가 발생한다`(numbers: Int) {
+    @ValueSource(ints = [1, 2, 3, 43, 44, 45])
+    fun `로또 번호는 1~45 사이여야 한다`(numbers: Int) {
+        assertDoesNotThrow {
+            LottoNumber(numbers)
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0, 46, 47])
+    fun `로또 번호가 1~45 사이가 아닌 경우 예외가 발생한다`(numbers: Int) {
         assertThrows<IllegalArgumentException> {
             LottoNumber(numbers)
         }
