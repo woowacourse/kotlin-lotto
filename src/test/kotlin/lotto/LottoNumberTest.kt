1cce3a7de642fc8abf5574cfa9dd115e4fcff80e
yrsel <xxknu88@gmail.com>
2025-02-20 15:16:25 +0900

    test: 로또 번호 정상범위 동작 테스트 추가

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import lotto.model.LottoNumber
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,5 +13,13 @@ class LottoNumberTest {
         assertThatIllegalArgumentException()
             .isThrownBy { LottoNumber(number) }
             .withMessage("로또 번호는 1 ~ 45 사이의 숫자입니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 45, 10])
+    fun `로또 번호는 1~45 사이의 숫자 일 경우 예외를 발생하지 않는다`(number: Int) {
+        Assertions.assertDoesNotThrow {
+            LottoNumber(number)
+        }
     }
 }
