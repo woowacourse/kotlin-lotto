d72fbd8f09bc802181d1839b1a543e17042a18fc
HamBeomJoon <hbj0802@naver.com>
2025-02-20 15:11:44 +0900

    feat: 구입 금액 음수 예외처리

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -5,6 +5,9 @@ class InputView {
         require(amount.toIntOrNull() != null) {
             IllegalArgumentException(ERROR_INVALID_AMOUNT)
         }
+        require(amount.toInt() > ZERO) {
+            IllegalArgumentException(ERROR_INVALID_OVER_ZERO)
+        }
         require(amount.toInt() % LOTTO_AMOUNT == ZERO) {
             IllegalArgumentException(ERROR_INVALID_DIVISION)
         }
@@ -42,6 +45,7 @@ class InputView {
         private const val MESSAGE_INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
 
         private const val ERROR_INVALID_AMOUNT = "구입금액은 정수를 입력해야 합니다."
+        private const val ERROR_INVALID_OVER_ZERO = "구입금액은 0보다 커야 합니다."
         private const val ERROR_INVALID_DIVISION = "구입금액은 1000의 배수여야 합니다."
         private const val ERROR_INVALID_WINNING_TYPE = "당첨번호는 정수를 입력해야 합니다."
 
