00ec1f0f347e9f68fe406cf135068d75e602a6d1
medAndro <med4ndro@gmail.com>
2025-02-20 16:40:21 +0900

    chore: 중복된 번호 예외 메시지 구체화

--- src/main/kotlin/lotto/domain/model/Lotto.kt
+++ src/main/kotlin/lotto/domain/model/Lotto.kt
@@ -13,7 +13,15 @@ class Lotto(
             )
         }
         require(lottoNumbers.map { it.number }.distinct().size == LottoConstants.NUMBER_OF_LOTTO_NUMBERS) {
-            HAS_DUPLICATE_NUMBERS
+            HAS_DUPLICATE_NUMBERS.format(
+                lottoNumbers
+                    .map { it.number }
+                    .groupBy { it }
+                    .filter { it.value.size > 1 }
+                    .keys
+                    .toList()
+                    .joinToString(", "),
+            )
         }
         require(lottoNumbers.map { it.number }.zipWithNext().all { (a, b) -> a < b }) {
             ASCENDING_ORDER_IS_REQUIRED
@@ -23,7 +31,7 @@ class Lotto(
     companion object {
         private const val TOO_MANY_LOTTO_NUMBERS =
             "%d개가 아닌 \n${LottoConstants.NUMBER_OF_LOTTO_NUMBERS}개로 구성된 lottoNumbers 리스트는를 입력하세요"
-        private const val HAS_DUPLICATE_NUMBERS = "lottoNumbers 리스트에 중복된 번호가 존재합니다."
+        private const val HAS_DUPLICATE_NUMBERS = "lottoNumbers 리스트에 중복된 번호(%s)가 존재합니다."
         private const val ASCENDING_ORDER_IS_REQUIRED = "lottoNumbers 리스트는 오름차순 정렬 후 입력되어야 합니다."
     }
 }
