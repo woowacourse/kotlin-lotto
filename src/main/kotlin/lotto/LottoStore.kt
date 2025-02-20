60d64727158e587eb465a73ae5d9cc4c43c17f8c
HamBeomJoon <hbj0802@naver.com>
2025-02-20 15:02:17 +0900

    feat: 당첨 결과, 수익률 출력

--- src/main/kotlin/lotto/LottoStore.kt
+++ src/main/kotlin/lotto/LottoStore.kt
@@ -3,6 +3,7 @@ package lotto
 import lotto.model.Lotto
 import lotto.model.LottoMachine
 import lotto.model.LottoNumber
+import lotto.model.LottoResult
 import lotto.model.LottoScanner
 import lotto.view.InputView
 import lotto.view.OutputView
@@ -12,6 +13,14 @@ class LottoStore(
     private val outputView: OutputView = OutputView(),
 ) {
     fun calculatePurchaseCount(amount: Int) = amount / 1000
+
+    fun formattingWinningStatus(result: LottoResult): Map<Rank, Int> {
+        val resultMap = mutableMapOf<Rank, Int>()
+        Rank.entries.filter { it != Rank.MISS }.map { rank ->
+            resultMap[rank] = result.getWinningStatus().getOrDefault(rank, 0)
+        }
+        return resultMap
+    }
 
     fun run() {
         val amount = inputView.inputPurchaseAmount()
@@ -25,8 +34,8 @@ class LottoStore(
         val bonusNumber = inputView.inputBonusNumber()
         val winning = Lotto(winningNumbers.map { LottoNumber(it) }, LottoNumber(bonusNumber))
         val result = LottoScanner(winning).getResult(lottoTicket.lottos)
-        println(result.getWinningStatus())
-        outputView.printResult(result)
+        val formattedWinningStatus = formattingWinningStatus(result)
+        outputView.printResult(formattedWinningStatus)
         outputView.printProfit(result.calculateProfit())
     }
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,7 +1,9 @@
 package lotto.view
 
+import lotto.Rank
 import lotto.model.Lotto
-import lotto.model.LottoResult
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class OutputView {
     fun printPurchaseCount(count: Int) {
@@ -12,12 +14,37 @@ class OutputView {
         println(lotto.getNumbers().map { it.number }.joinToString(", ", "[", "]"))
     }
 
-    fun printResult(results: LottoResult) {
+    fun printResult(results: Map<Rank, Int>) {
         println("\n당첨 통계")
         println("---------")
+        for (result in results) {
+            val matchMessage =
+                when (result.key) {
+                    Rank.FIRST -> "6개 일치"
+                    Rank.SECOND -> "5개 일치, 보너스 볼 일치"
+                    Rank.THIRD -> "5개 일치"
+                    Rank.FOURTH -> "4개 일치"
+                    Rank.FIFTH -> "3개 일치"
+                    Rank.MISS -> "0개 일치"
+                }
+            val prizeMessage =
+                when (result.key) {
+                    Rank.FIRST -> "2000000000원"
+                    Rank.SECOND -> "30000000원"
+                    Rank.THIRD -> "1500000원"
+                    Rank.FOURTH -> "50000원"
+                    Rank.FIFTH -> "5000원"
+                    Rank.MISS -> "0원"
+                }
+
+            println("$matchMessage ($prizeMessage) - ${result.value}개")
+        }
     }
 
     fun printProfit(profit: Double) {
-        println("총 수익률은 ${profit}입니다.")
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val formattedProfit = df.format(profit)
+        println("총 수익률은 ${formattedProfit}입니다.")
     }
 }
