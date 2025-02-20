971eea669af0a2d92609a7133f627fa56c0f4f49
moondev03 <moondev03@gmail.com>
2025-02-20 11:12:47 +0900

    feat: 당첨 번호 중복 검증 기능 구현

--- src/main/kotlin/lotto/domain/model/WinningNumbers.kt
+++ src/main/kotlin/lotto/domain/model/WinningNumbers.kt
@@ -3,10 +3,12 @@ package lotto.domain.model
 class WinningNumbers(val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_NUMBER_SIZE) { INVALID_WINNING_NUMBER_SIZE_MESSAGE }
+        require(numbers.size == numbers.distinct().size) { DUPLICATE_WINNING_NUMBER_MESSAGE }
     }
 
     private companion object {
         const val LOTTO_NUMBER_SIZE = 6
         const val INVALID_WINNING_NUMBER_SIZE_MESSAGE = "당첨 번호는 6개여야 합니다."
+        const val DUPLICATE_WINNING_NUMBER_MESSAGE = "당첨 번호는 중복될 수 없습니다."
     }
 }
