693959767fc7798210ced8ee175b4438a7e4b6fa
ijh1298 <imjh0417@gmail.com>
2025-02-20 14:26:56 +0900

    feat : 한 장의 Lotto에 대한 당첨 결과 구하는 메소드 추가

--- src/main/kotlin/domain/service/LottoMatchCalculator.kt
+++ src/main/kotlin/domain/service/LottoMatchCalculator.kt
@@ -1,7 +1,6 @@
 package domain.service
 
 import domain.model.Lotto
-import domain.model.LottoResult
 import domain.model.PurchaseLotto
 import domain.model.Rank
 import domain.model.WinningLotto
@@ -10,19 +9,23 @@ class LottoMatchCalculator(
     val purchaseLotto: PurchaseLotto,
     val winningLotto: WinningLotto,
 ) {
-    fun calculate() {
-        val result = LottoResult(emptyMap())
+    fun calculate(): Map<Rank, Int> {
+        val result: MutableMap<Rank, Int> =
+            Rank.entries
+                .reversed()
+                .associateWith { 0 }
+                .toMutableMap()
+        result[getRank(purchaseLotto.values.first())] =
+            result.getOrDefault(getRank(purchaseLotto.values.first()), 0) + 1
 
-        purchaseLotto.values.forEach {
-            val rank = getRank(it)
-        }
+        return result
     }
 
     fun getRank(lotto: Lotto): Rank {
         val buyLottoNumbers = lotto.numbers
         val winningLottoNumbers = winningLotto.lotto.numbers
 
-        val lottoMatches = buyLottoNumbers.intersect(winningLottoNumbers).size
+        val lottoMatches = buyLottoNumbers.intersect(winningLottoNumbers.toSet()).size
         val isBonusMatched = winningLotto.bonusNumber.value in buyLottoNumbers
         val rank = Rank.valueOf(lottoMatches, isBonusMatched)
         return rank
