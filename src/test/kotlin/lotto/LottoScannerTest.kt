73c4c63e10197d53f485f51380440a1d8e92f951
HamBeomJoon <hbj0802@naver.com>
2025-02-20 11:41:54 +0900

    feat: 수익률을 계산

--- src/test/kotlin/lotto/LottoScannerTest.kt
+++ src/test/kotlin/lotto/LottoScannerTest.kt
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Test
 
 class LottoResult(
     private val ranks: List<Rank>,
-)
+) {
+    fun calculateProfit(): Double {
+        val totalWinningMoney = ranks.sumOf { it.winningMoney }
+        return totalWinningMoney / (ranks.size * 1_000).toDouble()
+    }
+}
 
 class LottoScanner(
     private val winningNumbers: Lotto,
