ecbde2030aa8f88ccd199d2f7f2b1b155aafd95d
gahyunkim <mapledt001@naver.com>
2025-02-20 13:53:25 +0900

    feat: 로또 순위, 수익률 계산 기능 구현

--- src/main/kotlin/lotto/LottoResult.kt
+++ src/main/kotlin/lotto/LottoResult.kt
@@ -1,14 +1,24 @@
 package lotto
 
 class LottoResult(
-    val lottos: List<Lotto>,
+    private val lottos: List<Lotto>,
+    private val winningLotto: WinningLotto,
 ) {
-//    // 각각의 로또 비교하기
-//    fun compareWithWinningLotto(winningLotto: Lotto): List<Int> {
-//        return lottos.map { it.lottoNums.count { num -> num in winningLotto.lottoNums } }
-//    }
-//
-//    fun compareWithBonusNumber(bonusNumber: Int): List<Boolean> {
-//        return lottos.map { it.lottoNums.contains(bonusNumber) }
-//    }
+    fun getRanks(): List<Rank> {
+        return lottos.map { lotto ->
+            val matchCount = lotto.compareWithWinningLotto(winningLotto.winningLotto)
+            val matchBonus = lotto.compareWithBonusNumber(winningLotto.bounusNumber.toInt())
+            Rank.valueOf(matchCount, matchBonus)
+        }
+    }
+
+    fun getWinningStatistics(): Map<Rank, Int> {
+        return getRanks().groupingBy { it }.eachCount()
+    }
+
+    fun calculateProfitRate(): Double {
+        val totalWinningMoney = getRanks().sumOf { it.winningMoney }
+        val totalCost = lottos.size * 1000
+        return totalWinningMoney.toDouble() / totalCost
+    }
 }
