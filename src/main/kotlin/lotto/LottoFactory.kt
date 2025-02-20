fea381a3e8e47dadfdc73dcf017d541735ff4c29
gahyunkim <mapledt001@naver.com>
2025-02-20 13:52:57 +0900

    refactor: 로또 생성 후 반환값 수정

--- src/main/kotlin/lotto/LottoFactory.kt
+++ src/main/kotlin/lotto/LottoFactory.kt
@@ -5,8 +5,7 @@ class LottoFactory {
         return (1..45).shuffled().take(6).sorted()
     }
 
-    fun generateLottos(amount: Int): LottoResult {
-        val lottos = List(amount) { Lotto(generateLottoNumbers()) }
-        return LottoResult(lottos)
+    fun generateLottos(amount: Int): List<Lotto> {
+        return List(amount) { Lotto(generateLottoNumbers()) }
     }
 }
