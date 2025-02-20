233903364a7c2988afb4be8ada69710a8c40e4fe
ijh1298 <imjh0417@gmail.com>
2025-02-20 14:45:42 +0900

    test : n장의 Lotto에 대한 당첨 결과 구하기 테스트 추가

--- src/test/kotlin/domain/service/LottoMatchCalculatorTest.kt
+++ src/test/kotlin/domain/service/LottoMatchCalculatorTest.kt
@@ -19,8 +19,33 @@ class LottoMatchCalculatorTest {
             )
         val winningLotto = WinningLotto(Lotto(listOf(1, 3, 4, 5, 6, 7)), BonusNumber(2))
         val calculator = LottoMatchCalculator(purchaseLotto, winningLotto)
-        val result = calculator.calculate()
+        val result = calculator.calculate().result
 
         assertThat(result[Rank.SECOND]).isEqualTo(1)
+    }
+
+    @Test
+    fun `n장의 Lotto에 대한 당첨 결과 구하기`() {
+        val purchaseLotto =
+            PurchaseLotto(
+                listOf(
+                    Lotto(listOf(1, 2, 3, 4, 5, 6)),
+                    Lotto(listOf(1, 2, 3, 4, 5, 7)),
+                    Lotto(listOf(1, 2, 3, 4, 8, 7)),
+                    Lotto(listOf(1, 2, 3, 9, 8, 7)),
+                    Lotto(listOf(1, 2, 10, 9, 8, 7)),
+                    Lotto(listOf(1, 11, 10, 9, 8, 7)),
+                    Lotto(listOf(12, 11, 10, 9, 8, 7)),
+                ),
+            )
+
+        val winningLotto = WinningLotto(Lotto(listOf(1, 3, 4, 5, 6, 7)), BonusNumber(2))
+        val calculator = LottoMatchCalculator(purchaseLotto, winningLotto)
+        val result = calculator.calculate().result
+
+        assertThat(result[Rank.SECOND]).isEqualTo(2)
+        assertThat(result[Rank.FOURTH]).isEqualTo(1)
+        assertThat(result[Rank.FIFTH]).isEqualTo(1)
+        assertThat(result[Rank.MISS]).isEqualTo(3)
     }
 }
