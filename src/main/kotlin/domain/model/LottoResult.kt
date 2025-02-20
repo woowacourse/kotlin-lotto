9c2c8849447671103f1f3faf5ca405c80bc411fb
ijh1298 <imjh0417@gmail.com>
2025-02-20 15:30:39 +0900

    feat : 로또 결과에 따른 수익률 구하는 메소드 추가

--- src/main/kotlin/domain/model/LottoResult.kt
+++ src/main/kotlin/domain/model/LottoResult.kt
@@ -6,4 +6,18 @@ class LottoResult(
             .reversed()
             .associateWith { 0 }
             .toMutableMap(),
-)
+) {
+    fun getProfitRate(purchasePrice: PurchasePrice): String {
+        val totalPrice: Double =
+            result
+                .map { (rank, amount) ->
+                    rank.winningMoney * amount
+                }.sum()
+                .toDouble()
+        return ROUND.format(totalPrice / purchasePrice.value)
+    }
+
+    companion object {
+        const val ROUND = "%.2f"
+    }
+}
