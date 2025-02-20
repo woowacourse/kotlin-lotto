7b471ade177db72023133ef3c28f8cfcb75a3948
HamBeomJoon <hbj0802@naver.com>
2025-02-20 15:07:38 +0900

    refactor: 로또 번호 유효성 검사 상수화

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -4,8 +4,14 @@ data class LottoNumber(
     val number: Int,
 ) {
     init {
-        require(number in 1..45) {
-            "숫자는 1에서 45 사이여야 합니다."
+        require(number in MINIMUM_NUMBER..MAXIMUM_NUMBER) {
+            IllegalArgumentException(ERROR_INVALID_RANGE)
         }
+    }
+
+    companion object {
+        private const val ERROR_INVALID_RANGE = "숫자는 1에서 45 사이여야 합니다."
+        private const val MINIMUM_NUMBER = 1
+        private const val MAXIMUM_NUMBER = 45
     }
 }
--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -15,15 +15,6 @@ class InputView {
         require(winningNumbers.all { it.toIntOrNull() != null }) {
             IllegalArgumentException(ERROR_INVALID_WINNING_TYPE)
         }
-        require(winningNumbers.size == 6) {
-            IllegalArgumentException(ERROR_INVALID_WINNING_COUNT)
-        }
-        require(winningNumbers.all { it.toInt() in 1..45 }) {
-            IllegalArgumentException(ERROR_INVALID_WINNING_RANGE)
-        }
-        require(winningNumbers.size == winningNumbers.distinct().size) {
-            IllegalArgumentException(ERROR_INVALID_WINNING_DUPLICATE)
-        }
         return winningNumbers.map { it.toInt() }
     }
 
@@ -53,8 +44,5 @@ class InputView {
         private const val ERROR_INVALID_AMOUNT = "구입금액은 정수를 입력해야 합니다."
         private const val ERROR_INVALID_DIVISION = "구입금액은 1000의 배수여야 합니다."
         private const val ERROR_INVALID_WINNING_TYPE = "당첨번호는 정수를 입력해야 합니다."
-        private const val ERROR_INVALID_WINNING_COUNT = "당첨번호는 6개여야 합니다."
-        private const val ERROR_INVALID_WINNING_RANGE = "당첨번호는 1에서 45 사이여야 합니다."
-        private const val ERROR_INVALID_WINNING_DUPLICATE = "당첨번호는 서로 중복되면 안됩니다."
     }
 }
