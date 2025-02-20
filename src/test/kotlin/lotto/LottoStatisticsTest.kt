c3f2fa83fc5444987bbab0498f772c6c8c64236c
yrsel <xxknu88@gmail.com>
2025-02-20 15:08:37 +0900

    test: 수익률 손해 판단 테스트 추가

--- src/test/kotlin/lotto/LottoStatisticsTest.kt
+++ src/test/kotlin/lotto/LottoStatisticsTest.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 
 class LottoStatisticsTest {
@@ -72,6 +73,62 @@ class LottoStatisticsTest {
             )
         val actual = lottoStatistics.getRateOfReturn()
         assertThat(actual).isEqualTo(rateOfReturn)
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = [0.0, 0.9999999])
+    fun `수익률이 1 미만 일 경우 수익률 손해를 파악하는 메서드는 true를 반환한다`(rateOfReturn: Double) {
+        // given
+        val lottoStatistics =
+            LottoStatistics(
+                lottos = lottos,
+                winningLotto =
+                    WinningLotto(
+                        Lotto(
+                            listOf(
+                                LottoNumber(1),
+                                LottoNumber(2),
+                                LottoNumber(3),
+                                LottoNumber(4),
+                                LottoNumber(5),
+                                LottoNumber(6),
+                            ),
+                        ),
+                        LottoNumber(7),
+                    ),
+                purchaseMoney = purchaseAmount,
+            )
+        val expected = true
+
+        assertThat(lottoStatistics.getIsLossMoney(rateOfReturn)).isEqualTo(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = [1.0, 100.0])
+    fun `수익률이 1 이상 일 경우 수익률 손해를 파악하는 메서드는 false 반환한다`(rateOfReturn: Double) {
+        // given
+        val lottoStatistics =
+            LottoStatistics(
+                lottos = lottos,
+                winningLotto =
+                    WinningLotto(
+                        Lotto(
+                            listOf(
+                                LottoNumber(1),
+                                LottoNumber(2),
+                                LottoNumber(3),
+                                LottoNumber(4),
+                                LottoNumber(5),
+                                LottoNumber(6),
+                            ),
+                        ),
+                        LottoNumber(7),
+                    ),
+                purchaseMoney = purchaseAmount,
+            )
+        val expected = false
+
+        assertThat(lottoStatistics.getIsLossMoney(rateOfReturn)).isEqualTo(expected)
     }
 
     companion object {
