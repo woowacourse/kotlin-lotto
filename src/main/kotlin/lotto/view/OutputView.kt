cbf9fbe3de9b840b08ad85f71a36d0948da639f1
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-20 10:45:11 +0900

    refactor: change how to print

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -35,17 +35,30 @@ object OutputView {
     }
 
     fun showResult(
-        lottoResults: List<LottoResult>,
+        userLottoResults: List<LottoResult>,
         profitRate: Double,
     ) {
         println(MESSAGE_RESULT_HEADER)
-        println("3개 일치 (5000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FIFTH }}개")
-        println("4개 일치 (50000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FOURTH }}개")
-        println("5개 일치 (1500000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.THIRD }}개")
-        println("5개 일치, 보너스 볼 일치(30000000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.SECOND }}개")
-        println("6개 일치 (2000000000원) - ${lottoResults.count { lottoResult: LottoResult -> lottoResult == LottoResult.FIRST }}개")
+        LottoResult.entries.drop(1).forEach { entry: LottoResult ->
+            println(
+                "${entry.matchCount}개 일치${getBonusBallDescription(entry)} (${entry.prizeAmount}원) - ${
+                    countLottoResult(userLottoResults, entry)
+                }개",
+            )
+        }
         println("총 수익률은 ${floor(profitRate * 100) / 100}입니다.(기준이 1이기 때문에 결과적으로 ${makeProfitRateDescription(profitRate)} 의미임)")
     }
+
+    private fun getBonusBallDescription(entry: LottoResult): String =
+        if (entry.bonusMatched == LottoResult.BonusMatched.YES) ", 보너스 볼 일치" else ""
+
+    private fun countLottoResult(
+        userLottoResults: List<LottoResult>,
+        entry: LottoResult,
+    ): Int =
+        userLottoResults.count { lottoResult: LottoResult ->
+            lottoResult == entry
+        }
 
     private fun makeProfitRateDescription(profitRate: Double): String =
         when {
