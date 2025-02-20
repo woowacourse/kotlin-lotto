bdec3ae2e683046f8ba233d91d3b646a2c1a01c7
oungsi2000 <oungsi1000@gmail.com>
2025-02-20 16:03:22 +0900

    test:View테스트 당첨 로또를 입력받는다 테스트 추가 완료

--- src/test/kotlin/LottoViewTest.kt
+++ src/test/kotlin/LottoViewTest.kt
@@ -33,6 +33,38 @@ class LottoViewTest {
     }
 
     @Test
+    @DisplayName(" - , 로 번호를 구분하며,숫자로 변환할 수 없는 값을 입력받는다면 IllegalArgumentException를 반환하고, \"올바르지 않는 형식입니다\" 라는 메시지를 출력한다\n")
+    fun t2_1() {
+        assertThatThrownBy{ LottoView.checkValidLotto("1,2,3,4,5,2ㅂ34") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("올바르지 않은 형식입니다")
+    }
+
+    @Test
+    @DisplayName("  - 6개의 숫자를 입력받지 않는다면 IllegalArgumentException를 반환하고 \"6개의 숫자를 입력해주세요\" 라는 메시지를 출력한다\n")
+    fun t2_2() {
+        assertThatThrownBy{ LottoView.checkValidLotto("1,2,3,4,5,6,7") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("6개의 숫자를 입력해주세요")
+    }
+
+    @Test
+    @DisplayName("  - 하나의 숫자 이상이 1부터 45까지의 범위가 아니라면 IllegalArgumentException를 반환하고 \"1부터 45까지의 숫자를 입력해주세요\" 라는 메시지를 출력한다\n")
+    fun t2_3() {
+        assertThatThrownBy{ LottoView.checkValidLotto("1,2,300,4,5,6") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("1부터 45까지의 숫자를 입력해주세요")
+    }
+
+    @Test
+    @DisplayName("  - 중복된 숫자를 입력한 경우 IllegalArgumentException을 반환하고 \"중복되지 않은 숫자를 입력해주세요\" 라는 메시지를 출력한다\n")
+    fun t2_4() {
+        assertThatThrownBy{ LottoView.checkValidLotto("1,2,3,4,5,5") }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("중복되지 않은 숫자를 입력해주세요")
+    }
+
+    @Test
     @DisplayName("보너스 번호를 입력받는다")
     fun t3() {
         assertThat(LottoView.checkValidBonusNum("6"))
