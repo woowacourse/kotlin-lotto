78d277ae457493b8f5f969a3d0c1266ba780458a
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-20 16:43:25 +0900

    refactor(RandomLottoGenerator): 정렬 기능 추가

--- src/main/kotlin/lotto/model/RandomLottoGenerator.kt
+++ src/main/kotlin/lotto/model/RandomLottoGenerator.kt
@@ -1,9 +1,8 @@
 package lotto.model
 
 class RandomLottoGenerator : LottoGenerator {
-    override fun generate(count: Int): List<Lotto> {
-        return List(count) { Lotto((MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).shuffled().take(LOTTO_SIZE)) }
-    }
+    override fun generate(count: Int): List<Lotto> =
+        List(count) { Lotto((MINIMUM_LOTTO_NUMBER..MAXIMUM_LOTTO_NUMBER).shuffled().take(LOTTO_SIZE).sorted()) }
 
     companion object {
         private const val LOTTO_SIZE: Int = 6
