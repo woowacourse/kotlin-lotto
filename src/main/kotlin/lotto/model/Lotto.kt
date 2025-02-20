431caf19f59ca1c9ef10f44cc2176561a2c9b61a
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-20 16:33:34 +0900

    style(Lotto): 에러 메세지 수정

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,9 +1,11 @@
 package lotto.model
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(
+    val numbers: List<Int>,
+) {
     init {
         require(numbers.distinct().size == numbers.size) { "로또 번호가 중복될 수 없습니다." }
-        require(numbers.size == LOTTO_NUMBERS_SIZE) { "발급하는 로또 번호는 6개여야 합니다." }
+        require(numbers.size == LOTTO_NUMBERS_SIZE) { "로또 번호는 6개여야 합니다." }
         numbers.forEach { number ->
             require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { "로또 번호는 1에서 45 범위 내에서 있어야 합니다." }
         }
