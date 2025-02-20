176aca5d9f1983ad00b0fa3e8876e34184ddc507
cucumber99 <dlsguq1@gmail.com>
2025-02-20 13:56:51 +0900

    feat: 컨트롤러 구현

---
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -0,0 +1,63 @@
+package lotto.controller
+
+import lotto.model.Amount
+import lotto.model.Lotto
+import lotto.model.LottoMachine
+import lotto.model.LottoMatcher
+import lotto.model.LottoNumber
+import lotto.model.PrizeCalculator
+import lotto.view.InputView
+import lotto.view.OutputView
+
+class LottoController(
+    private val inputView: InputView = InputView(),
+    private val outputView: OutputView = OutputView(),
+) {
+    fun run() {
+        val amount = getAmount()
+        val lottoMachine = LottoMachine(amount)
+        val publishedLotto = publishLotto(lottoMachine, amount)
+        val winningLotto = getWinningLotto()
+        val bonusNumber = getBonusNumber()
+        val lottoMatcher = LottoMatcher(winningLotto, bonusNumber)
+        showEarningRate(amount, lottoMatcher, publishedLotto)
+    }
+
+    private fun getAmount(): Amount {
+        outputView.printAmountMessage()
+        val amountInput = inputView.getSingleNumber()
+        return Amount(amountInput)
+    }
+
+    private fun publishLotto(
+        lottoMachine: LottoMachine,
+        amount: Amount,
+    ): List<Lotto> {
+        val publishedLotto = lottoMachine.publishLottoTickets()
+        outputView.printPublishedLotto(amount.getLottoQuantity(), publishedLotto)
+        return publishedLotto
+    }
+
+    private fun getWinningLotto(): Lotto {
+        outputView.printWinningNumberMessage()
+        val winningInput = inputView.getMultipleNumber()
+        return Lotto(winningInput.map { number -> LottoNumber(number) })
+    }
+
+    private fun getBonusNumber(): LottoNumber {
+        outputView.printBonusMessage()
+        val bonusInput = inputView.getSingleNumber()
+        return LottoNumber(bonusInput)
+    }
+
+    private fun showEarningRate(
+        amount: Amount,
+        lottoMatcher: LottoMatcher,
+        publishedLotto: List<Lotto>,
+    ) {
+        val result = lottoMatcher.matchingLotto(publishedLotto)
+        val prizeCalculator = PrizeCalculator()
+        val earningRate = prizeCalculator.calculateEarningRate(amount.money, result)
+        outputView.printPrize(result, earningRate)
+    }
+}
