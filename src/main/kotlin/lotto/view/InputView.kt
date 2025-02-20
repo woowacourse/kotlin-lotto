f6d1cdd0c11dc997d1a29cd1ae3283a22353993c
cucumber99 <dlsguq1@gmail.com>
2025-02-20 13:53:05 +0900

    feat: 입력 및 출력 뷰 구현

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,7 +1,37 @@
 package lotto.view
 
 class InputView {
-    fun inputValue(): String {
-        return readln()
+    fun getSingleNumber(): Int {
+        val input = readln()
+        validateSingleInput(input)
+        return input.toInt()
+    }
+
+    fun getMultipleNumber(): List<Int> {
+        val input = readln()
+        validateMultipleInput(input)
+        return input.split(",").map { it.trim().toInt() }
+    }
+
+    private fun validateSingleInput(input: String) {
+        validateEmptyInput(input)
+        validateNumber(input)
+    }
+
+    private fun validateMultipleInput(input: String) {
+        validateEmptyInput(input)
+        input.split(",").forEach { validateNumber(it.trim()) }
+    }
+
+    private fun validateEmptyInput(input: String) {
+        require(input.trim().isNotEmpty()) {
+            "[ERROR] 입력이 존재하지 않습니다."
+        }
+    }
+
+    private fun validateNumber(input: String) {
+        require(input.toIntOrNull() != null) {
+            "[ERROR] 입력이 정수가 아닙니다: $input"
+        }
     }
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,23 +1,45 @@
 package lotto.view
 
 import lotto.model.Lotto
+import lotto.model.Rank
 
 class OutputView {
-    fun inputViewPrint(message: String) {
-        println(message)
+    fun printAmountMessage() {
+        println("구입금액을 입력해 주세요.")
     }
 
-    fun outputPublishedLotto(lottoList: List<Lotto>) {
-        println(lottoList.size.toString() + PRINT_LOTTO_COUNT)
-        lottoList.forEach { lotto ->
-            println(lotto.numbers)
+    fun printPublishedLotto(
+        quantity: Int,
+        publishedLotto: List<Lotto>,
+    ) {
+        println(quantity.toString() + "개를 구매했습니다.")
+        publishedLotto.forEach { lotto ->
+            val numbers = lotto.numbers.map { it.value }
+            println(numbers)
         }
     }
 
-    companion object {
-        const val INPUT_MONEY = "구입금액을 입력해 주세요."
-        const val INPUT_WINNING = "지난 주 당첨 번호를 입력해 주세요."
-        const val INPUT_BONUS = "보너스 볼을 입력해 주세요."
-        const val PRINT_LOTTO_COUNT = "개를 구매했습니다."
+    fun printWinningNumberMessage() {
+        println()
+        println("지난 주 당첨 번호를 입력해 주세요.")
+    }
+
+    fun printBonusMessage() {
+        println("보너스 볼을 입력해 주세요.")
+    }
+
+    fun printPrize(
+        result: Map<Rank, Int>,
+        earningRate: Double,
+    ) {
+        println()
+        println("당첨 통계")
+        println("---------")
+        println("3개 일치 (5000원) - ${result[Rank.FIFTH]}개")
+        println("4개 일치 (50000원) - ${result[Rank.FOURTH]}개")
+        println("5개 일치 (1500000원) - ${result[Rank.THIRD]}개")
+        println("5개 일치, 보너스 볼 일치 (30000000원) - ${result[Rank.SECOND]}개")
+        println("6개 일치 (2000000000원) - ${result[Rank.FIRST]}개")
+        println("총 수익률은 ${String.format("%.2f", earningRate)}입니다.")
     }
 }
