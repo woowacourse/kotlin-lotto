11e11d9a04744c560553a17445758ceb3d2039d9
HamBeomJoon <hbj0802@naver.com>
2025-02-20 11:14:53 +0900

    feat: 로또 구매 개수만큼 로또 발행

--- src/test/kotlin/lotto/LottoMachineTest.kt
+++ src/test/kotlin/lotto/LottoMachineTest.kt
@@ -1,18 +1,30 @@
 package lotto
 
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
+class LottoMachine(
+    val count: Int,
+) {
+    val lottos: List<Lotto> = generateLottos(count)
+
+    companion object {
+        private fun generateLottos(count: Int): List<Lotto> {
+            val lottos = mutableListOf<Lotto>()
+            repeat(count) {
+                lottos.add(Lotto())
+            }
+            return lottos
+        }
+    }
+}
+
 class LottoMachineTest {
     @ParameterizedTest
-    @ValueSource(ints = [10, 8])
+    @ValueSource(ints = [1, 3, 4])
     fun `로또 구입 개수만큼 로또를 발행한다`(count: Int) {
-        val lottos = mutableListOf<List<Int>>()
-        repeat(count) {
-            val lotto = LottoNumbers()
-            lottos.add(lotto.numbers)
-        }
-        assertThat(lottos.size).isEqualTo(count)
+        val lottoMachine = LottoMachine(count)
+        assertEquals(lottoMachine.lottos.size, count)
     }
 }
