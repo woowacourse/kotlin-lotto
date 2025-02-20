5099d4e675cb06066149ffdc691c72f7eecd650c
cucumber99 <dlsguq1@gmail.com>
2025-02-20 13:51:43 +0900

    feat: 수익률 계산 기능 구현

--- src/main/kotlin/lotto/model/LottoMatcher.kt
+++ src/main/kotlin/lotto/model/LottoMatcher.kt
@@ -4,15 +4,25 @@ class LottoMatcher(
     private val winningLotto: Lotto,
     private val bonusNumber: LottoNumber,
 ) {
+    private val rankMap: MutableMap<Rank, Int> = enumValues<Rank>().associateWith { 0 }.toMutableMap()
+
     init {
         require(!winningLotto.checkBonusNumber(bonusNumber)) {
             BONUS_DUPLICATE_MESSAGE
         }
     }
 
-    fun calculateRank(publishedLotto: Lotto): Rank {
-        val matchCount = publishedLotto.countMatchingNumber(winningLotto)
-        val matchBonus = publishedLotto.checkBonusNumber(bonusNumber)
+    fun matchingLotto(publishedLotto: List<Lotto>): Map<Rank, Int> {
+        publishedLotto.groupingBy { calculateRank(it) }
+            .eachCount()
+            .forEach { (rank, count) -> rankMap[rank] = count }
+
+        return rankMap
+    }
+
+    fun calculateRank(lotto: Lotto): Rank {
+        val matchCount = lotto.countMatchingNumber(winningLotto)
+        val matchBonus = lotto.checkBonusNumber(bonusNumber)
         return Rank.valueOf(matchCount, matchBonus)
     }
 
---
+++ src/main/kotlin/lotto/model/PrizeCalculator.kt
@@ -0,0 +1,12 @@
+package lotto.model
+
+class PrizeCalculator {
+    fun calculateEarningRate(
+        money: Int,
+        result: Map<Rank, Int>,
+    ): Double {
+        return calculateTotalPrize(result).toDouble() / money.toDouble()
+    }
+
+    private fun calculateTotalPrize(result: Map<Rank, Int>): Int = result.entries.sumOf { (rank, count) -> rank.winningMoney * count }
+}
