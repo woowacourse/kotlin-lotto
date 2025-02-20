a2263bb2de12f8c50d0acfa584e57eb9ecff2e73
cucumber99 <dlsguq1@gmail.com>
2025-02-20 13:55:36 +0900

    refactor: 구입금액 접근지정자 수정

--- src/main/kotlin/lotto/model/Amount.kt
+++ src/main/kotlin/lotto/model/Amount.kt
@@ -2,15 +2,15 @@ package lotto.model
 
 class Amount(val money: Int) {
     init {
-        require(money % LOTTO_PRICE == 0) { MONEY_UNIT_MESSAGE }
         require(money >= LOTTO_PRICE) { MONEY_UNDER_MIN }
+        require(money % LOTTO_PRICE == 0) { MONEY_UNIT_MESSAGE }
     }
 
     fun getLottoQuantity(): Int = money / LOTTO_PRICE
 
     companion object {
         const val LOTTO_PRICE = 1000
-        const val MONEY_UNIT_MESSAGE = "[ERROR] 구입 금액은 천원 단위여야 합니다"
-        const val MONEY_UNDER_MIN = "[ERROR] 구입 금액이 최소 금액보다 작습니다"
+        const val MONEY_UNIT_MESSAGE = "[ERROR] 구입 금액은 천원 단위여야 합니다."
+        const val MONEY_UNDER_MIN = "[ERROR] 구입 금액이 최소 금액보다 작습니다."
     }
 }
