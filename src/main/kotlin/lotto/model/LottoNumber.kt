fee219b6a8bad18af0c150be4ff22bb79d727abb
yrsel <xxknu88@gmail.com>
2025-02-20 14:18:32 +0900

    style: 로또 번호 객체 내부의 함수 순서 변경

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -7,16 +7,16 @@ class LottoNumber(
         require(number in MIN_NUMBER..MAX_NUMBER) { ERROR_OUT_OF_RANGE_NUMBERS }
     }
 
-    companion object {
-        private const val ERROR_OUT_OF_RANGE_NUMBERS = "로또 번호는 1 ~ 45 사이의 숫자입니다."
-        const val MIN_NUMBER = 1
-        const val MAX_NUMBER = 45
-    }
-
     override fun equals(other: Any?): Boolean {
         other as LottoNumber
         return number == other.number
     }
 
     override fun hashCode(): Int = number
+
+    companion object {
+        private const val ERROR_OUT_OF_RANGE_NUMBERS = "로또 번호는 1 ~ 45 사이의 숫자입니다."
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+    }
 }
