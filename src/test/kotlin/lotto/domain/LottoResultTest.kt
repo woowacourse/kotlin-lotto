d6c1520149f6e8fbef7ee5029399736336af3c72
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-21 17:41:19 +0900

    test: separate test

--- src/test/kotlin/lotto/domain/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoResultTest.kt
@@ -5,32 +5,44 @@ import org.junit.jupiter.api.Test
 
 class LottoResultTest {
     @Test
-    fun `지난 주 당첨 번호를 참고하여 구매한 로또의 결과를 계산한다`() {
-        val wonLotto =
-            WinningLotto(Lotto(setOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }.toSet()), bonusNumber = LottoNumber(7))
-
+    fun `6개 일치 시 1등`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), bonusNumber = LottoNumber(7))
         val expectedFirstLotto = Lotto(setOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }.toSet())
-        val expectedSecondLotto = Lotto(setOf(1, 2, 3, 4, 5, 7).map { LottoNumber(it) }.toSet())
-        val expectedThirdLotto = Lotto(setOf(1, 2, 3, 4, 5, 45).map { LottoNumber(it) }.toSet())
-        val expectedFourthLotto = Lotto(setOf(1, 2, 3, 4, 44, 45).map { LottoNumber(it) }.toSet())
-        val expectedFifthLotto = Lotto(setOf(1, 2, 3, 43, 44, 45).map { LottoNumber(it) }.toSet())
-        val expectedFailLotto = Lotto(setOf(1, 2, 42, 43, 44, 45).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedFirstLotto)).isEqualTo(LottoResult.FIRST)
+    }
 
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFirstLotto)).isEqualTo(LottoResult.FIRST)
-        assertThat(
-            LottoResult.from(
-                wonLotto = wonLotto,
-                boughtLotto = expectedSecondLotto,
-            ),
-        ).isEqualTo(LottoResult.SECOND)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedThirdLotto)).isEqualTo(LottoResult.THIRD)
-        assertThat(
-            LottoResult.from(
-                wonLotto = wonLotto,
-                boughtLotto = expectedFourthLotto,
-            ),
-        ).isEqualTo(LottoResult.FOURTH)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFifthLotto)).isEqualTo(LottoResult.FIFTH)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFailLotto)).isEqualTo(LottoResult.FAIL)
+    @Test
+    fun `5개 일치, 보너스 볼 일치 시 2등`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), bonusNumber = LottoNumber(7))
+        val expectedSecondLotto = Lotto(setOf(1, 2, 3, 4, 5, 7).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedSecondLotto)).isEqualTo(LottoResult.SECOND)
+    }
+
+    @Test
+    fun `5개 일치 시 3등`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), LottoNumber(7))
+        val expectedThirdLotto = Lotto(setOf(1, 2, 3, 4, 5, 45).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedThirdLotto)).isEqualTo(LottoResult.THIRD)
+    }
+
+    @Test
+    fun `4개 일치 시 4등`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), LottoNumber(7))
+        val expectedFourthLotto = Lotto(setOf(1, 2, 3, 4, 44, 45).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedFourthLotto)).isEqualTo(LottoResult.FOURTH)
+    }
+
+    @Test
+    fun `3개 일치 시 5등`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), LottoNumber(7))
+        val expectedFifthLotto = Lotto(setOf(1, 2, 3, 43, 44, 45).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedFifthLotto)).isEqualTo(LottoResult.FIFTH)
+    }
+
+    @Test
+    fun `2개 이하 일치 시 낙첨`() {
+        val wonLotto = WinningLotto(Lotto((1..6).map { LottoNumber(it) }.toSet()), LottoNumber(7))
+        val expectedFailLotto = Lotto(setOf(1, 2, 42, 43, 44, 45).map { LottoNumber(it) }.toSet())
+        assertThat(LottoResult.from(wonLotto, expectedFailLotto)).isEqualTo(LottoResult.FAIL)
     }
 }
