14450e5738520e6f2c1c05b2ef9ab03cdc14bc70
chanho0908 <uni070@naver.com>
2025-02-21 17:18:55 +0900

    test : 로또 테스트 함수 수정

--- src/test/kotlin/validator/NumericValidatorTest.kt
+++ src/test/kotlin/validator/NumericValidatorTest.kt
@@ -1,27 +1,24 @@
 package validator
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class NumericValidatorTest {
     @Test
     fun `빈 값이 입력되면 예외가 발생한다`() {
-        Assertions
-            .assertThatThrownBy {
-                NumericValidator("   ")
-            }.isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage("[ERROR] 빈 값을 입력하셨습니다.")
+        assertThrows<IllegalArgumentException> {
+            NumericValidator("   ")
+        }.apply { assertThat(this).hasMessage("[ERROR] 빈 값을 입력하셨습니다.") }
     }
 
     @ValueSource(strings = ["가나다, abc, ###, @we2as"])
     @ParameterizedTest
     fun `숫자가 아닌 값이 입력되면 예외가 발생한다`(value: String) {
-        Assertions
-            .assertThatThrownBy {
-                NumericValidator(value)
-            }.isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage("[ERROR] 숫자를 입력해야 합니다.")
+        assertThrows<IllegalArgumentException> {
+            NumericValidator(value)
+        }.apply { assertThat(this).hasMessage("[ERROR] 숫자를 입력해야 합니다.") }
     }
 }
