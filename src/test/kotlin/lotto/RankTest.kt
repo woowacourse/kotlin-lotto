cbed6dac0907628d190a867cf933ad114c056757
eden <devfeijoa@gmail.com>
2025-02-21 19:27:04 +0900

    test: CsvSource를 활용한 테스트코드 변경

--- src/test/kotlin/lotto/RankTest.kt
+++ src/test/kotlin/lotto/RankTest.kt
@@ -4,29 +4,25 @@ import lotto.model.Rank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.CsvSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RankTest {
     @ParameterizedTest
-    @MethodSource("getStatistics")
+    @CsvSource(
+        "6, false, FIRST",
+        "5, true, SECOND",
+        "5, false, THIRD",
+        "4, false, FOURTH",
+        "3, false, FIFTH",
+        "2, false, MISS",
+    )
     fun `당첨번호와 보너스볼 매치 갯수에 따라 당첨 등수 확인 `(
         countOfMatch: Int,
         matchBonus: Boolean,
-        rank: Rank,
+        expectedRank: Rank,
     ) {
         val result = Rank.fromMatch(countOfMatch, matchBonus)
-        assertThat(result).isEqualTo(rank)
+        assertThat(result).isEqualTo(expectedRank)
     }
-
-    private fun getStatistics() =
-        listOf(
-            Arguments.of(6, false, Rank.FIRST),
-            Arguments.of(5, true, Rank.SECOND),
-            Arguments.of(5, false, Rank.THIRD),
-            Arguments.of(4, false, Rank.FOURTH),
-            Arguments.of(3, false, Rank.FIFTH),
-            Arguments.of(2, false, Rank.MISS),
-        )
 }
