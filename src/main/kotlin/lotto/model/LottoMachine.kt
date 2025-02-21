178f5e95b7a4f82d5d78635c9224195193cb9005
HamBeomJoon <hbj0802@naver.com>
2025-02-21 15:55:35 +0900

    refactor: LottoMachine 가독성 개선

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -1,17 +1,5 @@
 package lotto.model
 
-class LottoMachine(
-    count: Int,
-) {
-    val lottoTickets: List<Lotto> = generateLottoTicket(count)
-
-    companion object {
-        private fun generateLottoTicket(count: Int): List<Lotto> {
-            val lottoTickets = mutableListOf<Lotto>()
-            repeat(count) {
-                lottoTickets.add(Lotto())
-            }
-            return lottoTickets
-        }
-    }
+class LottoMachine {
+    fun purchase(count: Int): List<LottoTicket> = List(count) { LottoTicket() }
 }
