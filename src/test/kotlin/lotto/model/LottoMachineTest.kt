18ca935c7a3771a892eee92dd13578c72d96d163
junseo511 <prime54@g.skku.edu>
2025-02-21 20:42:35 +0900

    test: 장수 입력에 따른 로또 머신 작동 테스트 추가

--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -1,3 +1,16 @@
 package lotto.model
 
-class LottoMachineTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LottoMachineTest {
+    @Test
+    fun `로또 머신에서 5장을 요청하면 5장의 로또를 반환한다`() {
+        val lottoMachine = LottoMachine()
+
+        val expectedQuantity = 5
+        val actualQuantity = lottoMachine.getLottos(expectedQuantity).value.size
+
+        assertEquals(expectedQuantity, actualQuantity)
+    }
+}
