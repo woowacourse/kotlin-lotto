406fd17f1c42c14cc5ac32ec516344cc204aabae
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-21 17:49:36 +0900

    refactor: LottoScanner -> WinningLotto로 class 변경
    - 로또 판별 로직은 변하지 않기 때문에 WinningLotto 클래스를 생성하면서, 판별하는 로직 생성

---
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -0,0 +1,28 @@
+package lotto.model
+
+import lotto.Constants.LOTTO_PICK_COUNT
+
+class WinningLotto(
+    private val winningNumbers: Set<LottoNumber>,
+    private val bonusNumber: LottoNumber,
+) {
+    init {
+        require(winningNumbers.size == LOTTO_PICK_COUNT)
+        require(!winningNumbers.contains(bonusNumber))
+    }
+
+    fun getResult(lottoTickets: List<LottoTicket>): LottoResult {
+        val result = lottoTickets.map { getRank(it) }
+        return LottoResult(result)
+    }
+
+    private fun getRank(lottoTicket: LottoTicket): Rank {
+        val countOfMatch: Int = getCountOfMatch(lottoTicket)
+        val matchBonus: Boolean = getMatchBonus(lottoTicket)
+        return Rank.valueOf(countOfMatch, matchBonus)
+    }
+
+    private fun getCountOfMatch(lottoTicket: LottoTicket): Int = winningNumbers.intersect(lottoTicket.getNumbers().toSet()).size
+
+    private fun getMatchBonus(lottoTicket: LottoTicket): Boolean = lottoTicket.getNumbers().contains(bonusNumber)
+}
