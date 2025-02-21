7453114c0eec3ad1f883d5c3376fee9273050bac
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-21 17:58:30 +0900

    refactor: 함수명 변경

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -1,19 +1,5 @@
 package lotto.model
 
-class LottoMachine(
-    private val count: Int,
-) {
+class LottoMachine {
     fun purchase(count: Int): List<LottoTicket> = List(count) { LottoTicket() }
-
-    val lottoTickets: List<LottoTicket> = generateLottoTickets(count)
-
-    companion object {
-        private fun generateLottoTickets(count: Int): List<LottoTicket> {
-            val lottoTickets = mutableListOf<LottoTicket>()
-            repeat(count) {
-                lottoTickets.add(LottoTicket())
-            }
-            return lottoTickets
-        }
-    }
 }
