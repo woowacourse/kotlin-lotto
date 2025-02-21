39cac7fef015f157a0c1f4f1f99217f854fe4f4e
hwannow <vnfdlvgid061@naver.com>
2025-02-21 15:52:53 +0900

    refactor: RandomGenerator 메소드 변경 및 유연성 개선

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -7,7 +7,7 @@ class LottoMachine(private val randomGenerator: RandomGenerator = RandomGenerato
 
     fun buyLottos(purchaseAmount: Int): List<Lotto> {
         val lottoCount = getLottoCount(purchaseAmount)
-        return List(lottoCount) { Lotto(randomGenerator.getRandomNumberList()) }
+        return List(lottoCount) { Lotto(randomGenerator.getRandomLottoNumbers(NUMBER_OF_LOTTO_NUMBER, MIN_BOUND, MAX_BOUND)) }
     }
 
     private fun getLottoCount(purchaseAmount: Int): Int {
@@ -15,6 +15,9 @@ class LottoMachine(private val randomGenerator: RandomGenerator = RandomGenerato
     }
 
     companion object {
-        const val LOTTO_PRICE: Int = 1_000
+        private const val LOTTO_PRICE: Int = 1_000
+        private const val MAX_BOUND: Int = 45
+        private const val MIN_BOUND: Int = 1
+        private const val NUMBER_OF_LOTTO_NUMBER: Int = 6
     }
 }
--- src/main/kotlin/lotto/generator/RandomGenerator.kt
+++ src/main/kotlin/lotto/generator/RandomGenerator.kt
@@ -3,13 +3,11 @@ package lotto.generator
 import lotto.domain.LottoNumber
 
 class RandomGenerator {
-    fun getRandomNumberList(): List<LottoNumber> {
-        return (MIN_BOUND..MAX_BOUND).shuffled().take(NUMBER_OF_LOTTO_NUMBER).map { LottoNumber(it) }
-    }
-
-    companion object {
-        const val MAX_BOUND: Int = 45
-        const val MIN_BOUND: Int = 1
-        const val NUMBER_OF_LOTTO_NUMBER: Int = 6
+    fun getRandomLottoNumbers(
+        size: Int,
+        min: Int,
+        max: Int,
+    ): List<LottoNumber> {
+        return (min..max).shuffled().take(size).map { LottoNumber(it) }
     }
 }
