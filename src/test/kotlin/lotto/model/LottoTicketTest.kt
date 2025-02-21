8fc89985e62c9f6bcc384dd07c7478634167d3d0
HamBeomJoon <hbj0802@naver.com>
2025-02-21 16:46:22 +0900

    test: 보너스 번호 중복 테스트

--- src/test/kotlin/lotto/model/LottoTicketTest.kt
+++ src/test/kotlin/lotto/model/LottoTicketTest.kt
@@ -12,6 +12,24 @@ class LottoTicketTest {
     }
 
     @Test
+    fun `보너스 번호가 당첨 번호와 중복되면 예외를 발생시킨다`() {
+        val lottoNumbers =
+            listOf(
+                LottoNumber(1),
+                LottoNumber(2),
+                LottoNumber(3),
+                LottoNumber(4),
+                LottoNumber(5),
+                LottoNumber(6),
+            )
+        val bonusNumber = 6
+
+        assertThrows<IllegalArgumentException> {
+            LottoTicket(lottoNumbers, LottoNumber(bonusNumber))
+        }
+    }
+
+    @Test
     fun `로또 번호가 서로 중복되면 예외를 발생시킨다`() {
         val lottoNumbers =
             listOf(
