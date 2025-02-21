ef63eb3df4f78f4b33032a69fcfb7e4de9f7e016
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-21 15:23:22 +0900

    refactor: separate counting lotto ticket logic

--- src/main/kotlin/lotto/domain/Lottos.kt
+++ src/main/kotlin/lotto/domain/Lottos.kt
@@ -8,10 +8,14 @@ class Lottos(
             price: Int,
             lottos: Set<Lotto>,
         ): Lottos {
-            require(price >= Lotto.PRICE) { ERROR_MESSAGE_NOT_ENOUGH_MONEY }
-            val count = price / Lotto.PRICE
-            require(count == lottos.size) { ERROR_MESSAGE_NOT_MATCHED_MONEY_AND_LOTTOS_COUNT }
+            val lottoTicketCount: Int = price.toLottoTicketCount()
+            require(lottoTicketCount == lottos.size) { ERROR_MESSAGE_NOT_MATCHED_MONEY_AND_LOTTOS_COUNT }
             return Lottos(lottos)
+        }
+
+        private fun Int.toLottoTicketCount(): Int {
+            require(this >= Lotto.PRICE) { ERROR_MESSAGE_NOT_ENOUGH_MONEY }
+            return this / Lotto.PRICE
         }
 
         private const val ERROR_MESSAGE_NOT_ENOUGH_MONEY = "최소 구입 금액은 ${Lotto.PRICE}원입니다."
