bd921b6eff26d8b5eaf10d66b5cb60dff65af0d0
moondev03 <moondev03@gmail.com>
2025-02-21 21:58:52 +0900

    chore: Lotto 테스트명 수정

--- src/test/kotlin/lotto/domain/model/LottoTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
-    fun `로또는 6개의 숫자를 가지지 않으면 예외가 발생한다`() {
+    fun `로또는 로또 번호의 개수가 6이 아니라면 예외가 발생한다`() {
         val numbers = listOf(1, 2, 3, 4, 5, 6, 7)
         val exception =
             assertThrows<IllegalArgumentException> {
@@ -17,7 +17,7 @@ class LottoTest {
     }
 
     @Test
-    fun `중복된 번호를 제거한 뒤 번호가 6개가 아니라면 예외가 발생한다`() {
+    fun `중복된 번호가 제거된 뒤 로또 번호의 개수가 6이 아니라면 예외가 발생한다`() {
         val numbers = listOf(1, 1, 2, 3, 4, 5)
         val exception =
             assertThrows<IllegalArgumentException> {
