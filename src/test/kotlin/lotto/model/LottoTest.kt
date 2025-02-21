fa74e03e60973a4820ea2a7682456134126dc851
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-21 16:16:31 +0900

    refeator: Lotto class -> LottoTicket class 네이밍 변경

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.assertThrows
 class LottoTest {
     @Test
     fun `로또는 한 장에 로또 번호 6개를 가진다`() {
-        val lotto = Lotto()
+        val lotto = LottoTicket()
         assertEquals(lotto.getSize(), 6)
     }
 
     @Test
     fun `로또 번호는 서로 중복되지 않는다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(
+            LottoTicket(
                 listOf(
                     LottoNumber(1),
                     LottoNumber(2),
