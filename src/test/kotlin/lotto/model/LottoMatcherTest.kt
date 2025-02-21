282cc4b84c2993aebdbf369bb3c3736bffd0c40a
Leeyerin0210 <lyl8661@naver.com>
2025-02-21 18:00:47 +0900

    refactor : LottoMatcherTest.kt 가독성 수정

--- src/test/kotlin/lotto/model/LottoMatcherTest.kt
+++ src/test/kotlin/lotto/model/LottoMatcherTest.kt
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoMatcherTest {
     private lateinit var winningLotto: Lotto
@@ -15,14 +14,14 @@ class LottoMatcherTest {
     @BeforeEach
     fun setup() {
         val bonusNumber = LottoNumber(7)
-        winningLotto = parseLottoNumber(listOf(1, 2, 3, 4, 5, 6))
+        winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
         lottoMatcher = LottoMatcher(winningLotto, bonusNumber)
     }
 
     @Test
     fun `당첨 번호와 보너스 볼은 중복될 수 없다`() {
         val winningNumber = listOf(1, 2, 3, 4, 5, 6)
-        val winningLotto = parseLottoNumber(winningNumber)
+        val winningLotto = Lotto(winningNumber)
         val bonusNumber = LottoNumber(1)
         val exception = assertThrows<IllegalArgumentException> { LottoMatcher(winningLotto, bonusNumber) }
         assertThat(exception.message).isEqualTo("[ERROR] 당첨 번호와 보너스 볼은 중복될 수 없습니다.")
@@ -31,7 +30,7 @@ class LottoMatcherTest {
     @Test
     fun `당첨 번호와 일치하는 번호가 6개인 경우 1등이 된다`() {
         val numbers = listOf(1, 2, 3, 4, 5, 6)
-        val publishedLotto = parseLottoNumber(numbers)
+        val publishedLotto = Lotto(numbers)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.FIRST]).isEqualTo(1)
     }
@@ -39,7 +38,7 @@ class LottoMatcherTest {
     @Test
     fun `당첨 번호와 일치하는 번호가 5개이고, 보너스 볼이 일치하는 경우 2등이 된다`() {
         val numbers = listOf(1, 2, 3, 4, 5, 7)
-        val publishedLotto = parseLottoNumber(numbers)
+        val publishedLotto = Lotto(numbers)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.SECOND]).isEqualTo(1)
     }
@@ -47,7 +46,7 @@ class LottoMatcherTest {
     @Test
     fun `당첨 번호와 일치하는 번호가 5개인 경우 3등이 된다`() {
         val numbers = listOf(1, 2, 3, 4, 5, 8)
-        val publishedLotto = parseLottoNumber(numbers)
+        val publishedLotto = Lotto(numbers)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.THIRD]).isEqualTo(1)
     }
@@ -55,7 +54,7 @@ class LottoMatcherTest {
     @Test
     fun `당첨 번호와 일치하는 번호가 4개인 경우 4등이 된다`() {
         val numbers = listOf(1, 2, 3, 4, 8, 9)
-        val publishedLotto = parseLottoNumber(numbers)
+        val publishedLotto = Lotto(numbers)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.FOURTH]).isEqualTo(1)
     }
@@ -63,28 +62,23 @@ class LottoMatcherTest {
     @Test
     fun `당첨 번호와 일치하는 번호가 3개인 경우 5등이 된다`() {
         val numbers = listOf(1, 2, 3, 8, 9, 10)
-        val publishedLotto = parseLottoNumber(numbers)
+        val publishedLotto = Lotto(numbers)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.FIFTH]).isEqualTo(1)
     }
 
     @ParameterizedTest
-    @MethodSource("failedNumbersProvider")
-    fun `당첨 번호와 일치하는 번호가 3개 미만인 경우 탈락된다`(numbers: List<Int>) {
-        val publishedLotto = parseLottoNumber(numbers)
+    @CsvSource(
+        "'1, 2, 8, 9, 10, 11'",
+        "'1, 8, 9, 10, 11, 12'",
+        "'8, 9, 10, 11, 12, 13'",
+    )
+    fun `당첨 번호와 일치하는 번호가 3개 미만인 경우 탈락된다`(numbers: String) {
+        val numbersList = numbers.split(",").map { it.trim().toInt() }
+        val publishedLotto = Lotto(numbersList)
         val rank = lottoMatcher.matchLotto(listOf(publishedLotto))
         assertThat(rank[Rank.MISS]).isEqualTo(1)
     }
 
-    private fun parseLottoNumber(numbers: List<Int>): Lotto = Lotto(LottoNumbers(numbers.map { number -> LottoNumber(number) }))
-
-    companion object {
-        @JvmStatic
-        fun failedNumbersProvider(): Stream<List<Int>> =
-            Stream.of(
-                listOf(1, 2, 8, 9, 10, 11),
-                listOf(1, 8, 9, 10, 11, 12),
-                listOf(8, 9, 10, 11, 12, 13),
-            )
-    }
+    fun Lotto(numbers: List<Int>): Lotto = Lotto(LottoNumbers(numbers.map { number -> LottoNumber(number) }))
 }
