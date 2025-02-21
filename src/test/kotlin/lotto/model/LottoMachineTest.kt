ad0485d4978ace140e86a21bc3fdd5a79a8141a5
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-21 22:32:55 +0900

    refactor: property -> method 변경

--- src/test/kotlin/lotto/model/LottoMachineTest.kt
+++ src/test/kotlin/lotto/model/LottoMachineTest.kt
@@ -8,7 +8,7 @@ class LottoMachineTest {
     @ParameterizedTest
     @ValueSource(ints = [1, 3, 4])
     fun `로또 구입 개수만큼 로또를 발행한다`(count: Int) {
-        val lottoMachine = LottoMachine(count)
-        assertEquals(lottoMachine.lottos.size, count)
+        val lottoMachine = LottoMachine()
+        assertEquals(lottoMachine.purchase(count).size, count)
     }
 }
