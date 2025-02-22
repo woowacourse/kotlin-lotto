491a6f788b87d81738b729613b92b06b9aff6ab4
DoableTuple <doabletuple@gmail.com>
2025-02-22 21:06:40 +0900

    refactor: 당첨 통계 관련 문자열 상수 처리

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -10,6 +10,8 @@ object OutputView {
     private const val MESSAGE_ENTER_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     private const val MESSAGE_RESULT_HEADER = "당첨 통계\n---------"
     private const val MESSAGE_BOUGHT = "개를 구매했습니다."
+    private const val MESSAGE_RESULT_PER_PRIZE = "%s개 일치%s (%s원) - %s개"
+    private const val MESSAGE_RESULT_SUMMARY = "총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 %s 의미임)"
     private const val MESSAGE_PROFIT_RATE_PROFIT = "이득이라는"
     private const val MESSAGE_PROFIT_RATE_LOSS = "손해라는"
     private const val MESSAGE_PROFIT_RATE_BREAKEVEN = "본전이라는"
@@ -41,16 +43,27 @@ object OutputView {
     ) {
         println()
         println(MESSAGE_RESULT_HEADER)
-        LottoResult.entries.drop(1).forEach { entry ->
-            println("${entry.matchCount}개 일치${getBonusBallDescription(entry)} (${entry.prizeAmount}원) - ${resultTally[entry]}개")
-        }
-        println("총 수익률은 ${floor(profitRate * 100) / 100}입니다.(기준이 1이기 때문에 결과적으로 ${resultDescription(profitRate)} 의미임)")
+        LottoResult.entries.drop(1).forEach { entry -> println(makePrizeDescription(entry, resultTally)) }
+        println(
+            MESSAGE_RESULT_SUMMARY.format(
+                floor(profitRate * 100) / 100,
+                makeResultSummary(profitRate)
+            )
+        )
     }
 
-    private fun getBonusBallDescription(entry: LottoResult): String =
-        if (entry.bonusMatched == LottoResult.BonusMatched.YES) ", 보너스 볼 일치" else ""
+    private fun makePrizeDescription(
+        lottoResult: LottoResult,
+        resultTally: Map<LottoResult, Int>,
+    ): String =
+        MESSAGE_RESULT_PER_PRIZE.format(
+            lottoResult.matchCount,
+            if (lottoResult.bonusMatched == LottoResult.BonusMatched.YES) ", 보너스 볼 일치" else "",
+            lottoResult.prizeAmount,
+            resultTally[lottoResult]
+        )
 
-    private fun resultDescription(profitRate: Double): String =
+    private fun makeResultSummary(profitRate: Double): String =
         when (ResultClassification.from(profitRate)) {
             ResultClassification.PROFIT -> MESSAGE_PROFIT_RATE_PROFIT
             ResultClassification.LOSS -> MESSAGE_PROFIT_RATE_LOSS
