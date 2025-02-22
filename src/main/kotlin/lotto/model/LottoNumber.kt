e428f880f7d826845cb1acfc11856655f3a9fb51
yrsel <xxknu88@gmail.com>
2025-02-22 18:10:53 +0900

    refactor: LottoNumber 클래스 변경
    equals, hashcode를 직접 재정의해주기 보다는 data class를 활용하여 언어에서 재정의해주는 방향으로 변경

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,18 +1,11 @@
 package lotto.model
 
-class LottoNumber(
+data class LottoNumber(
     val number: Int,
 ) {
     init {
         require(number in MIN_NUMBER..MAX_NUMBER) { ERROR_OUT_OF_RANGE_NUMBERS }
     }
-
-    override fun equals(other: Any?): Boolean {
-        other as LottoNumber
-        return number == other.number
-    }
-
-    override fun hashCode(): Int = number
 
     companion object {
         private const val ERROR_OUT_OF_RANGE_NUMBERS = "로또 번호는 1 ~ 45 사이의 숫자입니다."
