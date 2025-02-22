1d47423e02616e635ef681f2f23267934d09c4b9
HamBeomJoon <hbj0802@naver.com>
2025-02-22 14:07:02 +0900

    feat: LottoTicket 예외 메세지 추가

--- src/main/kotlin/lotto/model/LottoTicket.kt
+++ src/main/kotlin/lotto/model/LottoTicket.kt
@@ -7,9 +7,15 @@ class LottoTicket(
     private val bonusNumber: LottoNumber? = null,
 ) {
     init {
-        require(numbers.size == LOTTO_PICK_COUNT)
-        require(numbers.size == numbers.toSet().size)
-        require(!numbers.contains(bonusNumber))
+        require(numbers.size == LOTTO_PICK_COUNT) {
+            ERROR_LOTTO_SIZE
+        }
+        require(numbers.size == numbers.toSet().size) {
+            ERROR_LOTTO_DUPLICATE
+        }
+        require(!numbers.contains(bonusNumber)) {
+            ERROR_BONUS_DUPLICATE
+        }
     }
 
     fun getSize() = numbers.size
@@ -27,5 +33,8 @@ class LottoTicket(
                 .map { LottoNumber(it) }
 
         private const val LOTTO_PICK_COUNT = 6
+        private const val ERROR_LOTTO_SIZE = "로또 번호는 6개여야 합니다."
+        private const val ERROR_LOTTO_DUPLICATE = "로또 번호는 서로 중복되면 안 됩니다."
+        private const val ERROR_BONUS_DUPLICATE = "보너스 번호는 당첨 번호와 중복되면 안 됩니다."
     }
 }
