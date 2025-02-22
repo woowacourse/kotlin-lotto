ae8ba60477c88f7855a7f405879b880ed4605f30
jerry8282 <ka06196@naver.com>
2025-02-22 11:24:35 +0900

    test : 테스트 네이밍을 명확하게 작성

--- src/test/kotlin/domain/model/RankTest.kt
+++ src/test/kotlin/domain/model/RankTest.kt
@@ -5,31 +5,31 @@ import org.junit.jupiter.api.Test
 
 class RankTest {
     @Test
-    fun `6개가 일치하면 1등 순위가 반환된다`() {
+    fun `당첨 로또번호가 사용자가 입력한 로또 번호와 6개가 일치하면 1등 순위가 반환된다`() {
         val result = Rank.valueOf(6, false)
         assertThat(result).isEqualTo(Rank.FIRST)
     }
 
     @Test
-    fun `5개가 일치하고 보너스 번호가 일치하면 2등 순위가 반환된다`() {
+    fun `당첨 로또번호가 사용자가 입력한 로또 번호와 5개가 일치하고 보너스 번호가 일치하면 2등 순위가 반환된다`() {
         val result = Rank.valueOf(5, true)
         assertThat(result).isEqualTo(Rank.SECOND)
     }
 
     @Test
-    fun `5개가 일치하고 보너스 번호가 일치하지 않으면 3등 순위가 반환된다`() {
+    fun `당첨 로또번호가 사용자가 입력한 로또 번호와 5개 일치하고 보너스 번호가 일치하지 않으면 3등 순위가 반환된다`() {
         val result = Rank.valueOf(5, false)
         assertThat(result).isEqualTo(Rank.THIRD)
     }
 
     @Test
-    fun `4개가 일치하고 보너스 번호가 일치하지 않으면 4등 순위가 반환된다`() {
+    fun `당첨 로또번호가 사용자가 입력한 로또 번호와 4개가 일치하고 보너스 번호가 일치하지 않으면 4등 순위가 반환된다`() {
         val result = Rank.valueOf(4, false)
         assertThat(result).isEqualTo(Rank.FOURTH)
     }
 
     @Test
-    fun `4개가 일치하고 보너스 번호도 일치하면 4등 순위가 반환된다`() {
+    fun `당첨 로또번호가 사용자가 입력한 로또 번호와 4개가 일치하고 보너스 번호도 일치하면 4등 순위가 반환된다`() {
         val result = Rank.valueOf(4, true)
         assertThat(result).isEqualTo(Rank.FOURTH)
     }
