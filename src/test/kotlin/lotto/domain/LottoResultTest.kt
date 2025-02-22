f8c786d472762cf59b7bea1387066eb29c0408ce
DoableTuple <doabletuple@gmail.com>
2025-02-22 20:13:03 +0900

    refactor: LottoResultTest에 @ParameterizedTest 사용

--- src/test/kotlin/lotto/domain/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/LottoResultTest.kt
@@ -1,36 +1,29 @@
 package lotto.domain
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class LottoResultTest {
-    @Test
-    fun `지난 주 당첨 번호를 참고하여 구매한 로또의 결과를 계산한다`() {
-        val wonLotto =
-            WinningLotto(Lotto(1, 2, 3, 4, 5, 6), bonusNumber = LottoNumber(7))
+    @ParameterizedTest
+    @MethodSource("지난 주 당첨 번호를 참고하여 구매한 로또의 결과를 계산한다 테스트 케이스")
+    fun `지난 주 당첨 번호를 참고하여 구매한 로또의 결과를 계산한다`(boughtLotto: Lotto, expectedResult: LottoResult) {
+        val winningLotto = WinningLotto(Lotto(1, 2, 3, 4, 5, 6), bonusNumber = LottoNumber(7))
+        assertThat(LottoResult.from(winningLotto, boughtLotto)).isEqualTo(expectedResult)
+    }
 
-        val expectedFirstLotto = Lotto(1, 2, 3, 4, 5, 6)
-        val expectedSecondLotto = Lotto(1, 2, 3, 4, 5, 7)
-        val expectedThirdLotto = Lotto(1, 2, 3, 4, 5, 45)
-        val expectedFourthLotto = Lotto(1, 2, 3, 4, 44, 45)
-        val expectedFifthLotto = Lotto(1, 2, 3, 43, 44, 45)
-        val expectedFailLotto = Lotto(1, 2, 42, 43, 44, 45)
-
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFirstLotto)).isEqualTo(LottoResult.FIRST)
-        assertThat(
-            LottoResult.from(
-                wonLotto = wonLotto,
-                boughtLotto = expectedSecondLotto,
-            ),
-        ).isEqualTo(LottoResult.SECOND)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedThirdLotto)).isEqualTo(LottoResult.THIRD)
-        assertThat(
-            LottoResult.from(
-                wonLotto = wonLotto,
-                boughtLotto = expectedFourthLotto,
-            ),
-        ).isEqualTo(LottoResult.FOURTH)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFifthLotto)).isEqualTo(LottoResult.FIFTH)
-        assertThat(LottoResult.from(wonLotto = wonLotto, boughtLotto = expectedFailLotto)).isEqualTo(LottoResult.FAIL)
+    companion object {
+        @JvmStatic
+        fun `지난 주 당첨 번호를 참고하여 구매한 로또의 결과를 계산한다 테스트 케이스`() = listOf(
+            Arguments.of(Lotto(1, 2, 3, 4, 5, 6), LottoResult.FIRST),
+            Arguments.of(Lotto(1, 2, 3, 4, 5, 7), LottoResult.SECOND),
+            Arguments.of(Lotto(1, 2, 3, 4, 5, 45), LottoResult.THIRD),
+            Arguments.of(Lotto(1, 2, 3, 4, 44, 45), LottoResult.FOURTH),
+            Arguments.of(Lotto(1, 2, 3, 43, 44, 45), LottoResult.FIFTH),
+            Arguments.of(Lotto(1, 2, 42, 43, 44, 45), LottoResult.FAIL),
+            Arguments.of(Lotto(1, 41, 42, 43, 44, 45), LottoResult.FAIL),
+            Arguments.of(Lotto(40, 41, 42, 43, 44, 45), LottoResult.FAIL)
+        )
     }
 }
