34add19d8270c0cfd7e2f1e989ff3cdd424e97d9
yrsel <xxknu88@gmail.com>
2025-02-22 14:06:28 +0900

    refactor: 로또 등수 판단 테스트 데이터에 대한 이름 추가
    맞춘 번호와 보너스 일치 여부, 등수에 대한 정보를 ParameterizedTest의 name 값으로 넣어서 테스트에 대한 예측 결과가 명확하도록 수정

--- src/test/kotlin/lotto/RankTest.kt
+++ src/test/kotlin/lotto/RankTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class RankTest {
-    @ParameterizedTest
+    @ParameterizedTest(name = "{0}개의 번호를 맞추고 보너스 번호 일치 여부가 {1}라면 등수는 {2}다")
     @MethodSource("getSource")
     fun `맞춘 개수와 보너스 일치 여부에 따라 등수를 판단한다`(
         matchCount: Int,
@@ -28,7 +28,9 @@ class RankTest {
                 Arguments.arguments(5, false, Rank.THIRD),
                 Arguments.arguments(4, false, Rank.FOURTH),
                 Arguments.arguments(3, false, Rank.FIFTH),
+                Arguments.arguments(2, false, Rank.MISS),
                 Arguments.arguments(1, false, Rank.MISS),
+                Arguments.arguments(0, false, Rank.MISS),
             )
     }
 }
