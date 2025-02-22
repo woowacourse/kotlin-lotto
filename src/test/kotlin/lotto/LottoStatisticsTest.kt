dbee2df7fbfcb1812fcc4f4aa485f229259d24e0
m6z1 <sonmyungj1zz@gmail.com>
2025-02-22 21:38:23 +0900

    test: 당첨 통계에 대한 수익률 계산 테스트 수정

--- src/test/kotlin/lotto/LottoStatisticsTest.kt
+++ src/test/kotlin/lotto/LottoStatisticsTest.kt
@@ -8,11 +8,9 @@ import lotto.model.Lottos
 import lotto.model.WinningLotto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
-import java.util.stream.Stream
 
 class LottoStatisticsTest {
     private lateinit var lottos: Lottos
@@ -32,20 +30,21 @@ class LottoStatisticsTest {
         purchaseAmount = LottoPurchaseAmount(3000)
     }
 
-    @ParameterizedTest
-    @MethodSource("getWinningLottos")
-    fun `당첨 로또 금액에 대한 수익률을 게산한다`(
-        winningLotto: WinningLotto,
-        rateOfReturn: Double,
-    ) {
+    @Test
+    fun `당첨 로또 금액에 대한 수익률을 게산한다`() {
         val lottoStatistics =
             LottoStatistics(
                 lottos = lottos,
-                winningLotto = winningLotto,
+                winningLotto =
+                    WinningLotto(
+                        Lotto(1, 2, 3, 4, 10, 11),
+                        LottoNumber(13),
+                    ),
                 purchaseMoney = purchaseAmount,
             )
         val actual = lottoStatistics.getRateOfReturn()
-        assertThat(actual).isEqualTo(rateOfReturn)
+
+        assertThat(actual).isEqualTo(50.0)
     }
 
     @ParameterizedTest
@@ -82,33 +81,5 @@ class LottoStatisticsTest {
         val expected = false
 
         assertThat(lottoStatistics.getIsLossMoney(rateOfReturn)).isEqualTo(expected)
-    }
-
-    companion object {
-        @JvmStatic
-        fun getWinningLottos(): Stream<Arguments> =
-            Stream.of(
-                Arguments.arguments(
-                    WinningLotto(
-                        Lotto(1, 2, 3, 4, 5, 6),
-                        LottoNumber(7),
-                    ),
-                    677166.6666666666,
-                ),
-                Arguments.arguments(
-                    WinningLotto(
-                        Lotto(10, 11, 12, 14, 15, 18),
-                        LottoNumber(7),
-                    ),
-                    0.0,
-                ),
-                Arguments.arguments(
-                    WinningLotto(
-                        Lotto(1, 2, 3, 4, 10, 11),
-                        LottoNumber(13),
-                    ),
-                    50.0,
-                ),
-            )
     }
 }
