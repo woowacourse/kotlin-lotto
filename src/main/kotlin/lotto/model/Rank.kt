4437b3e01e5b3c610c0a83467dc29654cccc2aba
m6z1 <sonmyungj1zz@gmail.com>
2025-02-22 19:39:55 +0900

    refactor: 등수 객체의 보너스 맞춤 여부 필드 제거

--- src/main/kotlin/lotto/model/Rank.kt
+++ src/main/kotlin/lotto/model/Rank.kt
@@ -2,15 +2,14 @@ package lotto.model
 
 enum class Rank(
     val matchCount: Int,
-    private val isMatchedBonus: Boolean,
     val prizeMoney: Int,
 ) {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-    FOURTH(4, false, 50_000),
-    FIFTH(3, false, 5_000),
-    MISS(0, false, 0),
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0),
 
     ;
 
@@ -28,8 +27,8 @@ enum class Rank(
 
         private fun findSecondOrThird(isMatchedBonus: Boolean): Rank =
             when (isMatchedBonus) {
-                SECOND.isMatchedBonus -> SECOND
-                else -> THIRD
+                true -> SECOND
+                false -> THIRD
             }
     }
 }
