aff7d7aa371fac2cffe5d9273a1bf065c5d3f739
DoableTuple <doabletuple@gmail.com>
2025-02-22 22:00:41 +0900

    refactor: readLine()을 readln()으로 대체

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -2,18 +2,17 @@ package lotto.view
 
 object InputView {
     private const val LOTTO_NUMBERS_DELIMITER = ","
-    private const val ERROR_MESSAGE_INVALID_INPUT_STATE = "정상적으로 입력되지 않았습니다."
     private const val ERROR_MESSAGE_INPUT_NOT_A_NUMBER = "숫자를 입력해주세요."
     private const val ERROR_MESSAGE_INVALID_WINNING_NUMBERS = "양식에 맞게 입력해주세요. (1, 2, 3, 4, 5, 6)"
 
     fun readPrice(): Int {
-        val input: String = readLine() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT_STATE)
+        val input: String = readln()
         val price: Int = input.toIntOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_INPUT_NOT_A_NUMBER)
         return price
     }
 
     fun readWinningNumbers(): List<Int> {
-        val lottoInput: String = readLine() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT_STATE)
+        val lottoInput: String = readln()
         val wonLotto: List<Int> =
             lottoInput.split(LOTTO_NUMBERS_DELIMITER).map { number: String ->
                 number.trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_WINNING_NUMBERS)
@@ -22,7 +21,7 @@ object InputView {
     }
 
     fun readBonusNumber(): Int {
-        val bonusInput: String = readLine() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT_STATE)
+        val bonusInput: String = readln()
         val bonusNumber: Int =
             bonusInput.toIntOrNull() ?: throw IllegalArgumentException(ERROR_MESSAGE_INPUT_NOT_A_NUMBER)
         return bonusNumber
