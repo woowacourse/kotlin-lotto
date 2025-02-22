98bef16fb014867cfd4094ad4c5ad10b449d22a2
ijh1298 <imjh0417@gmail.com>
2025-02-22 23:09:55 +0900

    test : BonusNumberTest를 LottoNumberTest로 수정

--- src/test/kotlin/domain/model/LottoNumberTest.kt
+++ src/test/kotlin/domain/model/LottoNumberTest.kt
@@ -7,11 +7,11 @@ import org.junit.jupiter.params.provider.ValueSource
 class LottoNumberTest {
     @ValueSource(ints = [0, 46])
     @ParameterizedTest
-    fun `보너스볼 번호는 1이상 45 이하여야한다`(value: Int) {
+    fun `로또 번호는 1이상 45 이하여야한다`(value: Int) {
         Assertions
             .assertThatThrownBy {
                 LottoNumber(value)
             }.isInstanceOf(IllegalArgumentException::class.java)
-            .hasMessage("[ERROR] 보너스 볼 번호는 1부터 45 사이입니다.")
+            .hasMessage("[ERROR] 로또 번호는 1부터 45 사이입니다.")
     }
 }
