c2e2f2a35bd7063fee69927d40c48b66bc7c948e
m6z1 <sonmyungj1zz@gmail.com>
2025-02-22 19:16:28 +0900

    refactor: 로또 번호 value class 로 수정

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,18 +1,12 @@
 package lotto.model
 
-class LottoNumber(
+@JvmInline
+value class LottoNumber(
     val number: Int,
 ) {
     init {
         require(number in MIN_NUMBER..MAX_NUMBER) { ERROR_OUT_OF_RANGE_NUMBERS.format(number) }
     }
-
-    override fun equals(other: Any?): Boolean {
-        other as LottoNumber
-        return number == other.number
-    }
-
-    override fun hashCode(): Int = number
 
     companion object {
         private const val ERROR_OUT_OF_RANGE_NUMBERS = "%d는 범위를 벗어났습니다. 로또 번호는 1 ~ 45 사이의 숫자입니다."
