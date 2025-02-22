372e30a557e32f197298a2a297a1c9d3fd6e7a88
ijh1298 <imjh0417@gmail.com>
2025-02-22 23:17:13 +0900

    refactor : Lotto 클래스에서 불필요한 번호 범위 검증 제거

--- src/main/kotlin/domain/model/Lotto.kt
+++ src/main/kotlin/domain/model/Lotto.kt
@@ -3,7 +3,6 @@ package domain.model
 class Lotto(val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_SIZE) { INVALID_LOTTO_SIZE }
-        require(numbers.all { it.value in (1..45) }) { INVALID_LOTTO_NUMBERS }
         require(numbers.size == numbers.toSet().size) { DUPLICATED_LOTTO_NUMBERS }
     }
 
@@ -15,7 +14,6 @@ class Lotto(val numbers: List<LottoNumber>) {
         const val ERROR = "[ERROR]"
         const val LOTTO_SIZE = 6
         const val INVALID_LOTTO_SIZE = "$ERROR 로또 번호는 6개 입니다."
-        const val INVALID_LOTTO_NUMBERS = "$ERROR 로또 번호는 1부터 45 사이입니다."
         const val DUPLICATED_LOTTO_NUMBERS = "$ERROR 로또 번호는 중복이 없습니다."
     }
 }
