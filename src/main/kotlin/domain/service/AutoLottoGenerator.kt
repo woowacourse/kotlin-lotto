b8640c6733cf0a6915414d35eb1b2b9748b35550
ijh1298 <imjh0417@gmail.com>
2025-02-23 23:52:08 +0900

    refactor : 자동 로또 Generator, 수동 로또 Generator에서 하나의 로또만 생성하도록 수정

--- src/main/kotlin/domain/service/AutoLottoGenerator.kt
+++ src/main/kotlin/domain/service/AutoLottoGenerator.kt
@@ -2,15 +2,9 @@ package domain.service
 
 import domain.model.Lotto
 import domain.model.LottoNumber
-import domain.model.PurchasePrice
 
-class AutoLottoGenerator(
-    private val money: PurchasePrice,
-) : LottoNumbersGenerator {
-    fun makeLotto(): List<Lotto> {
-        val purchaseLottoCount = money.value / PurchasePrice.Companion.STANDARD_AMOUNT_UNIT
-        return List(purchaseLottoCount) { makeOneLotto(makeLottoNumbers()) }
-    }
+class AutoLottoGenerator : LottoNumbersGenerator {
+    fun makeLotto(): Lotto = Lotto(makeLottoNumbers())
 
     override fun makeLottoNumbers(): Set<LottoNumber> =
         (LOTTO_MIN..LOTTO_MAX)
@@ -18,8 +12,6 @@ class AutoLottoGenerator(
             .take(6)
             .map { LottoNumber(it) }
             .toSet()
-
-    private fun makeOneLotto(lottoNumbers: Set<LottoNumber>): Lotto = Lotto(lottoNumbers)
 
     companion object {
         const val LOTTO_MIN = 1
--- src/main/kotlin/domain/service/ManualLottoGenerator.kt
+++ src/main/kotlin/domain/service/ManualLottoGenerator.kt
@@ -2,18 +2,11 @@ package domain.service
 
 import domain.model.Lotto
 import domain.model.LottoNumber
-import domain.model.PurchasePrice
 
 class ManualLottoGenerator(
-    private val money: PurchasePrice,
     private val manualLottoNumbers: Set<Int>,
 ) : LottoNumbersGenerator {
-    fun makeLotto(): List<Lotto> {
-        val purchaseLottoCount = money.value / PurchasePrice.Companion.STANDARD_AMOUNT_UNIT
-        return List(purchaseLottoCount) { makeOneLotto(makeLottoNumbers()) }
-    }
+    fun makeLotto(): Lotto = Lotto(makeLottoNumbers())
 
     override fun makeLottoNumbers(): Set<LottoNumber> = manualLottoNumbers.map { LottoNumber(it) }.toSet()
-
-    private fun makeOneLotto(lottoNumbers: Set<LottoNumber>): Lotto = Lotto(lottoNumbers)
 }
