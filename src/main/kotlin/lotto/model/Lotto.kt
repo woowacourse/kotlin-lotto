d337850b35c2bff7c7c99e9efb74dad0a7ff8337
eden <devfeijoa@gmail.com>
2025-02-23 20:29:10 +0900

    refactor: 에러메세지도 검증하는 로직으로 변경

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -4,10 +4,10 @@ class Lotto(
     val numbers: List<Int>,
 ) {
     init {
-        require(numbers.distinct().size == numbers.size) { "로또 번호가 중복될 수 없습니다." }
-        require(numbers.size == LOTTO_NUMBERS_SIZE) { "로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == numbers.size) { LOTTO_DISTINCT_MESSAGE }
+        require(numbers.size == LOTTO_NUMBERS_SIZE) { LOTTO_COUNT_MESSAGE }
         numbers.forEach { number ->
-            require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { "로또 번호는 1에서 45 범위 내에서 있어야 합니다." }
+            require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { LOTTO_BOUND_MESSAGE }
         }
     }
 
@@ -15,5 +15,8 @@ class Lotto(
         const val LOTTO_NUMBERS_SIZE: Int = 6
         const val MAXIMUM_LOTTO_RANGE: Int = 45
         const val MINIMUM_LOTTO_RANGE: Int = 1
+        const val LOTTO_COUNT_MESSAGE = "로또 번호는 6개여야 합니다."
+        const val LOTTO_BOUND_MESSAGE = "로또 번호는 1에서 45 범위 내에서 있어야 합니다."
+        const val LOTTO_DISTINCT_MESSAGE = "로또 번호가 중복될 수 없습니다."
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,6 +1,9 @@
 package lotto
 
 import lotto.model.Lotto
+import lotto.model.Lotto.Companion.LOTTO_BOUND_MESSAGE
+import lotto.model.Lotto.Companion.LOTTO_COUNT_MESSAGE
+import lotto.model.Lotto.Companion.LOTTO_DISTINCT_MESSAGE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -14,16 +17,19 @@ class LottoTest {
 
     @Test
     fun `발급하는 로또 번호는 6개가 아니면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 6, 7)) }
+        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 6, 7)) }
+        assertThat(exception).hasMessage(LOTTO_COUNT_MESSAGE)
     }
 
     @Test
     fun `발급하는 로또 번호는 1부터 45 범위 내에 있어야 한다 `() {
-        assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 46)) }
+        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 46)) }
+        assertThat(exception).hasMessage(LOTTO_BOUND_MESSAGE)
     }
 
     @Test
     fun `로또 번호는 중복되지 않아야 한다 `() {
-        assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 6, 6)) }
+        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 6, 6)) }
+        assertThat(exception).hasMessage(LOTTO_DISTINCT_MESSAGE)
     }
 }
