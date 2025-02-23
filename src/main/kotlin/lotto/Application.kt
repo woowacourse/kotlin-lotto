d16ad55d89ec5a7d3e5dd99cae334979b3fa8eda
junseo511 <prime54@g.skku.edu>
2025-02-24 00:15:02 +0900

    fix: 컴파일 오류 해결

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,12 +1,14 @@
 package lotto
 
 import lotto.controller.LottoController
+import lotto.service.LottoService
 import lotto.view.InputView
 import lotto.view.OutputView
 
 fun main() {
     val inputView = InputView()
     val outputView = OutputView()
+    val lottoService = LottoService()
 
-    LottoController(inputView, outputView).run()
+    LottoController(inputView, outputView, lottoService).run()
 }
