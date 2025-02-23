88edb8d373a45bdc536ac3037137f3091d36e4a1
eden <devfeijoa@gmail.com>
2025-02-23 19:42:49 +0900

    refactor: 불필요한 LottoStore 제거 후 직접 RandomLottoGenerator를 부르는것으로 수정

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -3,7 +3,6 @@ package lotto.controller
 import lotto.model.Lotto
 import lotto.model.LottoMatchCalculator
 import lotto.model.LottoStatisticResult
-import lotto.model.LottoStore
 import lotto.model.LottoTicketCounter
 import lotto.model.RandomLottoGenerator
 import lotto.model.WinningLotto
@@ -13,7 +12,6 @@ import lotto.view.OutputView
 class LottoController {
     private val inputView = InputView()
     private val outputView = OutputView()
-    private val lottoStore = LottoStore()
 
     fun play() {
         val purchase = inputView.inputPurchase()
@@ -36,6 +34,6 @@ class LottoController {
     private fun getBonusNumber(): Int = inputView.inputBonusNumber()
 
     private fun purchaseLottos(count: Int): List<Lotto> {
-        return lottoStore.getTickets(count, RandomLottoGenerator())
+        return RandomLottoGenerator().generate(count)
     }
 }
