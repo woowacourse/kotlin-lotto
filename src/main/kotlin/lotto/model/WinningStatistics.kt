10ca98b4961504bc585b8ecbd177e3e9871621c6
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-23 21:29:03 +0900

    refactor(WinningStatistics): 로또 통계 계산 책임 분리 - 로또 통계 계산 책임을 `printResult`에서  `WinningStatistics`로 이동

---
+++ src/main/kotlin/lotto/model/WinningStatistics.kt
@@ -0,0 +1,22 @@
+package lotto.model
+
+class WinningStatistics(
+    lottoBundle: List<Lotto>,
+    winningLotto: WinningLotto,
+) {
+    private val statistics: Map<Rank, Int> = calculateStatistics(lottoBundle, winningLotto)
+
+    private fun calculateStatistics(
+        lottoBundle: List<Lotto>,
+        winningLotto: WinningLotto,
+    ): Map<Rank, Int> {
+        val result = Rank.entries.associateWith { 0 }.toMutableMap()
+        lottoBundle.forEach { lotto ->
+            val rank = Rank.fromMatch(winningLotto.getCountOfMatch(lotto), winningLotto.isBonusNumberMatch(lotto))
+            result[rank] = result[rank]!! + 1
+        }
+        return result
+    }
+
+    fun getAllStatistics(): Map<Rank, Int> = statistics
+}
