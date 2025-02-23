b38424f9b5333864d9dd979c7b97661cb4e57b28
chanho0908 <uni070@naver.com>
2025-02-23 14:50:53 +0900

    refactor : 수동 로또 팩토리 함수 파라미터 네이밍 수정

--- src/main/kotlin/domain/model/PassivityLottoAmount.kt
+++ src/main/kotlin/domain/model/PassivityLottoAmount.kt
@@ -6,12 +6,12 @@ value class PassivityLottoAmount private constructor(val value: Int) {
         private const val INVALID_PASSIVITY_LOTTO_SIZE = "수동 구매 개수가 구입 가능한 로또 개수를 초과했습니다."
 
         fun create(
-            value: Int,
+            lottoAmount: Int,
             money: PurchasePrice,
         ): PassivityLottoAmount {
             val purchasableAmount = money.getPurchasableLottoCount()
-            require(value <= purchasableAmount) { INVALID_PASSIVITY_LOTTO_SIZE }
-            return PassivityLottoAmount(value)
+            require(lottoAmount <= purchasableAmount) { INVALID_PASSIVITY_LOTTO_SIZE }
+            return PassivityLottoAmount(lottoAmount)
         }
     }
 }
