7b22409fa3ed5146a7185d37b372440ec596fe90
yrsel <xxknu88@gmail.com>
2025-02-23 19:16:05 +0900

    refactor: assertThrows{} 로 테스트 코드 수정

--- src/test/kotlin/lotto/LottoNumberTest.kt
+++ src/test/kotlin/lotto/LottoNumberTest.kt
@@ -1,8 +1,8 @@
 package lotto
 
 import lotto.model.LottoNumber
-import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -10,9 +10,7 @@ class LottoNumberTest {
     @ParameterizedTest
     @ValueSource(ints = [0, 46, -100])
     fun `로또 번호는 1~45 사이의 숫자가 아닐 경우 예외를 발생한다`(number: Int) {
-        assertThatIllegalArgumentException()
-            .isThrownBy { LottoNumber(number) }
-            .withMessage("로또 번호는 1 ~ 45 사이의 숫자입니다.")
+        assertThrows<IllegalArgumentException> { LottoNumber(number) }
     }
 
     @ParameterizedTest
