fe82d375b85a3e6e98fe9aa2e3475c483f7f05e9
gahyunkim <mapledt001@naver.com>
2025-02-23 15:17:12 +0900

    test: 보너스 볼 번호는 1~45 숫자 사이에 해당한다는 테스트 코드 구현

--- src/test/kotlin/lotto/WinningLottoTest.kt
+++ src/test/kotlin/lotto/WinningLottoTest.kt
@@ -3,7 +3,10 @@ package lotto
 import lotto.domain.LottoNumber
 import lotto.domain.WinningLotto
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class WinningLottoTest {
     @Test
@@ -12,5 +15,12 @@ class WinningLottoTest {
         val bonusNumber: LottoNumber = LottoNumber(6)
 
         assertThrows<IllegalArgumentException> { WinningLotto(winningNumbers, bonusNumber) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [7, 8, 9, 10, 22, 44])
+    fun `보너스 볼 번호는 1~45 숫자 사이에 해당한다`(bonusNumber: Int) {
+        val winningNumbers = lottoOf(1, 2, 3, 4, 5, 6)
+        assertDoesNotThrow { WinningLotto(winningNumbers, LottoNumber(bonusNumber)) }
     }
 }
