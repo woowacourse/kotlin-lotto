43700cca75540f5d5878a30900c75884374e5b79
지원 <cheerfulboys@naver.com>
2025-02-23 11:18:17 +0900

    refactor: Lotto class에 검증 로직 추가 및 리팩토링

--- src/test/kotlin/lotto/domain/LottoTest.kt
+++ src/test/kotlin/lotto/domain/LottoTest.kt
@@ -1,21 +1,28 @@
 package lotto.domain
 
-import lotto.generator.LottoRandomGenerator
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
     @Test
-    fun `로또 번호는 중복되지 않는 6개의 숫자를 갖는다`() {
-        val randomGenerator = LottoRandomGenerator()
-        val lotto = Lotto(randomGenerator.getRandomNumberList())
+    fun `로또는 중복되지 않는 6개의 숫자를 갖는다`() {
+        val lotto = Lotto(listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)))
         assertThat(lotto.numbers.size).isEqualTo(6)
     }
 
     @Test
-    fun `로또 번호는 오름차순으로 정렬한다`() {
-        val randomGenerator = LottoRandomGenerator()
-        val lotto = Lotto(randomGenerator.getRandomNumberList())
-        assertThat(lotto.numbers).isSorted
+    fun `로또는 중복된 숫자를 가지면 오류가 발생한다`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(LottoNumber(1), LottoNumber(1), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)))
+        }
+    }
+
+    @Test
+    fun `로또 번호는 오름차순으로 정렬된다`() {
+        val lotto = Lotto(listOf(LottoNumber(7), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6)))
+        assertThat(
+            lotto.numbers,
+        ).isEqualTo(listOf(LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(6), LottoNumber(7)))
     }
 }
