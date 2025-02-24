322a3d5370b2d2280592ea5d2a4cd6bd8637e471
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-24 22:02:38 +0900

    refactor: rename WinningLotto -> WinLotto

--- src/test/kotlin/lotto/domain/WinLottoTest.kt
+++ src/test/kotlin/lotto/domain/WinLottoTest.kt
@@ -3,7 +3,7 @@ package lotto.domain
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class WinningLottoTest {
+class WinLottoTest {
     @Test
     fun `보너스 번호는 당첨 번호와 달라야 한다`() {
         assertThrows<IllegalArgumentException> {
