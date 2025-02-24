cffcb38453c55ce3cd263b904ad80afadc941bf6
moondev03 <moondev03@gmail.com>
2025-02-24 12:52:01 +0900

    feat: 로또 생성 성공 테스트 추가

--- src/test/kotlin/lotto/domain/model/LottoTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoTest.kt
@@ -2,9 +2,16 @@ package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
+    @Test
+    fun `로또를 생성할 수 있다`() {
+        val numbers = listOf(1, 2, 3, 4, 5, 6)
+        assertDoesNotThrow { Lotto(numbers) }
+    }
+
     @Test
     fun `로또는 로또 번호의 개수가 6이 아니라면 예외가 발생한다`() {
         val numbers = listOf(1, 2, 3, 4, 5, 6, 7)
