0e9399f2d28a3193921e6f358a5a905525953b72
junseo511 <prime54@g.skku.edu>
2025-02-24 15:11:07 +0900

    refactor: GIVEN-WHEN-THEN 주석을 활용한 테스트 분리

--- src/test/kotlin/lotto/model/RankTest.kt
+++ src/test/kotlin/lotto/model/RankTest.kt
@@ -6,76 +6,144 @@ import org.junit.jupiter.api.Test
 class RankTest {
     @Test
     fun `6개 번호가 맞으면 1등을 반환한다`() {
-        testRankByCountOfMatch(6, Rank.FIRST)
+        // given
+        val countOfMatch = 6
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.FIRST, rank)
     }
 
     @Test
     fun `5개 번호가 맞으면 3등을 반환한다`() {
-        testRankByCountOfMatch(5, Rank.THIRD)
+        // given
+        val countOfMatch = 5
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.THIRD, rank)
     }
 
     @Test
     fun `4개 번호가 맞으면 4등을 반환한다`() {
-        testRankByCountOfMatch(4, Rank.FOURTH)
+        // given
+        val countOfMatch = 4
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.FOURTH, rank)
     }
 
     @Test
     fun `3개 번호가 맞으면 5등을 반환한다`() {
-        testRankByCountOfMatch(3, Rank.FIFTH)
+        // given
+        val countOfMatch = 3
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.FIFTH, rank)
     }
 
     @Test
     fun `2개 번호가 맞으면 MISS를 반환한다`() {
-        testRankByCountOfMatch(2, Rank.MISS)
+        // given
+        val countOfMatch = 2
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.MISS, rank)
     }
 
     @Test
     fun `1개 번호가 맞으면 MISS를 반환한다`() {
-        testRankByCountOfMatch(1, Rank.MISS)
+        // given
+        val countOfMatch = 1
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.MISS, rank)
     }
 
     @Test
     fun `0개 번호가 맞으면 MISS를 반환한다`() {
-        testRankByCountOfMatch(0, Rank.MISS)
-    }
-
-    private fun testRankByCountOfMatch(
-        countOfMatch: Int,
-        expectedRank: Rank,
-    ) {
+        // given
+        val countOfMatch = 0
         val matchBonus = false
+
+        // when
         val rank = Rank.from(countOfMatch, matchBonus)
 
-        assertEquals(expectedRank, rank)
+        // then
+        assertEquals(Rank.MISS, rank)
     }
 
     @Test
     fun `로또 내에 보너스 번호가 있으면 2등을 반환한다`() {
-        testRankByBonus(5, true, Rank.SECOND)
+        // given
+        val countOfMatch = 5
+        val matchBonus = true
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.SECOND, rank)
     }
 
     @Test
     fun `로또 내에 보너스 번호가 다르면 3등을 반환한다`() {
-        testRankByBonus(5, false, Rank.THIRD)
+        // given
+        val countOfMatch = 5
+        val matchBonus = false
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.THIRD, rank)
     }
 
     @Test
     fun `3개 번호가 맞고 보너스 번호가 맞아도 5등을 반환한다`() {
-        testRankByBonus(3, true, Rank.FIFTH)
+        // given
+        val countOfMatch = 3
+        val matchBonus = true
+
+        // when
+        val rank = Rank.from(countOfMatch, matchBonus)
+
+        // then
+        assertEquals(Rank.FIFTH, rank)
     }
 
     @Test
     fun `3개 번호가 맞고 보너스 번호가 틀려도 5등을 반환한다`() {
-        testRankByBonus(3, false, Rank.FIFTH)
-    }
+        // given
+        val countOfMatch = 3
+        val matchBonus = false
 
-    private fun testRankByBonus(
-        countOfMatch: Int,
-        matchBonus: Boolean,
-        expectedRank: Rank,
-    ) {
+        // when
         val rank = Rank.from(countOfMatch, matchBonus)
 
-        assertEquals(expectedRank, rank)
+        // then
+        assertEquals(Rank.FIFTH, rank)
     }
 }
--- src/test/kotlin/lotto/model/WinningDiscriminatorTest.kt
+++ src/test/kotlin/lotto/model/WinningDiscriminatorTest.kt
@@ -7,55 +7,112 @@ import org.junit.jupiter.api.assertThrows
 class WinningDiscriminatorTest {
     @Test
     fun `6개의 번호가 일치하면 1등을 반환한다`() {
-        testRank(listOf(1, 2, 3, 4, 5, 6), Rank.FIRST)
+        // given
+        val lottoNumbers = listOf(1, 2, 3, 4, 5, 6)
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = LottoNumber.from(7)
+        val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
+        val lotto = listOf(Lotto.from(lottoNumbers))
+
+        // when
+        val discriminateResult = winningDiscriminator.getResult(lotto)
+
+        // then
+        assertEquals(1, discriminateResult[Rank.FIRST])
     }
 
     @Test
     fun `5개의 번호가 일치하면 3등을 반환한다`() {
-        testRank(listOf(1, 2, 3, 4, 5, 8), Rank.THIRD)
+        // given
+        val lottoNumbers = listOf(1, 2, 3, 4, 5, 8)
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = LottoNumber.from(7)
+        val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
+        val lotto = listOf(Lotto.from(lottoNumbers))
+
+        // when
+        val discriminateResult = winningDiscriminator.getResult(lotto)
+
+        // then
+        assertEquals(1, discriminateResult[Rank.THIRD])
     }
 
     @Test
     fun `4개의 번호가 일치하면 4등을 반환한다`() {
-        testRank(listOf(1, 2, 3, 4, 9, 10), Rank.FOURTH)
+        // given
+        val lottoNumbers = listOf(1, 2, 3, 4, 9, 10)
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = LottoNumber.from(7)
+        val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
+        val lotto = listOf(Lotto.from(lottoNumbers))
+
+        // when
+        val discriminateResult = winningDiscriminator.getResult(lotto)
+
+        // then
+        assertEquals(1, discriminateResult[Rank.FOURTH])
     }
 
     @Test
     fun `3개의 번호가 일치하면 5등을 반환한다`() {
-        testRank(listOf(1, 2, 3, 8, 9, 10), Rank.FIFTH)
+        // given
+        val lottoNumbers = listOf(1, 2, 3, 8, 9, 10)
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = LottoNumber.from(7)
+        val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
+        val lotto = listOf(Lotto.from(lottoNumbers))
+
+        // when
+        val discriminateResult = winningDiscriminator.getResult(lotto)
+
+        // then
+        assertEquals(1, discriminateResult[Rank.FIFTH])
     }
 
     @Test
     fun `2개 이하의 번호가 일치하면 MISS를 반환한다`() {
-        testRank(listOf(1, 2, 23, 24, 25, 26), Rank.MISS)
-        testRank(listOf(1, 22, 23, 24, 25, 26), Rank.MISS)
-        testRank(listOf(21, 22, 23, 24, 25, 26), Rank.MISS)
+        listOf(
+            listOf(1, 2, 23, 24, 25, 26),
+            listOf(1, 22, 23, 24, 25, 26),
+            listOf(21, 22, 23, 24, 25, 26),
+        ).forEach { lottoNumbers ->
+            // given
+            val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+            val bonusNumber = LottoNumber.from(7)
+            val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
+            val lotto = listOf(Lotto.from(lottoNumbers))
+
+            // when
+            val discriminateResult = winningDiscriminator.getResult(lotto)
+
+            // then
+            assertEquals(1, discriminateResult[Rank.MISS])
+        }
     }
 
     @Test
     fun `보너스 번호가 일치하면서 5개의 번호가 동일한 경우 2등을 반환한다`() {
-        testRank(listOf(1, 2, 3, 4, 5, 7), Rank.SECOND)
-    }
-
-    private fun testRank(
-        lottoNumbers: List<Int>,
-        expectedRank: Rank,
-    ) {
+        // given
+        val lottoNumbers = listOf(1, 2, 3, 4, 5, 7)
         val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
         val bonusNumber = LottoNumber.from(7)
         val winningDiscriminator = WinningDiscriminator(winningLotto, bonusNumber)
-
         val lotto = listOf(Lotto.from(lottoNumbers))
+
+        // when
         val discriminateResult = winningDiscriminator.getResult(lotto)
 
-        assertEquals(1, discriminateResult[expectedRank])
+        // then
+        assertEquals(1, discriminateResult[Rank.SECOND])
     }
 
     @Test
     fun `당첨 번호와 보너스 번호가 중복되면 오류를 반환한다`() {
+        // given
         val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
         val bonusNumber = LottoNumber.from(1)
 
+        // when & then
         assertThrows<IllegalArgumentException> {
             WinningDiscriminator(winningLotto, bonusNumber)
         }
@@ -63,9 +120,11 @@ class WinningDiscriminatorTest {
 
     @Test
     fun `당첨 번호끼리 중복되지 않았을 때, 당첨 번호와 보너스 번호와 중복되면 오류를 반환한다`() {
+        // given
         val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
         val bonusNumber = LottoNumber.from(1)
 
+        // when & then
         assertThrows<IllegalArgumentException> {
             WinningDiscriminator(winningLotto, bonusNumber)
         }
