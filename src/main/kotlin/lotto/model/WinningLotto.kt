5ccf4f102ee55f8c1883e1067036d92dd346656f
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-24 16:52:11 +0900

    refactor: WinningLottoTest 내부 로직 수정

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -5,12 +5,17 @@ class WinningLotto(
     private val bonusNumber: LottoNumber,
 ) {
     init {
-        require(!lotto.numbers.contains(bonusNumber)) { ERROR_LOTTO_BONUS_DUPLICATED_MESSAGE }
+        require(!lotto.numbers.map { it.number }.contains(bonusNumber.number)) { ERROR_LOTTO_BONUS_DUPLICATED_MESSAGE }
     }
 
-    fun getCountOfMatch(lotto: Lotto): Int = lotto.numbers.count { number -> this.lotto.numbers.contains(number) }
+    fun getCountOfMatch(lotto: Lotto): Int =
+        lotto.numbers.count { number ->
+            this.lotto.numbers
+                .map { it.number }
+                .contains(number.number)
+        }
 
-    fun isBonusNumberMatch(lotto: Lotto): Boolean = lotto.numbers.contains(this.bonusNumber)
+    fun isBonusNumberMatch(lotto: Lotto): Boolean = lotto.numbers.map { it.number }.contains(this.bonusNumber.number)
 
     companion object {
         private const val ERROR_LOTTO_BONUS_DUPLICATED_MESSAGE = "당첨 번호와 보너스 번호는 중복될 수 없습니다."
