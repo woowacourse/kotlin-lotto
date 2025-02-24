0db158a43bcc307ca5e7942dd551e12763586c55
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-24 22:53:15 +0900

    refactor(Profit): 함수 이동

--- src/main/kotlin/lotto/model/Profit.kt
+++ src/main/kotlin/lotto/model/Profit.kt
@@ -3,20 +3,20 @@ package lotto.model
 import kotlin.math.floor
 
 class Profit {
-    fun calculateProfit(
-        input: String,
-        winningStatistics: WinningStatistics,
-    ): String {
-        val purchaseAmount = input.toDouble()
-        val sum =
-            winningStatistics.getAllStatistics().entries.sumOf { (rank, winningCount) ->
-                winningCount * rank.winningMoney
-            }
-        val profit = floor((sum.toDouble() / purchaseAmount) * PERCENT_FACTOR) / PERCENT_FACTOR
-        return profit.toString()
-    }
-
     companion object {
+        fun calculateProfit(
+            input: String,
+            winningStatistics: WinningStatistics,
+        ): String {
+            val purchaseAmount = input.toDouble()
+            val sum =
+                winningStatistics.getAllStatistics().entries.sumOf { (rank, winningCount) ->
+                    winningCount * rank.winningMoney
+                }
+            val profit = floor((sum.toDouble() / purchaseAmount) * PERCENT_FACTOR) / PERCENT_FACTOR
+            return profit.toString()
+        }
+
         private const val PERCENT_FACTOR = 100
     }
 }
