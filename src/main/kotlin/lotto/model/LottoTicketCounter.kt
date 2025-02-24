3503595ccfc6ae33526cc5e30e9e4b65cb509970
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-24 14:42:19 +0900

    refactor(LottoTicketCount): 검증 로직 수정

--- src/main/kotlin/lotto/model/LottoTicketCounter.kt
+++ src/main/kotlin/lotto/model/LottoTicketCounter.kt
@@ -7,13 +7,10 @@ class LottoTicketCounter(
 
     init {
         require(purchase.toInt() >= PURCHASE_STANDARD) { ERROR_MINIMUM_PURCHASE_MESSAGE }
-        require(purchase.toInt() % PURCHASE_STANDARD == REMAINDER) { ERROR_PURCHASE_DIVIDE_MESSAGE }
     }
 
     companion object {
         private const val PURCHASE_STANDARD = 1000
-        private const val REMAINDER = 0
         private const val ERROR_MINIMUM_PURCHASE_MESSAGE = "구입 금액은 최소 1000원 이상 이어야 합니다."
-        private const val ERROR_PURCHASE_DIVIDE_MESSAGE = "구입 금액은 1000원 단위로 나누어 떨어져야 합니다."
     }
 }
