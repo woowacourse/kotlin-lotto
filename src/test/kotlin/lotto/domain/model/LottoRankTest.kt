90c1b536145d5581ba1471db0a9d39012b55855c
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-24 16:46:44 +0900

    refactor: CsvSource로 가능한 테스트 변경

--- src/test/kotlin/lotto/domain/model/LottoRankTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoRankTest.kt
@@ -2,11 +2,16 @@ package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoRankTest {
-    @MethodSource("lottoRankTest")
+    @CsvSource(
+        "0, false, MISS",
+        "3, false, FIFTH",
+        "5, false, THIRD",
+        "5, true, SECOND",
+        "6, false, FIRST",
+    )
     @ParameterizedTest
     fun `당첨 등수를 계산할 수 있다`(
         matchCount: Int,
@@ -14,19 +19,5 @@ class LottoRankTest {
         lottoRank: LottoRank,
     ) {
         assertThat(LottoRank.calculate(matchCount, matchBonus)).isEqualTo(lottoRank)
-    }
-
-    companion object {
-        @JvmStatic
-        fun lottoRankTest(): List<Arguments> {
-            return listOf(
-                Arguments.arguments(0, false, LottoRank.MISS),
-                Arguments.arguments(3, false, LottoRank.FIFTH),
-                Arguments.arguments(4, false, LottoRank.FOURTH),
-                Arguments.arguments(5, false, LottoRank.THIRD),
-                Arguments.arguments(5, true, LottoRank.SECOND),
-                Arguments.arguments(6, false, LottoRank.FIRST),
-            )
-        }
     }
 }
