ce4e31c732db56a7ad0701816e24976c6a852f3d
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-02-24 17:10:51 +0900

    refactor: 필요한 조건값만 테스트

--- src/test/kotlin/lotto/domain/model/LottoRanksTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoRanksTest.kt
@@ -4,13 +4,14 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.math.BigDecimal
 
 class LottoRanksTest {
     @ParameterizedTest
     @MethodSource("calculateTotalReturnTest")
     fun `총 수익률을 계산할 수 있다`(
         lottoRanks: Map<LottoRank, Int>,
-        expectedReturn: Double,
+        expectedReturn: BigDecimal,
     ) {
         val totalReturn = LottoRanks(lottoRanks).calculateTotalReturn()
         assertThat(totalReturn.toDouble()).isEqualTo(expectedReturn)
@@ -20,10 +21,7 @@ class LottoRanksTest {
         @JvmStatic
         fun calculateTotalReturnTest(): List<Arguments> {
             return listOf(
-                Arguments.arguments(mapOf(LottoRank.FIRST to 1), 2_000_000.00),
-                Arguments.arguments(mapOf(LottoRank.SECOND to 1), 30_000.00),
-                Arguments.arguments(mapOf(LottoRank.THIRD to 1), 1_500.00),
-                Arguments.arguments(mapOf(LottoRank.FOURTH to 1, LottoRank.FIFTH to 1), 27.50),
+                Arguments.arguments(mapOf(LottoRank.SECOND to 1), 20_000_000.00),
                 Arguments.arguments(mapOf(LottoRank.MISS to 1), 0.00),
                 Arguments.arguments(
                     mapOf(LottoRank.THIRD to 1, LottoRank.FIFTH to 1, LottoRank.SECOND to 1),
