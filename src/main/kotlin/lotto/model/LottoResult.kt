cfc3aade5d0ae02e9d58f9d4755c5ada91888fcb
m6z1 <sonmyungj1zz@gmail.com>
2025-02-25 00:38:22 +0900

    refactor: 원금 수익률 상수화

--- src/main/kotlin/lotto/model/LottoResult.kt
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -5,7 +5,15 @@ class LottoResult(
 ) {
     fun getRateOfReturn(purchaseAmount: Int): Double = getTotalPrizeMoney() / purchaseAmount.toDouble()
 
-    private fun getTotalPrizeMoney(): Long = ranks.entries.sumOf { (rank, count) -> rank.prizeMoney * count }.toLong()
+    private fun getTotalPrizeMoney(): Long =
+        ranks
+            .entries
+            .sumOf { (rank, count) -> rank.prizeMoney * count }
+            .toLong()
 
-    fun getIsLossMoney(rateOfReturn: Double): Boolean = rateOfReturn < 1
+    fun getIsLossMoney(rateOfReturn: Double): Boolean = rateOfReturn < PRINCIPAL_RATE_OF_RETURN
+
+    companion object {
+        private const val PRINCIPAL_RATE_OF_RETURN = 1
+    }
 }
