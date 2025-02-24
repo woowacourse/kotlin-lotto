e93805b94777c10a8ac71dc23c982e672cf676fc
박지민 <pjm5489@naver.com>
2025-02-24 16:23:28 +0900

    refactor: 보너스번호와 당첨번호 중복 여부 확인 기능 변경

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -4,21 +4,30 @@ class WinningLotto(
     private val lotto: Lotto,
 ) {
     fun getRank(
-        winningNumbers: List<Int>,
+        winningNumbers: Set<Int>,
         bonusNumber: Int,
     ): Rank {
         val countOfMatch = countMatchWinningNumbers(winningNumbers)
         val matchBonus = isHaveBonusNumber(bonusNumber)
 
-        lotto.validateLottoNumbersDuplicate(winningNumbers.map { LottoNumber(it) } + listOf(LottoNumber(bonusNumber)))
+        validateWinningNumbersBonusNumberDuplicate(winningNumbers, bonusNumber)
 
         return Rank.fromMatchResult(countOfMatch, matchBonus)
     }
 
-    private fun countMatchWinningNumbers(winningNumbers: List<Int>): Int {
-        lotto.validateLottoNumbersCount(winningNumbers.map { LottoNumber(it) })
+    private fun countMatchWinningNumbers(winningNumbers: Set<Int>): Int {
+        lotto.validateLottoNumbersCount(winningNumbers.map { LottoNumber(it) }.toSet())
 
         return lotto.numbers.count { existNumber -> winningNumbers.contains(existNumber.number) }
+    }
+
+    private fun validateWinningNumbersBonusNumberDuplicate(
+        winningNumbers: Set<Int>,
+        bonusNumber: Int,
+    ) {
+        require(!winningNumbers.contains(bonusNumber)) {
+            "[ERROR] 당첨 번호와 보너스 번호는 중복될 수 없습니다."
+        }
     }
 
     private fun isHaveBonusNumber(bonusNumber: Int): Boolean = lotto.numbers.map { it.number }.contains(bonusNumber)
