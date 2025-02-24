868dcb55c003bff2029855870b51018efbb12255
yrsel <xxknu88@gmail.com>
2025-02-24 21:08:18 +0900

    refactor: 요구사항 변경에 따른 코드 제거
    금액을 로또 하나의 금액으로 나눈 나머지가 0인지 검증하는 로직 제거

--- src/main/kotlin/lotto/model/LottoPurchaseAmount.kt
+++ src/main/kotlin/lotto/model/LottoPurchaseAmount.kt
@@ -5,7 +5,6 @@ class LottoPurchaseAmount(
 ) {
     init {
         require(money in MIN_LOTTO_PURCHASE_AMOUNT..MAX_LOTTO_PURCHASE_AMOUNT) { ERROR_OUT_OF_RANGE_LOTTO_PURCHASE_AMOUNT }
-        require(money % LOTTO_PRICE == NO_REMAINDER) { ERROR_EXIST_REMAINDER }
     }
 
     fun getLottoCount(lottoPrice: Int = LOTTO_PRICE): Int = money / lottoPrice
@@ -13,9 +12,7 @@ class LottoPurchaseAmount(
     companion object {
         private const val MIN_LOTTO_PURCHASE_AMOUNT = 1_000
         private const val MAX_LOTTO_PURCHASE_AMOUNT = 100_000
-        private const val NO_REMAINDER = 0
         private const val LOTTO_PRICE = 1_000
         private const val ERROR_OUT_OF_RANGE_LOTTO_PURCHASE_AMOUNT = "구입 금액은 최소 1,000원 이상 최대 100,000원 이하 입니다."
-        private const val ERROR_EXIST_REMAINDER = "구입 금액은 1,000원 단위입니다."
     }
 }
--- src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
+++ src/test/kotlin/lotto/LottoPurchaseAmountTest.kt
@@ -18,14 +18,6 @@ class LottoPurchaseAmountTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [1_100, 10_100])
-    fun `로또 구입 금액은 1,000원 단위가 아닌 경우 예외를 발생한다`(money: Int) {
-        assertThatIllegalArgumentException()
-            .isThrownBy { LottoPurchaseAmount(money) }
-            .withMessage("구입 금액은 1,000원 단위입니다.")
-    }
-
-    @ParameterizedTest
     @ValueSource(ints = [1_000, 14_000, 100_000, 99_000, 21_000])
     fun `구입 금액은 최소 1,000원 이상 최대 100,000원 이하이면서 1,000원 단위일 경우 예외를 발생하지 않는다`(money: Int) {
         Assertions.assertDoesNotThrow {
