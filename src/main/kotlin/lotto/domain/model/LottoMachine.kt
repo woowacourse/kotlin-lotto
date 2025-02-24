997bc1a4f2d8d032a8d261fec332c68b278ecce5
moondev03 <moondev03@gmail.com>
2025-02-24 14:27:48 +0900

    chore: LottoMachine의 로또 가격 상수화

--- src/main/kotlin/lotto/domain/model/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/model/LottoMachine.kt
@@ -4,11 +4,15 @@ import lotto.domain.service.LottoNumbersGenerator
 import lotto.domain.service.RandomLottoNumbersGenerator
 
 class LottoMachine(
-    val lottoPrice: Int = 1000,
+    val lottoPrice: Int = LOTTO_PRICE,
     private val generator: LottoNumbersGenerator = RandomLottoNumbersGenerator(),
 ) {
     fun generateLottoBundle(purchaseAmount: Int): LottoBundle {
         val count = purchaseAmount / lottoPrice
         return LottoBundle(List(count) { Lotto(generator.generate()) })
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
     }
 }
