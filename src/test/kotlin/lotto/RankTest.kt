d9655568c16b847bf58e145857545963fa48a3d5
m6z1 <sonmyungj1zz@gmail.com>
2025-02-24 17:43:24 +0900

    test(RankTest): csvSource 사용해 테스트 수정

--- src/test/kotlin/lotto/RankTest.kt
+++ src/test/kotlin/lotto/RankTest.kt
@@ -3,32 +3,26 @@ package lotto
 import lotto.model.Rank
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-import java.util.stream.Stream
+import org.junit.jupiter.params.provider.CsvSource
 
 class RankTest {
     @ParameterizedTest
-    @MethodSource("getSource")
+    @CsvSource(
+        value = [
+            "6,false,FIRST",
+            "5,true,SECOND",
+            "5,false,THIRD",
+            "4,false,FOURTH",
+            "3,false,FIFTH",
+            "2,false,MISS",
+            "1,false,MISS",
+        ],
+    )
     fun `맞춘 개수와 보너스 일치 여부에 따라 등수를 판단한다`(
         matchCount: Int,
         isMatchedBonus: Boolean,
         expected: Rank,
     ) {
-        assertThat(Rank.findRank(matchCount, isMatchedBonus))
-            .isEqualTo(expected)
-    }
-
-    companion object {
-        @JvmStatic
-        fun getSource(): Stream<Arguments> =
-            Stream.of(
-                Arguments.arguments(6, false, Rank.FIRST),
-                Arguments.arguments(5, true, Rank.SECOND),
-                Arguments.arguments(5, false, Rank.THIRD),
-                Arguments.arguments(4, false, Rank.FOURTH),
-                Arguments.arguments(3, false, Rank.FIFTH),
-                Arguments.arguments(1, false, Rank.MISS),
-            )
+        assertThat(Rank.findRank(matchCount, isMatchedBonus)).isEqualTo(expected)
     }
 }
