3d71f02312d61a3acb1ae939258925d4107b680d
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-02-25 14:26:42 +0900

    refactor: InputView에서 검증되어 들어오는 부분 테스트 삭제

--- src/test/kotlin/lotto/LottoTicketCounterTest.kt
+++ src/test/kotlin/lotto/LottoTicketCounterTest.kt
@@ -4,34 +4,16 @@ import lotto.model.LottoTicketCounter
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.EmptySource
 
 class LottoTicketCounterTest {
     @Test
     fun `구입 금액에 따른 로또 갯수를 계산한다`() {
-        val calculator = LottoTicketCounter("14000")
+        val calculator = LottoTicketCounter(14000.0)
         assertThat(calculator.count()).isEqualTo(14)
-    }
-
-    @EmptySource
-    @ParameterizedTest
-    fun `구입 금액이 공백이면 예외가 발생한다`(input: String) {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter(input) }
-    }
-
-    @Test
-    fun `구입 금액은 숫자가 아니면 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("1000s") }
     }
 
     @Test
     fun `구입 금액이 1000원 미만인 경우 예외가 발생한다`() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("800") }
-    }
-
-    @Test
-    fun `구입 금액은 1000으로 나누어 떨어져야 한다`() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("1300") }
+        assertThrows<IllegalArgumentException> { LottoTicketCounter(800.0) }
     }
 }
