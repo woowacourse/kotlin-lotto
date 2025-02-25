2b3997094c0825d40fb58f9b3a7a4d53240a5d58
yrsel <xxknu88@gmail.com>
2025-02-25 15:14:28 +0900

    feat: 구매 수량 생성자 검증 로직 구현

--- src/main/kotlin/lotto/model/LottoCount.kt
+++ src/main/kotlin/lotto/model/LottoCount.kt
@@ -3,6 +3,10 @@ package lotto.model
 class LottoCount(
     val number: Int,
 ) {
+    init {
+        require(number >= 0) { ERROR_NEGATIVE_COUNT }
+    }
+
     fun isAvailPurchaseLottoCount(lottoCount: LottoCount): Boolean = this.number >= lottoCount.number
 
     fun minus(lottoCount: LottoCount): LottoCount {
@@ -12,5 +16,6 @@ class LottoCount(
 
     companion object {
         private const val ERROR_EXCEED_LOTTO_COUNT = "구매 가능한 수량을 초과하였습니다."
+        private const val ERROR_NEGATIVE_COUNT = "수량은 음수가 될 수 없습니다."
     }
 }
--- src/test/kotlin/lotto/LottoCountTest.kt
+++ src/test/kotlin/lotto/LottoCountTest.kt
@@ -9,6 +9,12 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
 class LottoCountTest {
+    @Test
+    fun `로또 수량은 0보다 크거나 같아야 한다`() {
+        assertThrows<java.lang.IllegalArgumentException> { LottoCount(-1) }
+        assertDoesNotThrow { LottoCount(0) }
+    }
+
     @ParameterizedTest(name = "현재 수량이 {0}이고 구매할 수량이 {1}이라면 구매 가능 여부는 {2}를 반환한다.")
     @CsvSource(value = ["3,0,true", "3,2,true", "3,3,true", "3,4,false"])
     fun `현재 로또 수량에서 구매 가능한 수량인지 판단한다`(
