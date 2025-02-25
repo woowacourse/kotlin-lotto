704071856df40f1776d5ea13b23b729d6e508e43
eden <devfeijoa@gmail.com>
2025-02-25 19:25:23 +0900

    refactor: 어떤 에러 메시지가 나왔는지 검증

--- src/main/kotlin/lotto/model/LottoTicketCounter.kt
+++ src/main/kotlin/lotto/model/LottoTicketCounter.kt
@@ -4,14 +4,18 @@ class LottoTicketCounter(purchase: String) {
     val ticketCount: Int = purchase.toInt() / PURCHASE_STANDARD
 
     init {
-        require(purchase.isNotBlank()) { "구입 금액은 공백일 수 없습니다." }
-        require(purchase.toIntOrNull() != null) { "구입 금액은 숫자만 입력할 수 있습니다." }
-        require(purchase.toInt() >= PURCHASE_STANDARD) { "구입 금액은 최소 1000원 이상 이어야 합니다." }
-        require(purchase.toInt() % PURCHASE_STANDARD == REMAINDER) { "구입 금액은 1000원 단위로 나누어 떨어져야 합니다." }
+        require(purchase.isNotBlank()) { INVALID_BLANK_MESSAGE }
+        require(purchase.toIntOrNull() != null) { NUMERIC_PURCHASE_MESSAGE }
+        require(purchase.toInt() >= PURCHASE_STANDARD) { MINIMUM_PURCHASE_MESSAGE }
+        require(purchase.toInt() % PURCHASE_STANDARD == REMAINDER) { PURCHASE_UNIT_MESSAGE }
     }
 
     companion object {
         private const val PURCHASE_STANDARD = 1000
         private const val REMAINDER = 0
+        const val INVALID_BLANK_MESSAGE = "구입 금액은 공백일 수 없습니다."
+        const val NUMERIC_PURCHASE_MESSAGE = "구입 금액은 숫자만 입력할 수 있습니다."
+        const val MINIMUM_PURCHASE_MESSAGE = "구입 금액은 최소 1000원 이상 이어야 합니다."
+        const val PURCHASE_UNIT_MESSAGE = "구입 금액은 1000원 단위로 나누어 떨어져야 합니다."
     }
 }
--- src/test/kotlin/lotto/LottoTicketCounterTest.kt
+++ src/test/kotlin/lotto/LottoTicketCounterTest.kt
@@ -1,6 +1,10 @@
 package lotto
 
 import lotto.model.LottoTicketCounter
+import lotto.model.LottoTicketCounter.Companion.INVALID_BLANK_MESSAGE
+import lotto.model.LottoTicketCounter.Companion.MINIMUM_PURCHASE_MESSAGE
+import lotto.model.LottoTicketCounter.Companion.NUMERIC_PURCHASE_MESSAGE
+import lotto.model.LottoTicketCounter.Companion.PURCHASE_UNIT_MESSAGE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,27 +15,31 @@ class LottoTicketCounterTest {
     @Test
     fun `구입 금액에 따른 로또 갯수를 계산한다 `() {
         val calculator = LottoTicketCounter("14000")
-        assertThat(calculator.count()).isEqualTo(14)
+        assertThat(calculator.ticketCount).isEqualTo(14)
     }
 
     @EmptySource
     @ParameterizedTest
     fun `구입 금액이 공백이면 예외가 발생한다 `(input: String) {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter(input) }
+        val exception = assertThrows<IllegalArgumentException> { LottoTicketCounter(input) }
+        assertThat(exception).hasMessage(INVALID_BLANK_MESSAGE)
     }
 
     @Test
     fun `구입 금액은 숫자가 아니면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("1000s") }
+        val exception = assertThrows<IllegalArgumentException> { LottoTicketCounter("1000s") }
+        assertThat(exception).hasMessage(NUMERIC_PURCHASE_MESSAGE)
     }
 
     @Test
     fun `구입 금액이 1000원 미만인 경우 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("800") }
+        val exception = assertThrows<IllegalArgumentException> { LottoTicketCounter("800") }
+        assertThat(exception).hasMessage(MINIMUM_PURCHASE_MESSAGE)
     }
 
     @Test
     fun `구입 금액은 1000으로 나누어 떨어져야 한다 `() {
-        assertThrows<IllegalArgumentException> { LottoTicketCounter("1300") }
+        val exception = assertThrows<IllegalArgumentException> { LottoTicketCounter("1300") }
+        assertThat(exception).hasMessage(PURCHASE_UNIT_MESSAGE)
     }
 }
