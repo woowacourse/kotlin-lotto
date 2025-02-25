d2d66d486611b5caea958b056f504cae9550f3df
eden <devfeijoa@gmail.com>
2025-02-25 21:28:30 +0900

    refactor: 검증부 하드코딩하여 테스트코드 변경

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -4,6 +4,7 @@ import lotto.model.Lotto
 import lotto.model.Lotto.Companion.LOTTO_BOUND_MESSAGE
 import lotto.model.Lotto.Companion.LOTTO_COUNT_MESSAGE
 import lotto.model.Lotto.Companion.LOTTO_DISTINCT_MESSAGE
+import lotto.model.LottoNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -11,25 +12,40 @@ import org.junit.jupiter.api.assertThrows
 class LottoTest {
     @Test
     fun `발급하는 로또 번호는 6개여야 한다 `() {
-        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
-        assertThat(lotto.numbers.size).isEqualTo(6)
+        val lotto = Lotto(List(6) { LottoNumber(it + 1) })
+        assertThat(lotto.getNumbers()).hasSize(6)
     }
 
     @Test
     fun `발급하는 로또 번호는 6개가 아니면 예외가 발생한다 `() {
-        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 6, 7)) }
+        val exception = assertThrows<IllegalArgumentException> { Lotto(List(7) { LottoNumber(it + 1) }) }
         assertThat(exception).hasMessage(LOTTO_COUNT_MESSAGE)
     }
 
     @Test
     fun `발급하는 로또 번호는 1부터 45 범위 내에 있어야 한다 `() {
-        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 5, 46)) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                Lotto(
+                    listOf(
+                        LottoNumber(1),
+                        LottoNumber(2),
+                        LottoNumber(3),
+                        LottoNumber(4),
+                        LottoNumber(5),
+                        LottoNumber(46),
+                    ),
+                )
+            }
         assertThat(exception).hasMessage(LOTTO_BOUND_MESSAGE)
     }
 
     @Test
     fun `로또 번호는 중복되지 않아야 한다 `() {
-        val exception = assertThrows<IllegalArgumentException> { Lotto(listOf(1, 2, 3, 4, 6, 6)) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                Lotto(listOf(LottoNumber(1), LottoNumber(2), LottoNumber(3), LottoNumber(4), LottoNumber(5), LottoNumber(5)))
+            }
         assertThat(exception).hasMessage(LOTTO_DISTINCT_MESSAGE)
     }
 }
--- src/test/kotlin/lotto/WinningLottoTest.kt
+++ src/test/kotlin/lotto/WinningLottoTest.kt
@@ -1,33 +1,70 @@
 package lotto
 
 import lotto.model.Lotto
+import lotto.model.LottoNumber
 import lotto.model.WinningLotto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class WinningLottoTest {
     @Test
     fun `당첨번호가 중복되면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(listOf(1, 2, 3, 4, 6, 6)), 7) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(
+                    Lotto(listOf(1, 2, 3, 4, 6, 6).map { LottoNumber(it) }),
+                    LottoNumber(7),
+                )
+            }
+        assertThat(exception).hasMessage("로또 번호가 중복될 수 없습니다.")
     }
 
     @Test
     fun `당첨번호가 6개가 아니면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6, 7)), 8) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(
+                    Lotto(listOf(1, 2, 3, 4, 5, 6, 7).map { LottoNumber(it) }),
+                    LottoNumber(8),
+                )
+            }
+        assertThat(exception).hasMessage("로또 번호는 6개여야 합니다.")
     }
 
     @Test
     fun `당첨 번호는 1부터 45 범위 내에 있지 않으면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 46)), 6) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(
+                    Lotto(listOf(1, 2, 3, 4, 5, 46).map { LottoNumber(it) }),
+                    LottoNumber(6),
+                )
+            }
+        assertThat(exception).hasMessage("로또 번호는 1에서 45 범위 내에서 있어야 합니다.")
     }
 
     @Test
     fun `당첨 번호는 보너스 번호와 중복되면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), 1) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(
+                    Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }),
+                    LottoNumber(1),
+                )
+            }
+        assertThat(exception).hasMessage("당첨 번호와 보너스 번호는 중복될 수 없습니다.")
     }
 
     @Test
     fun `보너스 번호는 1부터 45 범위 내에 있지 않으면 예외가 발생한다 `() {
-        assertThrows<IllegalArgumentException> { WinningLotto(Lotto(listOf(1, 2, 3, 4, 5, 6)), 46) }
+        val exception =
+            assertThrows<IllegalArgumentException> {
+                WinningLotto(
+                    Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber(it) }),
+                    LottoNumber(46),
+                )
+            }
+        assertThat(exception).hasMessage("로또 번호는 1에서 45 범위 내에서 있어야 합니다.")
     }
 }
