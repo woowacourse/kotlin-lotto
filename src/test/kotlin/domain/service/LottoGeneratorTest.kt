2ec3a63cfe5175148b962da1be95d7e003e02860
jerry8282 <ka06196@naver.com>
2025-02-25 15:49:08 +0900

    test : 사용자가 수동으로 번호를 입력함에 따른 테스트 추가

--- src/test/kotlin/domain/service/LottoGeneratorTest.kt
+++ src/test/kotlin/domain/service/LottoGeneratorTest.kt
@@ -1,13 +1,34 @@
 package domain.service
 
+import domain.model.Lotto
 import domain.model.PurchasePrice
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import util.LottoFactory
 
 class LottoGeneratorTest {
+    private lateinit var manualLottoNumbers: List<Lotto>
+
+    @BeforeEach
+    fun setup()  {
+        manualLottoNumbers =
+            listOf(
+                LottoFactory.lottoOf(1, 3, 4, 5, 6, 7),
+                LottoFactory.lottoOf(1, 2, 3, 4, 5, 6),
+            )
+    }
+
     @Test
-    fun `구입 금액이 2천원이면 로또를 2개 발행한다`() {
-        val money = PurchasePrice(2000)
-        assertThat(LottoGenerator(money).makeLotto().size).isEqualTo(2)
+    fun `구입 금액이 3천원이고 수동 로또가 2개면 로또를 3개 발행한다`() {
+        val money = PurchasePrice(3000)
+        assertThat(LottoGenerator(money).makeLotto(2, manualLottoNumbers).size).isEqualTo(3)
+    }
+
+    @Test
+    fun `구입 금액이 3천원이고 수동 로또가 2개면 해당 로또에 수동 로또 번호가 포함된다`() {
+        val money = PurchasePrice(3000)
+        assertTrue(LottoGenerator(money).makeLotto(2, manualLottoNumbers).containsAll(manualLottoNumbers))
     }
 }
