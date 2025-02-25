3e20ee942f554672d61fea3d9f4300d91b9a6439
DoableTuple <doabletuple@gmail.com>
2025-02-25 14:57:41 +0900

    feat: 수동으로 구매할 로또의 개수는 0 미만일 수 없다

--- src/main/kotlin/lotto/domain/LottoGenerator.kt
+++ src/main/kotlin/lotto/domain/LottoGenerator.kt
@@ -2,10 +2,12 @@ package lotto.domain
 
 class LottoGenerator(val totalQuantity: Int, val manualQuantity: Int) {
     init {
+        require(manualQuantity >= 0) { ERROR_MESSAGE_NOT_ENOUGH_MANUAL_LOTTOS }
         require(totalQuantity >= manualQuantity) { ERROR_MESSAGE_TOO_MANY_MANUAL_LOTTOS }
     }
 
     companion object {
+        private const val ERROR_MESSAGE_NOT_ENOUGH_MANUAL_LOTTOS = "수동으로 구매할 로또의 개수는 0보다 작을 수 없습니다."
         private const val ERROR_MESSAGE_TOO_MANY_MANUAL_LOTTOS = "수동으로 구매할 로또의 개수는 로또의 총 개수를 초과할 수 없습니다."
     }
 }
--- src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/LottoGeneratorTest.kt
@@ -8,4 +8,9 @@ class LottoGeneratorTest {
     fun `수동으로 구매할 로또의 총 금액은 구입 금액을 초과할 수 없다`() {
         assertThrows<IllegalArgumentException> { LottoGenerator(1, 2) }
     }
+
+    @Test
+    fun `수동으로 구매할 로또의 개수는 0 미만일 수 없다`() {
+        assertThrows<IllegalArgumentException> { LottoGenerator(1, -1) }
+    }
 }
