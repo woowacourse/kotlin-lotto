3ddffb862f42fcb8999d31d6ed7ba7d4a7ac403d
hwannow <vnfdlvgid061@naver.com>
2025-02-25 17:31:12 +0900

    refactor: ktlint 포맷 적용

--- src/main/kotlin/lotto/domain/OrderSheet.kt
+++ src/main/kotlin/lotto/domain/OrderSheet.kt
@@ -1,5 +1,9 @@
 package lotto.domain
 
-class OrderSheet(val purchaseAmount: LottoPurchaseAmount, val manualLottoCount: ManualLottoCount, val manualLottoNumber: List<Lotto>) {
+class OrderSheet(
+    val purchaseAmount: LottoPurchaseAmount,
+    val manualLottoCount: ManualLottoCount,
+    val manualLottoNumber: List<Lotto>,
+) {
     val autoCount: Int = purchaseAmount.purchasableCount - manualLottoCount.lottoCount
 }
