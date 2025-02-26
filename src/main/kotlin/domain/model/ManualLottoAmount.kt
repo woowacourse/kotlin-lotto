de9c1e202b87def262882d12c23c9c7dad3180ad
chanho0908 <uni070@naver.com>
2025-02-26 18:12:17 +0900

    feat : 수동 로또 개수 클래스 네이밍 수정, 부생성자 추가

---
+++ src/main/kotlin/domain/model/ManualLottoAmount.kt
@@ -0,0 +1,16 @@
+package domain.model
+
+@JvmInline
+value class ManualLottoAmount private constructor(val value: Int) {
+    constructor(
+        lottoAmount: Int,
+        money: PurchasePrice,
+    ) : this(lottoAmount) {
+        val purchasableAmount = money.getPurchasableLottoCount()
+        require(lottoAmount <= purchasableAmount) { INVALID_PASSIVITY_LOTTO_SIZE }
+    }
+
+    companion object {
+        private const val INVALID_PASSIVITY_LOTTO_SIZE = "수동 구매 개수가 구입 가능한 로또 개수를 초과했습니다."
+    }
+}
