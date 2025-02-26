94ef143535d7026afaa2fdc69448d9ed7c336c48
chanho0908 <uni070@naver.com>
2025-02-26 21:09:37 +0900

    feat : 구매 금액 에러 상태 추가

--- src/main/kotlin/domain/model/price/PurchasePriceException.kt
+++ src/main/kotlin/domain/model/price/PurchasePriceException.kt
@@ -5,10 +5,10 @@ import util.ErrorConstants.ERROR
 
 sealed class PurchasePriceException(override val message: String) : Throwable(message) {
     data class OverStandardPriceException(
-        private val standardPrice: Int = STANDARD_AMOUNT_UNIT
+        private val standardPrice: Int = STANDARD_AMOUNT_UNIT,
     ) : PurchasePriceException("$ERROR ${standardPrice}원 이상 입력해주세요.")
 
     data class InvalidUnitException(
-        private val standardPrice: Int = STANDARD_AMOUNT_UNIT
+        private val standardPrice: Int = STANDARD_AMOUNT_UNIT,
     ) : PurchasePriceException("$ERROR ${standardPrice}원 단위로 입력해주세요.")
 }
