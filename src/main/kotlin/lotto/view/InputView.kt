b7627a8aba630239aafba8a8364b22bdec16caa0
박지민 <pjm5489@naver.com>
2025-02-26 13:50:32 +0900

    refactor: 입력 값 검사 로직 위치 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,33 +1,45 @@
 package lotto.view
 
 class InputView {
-    fun readPurchaseAmount(): String {
+    fun readPurchaseAmount(): Int {
         println("구입금액을 입력해 주세요.")
-        return readln()
+        val purchaseAmount = readln().toIntOrNull()
+        return purchaseAmount.validateIsNumber()
     }
 
-    fun readWinningNumbers(): String {
+    fun readWinningNumbers(): Set<Int> {
         println("\n지난 주 당첨 번호를 입력해 주세요.")
-        return readln()
+        val rawWinningNumbers = readln().split(", ")
+        val winningNumbers = rawWinningNumbers.map { it.toIntOrNull() }
+        return winningNumbers.validateWinningNumbers()
     }
 
-    fun readPassivePurchaseQuantity(): String {
+    fun readPassivePurchaseQuantity(): Int {
         println("수동으로 구매할 로또 수를 입력해 주세요.")
-        return readln()
+        return readln().toIntOrNull().validateIsNumber()
     }
 
-    fun readPassiveLottoNumbers(passivityQuantity: Int): List<String> {
-        val passiveLottoNumbers = mutableListOf<String>()
+    fun readPassiveLottoNumbers(passivityQuantity: Int): List<Set<Int>> {
+        val passiveLottoNumbers = mutableListOf<Set<Int>>()
 
         println("수동으로 구매할 번호를 입력해 주세요.")
         repeat(passivityQuantity) {
-            passiveLottoNumbers.add(readln())
+            val rawPassiveWinningNumbers = readln().split(", ")
+            val mapToPassiveLottoNumbers = rawPassiveWinningNumbers.map { it.toIntOrNull() }
+            passiveLottoNumbers.add(mapToPassiveLottoNumbers.validateWinningNumbers())
         }
         return passiveLottoNumbers
     }
 
-    fun readBonusNumber(): String {
+    fun readBonusNumber(): Int {
         println("보너스 볼을 입력해 주세요.")
-        return readln()
+        return readln().toIntOrNull().validateIsNumber()
+    }
+
+    private fun List<Int?>.validateWinningNumbers(): Set<Int> = this.map { it.validateIsNumber() }.toSet()
+
+    private fun Int?.validateIsNumber(): Int {
+        if (this == null) throw IllegalArgumentException("[ERROR] 숫자를 입력해주세요.")
+        return this
     }
 }
