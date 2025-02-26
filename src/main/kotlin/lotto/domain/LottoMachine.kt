5754193bffc2d0da9633b834308a87c79628ea2f
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-26 20:12:08 +0900

    chore: rename function

--- src/main/kotlin/lotto/domain/LottoMachine.kt
+++ src/main/kotlin/lotto/domain/LottoMachine.kt
@@ -1,7 +1,7 @@
 package lotto.domain
 
 class LottoMachine {
-    fun getLotto(generator: LottoGenerator): List<Lotto> {
+    fun makeLotto(generator: LottoGenerator): List<Lotto> {
         val lottos: List<Lotto> = generator.generate()
         return lottos
     }
