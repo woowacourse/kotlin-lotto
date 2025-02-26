2a2d1ab9db17deda94243d487ce5e165052b31f0
ijh1298 <imjh0417@gmail.com>
2025-02-26 17:53:15 +0900

    test : ManualLottoGeneratorTest 로또 한 장 생성 테스트 수정, 여러 장 생성 테스트 추가

--- src/test/kotlin/domain/service/ManualLottoGeneratorTest.kt
+++ src/test/kotlin/domain/service/ManualLottoGeneratorTest.kt
@@ -6,13 +6,31 @@ import org.junit.jupiter.api.Test
 
 class ManualLottoGeneratorTest {
     @Test
-    fun `수동으로 로또가 생성된다`() {
-        val manualLottoNumbers = setOf(1, 2, 3, 4, 5, 6)
-        val lottoGenerator = ManualLottoGenerator()
+    fun `수동으로 로또 한 장 생성할 수 있다`() {
+        val lottoGenerator =
+            ManualLottoGenerator(
+                listOf("1,2,3,4,5,6"),
+            )
+        val lotto: List<Lotto> = lottoGenerator.makeLotto(1)
 
-        lottoGenerator.setManualLottoNumbers(manualLottoNumbers)
-        val lotto: Lotto = lottoGenerator.makeLotto()
+        val expected: Lotto = Lotto.of(1, 2, 3, 4, 5, 6)
 
-        assertThat(lotto).isEqualTo(Lotto.of(1, 2, 3, 4, 5, 6))
+        assertThat(lotto.first()).isEqualTo(expected)
+    }
+
+    @Test
+    fun `수동으로 로또 여러 장 생성할 수 있다`() {
+        val lottoInput =
+            listOf(
+                "1,2,3,4,5,6",
+                "10,20,30,40,41,42",
+                "3,1,6,9,2,7",
+                "11,22,33,14,35,26",
+            )
+        val lottoGenerator = ManualLottoGenerator(lottoInput)
+
+        val lotto: List<Lotto> = lottoGenerator.makeLotto(lottoInput.size)
+
+        assertThat(lotto.size).isEqualTo(lottoInput.size)
     }
 }
