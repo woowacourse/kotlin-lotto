4043adfb03d0ebde856839f57fe57d4a99ceb8a8
박지민 <pjm5489@naver.com>
2025-02-26 19:45:27 +0900

    refactor: 1000원 이상의 금액만 입력하면 실행 가능하게 변경

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -11,18 +11,11 @@ class LottoMachine(
 ) {
     init {
         validateAmountMinimumRange()
-        validateAmountUnit()
     }
 
     private fun validateAmountMinimumRange() {
         require(amount > LOTTO_MIN_AMOUNT) {
             "[ERROR] ${LOTTO_MIN_AMOUNT}원 이상의 금액으로 입력해 주세요. 입력값: $amount"
-        }
-    }
-
-    private fun validateAmountUnit() {
-        require(amount % LOTTO_EACH_AMOUNT == 0) {
-            "[ERROR] ${LOTTO_EACH_AMOUNT}원 단위의 금액으로 입력해 주세요. 입력값: $amount"
         }
     }
 
