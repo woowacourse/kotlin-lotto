584adfac231cc0a89e278881ef178a126dfbe14f
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-02-27 00:58:55 +0900

    refactor: change objects to class

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -1,11 +1,6 @@
 package lotto.view
 
-object InputView {
-    private const val LOTTO_NUMBERS_DELIMITER = ","
-    private const val ERROR_MESSAGE_INVALID_INPUT_STATE = "정상적으로 입력되지 않았습니다."
-    private const val ERROR_MESSAGE_INPUT_NOT_A_NUMBER = "숫자를 입력해주세요."
-    private const val ERROR_MESSAGE_INVALID_LOTTO_NUMBERS = "양식에 맞게 입력해주세요. 예시: 1, 2, 3, 4, 5, 6"
-
+class InputView {
     fun readNumber(): Int {
         val input: String = readLine() ?: throw IllegalArgumentException(ERROR_MESSAGE_INVALID_INPUT_STATE)
         val number: Int =
@@ -27,5 +22,12 @@ object InputView {
 
         val lottoNumbers: List<List<Int>> = (1..size).map { readNumbers() }
         return lottoNumbers
+    }
+
+    companion object {
+        private const val LOTTO_NUMBERS_DELIMITER = ","
+        private const val ERROR_MESSAGE_INVALID_INPUT_STATE = "정상적으로 입력되지 않았습니다."
+        private const val ERROR_MESSAGE_INPUT_NOT_A_NUMBER = "숫자를 입력해주세요."
+        private const val ERROR_MESSAGE_INVALID_LOTTO_NUMBERS = "양식에 맞게 입력해주세요. 예시: 1, 2, 3, 4, 5, 6"
     }
 }
--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -2,22 +2,7 @@ package lotto.view
 
 import kotlin.math.floor
 
-object OutputView {
-    private const val MESSAGE_ENTER_PRICE = "구입금액을 입력해 주세요."
-    private const val MESSAGE_ENTER_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
-    private const val MESSAGE_ENTER_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
-    private const val MESSAGE_RESULT_HEADER = "당첨 통계\n---------"
-    private const val MESSAGE_BOUGHT = "개를 구매했습니다."
-    private const val MESSAGE_PROFIT_RATE_LOSE = "손해라는"
-    private const val MESSAGE_PROFIT_RATE_DRAW = "본전이라는"
-    private const val MESSAGE_PROFIT_RATE_WIN = "이득이라는"
-    private const val MESSAGE_ENTER_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
-    private const val MESSAGE_ENTER_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요."
-    private const val RESULT_DESCRIPTION_TEMPLATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %S 의미임)"
-    private const val SHOW_LOTTO_SEPARATOR = "\n"
-    private const val SHOW_LOTTO_RESULT_SEPARATOR = "\n"
-    private const val PROFIT_RATE_DRAW = 1.0
-
+class OutputView {
     fun requestPrice() {
         println(MESSAGE_ENTER_PRICE)
     }
@@ -67,5 +52,22 @@ object OutputView {
 
     fun requestManualLottosNumbers() {
         println(MESSAGE_ENTER_MANUAL_LOTTO)
+    }
+
+    companion object {
+        private const val MESSAGE_ENTER_PRICE = "구입금액을 입력해 주세요."
+        private const val MESSAGE_ENTER_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
+        private const val MESSAGE_ENTER_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
+        private const val MESSAGE_RESULT_HEADER = "당첨 통계\n---------"
+        private const val MESSAGE_BOUGHT_LOTTO_COUNT = "수동으로 %d장, 자동으로 %d개를 구매했습니다.\n"
+        private const val MESSAGE_PROFIT_RATE_LOSE = "손해라는"
+        private const val MESSAGE_PROFIT_RATE_DRAW = "본전이라는"
+        private const val MESSAGE_PROFIT_RATE_WIN = "이득이라는"
+        private const val MESSAGE_ENTER_MANUAL_LOTTO_COUNT = "수동으로 구매할 로또 수를 입력해 주세요."
+        private const val MESSAGE_ENTER_MANUAL_LOTTO = "수동으로 구매할 번호를 입력해 주세요."
+        private const val RESULT_DESCRIPTION_TEMPLATE = "총 수익률은 %.2f입니다.(기준이 1이기 때문에 결과적으로 %S 의미임)"
+        private const val SHOW_LOTTO_SEPARATOR = "\n"
+        private const val SHOW_LOTTO_RESULT_SEPARATOR = "\n"
+        private const val PROFIT_RATE_DRAW = 1.0
     }
 }
