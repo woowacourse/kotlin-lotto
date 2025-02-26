32fafafeac7817fb4fdaffc3a06eb7193dc63952
chanho0908 <uni070@naver.com>
2025-02-26 18:09:54 +0900

    test : 수동 로또 함수 테스트 클래스 수정

--- src/test/kotlin/domain/model/ManualLottoAmountTest.kt
+++ src/test/kotlin/domain/model/ManualLottoAmountTest.kt
@@ -3,13 +3,13 @@ package domain.model
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class PassivityLottoAmountTest {
+class ManualLottoAmountTest {
     @Test
     fun `수동 로또 개수가 총 구입하려는 로또 개수보다 많으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException>(
             message = "수동 구매 개수가 구입 가능한 로또 개수를 초과했습니다.",
             executable = {
-                PassivityLottoAmount.create(3, PurchasePrice(2000))
+                ManualLottoAmount(3, PurchasePrice(2000))
             },
         )
     }
