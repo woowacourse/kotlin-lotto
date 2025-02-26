8c307d35ac22d76e2c231e92fb94cab0dde26c88
cucumber99 <dlsguq1@gmail.com>
2025-02-26 19:50:17 +0900

    refactor: 로또 객체 리팩터링
    랜덤 숫자 생성 기능을 다른 곳으로 위임합니다

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -17,18 +17,8 @@ class Lotto private constructor(val numbers: List<LottoNumber>) {
 
     companion object {
         const val NUMBER_COUNT = 6
-        private val LOTTO_NUMBERS = (LottoNumber.MIN_VALUE..LottoNumber.MAX_VALUE)
         private const val COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
         private const val DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다."
-
-        fun auto(): Lotto {
-            return Lotto(
-                LOTTO_NUMBERS.shuffled()
-                    .take(NUMBER_COUNT)
-                    .sorted()
-                    .map(::LottoNumber),
-            )
-        }
 
         fun from(input: List<Int>): Lotto {
             return Lotto(input.map(::LottoNumber))
