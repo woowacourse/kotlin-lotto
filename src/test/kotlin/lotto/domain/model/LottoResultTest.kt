e2ef65ccfdc787e416c1e5185ee45996b8543497
moondev03 <moondev03@gmail.com>
2025-02-26 10:58:18 +0900

    refactor: 총 수익률 테스트 변경
    - MethodSource -> CsvSource
    - ParameterizedTest 활용해 테스트명 설정

--- src/test/kotlin/lotto/domain/model/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoResultTest.kt
@@ -2,34 +2,24 @@ package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class LottoResultTest {
-    @ParameterizedTest
-    @MethodSource("calculateTotalReturnTest")
+    @ParameterizedTest(name = "로또 등수가 [{0}]일 때, 수익률은 \"{1}\"이다")
+    @CsvSource(
+        "FIRST, 2000000.00",
+        "SECOND, 30000.00",
+        "THIRD, 1500.00",
+        "FOURTH FIFTH, 27.50",
+        "MISS, 0.00",
+        "THIRD FIFTH SECOND, 10501.66",
+    )
     fun `총 수익률을 계산할 수 있다`(
-        lottoRanks: List<LottoRank>,
+        lottoRanks: String,
         expectedReturn: Double,
     ) {
-        val totalReturn = LottoResult(lottoRanks).calculateTotalReturn(1000)
+        val ranks = lottoRanks.split(" ").map { LottoRank.valueOf(it) }
+        val totalReturn = LottoResult(ranks).calculateTotalReturn(1000)
         assertThat(totalReturn.toDouble()).isEqualTo(expectedReturn)
-    }
-
-    companion object {
-        @JvmStatic
-        fun calculateTotalReturnTest(): List<Arguments> {
-            return listOf(
-                Arguments.arguments(listOf(LottoRank.FIRST), 2_000_000.00),
-                Arguments.arguments(listOf(LottoRank.SECOND), 30_000.00),
-                Arguments.arguments(listOf(LottoRank.THIRD), 1_500.00),
-                Arguments.arguments(listOf(LottoRank.FOURTH, LottoRank.FIFTH), 27.50),
-                Arguments.arguments(listOf(LottoRank.MISS), 0.00),
-                Arguments.arguments(
-                    listOf(LottoRank.THIRD, LottoRank.FIFTH, LottoRank.SECOND),
-                    10_501.66,
-                ),
-            )
-        }
     }
 }
