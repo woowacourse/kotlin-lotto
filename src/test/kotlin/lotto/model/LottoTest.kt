4c6a1c5d1f8aaadca5554ffa49ef31a06840f483
cucumber99 <dlsguq1@gmail.com>
2025-02-26 15:12:19 +0900

    test: 로또 테스트 추가
    일치하는 숫자 개수를 반환하는지 테스트합니다
    특정 번호를 포함했는지 테스트합니다

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto.model
 
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -15,5 +16,21 @@ class LottoTest {
     fun `로또 번호는 중복되지 않는다`() {
         val exception = assertThrows<IllegalArgumentException> { Lotto.from(listOf(1, 1, 2, 3, 4, 5)) }
         assertThat(exception.message).isEqualTo("[ERROR] 로또 번호는 중복될 수 없습니다.")
+    }
+
+    @Test
+    fun `두 로또 사이에서 일치하는 숫자의 개수를 반환한다`() {
+        val lotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val winningLotto = Lotto.from(listOf(1, 2, 3, 4, 5, 7))
+
+        lotto.countMatchingNumber(winningLotto) shouldBe 5
+    }
+
+    @Test
+    fun `특정 번호의 포함 여부를 반환한다`() {
+        val lotto = Lotto.from(listOf(1, 2, 3, 4, 5, 6))
+        val bonusNumber = LottoNumber(1)
+
+        lotto.isContain(bonusNumber) shouldBe true
     }
 }
