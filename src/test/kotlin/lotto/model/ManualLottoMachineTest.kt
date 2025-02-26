8e5ddd14471daecf91246ffb2c92bf6363d6b27c
cucumber99 <dlsguq1@gmail.com>
2025-02-26 19:53:05 +0900

    feat: 로또 수동 생성 기능 테스트

---
+++ src/test/kotlin/lotto/model/ManualLottoMachineTest.kt
@@ -0,0 +1,19 @@
+package lotto.model
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ManualLottoMachineTest {
+    @Test
+    fun `사용자가 로또 번호를 입력하면 로또를 발행한다`() {
+        // Given
+        val lottoPurchaseInfo = LottoPurchaseInfo(2000, 2)
+        val manualLottoMachine = ManualLottoMachine(listOf(listOf(1, 2, 3, 4, 5, 6), listOf(7, 8, 9, 10, 11, 12)))
+
+        // When
+        val manualLottoTickets = manualLottoMachine.publishLottoTickets(lottoPurchaseInfo)
+
+        // Then
+        manualLottoTickets.size shouldBe 2
+    }
+}
