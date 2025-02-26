8ede43e1b446f283d30757792af99d1c4605c509
yrsel <xxknu88@gmail.com>
2025-02-26 14:01:40 +0900

    refactor: 중복되는 숫자 변환 로직을 함수로 치환

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,28 +7,36 @@ import lotto.model.LottoPurchaseAmount
 
 class InputView {
     fun readLottoPurchaseAmount(): LottoPurchaseAmount {
-        val money = readln().trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER)
+        val money = readln().toIntOrException()
         return LottoPurchaseAmount(money)
     }
 
     fun readManualLottoPurchaseCount(): LottoCount {
-        val count = readln().trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER)
+        val count = readln().toIntOrException()
         return LottoCount(count)
     }
+
+    fun readManualLotto(): List<LottoNumber> =
+        readln()
+            .split(LOTTO_NUMBER_DELIMITER)
+            .map { number -> number.toIntOrException() }
+            .map { number -> LottoNumber(number) }
 
     fun readWinningLottoNumbersOfLastWeek(): Lotto {
         val lottoNumbersOfLastWeek =
             readln()
                 .split(LOTTO_NUMBER_DELIMITER)
-                .map { number -> number.trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER) }
+                .map { number -> number.toIntOrException() }
                 .map { number -> LottoNumber(number) }
         return Lotto(lottoNumbersOfLastWeek)
     }
 
     fun readBonusNumber(): LottoNumber {
-        val bonusNumber = readln().trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER)
+        val bonusNumber = readln().toIntOrException()
         return LottoNumber(bonusNumber)
     }
+
+    private fun String.toIntOrException(): Int = this.trim().toIntOrNull() ?: throw IllegalArgumentException(ERROR_NOT_A_NUMBER)
 
     companion object {
         private const val ERROR_NOT_A_NUMBER = "숫자만 입력 가능합니다."
