99e4611295fa6805c25b766723b5b64c4b75cff5
cucumber99 <dlsguq1@gmail.com>
2025-02-26 15:14:20 +0900

    feat: 로또 팩토리 메소드 추가
    내부에서 랜덤한 숫자를 추출해 로또 객체를 생성합니다

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -11,14 +11,24 @@ class Lotto private constructor(val numbers: List<LottoNumber>) {
         }
     }
 
-    fun countMatchingNumber(lotto: Lotto): Int = this.numbers.count { number -> lotto.numbers.contains(number) }
+    fun countMatchingNumber(lotto: Lotto): Int = numbers.count { it in lotto.numbers }
 
-    fun isContain(lottoNumber: LottoNumber): Boolean = this.numbers.contains(lottoNumber)
+    fun isContain(lottoNumber: LottoNumber): Boolean = lottoNumber in numbers
 
     companion object {
         const val NUMBER_COUNT = 6
+        private val LOTTO_NUMBERS = (LottoNumber.MIN_VALUE..LottoNumber.MAX_VALUE)
         private const val COUNT_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다."
         private const val DUPLICATE_ERROR_MESSAGE = "[ERROR] 로또 번호는 중복될 수 없습니다."
+
+        fun auto(): Lotto {
+            return Lotto(
+                LOTTO_NUMBERS.shuffled()
+                    .take(NUMBER_COUNT)
+                    .sorted()
+                    .map(::LottoNumber),
+            )
+        }
 
         fun from(input: List<Int>): Lotto {
             return Lotto(input.map(::LottoNumber))
