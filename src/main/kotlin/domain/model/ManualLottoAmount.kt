c79f51499eed23099816b4ac778338bcd3970009
ijh1298 <imjh0417@gmail.com>
2025-02-27 17:14:41 +0900

    refactor : 수동 로또 개수 class 생성자 private으로 수정 및 팩토리 함수에서 논리적 오류를 sealed class로 반환하도록 수정

--- src/main/kotlin/domain/model/ManualLottoAmount.kt
+++ src/main/kotlin/domain/model/ManualLottoAmount.kt
@@ -1,6 +1,16 @@
 package domain.model
 
-class ManualLottoAmount(
+sealed class ManualLottoAmountResult {
+    data class Success(
+        val amount: ManualLottoAmount,
+    ) : ManualLottoAmountResult()
+
+    data class CannotMoreThanTotalPurchaseAmount(
+        val errorMsg: String,
+    ) : ManualLottoAmountResult()
+}
+
+class ManualLottoAmount private constructor(
     val value: Int,
     totalPurchaseAmount: Int,
 ) {
@@ -9,6 +19,13 @@ class ManualLottoAmount(
     }
 
     companion object {
+        fun Int.toManualLottoAmountResult(totalPurchaseAmount: Int): ManualLottoAmountResult {
+            if (this > totalPurchaseAmount) {
+                return ManualLottoAmountResult.CannotMoreThanTotalPurchaseAmount(CANNOT_MORE_THAN_TOTAL_PURCHASE_AMOUNT)
+            }
+            return ManualLottoAmountResult.Success(ManualLottoAmount(this, totalPurchaseAmount))
+        }
+
         const val CANNOT_MORE_THAN_TOTAL_PURCHASE_AMOUNT = "[ERROR] 구매 가능한 개수보다 구매하려는 개수가 더 많습니다."
     }
 }
