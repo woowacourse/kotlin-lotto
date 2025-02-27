3e6a2e7e6e9a473893b7f57d7d4ba8fed1be658b
ijh1298 <imjh0417@gmail.com>
2025-02-27 17:16:18 +0900

    refactor : 수동 로또 생성기의 로또 단일 생성 함수 분리

--- src/main/kotlin/domain/service/ManualLottoGenerator.kt
+++ src/main/kotlin/domain/service/ManualLottoGenerator.kt
@@ -6,9 +6,11 @@ import domain.model.LottoNumber
 class ManualLottoGenerator(
     private val manualLottoInput: List<String>,
 ) : LottoGenerator {
+    private fun makeOneLotto(lottoNumbers: Set<Int>): Lotto = Lotto(getLottoNumbers(lottoNumbers))
+
     override fun getLottoNumbers(lottoNumbers: Set<Int>): Set<LottoNumber> = lottoNumbers.map { LottoNumber.from(it) }.toSet()
 
-    override fun makeLotto(amount: Int): List<Lotto> = manualLottoInput.map { Lotto(getLottoNumbers(it.toIntSet())) }
+    override fun makeLotto(amount: Int): List<Lotto> = manualLottoInput.map { makeOneLotto(it.toIntSet()) }
 
     companion object {
         private fun String.toIntSet() = this.split(',').map { it.toInt() }.toSet()
