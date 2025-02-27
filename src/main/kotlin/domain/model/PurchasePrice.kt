b7e31165f7a0c5fb5d2e4844f411624b1bbab832
ijh1298 <imjh0417@gmail.com>
2025-02-27 17:15:20 +0900

    refactor : 구매 금액 class 생성자 private으로 수정 및 팩토리 함수에서 논리적 오류를 sealed class로 반환하도록 수정

--- src/main/kotlin/domain/model/PurchasePrice.kt
+++ src/main/kotlin/domain/model/PurchasePrice.kt
@@ -2,20 +2,35 @@ package domain.model
 
 import domain.model.Lotto.Companion.ERROR
 
-class PurchasePrice(
+sealed class PurchasePriceResult {
+    data class Success(
+        val purchasePrice: PurchasePrice,
+    ) : PurchasePriceResult()
+
+    data class InvalidMinimumPurchaseAmount(
+        val errorMsg: String,
+    ) : PurchasePriceResult()
+
+    data class InvalidThousandWonUnit(
+        val errorMsg: String,
+    ) : PurchasePriceResult()
+}
+
+class PurchasePrice private constructor(
     val value: Int,
     private val unit: Int = DEFAULT_AMOUNT_UNIT,
 ) {
-    init {
-        require(value > 0) { INVALID_MINIMUM_PURCHASE_AMOUNT }
-        require(value % DEFAULT_AMOUNT_UNIT == 0) { INVALID_THOUSAND_WON_UNIT }
-    }
-
     companion object {
+        fun Int.toPurchasePriceResult(unit: Int = DEFAULT_AMOUNT_UNIT): PurchasePriceResult {
+            if (this <= 0) return PurchasePriceResult.InvalidMinimumPurchaseAmount(INVALID_MINIMUM_PURCHASE_AMOUNT)
+            if (this % unit != 0) return PurchasePriceResult.InvalidThousandWonUnit(INVALID_THOUSAND_WON_UNIT)
+            return PurchasePriceResult.Success(PurchasePrice(this))
+        }
+
         fun PurchasePrice.toAmount(): Int = value / unit
 
         const val DEFAULT_AMOUNT_UNIT = 1000
-        const val INVALID_MINIMUM_PURCHASE_AMOUNT = "$ERROR ${DEFAULT_AMOUNT_UNIT}원 이상 입력해주세요."
-        const val INVALID_THOUSAND_WON_UNIT = "$ERROR ${DEFAULT_AMOUNT_UNIT}원 단위로 입력해주세요."
+        private const val INVALID_MINIMUM_PURCHASE_AMOUNT = "$ERROR ${DEFAULT_AMOUNT_UNIT}원 이상 입력해주세요."
+        private const val INVALID_THOUSAND_WON_UNIT = "$ERROR ${DEFAULT_AMOUNT_UNIT}원 단위로 입력해주세요."
     }
 }
