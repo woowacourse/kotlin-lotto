354d0d2722ab846b0decb89764e364deec48adaf
DongjooSEO <readyjoo24@gmail.com>
2025-02-27 17:10:31 +0900

    feat: LottoCalculator 로또 수량 계산 기능 구현

--- src/main/kotlin/lotto/domain/LottoCalculator.kt
+++ src/main/kotlin/lotto/domain/LottoCalculator.kt
@@ -2,13 +2,21 @@ package lotto.domain
 
 class LottoCalculator(
     val amountOfPurchase: Int,
+    private val lottoPrice: Int = LOTTO_PRICE,
 ) {
     init {
-        require(amountOfPurchase % LOTTO_PRICE == 0) { "로또는 1000원 단위로 구입해주세요" }
+        validateAmount()
     }
 
-    fun calculateAmountOfLottos(): Int {
-        return amountOfPurchase / LOTTO_PRICE
+    fun calculateAmountOfLottos(): Int = amountOfPurchase / lottoPrice
+
+    fun calculateAutoLottos(manualCount: Int): Int {
+        require(manualCount in 0..calculateAmountOfLottos()) { "수동 로또 개수가 잘못되었습니다." }
+        return calculateAmountOfLottos() - manualCount
+    }
+
+    private fun validateAmount() {
+        require(amountOfPurchase % lottoPrice == 0) { "로또는 ${lottoPrice}원 단위로 구입해주세요" }
     }
 
     companion object {
