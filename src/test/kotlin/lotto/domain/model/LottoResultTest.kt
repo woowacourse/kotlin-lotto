46843792500540533b359ef47fb54a25853aa1c9
moondev03 <moondev03@gmail.com>
2025-02-27 11:10:38 +0900

    refactor: 총 수익률 계산 테스트 가독성 개선

--- src/test/kotlin/lotto/domain/model/LottoResultTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoResultTest.kt
@@ -2,24 +2,31 @@ package lotto.domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class LottoResultTest {
-    @ParameterizedTest(name = "로또 등수가 [{0}]일 때, 수익률은 \"{1}\"이다")
-    @CsvSource(
-        "FIRST, 2000000.00",
-        "SECOND, 30000.00",
-        "THIRD, 1500.00",
-        "FOURTH FIFTH, 27.50",
-        "MISS, 0.00",
-        "THIRD FIFTH SECOND, 10501.66",
-    )
+    @ParameterizedTest(name = "로또 등수가 {0}일 때, 수익률은 \"{1}\"이다")
+    @MethodSource("provideLottoRanksAndReturns")
     fun `총 수익률을 계산할 수 있다`(
-        lottoRanks: String,
+        lottoRanks: List<LottoRank>,
         expectedReturn: Double,
     ) {
-        val ranks = lottoRanks.split(" ").map { LottoRank.valueOf(it) }
-        val totalReturn = LottoResult(ranks).calculateTotalReturn(1000)
+        val totalReturn = LottoResult(lottoRanks).calculateTotalReturn(1000)
         assertThat(totalReturn.toDouble()).isEqualTo(expectedReturn)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideLottoRanksAndReturns(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(listOf(LottoRank.FIRST), 2000000.00),
+                Arguments.of(listOf(LottoRank.SECOND), 30000.00),
+                Arguments.of(listOf(LottoRank.THIRD), 1500.00),
+                Arguments.of(listOf(LottoRank.FOURTH, LottoRank.FIFTH), 27.50),
+                Arguments.of(listOf(LottoRank.MISS), 0.00),
+                Arguments.of(listOf(LottoRank.THIRD, LottoRank.FIFTH, LottoRank.SECOND), 10501.66),
+            )
     }
 }
