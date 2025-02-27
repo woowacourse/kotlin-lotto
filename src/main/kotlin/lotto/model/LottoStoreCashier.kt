87d9363c1f6d32fa9986ff13d42bc8fd5c1678af
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-27 16:15:07 +0900

    refactor: 로또 점원의 역할 세분화

--- src/main/kotlin/lotto/model/LottoStoreCashier.kt
+++ src/main/kotlin/lotto/model/LottoStoreCashier.kt
@@ -2,23 +2,23 @@ package lotto.model
 
 import lotto.contants.LottoRuleConstants
 
+// 돈 계산을 해주는 클래스
 class LottoStoreCashier(
-    private val money: Int,
+    private val customerMoney: Int,
 ) {
+    private val possibleToLottoTicketCount = calculatePossibleToBuyLottoTicketCount()
+
+    // 로또의 가격은 천원이다. 로또를 구매할려면 천원이상 넣어야한다.
     init {
-        require(money >= LottoRuleConstants.LOTTO_AMOUNT.value) { "로또는 천원이상 넣어야지 구매 가능합니다." }
+        require(customerMoney >= LottoRuleConstants.LOTTO_AMOUNT.value) { "로또는 천원이상 넣어야지 구매 가능합니다." }
     }
 
-    // 거스름 돈을 계산
-    fun calculateChange(): Int {
-        if (money % LottoRuleConstants.LOTTO_AMOUNT.value != 0) {
-            return money - LottoRuleConstants.LOTTO_AMOUNT.value * calculateCount()
-        }
-        return 0
-    }
+    // 고객이 준 돈에서 몇장을 살 수 있는 지 계산한다.
+    private fun calculatePossibleToBuyLottoTicketCount(): Int = customerMoney / LottoRuleConstants.LOTTO_AMOUNT.value
 
-    // 로또 몇 장 발행가능 한지 계산
-    fun calculateCount(): Int {
-        return money / LottoRuleConstants.LOTTO_AMOUNT.value
-    }
+    // 고객이 몇장을 사고 싶다고 이야기를 하면 가능한지 계산한다.
+    fun isPossibleToBuy(customerWantBuyLottoTicketCount: Int): Boolean = possibleToLottoTicketCount >= customerWantBuyLottoTicketCount
+
+    // 고객에게 전달할 잔돈을 계산한다.
+    fun calculateChange(customerWantBuyLottoTicketCount: Int): Int = customerMoney - (LottoRuleConstants.LOTTO_AMOUNT.value * possibleToLottoTicketCount)
 }
