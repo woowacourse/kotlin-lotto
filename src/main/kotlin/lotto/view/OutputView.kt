cb8ca92e326edcf160f9e6673c06422cae677f97
cucumber99 <dlsguq1@gmail.com>
2025-02-27 11:42:41 +0900

    refactor: 출력 메소드 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,5 +1,6 @@
 package lotto.view
 
+import lotto.model.LottoPurchaseInfo
 import lotto.model.Rank
 
 class OutputView {
@@ -7,11 +8,22 @@ class OutputView {
         println("구입금액을 입력해 주세요.")
     }
 
+    fun printManualLottoCountMessage() {
+        println()
+        println("수동으로 구매할 로또 수를 입력해 주세요.")
+    }
+
+    fun printManualLottoNumberMessage() {
+        println()
+        println("수동으로 구매할 번호를 입력해 주세요.")
+    }
+
     fun printPublishedLotto(
-        quantity: Int,
+        lottoPurchaseInfo: LottoPurchaseInfo,
         publishedLotto: List<String>,
     ) {
-        println("${quantity}개를 구매했습니다.")
+        println()
+        println("수동으로 ${lottoPurchaseInfo.manualLottoCount}장, 자동으로 ${lottoPurchaseInfo.getAutoLottoQuantity()}개를 구매했습니다.")
         publishedLotto.forEach { println(it) }
     }
 
@@ -28,17 +40,17 @@ class OutputView {
         result: Map<Rank, Int>,
         earningRate: Double,
     ) {
-        val prizeResult =
-            Rank.entries
-                .filter { it.winningMoney > 0 }
-                .map { rank ->
-                    "${rank.countOfMatch}개 일치${if (rank == Rank.SECOND) ", 보너스 볼 일치" else ""} " +
-                        "(${rank.winningMoney}원) - ${result.getOrDefault(rank, 0)}개"
-                }
         println()
         println("당첨 통계")
         println("---------")
-        println(prizeResult)
+        Rank.entries
+            .filter { it.winningMoney > 0 }
+            .forEach { rank ->
+                println(
+                    "${rank.countOfMatch}개 일치${if (rank == Rank.SECOND) ", 보너스 볼 일치" else ""} " +
+                        "(${rank.winningMoney}원) - ${result.getOrDefault(rank, 0)}개",
+                )
+            }
         println("총 수익률은 ${"%.2f".format(earningRate)}입니다.")
     }
 }
