493bab9b319b3bcec4dd87b7668704fe639f1f76
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-27 17:01:52 +0900

    refactor:  자동과 수동 로또 계산과 발행해주는 매니저로 역할 부여

---
+++ src/main/kotlin/lotto/model/LottoIssueType.kt
@@ -0,0 +1,4 @@
+package lotto.model
+
+enum class LottoIssuetype {
+}
---
+++ src/main/kotlin/lotto/model/LottoTicketIssueManager.kt
@@ -0,0 +1,21 @@
+package lotto.model
+
+// 수동이랑 자동 로또 계산해주는 매니저, 로또 티켓까지 주는 역할 담당
+class LottoTicketIssueManager (
+    customerWantBuyLottoTicketCount: Int,
+    customerWantToBuyManualLottoTicketCount: Int
+) {
+    private val customerWantBuyLottoTicketCount = LottoTicketCount(customerWantBuyLottoTicketCount)
+    private val customerWantToBuyManualLottoTicketCount = LottoTicketCount(customerWantToBuyManualLottoTicketCount)
+    private val customerWantToBuyAutoLottoTicketCount = LottoTicketCount(customerWantBuyLottoTicketCount - customerWantToBuyManualLottoTicketCount)
+
+    // 발행가능한 개수보다 수동으로 로또 발행받고 싶은 개수가 크면 안된다.
+    init {
+        require(customerWantBuyLottoTicketCount >= customerWantToBuyManualLottoTicketCount) { "전체 발행 가능한 로또 개수보다 수동로또 발행 개수가 더 많습니다." }
+    }
+
+    // 로또를 발행해서 준다.
+    fun issueLottoTickets(manualLottoNumbers: List<List<Int>>): List<LottoTicket> = LottoMachine(
+        customerWantToBuyAutoLottoTicketCount, customerWantToBuyManualLottoTicketCount, manualLottoNumbers
+    ).issueLottoTickets()
+}
