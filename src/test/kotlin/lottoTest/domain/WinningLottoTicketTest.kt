2be09b29f3ad9105a05377021e59b43063d492e7
oungsi2000 <oungsi1000@gmail.com>
2025-02-27 21:15:04 +0900

    refactor(WinningLottoTicketTest):ParaemterizedTest 도입

--- src/test/kotlin/lottoTest/domain/WinningLottoTicketTest.kt
+++ src/test/kotlin/lottoTest/domain/WinningLottoTicketTest.kt
@@ -8,105 +8,41 @@ import lotto.domain.WinningLottoTicket
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class WinningLottoTicketTest {
-    @Test
+    @ParameterizedTest
+    @MethodSource("t1Provider")
     @DisplayName("복권을 구매한 숫자만큼 복권의 당첨 등수를 반환한다")
-    fun t1() {
-        val manyLotto =
-            listOf(
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-            )
-        val winningLotto = Lotto.of(1, 2, 11, 10, 8, 9)
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRanks(manyLotto)
-        assertThat(result).isEqualTo(
-            ScoreRankMap.of(
-                mapOf(Rank.MISS to 3),
-            ),
-        )
-    }
-
-    @Test
-    @DisplayName("복권을 구매한 숫자만큼 복권의 당첨 등수를 반환한다")
-    fun t1_1() {
-        val manyLotto =
-            listOf(
-                Lotto.of(1, 2, 11, 10, 5, 6),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-            )
-        val winningLotto = Lotto.of(1, 2, 11, 10, 8, 9)
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRanks(manyLotto)
-        assertThat(result).isEqualTo(
-            ScoreRankMap.of(
-                mapOf(
-                    Rank.MISS to 2,
-                    Rank.FOURTH to 1,
-                ),
-            ),
-        )
-    }
-
-    @Test
-    @DisplayName("복권을 구매한 숫자만큼 복권의 당첨 등수를 반환한다")
-    fun t1_2() {
-        val manyLotto =
-            listOf(
-                Lotto.of(1, 2, 11, 10, 5, 6),
-                Lotto.of(1, 2, 3, 4, 5, 6),
-                Lotto.of(1, 2, 11, 10, 8, 9),
-            )
-        val winningLotto = Lotto.of(1, 2, 11, 10, 8, 9)
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRanks(manyLotto)
-        assertThat(result).isEqualTo(
-            ScoreRankMap.of(
-                mapOf(
-                    Rank.MISS to 1,
-                    Rank.FOURTH to 1,
-                    Rank.FIRST to 1,
-                ),
-            ),
-        )
-    }
-
-    @Test
-    @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
+    fun t1(
+        manyLotto: List<Lotto>,
+        scoreMap: ScoreRankMap,
+    ) {
         val winningLotto = Lotto.of(1, 2, 3, 4, 5, 6)
         val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.FIRST)
+        val result = WinningLottoTicket(winningLotto, bonus).findLottoRanks(manyLotto)
+        assertThat(result).isEqualTo(scoreMap)
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("t2Provider")
     @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_1() {
+    fun t2(
+        winningLotto: Lotto,
+        rank: Rank,
+    ) {
         val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 8).map { LottoNumber.of(it) })
         val bonus = LottoNumber.of(7)
         val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.THIRD)
+        assertThat(result).isEqualTo(rank)
     }
 
     @Test
     @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_2() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 7).map { LottoNumber.of(it) })
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.SECOND)
-    }
-
-    @Test
-    @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_2_1() {
+    fun t3() {
         val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
         val winningLotto = Lotto(listOf(1, 2, 3, 4, 5, 7).map { LottoNumber.of(it) })
         val bonus = LottoNumber.of(5)
@@ -114,33 +50,58 @@ class WinningLottoTicketTest {
         assertThat(result).isEqualTo(Rank.THIRD)
     }
 
-    @Test
-    @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_3() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        val winningLotto = Lotto(listOf(1, 2, 3, 4, 8, 9).map { LottoNumber.of(it) })
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.FOURTH)
-    }
+    companion object {
+        @JvmStatic
+        private fun t1Provider(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(
+                    listOf(
+                        Lotto.of(11, 12, 13, 14, 15, 16),
+                        Lotto.of(11, 12, 13, 14, 15, 16),
+                        Lotto.of(11, 12, 13, 14, 15, 16),
+                    ),
+                    ScoreRankMap.of(
+                        mapOf(Rank.MISS to 3),
+                    ),
+                ),
+                Arguments.of(
+                    listOf(
+                        Lotto.of(1, 2, 11, 10, 5, 6),
+                        Lotto.of(11, 12, 13, 14, 15, 16),
+                        Lotto.of(11, 12, 13, 14, 15, 16),
+                    ),
+                    ScoreRankMap.of(
+                        mapOf(
+                            Rank.MISS to 2,
+                            Rank.FOURTH to 1,
+                        ),
+                    ),
+                ),
+                Arguments.of(
+                    listOf(
+                        Lotto.of(1, 2, 11, 10, 5, 6),
+                        Lotto.of(1, 2, 3, 4, 5, 6),
+                        Lotto.of(1, 2, 11, 10, 8, 9),
+                    ),
+                    ScoreRankMap.of(
+                        mapOf(
+                            Rank.MISS to 1,
+                            Rank.FOURTH to 1,
+                            Rank.FIRST to 1,
+                        ),
+                    ),
+                ),
+            )
 
-    @Test
-    @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_4() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        val winningLotto = Lotto.of(1, 2, 3, 10, 8, 9)
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.FIFTH)
-    }
-
-    @Test
-    @DisplayName("지난 주 당첨 번호와 보너스 볼을 입력받으면 복권의 당첨 등수(enum 멤버)를 반환한다")
-    fun t2_5() {
-        val lotto = Lotto.of(1, 2, 3, 4, 5, 6)
-        val winningLotto = Lotto.of(1, 2, 11, 10, 8, 9)
-        val bonus = LottoNumber.of(7)
-        val result = WinningLottoTicket(winningLotto, bonus).findLottoRank(lotto)
-        assertThat(result).isEqualTo(Rank.MISS)
+        @JvmStatic
+        private fun t2Provider(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(Lotto.of(1, 2, 3, 4, 5, 6), Rank.FIRST),
+                Arguments.of(Lotto.of(1, 2, 3, 4, 5, 8), Rank.THIRD),
+                Arguments.of(Lotto.of(1, 2, 3, 4, 5, 7), Rank.SECOND),
+                Arguments.of(Lotto.of(1, 2, 3, 4, 8, 9), Rank.FOURTH),
+                Arguments.of(Lotto.of(1, 2, 3, 10, 8, 9), Rank.FIFTH),
+                Arguments.of(Lotto.of(1, 2, 11, 10, 8, 9), Rank.MISS),
+            )
     }
 }
