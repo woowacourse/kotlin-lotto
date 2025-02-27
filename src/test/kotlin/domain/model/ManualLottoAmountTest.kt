0f8185789a3d3c57dbd28391a9fdac0bb5f4a034
ijh1298 <imjh0417@gmail.com>
2025-02-27 19:43:10 +0900

    test : sealed class Result.Success가 반환되는 경우 테스트 추가

--- src/test/kotlin/domain/model/ManualLottoAmountTest.kt
+++ src/test/kotlin/domain/model/ManualLottoAmountTest.kt
@@ -3,6 +3,8 @@ package domain.model
 import domain.model.ManualLottoAmount.Companion.toManualLottoAmountResult
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class ManualLottoAmountTest {
     @Test
@@ -12,6 +14,17 @@ class ManualLottoAmountTest {
 
         val actual = tryManualLottoAmount.toManualLottoAmountResult(totalPurchaseAmount)
         val expected = ManualLottoAmountResult.CannotMoreThanTotalPurchaseAmount::class.java
+
+        assertThat(actual).isInstanceOf(expected)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 5, 10])
+    fun `총 구매 개수를 넘지 않을 경우 수동 로또가 정상 구매된다`(value: Int) {
+        val totalPurchaseAmount = 10
+
+        val actual = value.toManualLottoAmountResult(totalPurchaseAmount)
+        val expected = ManualLottoAmountResult.Success::class.java
 
         assertThat(actual).isInstanceOf(expected)
     }
--- src/test/kotlin/domain/model/PurchasePriceTest.kt
+++ src/test/kotlin/domain/model/PurchasePriceTest.kt
@@ -3,11 +3,14 @@ package domain.model
 import domain.model.PurchasePrice.Companion.toPurchasePriceResult
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PurchasePriceTest {
-    @Test
-    fun `구입 금액이 0 이하면 InvalidMinimumPurchaseAmount 씰드 클래스를 반환한다`() {
-        val actualResult = 0.toPurchasePriceResult()
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1000])
+    fun `구입 금액이 0 이하면 InvalidMinimumPurchaseAmount 씰드 클래스를 반환한다`(value: Int) {
+        val actualResult = value.toPurchasePriceResult()
 
         val expectedResult = PurchasePriceResult.InvalidMinimumPurchaseAmount::class.java
 
@@ -19,6 +22,16 @@ class PurchasePriceTest {
         val actualResult = 1500.toPurchasePriceResult()
 
         val expectedResult = PurchasePriceResult.InvalidThousandWonUnit::class.java
+
+        assertThat(actualResult).isInstanceOf(expectedResult)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1000, 35000])
+    fun `이외 정상적인 구입 금액을 입력하면 Success 씰드 클래스를 반환한다`(value: Int) {
+        val actualResult = value.toPurchasePriceResult()
+
+        val expectedResult = PurchasePriceResult.Success::class.java
 
         assertThat(actualResult).isInstanceOf(expectedResult)
     }
