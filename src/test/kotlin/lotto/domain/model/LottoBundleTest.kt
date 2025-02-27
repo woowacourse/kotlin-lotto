4385fed1ee8e3f62e606a906ddc85bf0ed0a3ff6
moondev03 <moondev03@gmail.com>
2025-02-27 11:13:05 +0900

    refactor: LottoBundle Combine 테스트 케이스 1개로 수정

--- src/test/kotlin/lotto/domain/model/LottoBundleTest.kt
+++ src/test/kotlin/lotto/domain/model/LottoBundleTest.kt
@@ -4,8 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 class LottoBundleTest {
     @Test
@@ -22,21 +20,13 @@ class LottoBundleTest {
         }
     }
 
-    @CsvSource(
-        "1, 2, 3",
-        "2, 4, 6",
-        "3, 11, 14",
-    )
-    @ParameterizedTest(name = "크기가 {0}인 로또 번들과 크기가 {1}인 로또 번들을 결합하면 크기가 {2}인 로또 번들이 되야한다 ")
-    fun `로또 번들을 결합할 수 있다`(
-        size1: Int,
-        size2: Int,
-        expected: Int,
-    ) {
-        val lottoBundle1 = LottoBundle(List(size1) { Lotto(listOf(1, 2, 3, 4, 5, 6)) })
-        val lottoBundle2 = LottoBundle(List(size2) { Lotto(listOf(1, 2, 3, 4, 5, 6)) })
+    @Test
+    fun `크기가 1인 로또 번들과 크기가 3인 로또 번들을 결합하면 크기가 4인 로또 번들이 되야한다`() {
+        val lottoBundle1 = LottoBundle(List(1) { Lotto(listOf(1, 2, 3, 4, 5, 6)) })
+        val lottoBundle2 = LottoBundle(List(3) { Lotto(listOf(1, 2, 3, 4, 5, 6)) })
 
         val actual = LottoBundle.combine(lottoBundle1, lottoBundle2).lottos.size
+        val expected = 4
         assertThat(actual).isEqualTo(expected)
     }
 }
