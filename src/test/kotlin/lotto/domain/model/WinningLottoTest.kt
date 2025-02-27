8882da7a0cf6cbf828635134449a860e77dfa7b5
HamBeomJoon <hbj0802@naver.com>
2025-02-27 09:48:36 +0900

    refactor: 가짜 생성자 반환 타입 수정

--- src/test/kotlin/lotto/domain/model/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/model/WinningLottoTest.kt
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class WinningLottoTest {
-    private fun lotto(vararg numbers: Int): List<LottoNumber> = numbers.map { LottoNumber(it) }
+    private fun lotto(vararg numbers: Int): LottoTicket = LottoTicket(numbers.map { LottoNumber(it) })
 
     @Test
     fun `보너스 번호가 당첨 번호와 중복되면 예외를 발생시킨다`() {
-        val winningNumbers = lotto(1, 2, 3, 4, 5, 6).toSet()
+        val winningNumbers = lotto(1, 2, 3, 4, 5, 6)
         val bonusNumber = 6
 
         assertThrows<IllegalArgumentException> {
-            WinningLotto(winningNumbers, LottoNumber(bonusNumber))
+            WinningLotto(winningNumbers.toSet(), LottoNumber(bonusNumber))
         }
     }
 
@@ -21,10 +21,7 @@ class WinningLottoTest {
     fun `로또 당첨을 판단한다`() {
         val bonusNumber = LottoNumber(7)
         val winningNumbers = lotto(1, 2, 3, 4, 5, 6).toSet()
-        val lottoTicket =
-            LottoTicket(
-                lotto(1, 2, 3, 4, 5, 6),
-            )
+        val lottoTicket = lotto(1, 2, 3, 4, 5, 6)
 
         val rank = WinningLotto(winningNumbers, bonusNumber).getRank(lottoTicket)
 
