d07db8c0340906a1407645b042ecf608700eb335
Leeyerin0210 <lyl8661@naver.com>
2025-02-27 17:00:18 +0900

    refactor: 기존 코드 구조를 폐기하고 새로운 설계를 시작

--- src/main/kotlin/lotto/model/Lotto.kt
+++
@@ -1,9 +0,0 @@
-package lotto.model
-
-class Lotto(
-    val numbers: LottoNumbers,
-) {
-    fun getNumbers(): List<LottoNumber> = numbers.numberList.map { it }
-
-    fun findNumber(number: LottoNumber): Boolean = numbers.include(number)
-}
