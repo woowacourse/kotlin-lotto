18d8dce025a453c9b54b3c7300320499ab923eeb
Hwnag ChaeWon <wondroid.world@gmail.com>
2025-02-27 14:08:28 +0900

    feat: 금액 계산해주는 캐셔 구현

--- src/main/kotlin/lotto/model/LottoStoreCashier.kt
+++ src/main/kotlin/lotto/model/LottoStoreCashier.kt
@@ -1,4 +1,24 @@
 package lotto.model
 
-class LottoStoreCashier {
+import lotto.contants.LottoRuleConstants
+
+class LottoStoreCashier(
+    private val money: Int,
+) {
+    init {
+        require(money >= LottoRuleConstants.LOTTO_AMOUNT.value) { "로또는 천원이상 넣어야지 구매 가능합니다." }
+    }
+
+    // 거스름 돈을 계산
+    fun calculateChange(): Int {
+        if (money % LottoRuleConstants.LOTTO_AMOUNT.value != 0) {
+            return money - LottoRuleConstants.LOTTO_AMOUNT.value * calculateCount()
+        }
+        return 0
+    }
+
+    // 로또 몇 장 발행가능 한지 계산
+    fun calculateCount(): Int {
+        return money / LottoRuleConstants.LOTTO_AMOUNT.value
+    }
 }
