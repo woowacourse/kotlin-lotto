e971973feed56577b6fc388a4f74de4b8450f006
eden <devfeijoa@gmail.com>
2025-02-28 20:19:25 +0900

    refactor: 여러번 생성자를 부르지 않기 위해서 팩토리 함수를 이용한 방법으로 수정

--- src/main/kotlin/lotto/model/LottoNumber.kt
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -1,14 +1,16 @@
 package lotto.model
 
-@JvmInline
-value class LottoNumber(val number: Int) {
-    init {
-        require(number in MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE) { LOTTO_BOUND_MESSAGE }
-    }
-
+class LottoNumber private constructor(private val value: Int) {
     companion object {
         const val MAXIMUM_LOTTO_RANGE: Int = 45
         const val MINIMUM_LOTTO_RANGE: Int = 1
         const val LOTTO_BOUND_MESSAGE = "로또 번호는 1에서 45 범위 내에서 있어야 합니다."
+
+        private val NUMBERS: Map<Int, LottoNumber> =
+            (MINIMUM_LOTTO_RANGE..MAXIMUM_LOTTO_RANGE).associateWith { LottoNumber(it) }
+
+        fun from(value: Int): LottoNumber {
+            return NUMBERS[value] ?: throw IllegalArgumentException(LOTTO_BOUND_MESSAGE)
+        }
     }
 }
