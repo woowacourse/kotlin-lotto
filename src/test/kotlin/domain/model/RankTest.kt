7f3f0cc205ed02145bdc0b6c5468220bca2cd7be
ijh1298 <imjh0417@gmail.com>
2025-03-01 00:32:52 +0900

    test : 누락되었던 RankTest 등수 반환 테스트 추가

--- src/test/kotlin/domain/model/RankTest.kt
+++ src/test/kotlin/domain/model/RankTest.kt
@@ -2,6 +2,8 @@ package domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class RankTest {
     @Test
@@ -22,15 +24,23 @@ class RankTest {
         assertThat(result).isEqualTo(Rank.THIRD)
     }
 
-    @Test
-    fun `4개가 일치하고 보너스 번호가 일치하지 않으면 4등 순위가 반환된다`() {
-        val result = Rank.valueOf(4, false)
+    @ParameterizedTest
+    @ValueSource(booleans = [true, false])
+    fun `4개가 일치하면 4등 순위가 반환된다`(isMatchBonus: Boolean) {
+        val result = Rank.valueOf(4, isMatchBonus)
         assertThat(result).isEqualTo(Rank.FOURTH)
     }
 
+    @ParameterizedTest
+    @ValueSource(booleans = [true, false])
+    fun `3개가 일치하면 5등 순위가 반환된다`(isMatchBonus: Boolean) {
+        val result = Rank.valueOf(3, isMatchBonus)
+        assertThat(result).isEqualTo(Rank.FIFTH)
+    }
+
     @Test
-    fun `4개가 일치하고 보너스 번호도 일치하면 4등 순위가 반환된다`() {
-        val result = Rank.valueOf(4, true)
-        assertThat(result).isEqualTo(Rank.FOURTH)
+    fun `2개 이하로 일치하면 MISS가 반환된다`() {
+        val result = Rank.valueOf(2, false)
+        assertThat(result).isEqualTo(Rank.MISS)
     }
 }
