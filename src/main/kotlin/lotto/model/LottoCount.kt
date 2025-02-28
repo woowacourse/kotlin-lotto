e4c8f69258077a8a8ecf7db6883ff2f86b54cec1
m6z1 <sonmyungj1zz@gmail.com>
2025-02-28 22:21:23 +0900

    fix: 로또 최소 개수 1에서 0으로 수정

--- src/main/kotlin/lotto/model/LottoCount.kt
+++ src/main/kotlin/lotto/model/LottoCount.kt
@@ -14,7 +14,7 @@ class LottoCount(
     fun subtract(lottoCount: LottoCount): LottoCount = LottoCount(count - lottoCount.count)
 
     companion object {
-        private const val MIN_LOTTO_COUNT = 1
+        private const val MIN_LOTTO_COUNT = 0
         private const val ERROR_MIN_LOTTO_COUNT = "로또 개수 %d는 $MIN_LOTTO_COUNT 이상이 아닙니다."
         private const val ERROR_MAX_LOTTO_COUNT = "발행할 수 있는 로또 개수 %d개보다 %d이(가) 클 수 없습니다."
     }
