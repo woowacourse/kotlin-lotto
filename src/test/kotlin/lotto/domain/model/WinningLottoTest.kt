c14328396926a265c1f83bac33747301553b13a5
HamBeomJoon <hbj0802@naver.com>
2025-03-01 18:02:40 +0900

    refactor: winningLottoTest 기존코드로 변경

--- src/test/kotlin/lotto/domain/model/WinningLottoTest.kt
+++ src/test/kotlin/lotto/domain/model/WinningLottoTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class WinningLottoTest {
-    private fun lotto(vararg numbers: Int): LottoTicket = LottoTicket(numbers.map { LottoNumber(it) })
+    private fun lotto(vararg numbers: Int): Set<LottoNumber> = numbers.map { LottoNumber(it) }.toSet()
 
     @Test
     fun `보너스 번호가 당첨 번호와 중복되면 예외를 발생시킨다`() {
@@ -13,7 +13,7 @@ class WinningLottoTest {
         val bonusNumber = 7
 
         assertThrows<IllegalArgumentException> {
-            WinningLotto(winningNumbers.numbers, LottoNumber(bonusNumber))
+            WinningLotto(winningNumbers, LottoNumber(bonusNumber))
         }
     }
 
@@ -21,10 +21,10 @@ class WinningLottoTest {
     fun `로또 당첨을 판단한다`() {
         val bonusNumber = LottoNumber(7)
         val winningNumbers = lotto(1, 2, 3, 4, 5, 6)
-        val lottoTicket = lotto(1, 2, 3, 4, 5, 6)
+        val lottoTicket = LottoTicket(lotto(1, 2, 3, 4, 5, 6).toList())
         val expect = Rank.FIRST
 
-        val rank = WinningLotto(winningNumbers.numbers, bonusNumber).getRank(lottoTicket)
+        val rank = WinningLotto(winningNumbers, bonusNumber).getRank(lottoTicket)
 
         assertThat(rank).isEqualTo(expect)
     }
