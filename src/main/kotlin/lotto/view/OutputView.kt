c16cf646d91b403250e93d83bdc2f241968cc73c
eden <devfeijoa@gmail.com>
2025-03-02 02:25:42 +0900

    Revert "refactor: 3위부터 나오게 하기 위해 출력 결과 정렬"
    This reverts commit eb3575c0e505480974e3e0de320d914a22d8aa54.

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -2,47 +2,44 @@ package lotto.view
 
 import lotto.model.Lotto
 import lotto.model.Rank
+import java.lang.String.format
 
 class OutputView {
-    fun printError(message: String) {
-        println("[ERROR] $message")
+    fun printLottoCount(number: Int) {
+        println(format(OUTPUT_LOTTO_COUNT_MESSAGE, number))
     }
 
-    fun printLottoCounts(
-        manual: Int,
-        auto: Int,
-    ) {
-        println("수동으로 ${manual}장, 자동으로 ${auto}개를 구매했습니다.")
-    }
-
-    fun printAllLottos(lottos: List<Lotto>) {
-        lottos.forEach { lotto ->
-            println(lotto.numbers.map { it.toString() }.sorted())
+    fun printLottoBundle(lottoBundle: List<Lotto>) {
+        lottoBundle.forEach { lotto ->
+            println(lotto.getNumbers().map { it.number })
         }
     }
+
+    private fun isBonusMatch(rank: Rank) = if (rank == Rank.SECOND) OUTPUT_STATISTICS_BONUS_NUMBER_MESSAGE else " "
 
     fun printResult(
-        matchResults: Map<Rank, Int>,
-        profitRate: String,
+        winningCounts: Map<Rank, Int>,
+        profit: String,
     ) {
         println(OUTPUT_STATISTIC_GUIDE_MESSAGE)
-
-        val ranksInOrder = listOf(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)
-        ranksInOrder.forEach { rank ->
-            val bonusMessage = if (rank == Rank.SECOND) ", 보너스 볼 일치" else ""
-            println("${rank.countOfMatch}개 일치$bonusMessage (${rank.winningMoney}원)- ${matchResults[rank] ?: 0}개")
+        winningCounts.forEach { (rank, winningCount) ->
+            if (rank != Rank.MISS) {
+                println(
+                    format(
+                        OUTPUT_STATISTICS_MESSAGE,
+                        rank.countOfMatch,
+                        isBonusMatch(rank),
+                        rank.winningMoney,
+                        winningCount,
+                    ),
+                )
+            }
         }
-
-//        Rank.entries.filterNot { it == Rank.MISS }.forEach { rank ->
-//            println(
-//                "${rank.countOfMatch}개 일치${if (rank == Rank.SECOND)OUTPUT_STATISTICS_BONUS_NUMBER_MESSAGE else ""} (${rank.winningMoney}원)- ${matchResults[rank] ?: 0}개",
-//            )
-//        }
-
-        println("총 수익률은 $profitRate 입니다.(기준이 1이기 때문에 결과적으로 손해라는 의미임)")
+        println(format(OUTPUT_PROFIT_MESSAGE, profit))
     }
 
     companion object {
+        private const val OUTPUT_LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다."
         private const val OUTPUT_STATISTIC_GUIDE_MESSAGE = "\n당첨 통계\n---------"
         private const val OUTPUT_STATISTICS_MESSAGE = "%d개 일치%s(%d원)- %d개"
         private const val OUTPUT_STATISTICS_BONUS_NUMBER_MESSAGE = ", 보너스 볼 일치"
