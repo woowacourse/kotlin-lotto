e7a7508a2a0be7e2cc2ecac2f8c808e661971d88
eden <devfeijoa@gmail.com>
2025-03-02 02:15:15 +0900

    refactor: Lotto 클래스로 역할 위임

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,28 +1,36 @@
 package lotto.model
 
 class Lotto(
-    private val numbers: Set<LottoNumber>,
+    val numbers: Set<LottoNumber>,
 ) {
     init {
         require(numbers.size == LOTTO_NUMBERS_SIZE) { LOTTO_COUNT_MESSAGE }
     }
 
-    constructor(numbers: List<LottoNumber>) : this(numbers.toSet()) {
-        require(numbers.size == this.numbers.size) { LOTTO_COUNT_MESSAGE }
+    fun match(winning: WinningLotto): Rank {
+        val matchCount = numbers.count { winning.contains(it) }
+        val bonusMatch = winning.isBonusMatch(this)
+        return Rank.valueOf(matchCount, bonusMatch)
     }
 
-    fun contains(it: LottoNumber): Boolean {
-        return numbers.contains(it)
-    }
-
-    fun match(lotto: Lotto): Int {
-        return numbers.count { lotto.contains(it) }
-    }
+    fun contains(number: LottoNumber): Boolean = numbers.contains(number)
 
     companion object {
         const val LOTTO_NUMBERS_SIZE: Int = 6
         const val LOTTO_COUNT_MESSAGE = "로또 번호는 6개여야 합니다."
         const val LOTTO_BOUND_MESSAGE = "로또 번호는 1에서 45 범위 내에서 있어야 합니다."
-        const val LOTTO_DISTINCT_MESSAGE = "로또 번호가 중복될 수 없습니다."
+
+        fun from(numbers: List<Int>): Lotto? {
+            val lottoNumbers = numbers.mapNotNull { LottoNumber.from(it) }.toSet()
+            return if (lottoNumbers.size == LOTTO_NUMBERS_SIZE) Lotto(lottoNumbers) else null
+        }
     }
 }
+
+//    fun contains(number: LottoNumber): Boolean {
+//        return numbers.contains(number)
+//    }
+
+//    fun matchCount(lotto: Lotto): Int {
+//        return numbers.count { lotto.contains(it) }
+//    }
