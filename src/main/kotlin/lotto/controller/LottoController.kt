a9070a480d59d6dcc5309615eb93d2d749ff5072
eden <devfeijoa@gmail.com>
2025-03-02 03:09:24 +0900

    Revert "style: 컨트롤러에서 직접 생성하고 있는 수동 로또 생성 기능 대신 ManualLottoGenerator 호출"
    This reverts commit fe9edce8f179a9a449bd7c0261640c3e1eac752c.

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -4,7 +4,6 @@ import lotto.model.Lotto
 import lotto.model.LottoGenerator
 import lotto.model.LottoNumber
 import lotto.model.LottoStatisticResult
-import lotto.model.ManualLottoGenerator
 import lotto.model.WinningLotto
 import lotto.view.InputView
 import lotto.view.OutputView
@@ -26,8 +25,10 @@ class LottoController(
         val manualNumbersList = inputView.inputManualLottoNumber(manualCount)
 
         // 수동 로또 생성 (명확히 List<Lotto> 형태로 변환)
-        val manualLottoGenerator = ManualLottoGenerator(manualNumbersList)
-        val manualLottos = manualLottoGenerator.generate(manualCount)
+        val manualLottos =
+            manualNumbersList.map { numbers ->
+                Lotto.from(numbers) ?: return outputView.printError("잘못된 수동 로또 번호입니다.")
+            }
 
         // 자동 로또 생성
         val autoLottos = randomLottoGenerator.generate(totalCount - manualCount)
