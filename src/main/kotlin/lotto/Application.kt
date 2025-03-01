f4ab69b9c27a1189df76b51078779ed26e153faf
eden <devfeijoa@gmail.com>
2025-03-02 02:25:57 +0900

    Revert "refactor: 메인함수 호출"
    This reverts commit cef7ecfe826eeb74b9f749874f4c30ad712a8d95.

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,17 +1,7 @@
 package lotto
 
 import lotto.controller.LottoController
-import lotto.model.RandomLottoGenerator
-import lotto.view.InputView
-import lotto.view.OutputView
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-
-    LottoController(
-        inputView = inputView,
-        outputView = outputView,
-        randomLottoGenerator = RandomLottoGenerator(),
-    ).play()
+    LottoController().play()
 }
