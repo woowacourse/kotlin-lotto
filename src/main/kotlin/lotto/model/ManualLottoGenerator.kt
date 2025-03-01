93946190afbf16ddf2c7d6a5627bc72c799fc9fb
eden <devfeijoa@gmail.com>
2025-03-02 01:25:30 +0900

    refactor: 입력할때의 반환타입 변경으로 인해 타입 변경

--- src/main/kotlin/lotto/model/ManualLottoGenerator.kt
+++ src/main/kotlin/lotto/model/ManualLottoGenerator.kt
@@ -1,15 +1,11 @@
 package lotto.model
 
-import lotto.view.InputView
-
-class ManualLottoGenerator(private val inputView: InputView) : LottoGenerator {
+class ManualLottoGenerator(private val manualNumbersList: List<List<Int>>) : LottoGenerator {
     override fun generate(count: Int): List<Lotto> {
-        return List(count) {
-            val numbers =
-                inputView.inputManualLottoNumber()
-                    .map { LottoNumber.from(it) }
-                    .toSet()
-            Lotto(numbers)
+        require(manualNumbersList.size == count) { "로또 개수와 입력된 번호 개수가 일치하지 않습니다." }
+
+        return manualNumbersList.map { numbers ->
+            Lotto.from(numbers) ?: error("잘못된 로또 번호입니다.")
         }
     }
 }
