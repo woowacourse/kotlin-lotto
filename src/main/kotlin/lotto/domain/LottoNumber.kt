fb3f42698eeff7b98322c5cb3744e3e9c827fd3e
DoableTuple <doabletuple@gmail.com>
2025-03-01 19:25:03 +0900

    style: 멤버 선언 순서 컨벤션 준수

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -8,14 +8,14 @@ class LottoNumber private constructor(
     }
 
     companion object {
-        fun from(value: Int): LottoNumber {
-            return NUMBER.getOrPut(value) { LottoNumber(value) }
-        }
-
         private const val MIN = 1
         private const val MAX = 45
         val RANGE = (MIN..MAX)
         private val NUMBER: MutableMap<Int, LottoNumber> = mutableMapOf()
         private const val ERROR_MESSAGE_LOTTO_NUMBER_NOT_IN_RANGE = "로또는 1부터 45 사이의 숫자만 가질 수 있습니다."
+
+        fun from(value: Int): LottoNumber {
+            return NUMBER.getOrPut(value) { LottoNumber(value) }
+        }
     }
 }
