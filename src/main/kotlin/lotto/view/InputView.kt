7c339758795132a38f6fc7e593b632e7422aeb23
eden <devfeijoa@gmail.com>
2025-03-02 01:22:32 +0900

    refactor: 수동 로또 번호를 입력할 때, 여러번 안내 메세지가 나오는것을 막기 위해 반환타입 변경

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -14,11 +14,11 @@ class InputView {
         return readln().toIntOrNull()?.takeIf { it in 0..totalCount }
     }
 
-    fun inputManualLottoNumber(): List<Int> {
+    fun inputManualLottoNumber(manualCount: Int): List<List<Int>> {
         println(INPUT_MANUAL_LOTTO_NUMBER_MESSAGE)
-        val numbers = readln().split(",").mapNotNull { it.trim().toIntOrNull() }
-
-        return return if (numbers.size == Lotto.LOTTO_NUMBERS_SIZE) numbers else inputManualLottoNumber()
+        return List(manualCount) {
+            readln().split(",").mapNotNull { it.trim().toIntOrNull() }
+        }
     }
 
     fun inputWinningNumbers(): List<Int>? {
