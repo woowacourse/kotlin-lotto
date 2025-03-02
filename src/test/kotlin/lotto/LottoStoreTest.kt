b24ca8f63c03954a735d12817286fb62f74d98bd
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2025-03-03 01:48:35 +0900

    refactor: 수동 로또 생성 로직 변경에 따른 LottoStoreTest 수정

--- src/test/kotlin/lotto/LottoStoreTest.kt
+++ src/test/kotlin/lotto/LottoStoreTest.kt
@@ -19,46 +19,38 @@ class LottoStoreTest {
     fun `수동 로또를 2개 발행한다`() {
         val lottoStore = LottoStore()
         val manualCount = 2
-        val manualLottoBundle = listOf(listOf(1, 2, 3, 4, 5, 6), listOf(3, 4, 5, 6, 7, 8))
-        assertThat(lottoStore.getTickets(manualCount, ManualLottoGenerator(manualLottoBundle)).size).isEqualTo(2)
+        val manualLottoGenerator = ManualLottoGenerator()
+
+        manualLottoGenerator.add(listOf(1, 2, 3, 4, 5, 6))
+        manualLottoGenerator.add(listOf(3, 4, 5, 6, 7, 8))
+
+        assertThat(lottoStore.getTickets(manualCount, manualLottoGenerator).size).isEqualTo(2)
     }
 
     @Test
     fun `입력 받은 수동 로또의 개수가 6개가 아니면 에러가 발생한다`() {
-        val lottoStore = LottoStore()
-        val manualCount = 1
-        val manualLottoBundle = listOf(listOf(1, 2, 3, 4, 5, 6, 7))
+        val manualLottoGenerator = ManualLottoGenerator()
+
         assertThrows<IllegalArgumentException> {
-            lottoStore.getTickets(
-                manualCount,
-                ManualLottoGenerator(manualLottoBundle),
-            )
+            manualLottoGenerator.add(listOf(1, 2, 3, 4, 5, 6, 7))
         }
     }
 
     @Test
     fun `입력 받은 수동 로또는 중복되지 않아야 한다`() {
-        val lottoStore = LottoStore()
-        val manualCount = 1
-        val manualLottoBundle = listOf(listOf(1, 2, 3, 4, 5, 5))
+        val manualLottoGenerator = ManualLottoGenerator()
+
         assertThrows<IllegalArgumentException> {
-            lottoStore.getTickets(
-                manualCount,
-                ManualLottoGenerator(manualLottoBundle),
-            )
+            manualLottoGenerator.add(listOf(1, 2, 3, 4, 5, 5))
         }
     }
 
     @Test
     fun `입력 받은 수동 로또는 1부터 45 범위 내에 있어야 한다`() {
-        val lottoStore = LottoStore()
-        val manualCount = 1
-        val manualLottoBundle = listOf(listOf(1, 2, 3, 4, 5, 46))
+        val manualLottoGenerator = ManualLottoGenerator()
+
         assertThrows<IllegalArgumentException> {
-            lottoStore.getTickets(
-                manualCount,
-                ManualLottoGenerator(manualLottoBundle),
-            )
+            manualLottoGenerator.add(listOf(1, 2, 3, 4, 5, 46))
         }
     }
 }
