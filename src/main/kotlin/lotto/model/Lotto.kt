3ae449c7852bd426de35f0600c6919f8938c5bab
eden <devfeijoa@gmail.com>
2025-03-02 13:39:59 +0900

    refactor: 로또 테스트코드 작성, 중복 검사 안되는 부분 수정

--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -18,8 +18,11 @@ class Lotto(
     companion object {
         const val LOTTO_NUMBERS_SIZE: Int = 6
         const val LOTTO_COUNT_MESSAGE = "로또 번호는 6개여야 합니다."
+        const val LOTTO_DISTINCT_MESSAGE = "로또 번호가 중복될 수 없습니다."
 
         fun from(numbers: List<Int>): Lotto? {
+            require(numbers.size == LOTTO_NUMBERS_SIZE) { LOTTO_COUNT_MESSAGE }
+            require(numbers.distinct().size == LOTTO_NUMBERS_SIZE) { LOTTO_DISTINCT_MESSAGE }
             val lottoNumbers = numbers.mapNotNull { LottoNumber.from(it) }.toSet()
             return if (lottoNumbers.size == LOTTO_NUMBERS_SIZE) Lotto(lottoNumbers) else null
         }
