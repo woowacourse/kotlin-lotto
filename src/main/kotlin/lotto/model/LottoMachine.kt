fa3614b73991aacc16859db5ff7977962e5d3e40
박지민 <pjm5489@naver.com>
2025-03-02 14:35:11 +0900

    refactor: 구입금액 입력 범위 변경

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -14,8 +14,8 @@ class LottoMachine(
     }
 
     private fun validateAmountMinimumRange() {
-        require(amount > LOTTO_MIN_AMOUNT) {
-            "[ERROR] ${LOTTO_MIN_AMOUNT}원 이상의 금액으로 입력해 주세요. 입력값: $amount"
+        require(amount >= LOTTO_EACH_AMOUNT) {
+            "[ERROR] ${LOTTO_EACH_AMOUNT}원 이상의 금액으로 입력해 주세요. 입력값: $amount"
         }
     }
 
