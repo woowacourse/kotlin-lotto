0801ecec494a2066368d5d6939d0d826e447a50d
박지민 <pjm5489@naver.com>
2025-03-03 01:09:18 +0900

    refactor: 로또 게임 전체 흐름 재구현

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -3,9 +3,10 @@ package lotto.controller
 import lotto.model.Lotto
 import lotto.model.LottoMachine
 import lotto.model.LottoNumber
+import lotto.model.LottoQuantity
 import lotto.model.Lottos
 import lotto.model.ProfitStatus
-import lotto.model.Rank
+import lotto.model.WinningResult
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -14,78 +15,44 @@ class LottoController(
     private val outputView: OutputView,
 ) {
     fun run() {
-        val lottoMachine = generateLottoMachineByAmount()
-        val lottos = displayLottos(lottoMachine)
-
-        val lastWeekLottoWinningNumbers = inputView.readWinningNumbers()
-        val lottoBonusNumber = inputView.readBonusNumber()
-
-        val lottoWinningResult =
-            displayLottosWinningResult(
-                lottos,
-                mapToLottoNumber(lastWeekLottoWinningNumbers),
-                LottoNumber(lottoBonusNumber),
-            )
-        displayLottoWinningProfit(lottoMachine, lottoWinningResult)
-    }
-
-    private fun displayLottos(lottoMachine: LottoMachine): Lottos {
-        val activeLottoQuantity = lottoMachine.getActiveLottoQuantity()
-        outputView.printPurchaseLottoQuantity(lottoMachine.passiveLottoQuantity, activeLottoQuantity)
-        val lottos = lottoMachine.getTotalLottos(activeLottoQuantity)
-
-        lottos.getAllLottoNumbers().forEach { lottoNumbers ->
-            outputView.printLottoNumbers(lottoNumbers)
-        }
-
-        return lottos
-    }
-
-    private fun List<Set<Int>>.mapToLotto(): List<Lotto> = this.map { Lotto(mapToLottoNumber(it)) }
-
-    private fun mapToLottoNumber(lottoNumbers: Set<Int>): Set<LottoNumber> = lottoNumbers.map { LottoNumber(it) }.toSet()
-
-    private fun generateLottoMachineByAmount(): LottoMachine {
-        val purchaseAmount = inputView.readPurchaseAmount()
+        val amount = inputView.readPurchaseAmount()
         val passiveLottoQuantity = inputView.readPassivePurchaseQuantity()
-        val passiveLottoNumbers = inputView.readPassiveLottoNumbers(passiveLottoQuantity)
-        return LottoMachine(purchaseAmount, passiveLottoQuantity, passiveLottoNumbers.mapToLotto())
-    }
+        val passiveNumbers = inputView.readPassiveLottoNumbers(passiveLottoQuantity)
+        val passiveLottos = passiveNumbers.map { Lotto(it.map { number -> LottoNumber(number) }.toSet()) }
 
-    private fun displayLottosWinningResult(
-        lottos: Lottos,
-        lastWeekWinningNumbers: Set<LottoNumber>,
-        bonusNumber: LottoNumber,
-    ): Map<Rank, Int> {
-        val lottoWinningResult = lottos.countLottoByRank(lastWeekWinningNumbers, bonusNumber)
-        outputView.printWinningResultTitle()
-        lottoWinningResult.forEach { (rank, count) ->
-            displayLottoWinningResult(rank, count)
+        val lottoQuantity = LottoQuantity(amount, passiveLottoQuantity)
+        val lottoMachine = LottoMachine(lottoQuantity)
+
+        outputView.printPurchaseLottoQuantity(passiveLottoQuantity, lottoQuantity.getActiveLottoQuantity())
+
+        val activeLottos = lottoMachine.generateActiveLottos()
+        val lottos = Lottos(passiveLottos, activeLottos)
+        val allLottos = lottos.getAllLottoNumbers()
+        allLottos.forEach { lotto ->
+            outputView.printLottoNumbers(lotto)
         }
-        return lottoWinningResult
-    }
 
-    private fun displayLottoWinningResult(
-        rank: Rank,
-        count: Int,
-    ) {
-        if (rank == Rank.MISS) return
+        val winningNumbers = inputView.readWinningNumbers().mapToLotto()
+        val bonusNumber = inputView.readBonusNumber().mapToLottoNumber()
 
-        outputView.printWinningResult(
-            requiredMatch = rank.countOfMatch,
-            profit = rank.winningMoney,
-            matchBonus = rank.matchBonus,
-            countOfMatch = count,
-        )
-    }
+        outputView.printWinningResultTitle()
+        val winningResult = WinningResult(lottos, amount)
+        val countLottoByRank = winningResult.countLottoByRank(winningNumbers, bonusNumber)
+        countLottoByRank.forEach { (rank, count) ->
+            outputView.printWinningResult(
+                requiredMatch = rank.countOfMatch,
+                profit = rank.winningMoney,
+                matchBonus = rank.matchBonus,
+                countOfMatch = count,
+            )
+        }
 
-    private fun displayLottoWinningProfit(
-        lottoMachine: LottoMachine,
-        lottoWinningResult: Map<Rank, Int>,
-    ) {
-        val profitRate = lottoMachine.getProfitRate(lottoWinningResult)
+        val profitRate = winningResult.getProfitRate(winningNumbers, bonusNumber)
         val profitStatus = ProfitStatus.fromProfitStatus(profitRate)
-
         outputView.printProfitRate(profitRate, profitStatus.krDescription)
     }
+
+    private fun Int.mapToLottoNumber() = LottoNumber(this)
+
+    private fun Set<Int>.mapToLotto() = this.map { number -> number.mapToLottoNumber() }.toSet()
 }
