6ec84909c8f6310ef4ff66a6a08b489423bf50a3
DongjooSEO <readyjoo24@gmail.com>
2025-03-03 14:06:52 +0900

    fix: 변수 오탈자 수정

--- src/main/kotlin/lotto/domain/WinningLotto.kt
+++ src/main/kotlin/lotto/domain/WinningLotto.kt
@@ -2,9 +2,9 @@ package lotto.domain
 
 class WinningLotto(
     val winningLotto: Lotto,
-    val bounusNumber: LottoNumber,
+    val bonusNumber: LottoNumber,
 ) {
     init {
-        require(!winningLotto.lottoNums.contains(bounusNumber)) { "보너스 볼 번호는 당첨 번호와 중복될 수 없습니다." }
+        require(!winningLotto.lottoNums.contains(bonusNumber)) { "보너스 볼 번호는 당첨 번호와 중복될 수 없습니다." }
     }
 }
