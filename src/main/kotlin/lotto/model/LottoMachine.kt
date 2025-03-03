87abc07b7e13858de0027459f14701d65c275cff
박지민 <pjm5489@naver.com>
2025-03-03 15:21:07 +0900

    refactor: 자동 로또를 생성하는 함수 재거

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -4,18 +4,12 @@ import lotto.model.Lotto.Companion.LOTTO_NUMBER_MAX_RANGE
 import lotto.model.Lotto.Companion.LOTTO_NUMBER_MIN_RANGE
 import lotto.model.Lotto.Companion.LOTTO_NUMBER_SIZE
 
-class LottoMachine(
-    private val lottoQuantity: LottoQuantity,
-) {
-    fun generateActiveLottos(): List<Lotto> {
-        val activeLottoQuantity = lottoQuantity.getActiveLottoQuantity()
-        return List<Lotto>(activeLottoQuantity) { Lotto(getLottoNumbers().map { LottoNumber((it)) }.toSet()) }
-    }
-
-    private fun getLottoNumbers(): List<Int> {
+class LottoMachine : LottoGenerator {
+    override fun getLottoNumbers(): Lotto {
         val shuffledLottoNumbers = (LOTTO_NUMBER_MIN_RANGE..LOTTO_NUMBER_MAX_RANGE).shuffled()
-        val selectedLottoNumbers = shuffledLottoNumbers.take(LOTTO_NUMBER_SIZE)
+        val selectedLottoNumbers = shuffledLottoNumbers.take(LOTTO_NUMBER_SIZE).sorted()
+        val selectedLotto = selectedLottoNumbers.map { LottoNumber(it) }.toSet()
 
-        return selectedLottoNumbers.sorted()
+        return Lotto(selectedLotto)
     }
 }
