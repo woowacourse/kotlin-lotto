2c24f61f7a31c781d473ba8bbda2419099568555
박지민 <pjm5489@naver.com>
2025-03-03 15:27:21 +0900

    feat: 수동 로또의 개수 유효성검사 로직 추가

--- src/main/kotlin/lotto/model/LottoQuantity.kt
+++ src/main/kotlin/lotto/model/LottoQuantity.kt
@@ -6,13 +6,22 @@ class LottoQuantity(
 ) {
     init {
         validateAmountMinimumRange()
+        validatePassiveQuantity()
     }
 
-    fun getActiveLottoQuantity(): Int = amount / LOTTO_EACH_AMOUNT - passiveLottoQuantity
+    private fun getTotalLottoQuantity(): Int = amount / LOTTO_EACH_AMOUNT
+
+    fun getActiveLottoQuantity(): Int = getTotalLottoQuantity() - passiveLottoQuantity
 
     private fun validateAmountMinimumRange() {
         require(amount >= LOTTO_EACH_AMOUNT) {
             "[ERROR] ${LOTTO_EACH_AMOUNT}원 이상의 금액으로 입력해 주세요. 입력값: $amount"
+        }
+    }
+
+    private fun validatePassiveQuantity() {
+        require(getTotalLottoQuantity() >= passiveLottoQuantity) {
+            "[ERROR] 수동 로또의 수는 전체 구매 로또의 수보다 클 수 없습니다."
         }
     }
 
