a34be24a62402b0a2f4ac0fb931cb03a5029439b
HamBeomJoon <hbj0802@naver.com>
2025-03-03 14:08:06 +0900

    refactor: 수동 로또 구매 횟수 0개 미만 테스트

--- src/main/kotlin/lotto/domain/model/PurchaseCount.kt
+++ src/main/kotlin/lotto/domain/model/PurchaseCount.kt
@@ -8,6 +8,8 @@ class PurchaseCount private constructor(
         require(manualCount >= 0) { ERROR_INVALID_COUNT_RANGE }
     }
 
+    internal constructor(manualCount: Int) : this(manualCount, 0)
+
     companion object {
         private const val ERROR_INVALID_MANUAL_COUNT = "구입 금액을 넘겨서 구매할 수 없습니다."
         private const val ERROR_INVALID_COUNT_RANGE = "구매할 로또 수는 0보다 커야 합니다."
