f29e0d5ee9469d6a1150488fc14d421dbbb97959
medAndro <med4ndro@gmail.com>
2025-03-04 23:56:13 +0900

    test: LottoQuantity의 테스트 코드 작성

---
+++ src/test/kotlin/lottotest/domain/valueobject/LottoQuantityTest.kt
@@ -0,0 +1,25 @@
+package lottotest.domain.valueobject
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoQuantityTest {
+    @ParameterizedTest
+    @ValueSource(ints = [1, 123546])
+    fun `로또의 개수는 1 이상의 정수 값이다`(quantity: Int) {
+        // given when
+        val lottoQuantity = LottoQuantity(quantity)
+
+        // then
+        assertThat(lottoQuantity.quantity).isEqualTo(quantity)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, -1000])
+    fun `로또의 개수가 0 이하일 수는 없다`(quantity: Int) {
+        assertThrows<IllegalArgumentException> {
+            LottoQuantity(quantity)
+        }
+    }
+}
