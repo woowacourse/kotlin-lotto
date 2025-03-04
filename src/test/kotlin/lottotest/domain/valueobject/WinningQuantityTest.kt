47ed66d0840bd6883249aa1e94a5c636609460b9
medAndro <med4ndro@gmail.com>
2025-03-05 00:10:45 +0900

    test: WinningQuantity의 테스트 코드 작성

---
+++ src/test/kotlin/lottotest/domain/valueobject/WinningQuantityTest.kt
@@ -0,0 +1,26 @@
+package lottotest.domain.valueobject
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class WinningQuantityTest {
+    @ParameterizedTest
+    @ValueSource(ints = [0, 123546])
+    fun `당첨 수량은 0 이상의 정수 값이다`(quantity: Int) {
+        // given when
+        val winningQuantity = WinningQuantity(quantity)
+
+        // then
+        assertThat(winningQuantity.quantity).isEqualTo(quantity)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, -1000])
+    fun `당첨 수량이 음수일 수는 없다`(quantity: Int) {
+        assertThrows<IllegalArgumentException> {
+            WinningQuantity(quantity)
+        }
+    }
+}
