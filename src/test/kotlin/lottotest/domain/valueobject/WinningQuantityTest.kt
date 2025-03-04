d9cf36f8210f618966b58755728f3821b1914be5
medAndro <med4ndro@gmail.com>
2025-03-05 00:14:49 +0900

    feat: WinningQuantity 클래스 구현 코드 작성

--- src/test/kotlin/lottotest/domain/valueobject/WinningQuantityTest.kt
+++ src/test/kotlin/lottotest/domain/valueobject/WinningQuantityTest.kt
@@ -1,5 +1,6 @@
 package lottotest.domain.valueobject
 
+import lotto.domain.valueobject.WinningQuantity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
