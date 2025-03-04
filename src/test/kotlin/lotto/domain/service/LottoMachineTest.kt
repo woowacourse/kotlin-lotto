9dfb5946a87698c673cc89095bda7213d3632e44
HamBeomJoon <hbj0802@naver.com>
2025-03-04 20:22:59 +0900

    refactor: machine 테스트 함수 삭제

--- src/test/kotlin/lotto/domain/service/LottoMachineTest.kt
+++ src/test/kotlin/lotto/domain/service/LottoMachineTest.kt
@@ -1,22 +1,12 @@
 package lotto.domain.service
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
 class LottoMachineTest {
     @Test
     fun `로또 구입 금액을 로또 1장 가격으로 나눈 값이 구입 개수이다`() {
         val count = LottoMachine().calculateTotalCount(12000)
         assertThat(count).isEqualTo(12)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [1, 3, 4])
-    fun `로또 구입 개수만큼 로또를 발행한다`(count: Int) {
-        val lottoTickets = LottoMachine().generateAutoTicket(count)
-        assertEquals(lottoTickets.size, count)
     }
 }
