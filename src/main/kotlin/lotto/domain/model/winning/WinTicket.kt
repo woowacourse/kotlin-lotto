ef308c890d8500be7f6f72bf86c1ea28413c6932
medAndro <med4ndro@gmail.com>
2025-03-05 22:55:54 +0900

    feat: WinTicket에서 로또 당첨 통계정보를 구하는 코드 작성

--- src/main/kotlin/lotto/domain/model/winning/WinTicket.kt
+++ src/main/kotlin/lotto/domain/model/winning/WinTicket.kt
@@ -2,6 +2,7 @@ package lotto.domain.model.winning
 
 import lotto.domain.model.lottoticket.LottoTicket
 import lotto.domain.valueobject.LottoNumber
+import lotto.domain.valueobject.WinningQuantity
 
 class WinTicket(
     val winLottoTicket: LottoTicket,
@@ -11,6 +12,21 @@ class WinTicket(
         require(bonusNumber !in winLottoTicket.lottoNumbers) {
             ERROR_DUPLICATE_BONUS_NUMBER
         }
+    }
+
+    fun calculateWinningStatistics(boughtTickets: List<LottoTicket>): Map<Rank, WinningQuantity> {
+        val rawValueResult = boughtTickets.groupingBy { getSingleTicketRank(it) }.eachCount()
+        return rawValueResult.mapValues { WinningQuantity(it.value) }
+    }
+
+    private fun getSingleTicketRank(boughtTicket: LottoTicket): Rank {
+        val countOfMatch =
+            winLottoTicket.lottoNumbers
+                .intersect(boughtTicket.lottoNumbers)
+                .size
+        val isMatchedBonus = bonusNumber in boughtTicket.lottoNumbers
+
+        return Rank.valueOf(countOfMatch, isMatchedBonus)
     }
 
     companion object {
