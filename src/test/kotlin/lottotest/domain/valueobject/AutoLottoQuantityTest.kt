261a1c122696fbd038c5b3195cd7bf84cc9ea3db
medAndro <med4ndro@gmail.com>
2025-03-09 17:18:57 +0900

    test: 자동, 수동 로또 개수의 불필요한 테스트 제거

--- src/test/kotlin/lottotest/domain/valueobject/AutoLottoQuantityTest.kt
+++ src/test/kotlin/lottotest/domain/valueobject/AutoLottoQuantityTest.kt
@@ -4,30 +4,10 @@ import lotto.domain.valueobject.AutoLottoQuantity
 import lotto.domain.valueobject.LottoQuantity
 import lotto.domain.valueobject.ManualLottoQuantity
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class AutoLottoQuantityTest {
-    @ParameterizedTest
-    @ValueSource(ints = [0, 1, 123546])
-    fun `자동 로또 수량은 0 이상의 정수 값이다`(quantity: Int) {
-        // given when
-        val partialLottoQuantity = AutoLottoQuantity(quantity)
-
-        // then
-        assertThat(partialLottoQuantity.quantity).isEqualTo(quantity)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [-1, -1000])
-    fun `자동 로또 수량은 음수가 될 수 없다`(quantity: Int) {
-        assertThrows<IllegalArgumentException> {
-            AutoLottoQuantity(quantity)
-        }
-    }
-
     @ParameterizedTest
     @CsvSource(
         "10, 5, 5",
@@ -40,11 +20,10 @@ class AutoLottoQuantityTest {
         expectedAutoLottoQuantity: Int,
     ) {
         val lottoQuantityObject = LottoQuantity(lottoQuantity)
-        val manualLottoQuantityObject = ManualLottoQuantity(manualLottoQuantity)
-        val expectedAutoLottoQuantityObject = AutoLottoQuantity(expectedAutoLottoQuantity)
+        val manualLottoQuantityObject = ManualLottoQuantity(lottoQuantityObject, manualLottoQuantity)
 
         val actualAutoLottoQuantity = AutoLottoQuantity(lottoQuantityObject, manualLottoQuantityObject)
 
-        assertThat(actualAutoLottoQuantity).isEqualTo(expectedAutoLottoQuantityObject)
+        assertThat(actualAutoLottoQuantity.quantity).isEqualTo(expectedAutoLottoQuantity)
     }
 }
--- src/test/kotlin/lottotest/domain/valueobject/ManualLottoQuantityTest.kt
+++ src/test/kotlin/lottotest/domain/valueobject/ManualLottoQuantityTest.kt
@@ -2,43 +2,23 @@ package lottotest.domain.valueobject
 
 import lotto.domain.valueobject.LottoQuantity
 import lotto.domain.valueobject.ManualLottoQuantity
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class ManualLottoQuantityTest {
-    @ParameterizedTest
-    @ValueSource(ints = [0, 1, 123546])
-    fun `수동 로또 수량은 0 이상의 정수 값이다`(quantity: Int) {
-        // given when
-        val partialLottoQuantity = ManualLottoQuantity(quantity)
-
-        // then
-        assertThat(partialLottoQuantity.quantity).isEqualTo(quantity)
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = [-1, -1000])
-    fun `수동 로또 수량은 음수가 될 수 없다`(quantity: Int) {
-        assertThrows<IllegalArgumentException> {
-            ManualLottoQuantity(quantity)
-        }
-    }
-
     @ParameterizedTest
     @CsvSource(
         "10, 11",
         "1, 2",
     )
-    fun `수동 로또 수량은 전체 로또 수량을 넘어설 수 없다`(
-        lottoQuantity: Int,
+    fun `수동 로또 수량은 구매한 로또 수량을 넘어설 수 없다`(
+        boughtLottoQuantity: Int,
         manualLottoQuantity: Int,
     ) {
-        val lottoQuantityObject = LottoQuantity(lottoQuantity)
+        val boughtLottoQuantityObject = LottoQuantity(boughtLottoQuantity)
         assertThrows<IllegalArgumentException> {
-            ManualLottoQuantity(lottoQuantityObject, manualLottoQuantity)
+            ManualLottoQuantity(boughtLottoQuantityObject, manualLottoQuantity)
         }
     }
 }
