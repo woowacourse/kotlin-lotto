74ce74eff3c75b23429b70cf884746185ca50da6
medAndro <med4ndro@gmail.com>
2025-03-09 15:21:13 +0900

    test: 로또 번호가 1 이상의 값을 가지도록 테스트 수정

--- src/test/kotlin/lottotest/domain/valueobject/LottoQuantityTest.kt
+++ src/test/kotlin/lottotest/domain/valueobject/LottoQuantityTest.kt
@@ -8,8 +8,8 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class LottoQuantityTest {
     @ParameterizedTest
-    @ValueSource(ints = [0, 123546])
-    fun `로또 수량은 0 이상의 정수 값이다`(quantity: Int) {
+    @ValueSource(ints = [1, 123546])
+    fun `로또 수량은 1 이상의 정수 값이다`(quantity: Int) {
         // given when
         val lottoQuantity = LottoQuantity(quantity)
 
@@ -18,8 +18,8 @@ class LottoQuantityTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [-1, -1000])
-    fun `로또 수량이 음수일 수는 없다`(quantity: Int) {
+    @ValueSource(ints = [0, -1, -1000])
+    fun `로또 수량이 0 이하 일 수 없다`(quantity: Int) {
         assertThrows<IllegalArgumentException> {
             LottoQuantity(quantity)
         }
