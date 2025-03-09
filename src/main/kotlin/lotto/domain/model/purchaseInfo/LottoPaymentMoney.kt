f5b47d57918911031fd4ffb48ff717ceb7e95570
medAndro <med4ndro@gmail.com>
2025-03-09 17:53:21 +0900

    refactor: 로또 구입 금액은 최소 로또 한장의 이상의 금액인지 검사하도록 코드 수정

--- src/main/kotlin/lotto/domain/model/purchaseInfo/LottoPaymentMoney.kt
+++ src/main/kotlin/lotto/domain/model/purchaseInfo/LottoPaymentMoney.kt
@@ -8,7 +8,7 @@ value class LottoPaymentMoney(
     val money: Int,
 ) {
     init {
-        require(money >= 0) { ERROR_NEGATIVE_NUMBER }
+        require(money >= LOTTO_PRICE_UNIT) { ERROR_MINIMUM_NUMBER }
         require(money % LOTTO_PRICE_UNIT == 0) { ERROR_DIVIDE_BY_LOTTO_UNIT }
     }
 
@@ -17,7 +17,7 @@ value class LottoPaymentMoney(
     companion object {
         private const val LOTTO_PRICE_UNIT = 1000
 
-        private const val ERROR_NEGATIVE_NUMBER = "로또 구입 금액은 음수가 될 수 없습니다."
+        private const val ERROR_MINIMUM_NUMBER = "로또 구입 금액은 로또 한장보다 작을 수 없습니다."
         private const val ERROR_DIVIDE_BY_LOTTO_UNIT = "로또 구입 금액은 ${LOTTO_PRICE_UNIT}원 단위로 지불해야 합니다."
     }
 }
