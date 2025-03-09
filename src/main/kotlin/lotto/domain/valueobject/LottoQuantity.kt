329301324e19be969b25421212d8999b43194db6
medAndro <med4ndro@gmail.com>
2025-03-09 15:23:12 +0900

    refactor: 로또 수량은 1이상의 값만 가지도록 변경

--- src/main/kotlin/lotto/domain/valueobject/LottoQuantity.kt
+++ src/main/kotlin/lotto/domain/valueobject/LottoQuantity.kt
@@ -5,12 +5,12 @@ value class LottoQuantity(
     val quantity: Int,
 ) : Comparable<LottoQuantity> {
     init {
-        require(quantity >= 0) { ERROR_NEGATIVE_LOTTO_NUMBER }
+        require(quantity >= 1) { ERROR_NEGATIVE_LOTTO_NUMBER }
     }
 
     override fun compareTo(other: LottoQuantity): Int = this.quantity.compareTo(other.quantity)
 
     companion object {
-        private const val ERROR_NEGATIVE_LOTTO_NUMBER = "로또 수량은 음수가 될 수 없습니다."
+        private const val ERROR_NEGATIVE_LOTTO_NUMBER = "로또 수량은 최소 1 이상의 값이어야 합니다"
     }
 }
